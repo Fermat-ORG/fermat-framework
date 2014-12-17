68a2342f4d5da263369f23bbad0b977570731f92
Loui_Molina <loui.molina1@gmail.com>
2014-12-17 16:05:39 +0100

    Account detail all fragment and layout.

--- app/src/main/java/com/bitdubai/smartwallet/wallets/teens/AccountDetailAllFragment.java
+++ app/src/main/java/com/bitdubai/smartwallet/wallets/teens/AccountDetailAllFragment.java
@@ -11,7 +11,6 @@ import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bitdubai.smartwallet.R;
 import com.bitdubai.smartwallet.walletframework.MyApplication;
 import com.bitdubai.smartwallet.walletframework.SendToNewContactActivity;
@@ -22,8 +21,10 @@ public  class AccountDetailAllFragment extends Fragment {
 
     private static final String ARG_POSITION = "position";
 
+
     View rootView;
     ExpandableListView lv;
+    private String[] types;
     private String[] contacts;
     private String[] amounts;
     private String[] whens;
@@ -32,6 +33,8 @@ public  class AccountDetailAllFragment extends Fragment {
     private String[][] transactions;
     String[][] transactions_amounts;
     private String[][] transactions_whens;
+    private String[] debit_credit;
+
 
 
 
@@ -47,12 +50,14 @@ public  class AccountDetailAllFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        contacts = new String[]{ "Luis Fernando Molina", "Guillermo Villanueva", "Pedro Perrotta", "Mariana Duyos"};
-        amounts = new String[]{  "$325.00", "$1,400.00", "$0.50", "$25.00"};
-        whens = new String[]{  "3 min ago", "2 hours ago", "today 9:24 AM", "yesterday"};
-        notes = new String[]{  "Electricity bill", "Flat rent", "Test address", "More pictures"};
+        types = new String[]{ "Received", "Received", "sent", "Refill","sent", "Refill" , "Received", "sent","received", "Refill"};
+        contacts = new String[]{"Lucia Alarcon De Zam...", "Juan Luis R. Pons", "Luis Fernando Molina","Kalustyan´s","Guillermo Villanueva","Kings Super Market", "Karina Rodriguez", "Mariana Duyos", "Taylor Backus", "D´Agostino"};
+        amounts = new String[]{"$200.00", "$3,000.00", "$325.00", "$350.00", "$1,400.00", "$1,500.00", "$400.00"," $25.00","$600.00", "$250.00"};
+        whens = new String[]{"4 hours ago", "5 hours ago", "yesterday", "yesterday", "yesterday", "yesterday", "31 dec 14", "23 may 14", "1 year ago", "5 sep 14"};
+        notes = new String[]{"New telephone", "Old desk", "electricity bill ", "For electricity bill", "Flat rent", "for this week expenses","Car oil", "More pictures","Computer monitor", "Refill test"};
+        pictures = new String[]{"lucia_profile_picture", "juan_profile_picture", "luis_profile_picture", "refill_2", "Guillermo_profile_picture", "refill_4","karina_profile_picture","mariana_profile_picture","taylor_profile_picture","refill_1"};
+        debit_credit = new String[]{"credit", "credit","debit","credit","credit","debit","credit","debit"};
 
-        pictures = new String[]{"", "luis_profile_picture", "guillermo_profile_picture", "pedro_profile_picture", "mariana_profile_picture"};
 
         transactions = new String[][]{
 
@@ -84,7 +89,7 @@ public  class AccountDetailAllFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.wallets_teens_fragment_send_and_receive, container, false);
+        rootView = inflater.inflate(R.layout.wallets_teens_fragment_account_detail_all_list, container, false);
 
 
 
@@ -121,7 +126,7 @@ public  class AccountDetailAllFragment extends Fragment {
                     Intent intent;
                     intent = new Intent(getActivity(), SendToNewContactActivity.class);
                     startActivity(intent);
-                    return true;
+                    return false;
                 }
                 else
                 {
@@ -187,13 +192,18 @@ public  class AccountDetailAllFragment extends Fragment {
             ViewHolder holder;
             ViewHolder amount;
             ViewHolder when;
+            ViewHolder type;
 
 
             //*** Seguramente por una cuestion de performance lo hacia asi, yo lo saque para que ande el prototippo
             // if (convertView == null) {
             if (1 == 1) {
-                convertView = inf.inflate(R.layout.wallets_teens_fragment_account_detail_debits, parent, false);
+                convertView = inf.inflate(R.layout.wallets_teens_fragment_account_detail_all, parent, false);
                 holder = new ViewHolder();
+
+                type = new ViewHolder();
+                type.text = (TextView) convertView.findViewById(R.id.type);
+                type.text.setTypeface(MyApplication.getDefaultTypeface());
 
                 holder.text = (TextView) convertView.findViewById(R.id.notes);
                 holder.text.setTypeface(MyApplication.getDefaultTypeface());
@@ -227,31 +237,48 @@ public  class AccountDetailAllFragment extends Fragment {
             ViewHolder when;
             ViewHolder note;
             ImageView profile_picture;
-
+            ViewHolder type;
 
 
 
             //*** Seguramente por una cuestion de performance lo hacia asi, yo lo saque para que ande el prototippo
             // if (convertView == null) {
             if (1 == 1) {
-                convertView = inf.inflate(R.layout.wallets_teens_fragment_account_detail_credits, parent, false);
+                convertView = inf.inflate(R.layout.wallets_teens_fragment_account_detail_all, parent, false);
 
                 profile_picture = (ImageView) convertView.findViewById(R.id.profile_picture);
 
                 switch (groupPosition)
                 {
                     case 0:
-                        profile_picture.setImageResource(R.drawable.luis_profile_picture);
+                        profile_picture.setImageResource(R.drawable.lucia_profile_picture);
                         break;
                     case 1:
-                        profile_picture.setImageResource(R.drawable.guillermo_profile_picture);
+                        profile_picture.setImageResource(R.drawable.juan_profile_picture);
                         break;
                     case 2:
-                        profile_picture.setImageResource(R.drawable.pedro_profile_picture);
+                        profile_picture.setImageResource(R.drawable.luis_profile_picture);
                         break;
                     case 3:
+                        profile_picture.setImageResource(R.drawable.refill_2);
+                        break;
+                    case 4:
+                        profile_picture.setImageResource(R.drawable.guillermo_profile_picture);
+                        break;
+                    case 5:
+                        profile_picture.setImageResource(R.drawable.refill_4);
+                        break;
+                    case 6:
+                        profile_picture.setImageResource(R.drawable.karina_profile_picture);
+                        break;
+                    case 7:
                         profile_picture.setImageResource(R.drawable.mariana_profile_picture);
                         break;
+                    case 8:
+                        profile_picture.setImageResource(R.drawable.taylor_profile_picture);
+                        break;
+                    case 9:
+                        profile_picture.setImageResource(R.drawable.refill_1);
                 }
 
 
@@ -267,10 +294,14 @@ public  class AccountDetailAllFragment extends Fragment {
 
                 amount.text.setText(amounts[groupPosition].toString());
 
+                type = new ViewHolder();
+                type.text = (TextView) convertView.findViewById(R.id.type);
+                type.text.setTypeface(MyApplication.getDefaultTypeface());
+                type.text.setText(types[groupPosition].toString());
+
                 when = new ViewHolder();
                 when.text = (TextView) convertView.findViewById(R.id.when);
                 when.text.setTypeface(MyApplication.getDefaultTypeface());
-
                 when.text.setText(whens[groupPosition].toString());
 
                 note = new ViewHolder();
