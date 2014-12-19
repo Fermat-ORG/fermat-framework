9c08cb63e4cc31197521c94b29c87d3f11374350
Luis Fernando Molina <luis.molina@bitDubai.com>
2014-12-19 21:10:44 +0100

    Contacts Activity

--- app/src/main/java/com/bitdubai/smartwallet/wallets/teens/ContactsFragment.java
+++ app/src/main/java/com/bitdubai/smartwallet/wallets/teens/ContactsFragment.java
@@ -43,10 +43,10 @@ public  class ContactsFragment extends android.app.Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        contacts = new String[]{"Céline Begnis", "Guillermo Villanueva", "Juan Luis R. Pons", "Karina Rodríguez","Kimberly Brown","Lucia Alarcon De Zamacona", "Luis Fernando Molina", "Mariana Duyos", "Pedro Perrotta", "Simon Cushing","Stephanie Himonidis","Taylor Backus" };
-        countries = new String[]{"United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States","United States"};
-        states = new String[]{"FL","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY","NY"};
-        cities = new String[]{"Miami","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York","New York"};
+        contacts = new String[]{"Céline Begnis", "Kimberly Brown", "Juan Luis R. Pons", "Karina Rodríguez","Guillermo Villanueva","Lucia Alarcon De Zamacona", "Luis Fernando Molina", "Mariana Duyos", "Pedro Perrotta", "Simon Cushing","Stephanie Himonidis","Taylor Backus", "Ginny Kaltabanis","Piper Faust","Deniz Caglar","Helen Nisbet","Dea Vanagan","Tim Hunter","Madeleine Jordan","Kate Bryan","Victoria Gandit","Jennifer Johnson","Robert Wint","Kevin Helms","Teddy Truchot","Hélène Derosier","John Smith","Caroline Mignaux","Guillaume Thery","Brant Cryder","Thomas Levy","Louis Stenz" };
+        countries = new String[]{"United States","United States","United States","Canada","United States","Spain","Hungary","United States","Turkey","United States","United States","United States","Mexico","United States","Australia","Brazil","United States","United States","United Kingdom","Germany","United States","United States","United States","Hungary","United States","United States","United States","United Kingdom","United States","United States","Canda","United States",};
+        states = new String[]{"FL"       ,"CA",         "NY"      ,"AB",    "FL",  "Andalusia",   "Pest",       "NY",    "Istambul"   ,"MS",   "NY",     "UT",   "JAL",     "KY"      ,"WA",   "ES",     "NY",       "KY",     "London",  "Berlin",  "NY",      "NY",     "TX",     "Pest",     "NY",      "NV"    ,"London"   ,"NY",        "OH",      "NY"      ,"ON"      ,"NY"     ,"La",       "ON"      ,"NY"};
+        cities = new String[]{"Miami","San Francisco","Buffalo","Calgary","Orlando","Córdoba",   "Budapest",  "New York","Adalar", "Jackson","New York","Provo","Guadalajara","Lexigton","Perth","Vitoria","New York","Louisville","Sutton",  "Berlin","New York","New York","Dallas","Budapest","New York","Las Vegas","Croydon","New York","Pittsburgh","New York","Sudbury","New York","New Orleans","Sudbury","Kingston"};
 
         //pictures = new String[]{"luis_profile_picture", "guillermo_profile_picture", "pedro_profile_picture", "mariana_profile_picture"};
 
@@ -208,7 +208,7 @@ public  class ContactsFragment extends android.app.Fragment {
                         profile_picture.setImageResource(R.drawable.celine_profile_picture);
                         break;
                     case 1:
-                        profile_picture.setImageResource(R.drawable.guillermo_profile_picture);
+                        profile_picture.setImageResource(R.drawable.kimberly_profile_picture);
                         break;
                     case 2:
                         profile_picture.setImageResource(R.drawable.juan_profile_picture);
@@ -217,7 +217,7 @@ public  class ContactsFragment extends android.app.Fragment {
                         profile_picture.setImageResource(R.drawable.karina_profile_picture);
                         break;
                     case 4:
-                        profile_picture.setImageResource(R.drawable.kimberly_profile_picture);
+                        profile_picture.setImageResource(R.drawable.guillermo_profile_picture);
                         break;
                     case 5:
                         profile_picture.setImageResource(R.drawable.lucia_profile_picture);
@@ -240,6 +240,68 @@ public  class ContactsFragment extends android.app.Fragment {
                     case 11:
                         profile_picture.setImageResource(R.drawable.taylor_profile_picture);
                         break;
+                    case 12:
+                        profile_picture.setImageResource(R.drawable.ginny_profile_picture);
+                        break;
+                    case 13:
+                        profile_picture.setImageResource(R.drawable.piper_profile_picture);
+                        break;
+                    case 14:
+                        profile_picture.setImageResource(R.drawable.deniz_profile_picture);
+                        break;
+                    case 15:
+                        profile_picture.setImageResource(R.drawable.helen_profile_picture);
+                        break;
+                    case 16:
+                        profile_picture.setImageResource(R.drawable.dea_profile_picture);
+                        break;
+                    case 17:
+                        profile_picture.setImageResource(R.drawable.tim_profile_picture);
+                        break;
+                    case 18:
+                        profile_picture.setImageResource(R.drawable.madaleine_profile_picture);
+                        break;
+                    case 19:
+                        profile_picture.setImageResource(R.drawable.kate_profile_picture);
+                        break;
+                    case 20:
+                        profile_picture.setImageResource(R.drawable.victoria_profile_picture);
+                        break;
+                    case 21:
+                        profile_picture.setImageResource(R.drawable.jennifer_profile_picture);
+                        break;
+                    case 22:
+                        profile_picture.setImageResource(R.drawable.robert_profile_picture);
+                        break;
+                    case 23:
+                        profile_picture.setImageResource(R.drawable.kevin_profile_picture);
+                        break;
+                    case 24:
+                        profile_picture.setImageResource(R.drawable.teddy_profile_picture);
+                        break;
+                    case 25:
+                        profile_picture.setImageResource(R.drawable.helene_profile_picture);
+                        break;
+                    case 26:
+                        profile_picture.setImageResource(R.drawable.john_profile_picture);
+                        break;
+                    case 27:
+                        profile_picture.setImageResource(R.drawable.caroline_profile_picture);
+                        break;
+                    case 28:
+                        profile_picture.setImageResource(R.drawable.guillaume_profile_picture);
+                        break;
+                    case 29:
+                        profile_picture.setImageResource(R.drawable.brant_profile_picture);
+                        break;
+                    case 30:
+                        profile_picture.setImageResource(R.drawable.thomas_profile_picture);
+                        break;
+                    case 31:
+                        profile_picture.setImageResource(R.drawable.louis_profile_picture);
+                        break;
+
+
 
 
                 }
