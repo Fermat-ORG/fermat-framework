3b9637aaec93e68b658a8816db24f5218b518909
nattyco <natalia_veronica_c@hotmail.com>
2014-12-22 13:40:27 -0300

    Definitions of Discounts Fragment

--- app/src/main/java/com/bitdubai/smartwallet/walletframework/MonthlyDiscountsActivity.java
+++ app/src/main/java/com/bitdubai/smartwallet/walletframework/MonthlyDiscountsActivity.java
@@ -1,24 +1,43 @@
 package com.bitdubai.smartwallet.walletframework;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.bitdubai.smartwallet.R;
+import com.bitdubai.smartwallet.wallets.teens.MonthlyDiscountsFragment;
 
 public class MonthlyDiscountsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.wallet_framework_activity_monthly_discounts);
+        setContentView(R.layout.wallet_framework_activity_contacts);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new MonthlyDiscountsFragment())
+                    .commit();
+        }
+
+        // I get the action bar title id and put it on a text view in order to later change its color
+        int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        TextView abTitle = (TextView) findViewById(titleId);
+        abTitle.setTextColor(Color.WHITE);
+
+
+        PagerSlidingTabStrip  tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        ((MyApplication) this.getApplication()).setActionBarProperties(this,getWindow(),tabs, getActionBar(), getResources(),abTitle, "Monthly Discounts");
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present. Natalia Cortez
         getMenuInflater().inflate(R.menu.wallet_framework_activity_monthly_discounts_menu, menu);
         return true;
     }
@@ -37,4 +56,6 @@ public class MonthlyDiscountsActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/bitdubai/smartwallet/walletframework/WeeklyDiscountsActivity.java
+++ app/src/main/java/com/bitdubai/smartwallet/walletframework/WeeklyDiscountsActivity.java
@@ -1,24 +1,44 @@
 package com.bitdubai.smartwallet.walletframework;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.bitdubai.smartwallet.R;
+import com.bitdubai.smartwallet.wallets.teens.WeeklyDiscountsFragment;
 
 public class WeeklyDiscountsActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.wallet_framework_activity_weekly_discounts);
+        setContentView(R.layout.wallet_framework_activity_contacts);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new WeeklyDiscountsFragment())
+                    .commit();
+        }
+
+        // I get the action bar title id and put it on a text view in order to later change its color
+        int titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        TextView abTitle = (TextView) findViewById(titleId);
+        abTitle.setTextColor(Color.WHITE);
+
+
+        PagerSlidingTabStrip  tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        ((MyApplication) this.getApplication()).setActionBarProperties(this,getWindow(),tabs, getActionBar(), getResources(),abTitle, "Weekly Discounts");
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present. Natalia Cortez
         getMenuInflater().inflate(R.menu.wallet_framework_activity_weekly_discounts_menu, menu);
         return true;
     }
@@ -37,4 +57,6 @@ public class WeeklyDiscountsActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
