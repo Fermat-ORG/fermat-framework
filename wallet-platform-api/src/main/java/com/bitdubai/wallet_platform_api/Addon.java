7be45ebf4cad65c1a8f0e9264f25cfc99bb920d8
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-02-06 15:05:51 +0100

    Addon concept born

--- wallet-platform-api/src/main/java/com/bitdubai/wallet_platform_api/Addon.java
+++ wallet-platform-api/src/main/java/com/bitdubai/wallet_platform_api/Addon.java
@@ -3,5 +3,5 @@ package com.bitdubai.wallet_platform_api;
 /**
  * Created by ciencias on 2/6/15.
  */
-public interface PlatformComponent {
+public interface Addon {
 }
---
+++ wallet-platform-api/src/main/java/com/bitdubai/wallet_platform_api/layer/_6_world/WorldSubsystem.java
@@ -0,0 +1,7 @@
+package com.bitdubai.wallet_platform_api.layer._6_world;
+
+/**
+ * Created by ciencias on 2/6/15.
+ */
+public interface WorldSubsystem {
+}
