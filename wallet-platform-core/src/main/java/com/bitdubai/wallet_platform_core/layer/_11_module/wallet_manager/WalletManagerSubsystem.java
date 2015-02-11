d15a1e22a276d38bb97abf3808ea952f34888bfb
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-02-11 20:59:33 +0100

    DexException problem solved

--- wallet-platform-core/src/main/java/com/bitdubai/wallet_platform_core/layer/_11_module/wallet_manager/WalletManagerSubsystem.java
+++ wallet-platform-core/src/main/java/com/bitdubai/wallet_platform_core/layer/_11_module/wallet_manager/WalletManagerSubsystem.java
@@ -3,7 +3,6 @@ package com.bitdubai.wallet_platform_core.layer._11_module.wallet_manager;
 import com.bitdubai.wallet_platform_api.Plugin;
 import com.bitdubai.wallet_platform_api.PluginDeveloper;
 import com.bitdubai.wallet_platform_api.layer._11_module.CantStartSubsystemException;
-import com.bitdubai.wallet_platform_api.Service;
 import com.bitdubai.wallet_platform_api.layer._11_module.ModuleSubsystem;
 import com.bitdubai.wallet_platform_plugin.layer._11_module.wallet_manager.developer.bitdubai.DeveloperBitDubai;
 
