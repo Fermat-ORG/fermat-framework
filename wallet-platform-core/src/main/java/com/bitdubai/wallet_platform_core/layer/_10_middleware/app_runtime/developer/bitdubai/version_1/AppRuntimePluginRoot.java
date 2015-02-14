1e96fd9ba51782ad4d39d121c6a116722fca00f1
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-02-14 10:26:45 +0100

    App Runtime born

---
+++ wallet-platform-core/src/main/java/com/bitdubai/wallet_platform_core/layer/_10_middleware/app_runtime/developer/bitdubai/version_1/AppRuntimePluginRoot.java
@@ -0,0 +1,13 @@
+package com.bitdubai.wallet_platform_core.layer._10_middleware.app_runtime.developer.bitdubai.version_1;
+
+/**
+ * Created by ciencias on 2/14/15.
+ */
+
+/**
+ * The App Runtime is the module in charge of the UI navigation structure. A user is always at a certain point in this 
+ * structure.
+ */
+
+public class AppRuntimePluginRoot {
+}
