13d1058f033d8f5132ef14a3f64b6810eb438e20
Loui-Molina <loui.molina1@gmail.com>
2015-02-14 15:06:53 +0100

    Methods implementation in AppRuntimePluginRoot.

--- wallet-platform-core/src/main/java/com/bitdubai/wallet_platform_core/layer/_10_middleware/app_runtime/developer/bitdubai/version_1/AppRuntimePluginRoot.java
+++ wallet-platform-core/src/main/java/com/bitdubai/wallet_platform_core/layer/_10_middleware/app_runtime/developer/bitdubai/version_1/AppRuntimePluginRoot.java
@@ -4,8 +4,24 @@ package com.bitdubai.wallet_platform_core.layer._10_middleware.app_runtime.devel
  * Created by ciencias on 2/14/15.
  */
 
+import com.bitdubai.wallet_platform_api.Plugin;
+import com.bitdubai.wallet_platform_api.Service;
+import com.bitdubai.wallet_platform_api.layer._10_middleware.MiddlewareEngine;
 import com.bitdubai.wallet_platform_api.layer._10_middleware.app_runtime.*;
+import com.bitdubai.wallet_platform_api.layer._1_definition.enums.ServiceStatus;
+import com.bitdubai.wallet_platform_api.layer._2_platform_service.error_manager.DealsWithErrors;
+import com.bitdubai.wallet_platform_api.layer._2_platform_service.error_manager.ErrorManager;
+import com.bitdubai.wallet_platform_api.layer._2_platform_service.event_manager.DealsWithEvents;
+import com.bitdubai.wallet_platform_api.layer._2_platform_service.event_manager.EventHandler;
+import com.bitdubai.wallet_platform_api.layer._2_platform_service.event_manager.EventListener;
+import com.bitdubai.wallet_platform_api.layer._2_platform_service.event_manager.EventManager;
+import com.bitdubai.wallet_platform_api.layer._3_os.File_System.DealsWithPluginFileSystem;
+import com.bitdubai.wallet_platform_api.layer._3_os.File_System.PluginFileSystem;
 import com.bitdubai.wallet_platform_core.layer._10_middleware.app_runtime.developer.bitdubai.version_1.structure.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * The App Runtime is the module in charge of the UI navigation structure. A user is always at a certain point in this 
@@ -19,16 +35,112 @@ import com.bitdubai.wallet_platform_core.layer._10_middleware.app_runtime.develo
 
 
 
-public class AppRuntimePluginRoot {
+public class AppRuntimePluginRoot implements Service, MiddlewareEngine, DealsWithEvents, DealsWithErrors, DealsWithPluginFileSystem, Plugin {
 
+    /**
+     * Service Interface member variables.
+     */
+    ServiceStatus serviceStatus = ServiceStatus.CREATED;
+    List<EventListener> listenersAdded = new ArrayList<>();
 
+    /**
+     * UsesFileSystem Interface member variables.
+     */
+    PluginFileSystem pluginFileSystem;
 
+    /**
+     * DealWithEvents Interface member variables.
+     */
+    EventManager eventManager;
 
+    /**
+     * Plugin Interface member variables.
+     */
+    UUID pluginId;
 
+       @Override
+       public void start(){
+           /**
+            * I will initialize the handling of com.bitdubai.platform events.
+            */
+
+           EventListener eventListener;
+           EventHandler eventHandler;
+
+           this.serviceStatus = ServiceStatus.STARTED;
+           
+       }
+    @Override
+    public void pause(){
+        
+        this.serviceStatus = ServiceStatus.PAUSED;
+        
+    }
+
+    @Override
+    public void resume(){
+        
+        this.serviceStatus = ServiceStatus.STARTED;
+        
+    }
     
+    @Override
+    public void stop(){
+
+        /**
+         * I will remove all the listeners registered with the event manager. 
+         */
+        
+        for (EventListener eventListener : listenersAdded){
+            eventManager.removeListener(eventListener);
+        }
+        
+        listenersAdded.clear();
+        
+        this.serviceStatus = ServiceStatus.STOPPED;
+        
+    }
+
+    @Override
+    public ServiceStatus getStatus() { 
+        return this.serviceStatus;
+    }
+
+    /**
+     * UsesFileSystem Interface implementation.
+     */
+
+    @Override
+    public void setPluginFileSystem(PluginFileSystem pluginFileSystem) {
+        this.pluginFileSystem = pluginFileSystem;
+    }
+
+    /**
+     * DealWithEvents Interface implementation. 
+     */
     
+    @Override
+    public void setEventManager(EventManager eventManager) {
+        this.eventManager = eventManager;
+    }
+
+    /**
+     * DealWithErrors Interface implementation. 
+     */
     
+    @Override
+    public void setErrorManager(ErrorManager errorManager) {
+        
+    }
     
+    /**
+     * DealsWithPluginIdentity methods implmentation. 
+     */
+    
+    @Override
+    public void setId(UUID pluginId) {
+        this.pluginId = pluginId;
+    }
     
     
     /**
