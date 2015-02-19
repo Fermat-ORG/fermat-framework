3bcf1213b6bf53be134280a940bede1328c549b5
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-02-19 09:37:20 +0100

    New layer crypto born

--- wallet-platform-api/src/main/java/com/bitdubai/wallet_platform_api/layer/_9_network_service/NetworkService.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.wallet_platform_api.layer._9_network_service;
-
-/**
- * Created by ciencias on 30.12.14.
- */
-public interface NetworkService {
-}
--- wallet-platform-api/src/main/java/com/bitdubai/wallet_platform_api/layer/_9_network_service/wallet_resources/WalletResourcesManager.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.wallet_platform_api.layer._9_network_service.wallet_resources;
-
-/**
- * Created by loui on 18/02/15.
- */
-public interface WalletResourcesManager {
-}
