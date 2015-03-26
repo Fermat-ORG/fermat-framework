d25a6a397865a2fe792b6d5a1f2fd26561217963
nattyco <natalia_veronica_c@hotmail.com>
2015-03-26 15:11:16 -0300

    DatbaseTable add methods to set Filters

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/_2_os/database_system/DatabaseTable.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/_2_os/database_system/DatabaseTable.java
@@ -1,6 +1,8 @@
 package com.bitdubai.fermat_api.layer._2_os.database_system;
 
 import java.util.List;
+import java.util.UUID;
+
 import com.bitdubai.fermat_api.layer._2_os.database_system.exceptions.*;
 /**
  * Created by ciencias on 01.02.15.
@@ -27,6 +29,10 @@ public interface DatabaseTable {
     public void insertRecord (DatabaseTableRecord record) throws CantInsertRecord;
 
     public void loadToMemory() throws CantLoadTableToMemory;
+
+    public void setStringFilter(String columName, String value,DatabaseFilterType type);
+
+    public void setUUIDFilter(String columName, UUID value,DatabaseFilterType type);
 
 
 
