c2462da4627334e40d1c8505dcce816063ff8cc8
Loui-Molina <loui.molina1@gmail.com>
2015-04-05 12:11:05 -0300

    Error Manager Module

--- fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_3_platform_service/error_manager/developer/version_1/exceptions/CantStartAgentException.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.fermat_core.layer._3_platform_service.error_manager.developer.version_1.exceptions;
-
-/**
- * Created by ciencias on 4/3/15.
- */
-public class CantStartAgentException extends Exception {
-}
