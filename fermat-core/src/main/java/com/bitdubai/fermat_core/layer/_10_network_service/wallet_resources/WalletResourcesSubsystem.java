8e2507446398e373f8577c8ff0b6819affcc6ae7
Loui-Molina <loui.molina1@gmail.com>
2015-04-08 00:01:50 -0300

    Wallet resources module

--- fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_10_network_service/wallet_resources/WalletResourcesSubsystem.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_10_network_service/wallet_resources/WalletResourcesSubsystem.java
@@ -3,7 +3,7 @@ package com.bitdubai.fermat_core.layer._10_network_service.wallet_resources;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.layer._10_network_service.CantStartSubsystemException;
 import com.bitdubai.fermat_api.layer._10_network_service.NetworkSubsystem;
-import com.bitdubai.fermat_core.layer._10_network_service.wallet_resources.developer.bitdubai.DeveloperBitDubai;
+import com.bitdubai.fermat_dmp_plugin.layer._10_network_service.wallet_resources.developer.bitdubai.DeveloperBitDubai;
 
 /**
  * Created by loui on 17/02/15.
