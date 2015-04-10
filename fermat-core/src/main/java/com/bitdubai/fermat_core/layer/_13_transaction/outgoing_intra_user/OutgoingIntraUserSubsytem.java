daaf88350a20239fdbff2e52e34b2285497c4af9
Loui-Molina <loui.molina1@gmail.com>
2015-04-10 00:00:44 -0300

    Outgoing intra user module created

--- fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_13_transaction/outgoing_intra_user/OutgoingIntraUserSubsytem.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_13_transaction/outgoing_intra_user/OutgoingIntraUserSubsytem.java
@@ -3,7 +3,7 @@ package com.bitdubai.fermat_core.layer._13_transaction.outgoing_intra_user;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.layer._13_transaction.CantStartSubsystemException;
 import com.bitdubai.fermat_api.layer._13_transaction.TransactionSubsystem;
-import com.bitdubai.fermat_core.layer._13_transaction.outgoing_intra_user.developer.bitdubai.DeveloperBitDubai;
+import com.bitdubai.fermat_dmp_plugin.layer._13_transaction.outgoing_intra_user.developer.bitdubai.DeveloperBitDubai;
 
 /**
  * Created by loui on 20/02/15.
