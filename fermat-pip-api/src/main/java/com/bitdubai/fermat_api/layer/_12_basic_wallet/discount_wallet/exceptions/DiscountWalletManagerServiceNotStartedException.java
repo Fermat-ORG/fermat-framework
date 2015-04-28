d244d11c80c2df6b09e6dc1b27bd341de2c6613b
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-04-28 21:37:43 +0200

    Changes in Discount Wallet Api events and exceptions

---
+++ fermat-pip-api/src/main/java/com/bitdubai/fermat_api/layer/_12_basic_wallet/discount_wallet/exceptions/DiscountWalletManagerServiceNotStartedException.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_api.layer._12_basic_wallet.discount_wallet.exceptions;
+
+/**
+ * Created by ciencias on 3/25/15.
+ */
+public class DiscountWalletManagerServiceNotStartedException extends Exception {
+}
