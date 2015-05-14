aeba8b1928aff4c1383cc766f993ad3ec5c4c9cb
nattyco <natalia_veronica_c@hotmail.com>
2015-05-14 17:55:48 -0300

    Separated Device Connectivity and Device Location in a individual Addon

--- fermat-dmp-android-core/src/main/java/com/bitdubai/android_core/layer/_2_os/android/developer/bitdubai/version_1/AndroidOsAddonRoot.java
+++ fermat-dmp-android-core/src/main/java/com/bitdubai/android_core/layer/_2_os/android/developer/bitdubai/version_1/AndroidOsAddonRoot.java
@@ -3,10 +3,9 @@ package com.bitdubai.android_core.layer._2_os.android.developer.bitdubai.version
 import android.content.Context;
 
 import com.bitdubai.android_core.layer._2_os.android.developer.bitdubai.version_1.database_system.AndroidPlatformDatabaseSystem;
-import com.bitdubai.android_core.layer._2_os.android.developer.bitdubai.version_1.device_location.AndroidLocationManager;
 import com.bitdubai.android_core.layer._2_os.android.developer.bitdubai.version_1.file_system.AndroidPlatformFileSystem;
 import com.bitdubai.fermat_api.layer._2_os.database_system.PlatformDatabaseSystem;
-import com.bitdubai.fermat_api.layer._2_os.device_location.LocationManager;
+
 import com.bitdubai.fermat_api.layer._2_os.file_system.PlatformFileSystem;
 import com.bitdubai.fermat_api.layer._2_os.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer._2_os.file_system.PluginFileSystem;
@@ -34,7 +33,7 @@ public class AndroidOsAddonRoot implements Os {
     PluginFileSystem pluginFileSystem;
     PlatformFileSystem platformFileSystem;
     LocationSystem pluginLocationSystem;
-    LocationManager pluginLocationManager;
+
 
 
     Context context;
@@ -45,7 +44,7 @@ public class AndroidOsAddonRoot implements Os {
         this.pluginFileSystem = new AndroidPluginFileSystem();
         this.platformFileSystem = new AndroidPlatformFileSystem();
         this.platformDatabaseSystem = new AndroidPlatformDatabaseSystem();
-        this.pluginLocationManager = new AndroidLocationManager();
+
 
     }
 
@@ -79,10 +78,7 @@ public class AndroidOsAddonRoot implements Os {
         return this.pluginLocationSystem;
     }
 
-    @Override
-    public LocationManager getLocationManager(){
-        return this.pluginLocationManager;
-    }
+
 
     @Override
     public void setContext(Object context) {
@@ -93,7 +89,7 @@ public class AndroidOsAddonRoot implements Os {
         this.pluginDatabaseSystem.setContext(context);
         this.platformFileSystem.setContext(context);
         this.platformDatabaseSystem.setContext(context);
-        this.pluginLocationManager.setContext(context);
+
     }
 
 
