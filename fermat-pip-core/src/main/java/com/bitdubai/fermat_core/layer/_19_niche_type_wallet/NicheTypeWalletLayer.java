93a846d4bb9175da27aa30fe66111b0e9aa4b18a
Loui-Molina <loui.molina1@gmail.com>
2015-05-22 16:25:30 -0300

    Missing Layers added.

---
+++ fermat-pip-core/src/main/java/com/bitdubai/fermat_core/layer/_19_niche_type_wallet/NicheTypeWalletLayer.java
@@ -0,0 +1,18 @@
+package com.bitdubai.fermat_core.layer._19_niche_type_wallet;
+
+import com.bitdubai.fermat_api.layer.CantStartLayerException;
+import com.bitdubai.fermat_api.layer.PlatformLayer;
+
+/**
+ * Created by loui on 21/05/15.
+ */
+public class NicheTypeWalletLayer implements PlatformLayer {
+
+
+
+
+    @Override
+    public void start() throws CantStartLayerException {
+
+    }
+}
---
+++ fermat-pip-core/src/main/java/com/bitdubai/fermat_core/layer/_8_cypto_vault/CryptoVaultLayer.java
@@ -0,0 +1,18 @@
+package com.bitdubai.fermat_core.layer._8_cypto_vault;
+
+import com.bitdubai.fermat_api.layer.CantStartLayerException;
+import com.bitdubai.fermat_api.layer.PlatformLayer;
+
+/**
+ * Created by loui on 20/05/15.
+ */
+public class CryptoVaultLayer implements PlatformLayer {
+
+
+
+
+    @Override
+    public void start() throws CantStartLayerException {
+
+    }
+}
