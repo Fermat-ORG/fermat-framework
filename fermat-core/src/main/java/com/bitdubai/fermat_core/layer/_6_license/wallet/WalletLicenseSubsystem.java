8d90b4ead4e6bab21655fba8377ff0488e4ab443
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-06-15 12:15:39 +0200

    License addon  Refactored - Compiling

--- fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_6_license/wallet/WalletLicenseSubsystem.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/layer/_6_license/wallet/WalletLicenseSubsystem.java
@@ -3,7 +3,7 @@ package com.bitdubai.fermat_core.layer._6_license.wallet;
 import com.bitdubai.fermat_api.Addon;
 import com.bitdubai.fermat_api.layer._6_license.CantStartSubsystemException;
 import com.bitdubai.fermat_api.layer._6_license.LicenseSubsystem;
-import com.bitdubai.fermat_dmp_plugin.layer._6_license.wallet.developer.DeveloperBitDubai;
+import com.bitdubai.fermat_dmp_plugin.layer.license.wallet.developer.DeveloperBitDubai;
 
 /**
  * Created by ciencias on 21.01.15.
