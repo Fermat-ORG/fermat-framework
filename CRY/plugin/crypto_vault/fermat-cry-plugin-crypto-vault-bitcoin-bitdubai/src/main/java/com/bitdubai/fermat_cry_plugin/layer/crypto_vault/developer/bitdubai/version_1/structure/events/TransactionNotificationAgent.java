c47a32655229a0aa22347651832366e2fc15af12
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-06-18 19:13:19 +0200

    added files to merge

--- CRY/plugin/crypto_vault/fermat-cry-plugin-crypto-vault-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_vault/developer/bitdubai/version_1/structure/events/TransactionNotificationAgent.java
+++ CRY/plugin/crypto_vault/fermat-cry-plugin-crypto-vault-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_vault/developer/bitdubai/version_1/structure/events/TransactionNotificationAgent.java
@@ -3,72 +3,122 @@ package com.bitdubai.fermat_cry_plugin.layer.crypto_vault.developer.bitdubai.ver
 import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.Service;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.dmp_world.Agent;
+import com.bitdubai.fermat_api.layer.dmp_world.wallet.exceptions.CantStartAgentException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.ErrorManager;
 
 /**
  * Created by rodrigo on 2015.06.18..
  */
-public class TransactionNotificationAgent implements DealsWithErrors, Service {
+public class TransactionNotificationAgent implements Agent, DealsWithErrors, DealsWithPluginDatabaseSystem{
 
-    /**
+        /**
      * TransactionNotificationAgent variables
      */
     Database database;
+
+    /**
+     * Agent interface member variables
+     */
+    Thread agentThread;
+    MonitorAgent monitorAgent;
+
+    /**
+     * DealsWithPluginDatabaseSystem interface member variable
+     */
+    PluginDatabaseSystem pluginDatabaseSystem;
+
     /**
      * DealsWithErrors interface member variables
      */
     ErrorManager errorManager;
-    ServiceStatus serviceStatus = ServiceStatus.CREATED;
-
-    /**
-     * Constructor
-     * @param database
-     */
-    public TransactionNotificationAgent (Database database){
-        this.database = database;
-    }
 
 
     /**
-     * DealsWithErrors interface implementation
+     * Agent interface implementation
+     * @throws CantStartAgentException
      */
     @Override
-    public void setErrorManager(ErrorManager errorManager) {
-        this.errorManager = errorManager;
-    }
+    public void start() throws CantStartAgentException {
+        monitorAgent = new MonitorAgent();
 
-    /**
-     * Service interface implementation
-     * @throws CantStartPluginException
-     */
-    @Override
-    public void start() throws CantStartPluginException {
+        ((DealsWithPluginDatabaseSystem) this.monitorAgent).setPluginDatabaseSystem(this.pluginDatabaseSystem);
+        ((DealsWithErrors) this.monitorAgent).setErrorManager(this.errorManager);
 
-        serviceStatus = ServiceStatus.STARTED;
-    }
-
-    @Override
-    public void pause() {
-
-        serviceStatus = ServiceStatus.PAUSED;
-    }
-
-    @Override
-    public void resume() {
-
-        serviceStatus = ServiceStatus.STARTED;
+        this.agentThread = new Thread(monitorAgent);
+        this.agentThread.start();
     }
 
     @Override
     public void stop() {
 
-        serviceStatus = ServiceStatus.STOPPED;
     }
 
+    /**
+     * DealsWithPluginDatabaseSystem interfaz implementation
+     */
     @Override
-    public ServiceStatus getStatus() {
-        return serviceStatus;
+    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+        this.pluginDatabaseSystem = pluginDatabaseSystem;
     }
+
+    /**
+     * DealsWithErrors interface implementation
+     */
+     @Override
+    public void setErrorManager(ErrorManager errorManager) {
+        this.errorManager = errorManager;
+    }
+
+    /**
+     * Private class which implements runnable and is started by the Agent
+     */
+    private class MonitorAgent implements DealsWithPluginDatabaseSystem, DealsWithErrors, Runnable{
+
+        /**
+         * how often I will search for transactions to notify
+         */
+        public final int SLEEP_TIME = 10000;
+
+        /**
+         * PluginDatabaseSystem interfaz member variables
+         */
+        PluginDatabaseSystem pluginDatabaseSystem;
+
+        /**
+         * DealsWithErrors interfaz member variables
+         */
+        ErrorManager errorManager;
+
+        /**
+         * DealsWithErrors interface implementation
+         * @param errorManager
+         */
+        @Override
+        public void setErrorManager(ErrorManager errorManager) {
+            this.errorManager = errorManager;
+        }
+
+        @Override
+        public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+            this.pluginDatabaseSystem = pluginDatabaseSystem;
+        }
+
+        @Override
+        public void run() {
+            doTheMainTask();
+        }
+    }
+
+    /**
+     * Implements the agent
+     */
+    private void doTheMainTask() {
+
+    }
+
 }
