e87a2ea4e8005d3fbca6fd61bfbb107be0a56310
lnacosta <laion.cj91@gmail.com>
2015-06-18 08:52:05 +0200

    Removed implementation of Manager

--- CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-actor-address-book-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_module/actor_address_book/developer/bitdubai/version_1/ActorAddressBookPluginRoot.java
+++ CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-actor-address-book-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_module/actor_address_book/developer/bitdubai/version_1/ActorAddressBookPluginRoot.java
@@ -1,11 +1,14 @@
 package com.bitdubai.fermat_cry_plugin.layer.crypto_module.actor_address_book.developer.bitdubai.version_1;
 
+import com.bitdubai.fermat_api.DealsWithPluginIdentity;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Actors;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantGetActorCryptoAddress;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantRegisterActorCryptoAddress;
+import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.ActorAddressBook;
+import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantGetActorAddressBook;
+import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantRegisterActorAddressBook;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.DealsWithErrors;
@@ -14,7 +17,6 @@ import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.DealsWit
 import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventHandler;
 import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventListener;
 import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventManager;
-import com.bitdubai.fermat_api.layer.pip_user.UserTypes;
 import com.bitdubai.fermat_cry_api.layer.crypto_module.Crypto;
 import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.ActorAddressBookManager;
 import com.bitdubai.fermat_cry_plugin.layer.crypto_module.actor_address_book.developer.bitdubai.version_1.structure.ActorAddressBookDao;
@@ -33,30 +35,23 @@ import java.util.UUID;
  * * * * * *
  */
 
-public class ActorAddressBookPluginRoot implements ActorAddressBookManager, Crypto,DealsWithPluginDatabaseSystem,DealsWithErrors,DealsWithEvents, Plugin, Service {
-
-    /**
-     * ActorAddressBookManager Interface member variables.
-     */
-    private ActorAddressBookDao actorAddressBook;
-
-
-    /**
-     * DealsWithPluginDatabaseSystem Interface member variables.
-     */
-    PluginDatabaseSystem pluginDatabaseSystem;
-
+public class ActorAddressBookPluginRoot implements DealsWithErrors, DealsWithEvents, DealsWithPluginDatabaseSystem, Plugin, Service {
 
     /**
      * DealsWithErrors Interface member variables.
      */
     ErrorManager errorManager;
 
+
     /**
      * DealWithEvents Interface member variables.
      */
     EventManager eventManager;
 
+    /**
+     * DealsWithPluginDatabaseSystem Interface member variables.
+     */
+    PluginDatabaseSystem pluginDatabaseSystem;
 
     /**
      * Plugin Interface member variables.
@@ -69,43 +64,6 @@ public class ActorAddressBookPluginRoot implements ActorAddressBookManager, Cryp
     ServiceStatus serviceStatus = ServiceStatus.CREATED;
     List<EventListener> listenersAdded = new ArrayList<>();
 
-    /**
-     * DealsWithPluginDatabaseSystem interface implementation.
-     */
-    @Override
-    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
-        this.pluginDatabaseSystem = pluginDatabaseSystem;
-    }
-
-
-
-    /**
-     *DealWithErrors Interface implementation.
-     */
-
-    @Override
-    public void setErrorManager(ErrorManager errorManager) {
-        this.errorManager = errorManager;
-    }
-
-    /**
-     * DealWithEvents Interface implementation.
-     */
-
-    @Override
-    public void setEventManager(EventManager eventManager) {
-        this.eventManager = eventManager;
-    }
-
-
-    /**
-     * Plugin methods implementation.
-     */
-
-    @Override
-    public void setId(UUID pluginId) {
-        this.pluginId = pluginId;
-    }
 
     /**
      * Service Interface implementation.
@@ -113,32 +71,21 @@ public class ActorAddressBookPluginRoot implements ActorAddressBookManager, Cryp
 
     @Override
     public void start() {
-        /**
-         * I will initialize the handling of com.bitdubai.platform events.
-         */
-
-        this.actorAddressBook = new ActorAddressBookDao(this.pluginId,this.errorManager);
-
-        this.actorAddressBook.setPluginDatabaseSystem(this.pluginDatabaseSystem);
-
 
         EventListener eventListener;
         EventHandler eventHandler;
 
         this.serviceStatus = ServiceStatus.STARTED;
-
     }
 
     @Override
     public void pause() {
-
         this.serviceStatus = ServiceStatus.PAUSED;
 
     }
 
     @Override
     public void resume() {
-
         this.serviceStatus = ServiceStatus.STARTED;
 
     }
@@ -165,32 +112,35 @@ public class ActorAddressBookPluginRoot implements ActorAddressBookManager, Cryp
         return this.serviceStatus;
     }
 
+    /**
+     *DealWithErrors Interface implementation.
+     */
+    @Override
+    public void setErrorManager(ErrorManager errorManager) {
+        this.errorManager = errorManager;
+    }
 
     /**
-     * Address Book Manager implementation.
+     * DealWithEvents Interface implementation.
      */
-
     @Override
-
-    public com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.ActorAddressBook getActorAddressBookByCryptoAddress(CryptoAddress cryptoAddress) throws CantGetActorCryptoAddress {
-        return actorAddressBook.getActorAddressBookByCryptoAddress(cryptoAddress);
+    public void setEventManager(EventManager eventManager) {
+        this.eventManager = eventManager;
     }
 
+    /**
+     * DealsWithPluginDatabaseSystem interface implementation.
+     */
     @Override
-    public List<com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.ActorAddressBook>  getAllActorAddressBookByUserId(UUID userId) throws CantGetActorCryptoAddress {
-        return actorAddressBook.getAllActorAddressBookByUserId(userId);
+    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+        this.pluginDatabaseSystem = pluginDatabaseSystem;
     }
 
-
+    /**
+     * DealsWithPluginIdentity methods implementation.
+     */
     @Override
-    public void registerActorCryptoAddress (UserTypes userType, UUID userId,CryptoAddress cryptoAddress)throws CantRegisterActorCryptoAddress {
-
-        /**
-         * Here I create the Address book record for new user.
-         */
-        actorAddressBook.registerActorCryptoAddress( userType, userId, cryptoAddress);
-
+    public void setId(UUID pluginId) {
+        this.pluginId = pluginId;
     }
-
-
 }
