5b4b5aad007241163acf28e36c1031ce4487f933
lnacosta <laion.cj91@gmail.com>
2015-06-18 12:48:15 +0200

    Created interface DealsWithWalletCryptoAddressBook: indicates that the plugin needs the functionality of a WalletCryptoAddressBookManager.

---
+++ fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_module/wallet_address_book/DealsWithWalletCryptoAddressBook.java
@@ -0,0 +1,14 @@
+package com.bitdubai.fermat_cry_api.layer.crypto_module.wallet_address_book;
+
+/**
+ * The interface <code>com.bitdubai.fermat_api.layer.crypto_module.wallet_address_book.DealsWithWalletCryptoAddressBook</code>
+ * indicates that the plugin needs the functionality of a WalletCryptoAddressBookManager.
+ *
+ * Created by Leon Acosta - (laion.cj91@gmail.com) on 18/06/15.
+ * @version 1.0
+ */
+public interface DealsWithWalletCryptoAddressBook {
+
+    void setWalletCryptoAddressBookManager(WalletCryptoAddressBookManager walletCryptoAddressBookManager);
+
+}
