8594b68343687c9cde538bf1752efe424e0a14b3
lnacosta <laion.cj91@gmail.com>
2015-06-19 11:24:00 +0200

    changed name of class for understanding

--- fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_module/wallet_address_book/interfaces/WalletAddressBookRegistry.java
+++ fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_module/wallet_address_book/interfaces/WalletAddressBookRegistry.java
@@ -1,4 +1,4 @@
-package com.bitdubai.fermat_cry_api.layer.crypto_module.wallet_address_book;
+package com.bitdubai.fermat_cry_api.layer.crypto_module.wallet_address_book.interfaces;
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.PlatformWalletType;
 import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
@@ -9,17 +9,17 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * The interface <code>com.bitdubai.fermat_api.layer.crypto_module.wallet_address_book.WalletCryptoAddressBookManager</code>
- * indicates the functionality of a WalletCryptoAddressBookManager.
+ * The interface <code>com.bitdubai.fermat_api.layer.crypto_module.wallet_address_book.interfaces.WalletAddressBookRegistry</code>
+ * indicates the functionality of a WalletAddressBookRegistry.
  *
  * Created by Leon Acosta - (laion.cj91@gmail.com) on 18/06/15.
  * @version 1.0
  */
-public interface WalletCryptoAddressBookManager {
+public interface WalletAddressBookRegistry {
 
-    WalletCryptoAddressBook getWalletCryptoAddressBookByCryptoAddress(CryptoAddress cryptoAddress) throws CantGetWalletCryptoAddressBookException;
+    WalletAddressBookRecord getWalletCryptoAddressBookByCryptoAddress(CryptoAddress cryptoAddress) throws CantGetWalletCryptoAddressBookException;
 
-    List<WalletCryptoAddressBook> getAllWalletCryptoAddressBookByWalletId(UUID walletId) throws CantGetWalletCryptoAddressBookException;
+    List<WalletAddressBookRecord> getAllWalletCryptoAddressBookByWalletId(UUID walletId) throws CantGetWalletCryptoAddressBookException;
 
     void registerWalletCryptoAddressBook(CryptoAddress cryptoAddress, PlatformWalletType walletType, UUID walletId) throws CantRegisterWalletCryptoAddressBookException;
 
