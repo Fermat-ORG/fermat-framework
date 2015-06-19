c758afb3e91a69ec13ebf160e2c0980d5892d9fe
lnacosta <laion.cj91@gmail.com>
2015-06-19 11:51:45 +0200

    Added manager implementation and refactored

--- CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-actor-address-book-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_module/actor_address_book/developer/bitdubai/version_1/ActorAddressBookPluginRoot.java
+++ CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-actor-address-book-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_module/actor_address_book/developer/bitdubai/version_1/ActorAddressBookPluginRoot.java
@@ -1,32 +1,24 @@
 package com.bitdubai.fermat_cry_plugin.layer.crypto_module.actor_address_book.developer.bitdubai.version_1;
 
-import com.bitdubai.fermat_api.DealsWithPluginIdentity;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
-import com.bitdubai.fermat_api.layer.all_definition.enums.Actors;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
-import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.ActorAddressBook;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantGetActorAddressBook;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantRegisterActorAddressBook;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.ErrorManager;
-import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.DealsWithEvents;
-import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventHandler;
-import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventListener;
-import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventManager;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.Crypto;
-import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.ActorAddressBookManager;
-import com.bitdubai.fermat_cry_plugin.layer.crypto_module.actor_address_book.developer.bitdubai.version_1.structure.ActorAddressBookDao;
+import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.exceptions.CantGetActorAddressBookRegistryException;
+import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.interfaces.ActorAddressBookManager;
+import com.bitdubai.fermat_cry_api.layer.crypto_module.actor_address_book.interfaces.ActorAddressBookRegistry;
+import com.bitdubai.fermat_cry_plugin.layer.crypto_module.actor_address_book.developer.bitdubai.version_1.exceptions.CantInitializeActorAddressBookException;
+import com.bitdubai.fermat_cry_plugin.layer.crypto_module.actor_address_book.developer.bitdubai.version_1.structure.ActorCryptoAddressBookRegistry;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 /**
- * Created by loui on 20/02/15.
+ * Created by loui on 20/02/15.asd
  */
 
 /**
@@ -35,18 +27,12 @@ import java.util.UUID;
  * * * * * *
  */
 
-public class ActorAddressBookPluginRoot implements DealsWithErrors, DealsWithEvents, DealsWithPluginDatabaseSystem, Plugin, Service {
+public class ActorAddressBookPluginRoot implements ActorAddressBookManager, DealsWithErrors, DealsWithPluginDatabaseSystem, Plugin, Service {
 
     /**
      * DealsWithErrors Interface member variables.
      */
     ErrorManager errorManager;
-
-
-    /**
-     * DealWithEvents Interface member variables.
-     */
-    EventManager eventManager;
 
     /**
      * DealsWithPluginDatabaseSystem Interface member variables.
@@ -62,8 +48,30 @@ public class ActorAddressBookPluginRoot implements DealsWithErrors, DealsWithEve
      * Service Interface member variables.
      */
     ServiceStatus serviceStatus = ServiceStatus.CREATED;
-    List<EventListener> listenersAdded = new ArrayList<>();
 
+
+    /**
+     * ActorAddressBookManager Interface implementation.
+     */
+    @Override
+    public ActorAddressBookRegistry getActorAddressBookRegistry() throws CantGetActorAddressBookRegistryException {
+        /**
+         * I created instance of WalletCryptoAddressBookRegistry
+         */
+        ActorCryptoAddressBookRegistry actorCryptoAddressBookRegistry = new ActorCryptoAddressBookRegistry();
+
+        actorCryptoAddressBookRegistry.setErrorManager(this.errorManager);
+        actorCryptoAddressBookRegistry.setPluginDatabaseSystem(this.pluginDatabaseSystem);
+        actorCryptoAddressBookRegistry.setPluginId(this.pluginId);
+
+        try {
+            actorCryptoAddressBookRegistry.initialize();
+        } catch (CantInitializeActorAddressBookException cantInitializeActorCryptoAddressBookException) {
+            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_USER_ADDRESS_BOOK_CRYPTO, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, cantInitializeActorCryptoAddressBookException);
+            throw new CantGetActorAddressBookRegistryException();
+        }
+        return actorCryptoAddressBookRegistry;
+    }
 
     /**
      * Service Interface implementation.
@@ -71,10 +79,6 @@ public class ActorAddressBookPluginRoot implements DealsWithErrors, DealsWithEve
 
     @Override
     public void start() {
-
-        EventListener eventListener;
-        EventHandler eventHandler;
-
         this.serviceStatus = ServiceStatus.STARTED;
     }
 
@@ -92,19 +96,7 @@ public class ActorAddressBookPluginRoot implements DealsWithErrors, DealsWithEve
 
     @Override
     public void stop() {
-
-
-        /**
-         * I will remove all the event listeners registered with the event manager.
-         */
-
-        for (EventListener eventListener : listenersAdded) {
-            eventManager.removeListener(eventListener);
-        }
-
-        listenersAdded.clear();
         this.serviceStatus = ServiceStatus.STOPPED;
-
     }
 
     @Override
@@ -118,14 +110,6 @@ public class ActorAddressBookPluginRoot implements DealsWithErrors, DealsWithEve
     @Override
     public void setErrorManager(ErrorManager errorManager) {
         this.errorManager = errorManager;
-    }
-
-    /**
-     * DealWithEvents Interface implementation.
-     */
-    @Override
-    public void setEventManager(EventManager eventManager) {
-        this.eventManager = eventManager;
     }
 
     /**
