b89aa10252c9af4d1ebc765d399f29b7ebf514e0
Luis Fernando Molina <luis.molina@bitDubai.com>
2015-06-25 12:20:47 +0200

    Developer Plugin - ALL Interfaces Defined | Compiling & Running

---
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/developer/DeveloperDatabase.java
@@ -0,0 +1,12 @@
+package com.bitdubai.fermat_api.layer.all_definition.developer;
+
+/**
+ * Created by ciencias on 6/25/15.
+ */
+public interface DeveloperDatabase {
+
+    public String getName();
+
+    public String getId();
+
+}
