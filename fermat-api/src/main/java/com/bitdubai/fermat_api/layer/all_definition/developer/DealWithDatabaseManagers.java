668a199b20c3e5a4e0dbbda5c7aef54708b5e9bc
furszy <mati_fur@hotmail.com>
2015-06-25 17:46:11 -0300

    first plugin Mati ActorDeveloper

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/developer/DealWithDatabaseManagers.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/developer/DealWithDatabaseManagers.java
@@ -7,13 +7,12 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ciencias on 6/25/15.
  */
 public interface DealWithDatabaseManagers {
 
-    public void setDatabaseManagers (Map<Plugins,Plugin> databaseManagersOnPlugins,Map<Addons,Addon> databaseManagersOnAddons);
+    public void setDatabaseManagers (HashMap<Plugins,Plugin> databaseLstPlugins,HashMap<Addons,Addon> databaseLstAddonds);
 
 }
