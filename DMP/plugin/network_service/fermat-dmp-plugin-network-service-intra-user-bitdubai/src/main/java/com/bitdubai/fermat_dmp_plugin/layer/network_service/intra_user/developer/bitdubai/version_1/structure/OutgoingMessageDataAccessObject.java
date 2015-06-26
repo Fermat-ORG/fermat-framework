351613135daed4d3a8675f12514c96e2acfbaaaa
Roberto Requena <rart3001@gmail.com>
2015-06-26 07:53:53 -0430

    Remove boolean return and add data base exceptions

--- DMP/plugin/network_service/fermat-dmp-plugin-network-service-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/network_service/intra_user/developer/bitdubai/version_1/structure/OutgoingMessageDataAccessObject.java
+++ DMP/plugin/network_service/fermat-dmp-plugin-network-service-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/network_service/intra_user/developer/bitdubai/version_1/structure/OutgoingMessageDataAccessObject.java
@@ -20,6 +20,9 @@ import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.Cant
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseTransactionFailedException;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_dmp_plugin.layer.network_service.intra_user.developer.bitdubai.version_1.exceptions.CantDeleteRecordDataBaseException;
+import com.bitdubai.fermat_dmp_plugin.layer.network_service.intra_user.developer.bitdubai.version_1.exceptions.CantInsertRecordDataBaseException;
+import com.bitdubai.fermat_dmp_plugin.layer.network_service.intra_user.developer.bitdubai.version_1.exceptions.CantUpdateRecordDataBaseException;
 import com.bitdubai.fermat_p2p_api.layer.p2p_communication.MessagesStatus;
 import com.bitdubai.fermat_p2p_api.layer.p2p_communication.MessagesTypes;
 
@@ -344,7 +347,7 @@ public class OutgoingMessageDataAccessObject {
      *
      *  @param entity OutgoingIntraUserNetworkServiceMessage to create.
      */
-    public boolean create (OutgoingIntraUserNetworkServiceMessage entity){
+    public void create (OutgoingIntraUserNetworkServiceMessage entity) throws CantInsertRecordDataBaseException {
 
         if (entity == null){
             throw new IllegalArgumentException("The entity is required, can not be null");
@@ -367,10 +370,9 @@ public class OutgoingMessageDataAccessObject {
         } catch (DatabaseTransactionFailedException databaseTransactionFailedException) {
             // Register the failure.
             errorManager.reportUnexpectedPluginException (Plugins.BITDUBAI_USER_NETWORK_SERVICE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, databaseTransactionFailedException);
-            return  Boolean.FALSE;
+            throw new CantInsertRecordDataBaseException();
         }
 
-        return Boolean.FALSE;
     }
 
     /**
@@ -378,7 +380,7 @@ public class OutgoingMessageDataAccessObject {
      *
      *  @param entity OutgoingIntraUserNetworkServiceMessage to update.
      */
-    public boolean update(OutgoingIntraUserNetworkServiceMessage entity){
+    public void update(OutgoingIntraUserNetworkServiceMessage entity) throws CantUpdateRecordDataBaseException {
 
         if (entity == null){
             throw new IllegalArgumentException("The entity is required, can not be null");
@@ -401,10 +403,9 @@ public class OutgoingMessageDataAccessObject {
         } catch (DatabaseTransactionFailedException databaseTransactionFailedException) {
             // Register the failure.
             errorManager.reportUnexpectedPluginException (Plugins.BITDUBAI_USER_NETWORK_SERVICE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, databaseTransactionFailedException);
-            return  Boolean.FALSE;
+            throw  new CantUpdateRecordDataBaseException();
         }
 
-        return Boolean.FALSE;
     }
 
     /**
@@ -412,7 +413,7 @@ public class OutgoingMessageDataAccessObject {
      *
      *  @param id Long id.
      * */
-    public boolean delete (Long id){
+    public void delete (Long id) throws CantDeleteRecordDataBaseException {
 
         if (id == null){
             throw new IllegalArgumentException("The id is required can not be null");
@@ -432,10 +433,9 @@ public class OutgoingMessageDataAccessObject {
         } catch (DatabaseTransactionFailedException databaseTransactionFailedException) {
             // Register the failure.
             errorManager.reportUnexpectedPluginException (Plugins.BITDUBAI_USER_NETWORK_SERVICE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, databaseTransactionFailedException);
-            return  Boolean.FALSE;
+            throw new CantDeleteRecordDataBaseException();
         }
 
-        return Boolean.FALSE;
     }
 
 
