dc374426c434d0603c19bdb973b4f11531d4aefd
lnacosta <laion.cj91@gmail.com>
2015-06-29 14:57:30 +0200

    Implemented DeveloperDatabaseManager for Wallet Address Book plugin

--- CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-wallet-address-book-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_module/wallet_address_book/developer/bitdubai/version_1/WalletAddressBookCryptoModulePluginRoot.java
+++ CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-wallet-address-book-bitdubai/src/main/java/com/bitdubai/fermat_cry_plugin/layer/crypto_module/wallet_address_book/developer/bitdubai/version_1/WalletAddressBookCryptoModulePluginRoot.java
@@ -3,6 +3,11 @@ package com.bitdubai.fermat_cry_plugin.layer.crypto_module.wallet_address_book.d
 import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DatabaseManagerForDevelopers;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabase;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTable;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTableRecord;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
@@ -15,8 +20,10 @@ import com.bitdubai.fermat_cry_api.layer.crypto_module.wallet_address_book.excep
 import com.bitdubai.fermat_cry_api.layer.crypto_module.wallet_address_book.interfaces.WalletAddressBookManager;
 import com.bitdubai.fermat_cry_api.layer.crypto_module.wallet_address_book.interfaces.WalletAddressBookRegistry;
 import com.bitdubai.fermat_cry_plugin.layer.crypto_module.wallet_address_book.developer.bitdubai.version_1.exceptions.CantInitializeWalletAddressBookCryptoModuleException;
+import com.bitdubai.fermat_cry_plugin.layer.crypto_module.wallet_address_book.developer.bitdubai.version_1.structure.WalletAddressBookCryptoModuleDeveloperDatabaseFactory;
 import com.bitdubai.fermat_cry_plugin.layer.crypto_module.wallet_address_book.developer.bitdubai.version_1.structure.WalletAddressBookCryptoModuleRegistry;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -29,8 +36,66 @@ import java.util.UUID;
  * * * * * *
  */
 
-public class WalletAddressBookCryptoModulePluginRoot implements Crypto, DealsWithErrors, DealsWithPluginDatabaseSystem, Plugin, Service, WalletAddressBookManager {
+public class WalletAddressBookCryptoModulePluginRoot implements Crypto, DatabaseManagerForDevelopers, DealsWithErrors, DealsWithPluginDatabaseSystem, Plugin, Service, WalletAddressBookManager {
 
+
+    /**
+     * DatabaseManagerForDevelopers interface implementation
+     * Returns the list of databases implemented on this plug in.
+     */
+    @Override
+    public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
+        WalletAddressBookCryptoModuleDeveloperDatabaseFactory dbFactory = new WalletAddressBookCryptoModuleDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        List<DeveloperDatabase> developerDatabaseList = null;
+        try {
+            dbFactory.initializeDatabase();
+            developerDatabaseList = dbFactory.getDatabaseList(developerObjectFactory);
+        } catch (Exception e) {
+            System.out.println("******* Error trying to get database list for plugin Wallet Contacts");
+        }
+        return developerDatabaseList;
+    }
+
+    /**
+     * returns the list of tables for the given database
+     *
+     * @param developerObjectFactory
+     * @param developerDatabase
+     * @return
+     */
+    @Override
+    public List<DeveloperDatabaseTable> getDatabaseTableList(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase) {
+        WalletAddressBookCryptoModuleDeveloperDatabaseFactory dbFactory = new WalletAddressBookCryptoModuleDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        List<DeveloperDatabaseTable> developerDatabaseTableList = null;
+        try {
+            dbFactory.initializeDatabase();
+            developerDatabaseTableList = dbFactory.getDatabaseTableList(developerObjectFactory);
+        } catch (Exception e) {
+            System.out.println("******* Error trying to get database table list for plugin Wallet Contacts");
+        }
+        return developerDatabaseTableList;
+    }
+
+    /**
+     * returns the list of records for the passed table
+     *
+     * @param developerObjectFactory
+     * @param developerDatabase
+     * @param developerDatabaseTable
+     * @return
+     */
+    @Override
+    public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
+        WalletAddressBookCryptoModuleDeveloperDatabaseFactory dbFactory = new WalletAddressBookCryptoModuleDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        List<DeveloperDatabaseTableRecord> developerDatabaseTableRecordList = null;
+        try {
+            dbFactory.initializeDatabase();
+            developerDatabaseTableRecordList = dbFactory.getDatabaseTableContent(developerObjectFactory, developerDatabaseTable);
+        } catch (Exception e) {
+            System.out.println("******* Error trying to get database table list for plugin Wallet Contacts");
+        }
+        return developerDatabaseTableRecordList;
+    }
     /**
      * DealsWithPluginDatabaseSystem Interface member variables.
      */
