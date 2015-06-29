60698a53bb5491d9e4227ed88623fbcd29e0644f
jorgeejgonzalez <jorgegonzalez@gmail.com>
2015-06-29 18:14:37 +0200

    changed the minSdk to the 16 version so that we can use the SQLiteDatabase.deleteDatabase method

---
+++ OSA/addon/android/fermat-osa-addon-android-database-system-bitdubai/src/test/java/unit/com/bitdubai/fermat_osa_addon/layer/android/database_system/developer/bitdubai/version_1/structure/AndroidDatabase/FileStorageHelper.java
@@ -0,0 +1,7 @@
+package unit.com.bitdubai.fermat_osa_addon.layer.android.database_system.developer.bitdubai.version_1.structure.AndroidDatabase;
+
+/**
+ * Created by jorgegonzalez on 2015.06.29..
+ */
+public class FileStorageHelper {
+}
---
+++ OSA/addon/android/fermat-osa-addon-android-database-system-bitdubai/src/test/java/unit/com/bitdubai/fermat_osa_addon/layer/android/database_system/developer/bitdubai/version_1/structure/AndroidDatabaseTableColumn/EqualsAndHashTest.java
@@ -0,0 +1,7 @@
+package unit.com.bitdubai.fermat_osa_addon.layer.android.database_system.developer.bitdubai.version_1.structure.AndroidDatabaseTableColumn;
+
+/**
+ * Created by jorgegonzalez on 2015.06.29..
+ */
+public class EqualsAndHashTest {
+}
