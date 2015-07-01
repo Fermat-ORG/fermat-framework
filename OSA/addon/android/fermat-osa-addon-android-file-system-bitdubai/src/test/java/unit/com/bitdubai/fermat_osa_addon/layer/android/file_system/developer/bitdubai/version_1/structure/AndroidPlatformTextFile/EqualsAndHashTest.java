0dec71be319a0a1e9a6b5bfa600207bb472094f8
jorgeejgonzalez <jorgegonzalez@gmail.com>
2015-07-01 13:28:28 +0200

    added the excpetion handling and unit tests to the OSA Android File System Plugin

--- OSA/addon/android/fermat-osa-addon-android-file-system-bitdubai/src/test/java/unit/com/bitdubai/fermat_osa_addon/layer/android/file_system/developer/bitdubai/version_1/structure/AndroidPlatformTextFile/EqualsAndHashTest.java
+++ OSA/addon/android/fermat-osa-addon-android-file-system-bitdubai/src/test/java/unit/com/bitdubai/fermat_osa_addon/layer/android/file_system/developer/bitdubai/version_1/structure/AndroidPlatformTextFile/EqualsAndHashTest.java
@@ -1,7 +1,4 @@
-package unit.com.bitdubai.fermat_osa_addon.layer.android.file_system.developer.bitdubai.version_1.structure.AndroidPluginTextFile;
-
-import static org.robolectric.Shadows.shadowOf;
-import static org.fest.assertions.api.Assertions.*;
+package unit.com.bitdubai.fermat_osa_addon.layer.android.file_system.developer.bitdubai.version_1.structure.AndroidPlatformTextFile;
 
 import android.app.Activity;
 import android.content.Context;
@@ -9,6 +6,7 @@ import android.support.v13.BuildConfig;
 
 import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.FilePrivacy;
+import com.bitdubai.fermat_osa_addon.layer.android.file_system.developer.bitdubai.version_1.structure.AndroidPlatformTextFile;
 import com.bitdubai.fermat_osa_addon.layer.android.file_system.developer.bitdubai.version_1.structure.AndroidPluginTextFile;
 
 import org.junit.Before;
@@ -20,6 +18,9 @@ import org.robolectric.annotation.Config;
 
 import java.util.UUID;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+
 
 /**
  * Created by jorgegonzalez on 2015.06.29..
@@ -28,10 +29,9 @@ import java.util.UUID;
 @Config(constants = BuildConfig.class)
 public class EqualsAndHashTest {
 
-    private AndroidPluginTextFile testFile1;
-    private AndroidPluginTextFile testFile2;
+    private AndroidPlatformTextFile testFile1;
+    private AndroidPlatformTextFile testFile2;
 
-    private UUID testId;
     private Context testContext;
     private String testDirectory;
     private String testFileName;
@@ -40,7 +40,6 @@ public class EqualsAndHashTest {
 
     @Before
     public void setUpValues(){
-        testId = UUID.randomUUID();
         Activity mockActivity = Robolectric.setupActivity(Activity.class);
         testContext = shadowOf(mockActivity).getApplicationContext();
         testDirectory = "ROBOLECTRICTEST";
@@ -48,39 +47,39 @@ public class EqualsAndHashTest {
         testPrivacyLevel = FilePrivacy.PUBLIC;
         testLifeSpan = FileLifeSpan.TEMPORARY;
 
-        testFile1 = constructAndroidPluginTextFile(testId, testContext, testDirectory, testFileName, testPrivacyLevel, testLifeSpan);
+        testFile1 = constructAndroidPlatformTextFile(testContext, testDirectory, testFileName, testPrivacyLevel, testLifeSpan);
     }
 
     @Test
     public void SameValues_Equals_SameHash(){
-        testFile2 = constructAndroidPluginTextFile(testId, testContext, testDirectory, testFileName, testPrivacyLevel, testLifeSpan);
+        testFile2 = constructAndroidPlatformTextFile(testContext, testDirectory, testFileName, testPrivacyLevel, testLifeSpan);
         assertThat(testFile1).isEqualTo(testFile2);
         assertThat(testFile1.hashCode()).isEqualTo(testFile2.hashCode());
     }
 
     @Test
     public void DifferentDirectory_NotEquals_DifferentHash(){
-        testFile2 = constructAndroidPluginTextFile(testId, testContext, testDirectory+"abc", testFileName, testPrivacyLevel, testLifeSpan);
+        testFile2 = constructAndroidPlatformTextFile( testContext, testDirectory + "abc", testFileName, testPrivacyLevel, testLifeSpan);
         assertThat(testFile1).isNotEqualTo(testFile2);
         assertThat(testFile1.hashCode()).isNotEqualTo(testFile2.hashCode());
     }
 
     @Test
     public void DifferentFileName_NotEquals_DifferentHash(){
-        testFile2 = constructAndroidPluginTextFile(testId, testContext, testDirectory, testFileName+"abc", testPrivacyLevel, testLifeSpan);
+        testFile2 = constructAndroidPlatformTextFile( testContext, testDirectory, testFileName + "abc", testPrivacyLevel, testLifeSpan);
         assertThat(testFile1).isNotEqualTo(testFile2);
         assertThat(testFile1.hashCode()).isNotEqualTo(testFile2.hashCode());
     }
 
     @Test
     public void DifferentPrivacyLevel_NotEquals_DifferentHash(){
-        testFile2 = constructAndroidPluginTextFile(testId, testContext, testDirectory, testFileName, FilePrivacy.PRIVATE, testLifeSpan);
+        testFile2 = constructAndroidPlatformTextFile( testContext, testDirectory, testFileName, FilePrivacy.PRIVATE, testLifeSpan);
         assertThat(testFile1).isNotEqualTo(testFile2);
         assertThat(testFile1.hashCode()).isNotEqualTo(testFile2.hashCode());
     }
 
-    private AndroidPluginTextFile constructAndroidPluginTextFile(final UUID ownerId, final Context context, final String directoryName, final String fileName, final FilePrivacy privacyLevel, final FileLifeSpan lifeSpan){
-        return new AndroidPluginTextFile(ownerId, context, directoryName, fileName, privacyLevel, lifeSpan);
+    private AndroidPlatformTextFile constructAndroidPlatformTextFile(final Context context, final String directoryName, final String fileName, final FilePrivacy privacyLevel, final FileLifeSpan lifeSpan){
+        return new AndroidPlatformTextFile(context, directoryName, fileName, privacyLevel, lifeSpan);
     }
 
 }
---
+++ OSA/addon/android/fermat-osa-addon-android-file-system-bitdubai/src/test/java/unit/com/bitdubai/fermat_osa_addon/layer/android/file_system/developer/bitdubai/version_1/structure/AndroidPluginTextFile/PersistToMediaTest.java
@@ -0,0 +1,66 @@
+package unit.com.bitdubai.fermat_osa_addon.layer.android.file_system.developer.bitdubai.version_1.structure.AndroidPluginTextFile;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v13.BuildConfig;
+
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FilePrivacy;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantPersistFileException;
+import com.bitdubai.fermat_osa_addon.layer.android.file_system.developer.bitdubai.version_1.structure.AndroidPluginTextFile;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.UUID;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.robolectric.Shadows.shadowOf;
+import static com.googlecode.catchexception.CatchException.*;
+
+/**
+ * Created by jorgegonzalez on 2015.06.29..
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class PersistToMediaTest {
+
+    private AndroidPluginTextFile testTextFile;
+
+    private UUID testId;
+    private Context testContext;
+    private String testDirectory;
+    private String testFileName;
+    private FilePrivacy testPrivacyLevel;
+    private FileLifeSpan testLifeSpan;
+
+    @Before
+    public void setUpValues(){
+        testId = UUID.randomUUID();
+        Activity mockActivity = Robolectric.setupActivity(Activity.class);
+        testContext = shadowOf(mockActivity).getApplicationContext();
+        testDirectory = "ROBOLECTRICTEST";
+        testFileName = "TESTFILE.txt";
+        testPrivacyLevel = FilePrivacy.PUBLIC;
+        testLifeSpan = FileLifeSpan.TEMPORARY;
+    }
+
+    @Test
+    public void PersistToMedia_ValidValues_MethodInvokedSuccesFully() throws Exception{
+        testTextFile = new AndroidPluginTextFile(testId, testContext, testDirectory, testFileName, testPrivacyLevel, testLifeSpan);
+        catchException(testTextFile).persistToMedia();
+        assertThat(caughtException()).isNull();
+    }
+
+    @Test
+    public void PersistToMedia_InValidFileName_MethodInvokedSuccesFully() throws Exception{
+        testTextFile = new AndroidPluginTextFile(testId, testContext, testDirectory, "", testPrivacyLevel, testLifeSpan);
+        catchException(testTextFile).persistToMedia();
+        assertThat(caughtException()).isInstanceOf(CantPersistFileException.class);
+        caughtException().printStackTrace();
+    }
+}
