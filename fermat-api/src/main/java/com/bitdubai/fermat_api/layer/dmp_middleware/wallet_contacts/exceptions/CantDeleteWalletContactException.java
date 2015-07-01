783419a862fced00eccb5140c4c62d801fe874f1
lnacosta <laion.cj91@gmail.com>
2015-07-01 19:29:11 +0200

    Added Wallet Contact to the fermat exception scheme | Compiling & Running

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_middleware/wallet_contacts/exceptions/CantDeleteWalletContactException.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_middleware/wallet_contacts/exceptions/CantDeleteWalletContactException.java
@@ -1,5 +1,7 @@
 package com.bitdubai.fermat_api.layer.dmp_middleware.wallet_contacts.exceptions;
 
+import com.bitdubai.fermat_api.FermatException;
+
 /**
  * The Class <code>com.bitdubai.fermat_api.layer.middleware.wallet_contacts.exceptions.CantDeleteWalletContactException</code>
  * is thrown when an error occurs trying to delete any contact from a wallet
@@ -9,20 +11,23 @@ package com.bitdubai.fermat_api.layer.dmp_middleware.wallet_contacts.exceptions;
  * @version 1.0
  * @since Java JDK 1.7
  */
-public class CantDeleteWalletContactException extends Exception {
+public class CantDeleteWalletContactException extends WalletContactsException {
 
-    /**
-     * Constructor
-     */
-    public CantDeleteWalletContactException(){
-        super();
+    public static final String DEFAULT_MESSAGE = "CAN'T DELETE WALLET CONTACT EXCEPTION";
+
+    public CantDeleteWalletContactException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
     }
 
-    /**
-     * Constructor whit parameter
-     * @param msg
-     */
-    public CantDeleteWalletContactException(String msg){
-        super(msg);
+    public CantDeleteWalletContactException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    public CantDeleteWalletContactException(final String message) {
+        this(message, null);
+    }
+
+    public CantDeleteWalletContactException() {
+        this(DEFAULT_MESSAGE);
     }
 }
