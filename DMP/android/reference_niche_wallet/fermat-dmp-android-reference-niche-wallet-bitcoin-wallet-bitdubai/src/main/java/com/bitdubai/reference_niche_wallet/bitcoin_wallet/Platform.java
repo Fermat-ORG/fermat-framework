8a52ef51b410f26dddd736c850dafeac7bd4cff3
lnacosta <laion.cj91@gmail.com>
2015-07-01 17:27:16 +0200

    Removed unused imports

--- DMP/android/reference_niche_wallet/fermat-dmp-android-reference-niche-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/Platform.java
+++ DMP/android/reference_niche_wallet/fermat-dmp-android-reference-niche-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/Platform.java
@@ -6,8 +6,6 @@ package com.bitdubai.reference_niche_wallet.bitcoin_wallet;
 
 import com.bitdubai.fermat_api.layer.dmp_niche_wallet_type.crypto_wallet.interfaces.DealsWithNicheWalletTypeCryptoWallet;
 import com.bitdubai.fermat_api.layer.dmp_niche_wallet_type.crypto_wallet.interfaces.CryptoWalletManager;
-import com.bitdubai.fermat_api.layer.pip_actor.developer.DealsWithToolManager;
-import com.bitdubai.fermat_api.layer.pip_actor.developer.ToolManager;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_api.layer.pip_platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.DealsWithEvents;
@@ -32,19 +30,6 @@ public class Platform implements DealsWithErrors, DealsWithEvents, DealsWithNich
      */
     private EventManager eventManager;
 
-    /**
-     * DealsWithToolManager Interface member variables.
-     */
-    private static ToolManager toolManager;
-
-    /**
-     * Platform class member variables.
-     */
-
-    private static int tagId;
-    private static int id;
-    private static String ticketId;
-
 
     /**
      * DealsWithWalletContacts Interface implementation.
@@ -53,19 +38,6 @@ public class Platform implements DealsWithErrors, DealsWithEvents, DealsWithNich
         this.cryptoWalletManager = cryptoWalletManager;
     }
 
-
-    /**
-     * DealsWithErrors Interface implementation.
-     */
-
-    public ErrorManager getErrorManager() {
-        return errorManager;
-    }
-
-
-    public EventManager getEventManager() {
-        return eventManager;
-    }
 
     /**
      * DealsWithErrors Interface implementation.
