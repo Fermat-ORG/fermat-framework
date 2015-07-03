a12d3bfbdeebf77888c6b0c5cc72f0b5ad7d8790
Roberto Requena <rart3001@gmail.com>
2015-07-03 12:36:17 -0430

    New Exception Handle Implementation (FermatException)

--- DMP/plugin/network_service/fermat-dmp-plugin-network-service-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/network_service/intra_user/developer/bitdubai/version_1/exceptions/CantLogInNetworkIntraUserException.java
+++ DMP/plugin/network_service/fermat-dmp-plugin-network-service-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/network_service/intra_user/developer/bitdubai/version_1/exceptions/CantLogInNetworkIntraUserException.java
@@ -7,6 +7,8 @@
 package com.bitdubai.fermat_dmp_plugin.layer.network_service.intra_user.developer.bitdubai.version_1.exceptions;
 
 
+import com.bitdubai.fermat_api.FermatException;
+
 /**
  * The Class <code>com.bitdubai.fermat_dmp_plugin.layer.network_service.intra_user.developer.bitdubai.version_1.exceptions.CantLogInNetworkIntraUserException</code> is
  * throw when error occurred initialize the data base
@@ -16,20 +18,58 @@ package com.bitdubai.fermat_dmp_plugin.layer.network_service.intra_user.develope
  * @version 1.0
  * @since Java JDK 1.7
  */
-public class CantLogInNetworkIntraUserException extends Exception {
+public class CantLogInNetworkIntraUserException extends FermatException {
+
+    /**
+     * Represent the default message
+     */
+    public static final String DEFAULT_MESSAGE = "CAN'T LOGIN NETWORK INTRAUSER";
+
+    /**
+     * Constructor with parameters
+     *
+     * @param message
+     * @param cause
+     * @param context
+     * @param possibleReason
+     */
+    public CantLogInNetworkIntraUserException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    /**
+     * Constructor with parameters
+     *
+     * @param message
+     * @param cause
+     */
+    public CantLogInNetworkIntraUserException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    /**
+     * Constructor with parameter
+     *
+     * @param message
+     */
+    public CantLogInNetworkIntraUserException(final String message) {
+        this(message, null);
+    }
+
+    /**
+     * Constructor with parameter
+     *
+     * @param exception
+     */
+    public CantLogInNetworkIntraUserException(final Exception exception) {
+        this(exception.getMessage());
+        setStackTrace(exception.getStackTrace());
+    }
 
     /**
      * Constructor
      */
-    public CantLogInNetworkIntraUserException(){
-        super();
-    }
-
-    /**
-     * Constructor whit parameter
-     * @param msj
-     */
-    public CantLogInNetworkIntraUserException(String msj){
-        super(msj);
+    public CantLogInNetworkIntraUserException() {
+        this(DEFAULT_MESSAGE);
     }
 }
