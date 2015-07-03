1e270d91e656687cdf0f806c5fc095ae3d68d037
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-07-03 19:57:15 +0200

    Migrated exceptions to FermatExceptions

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/transaction_transference_protocol/exceptions/CantConfirmTransactionException.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/transaction_transference_protocol/exceptions/CantConfirmTransactionException.java
@@ -1,7 +1,12 @@
 package com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.exceptions;
 
+import com.bitdubai.fermat_api.FermatException;
+
 /**
  * Created by eze on 09/06/15.
  */
-public class CantConfirmTransactionException extends Exception {
+public class CantConfirmTransactionException extends FermatException {
+    public CantConfirmTransactionException(String message, Exception cause, String context, String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
 }
