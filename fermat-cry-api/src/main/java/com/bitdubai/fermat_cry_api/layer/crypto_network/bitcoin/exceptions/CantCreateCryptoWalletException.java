8a6538176815d4d0247b8c817c02e1ef22ac4a81
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-07-03 19:56:34 +0200

    Migrated exceptions to FermatExceptions

--- fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_network/bitcoin/exceptions/CantCreateCryptoWalletException.java
+++ fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_network/bitcoin/exceptions/CantCreateCryptoWalletException.java
@@ -1,14 +1,19 @@
 package com.bitdubai.fermat_cry_api.layer.crypto_network.bitcoin.exceptions;
 
+import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_cry_api.CryptoException;
 
 /**
  * Created by ciencias on 26.01.15.
  */
-public class CantCreateCryptoWalletException extends CryptoException {
+public class CantCreateCryptoWalletException extends FermatException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1693468929863890293L;
+
+	public CantCreateCryptoWalletException(String message, Exception cause, String context, String possibleReason) {
+		super(message, cause, context, possibleReason);
+	}
 }
--- fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_vault/exceptions/CouldNotSendMoneyException.java
+++ fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_vault/exceptions/CouldNotSendMoneyException.java
@@ -1,7 +1,12 @@
 package com.bitdubai.fermat_cry_api.layer.crypto_vault.exceptions;
 
+import com.bitdubai.fermat_api.FermatException;
+
 /**
  * Created by rodrigo on 2015.06.25..
  */
-public class CouldNotSendMoneyException extends Exception {
+public class CouldNotSendMoneyException extends FermatException {
+    public CouldNotSendMoneyException(String message, Exception cause, String context, String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
 }
