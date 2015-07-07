2c92256b1b26b5befa6934df9fb142c46faacc0d
natalia <natalia_veronica_c@homail.com>
2015-07-06 22:15:43 -0300

    Bitcoinwallet plugin: database factory corrections

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_basic_wallet/bitcoin_wallet/interfaces/BitcoinWalletWallet.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_basic_wallet/bitcoin_wallet/interfaces/BitcoinWalletWallet.java
@@ -20,22 +20,11 @@ public interface BitcoinWalletWallet {
    */
     public UUID getWalletId();
 
-    /*
-     * Get the balance of the wallet, the result represents the
-     * amount of satoshis the user has.
-    */
+    public BitcoinWalletBalance getAvailableBalance() throws CantCalculateBalanceException;
 
-    //TODO: NATALIA este metodo pasa a la interfaz Wallet Balance
-    public long getBalance() throws CantCalculateBalanceException;
-
-    //TODO: NATALIA este metodo pasa a la interfaz Wallet Balance
-    public void debit(BitcoinWalletTransactionRecord cryptoTransaction) throws CantRegisterDebitDebitException;
-
-    //TODO: NATALIA este metodo pasa a la interfaz Wallet Balance
-    public void credit(BitcoinWalletTransactionRecord cryptoTransaction) throws CantRegisterCreditException;
+    public BitcoinWalletBalance getBookBalance() throws CantCalculateBalanceException;
 
     public List<BitcoinWalletTransactionRecord> getTransactions(int max, int offset) throws CantGetTransactionsException;
 
-    //TODO: NATALIA este metodo tiene que estar en BitcoinWalletTransactionRecord
     public void setDescription(UUID transactionID, String memo) throws CabtStoreMemoException, CantFindTransactionException;
 }
