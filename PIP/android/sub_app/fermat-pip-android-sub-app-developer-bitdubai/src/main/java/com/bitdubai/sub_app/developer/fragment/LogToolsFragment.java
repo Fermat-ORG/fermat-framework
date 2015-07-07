024faaf95bf85c692175cb241c1bfc0a02d42106
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-07-07 23:07:14 +0200

    logger implementation fix

--- PIP/android/sub_app/fermat-pip-android-sub-app-developer-bitdubai/src/main/java/com/bitdubai/sub_app/developer/fragment/LogToolsFragment.java
+++ PIP/android/sub_app/fermat-pip-android-sub-app-developer-bitdubai/src/main/java/com/bitdubai/sub_app/developer/fragment/LogToolsFragment.java
@@ -214,10 +214,6 @@ public class LogToolsFragment extends Fragment {
 
     }
 
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_log_tools, container, false);
