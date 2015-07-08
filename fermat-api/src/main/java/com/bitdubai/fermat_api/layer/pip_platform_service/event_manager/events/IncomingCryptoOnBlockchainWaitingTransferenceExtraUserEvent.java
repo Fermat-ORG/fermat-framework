56a595fb8e956ffc55177fa35cc079b9a76e46ca
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-07-09 00:47:50 +0200

    #543 created new events

---
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/pip_platform_service/event_manager/events/IncomingCryptoOnBlockchainWaitingTransferenceExtraUserEvent.java
@@ -0,0 +1,32 @@
+package com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.events;
+
+import com.bitdubai.fermat_api.layer.all_definition.event.PlatformEvent;
+import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventSource;
+import com.bitdubai.fermat_api.layer.pip_platform_service.event_manager.EventType;
+
+/**
+ * Created by rodrigo on 2015.07.08..
+ */
+public class IncomingCryptoOnBlockchainWaitingTransferenceExtraUserEvent implements PlatformEvent {
+    private EventType eventType;
+    private EventSource eventSource;
+
+    public IncomingCryptoOnBlockchainWaitingTransferenceExtraUserEvent(EventType eventType){
+        this.eventType = eventType;
+    }
+
+    @Override
+    public EventType getEventType() {
+        return this.eventType;
+    }
+
+    @Override
+    public void setSource(EventSource eventSource) {
+        this.eventSource = eventSource;
+    }
+
+    @Override
+    public EventSource getSource() {
+        return this.eventSource;
+    }
+}
