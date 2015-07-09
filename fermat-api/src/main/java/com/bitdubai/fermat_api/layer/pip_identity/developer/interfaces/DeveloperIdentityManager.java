9f4d6fe81f760597694b630bba09e4be73b82ab6
lnacosta <laion.cj91@gmail.com>
2015-07-09 18:44:12 +0200

    Created Layer Identity and new plugin developer identity| Compiling & Running

---
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/pip_identity/developer/interfaces/DeveloperIdentityManager.java
@@ -0,0 +1,14 @@
+package com.bitdubai.fermat_api.layer.pip_identity.developer.interfaces;
+
+/**
+ * The Class <code>com.bitdubai.fermat_api.layer.pip_identity.developer.interfaces.DeveloperIdentityManager</code>
+ * indicates the functionality of a DeveloperIdentityManager
+ * <p/>
+ *
+ * Created by Leon Acosta - (laion.cj91@gmail.com) on 09/07/15.
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public interface DeveloperIdentityManager {
+
+}
