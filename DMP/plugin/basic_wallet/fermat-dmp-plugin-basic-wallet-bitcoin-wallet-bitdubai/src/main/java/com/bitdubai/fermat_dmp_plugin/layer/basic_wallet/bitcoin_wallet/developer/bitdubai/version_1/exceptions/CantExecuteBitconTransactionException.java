cd3405bae118b50daec408150e15eb7a6c424008
jorgeejgonzalez <jorgegonzalez@gmail.com>
2015-07-10 01:08:47 +0200

    fixed the DAO implementation of the Basic Bitcoin Wallet

--- DMP/plugin/basic_wallet/fermat-dmp-plugin-basic-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/basic_wallet/bitcoin_wallet/developer/bitdubai/version_1/exceptions/CantExecuteBitconTransactionException.java
+++ DMP/plugin/basic_wallet/fermat-dmp-plugin-basic-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/basic_wallet/bitcoin_wallet/developer/bitdubai/version_1/exceptions/CantExecuteBitconTransactionException.java
@@ -5,7 +5,7 @@ import com.bitdubai.fermat_api.FermatException;
 /**
  * Created by natalia on 06/07/15.
  */
-public class CantExcecuteBitconTransaction extends FermatException {
+public class CantExecuteBitconTransactionException extends FermatException {
     /**
      * This is the constructor that every inherited FermatException must implement
      *
@@ -14,7 +14,7 @@ public class CantExcecuteBitconTransaction extends FermatException {
      * @param context        a String that provides the values of the variables that could have affected the exception
      * @param possibleReason an explicative reason of why we believe this exception was most likely thrown
      */
-    public CantExcecuteBitconTransaction(String message, Exception cause, String context, String possibleReason) {
+    public CantExecuteBitconTransactionException(String message, Exception cause, String context, String possibleReason) {
         super(message, cause, context, possibleReason);
     }
 }
