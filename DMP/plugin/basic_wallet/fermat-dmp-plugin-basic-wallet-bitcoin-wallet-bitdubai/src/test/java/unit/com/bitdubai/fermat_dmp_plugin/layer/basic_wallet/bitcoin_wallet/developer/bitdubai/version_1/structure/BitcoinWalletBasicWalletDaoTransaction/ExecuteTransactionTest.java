6ba4bf16a44333323841a90e8f2d9162947f31e6
jorgeejgonzalez <jorgegonzalez@gmail.com>
2015-07-13 21:22:07 +0200

    added the jacoco configuration

---
+++ DMP/plugin/basic_wallet/fermat-dmp-plugin-basic-wallet-bitcoin-wallet-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/basic_wallet/bitcoin_wallet/developer/bitdubai/version_1/structure/BitcoinWalletBasicWalletDaoTransaction/ExecuteTransactionTest.java
@@ -0,0 +1,7 @@
+package unit.com.bitdubai.fermat_dmp_plugin.layer.basic_wallet.bitcoin_wallet.developer.bitdubai.version_1.structure.BitcoinWalletBasicWalletDaoTransaction;
+
+/**
+ * Created by jorgegonzalez on 2015.07.13..
+ */
+public class ExecuteTransactionTest {
+}
