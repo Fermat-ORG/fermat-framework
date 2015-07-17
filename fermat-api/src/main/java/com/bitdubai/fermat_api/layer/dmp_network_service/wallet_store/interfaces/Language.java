7b3926bae611b4392f516d2060d3bb73cfc26c89
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-07-17 16:54:25 +0200

    Wallet store interfaces | Compiling & Running

---
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_network_service/wallet_store/interfaces/Language.java
@@ -0,0 +1,48 @@
+package com.bitdubai.fermat_api.layer.dmp_network_service.wallet_store.interfaces;
+
+import com.bitdubai.fermat_api.layer.all_definition.enums.Languages;
+
+import java.util.UUID;
+
+/**
+ * This class gives us a language package information
+ *
+ * @author Ezequiel Postan - (ezequiel.postan@gmail.com)
+ */
+public interface Language {
+
+    /**
+     * This method gives us the identifier of the language package
+     *
+     * @return the language id
+     */
+    public UUID getLanguageId();
+
+    /**
+     * This method gives us the name of the language
+     *
+     * @return an enum representing the language
+     */
+    public Languages getLanguageName();
+
+    /**
+     * This method gives us the size of the language package
+     *
+     * @return an int representing the language package size
+     */
+    public int getLanguagePackageSizeInBytes();
+
+    /**
+     * This method gives us the name of the author of this translation
+     *
+     * @return the alias of the translator
+     */
+    public String getTranslatorName();
+
+    /**
+     * This method gives us the public key of the author of this translation
+     *
+     * @return the public key of the translator
+     */
+    public String getTranslatorPublicKey();
+}
