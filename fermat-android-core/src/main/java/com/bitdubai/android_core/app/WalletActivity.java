1224c5ca46338adc324f532172dbbeb6cc04e847
mati <mati_fur@hotmail.com>
2015-07-17 12:46:48 +0200

    Fix #737 | Compiling & Running

--- fermat-android-core/src/main/java/com/bitdubai/android_core/app/WalletActivity.java
+++ fermat-android-core/src/main/java/com/bitdubai/android_core/app/WalletActivity.java
@@ -22,7 +22,8 @@ import android.widget.Toast;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import com.bitdubai.android_core.app.common.PagerAdapter;
+
+import com.bitdubai.android_core.app.common.version_1.TabsPagerAdapter;
 import com.bitdubai.android_core.app.common.version_1.tabbed_dialog.PagerSlidingTabStrip;
 import com.bitdubai.android_core.app.common.version_1.navigation_drawer.NavigationDrawerFragment;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
@@ -57,7 +58,7 @@ import com.bitdubai.fermat.R;
 public class WalletActivity extends FragmentActivity implements com.bitdubai.android_core.app.common.version_1.navigation_drawer.NavigationDrawerFragment.NavigationDrawerCallbacks{
 
     private NavigationDrawerFragment NavigationDrawerFragment;
-    private MyPagerAdapter adapter;
+    private TabsPagerAdapter adapter;
     private int currentColor = 0xFF666666;
 
     /**
@@ -322,7 +323,7 @@ public class WalletActivity extends FragmentActivity implements com.bitdubai.and
             else{
                 ViewPager pagertabs = (ViewPager) findViewById(R.id.pager);
                 pagertabs.setVisibility(View.VISIBLE);
-                adapter = new MyPagerAdapter(getSupportFragmentManager());
+                adapter = new TabsPagerAdapter(getSupportFragmentManager(),getApplicationContext());
 
                 pagertabs.setAdapter(adapter);
 
@@ -413,138 +414,5 @@ public class WalletActivity extends FragmentActivity implements com.bitdubai.and
 
     }
 
-    /**
-     * Tabs adapter
-     */
-    public class MyPagerAdapter extends FragmentPagerAdapter {
 
-        private String[] titles;
-
-
-
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-            if(ApplicationSession.getwalletRuntime().getLasActivity().getTabStrip() != null){
-                List<Tab> titleTabs = ApplicationSession.getwalletRuntime().getLasActivity().getTabStrip().getTabs();
-                titles = new String[titleTabs.size()];
-                for (int i = 0; i < titleTabs.size(); i++) {
-                    Tab tab = titleTabs.get(i);
-                    titles[i] = tab.getLabel();
-                }
-            }
-
-        }
-
-
-        public void destroyItem(android.view.ViewGroup container, int position, Object object) {
-
-            FragmentManager manager = ((android.support.v4.app.Fragment) object).getFragmentManager();
-            if(manager != null) {
-                FragmentTransaction trans = manager.beginTransaction();
-                trans.detach((android.support.v4.app.Fragment) object);
-                trans.remove((android.support.v4.app.Fragment) object);
-                trans.commit();
-
-
-            }
-
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            return titles[position];
-        }
-
-        @Override
-        public int getCount() {
-
-            if (titles != null)
-                return titles.length;
-            else
-                return 0;
-        }
-
-        @Override
-        public android.support.v4.app.Fragment getItem(int position) {
-
-            com.bitdubai.reference_niche_wallet.bitcoin_wallet.Platform bitcoinPlatform = null;
-            com.bitdubai.sub_app.developer.fragment.Platform developerPlatform = null;
-
-            android.support.v4.app.Fragment currentFragment = null;
-            Fragments fragmentType = Fragments.CWP_SHELL_LOGIN;
-            List<Tab> titleTabs = ApplicationSession.getwalletRuntime().getLasActivity().getTabStrip().getTabs();
-            for (int j = 0; j < titleTabs.size(); j++) {
-                if (j == position)
-                {
-                    Tab tab = titleTabs.get(j);
-                    fragmentType = tab.getFragment();
-                    break;
-                }
-            }
-            //execute current activity fragments
-            try {
-                switch (fragmentType) {
-                    case CWP_SHELL_LOGIN:
-
-                        break;
-                    case CWP_WALLET_MANAGER_MAIN:
-                        currentFragment =  WalletDesktopFragment.newInstance(position);
-                        break;
-
-
-                    /**
-                     * Executing fragments for BITCOIN WALLET.
-                     */
-
-                    case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_BALANCE:
-                        bitcoinPlatform = new com.bitdubai.reference_niche_wallet.bitcoin_wallet.Platform();
-                        bitcoinPlatform.setNicheWalletTypeCryptoWalletManager((CryptoWalletManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getPlugin(Plugins.BITDUBAI_CRYPTO_WALLET_NICHE_WALLET_TYPE));
-                        bitcoinPlatform.setErrorManager((ErrorManager)ApplicationSession.getFermatPlatform().getCorePlatformContext().getAddon(Addons.ERROR_MANAGER));
-                        currentFragment =  BalanceFragment.newInstance(0);
-                        break;
-                    case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_RECEIVE:
-                        bitcoinPlatform = new com.bitdubai.reference_niche_wallet.bitcoin_wallet.Platform();
-                        bitcoinPlatform.setNicheWalletTypeCryptoWalletManager((CryptoWalletManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getPlugin(Plugins.BITDUBAI_CRYPTO_WALLET_NICHE_WALLET_TYPE));
-                        bitcoinPlatform.setErrorManager((ErrorManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getAddon(Addons.ERROR_MANAGER));
-                        currentFragment = ReceiveFragment.newInstance(0);
-                        break;
-                    case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_SEND:
-                        bitcoinPlatform = new com.bitdubai.reference_niche_wallet.bitcoin_wallet.Platform();
-                        bitcoinPlatform.setNicheWalletTypeCryptoWalletManager((CryptoWalletManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getPlugin(Plugins.BITDUBAI_CRYPTO_WALLET_NICHE_WALLET_TYPE));
-                        bitcoinPlatform.setErrorManager((ErrorManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getAddon(Addons.ERROR_MANAGER));
-                        currentFragment =  SendFragment.newInstance(0);
-                        break;
-
-                    case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_TRANSACTIONS:
-                        bitcoinPlatform = new com.bitdubai.reference_niche_wallet.bitcoin_wallet.Platform();
-                        bitcoinPlatform.setNicheWalletTypeCryptoWalletManager((CryptoWalletManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getPlugin(Plugins.BITDUBAI_CRYPTO_WALLET_NICHE_WALLET_TYPE));
-                        bitcoinPlatform.setErrorManager((ErrorManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getAddon(Addons.ERROR_MANAGER));
-                        currentFragment =  TransactionsFragment.newInstance(0);
-
-                        break;
-                    case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_CONTACTS:
-                        bitcoinPlatform = new com.bitdubai.reference_niche_wallet.bitcoin_wallet.Platform();
-                        bitcoinPlatform.setNicheWalletTypeCryptoWalletManager((CryptoWalletManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getPlugin(Plugins.BITDUBAI_CRYPTO_WALLET_NICHE_WALLET_TYPE));
-                        bitcoinPlatform.setErrorManager((ErrorManager) ApplicationSession.getFermatPlatform().getCorePlatformContext().getAddon(Addons.ERROR_MANAGER));
-                        currentFragment =  ContactsFragment.newInstance(0);
-                        break;
-
-
-
-                }
-
-            }
-            catch(Exception ex)
-            {
-                ApplicationSession.getErrorManager().reportUnexpectedPlatformException(PlatformComponents.PLATFORM, UnexpectedPlatformExceptionSeverity.DISABLES_ONE_PLUGIN, ex);
-
-                Toast.makeText(getApplicationContext(), "Error in PagerAdapter GetItem " + ex.getMessage(),
-                        Toast.LENGTH_LONG).show();
-            }
-
-            return currentFragment;
-        }
-
-    }
 }
