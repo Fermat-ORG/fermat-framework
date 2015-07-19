bda371d88e82b5589fef8797aa820b77d6f916af
Nerio Indriago <nerioindriago@gmail.com>
2015-07-19 09:49:55 -0430

    Para PULL Request

---
+++ DMP/plugin/middleware/fermat-dmp-plugin-middleware-wallet-contacts-bitdubai/src/test/java/test/com/bitdubai/fermat_dmp_plugin/layer/middleware/wallet_contacts/developer/bitdubai/version_1/WalletContactsMiddlewarePluginRoot/serviceStatusTest.java
@@ -0,0 +1,7 @@
+package test.com.bitdubai.fermat_dmp_plugin.layer.middleware.wallet_contacts.developer.bitdubai.version_1.WalletContactsMiddlewarePluginRoot;
+
+/**
+ * Created by root on 18/07/15.
+ */
+public class serviceStatusTest {
+}
---
+++ PIP/plugin/module/fermat-pip-plugin-module-developer-bitdubai/src/test/java/unit/com/bitdubai/fermat_pip_plugin/layer/module/developer/developer/bitdubai/version_1/DeveloperModulePluginRoot/getClassesFullPath.java
@@ -0,0 +1,7 @@
+package unit.com.bitdubai.fermat_pip_plugin.layer.module.developer.developer.bitdubai.version_1.DeveloperModulePluginRoot;
+
+/**
+ * Created by root on 18/07/15.
+ */
+public class getClassesFullPath {
+}
