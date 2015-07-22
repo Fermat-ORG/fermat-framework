415243dbc2284bb5cf55f412d33d83cbb3c599fc
Manuel Perez <darkpriestrelative@gmail.com>
2015-07-21 22:55:44 -0430

    Added new tests and old tests from EstraUserUserAddonRoot in the new test structure.

---
+++ DMP/plugin/actor/fermat-dmp-plugin-actor-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/actor/extra_user/developer/bitdubai/version_1/ExtraUserUserAddonRoot/ResumeTest.java
@@ -0,0 +1,7 @@
+package unit.com.bitdubai.fermat_dmp_plugin.layer.actor.extra_user.developer.bitdubai.version_1.ExtraUserUserAddonRoot;
+
+/**
+ * Created by Manuel Perez on 21/07/15.
+ */
+public class ResumeTest {
+}
