7dc589be400ad83c6a23a13d2ef1ead5ec599bf1
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-07-22 13:02:46 +0200

    Fixing interfaces duplication  | Compiling & Running

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_module/wallet_factory/WalletFactoryManager.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_module/wallet_factory/WalletFactoryManager.java
@@ -3,5 +3,5 @@ package com.bitdubai.fermat_api.layer.dmp_module.wallet_factory;
 /**
  * Created by eze on 2015.07.09..
  */
-public class WalletFactoryManager {
+public interface WalletFactoryManager {
 }
