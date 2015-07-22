a38ece15fb0fe8394db9c8d1cc54ce20df33ae4d
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-07-22 20:45:02 +0200

    commit before merge | Compiling

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_middleware/wallet_manager/interfaces/InstalledWallet.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_middleware/wallet_manager/interfaces/InstalledWallet.java
@@ -1,7 +1,9 @@
 package com.bitdubai.fermat_api.layer.dmp_middleware.wallet_manager.interfaces;
 
+import com.bitdubai.fermat_api.layer.all_definition.enums.NicheWallet;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ReferenceWallet;
 import com.bitdubai.fermat_api.layer.all_definition.enums.WalletCategory;
+import com.bitdubai.fermat_api.layer.all_definition.enums.WalletType;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,8 +16,10 @@ public interface InstalledWallet {
     List<InstalledLanguage> getLanguagesId();
     List<InstalledSkin> getSkinsId();
     WalletCategory getWalletCategory();
+    WalletType getWalletType();
+    NicheWallet getNicheWallet();
     String getWalletIcon();
     UUID getWalletIdInThisDevice();
     String getWalletName();
-    ReferenceWallet getWalletType();
+    ReferenceWallet getReferenceWallet();
 }
