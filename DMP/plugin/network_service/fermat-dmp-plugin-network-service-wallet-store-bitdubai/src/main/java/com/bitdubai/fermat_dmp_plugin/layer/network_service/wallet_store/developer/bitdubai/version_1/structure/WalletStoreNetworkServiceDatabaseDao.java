e8f8088f57235f90df83923acbab85a757e5dd00
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-07-24 16:42:15 -0300

    Minor fix to incorrect reference.

--- DMP/plugin/network_service/fermat-dmp-plugin-network-service-wallet-store-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/network_service/wallet_store/developer/bitdubai/version_1/structure/WalletStoreNetworkServiceDatabaseDao.java
+++ DMP/plugin/network_service/fermat-dmp-plugin-network-service-wallet-store-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/network_service/wallet_store/developer/bitdubai/version_1/structure/WalletStoreNetworkServiceDatabaseDao.java
@@ -31,7 +31,7 @@ import com.bitdubai.fermat_dmp_plugin.layer.network_service.wallet_store.develop
 import com.bitdubai.fermat_dmp_plugin.layer.network_service.wallet_store.developer.bitdubai.version_1.structure.common.DatabaseOperations;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
-import com.sun.istack.internal.Nullable;
+
 
 import java.net.MalformedURLException;
 import java.net.URL;
