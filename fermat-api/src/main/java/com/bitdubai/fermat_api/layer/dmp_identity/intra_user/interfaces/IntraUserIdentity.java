cb593c46b8adde3438fd8a6be9f78335b5d165b1
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-07-28 00:42:10 +0200

    Fix #907 and Fix #898 and Fix #900 | Compiling & Running

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_identity/intra_user/interfaces/IntraUserIdentity.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_identity/intra_user/interfaces/IntraUserIdentity.java
@@ -1,5 +1,7 @@
 package com.bitdubai.fermat_api.layer.dmp_identity.intra_user.interfaces;
 
+import com.bitdubai.fermat_api.layer.dmp_identity.intra_user.exceptions.CantSingIntraUserMessageException;
+
 /**
  * The interface <code>com.bitdubai.fermat_api.layer.dmp_identity.intra_user.interfaces.IntraUserIdentity</code>
  * defines the methods related to the extraction of the information of an intra user
@@ -18,4 +20,12 @@ public interface IntraUserIdentity {
      * @return the public key of the represented intra user
      */
     String getPublicKey();
+
+    /**
+     * This method let an intra user sign a message with his unique private key
+     * @param message the message to sign
+     * @return the signature
+     * @throws CantSingIntraUserMessageException
+     */
+    String createMessageSignature(String message) throws CantSingIntraUserMessageException;
 }
--- fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_router/incoming_crypto/IncomingCryptoManager.java
+++ fermat-cry-api/src/main/java/com/bitdubai/fermat_cry_api/layer/crypto_router/incoming_crypto/IncomingCryptoManager.java
@@ -4,7 +4,12 @@ import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_pro
 import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
 
 /**
- * Created by eze on 2015.06.17..
+ * The interface <code>com.bitdubai.fermat_cry_api.layer.crypto_router.incoming_crypto.IncomingCryptoManager</code>
+ * define the methods that the Incoming Crypto plugin exposes to its clients. As this plugin only function is to
+ * route crypto transactions it extends <code></code>TransactionSender<CryptoTransaction> interface
+ *
+ * @author Ezequiel Postan
+ * @since Java JDK 1.7
  */
 public interface IncomingCryptoManager extends TransactionSender<CryptoTransaction> {
 }
