7fcc4de780df7d75c84ef8335035c917b2b374ff
Manuel Perez <darkpriestrelative@gmail.com>
2015-07-28 17:18:54 -0430

    The test is fixed. The test is now focusing in use the statics properties from this method.

--- DMP/plugin/actor/fermat-dmp-plugin-actor-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/actor/extra_user/developer/bitdubai/version_1/ExtraUserUserAddonRoot/GetLogLevelByClassTest_NotSetLogginLevel.java
+++ DMP/plugin/actor/fermat-dmp-plugin-actor-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/actor/extra_user/developer/bitdubai/version_1/ExtraUserUserAddonRoot/GetLogLevelByClassTest_NotSetLogginLevel.java
@@ -14,22 +14,26 @@ public class GetLogLevelByClassTest_NotSetLogginLevel {
 
     ExtraUserUserAddonRoot extraUserUserAddonRoot;
 
+    /**
+     * I need to test the static method behavior and call the static LogLevel Map
+     */
     @Test
-    public void getLogLevelByClassTest_notSetLogginLevel_returnNullLogLevel() throws Exception{
+    public void getLogLevelByClassTest_testingStaticBehaviorSetAValidArgument_returnNullLogLevel() throws Exception{
 
         LogLevel resultLogLevel=extraUserUserAddonRoot.getLogLevelByClass("com.bitdubai.fermat_dmp_plugin.layer.actor.extra_user.developer.bitdubai.version_1.structure.ExtraUser");
-        Assertions.assertThat(resultLogLevel).isNull();
+        Assertions.assertThat(resultLogLevel)
+                .isNull();
 
     }
 
-    //@Ignore
     @Test
-    public void getLogLevelByClassTest_NotSetLogginLevelNullArgument_returnNullLogLevel() throws Exception{
+    public void getLogLevelByClassTest_testingStaticBehaviorLevelNullArgument_returnNullLogLevel() throws Exception{
 
         extraUserUserAddonRoot=new ExtraUserUserAddonRoot();
         LogLevel resultLogLevel=extraUserUserAddonRoot.getLogLevelByClass(null);
         Assertions.assertThat(resultLogLevel)
-                .isNotNull().isEqualTo(LogLevel.MINIMAL_LOGGING);
+                .isNotNull()
+                .isEqualTo(LogLevel.MINIMAL_LOGGING);
 
     }
 
