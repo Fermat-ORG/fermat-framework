20a42c4b91f54924f58e585309ca66a1e4aae9ca
Manuel Perez <darkpriestrelative@gmail.com>
2015-07-29 18:41:55 -0430

    Updating fork from QA branch

--- DMP/plugin/actor/fermat-dmp-plugin-actor-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/actor/extra_user/developer/bitdubai/version_1/ExtraUserUserAddonRoot/GetLogLevelByClassTest_NotSetLogginLevel.java
+++ DMP/plugin/actor/fermat-dmp-plugin-actor-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/actor/extra_user/developer/bitdubai/version_1/ExtraUserUserAddonRoot/GetLogLevelByClassTest_NotSetLogginLevel.java
@@ -1,6 +1,5 @@
 package unit.com.bitdubai.fermat_dmp_plugin.layer.actor.extra_user.developer.bitdubai.version_1.ExtraUserUserAddonRoot;
 
-import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
 import com.bitdubai.fermat_dmp_plugin.layer.actor.extra_user.developer.bitdubai.version_1.ExtraUserUserAddonRoot;
 
@@ -13,7 +12,6 @@ import org.junit.Test;
  */
 public class GetLogLevelByClassTest_NotSetLogginLevel {
 
-<<<<<<< HEAD
     /**
      * I need to test the static method behavior and call the static LogLevel Map
      */
@@ -24,30 +22,15 @@ public class GetLogLevelByClassTest_NotSetLogginLevel {
         Assertions.assertThat(resultLogLevel)
                 .isNotNull();
 
-=======
-    ExtraUserUserAddonRoot extraUserUserAddonRoot;
-
-    //TODO: This is not going to work in real life, the whole sense of the static logic of the method is going to bug this behaviour. This should be an issue of the logging system
-    @Ignore
-    @Test
-    public void getLogLevelByClassTest_notSetLogginLevel_ReturnNull() throws Exception{
-        LogLevel resultLogLevel =  ExtraUserUserAddonRoot.getLogLevelByClass("com.bitdubai.fermat_dmp_plugin.layer.actor.extra_user.developer.bitdubai.version_1.structure.ExtraUser");
-        Assertions.assertThat(resultLogLevel).isNull();
->>>>>>> newbranch/master
     }
 
     @Test
     public void getLogLevelByClassTest_testingStaticBehaviorLevelNullArgument_returnNullLogLevel() throws Exception{
 
-<<<<<<< HEAD
         LogLevel resultLogLevel=ExtraUserUserAddonRoot.getLogLevelByClass(null);
-=======
-        extraUserUserAddonRoot=new ExtraUserUserAddonRoot();
-        LogLevel resultLogLevel =  ExtraUserUserAddonRoot.getLogLevelByClass(null);
->>>>>>> newbranch/master
         Assertions.assertThat(resultLogLevel)
                 .isNotNull()
-                .isEqualTo(LogManagerForDevelopers.DEFAULT_LOG_LEVEL);
+                .isEqualTo(LogLevel.MINIMAL_LOGGING);
 
     }
 
