7a84781dc3446ce864006b49006ffc6cad66a666
Manuel Perez <darkpriestrelative@gmail.com>
2015-07-31 20:46:51 -0430

    Added new tests.

---
+++ DMP/plugin/actor/fermat-dmp-plugin-actor-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/actor/extra_user/developer/bitdubai/version_1/structure/ExtraUserDeveloperDatabaseFactory/SetPluginDatabaseSystemTest.java
@@ -0,0 +1,7 @@
+package unit.com.bitdubai.fermat_dmp_plugin.layer.actor.extra_user.developer.bitdubai.version_1.structure.ExtraUserDeveloperDatabaseFactory;
+
+/**
+ * Created by Manuel Perez on 31/07/15.
+ */
+public class SetPluginDatabaseSystemTest {
+}
