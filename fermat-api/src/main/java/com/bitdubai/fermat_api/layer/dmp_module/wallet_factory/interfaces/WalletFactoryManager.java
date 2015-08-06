70eb4f0eea1170bda42d51c5e5a8d4247268a98e
mati <mati_fur@hotmail.com>
2015-08-07 01:40:54 +0200

    Wallet factory module interface

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_module/wallet_factory/interfaces/WalletFactoryManager.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/dmp_module/wallet_factory/interfaces/WalletFactoryManager.java
@@ -1,9 +1,19 @@
 package com.bitdubai.fermat_api.layer.dmp_module.wallet_factory.interfaces;
 
+import com.bitdubai.fermat_api.layer.all_definition.enums.WalletCategory;
+import com.bitdubai.fermat_api.layer.all_definition.enums.WalletType;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.exceptions.CantCreateWalletFactoryProjectException;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.exceptions.CantGetWalletFactoryProjectException;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.exceptions.CantImportWalletFactoryProjectException;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.exceptions.ProjectNotFoundException;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.interfaces.WalletFactoryProject;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.interfaces.WalletFactoryProjectProposal;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_factory.interfaces.WalletFactoryProjectProposalManager;
 import com.bitdubai.fermat_api.layer.dmp_module.wallet_factory.exceptions.CantGetAvailableDevelopersException;
 import com.bitdubai.fermat_api.layer.dmp_module.wallet_factory.exceptions.CantGetAvailableProjectsException;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * The interface <code>com.bitdubai.fermat_api.layer.dmp_module.wallet_factory.interfaces.WalletFactoryManager</code>
@@ -20,6 +30,26 @@ public interface WalletFactoryManager {
 
     List<WalletFactoryDeveloper> getAvailableDevelopers() throws CantGetAvailableDevelopersException;
 
-    List<String> getAvailableProjects() throws CantGetAvailableProjectsException;
+    List<WalletFactoryProject> getAvailableProjects() throws CantGetAvailableProjectsException;
+
+    public WalletFactoryProject createEmptyProject(String name, WalletCategory walletCategory,WalletType walletType)throws CantCreateWalletFactoryProjectException;
+
+    public void importProjectFromRepository(String newName, String repository) throws CantImportWalletFactoryProjectException;
+
+    public WalletFactoryProject getProject(String name)throws CantGetWalletFactoryProjectException, ProjectNotFoundException;
+
+
+
+    // metodos para el walletFactoryProject
+
+    // cambiar el nombre de WalletFactoryProjectProposalManager a WalletFactoryProjectVersion o algo como para decir eso
+    public WalletFactoryProjectProposalManager getProjectVersion(UUID projectId,UUID versionId);
+
+    // para la version en uso
+    public WalletFactoryProjectProposal getProjectWorkingVersion(UUID projectId);
+
+    //public List<WalletFactoryProjectProposal>  getAllProjectVersion(List<>);
+
+
 
 }
