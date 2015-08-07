35c61980b80f27c634e1bb4c47b3ddda7172e906
natalia <natalia_veronica_c@hotmail.com>
2015-08-07 17:41:45 -0300

    Fix #1140 Crear Nuevo Plugin: DMP / Middleware / NavigationStructure

---
+++ DMP/plugin/middleware/fermat-dmp-plugin-middleware-wallet-navigation-structure-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/middleware/navigation_structure/developer/bitdubai/version_1/structure/WalletNavigationStructureMiddleware.java
@@ -0,0 +1,24 @@
+package com.bitdubai.fermat_dmp_plugin.layer.middleware.navigation_structure.developer.bitdubai.version_1.structure;
+
+
+
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_navigation_structure.interfaces.WalletNavigationStructure;
+
+
+
+/**
+ * The Class <code>com.bitdubai.fermat_dmp_plugin.layer.middleware.navigation_structure.developer.bitdubai.version_1.structure.WalletNavigationStructureMiddleware</code>
+ * is the implementation of WalletNavigationStructure.
+ * <p/>
+ *
+ * Created by Natalia on 07/08/15.
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+
+
+public class WalletNavigationStructureMiddleware implements WalletNavigationStructure {
+
+
+
+}
--- fermat-core/src/main/java/com/bitdubai/fermat_core/Platform.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/Platform.java
@@ -834,6 +834,14 @@ public class Platform  {
             injectPluginReferencesAndStart(walletStoreModule, Plugins.BITDUBAI_WALLET_STORE_MODULE);
 
 
+              /*
+             * Plugin Wallet Navigation Structure Middleware
+             * ----------------------------------
+             */
+            Plugin walletNavigationStructureMiddleware = ((MiddlewareLayer) corePlatformContext.getPlatformLayer(PlatformLayers.BITDUBAI_MIDDLEWARE_LAYER)).getWalletNavigationStructurePlugin();
+            injectPluginReferencesAndStart(walletNavigationStructureMiddleware, Plugins.BITDUBAI_WALLET_NAVIGATION_STRUCTURE_MIDDLEWARE);
+
+
             /*
              * Plugin Bitcoin Crypto Vault
              * ----------------------------------
