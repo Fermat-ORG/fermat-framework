3b91047396240426cf593cec5b7a82f51fc19593
francisco <sonnik42@hotmail.com>
2015-08-13 23:48:33 -0300

    Tasas de Cambio

---
+++ DMP/plugin/world/fermat-dmp-plugin-world-crypto-index-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/world/crypto_index/developer/bitdubai/version_1/structure/RateExchange.java
@@ -0,0 +1,15 @@
+package com.bitdubai.fermat_dmp_plugin.layer.world.crypto_index.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by francisco on 12/08/15.
+ */
+public class RateExchange {
+
+    private String serviceOne;
+    private String serviceTwo;
+    private String servicethree;
+
+
+
+
+}
