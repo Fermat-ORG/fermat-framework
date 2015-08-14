0567eb13db37b8f4cdddd6e02ba93cc16f6a0971
Manuel Perez <darkpriestrelative@gmail.com>
2015-08-14 09:04:08 -0430

    Implemented com/bitdubai/fermat_dmp_plugin/layer/middleware/wallet_language/developer/bitdubai/version_1/database/WalletLanguageMiddlewareDao.java

--- DMP/plugin/middleware/fermat-dmp-plugin-middleware-wallet-navigation-structure-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/middleware/navigation_structure/developer/bitdubai/version_1/database/WalletNavigationStructureMiddlewareDao.java
+++ DMP/plugin/middleware/fermat-dmp-plugin-middleware-wallet-navigation-structure-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/middleware/navigation_structure/developer/bitdubai/version_1/database/WalletNavigationStructureMiddlewareDao.java
@@ -5,6 +5,7 @@ import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.Activity;
 import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Activities;
 import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_navigation_structure.exceptions.CantCreateEmptyWalletNavigationStructureException;
+import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_navigation_structure.exceptions.CantDeleteNavigationStructureException;
 import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_navigation_structure.exceptions.CantListNavigationStructuresException;
 import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_navigation_structure.exceptions.CantUpdateNavigationStructureException;
 import com.bitdubai.fermat_api.layer.dmp_middleware.wallet_navigation_structure.exceptions.NavigationStructureNotFoundException;
@@ -17,18 +18,18 @@ import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantCreateDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantDeleteRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantInsertRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantLoadTableToMemoryException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantUpdateRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
 import com.bitdubai.fermat_dmp_plugin.layer.middleware.navigation_structure.developer.bitdubai.version_1.exceptions.CantExecuteDatabaseOperationException;
 import com.bitdubai.fermat_dmp_plugin.layer.middleware.navigation_structure.developer.bitdubai.version_1.exceptions.CantInitializeWalletNavigationStructureMiddlewareDatabaseException;
 import com.bitdubai.fermat_dmp_plugin.layer.middleware.navigation_structure.developer.bitdubai.version_1.structure.WalletNavigationStructureMiddleware;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -82,6 +83,43 @@ public class WalletNavigationStructureMiddlewareDao {
         } catch (Exception exception){
             this.database.closeDatabase();
             throw new CantCreateEmptyWalletNavigationStructureException(CantCreateEmptyWalletNavigationStructureException.DEFAULT_MESSAGE,FermatException.wrapException(exception),"Unexpected failure", "Please, check the cause");
+        }
+
+    }
+
+    public void deleteNavigationStructure(String publicKey) throws CantDeleteNavigationStructureException, NavigationStructureNotFoundException {
+
+        try{
+
+            this.database.openDatabase();
+            DatabaseTable projectNavigationStructureTable=this.database.getTable(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_TABLE_NAME);
+            projectNavigationStructureTable.setStringFilter(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_PUBLIC_KEY, publicKey, DatabaseFilterType.EQUAL);
+            projectNavigationStructureTable.loadToMemory();
+
+            List<DatabaseTableRecord> records =projectNavigationStructureTable.getRecords();
+
+            if(!records.isEmpty()){
+                DatabaseTableRecord record = records.get(0);
+
+                projectNavigationStructureTable.deleteRecord(record);
+                this.database.closeDatabase();
+
+            }else{
+                this.database.closeDatabase();
+                throw new NavigationStructureNotFoundException(NavigationStructureNotFoundException.DEFAULT_MESSAGE, null, "Cannot find this WalletNavigationStructure with "+publicKey+" id","Please, check the cause");
+            }
+
+        }catch (CantOpenDatabaseException | DatabaseNotFoundException exception) {
+
+            throw new CantDeleteNavigationStructureException(CantDeleteNavigationStructureException.DEFAULT_MESSAGE,exception,"Trying to open the database","Please, check the cause");
+        }catch (CantLoadTableToMemoryException exception) {
+            this.database.closeDatabase();
+            throw new CantDeleteNavigationStructureException(CantLoadTableToMemoryException.DEFAULT_MESSAGE,exception,"Trying to load the database to memory","Please, check the cause");
+        } catch (CantDeleteRecordException exception) {
+            this.database.closeDatabase();
+            throw new CantDeleteNavigationStructureException(CantDeleteRecordException.DEFAULT_MESSAGE, exception, "Cannot delete the WalletNavigationStructure", "Please, check the cause");
+        } catch(Exception exception){
+            throw new CantDeleteNavigationStructureException(CantDeleteRecordException.DEFAULT_MESSAGE, FermatException.wrapException(exception),"Unexpected failure", "Please, check the cause");
         }
 
     }
@@ -274,13 +312,42 @@ public class WalletNavigationStructureMiddlewareDao {
 
         try{
 
+            String navigationStructurePublicKey=walletNavigationStructure.getPublicKey();
             this.database.openDatabase();
             DatabaseTable projectNavigationStructureTable=this.database.getTable(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_TABLE_NAME);
-            projectNavigationStructureTable.setStringFilter(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_PUBLIC_KEY, walletNavigationStructure.getPublicKey(), DatabaseFilterType.EQUAL);
+            projectNavigationStructureTable.setStringFilter(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_PUBLIC_KEY, navigationStructurePublicKey, DatabaseFilterType.EQUAL);
             projectNavigationStructureTable.loadToMemory();
 
             List<DatabaseTableRecord> records =projectNavigationStructureTable.getRecords();
-            //TODO Lo que resta, esperando el refactor del 13 de junio de 2015
+            if (!records.isEmpty()) {
+                DatabaseTableRecord record = records.get(0);
+
+                String recordPublicKey=record.getStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_PUBLIC_KEY);
+                if(recordPublicKey.equals(navigationStructurePublicKey)){
+                    record.setStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_PUBLIC_KEY,navigationStructurePublicKey);
+                }
+
+                String activity=record.getStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_ACTIVITY);
+                if(!activity.equals(walletNavigationStructure.getActivity().toString())){
+                    record.setStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_ACTIVITY, walletNavigationStructure.getActivity().toString());
+                }
+
+                String lastActivity=record.getStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_LAST_ACTIVITY);
+                if(!lastActivity.equals(walletNavigationStructure.getLastActivity().toString())){
+                    record.setStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_LAST_ACTIVITY, walletNavigationStructure.getLastActivity().toString());
+                }
+
+                String startActivity=record.getStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_START_ACTIVITY);
+                if(!startActivity.equals(walletNavigationStructure.getActivity().toString())){
+                    record.setStringValue(WalletNavigationStructureMiddlewareDatabaseConstants.WALLET_NAVIGATION_STRUCTURE_START_ACTIVITY, walletNavigationStructure.getStartActivity().toString());
+                }
+
+                projectNavigationStructureTable.updateRecord(record);
+
+            }else {
+                database.closeDatabase();
+                throw new NavigationStructureNotFoundException(NavigationStructureNotFoundException.DEFAULT_MESSAGE, null, "Cannot find this WalletNavigationStructure with "+navigationStructurePublicKey+" id","Please, check the cause");
+            }
 
         } catch (CantOpenDatabaseException | DatabaseNotFoundException exception){
 
@@ -289,9 +356,17 @@ public class WalletNavigationStructureMiddlewareDao {
         } catch (CantLoadTableToMemoryException exception) {
             this.database.closeDatabase();
             throw new CantUpdateNavigationStructureException(CantLoadTableToMemoryException.DEFAULT_MESSAGE,exception,"Trying to load the database to memory","Please, check the cause");
+        } catch (CantUpdateRecordException e) {
+            this.database.closeDatabase();
+            throw new NavigationStructureNotFoundException(NavigationStructureNotFoundException.DEFAULT_MESSAGE, null, "Cannot update the WalletNavigationStructure","Please, check the cause");
+        } catch(Exception exception){
+            this.database.closeDatabase();
+            throw new CantUpdateNavigationStructureException(CantUpdateNavigationStructureException.DEFAULT_MESSAGE,FermatException.wrapException(exception),"Unexpected failure", "Please, check the cause");
         }
 
     }
+
+
 
     private Database openDatabase() throws CantExecuteDatabaseOperationException {
       //  try {
