fba277e378a25f94871b9b565eaaa3c04bc030cf
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-08-17 22:58:23 -0300

    Refactoring Wallet Factory

---
+++ DMP/plugin/middleware/fermat-dmp-plugin-middleware-wallet-factory-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/middleware/wallet_factory/developer/bitdubai/version_1/database/WalletFactoryMiddlewareDAO.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dmp_plugin.layer.middleware.wallet_factory.developer.bitdubai.version_1.database;
+
+/**
+ * Created by rodrigo on 8/17/15.
+ */
+public class WalletFactoryMiddlewareDAO {
+}
