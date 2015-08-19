82c6c36d978c0b13b13b5b8b185b37bf87b32243
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-08-19 16:17:52 +0200

    Outgoing Extra User - Tests corrections

--- DMP/plugin/transaction/fermat-dmp-plugin-transaction-outgoing-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/transaction/outgoing_extra_user/developer/bitdubai/version_1/structure/OutgoingExtraUserTransactionManager/sendTest.java
+++ DMP/plugin/transaction/fermat-dmp-plugin-transaction-outgoing-extra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/transaction/outgoing_extra_user/developer/bitdubai/version_1/structure/OutgoingExtraUserTransactionManager/sendTest.java
@@ -95,7 +95,7 @@ public class sendTest {
         cryptoAddress = new CryptoAddress();
         cryptoAddress.setCryptoCurrency(CryptoCurrency.BITCOIN);
         cryptoAddress.setAddress(UUID.randomUUID().toString());
-        testwalletID = anyString();
+        testwalletID = "0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6";
         testId = UUID.randomUUID();
         testDataBaseName = OutgoingExtraUserDatabaseConstants.OUTGOING_EXTRA_USER_DATABASE_NAME;
 
@@ -147,7 +147,6 @@ public class sendTest {
         testTransactionManager.setPluginDatabaseSystem(mockPluginDatabaseSystem);
         testTransactionManager.setPluginId(testId);
         testTransactionManager.setErrorManager(mockErrorManager);
-
 
 
         testTransactionManager.setBitcoinWalletManager(mockBitcoinWalletManager);
