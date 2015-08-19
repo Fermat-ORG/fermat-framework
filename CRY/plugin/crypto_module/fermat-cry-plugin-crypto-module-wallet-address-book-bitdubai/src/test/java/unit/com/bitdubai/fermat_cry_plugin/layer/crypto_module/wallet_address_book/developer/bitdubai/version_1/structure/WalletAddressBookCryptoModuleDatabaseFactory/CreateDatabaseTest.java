37f91425ed19a2ea9716dd0527e43c93f2d4bdd4
lnacosta <laion.cj91@gmail.com>
2015-08-19 15:14:17 +0200

    Fixed Tests that failed after the wallet public key refactoring. Checked coverage of 55% covering all main functions of plugin.

--- CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-wallet-address-book-bitdubai/src/test/java/unit/com/bitdubai/fermat_cry_plugin/layer/crypto_module/wallet_address_book/developer/bitdubai/version_1/structure/WalletAddressBookCryptoModuleDatabaseFactory/CreateDatabaseTest.java
+++ CRY/plugin/crypto_module/fermat-cry-plugin-crypto-module-wallet-address-book-bitdubai/src/test/java/unit/com/bitdubai/fermat_cry_plugin/layer/crypto_module/wallet_address_book/developer/bitdubai/version_1/structure/WalletAddressBookCryptoModuleDatabaseFactory/CreateDatabaseTest.java
@@ -1,6 +1,7 @@
 package unit.com.bitdubai.fermat_cry_plugin.layer.crypto_module.wallet_address_book.developer.bitdubai.version_1.structure.WalletAddressBookCryptoModuleDatabaseFactory;
 
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseFactory;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableFactory;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantCreateDatabaseException;
@@ -9,11 +10,11 @@ import com.bitdubai.fermat_cry_plugin.layer.crypto_module.wallet_address_book.de
 import junit.framework.TestCase;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
 
 import java.util.UUID;
 
@@ -27,7 +28,14 @@ public class CreateDatabaseTest extends TestCase {
     PluginDatabaseSystem pluginDatabaseSystem;
 
     @Mock
-    DatabaseTableFactory table;
+    DatabaseTableFactory mockTable;
+
+    @Mock
+    Database mockDatabase;
+
+    @Mock
+    DatabaseFactory mockDatabaseFactory;
+
 
     WalletAddressBookCryptoModuleDatabaseFactory databaseFactory;
 
@@ -38,20 +46,20 @@ public class CreateDatabaseTest extends TestCase {
         pluginId = UUID.randomUUID();
         databaseFactory = new WalletAddressBookCryptoModuleDatabaseFactory();
         databaseFactory.setPluginDatabaseSystem(pluginDatabaseSystem);
+        when(pluginDatabaseSystem.createDatabase(any(UUID.class), anyString())).thenReturn(mockDatabase);
+        when(mockDatabase.getDatabaseFactory()).thenReturn(mockDatabaseFactory);
+        when(mockDatabaseFactory.newTableFactory(any(UUID.class), anyString())).thenReturn(mockTable);
     }
 
-    @Ignore
+    @Test
     public void testCreateDatabase_NotNull() throws Exception {
-        /*
-         * TODO This test should pass but there is a wrong design decision that makes a cast of the Database interface into the DatabaseFactory; we should really look into that
-         */
         Database database = databaseFactory.createDatabase(pluginId);
         assertNotNull(database);
     }
 
     @Test(expected=CantCreateDatabaseException.class)
     public void testCreateDatabase_CantCreateDatabaseException() throws Exception {
-        when(pluginDatabaseSystem.createDatabase(pluginId, pluginId.toString())).thenThrow(new CantCreateDatabaseException());
+        when(pluginDatabaseSystem.createDatabase(any(UUID.class), anyString())).thenThrow(new CantCreateDatabaseException());
 
         databaseFactory.createDatabase(pluginId);
     }
