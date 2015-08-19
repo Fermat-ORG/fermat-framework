71005c5584cb2dad78963df6d7b302ed2ae6aac8
vlzangel <vlzangel@gmail.com>
2015-08-19 13:27:53 -0430

    Correcciones
    Correcciones

--- DMP/plugin/identity/fermat-dmp-plugin-identity-intra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/identity/intra_user/developer/bitdubai/version_1/structure/IntraUserIdentityIdentity/createMessageSignatureTest.java
+++ DMP/plugin/identity/fermat-dmp-plugin-identity-intra-user-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/identity/intra_user/developer/bitdubai/version_1/structure/IntraUserIdentityIdentity/createMessageSignatureTest.java
@@ -1,5 +1,8 @@
 package unit.com.bitdubai.fermat_dmp_plugin.layer.identity.intra_user.developer.bitdubai.version_1.structure.IntraUserIdentityIdentity;
 
+import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.ECCKeyPair;
+import com.bitdubai.fermat_api.layer.all_definition.crypto.util.CryptoHasher;
+import com.bitdubai.fermat_api.layer.dmp_identity.intra_user.exceptions.CantSetNewProfileImageException;
 import com.bitdubai.fermat_api.layer.dmp_identity.intra_user.exceptions.CantSingIntraUserMessageException;
 import com.bitdubai.fermat_dmp_plugin.layer.identity.intra_user.developer.bitdubai.version_1.structure.IntraUserIdentityIdentity;
 import com.googlecode.catchexception.CatchException;
@@ -16,13 +19,31 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class createMessageSignatureTest{
 
+
     @Test
     public void createMessageSignature() throws CantSingIntraUserMessageException {
+        ECCKeyPair eccKeyPair = new ECCKeyPair();
+        String hashMessage = CryptoHasher.performSha256("prueba");
+        IntraUserIdentityIdentity identity_1 = new IntraUserIdentityIdentity("alias_1", eccKeyPair.getPrivateKey(), eccKeyPair.getPublicKey(), new byte[10]);
+
+        identity_1.createMessageSignature(hashMessage);
+    }
+
+    @Test
+    public void createMessageSignatureExceptionTest() throws CantSingIntraUserMessageException {
         IntraUserIdentityIdentity identity_1 = new IntraUserIdentityIdentity("alias_1", "publicKey_1", "privateKey_1", new byte[10]);
 
         catchException(identity_1).createMessageSignature("Prueba");
         assertThat(CatchException.<Exception>caughtException()).isInstanceOf(CantSingIntraUserMessageException.class);
         caughtException().printStackTrace();
+    }
+
+    @Test
+    public void setNewProfileImageTest() throws CantSingIntraUserMessageException, CantSetNewProfileImageException {
+        ECCKeyPair eccKeyPair = new ECCKeyPair();
+        IntraUserIdentityIdentity identity_1 = new IntraUserIdentityIdentity("alias_1", eccKeyPair.getPrivateKey(), eccKeyPair.getPublicKey(), new byte[10]);
+
+        identity_1.setNewProfileImage(new byte[10]);
     }
 
 }
