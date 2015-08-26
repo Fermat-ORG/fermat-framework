c4b9ef82378e214e53d274b5c323452acf69b77f
Francisco Vasquez <fvasquezjatar@gmail.com>
2015-08-26 15:32:41 -0430

    WalletPublisher Manager

--- DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-publisher-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_publisher/fragment/MainFragment.java
+++ DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-publisher-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_publisher/fragment/MainFragment.java
@@ -3,6 +3,7 @@ package com.bitdubai.sub_app.wallet_publisher.fragment;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,37 +12,42 @@ import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.interfaces.SubAppsSession;
 import com.bitdubai.fermat_api.layer.dmp_module.wallet_publisher.interfaces.WalletPublisherModuleManager;
 import com.bitdubai.sub_app.wallet_publisher.R;
+import com.bitdubai.sub_app.wallet_publisher.session.WalletPublisherSubAppSession;
 
 /**
  * Created by natalia on 09/07/15.
  */
 public class MainFragment extends FermatFragment {
 
-    private static final String ARG_POSITION = "position";
+    private static final String TAG = "WalletPublisher";
+    /**
+     * WalletPublisherModuleManager
+     */
+    private WalletPublisherModuleManager manager;
+    /**
+     * UI Reference
+     */
+    private View rootView;
 
-    WalletPublisherModuleManager walletPublisherManager;
-    Typeface tf;
-
-    public static MainFragment newInstance(int position,SubAppsSession subAppsSession) {
+    public static MainFragment newInstance() {
         MainFragment f = new MainFragment();
-        Bundle b = new Bundle();
-        b.putInt(ARG_POSITION, position);
-        f.setArguments(b);
         return f;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            manager = ((WalletPublisherSubAppSession) subAppsSession).getWalletPublisherManager();
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage());
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.wallet_publisher_main_fragment, container, false);
-    }
+        rootView = inflater.inflate(R.layout.wallet_publisher_main_fragment, container, false);
 
-    public void setWalletPublisherManager(WalletPublisherModuleManager walletPublisherManager) {
-        this.walletPublisherManager = walletPublisherManager;
+        return rootView;
     }
 }
