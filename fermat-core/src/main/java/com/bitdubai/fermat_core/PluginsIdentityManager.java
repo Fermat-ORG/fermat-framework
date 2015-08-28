fca6124e55ccfcb60358f6fe9bf8812911c35cd7
lnacosta <laion.cj91@gmail.com>
2015-08-28 03:49:29 +0200

    PluginsIdentityManager enhancement creates new id for an unrecognized plugin.

--- fermat-core/src/main/java/com/bitdubai/fermat_core/PluginsIdentityManager.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/PluginsIdentityManager.java
@@ -4,6 +4,7 @@ import com.bitdubai.fermat_api.CantInitializePluginsManagerException;
 import com.bitdubai.fermat_api.PluginNotRecognizedException;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.exceptions.InvalidParameterException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.DealsWithPlatformFileSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.FilePrivacy;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PlatformTextFile;
@@ -21,15 +22,23 @@ import java.util.UUID;
  * Created by ciencias on 02.02.15.
  * Modified by Leon Acosta (laion.cj01@gmail.com) on 28/08/2015.
  */
-public class PluginsIdentityManager implements Serializable {
+public class PluginsIdentityManager implements DealsWithPlatformFileSystem, Serializable {
+
+    private static final String PLUGIN_IDS_DIRECTORY_NAME = "Platform";
+
+    private static final String PLUGIN_IDS_FILE_NAME = "PluginsIds";
+
+    private static final String PAIR_SEPARATOR = "|";
+
+    private PlatformFileSystem platformFileSystem;
 
     private Map<Plugins, UUID> pluginIdsMap = new HashMap<>();
 
     public PluginsIdentityManager(PlatformFileSystem platformFileSystem) throws CantInitializePluginsManagerException {
+        this.platformFileSystem = platformFileSystem;
         try {
             try {
-                PlatformTextFile platformTextFile = platformFileSystem.getFile("Platform", "PluginsIds", FilePrivacy.PRIVATE, FileLifeSpan.PERMANENT);
-                String[] stringPluginIdPairs = platformTextFile.getContent().split("\\|");
+                String[] stringPluginIdPairs = getPluginIdsFile().getContent().split("\\|");
 
                 for (String stringPluginIdPair : stringPluginIdPairs) {
                     if (!stringPluginIdPair.equals("")) {
@@ -38,34 +47,64 @@ public class PluginsIdentityManager implements Serializable {
                     }
                 }
             } catch (CantCreateFileException | InvalidParameterException e) {
+                e.printStackTrace();
                 throw new CantInitializePluginsManagerException(CantInitializePluginsManagerException.DEFAULT_MESSAGE, e, "", "I don't know really what happened.");
             }
         } catch (FileNotFoundException fileNotFoundException) {
             try {
-                PlatformTextFile platformTextFile = platformFileSystem.createFile("Platform", "PluginsIds", FilePrivacy.PRIVATE, FileLifeSpan.PERMANENT);
+                PlatformTextFile platformTextFile = platformFileSystem.createFile(PLUGIN_IDS_DIRECTORY_NAME, PLUGIN_IDS_FILE_NAME, FilePrivacy.PRIVATE, FileLifeSpan.PERMANENT);
 
-                String fileContent = "";
+                for (Plugins plugin : Plugins.values())
+                    pluginIdsMap.put(plugin, UUID.randomUUID());
 
-                for (Plugins plugin : Plugins.values()) {
-                    UUID newId = UUID.randomUUID();
-                    fileContent = fileContent + plugin.getKey() + ";" + newId + "|";
-                    pluginIdsMap.put(plugin, newId);
-                }
+                savePluginIdsFile(platformTextFile);
 
-                platformTextFile.setContent(fileContent);
-                platformTextFile.persistToMedia();
             } catch (CantCreateFileException | CantPersistFileException e) {
                 throw new CantInitializePluginsManagerException(CantInitializePluginsManagerException.DEFAULT_MESSAGE, e, "", "I don't know really what happened.");
             }
         }
     }
 
-    public UUID getPluginId(Plugins descriptor) throws PluginNotRecognizedException {
-        UUID pluginId = pluginIdsMap.get(descriptor);
+    /**
+     * returns the id of the desired plugin.
+     * if the id doesn't exists, creates a new one and saves it in the fil.
+     *
+     * @param plugin descriptor of the plugin of which you want to have the id.
+     * @return the id of the plugin
+     * @throws PluginNotRecognizedException if something goes wrong.
+     */
+    public UUID getPluginId(Plugins plugin) throws PluginNotRecognizedException {
+        UUID pluginId = pluginIdsMap.get(plugin);
 
-        if (pluginId != null)
+        if (pluginId != null) {
             return pluginId;
-        else
-            throw new PluginNotRecognizedException(PluginNotRecognizedException.DEFAULT_MESSAGE, null, "Plugin Descriptor: " + descriptor.getKey(), null);
+        } else {
+            try {
+                UUID newId = UUID.randomUUID();
+                pluginIdsMap.put(plugin, newId);
+                savePluginIdsFile(getPluginIdsFile());
+                return newId;
+            } catch (CantPersistFileException | CantCreateFileException | FileNotFoundException e) {
+                throw new PluginNotRecognizedException(PluginNotRecognizedException.DEFAULT_MESSAGE, null, "Plugin Descriptor: " + plugin.getKey(), null);
+            }
+        }
+    }
+
+    private PlatformTextFile getPluginIdsFile() throws CantCreateFileException, FileNotFoundException {
+        return platformFileSystem.getFile(PLUGIN_IDS_DIRECTORY_NAME, PLUGIN_IDS_FILE_NAME, FilePrivacy.PRIVATE, FileLifeSpan.PERMANENT);
+    }
+
+    private void savePluginIdsFile(PlatformTextFile platformTextFile) throws CantCreateFileException, CantPersistFileException {
+        String fileContent = "";
+        for (Map.Entry<Plugins, UUID> plugin : pluginIdsMap.entrySet())
+            fileContent = fileContent + plugin.getKey().getKey() + ";" + plugin.getValue() + "|";
+
+        platformTextFile.setContent(fileContent);
+        platformTextFile.persistToMedia();
+    }
+
+    @Override
+    public void setPlatformFileSystem(PlatformFileSystem platformFileSystem) {
+        this.platformFileSystem = platformFileSystem;
     }
 }
