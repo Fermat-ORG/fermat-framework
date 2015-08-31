b27ca5b3877908501b0c56a991d526f02d1b6f41
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-08-31 19:55:20 -0300

    Added Asset Crypto Vault as a new plugin into platform

--- fermat-core/src/main/java/com/bitdubai/fermat_core/layer/cry_cypto_vault/CryptoVaultLayer.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/layer/cry_cypto_vault/CryptoVaultLayer.java
@@ -3,6 +3,7 @@ package com.bitdubai.fermat_core.layer.cry_cypto_vault;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.layer.CantStartLayerException;
 import com.bitdubai.fermat_api.layer.PlatformLayer;
+import com.bitdubai.fermat_core.layer.cry_cypto_vault.assets.AssetsSubsystem;
 import com.bitdubai.fermat_cry_api.layer.crypto_vault.CantStartSubsystemException;
 import com.bitdubai.fermat_cry_api.layer.crypto_vault.CryptoVaultSubsystem;
 import com.bitdubai.fermat_core.layer.cry_cypto_vault.bitcoin.BitcoinSubsystem;
@@ -13,13 +14,18 @@ import com.bitdubai.fermat_core.layer.cry_cypto_vault.bitcoin.BitcoinSubsystem;
 public class CryptoVaultLayer implements PlatformLayer {
 
     private Plugin mBitcoin;
-
+    private Plugin mAssetsVault;
 
 
 
     public Plugin getmBitcoin() {
         return mBitcoin;
     }
+
+    public Plugin getmAssetsVault() {
+        return mAssetsVault;
+    }
+
 
 
 
@@ -29,10 +35,13 @@ public class CryptoVaultLayer implements PlatformLayer {
 
 
         CryptoVaultSubsystem bitcoinSubsystem = new BitcoinSubsystem();
+        AssetsSubsystem assetsSubsystem = new AssetsSubsystem();
 
         try {
             bitcoinSubsystem.start();
+            assetsSubsystem.start();
             mBitcoin = ((BitcoinSubsystem) bitcoinSubsystem).getPlugin();
+            mAssetsVault  = ((AssetsSubsystem) assetsSubsystem).getPlugin();
 
         } catch (CantStartSubsystemException e) {
             System.err.println("CantStartCryptoNetworkException: " + e.getMessage());
