bf7435cec06609bd7f5f4f68dc8560da0bec8268
Manuel Perez <darkpriestrelative@gmail.com>
2015-08-31 16:19:13 -0430

    Creating OutgoinIssuer plugin structure

--- DAP/plugin/transaction/fermat-dap-plugin-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_issuing/developer/bitdubai/DeveloperBitDubai.java
+++ DAP/plugin/transaction/fermat-dap-plugin-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_issuing/developer/bitdubai/DeveloperBitDubai.java
@@ -1,4 +1,4 @@
-package com.bitdubai;
+package com.bitdubai.fermat_dap_plugin.layer.transaction.asset_issuing.developer.bitdubai;
 
 public class DeveloperBitDubai {
 }
--- DAP/plugin/transaction/fermat-dap-plugin-transaction-incoming-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/incoming_issuer/developer/bitdubai/DeveloperBitDubai.java
+++ DAP/plugin/transaction/fermat-dap-plugin-transaction-incoming-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/incoming_issuer/developer/bitdubai/DeveloperBitDubai.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.bitdubai.fermat_dap_plugin.layer.transaction.incoming_issuer.developer.bitdubai;
 
 public class DeveloperBitDubai {
 }
