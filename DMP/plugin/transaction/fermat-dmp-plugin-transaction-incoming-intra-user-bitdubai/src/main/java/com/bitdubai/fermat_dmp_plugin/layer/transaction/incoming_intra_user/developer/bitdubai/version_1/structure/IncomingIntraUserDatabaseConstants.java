74ccbbc224b59f172946aa2f5dceb55e38272ac3
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-09-02 15:33:01 +0200

    Transactional modules documentation organization

---
+++ DMP/plugin/transaction/fermat-dmp-plugin-transaction-incoming-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/transaction/incoming_intra_user/developer/bitdubai/version_1/structure/IncomingIntraUserDatabaseConstants.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dmp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by eze on 2015.09.01..
+ */
+public class IncomingIntraUserDatabaseConstants {
+}
