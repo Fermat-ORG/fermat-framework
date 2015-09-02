2c7f5b719ff20abfc8a12538e0aee9a8fa3cc0fa
Ezequiel Postan <ezequiel_postan@hotmail.com>
2015-09-02 15:42:03 +0200

    Incoming Intra User Database Constants

--- DMP/plugin/transaction/fermat-dmp-plugin-transaction-incoming-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/transaction/incoming_intra_user/developer/bitdubai/version_1/structure/IncomingIntraUserDatabaseConstants.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.fermat_dmp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.structure;
-
-/**
- * Created by eze on 2015.09.01..
- */
-public class IncomingIntraUserDatabaseConstants {
-}
