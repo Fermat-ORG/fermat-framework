42ba74bb092c9f271a97d43d3086c9e0229f509a
francisco <sonnik42@hotmail.com>
2015-09-03 18:16:54 -0300

    Class Structure and Implementation: Plugin DMP Crypto Index World Issue 1218

--- DMP/plugin/world/fermat-dmp-plugin-world-crypto-index-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/world/crypto_index/developer/bitdubai/version_1/providers/CryptocoinchartsServiceAPI.java
+++ DMP/plugin/world/fermat-dmp-plugin-world-crypto-index-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/world/crypto_index/developer/bitdubai/version_1/providers/CryptocoinchartsServiceAPI.java
@@ -1,22 +1,12 @@
 package com.bitdubai.fermat_dmp_plugin.layer.world.crypto_index.developer.bitdubai.version_1.providers;
 
 import com.bitdubai.fermat_dmp_plugin.layer.world.crypto_index.developer.bitdubai.version_1.interfaces.InterfaceUrlAPI;
-import com.bitdubai.fermat_dmp_plugin.layer.world.crypto_index.developer.bitdubai.version_1.structure.JsonService;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by francisco on 13/08/15.
  */
 public class CryptocoinchartsServiceAPI implements InterfaceUrlAPI {
 
-
-    public String coin;
     public String UrlAPI;
 
     @Override
