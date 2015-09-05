64f055dedbcf601fb9c38b1a739ea805e4c301f1
Manuel Perez <darkpriestrelative@gmail.com>
2015-09-04 21:56:11 -0430

    Update fermat-dap-documentation and create some AssetIssuing classes

---
+++ DAP/plugin/transaction/fermat-dap-plugin-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_issuing/developer/bitdubai/version_1/structure/DigitalAssetCryptoTransactionFactory.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_plugin.layer.transaction.asset_issuing.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 04/09/15.
+ */
+public class DigitalAssetCryptoTransactionFactory {
+}
