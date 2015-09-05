7524b2dde266418c818785749ee3d5acb3c338d3
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-09-05 15:40:09 -0430

    Pluggin structure - documentacion

--- fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_wallet/asset_issuer_wallet/interfaces/AssetIssuerWalletManager.java
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_wallet/asset_issuer_wallet/interfaces/AssetIssuerWalletManager.java
@@ -4,4 +4,8 @@ package com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.interfa
  * Created by franklin on 04/09/15.
  */
 public interface AssetIssuerWalletManager {
+
+    AssetIssuerWallet loadAssetIssuerWallet(String walletPublicKey);
+
+    void createWalletAssetIssuer (String walletPublicKey);
 }
