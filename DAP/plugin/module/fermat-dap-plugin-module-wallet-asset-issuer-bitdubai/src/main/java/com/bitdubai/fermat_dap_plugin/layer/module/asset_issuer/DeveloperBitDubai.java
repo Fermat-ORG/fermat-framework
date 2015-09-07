7552c5406700a26da33526d707113b3fdf56f87e
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-09-07 20:52:11 -0300

    Added new plugin Module Asset Issuer basic structure

---
+++ DAP/plugin/module/fermat-dap-plugin-module-wallet-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/module/asset_issuer/DeveloperBitDubai.java
@@ -0,0 +1,48 @@
+package com.bitdubai.fermat_dap_plugin.layer.module.asset_issuer;
+
+import com.bitdubai.fermat_api.Plugin;
+import com.bitdubai.fermat_api.PluginDeveloper;
+import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.enums.TimeFrequency;
+import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
+import com.bitdubai.fermat_dap_plugin.layer.module.asset_issuer.version_1.AssetIssuerModulePluginRoot;
+
+/**
+ * Created by rodrigo on 9/7/15.
+ */
+public class DeveloperBitDubai implements PluginDeveloper, PluginLicensor {
+
+    Plugin plugin;
+
+    public DeveloperBitDubai() {
+        plugin = new AssetIssuerModulePluginRoot();
+    }
+
+    @Override
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    @Override
+    public int getAmountToPay() {
+        return 100;
+    }
+
+    @Override
+    public CryptoCurrency getCryptoCurrency() {
+        return CryptoCurrency.BITCOIN;
+    }
+
+    @Override
+    public String getAddress() {
+        return "13gpMizSNvQCbJzAPyGCUnfUGqFD8ryzcv";
+    }
+
+    @Override
+    public TimeFrequency getTimePeriod() {
+        return TimeFrequency.MONTHLY;
+    }
+
+    {
+    }
+}
---
+++ DAP/plugin/module/fermat-dap-plugin-module-wallet-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/module/asset_issuer/version_1/AssetIssuerModulePluginRoot.java
@@ -0,0 +1,15 @@
+package com.bitdubai.fermat_dap_plugin.layer.module.asset_issuer.version_1;
+
+import com.bitdubai.fermat_api.Plugin;
+
+import java.util.UUID;
+
+/**
+ * Created by rodrigo on 9/7/15.
+ */
+public class AssetIssuerModulePluginRoot implements Plugin {
+    @Override
+    public void setId(UUID pluginId) {
+
+    }
+}
