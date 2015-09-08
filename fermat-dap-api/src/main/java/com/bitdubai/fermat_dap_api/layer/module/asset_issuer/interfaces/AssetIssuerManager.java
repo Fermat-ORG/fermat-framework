4a8cb2e57da604761e2c05607d6f681a8abc5e87
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-09-08 10:34:22 -0430

    Module Pluggin

--- fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/module/asset_issuer/interfaces/AssetIssuerManager.java
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/module/asset_issuer/interfaces/AssetIssuerManager.java
@@ -15,9 +15,15 @@ public interface AssetIssuerManager {
     List<AssetIssuer> getAssetIssuerByState(State state);
 
     AssetIssuer createEmptyAssetIssuer();
-
     void createAssetIssuer(AssetIssuer assetIssuer);
     void removeAssetIssuer(AssetIssuer assetIssuer);
 
+    //Para verificar en la wallet si tiene fondo suficiente
     boolean verifiedGenesisAmount(long genesisAmount);
+
+    //Sera pedido por la interfaz del usuario pero debemos buscarlo ya que sera calculado en la crpytoVault
+    long getEstimatedFeeValue(long transactionFee);
+
+    //Este metodo sera el que se comunique con la capa transaccional a traves del DealsWithAssetIssuing
+    void IssueAsset(DigitalAsset digitalAsset);
 }
