6168ca24b13d8da8bd54cbc7236449c7806a42c6
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-09-08 15:07:28 -0430

    Module Pluggin

--- fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/module/asset_issuer/interfaces/AssetIssuer.java
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/module/asset_issuer/interfaces/AssetIssuer.java
@@ -8,11 +8,12 @@ import com.bitdubai.fermat_dap_api.all_definition.digital_asset.DigitalAsset;
  * Created by franklin on 07/09/15.
  */
 public interface AssetIssuer {
+    //Propiedades que le sera pedidas a la interfaz del usuario desde el module de la sup app.
     //Esta interfaz tendra que contener todos las propiedades que se van a pedir en la sub app, para que se guarde el objeto digital asset
     //en su creacion, y manipular toda la informacion y workflow respectivo.
     //Falta los seters
-    //String getWalletPublicKey();
-    //void setWalletPublicKey(String walletPublicKey);
+    String getWalletPublicKey();
+    void setWalletPublicKey(String walletPublicKey);
 
     String getAssetUserIdentityPublicKey();
     void setAssetUserIdentityPublicKey(String assetUserIdentityPublicKey);
@@ -22,4 +23,7 @@ public interface AssetIssuer {
 
     WalletType getWalletType();
     void setWalletType(WalletType walletType);
+
+    DigitalAsset getDigitalAsset();
+    void setDigitalAsset(DigitalAsset digitalAsset);
 }
--- fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/module/asset_issuer/interfaces/AssetIssuerManager.java
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/module/asset_issuer/interfaces/AssetIssuerManager.java
@@ -2,6 +2,10 @@ package com.bitdubai.fermat_dap_api.layer.module.asset_issuer.interfaces;
 
 import com.bitdubai.fermat_dap_api.all_definition.digital_asset.DigitalAsset;
 import com.bitdubai.fermat_dap_api.all_definition.digital_asset.enums.State;
+import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.interfaces.AssetIssuerWallet;
+import com.bitdubai.fermat_dap_api.layer.module.asset_issuer.exceptions.CantCreateAssetIssuerException;
+import com.bitdubai.fermat_dap_api.layer.module.asset_issuer.exceptions.CantLoadAsserIssuerException;
+import com.bitdubai.fermat_dap_api.layer.module.asset_issuer.exceptions.CantSaveAssetIssuerException;
 
 import java.util.List;
 
@@ -10,20 +14,21 @@ import java.util.List;
  */
 public interface AssetIssuerManager {
     //Falta documentar y colocar las excepciones
-    List<AssetIssuer> getAllAssetIssuer();
-    List<AssetIssuer> getAssetIssuerByIssuer(String issuerPublicKey);
-    List<AssetIssuer> getAssetIssuerByState(State state);
+    List<AssetIssuer> getAllAssetIssuer() throws CantLoadAsserIssuerException;
+    List<AssetIssuer> getAssetIssuerByIssuer(String issuerPublicKey) throws CantLoadAsserIssuerException;
+    List<AssetIssuer> getAssetIssuerByState(State state) throws CantLoadAsserIssuerException;
 
     AssetIssuer createEmptyAssetIssuer();
-    void createAssetIssuer(AssetIssuer assetIssuer);
+    void createAssetIssuer(AssetIssuer assetIssuer) throws CantCreateAssetIssuerException;
+    void saveAssetIssuer(AssetIssuer assetIssuer) throws CantSaveAssetIssuerException;
     void removeAssetIssuer(AssetIssuer assetIssuer);
 
-    //Para verificar en la wallet si tiene fondo suficiente
-    boolean verifiedGenesisAmount(long genesisAmount);
-
-    //Sera pedido por la interfaz del usuario pero debemos buscarlo ya que sera calculado en la crpytoVault
-    long getEstimatedFeeValue(long transactionFee);
+    //Este metodo sera el que se comunique con la capa transaccional a traves del DealsWithAssetIssuing
+    boolean verifiedGenesisAmount(AssetIssuer assetIssuer);
 
     //Este metodo sera el que se comunique con la capa transaccional a traves del DealsWithAssetIssuing
-    void IssueAsset(DigitalAsset digitalAsset);
+    long getEstimatedFeeValue(AssetIssuer assetIssuer);
+
+    //Este metodo sera el que se comunique con la capa transaccional a traves del DealsWithAssetIssuing
+    void IssueAsset(AssetIssuer assetIssuer);
 }
