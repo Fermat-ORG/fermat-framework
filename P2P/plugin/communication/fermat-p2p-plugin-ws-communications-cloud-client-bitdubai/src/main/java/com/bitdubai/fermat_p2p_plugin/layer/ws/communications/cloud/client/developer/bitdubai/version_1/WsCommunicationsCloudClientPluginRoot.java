2f482e370f024b1c8b780e0869657cb03d955429
Roberto Requena <rart3001@gmail.com>
2015-09-09 10:48:14 -0430

    Merge branch 'develop' of https://github.com/acostarodrigo/fermat into develop
    # Conflicts:
    #	P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java

--- P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
+++ P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
@@ -70,7 +70,6 @@ public class WsCommunicationsCloudClientPluginRoot implements Service, DealsWith
      */
     private static final String SERVER_IP = "52.11.156.16";
     //private static final String SERVER_IP = "192.168.0.7";
-    private static final String SERVER_IP = "52.11.156.16"; //AWS
 
     /**
      * Represents the value of DISABLE_CLIENT
@@ -157,7 +156,7 @@ public class WsCommunicationsCloudClientPluginRoot implements Service, DealsWith
             wsCommunicationsCloudClientConnection.initializeAndConnect();
 
 
-            /* ONLY FOR TEST */
+            /* ONLY FOR TEST
 
                 new Thread(new Runnable() {
 
@@ -179,7 +178,7 @@ public class WsCommunicationsCloudClientPluginRoot implements Service, DealsWith
                     }
                 }).start();
 
-
+            */
 
         } catch (Exception e) {
             e.printStackTrace();
