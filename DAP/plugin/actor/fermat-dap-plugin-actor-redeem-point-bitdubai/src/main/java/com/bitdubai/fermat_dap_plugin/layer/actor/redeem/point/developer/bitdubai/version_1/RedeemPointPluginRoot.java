66af3d7a35dc9f0265cd18a43b03d961bdb5c9dc
Nerio Indriago <nerioindriago@gmail.com>
2015-09-09 16:25:15 -0430

    updateElementsDAP-Actor

---
+++ DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/redeem/point/developer/bitdubai/version_1/RedeemPointPluginRoot.java
@@ -0,0 +1,19 @@
+package com.bitdubai.fermat_dap_plugin.layer.actor.redeem.point.developer.bitdubai.version_1;
+
+import com.bitdubai.fermat_api.Plugin;
+
+import java.util.UUID;
+
+/**
+ * Created by Nerio on 07/09/15.
+ */
+public class RedeemPointPluginRoot implements Plugin {
+
+    UUID pluginId;
+
+    @Override
+    public void setId(UUID pluginId) {
+        this.pluginId = pluginId;
+    }
+
+}
