8d7fde964988e26c9085ed55f5a3f7ae57737fdf
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-09-09 20:57:04 -0430

    wallet asset issuer pluggin

---
+++ DAP/plugin/asset_wallet/fermat-dap-plugin-asset-wallet-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/asset_wallet/issuer/developer/bitdubai/version_1/AssetWalletIssuerPluginRoot.java
@@ -0,0 +1,19 @@
+package com.bitdubai.fermat_dap_plugin.layer.asset_wallet.issuer.developer.bitdubai.version_1;
+
+import com.bitdubai.fermat_api.Plugin;
+
+import java.util.UUID;
+
+/**
+ * Created by Nerio on 07/09/15.
+ */
+public class AssetWalletIssuerPluginRoot implements Plugin {
+
+    UUID pluginId;
+
+    @Override
+    public void setId(UUID pluginId) {
+        this.pluginId = pluginId;
+    }
+
+}
