02c1fbdc237cdc66c38caed1653eca812f01fb5f
mati <mati_fur@hotmail.com>
2015-09-11 03:08:06 +0200

    Design layout parser

---
+++ DMP/android/reference_niche_wallet/fermat-dmp-android-reference-niche-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/TestFragment.java
@@ -0,0 +1,12 @@
+package com.bitdubai.reference_niche_wallet.bitcoin_wallet.fragments;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
+
+/**
+ * Created by mati on 2015.09.10..
+ */
+public class TestFragment extends FermatFragment{
+
+
+
+}
