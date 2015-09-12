cfcf396278aeb2bf30d38035707cc08350e3b60c
mati <mati_fur@hotmail.com>
2015-09-12 14:11:13 +0200

    Design layout parser

--- DMP/android/reference_niche_wallet/fermat-dmp-android-reference-niche-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragmentFactory/ReferenceWalletFragmentFactory.java
+++ DMP/android/reference_niche_wallet/fermat-dmp-android-reference-niche-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragmentFactory/ReferenceWalletFragmentFactory.java
@@ -50,7 +50,7 @@ public class ReferenceWalletFragmentFactory implements com.bitdubai.fermat_andro
                  * Executing fragments for BITCOIN REQUESTED.
                  */
                 case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_BALANCE:
-                    currentFragment = BlankFragment.newInstance(null,null);//BalanceFragment.newInstance(0, refereceWalletSession, walletResourcesProviderManager);
+                    currentFragment = BalanceFragment.newInstance(0, refereceWalletSession, walletResourcesProviderManager);
                     break;
                 case CWP_WALLET_RUNTIME_WALLET_BITCOIN_ALL_BITDUBAI_RECEIVE:
                     currentFragment = ReceiveFragment.newInstance(0, refereceWalletSession, walletResourcesProviderManager);
