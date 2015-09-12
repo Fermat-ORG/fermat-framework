d70eeaf2b452acd65a8cb332e05ff4be6e2cb819
Manuel Perez <darkpriestrelative@gmail.com>
2015-09-12 12:00:54 -0430

    fermat-dap-api refactor

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_issuing/developer/bitdubai/version_1/structure/events/DigitalAssetTransactionAgent.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_issuing/developer/bitdubai/version_1/structure/events/DigitalAssetTransactionAgent.java
@@ -8,7 +8,7 @@ import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPlugin
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
-import com.bitdubai.fermat_dap_api.dap_transaction.asset_issuing.interfaces.AssetIssuingTransactionNotificationAgent;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_issuing.interfaces.AssetIssuingTransactionNotificationAgent;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.event_manager.interfaces.DealsWithEvents;
