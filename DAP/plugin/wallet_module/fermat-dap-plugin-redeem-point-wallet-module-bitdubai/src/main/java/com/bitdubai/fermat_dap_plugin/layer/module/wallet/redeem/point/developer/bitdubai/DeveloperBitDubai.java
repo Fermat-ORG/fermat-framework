20afd317055cf5cb174da5b2712dd9a9ef21bc6a
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-09-13 08:40:49 -0430

    Merge branch 'develop' of https://github.com/acostarodrigo/fermat into develop
    Conflicts:
    	DAP/plugin/dap_middleware/fermat-dap-plugin-middleware-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/middleware/asset_factory/developer/bitdubai/version_1/AssetFactoryPluginRoot.java

--- DAP/plugin/wallet_module/fermat-dap-plugin-redeem-point-wallet-module-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/module/wallet/redeem/point/developer/bitdubai/DeveloperBitDubai.java
+++ DAP/plugin/wallet_module/fermat-dap-plugin-redeem-point-wallet-module-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/module/wallet/redeem/point/developer/bitdubai/DeveloperBitDubai.java
@@ -1,11 +1,11 @@
-package com.bitdubai.fermat_dap_plugin.layer.asset_wallet.redeem_point.developer.bitdubai;
+package com.bitdubai.fermat_dap_plugin.layer.module.wallet.redeem.point.developer.bitdubai;
 
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.PluginDeveloper;
 import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
 import com.bitdubai.fermat_api.layer.all_definition.enums.TimeFrequency;
 import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
-import com.bitdubai.fermat_dap_plugin.layer.asset_wallet.redeem_point.developer.bitdubai.version_1.AssetWalletRedeemPointPluginRoot;
+import com.bitdubai.fermat_dap_plugin.layer.module.wallet.redeem.point.developer.bitdubai.version_1.AssetWalletRedeemPointModulePluginRoot;
 
 /**
  * Created by Franklin on 07/09/15.
@@ -15,7 +15,7 @@ public class DeveloperBitDubai implements PluginDeveloper, PluginLicensor {
     Plugin plugin;
 
     public DeveloperBitDubai() {
-        plugin = new AssetWalletRedeemPointPluginRoot();
+        plugin = new AssetWalletRedeemPointModulePluginRoot();
     }
 
     @Override
