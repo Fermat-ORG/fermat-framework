60f0cdddc6c1dabf80a4fd726a0102dba0ae20c5
Manuel Perez <darkpriestrelative@gmail.com>
2015-09-15 23:03:06 -0430

    Refactor in transaction layer.

---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_distribution/developer/bitdubai/DeveloperBitDubai.java
@@ -0,0 +1,4 @@
+package com.bitdubai.fermat_dap_plugin.layer.transaction.asset_distribution.developer.bitdubai;
+
+public class DeveloperBitDubai {
+}
---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-reception-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/transaction/asset_reception/developer/bitdubai/DeveloperBitdubai.java
@@ -0,0 +1,4 @@
+package com.bitdubai.fermat_dap_plugin.layer.transaction.asset_reception.developer.bitdubai;
+
+public class DeveloperBitdubai {
+}
