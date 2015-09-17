33306318ee9b7041db8ac332ebf80b29603c0960
natalia <natalia_veronica_c@hotmail.com>
2015-09-17 15:52:00 -0300

    Definir Interfaces del CBP - Middleware- Customer

---
+++ fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_middleware/customer/interfaces/CustomerInformation.java
@@ -0,0 +1,38 @@
+package com.bitdubai.fermat_cbp_api.layer.cbp_middleware.customer.interfaces;
+
+import com.bitdubai.fermat_cbp_api.all_definition.enums.CustomerType;
+
+/**
+ * Created by natalia on 17/09/15.
+ */
+
+/**
+ * The interface <code>com.bitdubai.fermat_cbp_api.layer.cbp_middleware.customer.interfaces.CustomerInformation</code>
+ * provides the method to extract information about an crypto customer identity.
+ */
+
+public interface CustomerInformation {
+
+    /**
+     * The method <code>getPublicKey</code> returns the public key of the represented crypto broker
+     * @return the public key of the crypto broker
+     */
+    public String getPublicKey();
+
+    /**
+     * The method <code>getName</code> returns the name of the represented crypto broker
+     *
+     * @return the name of the crypto broker
+     */
+    public String getName();
+
+    /**
+     * The method <code>getProfileImage</code> returns the profile image of the represented crypto broker
+     *
+     * @return the profile image
+     */
+    public byte[] getProfileImage();
+
+
+    public CustomerType getCustomerType();
+}
