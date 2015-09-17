eaddeaf2eb6f9ca460037c11b43d12e1d09ecdb3
mati <mati_fur@hotmail.com>
2015-09-17 16:08:45 -0300

    crypto wallet module changed

--- DMP/plugin/wallet_module/fermat-dmp-plugin-wallet-module-crypto-wallet-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/version_1/structure/CryptoWalletWalletModuleManager.java
+++ DMP/plugin/wallet_module/fermat-dmp-plugin-wallet-module-crypto-wallet-bitdubai/src/main/java/com/bitdubai/fermat_dmp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/version_1/structure/CryptoWalletWalletModuleManager.java
@@ -6,6 +6,7 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrencyVault;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ReferenceWallet;
 import com.bitdubai.fermat_api.layer.all_definition.enums.VaultType;
+import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
 import com.bitdubai.fermat_api.layer.dmp_actor.extra_user.exceptions.CantCreateExtraUserException;
 import com.bitdubai.fermat_api.layer.dmp_actor.extra_user.exceptions.CantGetExtraUserException;
 import com.bitdubai.fermat_api.layer.dmp_actor.extra_user.exceptions.CantSetPhotoException;
@@ -37,6 +38,7 @@ import com.bitdubai.fermat_api.layer.dmp_wallet_module.crypto_wallet.interfaces.
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
 import com.bitdubai.fermat_api.layer.dmp_wallet_module.crypto_wallet.interfaces.PaymentRequest;
+import com.bitdubai.fermat_api.layer.dmp_wallet_module.crypto_wallet.interfaces.WalletInfo;
 import com.bitdubai.fermat_cry_api.layer.crypto_module.crypto_address_book.interfaces.CryptoAddressBookManager;
 import com.bitdubai.fermat_dmp_plugin.layer.wallet_module.crypto_wallet.developer.bitdubai.version_1.exceptions.CantEnrichTransactionException;
 import com.bitdubai.fermat_dmp_plugin.layer.wallet_module.crypto_wallet.developer.bitdubai.version_1.exceptions.CantGetActorException;
@@ -465,12 +467,17 @@ public class CryptoWalletWalletModuleManager implements CryptoWallet, DealsWithB
     }
 
     @Override
-    public List<CryptoWalletWalletContact> listContactOrdererByLastSendTransaction() {
+    public List<CryptoWalletTransaction> listContactOrdererByLastSendTransaction() {
         return null;
     }
 
     @Override
-    public List<CryptoWalletWalletContact> listContactOrdererByLastReceiveTransaction() {
+    public List<CryptoWalletTransaction> listContactOrdererByLastReceiveTransaction() {
+        return null;
+    }
+
+    @Override
+    public WalletInfo getWalletInfo() {
         return null;
     }
 
