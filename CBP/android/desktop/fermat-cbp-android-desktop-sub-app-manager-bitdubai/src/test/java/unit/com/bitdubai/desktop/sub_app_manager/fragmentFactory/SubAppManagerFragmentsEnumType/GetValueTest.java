5154119b8a9b2b7d9192a0d3e8947ec18ec73a06
Nelson Ramirez <nelsonalfo@gmail.com>
2015-09-18 09:50:19 -0430

    correccion de prueba GetValueTest

--- CBP/android/desktop/fermat-cbp-android-desktop-sub-app-manager-bitdubai/src/test/java/unit/com/bitdubai/desktop/sub_app_manager/fragmentFactory/SubAppManagerFragmentsEnumType/GetValueTest.java
+++ CBP/android/desktop/fermat-cbp-android-desktop-sub-app-manager-bitdubai/src/test/java/unit/com/bitdubai/desktop/sub_app_manager/fragmentFactory/SubAppManagerFragmentsEnumType/GetValueTest.java
@@ -7,25 +7,27 @@ import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 /**
+ * Unit Test para el metodo getValue() de SubAppManagerFragmentsEnumType
+ *
  * Created by nelson on 17/09/15.
  */
 public class GetValueTest {
-    private final String ENUM_TYPE_CODE = "MF";
-
     @Test
     public void recibeString_valueFound() throws Exception {
+        final String ENUM_TYPE_CODE = "MF";
+
         SubAppManagerFragmentsEnumType value = SubAppManagerFragmentsEnumType.getValue(ENUM_TYPE_CODE);
         assertThat(value).isNotNull();
     }
 
     @Test
-    public void recibeString_valueNotFound() throws Exception {
+    public void recibeString_valueNotFound_returnNull() throws Exception {
         SubAppManagerFragmentsEnumType value = SubAppManagerFragmentsEnumType.getValue("");
-        assertThat(value).isNotNull();
+        assertThat(value).isNull();
     }
 
     @Test
-    public void recibeNull_valueNotFound() throws Exception {
+    public void recibeNull_valueNotFound_returnNull() throws Exception {
         SubAppManagerFragmentsEnumType value = SubAppManagerFragmentsEnumType.getValue(null);
         assertThat(value).isNull();
     }
