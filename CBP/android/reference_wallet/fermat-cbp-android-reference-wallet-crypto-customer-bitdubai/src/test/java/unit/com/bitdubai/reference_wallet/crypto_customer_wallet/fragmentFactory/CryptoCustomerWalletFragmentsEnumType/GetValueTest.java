9846eac8ec881afb4c39af0820c8997f6fe0e952
Nelson Ramirez <nelsonalfo@gmail.com>
2015-09-21 19:06:24 -0430

    se agregaron las pruebas unitarias funcionales basicas a todos los proyectos Android en plataforma CBP

---
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/test/java/unit/com/bitdubai/reference_wallet/crypto_customer_wallet/fragmentFactory/CryptoCustomerWalletFragmentsEnumType/GetValueTest.java
@@ -0,0 +1,34 @@
+package unit.com.bitdubai.reference_wallet.crypto_customer_wallet.fragmentFactory.CryptoCustomerWalletFragmentsEnumType;
+
+import com.bitdubai.reference_wallet.crypto_customer_wallet.fragmentFactory.CryptoCustomerWalletFragmentsEnumType;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Unit Test para el metodo getValue() de CryptoBrokerWalletFragmentsEnumType
+ *
+ * Created by nelson on 17/09/15.
+ */
+public class GetValueTest {
+    @Test
+    public void recibeString_valueFound() throws Exception {
+        final String ENUM_TYPE_CODE = "MF";
+
+        CryptoCustomerWalletFragmentsEnumType value = CryptoCustomerWalletFragmentsEnumType.getValue(ENUM_TYPE_CODE);
+        assertThat(value).isNotNull();
+    }
+
+    @Test
+    public void recibeString_valueNotFound_returnNull() throws Exception {
+        CryptoCustomerWalletFragmentsEnumType value = CryptoCustomerWalletFragmentsEnumType.getValue("");
+        assertThat(value).isNull();
+    }
+
+    @Test
+    public void recibeNull_valueNotFound_returnNull() throws Exception {
+        CryptoCustomerWalletFragmentsEnumType value = CryptoCustomerWalletFragmentsEnumType.getValue(null);
+        assertThat(value).isNull();
+    }
+}
