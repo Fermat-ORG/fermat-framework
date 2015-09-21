dc3f4fb7a53a10384e6b80704207b735482ed632
Nelson Ramirez <nelsonalfo@gmail.com>
2015-09-21 13:25:03 -0430

    ya corre la prueba MainFragmentTest, se agrego la clase TestActivity para poder correr la prueba

--- CBP/android/desktop/fermat-cbp-android-desktop-sub-app-manager-bitdubai/src/main/java/com/bitdubai/desktop/sub_app_manager/fragments/MainFragment.java
+++ CBP/android/desktop/fermat-cbp-android-desktop-sub-app-manager-bitdubai/src/main/java/com/bitdubai/desktop/sub_app_manager/fragments/MainFragment.java
@@ -23,7 +23,8 @@ public class MainFragment extends FermatFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View layout = inflater.inflate(R.layout.fragment_main, container, false);
+        return layout;
     }
 
 
