1982511d55ff74ca3b00710a029bb83948e89995
Nerio Indriago <nerioindriago@gmail.com>
2015-09-22 17:02:13 -0430

    updateElementsActors #1483 #1484 #1485 25%

---
+++ DAP/plugin/actor/fermat-dap-plugin-actor-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/asset/user/developer/bitdubai/version_1/structure/AssetUserActorRecord.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_plugin.layer.actor.asset.user.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by Nerio on 22/09/15.
+ */
+public class AssetUserActorRecord {
+}
---
+++ DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/redeem/point/developer/bitdubai/version_1/structure/RedeemPointActorRecord.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_plugin.layer.actor.redeem.point.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by Nerio on 22/09/15.
+ */
+public class RedeemPointActorRecord {
+}
