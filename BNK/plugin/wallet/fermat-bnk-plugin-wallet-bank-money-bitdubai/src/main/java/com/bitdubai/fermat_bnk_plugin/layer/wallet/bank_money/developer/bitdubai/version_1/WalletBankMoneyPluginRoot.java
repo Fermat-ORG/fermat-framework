66e754942cccdd5fb687f97ed7e98b8f1fd326d6
root <y.alayn@gmail.com>
2015-09-23 08:24:16 -0430

    Definicion de Interfaces CSH y BNK

--- BNK/plugin/wallet/fermat-bnk-plugin-wallet-bank-money-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/wallet/bank_money/developer/bitdubai/version_1/WalletBankMoneyPluginRoot.java
+++ BNK/plugin/wallet/fermat-bnk-plugin-wallet-bank-money-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/wallet/bank_money/developer/bitdubai/version_1/WalletBankMoneyPluginRoot.java
@@ -1,11 +1,11 @@
-package com.bitdubai.fermat_cbp_plugin.layer.sub_app_module.crypto_broker_community.developer.bitdubai.version_1;
+package com.bitdubai.fermat_bnk_plugin.layer.wallet.bank_money.developer.bitdubai.version_1;
 
 import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
 import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
-import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
+//import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
@@ -21,29 +21,15 @@ import java.util.UUID;
 import java.util.regex.Pattern;
 
 /**
- * Created by natalia on 16.09.15.
+ * Created by Yordin Alayn on 21.09.15.
  */
 
-/**
+public class WalletBankMoneyPluginRoot implements  DealsWithErrors, DealsWithLogger, LogManagerForDevelopers, Service, Plugin {
 
- */
-
-public class CommunitySubAppModuleCryptoBrokenPluginRoot implements  DealsWithErrors, DealsWithLogger, LogManagerForDevelopers, Service, Plugin {
-
-
-    /**
-     * DealsWithErrors interface member variables
-     */
     ErrorManager errorManager;
 
-    /**
-     * DealsWithPluginIdentity interface member variable
-     */
     UUID pluginId;
 
-    /**
-     * DealsWithLogManager interface member variable
-     */
     LogManager logManager;
     static Map<String, LogLevel> newLoggingLevel = new HashMap<String, LogLevel>();
 
@@ -51,48 +37,29 @@ public class CommunitySubAppModuleCryptoBrokenPluginRoot implements  DealsWithEr
     @Override
     public List<String> getClassesFullPath() {
         List<String> returnedClasses = new ArrayList<String>();
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.sub_app_module.crypto_broker_community.developer.bitdubai.version_1.CommunitySubAppModuleCryptoBrokenPluginRoot");
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.sub_app_module.crypto_broker_community.developer.bitdubai.version_1.WalletBankMoneyPluginRoot");
 
-        /**
-         * I return the values.
-         */
         return returnedClasses;
     }
 
     @Override
     public void setLoggingLevelPerClass(Map<String, LogLevel> newLoggingLevel) {
-        /**
-         * I will check the current values and update the LogLevel in those which is different
-         */
         try {
             for (Map.Entry<String, LogLevel> pluginPair : newLoggingLevel.entrySet()) {
-                /**
-                 * if this path already exists in the Root.bewLoggingLevel I'll update the value, else, I will put as new
-                 */
-                if (CommunitySubAppModuleCryptoBrokenPluginRoot.newLoggingLevel.containsKey(pluginPair.getKey())) {
-                    CommunitySubAppModuleCryptoBrokenPluginRoot.newLoggingLevel.remove(pluginPair.getKey());
-                    CommunitySubAppModuleCryptoBrokenPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
+                if (WalletBankMoneyPluginRoot.newLoggingLevel.containsKey(pluginPair.getKey())) {
+                    WalletBankMoneyPluginRoot.newLoggingLevel.remove(pluginPair.getKey());
+                    WalletBankMoneyPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
                 } else {
-                    CommunitySubAppModuleCryptoBrokenPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
+                    WalletBankMoneyPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
                 }
             }
         } catch (Exception exception) {
-           // this.errorManager.reportUnexpectedPluginException(Plugins., UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, exception);
+            // this.errorManager.reportUnexpectedPluginException(Plugins., UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, exception);
         }
     }
 
-
-    /**
-     * Service Interface member variables.
-     */
     ServiceStatus serviceStatus = ServiceStatus.CREATED;
 
-
-    /**
-     * DealsWithError interface implementation
-     *
-     * @param errorManager
-     */
     @Override
     public void setErrorManager(ErrorManager errorManager) {
         this.errorManager = errorManager;
@@ -102,12 +69,6 @@ public class CommunitySubAppModuleCryptoBrokenPluginRoot implements  DealsWithEr
     public void setLogManager(LogManager logManager) {
         this.logManager = logManager;
     }
-
-
-
-    /**
-     * PlatformService Interface implementation.
-     */
 
     @Override
     public void start() throws CantStartPluginException {
@@ -138,31 +99,17 @@ public class CommunitySubAppModuleCryptoBrokenPluginRoot implements  DealsWithEr
         return serviceStatus;
     }
 
-    /**
-     * Plugin interface implementation
-     *
-     * @param uuid
-     */
     @Override
     public void setId(UUID uuid) {
         this.pluginId = uuid;
     }
 
-
-
     public static LogLevel getLogLevelByClass(String className) {
         try {
-            /**
-             * sometimes the classname may be passed dinamically with an $moretext
-             * I need to ignore whats after this.
-             */
             String[] correctedClass = className.split((Pattern.quote("$")));
-            return CommunitySubAppModuleCryptoBrokenPluginRoot.newLoggingLevel.get(correctedClass[0]);
+            return WalletBankMoneyPluginRoot.newLoggingLevel.get(correctedClass[0]);
         } catch (Exception e) {
             System.err.println("CantGetLogLevelByClass: " + e.getMessage());
-            /**
-             * If I couldn't get the correct loggin level, then I will set it to minimal.
-             */
             return DEFAULT_LOG_LEVEL;
         }
     }
