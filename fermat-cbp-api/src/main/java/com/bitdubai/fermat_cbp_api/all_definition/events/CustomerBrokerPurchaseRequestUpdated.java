3dde79c5a5a5bf078424a52ac67bf51836613cce
vlzangel <vlzangel@gmail.com>
2015-09-24 16:13:08 -0430

    Event Request
    Event Request

--- fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/events/CustomerBrokerPurchaseRequestUpdated.java
+++ fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/events/CustomerBrokerPurchaseRequestUpdated.java
@@ -3,12 +3,12 @@ package com.bitdubai.fermat_cbp_api.all_definition.events;
 import com.bitdubai.fermat_api.layer.all_definition.enums.interfaces.FermatEventEnum;
 
 /**
- * Created by jorgegonzalez on 23-09-2015.
+ * Created by angel on 24/9/15.
  */
+public class CustomerBrokerPurchaseRequestUpdated extends CBPEvent {
 
-public class CustomerBrokerPurchaseRequestCreated extends CBPEvent {
-
-    public CustomerBrokerPurchaseRequestCreated(FermatEventEnum eventType) {
+    public CustomerBrokerPurchaseRequestUpdated(FermatEventEnum eventType) {
         super(eventType);
     }
+
 }
