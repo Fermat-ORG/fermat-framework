0b5ea94e686603d7a7016011361ca9f3a6d41ee9
Nerio Indriago <nerioindriago@gmail.com>
2015-09-24 23:07:36 -0430

    ADD plugin-middleware #1527 #1530 30%

---
+++ CCP/plugin/middleware/fermat-ccp-plugin-middleware-money-request-bitdubai/README.md
@@ -0,0 +1,1 @@
+# fermat-dmp-plugin-middleware-money-request-bitdubai 
---
+++ CCP/plugin/middleware/fermat-ccp-plugin-middleware-money-request-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/middleware/money_request/developer/bitdubai/version_1/structure/FiatMoneyRequest.java
@@ -0,0 +1,21 @@
+package com.bitdubai.fermat_ccp_plugin.layer.middleware.money_request.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by loui on 23/02/15.
+ */
+public class FiatMoneyRequest {
+    
+    public void send(){
+        
+    }
+    
+    public void reject(){
+        
+    }
+    
+    public void accept(){
+        
+    }
+    
+    
+}
