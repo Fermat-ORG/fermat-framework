935015b3b844c29159e8b95de69f95703abf4d99
Nerio Indriago <nerioindriago@gmail.com>
2015-09-24 23:05:23 -0430

    ADD plugin-basic_wallet #1527 #1530 30%

---
+++ CCP/plugin/basic_wallet/fermat-ccp-plugin-basic-wallet-bitcoin-wallet-bitdubai/src/test/java/unit/com/bitdubai/fermat_ccp_plugin/layer/basic_wallet/bitcoin_wallet/developer/bitdubai/version_1/structure/mocks/MockWalletIdsFile.java
@@ -0,0 +1,38 @@
+package unit.com.bitdubai.fermat_ccp_plugin.layer.basic_wallet.bitcoin_wallet.developer.bitdubai.version_1.structure.mocks;
+
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginTextFile;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantLoadFileException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantPersistFileException;
+
+/**
+ * Created by jorgegonzalez on 2015.07.10..
+ */
+public class MockWalletIdsFile implements PluginTextFile {
+
+
+
+    @Override
+    public String getContent() {
+        return null;
+    }
+
+    @Override
+    public void setContent(String content) {
+
+    }
+
+    @Override
+    public void persistToMedia() throws CantPersistFileException {
+
+    }
+
+    @Override
+    public void loadFromMedia() throws CantLoadFileException {
+
+    }
+
+    @Override
+    public void delete() {
+        
+    }
+}
