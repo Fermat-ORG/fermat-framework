14ae0449e492d9ba44aa8463c7e7c9ad31b0e377
Manuel Perez <darkpriestrelative@gmail.com>
2015-09-29 17:24:07 -0430

    Implemented A test about the contract expiration date #1517

---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/digital_asset_distributor/IsValidContractTest.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.digital_asset_distributor;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 29/09/15.
+ */
+public class IsValidContractTest {
+}
