366f0cde0aaf9bd620709ed7f70e487c80506a91
Manuel Perez <darkpriestrelative@gmail.com>
2015-09-29 17:44:31 -0430

    Implemented A test about the contract expiration date #1517

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version_1/structure/DigitalAssetDistributor.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version_1/structure/DigitalAssetDistributor.java
@@ -3,10 +3,17 @@ package com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_dis
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FilePrivacy;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginTextFile;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantCreateFileException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantPersistFileException;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.AssetVaultManager;
+import com.bitdubai.fermat_dap_api.layer.all_definition.contracts.ContractProperty;
 import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.DigitalAsset;
 import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.DigitalAssetContract;
+import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.DigitalAssetContractPropertiesConstants;
 import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.DigitalAssetMetadata;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces.ActorAssetUser;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.CantCreateDigitalAssetFileException;
@@ -18,6 +25,8 @@ import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_dist
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.UnexpectedPluginExceptionSeverity;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -31,12 +40,13 @@ public class DigitalAssetDistributor {
     ErrorManager errorManager;
     final String LOCAL_STORAGE_PATH="digital-asset-distribution/";
     //String digitalAssetLocalFilePath;
-    //String digitalAssetFileName;
+    private final String digitalAssetFileName="digital-asset.xml";
+    private final String digitalAssetMetadataFileName="digital-asset-metadata.xml";
     String digitalAssetFileStoragePath;
-    //String digitalAssetMetadataFileName;
     //String digitalAssetMetadataFileStoragePath;
     AssetDistributionDao assetDistributionDao;
     PluginFileSystem pluginFileSystem;
+    UUID pluginId;
 
     public DigitalAssetDistributor(AssetVaultManager assetVaultManager,
                                    ErrorManager errorManager,
@@ -45,6 +55,8 @@ public class DigitalAssetDistributor {
                                    PluginFileSystem pluginFileSystem) throws CantExecuteDatabaseOperationException {
         this.assetVaultManager=assetVaultManager;
         this.errorManager=errorManager;
+        this.pluginFileSystem=pluginFileSystem;
+        this.pluginId=pluginId;
         assetDistributionDao=new AssetDistributionDao(pluginDatabaseSystem,pluginId);
     }
 
@@ -78,13 +90,23 @@ public class DigitalAssetDistributor {
             persistDigitalAsset(digitalAssetMetadata, actorAssetUser);
             DigitalAsset digitalAsset=digitalAssetMetadata.getDigitalAsset();
             checkAvailableBalanceInAssetVault(digitalAsset.getGenesisAmount());
+            DigitalAssetContract digitalAssetContract=digitalAsset.getContract();
+            if(!isValidContract(digitalAssetContract)){
+                throw new CantDeliverDigitalAssetException("The DigitalAssetContract is not valid, the expiration date has passed");
+            }
         } catch (CantPersistDigitalAssetException exception) {
             throw new CantDeliverDigitalAssetException(exception, "Delivering digital assets", "Cannot persist digital asset into database");
+        } catch (CantCreateDigitalAssetFileException exception) {
+            throw new CantDeliverDigitalAssetException(exception, "Delivering digital assets", "Cannot persist digital asset into local storage");
         }
     }
 
-    private void persistDigitalAsset(DigitalAssetMetadata digitalAssetMetadata, ActorAssetUser actorAssetUser) throws CantPersistDigitalAssetException {
-        this.assetDistributionDao.persistDigitalAsset(digitalAssetMetadata.getGenesisTransaction(),"", digitalAssetMetadata.getDigitalAssetHash(), actorAssetUser.getPublicKey());
+    //TODO: create a method that delete the digital asset from local storage.
+
+    private void persistDigitalAsset(DigitalAssetMetadata digitalAssetMetadata, ActorAssetUser actorAssetUser) throws CantPersistDigitalAssetException, CantCreateDigitalAssetFileException {
+        setDigitalAssetLocalFilePath(digitalAssetMetadata);
+        this.assetDistributionDao.persistDigitalAsset(digitalAssetMetadata.getGenesisTransaction(), this.digitalAssetFileStoragePath, digitalAssetMetadata.getDigitalAssetHash(), actorAssetUser.getPublicKey());
+        persistInLocalStorage(digitalAssetMetadata);
     }
 
     private boolean checkAvailableBalanceInAssetVault(long genesisAmount){
@@ -93,16 +115,34 @@ public class DigitalAssetDistributor {
     }
 
     private boolean isValidContract(DigitalAssetContract digitalAssetContract){
-        //TODO: implement this 29/09/2015
-        return true;
+        //For now, we going to check, only, the expiration date
+        ContractProperty contractProperty=digitalAssetContract.getContractProperty(DigitalAssetContractPropertiesConstants.EXPIRATION_DATE);
+        Timestamp expirationDate= (Timestamp) contractProperty.getValue();
+        Date date= new java.util.Date();
+        return expirationDate.after(new Timestamp(date.getTime()));
     }
 
     //TODO: preguntar al thunder team si sería conveniente persistir en archivo el digitalAssetMetadata
     private void persistInLocalStorage(DigitalAssetMetadata digitalAssetMetadata) throws CantCreateDigitalAssetFileException {
         //DigitalAsset Path structure: digital-asset-distribution/hash/digital-asset.xml
         //DigitalAssetMetadata Path structure: digital-asset-distribution/hash/digital-asset-metadata.xml
-        setDigitalAssetLocalFilePath(digitalAssetMetadata);
+        DigitalAsset digitalAsset=digitalAssetMetadata.getDigitalAsset();
+        try{
+            String digitalAssetInnerXML=digitalAsset.toString();
+            persistXMLStringInLocalStorage(digitalAssetInnerXML,digitalAssetFileName);
+            String digitalAssetMetadataInnerXML=digitalAssetMetadata.toString();
+            persistXMLStringInLocalStorage(digitalAssetMetadataInnerXML,digitalAssetMetadataFileName);
+        } catch (CantPersistFileException | CantCreateFileException exception) {
+            throw new CantCreateDigitalAssetFileException(exception, "Persisting the digital asset objects in local storage", "Cannot create or persist the file");
+        }
 
+    }
+
+    private void persistXMLStringInLocalStorage(String innerXML, String fileName) throws CantCreateFileException, CantPersistFileException {
+        //TODO: for testing the FilePrivacy is public, change to private in production
+        PluginTextFile digitalAssetFile=this.pluginFileSystem.createTextFile(this.pluginId, this.digitalAssetFileStoragePath, fileName, FilePrivacy.PUBLIC, FileLifeSpan.PERMANENT);
+        digitalAssetFile.setContent(innerXML);
+        digitalAssetFile.persistToMedia();
     }
 
     private void setDigitalAssetLocalFilePath(DigitalAssetMetadata digitalAssetMetadata){
