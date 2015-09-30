e0d22f55d02de5513c197a297198e061615f4434
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-09-29 22:27:01 -0300

    Included new methods for Asset Distribution in Asset Vault

--- BCH/plugin/crypto_vault/fermat-bch-plugin-crypto-vault-assets-over-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/asset_vault/developer/bitdubai/version_1/AssetVaultPluginRoot.java
+++ BCH/plugin/crypto_vault/fermat-bch-plugin-crypto-vault-assets-over-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/asset_vault/developer/bitdubai/version_1/AssetVaultPluginRoot.java
@@ -5,12 +5,14 @@ import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
+import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.DealsWithPluginFileSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_api.layer.all_definition.enums.BlockchainNetworkType;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.AssetVaultManager;
+import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.exceptions.CantGetGenesisTransactionException;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.exceptions.GetNewCryptoAddressException;
 import com.bitdubai.fermat_bch_plugin.layer.asset_vault.developer.bitdubai.version_1.structure.AssetCryptoVaultManager;
 
@@ -98,5 +100,15 @@ public class AssetVaultPluginRoot implements AssetVaultManager, DealsWithPluginD
 
     @Override public CryptoAddress getNewAssetVaultCryptoAddress(BlockchainNetworkType blockchainNetworkType) throws GetNewCryptoAddressException {
         return assetCryptoVaultManager.getNewAssetVaultCryptoAddress(blockchainNetworkType);
+    }
+
+    @Override
+    public CryptoTransaction getGenesisTransaction(String transactionId) throws CantGetGenesisTransactionException {
+        return null;
+    }
+
+    @Override
+    public long getAvailableBalanceForTransaction(String genesisTransaction) {
+        return 0;
     }
 }
