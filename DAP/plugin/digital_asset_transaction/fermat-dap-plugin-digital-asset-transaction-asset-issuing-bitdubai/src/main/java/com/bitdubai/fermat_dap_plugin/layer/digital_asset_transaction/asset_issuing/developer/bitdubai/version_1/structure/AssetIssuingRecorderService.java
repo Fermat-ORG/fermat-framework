d3a5d29462dfdfa6abe74c3c63741d7630df5eae
Manuel Perez <darkpriestrelative@gmail.com>
2015-09-29 23:06:20 -0430

    Working on Asset Issuing plugin event handler #1472

---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_issuing/developer/bitdubai/version_1/structure/AssetIssuingRecorderService.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_issuing.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 29/09/15.
+ */
+public class AssetIssuingRecorderService {
+}
---
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_transaction/AssetTransactionService.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_api.layer.dap_transaction;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 29/09/15.
+ */
+public class AssetTransactionService {
+}
