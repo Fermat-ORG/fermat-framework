f9860768fffe3eb0536bed244f1532c0b7005ede
yalayn <y.alayn@gmail.com>
2015-09-29 20:34:15 -0430

    Interface Agent

--- fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_agent/crypto_broker/interfaces/CryptoBrokerManager.java
+++ fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_agent/crypto_broker/interfaces/CryptoBrokerManager.java
@@ -1,8 +1,15 @@
 package com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.interfaces;
 
 import com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.exceptions.CantCalculateBasePriceMerchandiseCurrencyException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.exceptions.CantCalculateSpreadException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.exceptions.CantCalculateSuggestedPriceSaleException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.exceptions.CantCalculateSuggestedPricePurchaseException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.exceptions.CantCalculateProtecionInvestmentSaleException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_agent.crypto_broker.exceptions.CantCalculateProtecionInvestmentPurchaseException;
+
+
 /**
- * Created by Yordin Alayn on 29/09/15.
+ * Created by Yordin Alayn on 29.09.15.
  */
 public interface CryptoBrokerManager {
 
@@ -14,29 +21,29 @@ public interface CryptoBrokerManager {
     CryptoBroker Spread(
          final Float percentageSale
         ,final Float percentagePurchase
-    ) throws CantCalculateBasePriceMerchandiseCurrencyException;
+    ) throws CantCalculateSpreadException;
 
     CryptoBroker SuggestedPriceSale(
          final Float percentageSale
         ,final Float basePriceMerchandiseCurrency
-    ) throws CantCalculateBasePriceMerchandiseCurrencyException;
+    ) throws CantCalculateSuggestedPriceSaleException;
 
     CryptoBroker SuggestedPricePurchase(
          final Float percentagePurchase
         ,final Float basePriceMerchandiseCurrency
-    ) throws CantCalculateBasePriceMerchandiseCurrencyException;
+    ) throws CantCalculateSuggestedPricePurchaseException;
 
     CryptoBroker ProtecionInvestmentSale(
          final Double referenceCurrency
         ,final Double priceMarketCurrent
         ,final Double suggestedPriceSale
         ,final Double priceSale
-    ) throws CantCalculateBasePriceMerchandiseCurrencyException;
+    ) throws CantCalculateProtecionInvestmentSaleException;
 
     CryptoBroker ProtecionInvestmentPurchase(
-            final Double referenceCurrency
-            ,final Double priceMarketCurrent
-            ,final Double suggestedPricePurchase
-            ,final Double pricePurchase
-    ) throws CantCalculateBasePriceMerchandiseCurrencyException;
+         final Double referenceCurrency
+        ,final Double priceMarketCurrent
+        ,final Double suggestedPricePurchase
+        ,final Double pricePurchase
+    ) throws CantCalculateProtecionInvestmentPurchaseException;
 }
