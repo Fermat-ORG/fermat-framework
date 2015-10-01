d220e4275a9a599d4c4a6109b53b0f6b980fda48
Francisco Vasquez <fvasquezjatar@gmail.com>
2015-10-01 10:48:37 -0430

    MainFragment override init views

--- DAP/android/sub_app/fermat-dap-android-sub-app-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_asset_factory_bitdubai/fragments/MainFragment.java
+++ DAP/android/sub_app/fermat-dap-android-sub-app-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_asset_factory_bitdubai/fragments/MainFragment.java
@@ -1,13 +1,28 @@
 package com.bitdubai.fermat_dap_android_sub_app_asset_factory_bitdubai.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 
-import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
+import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_android_api.ui.enums.FermatRefreshTypes;
+import com.bitdubai.fermat_android_api.ui.fragments.FermatListFragment;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantCreateFileException;
 import com.bitdubai.fermat_dap_android_sub_app_asset_factory_bitdubai.R;
+import com.bitdubai.fermat_dap_android_sub_app_asset_factory_bitdubai.adapters.AssetFactoryAdapter;
+import com.bitdubai.fermat_dap_android_sub_app_asset_factory_bitdubai.sessions.AssetFactorySession;
+import com.bitdubai.fermat_dap_android_sub_app_asset_factory_bitdubai.util.CommonLogger;
+import com.bitdubai.fermat_dap_api.layer.all_definition.enums.State;
+import com.bitdubai.fermat_dap_api.layer.dap_middleware.dap_asset_factory.exceptions.CantGetAssetFactoryException;
+import com.bitdubai.fermat_dap_api.layer.dap_middleware.dap_asset_factory.interfaces.AssetFactory;
+import com.bitdubai.fermat_dap_api.layer.dap_module.asset_factory.interfaces.AssetFactoryModuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Main Fragment
@@ -15,24 +30,131 @@ import com.bitdubai.fermat_dap_android_sub_app_asset_factory_bitdubai.R;
  * @author Francisco Vásquez
  * @version 1.0
  */
-public class MainFragment extends FermatFragment {
+public class MainFragment extends FermatListFragment<AssetFactory> {
 
-    /**
-     * UI
-     */
-    private View rootView;
+    private ArrayList<AssetFactory> dataSet;
 
+    private AssetFactoryAdapter adapter;
+    private AssetFactoryModuleManager manager;
 
     public static MainFragment newInstance() {
         return new MainFragment();
     }
 
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.main_fragment, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            manager = ((AssetFactorySession) subAppsSession).getManager();
+        } catch (Exception ex) {
+            CommonLogger.exception(TAG, ex.getMessage(), ex);
+        }
 
-        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        swipeRefreshLayout.setRefreshing(true);
+        onRefresh();
+    }
+
+    @Override
+    protected void initViews(View layout) {
+        Log.i(TAG, "recycler view setup");
+        if (layout == null)
+            return;
+        recyclerView = (RecyclerView) layout.findViewById(R.id.recyclerView);
+        if (recyclerView != null) {
+            recyclerView.setHasFixedSize(recyclerHasFixedSize());
+            layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+            recyclerView.setLayoutManager(layoutManager);
+            adapter = new AssetFactoryAdapter(getActivity());
+            recyclerView.setAdapter(adapter);
+        }
+        swipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(getSwipeRefreshLayoutId());
+        if (swipeRefreshLayout != null) {
+            isRefreshing = false;
+            swipeRefreshLayout.setRefreshing(false);
+            swipeRefreshLayout.setColorSchemeColors(Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE);
+            swipeRefreshLayout.setOnRefreshListener(this);
+        }
+    }
+
+    @Override
+    protected boolean hasMenu() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.main_fragment;
+    }
+
+    @Override
+    protected int getSwipeRefreshLayoutId() {
+        return R.id.swipe;
+    }
+
+    @Override
+    protected int getRecyclerLayoutId() {
+        return R.id.recyclerView;
+    }
+
+    @Override
+    protected boolean recyclerHasFixedSize() {
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onPostExecute(Object... result) {
+        isRefreshing = false;
+        if (isAttached) {
+            swipeRefreshLayout.setRefreshing(false);
+            if (result != null && result.length > 0) {
+                dataSet = (ArrayList<AssetFactory>) result[0];
+                adapter.changeDataSet(dataSet);
+                if (dataSet == null || dataSet.isEmpty()) {
+                    //// TODO: 01/10/15 Change to Create new Asset Factory
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onErrorOccurred(Exception ex) {
+        isRefreshing = false;
+        if (isAttached) {
+            swipeRefreshLayout.setRefreshing(false);
+        }
+    }
+
+    @Override
+    public FermatAdapter getAdapter() {
+        if (adapter == null) {
+            adapter = new AssetFactoryAdapter(getActivity());
+        }
+        return adapter;
+    }
+
+    @Override
+    public RecyclerView.LayoutManager getLayoutManager() {
+        if (layoutManager == null) {
+            layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        }
+        return layoutManager;
+    }
+
+    @Override
+    public List<AssetFactory> getMoreDataAsync(FermatRefreshTypes refreshType, int pos) {
+        try {
+            return manager.getAssetFactoryByState(State.DRAFT);
+        } catch (CantGetAssetFactoryException e) {
+            CommonLogger.exception(TAG, e.getMessage(), e);
+        } catch (CantCreateFileException e) {
+            CommonLogger.exception(TAG, e.getMessage(), e);
+        }
+        return null;
     }
 }
