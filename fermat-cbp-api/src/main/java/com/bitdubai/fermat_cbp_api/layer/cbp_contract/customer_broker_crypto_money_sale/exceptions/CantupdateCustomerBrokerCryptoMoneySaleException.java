989d8ae2a5110bb1d028d26c2205470e2caab7da
vlzangel <vlzangel@gmail.com>
2015-10-02 16:40:01 -0430

    CantupdateCustomerBrokerCryptoMoneySaleException
    CantupdateCustomerBrokerCryptoMoneySaleException

--- fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_contract/customer_broker_crypto_money_sale/exceptions/CantupdateCustomerBrokerCryptoMoneySaleException.java
+++ fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_contract/customer_broker_crypto_money_sale/exceptions/CantupdateCustomerBrokerCryptoMoneySaleException.java
@@ -5,9 +5,9 @@ import com.bitdubai.fermat_api.FermatException;
 /**
  * Created by angel on 18/9/15.
  */
-public class CantupdateStatusCustomerBrokerCryptoMoneySaleException extends FermatException {
+public class CantupdateCustomerBrokerCryptoMoneySaleException extends FermatException {
 
-    public static final String DEFAULT_MESSAGE = "CAN'T UPDATE THE STATUS THE CONTRACT CUSTOMER BROKER CRYPTO MONEY SALE";
+    public static final String DEFAULT_MESSAGE = "CAN'T UPDATE THE CONTRACT CUSTOMER BROKER CRYPTO MONEY SALE";
 
     /**
      * This is the constructor that every inherited FermatException must implement
@@ -17,7 +17,8 @@ public class CantupdateStatusCustomerBrokerCryptoMoneySaleException extends Ferm
      * @param context        a String that provides the values of the variables that could have affected the exception
      * @param possibleReason an explicative reason of why we believe this exception was most likely thrown
      */
-    public CantupdateStatusCustomerBrokerCryptoMoneySaleException(String message, Exception cause, String context, String possibleReason) {
+    public CantupdateCustomerBrokerCryptoMoneySaleException(String message, Exception cause, String context, String possibleReason) {
         super(message, cause, context, possibleReason);
     }
 }
+
