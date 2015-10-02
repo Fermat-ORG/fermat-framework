5316cf79ab12ab2daa6a96be5715915f0268d5a8
furszy <mati_fur@hotmail.com>
2015-10-02 20:17:27 -0300

    minor changes

--- CCP/android/reference_wallet/fermat-ccp-android-reference-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendTransactionsFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-reference-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendTransactionsFragment.java
@@ -18,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -171,13 +172,13 @@ public class SendTransactionsFragment extends FermatWalletListFragment<CryptoWal
                         Fx.slide_up(getActivity(), linear_layout_send_form);
                         linear_layout_send_form.setVisibility(View.GONE);
                         if(lstCryptoWalletTransactions.isEmpty()){
-                            empty.setVisibility(View.VISIBLE);
+                            ((ImageView)rootView.findViewById(R.id.image_empty)).setVisibility(View.VISIBLE);
                         }
                         //showDialog();
                     } else {
                         linear_layout_send_form.setVisibility(View.VISIBLE);
                         Fx.slide_down(getActivity(), linear_layout_send_form);
-                        empty.setVisibility(View.GONE);
+                        ((ImageView)rootView.findViewById(R.id.image_empty)).setVisibility(View.GONE);
                     }
 
                 }
