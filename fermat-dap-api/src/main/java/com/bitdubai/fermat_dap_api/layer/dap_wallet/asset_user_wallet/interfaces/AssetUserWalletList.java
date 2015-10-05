50910d95507dfbe7d9e76e6f19481d4cfc9fb295
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-05 08:57:18 -0430

    Asset Wser Wallet #1470 10% #1653 20%

--- fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_wallet/asset_user_wallet/interfaces/AssetUserWalletList.java
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_wallet/asset_user_wallet/interfaces/AssetUserWalletList.java
@@ -4,4 +4,18 @@ package com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_user_wallet.interface
  * Created by franklin on 05/10/15.
  */
 public interface AssetUserWalletList {
+    public String getAssetPublicKey();
+    public void setAssetPublicKey(String assetPublicKey);
+
+    public String getDescription();
+    public void setDescription(String description);
+
+    public String getName();
+    public void setName(String name);
+
+    public long getBookBalance();
+    public void setBookBalance(long bookBalance);
+
+    public long getAvailableBalance();
+    public void setAvailableBalance(long availableBalance);
 }
