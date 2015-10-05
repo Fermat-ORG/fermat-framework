68cb38fd8f1f5d154237e82cfed36c77f6908f1f
hendry19901990 <elnegroevaristo@gmail.com>
2015-10-05 17:05:13 -0430

    Falto 1

--- DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/user/developer/bitdubai/version_1/AssetUserPluginRoot.java
+++ DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/user/developer/bitdubai/version_1/AssetUserPluginRoot.java
@@ -33,122 +33,136 @@ import java.util.UUID;
 * @version 1.0
 */
 public class AssetUserPluginRoot implements DealsWithErrors, DealsWithEvents, DealsWithPluginDatabaseSystem, DealsWithPluginFileSystem, LogManagerForDevelopers, Plugin, Service, Serializable {
+    /**
+     * Service Interface member variables.
+     */
+    ServiceStatus serviceStatus = ServiceStatus.CREATED;
+    /**
+     * DealsWithPlatformDatabaseSystem Interface member variables.
+     */
+    PluginDatabaseSystem pluginDatabaseSystem;
+    /**
+     * FileSystem Interface member variables.
+     */
+    PluginFileSystem pluginFileSystem;
+    /**
+     * Plugin Interface member variables.
+     */
+    UUID pluginId;
+    /**
+     * DealsWithErrors Interface member variables.
+     */
+    ErrorManager errorManager;
+    /**
+     * DealsWithEvents Interface member variables.
+     */
+    EventManager eventManager;
+    List<FermatEventListener> listenersAdded = new ArrayList<>();
+    /**
+     * DealsWithLogger interface member variable
+     */
+    static Map<String, LogLevel> newLoggingLevel = new HashMap<String, LogLevel>();
+
+    @Override
+    public void setEventManager(EventManager DealsWithEvents) {
+        this.eventManager = DealsWithEvents;
+    }
+
+    /**
+     * DealsWithErrors Interface implementation.
+     */
+    @Override
+    public void setErrorManager(ErrorManager errorManager) {
+        this.errorManager = errorManager;
+    }
+
+    /**
+     * DealsWithPluginDatabaseSystem interface implementation.
+     */
+    @Override
+    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+        this.pluginDatabaseSystem = pluginDatabaseSystem;
+    }
+
+    /**
+     * DealWithPluginFileSystem Interface implementation.
+     */
+    @Override
+    public void setPluginFileSystem(PluginFileSystem pluginFileSystem) {
+        this.pluginFileSystem = pluginFileSystem;
+    }
+
+    @Override
+    public void setId(UUID pluginId) {
+        this.pluginId = pluginId;
+    }
+
+    @Override
+    public List<String> getClassesFullPath() {
+        List<String> returnedClasses = new ArrayList<String>();
+        returnedClasses.add("com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.issuer.developer.bitdubai.version_1.AssetUserPluginRoot");
 /**
-* Service Interface member variables.
-*/
-ServiceStatus serviceStatus = ServiceStatus.CREATED;
+ * I return the values.
+ */
+        return returnedClasses;
+    }
+
+    @Override
+    public void setLoggingLevelPerClass(Map<String, LogLevel> newLoggingLevel) {
 /**
-* DealsWithPlatformDatabaseSystem Interface member variables.
-*/
-PluginDatabaseSystem pluginDatabaseSystem;
+ * Modify by Manuel on 25/07/2015
+ * I will wrap all this method within a try, I need to catch any generic java Exception
+ */
+        try {
 /**
-* FileSystem Interface member variables.
-*/
-PluginFileSystem pluginFileSystem;
+ * I will check the current values and update the LogLevel in those which is different
+ */
+            for (Map.Entry<String, LogLevel> pluginPair : newLoggingLevel.entrySet()) {
 /**
-* Plugin Interface member variables.
-*/
-UUID pluginId;
-/**
-* DealsWithErrors Interface member variables.
-*/
-ErrorManager errorManager;
-/**
-* DealsWithEvents Interface member variables.
-*/
-EventManager eventManager;
-List<FermatEventListener> listenersAdded = new ArrayList<>();
-/**
-* DealsWithLogger interface member variable
-*/
-static Map<String, LogLevel> newLoggingLevel = new HashMap<String, LogLevel>();
-@Override
-public void setEventManager(EventManager DealsWithEvents) {
-this.eventManager = DealsWithEvents;
-}
-/**
-* DealsWithErrors Interface implementation.
-*/
-@Override
-public void setErrorManager(ErrorManager errorManager) {
-this.errorManager = errorManager;
-}
-/**
-* DealsWithPluginDatabaseSystem interface implementation.
-*/
-@Override
-public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
-this.pluginDatabaseSystem = pluginDatabaseSystem;
-}
-/**
-* DealWithPluginFileSystem Interface implementation.
-*/
-@Override
-public void setPluginFileSystem(PluginFileSystem pluginFileSystem) {
-this.pluginFileSystem = pluginFileSystem;
-}
-@Override
-public void setId(UUID pluginId) {
-this.pluginId = pluginId;
-}
-@Override
-public List<String> getClassesFullPath() {
-List<String> returnedClasses = new ArrayList<String>();
-returnedClasses.add("com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.issuer.developer.bitdubai.version_1.AssetUserPluginRoot");
-/**
-* I return the values.
-*/
-return returnedClasses;
-}
-@Override
-public void setLoggingLevelPerClass(Map<String, LogLevel> newLoggingLevel) {
-/**
-* Modify by Manuel on 25/07/2015
-* I will wrap all this method within a try, I need to catch any generic java Exception
-*/
-try {
-/**
-* I will check the current values and update the LogLevel in those which is different
-*/
-for (Map.Entry<String, LogLevel> pluginPair : newLoggingLevel.entrySet()) {
-/**
-* if this path already exists in the Root.bewLoggingLevel I'll update the value, else, I will put as new
-*/
-if (AssetUserPluginRoot.newLoggingLevel.containsKey(pluginPair.getKey())) {
-AssetUserPluginRoot.newLoggingLevel.remove(pluginPair.getKey());
-AssetUserPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
-} else {
-AssetUserPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
-}
-}
-} catch (Exception exception) {
-FermatException e = new CantGetLogTool(CantGetLogTool.DEFAULT_MESSAGE, FermatException.wrapException(exception), "setLoggingLevelPerClass: " + AssetUserPluginRoot.newLoggingLevel, "Check the cause");
+ * if this path already exists in the Root.bewLoggingLevel I'll update the value, else, I will put as new
+ */
+                if (AssetUserPluginRoot.newLoggingLevel.containsKey(pluginPair.getKey())) {
+                    AssetUserPluginRoot.newLoggingLevel.remove(pluginPair.getKey());
+                    AssetUserPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
+                } else {
+                    AssetUserPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
+                }
+            }
+        } catch (Exception exception) {
+            FermatException e = new CantGetLogTool(CantGetLogTool.DEFAULT_MESSAGE, FermatException.wrapException(exception), "setLoggingLevelPerClass: " + AssetUserPluginRoot.newLoggingLevel, "Check the cause");
 // this.errorManager.reportUnexpectedAddonsException(Addons.EXTRA_USER, UnexpectedAddonsExceptionSeverity.DISABLES_THIS_ADDONS, e);
-}
-}
-@Override
-public void start() throws CantStartPluginException {
-try {
-this.serviceStatus = ServiceStatus.STARTED;
-} catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void start() throws CantStartPluginException {
+        try {
+            this.serviceStatus = ServiceStatus.STARTED;
+        } catch (Exception e) {
 // errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_INTRA_USER_ACTOR, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
 // throw new CantStartPluginException(e, Plugins.BITDUBAI_INTRA_USER_ACTOR);
-}
-}
-@Override
-public void pause() {
-this.serviceStatus = ServiceStatus.PAUSED;
-}
-@Override
-public void resume() {
-this.serviceStatus = ServiceStatus.STARTED;
-}
-@Override
-public void stop() {
-this.serviceStatus = ServiceStatus.STOPPED;
-}
-@Override
-public ServiceStatus getStatus() {
-return serviceStatus;
+        }
+    }
+
+    @Override
+    public void pause() {
+        this.serviceStatus = ServiceStatus.PAUSED;
+    }
+
+    @Override
+    public void resume() {
+        this.serviceStatus = ServiceStatus.STARTED;
+    }
+
+    @Override
+    public void stop() {
+        this.serviceStatus = ServiceStatus.STOPPED;
+    }
+
+    @Override
+    public ServiceStatus getStatus() {
+        return serviceStatus;
+    }
+
 }
 
