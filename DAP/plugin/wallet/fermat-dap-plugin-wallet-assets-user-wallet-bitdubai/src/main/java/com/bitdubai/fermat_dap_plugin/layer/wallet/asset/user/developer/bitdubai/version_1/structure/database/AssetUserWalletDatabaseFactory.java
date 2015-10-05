daa26c159af0821debc6c6bfda4366e9ccc9201b
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-05 10:00:44 -0430

    Asset Wser Wallet #1470 15% #1654 10%

---
+++ DAP/plugin/wallet/fermat-dap-plugin-wallet-assets-user-wallet-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/wallet/asset/user/developer/bitdubai/version_1/structure/database/AssetUserWalletDatabaseFactory.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_plugin.layer.wallet.asset.user.developer.bitdubai.version_1.structure.database;
+
+/**
+ * Created by franklin on 05/10/15.
+ */
+public class AssetUserWalletDatabaseFactory {
+}
