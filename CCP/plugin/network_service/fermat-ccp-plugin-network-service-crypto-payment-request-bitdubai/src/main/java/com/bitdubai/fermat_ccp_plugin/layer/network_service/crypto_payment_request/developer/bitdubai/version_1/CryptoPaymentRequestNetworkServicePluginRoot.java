ab0dc425311f4d0e5c5d88f004ce18342017645f
lnacosta <laion.cj91@gmail.com>
2015-10-06 09:32:08 -0300

    implemented simple methods for crypto payment request manager interface

--- CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-payment-request-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_payment_request/developer/bitdubai/version_1/CryptoPaymentRequestNetworkServicePluginRoot.java
+++ CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-payment-request-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_payment_request/developer/bitdubai/version_1/CryptoPaymentRequestNetworkServicePluginRoot.java
@@ -2,16 +2,35 @@ package com.bitdubai.fermat_ccp_plugin.layer.network_service.crypto_payment_requ
 
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Actors;
+import com.bitdubai.fermat_api.layer.all_definition.enums.BlockchainNetworkType;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.enums.RequestProtocolState;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantConfirmRequestException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantGetRequestException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantInformApprovalException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantInformDenialException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantInformReceptionException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantInformRefusalException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantListPendingRequestsException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.CantSendRequestException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.exceptions.RequestNotFoundException;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.interfaces.CryptoPaymentRequest;
+import com.bitdubai.fermat_ccp_api.layer.network_service.crypto_payment_request.interfaces.CryptoPaymentRequestManager;
+import com.bitdubai.fermat_ccp_plugin.layer.network_service.crypto_payment_request.developer.bitdubai.version_1.database.CryptoPaymentRequestNetworkServiceDao;
+import com.bitdubai.fermat_ccp_plugin.layer.network_service.crypto_payment_request.developer.bitdubai.version_1.exceptions.CantDeleteRequestException;
+import com.bitdubai.fermat_ccp_plugin.layer.network_service.crypto_payment_request.developer.bitdubai.version_1.exceptions.CantListRequestsException;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.UnexpectedPluginExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.event_manager.interfaces.DealsWithEvents;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.event_manager.interfaces.EventManager;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -20,47 +39,146 @@ import java.util.UUID;
  *
  * Created by Leon Acosta - (laion.cj91@gmail.com) on 01/10/2015.
  */
-public class CryptoPaymentRequestNetworkServicePluginRoot implements DealsWithErrors, DealsWithEvents, DealsWithPluginDatabaseSystem, Plugin, Service {
+public class CryptoPaymentRequestNetworkServicePluginRoot implements
+        CryptoPaymentRequestManager,
+        DealsWithErrors,
+        DealsWithEvents,
+        DealsWithPluginDatabaseSystem,
+        Plugin,
+        Service {
 
-    /*
+    /**
      * DealsWithErrors Interface member variables.
      */
     private ErrorManager errorManager;
 
-    /*
+    /**
      * DealsWithEvents Interface member variables
      */
     private EventManager eventManager;
-    /*
+
+    /**
      * DealsWithPluginDatabaseSystem Interface member variables.
      */
     private PluginDatabaseSystem pluginDatabaseSystem;
 
-    /*
-     * OutgoingIntraActorManager Interface member variables.
-     */
-
-    /*
+    /**
      * Plugin Interface member variables.
      */
     private UUID pluginId;
 
-
-    /*
+    /**
      * Service Interface member variables.
      */
     private ServiceStatus serviceStatus = ServiceStatus.CREATED;
 
+
+    private CryptoPaymentRequestNetworkServiceDao cryptoPaymentRequestNetworkServiceDao;
+
+    @Override
+    public void sendCryptoPaymentRequest(String                walletPublicKey  ,
+                                         String                identityPublicKey,
+                                         Actors                identityType     ,
+                                         String                actorPublicKey   ,
+                                         Actors                actorType        ,
+                                         CryptoAddress         cryptoAddress    ,
+                                         String                description      ,
+                                         long                  amount           ,
+                                         BlockchainNetworkType networkType      ) throws CantSendRequestException {
+
+    }
+
+    @Override
+    public void informRefusal(UUID requestId) throws RequestNotFoundException, CantInformRefusalException {
+
+    }
+
+    @Override
+    public void informDenial(UUID requestId) throws RequestNotFoundException, CantInformDenialException {
+
+    }
+
+    @Override
+    public void informApproval(UUID requestId) throws CantInformApprovalException, RequestNotFoundException {
+
+    }
+
+    @Override
+    public void informReception(UUID requestId) throws CantInformReceptionException, RequestNotFoundException {
+
+    }
+
+    @Override
+    public void confirmRequest(UUID requestId) throws CantConfirmRequestException,
+                                                      RequestNotFoundException   {
+
+        try {
+
+            cryptoPaymentRequestNetworkServiceDao.deleteRequest(requestId);
+
+        } catch(CantDeleteRequestException |
+                RequestNotFoundException e) {
+            // i inform to error manager the error.
+            reportUnexpectedException(e);
+            throw new CantConfirmRequestException(e, "", "Error in Crypto Payment Request NS Dao.");
+        } catch(Exception e) {
+
+            reportUnexpectedException(e);
+            throw new CantConfirmRequestException(e, "", "Unhandled Exception.");
+        }
+    }
+
+    @Override
+    public CryptoPaymentRequest getRequestById(UUID requestId) throws CantGetRequestException  ,
+                                                                      RequestNotFoundException {
+
+        try {
+
+            return cryptoPaymentRequestNetworkServiceDao.getRequestById(requestId);
+
+        } catch(CantGetRequestException |
+                RequestNotFoundException e) {
+            // i inform to error manager the error.
+            reportUnexpectedException(e);
+            throw e;
+        } catch(Exception e) {
+
+            reportUnexpectedException(e);
+            throw new CantGetRequestException(e, "", "Unhandled Exception.");
+        }
+    }
+
+    @Override
+    public List<CryptoPaymentRequest> listPendingRequests() throws CantListPendingRequestsException {
+
+        try {
+
+            return cryptoPaymentRequestNetworkServiceDao.listRequestsByProtocolState(
+                    RequestProtocolState.PENDING_ACTION
+            );
+
+        } catch(CantListRequestsException e) {
+            // i inform to error manager the error.
+            reportUnexpectedException(e);
+            throw new CantListPendingRequestsException(e, "", "Error in Crypto Payment Request NS Dao.");
+        } catch(Exception e) {
+
+            reportUnexpectedException(e);
+            throw new CantListPendingRequestsException(e, "", "Unhandled Exception.");
+        }
+
+    }
+
     /*
-     * Service Interface implementation
-     */
+         * Service Interface implementation
+         */
     @Override
     public void start() {
         this.serviceStatus = ServiceStatus.STARTED;
     }
 
     private void reportUnexpectedException(Exception e) {
-        this.errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CCP_CRYPTO_PAYMENT_REQUEST, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
+        this.errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CCP_CRYPTO_PAYMENT_REQUEST_NETWORK_SERVICE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
     }
 
     @Override
