7b5995d2f79854f00fe6fa473469ea092e4305bd
furszy <mati_fur@hotmail.com>
2015-10-07 12:13:12 -0300

    contacts fragment fixed

--- CCP/android/reference_wallet/fermat-ccp-android-reference-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/MyComparator.java
+++ CCP/android/reference_wallet/fermat-ccp-android-reference-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/MyComparator.java
@@ -1,7 +1,12 @@
 package com.bitdubai.reference_niche_wallet.bitcoin_wallet.fragments;
 
-class MyComparator implements Comparator<String> {
-  public int compare(String strA, String strB) {
-    return strA.compareToIgnoreCase(strB);
+import com.bitdubai.fermat_api.layer.dmp_wallet_module.crypto_wallet.interfaces.CryptoWalletWalletContact;
+
+import java.util.Comparator;
+
+class MyComparator implements Comparator<CryptoWalletWalletContact> {
+
+  public int compare(CryptoWalletWalletContact strA, CryptoWalletWalletContact strB) {
+    return strA.getActorName().compareToIgnoreCase(strB.getActorName());
   }
 }
