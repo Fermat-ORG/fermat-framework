5bee75faf2621b79d3d937aef510bf189c20ea81
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-10-06 23:11:11 -0300

    Fixed issue

--- DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/interfaces/ActorAssetUserManager.java
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/interfaces/ActorAssetUserManager.java
@@ -1,13 +1,7 @@
 package com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces;
 
 import com.bitdubai.fermat_api.layer.all_definition.components.interfaces.PlatformComponentProfile;
-import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantRegisterActorAssetUserException;
 
-import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantRequestListActorAssetUserRegisteredException;
-
-import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantConnecToException;
-
-import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantSendMessageException;
 
 
 /**
@@ -16,13 +10,13 @@ import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantSen
 public interface ActorAssetUserManager {
 
 
-    public void registerActorAssetUser(ActorAssetUser actorAssetUserToRegister) throws CantRegisterActorAssetUserException;
+    public void registerActorAssetUser(ActorAssetUser actorAssetUserToRegister) ;
 
-    public void requestListActorAssetUserRegistered()  throws CantRequestListActorAssetUserRegisteredException;
+    public void requestListActorAssetUserRegistered() ;
 
-    public void connectTo(PlatformComponentProfile actorAssetUser)  throws CantConnecToException;
+    public void connectTo(PlatformComponentProfile actorAssetUser)  ;
 
-    public void sendMessage(PlatformComponentProfile actorAssetUser, String msjContent)  throws CantSendMessageException;
+    public void sendMessage(PlatformComponentProfile actorAssetUser, String msjContent)  ;
 
 
 
