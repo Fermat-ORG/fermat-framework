a3fffaca6549a158d8624608f75c6083a0ec390e
hendry19901990 <elnegroevaristo@gmail.com>
2015-10-07 11:54:21 -0430

    commit there left one file of actor network service

---
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/exceptions/CantSenMessageException.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions;
+
+/**
+ * Created by root on 06/10/15.
+ */
+public class CantSenMessageException {
+}
---
+++ fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/interfaces/DealsWhitAssetUserActorNetworkServiceManager.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces;
+
+/**
+ * Created by root on 07/10/15.
+ */
+public interface DealsWhitAssetUserActorNetworkServiceManager {
+}
