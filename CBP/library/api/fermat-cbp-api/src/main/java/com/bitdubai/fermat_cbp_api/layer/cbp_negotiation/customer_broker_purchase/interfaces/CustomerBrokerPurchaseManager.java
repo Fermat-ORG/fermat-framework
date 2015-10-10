9f99687e3b5ada3b99f52855539e6dda5946899d
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2015-10-10 16:08:09 -0430

    removed unused imports

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_negotiation/customer_broker_purchase/interfaces/CustomerBrokerPurchaseManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_negotiation/customer_broker_purchase/interfaces/CustomerBrokerPurchaseManager.java
@@ -3,7 +3,6 @@ package com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purcha
 import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
 import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purchase.exceptions.CantCreateCustomerBrokerPurchaseException;
-import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_sale.exceptions.CantCreateCustomerBrokerSaleException;
 import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_sale.interfaces.CustomerBrokerSale;
 
 import java.util.Collection;
