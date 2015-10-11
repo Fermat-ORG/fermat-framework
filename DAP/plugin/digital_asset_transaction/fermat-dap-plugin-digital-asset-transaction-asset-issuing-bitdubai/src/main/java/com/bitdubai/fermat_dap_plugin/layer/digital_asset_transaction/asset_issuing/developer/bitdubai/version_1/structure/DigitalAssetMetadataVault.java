9945a9f222e75a4b6d23f32ebd74863db0cedea2
Manuel Perez <darkpriestrelative@gmail.com>
2015-10-10 22:05:55 -0430

    Working on Asset Reception plugin basic structure #1710

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_issuing/developer/bitdubai/version_1/structure/DigitalAssetMetadataVault.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_issuing/developer/bitdubai/version_1/structure/DigitalAssetMetadataVault.java
@@ -30,12 +30,8 @@ import java.util.UUID;
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 01/10/15.
  * This class must be started with the AssetIssuing Plugin
  */
-public class DigitalAssetMetadataVault implements DigitalAssetVault {
+public class DigitalAssetMetadataVault extends DigitalAssetVault {
 
-    PluginFileSystem pluginFileSystem;
-    UUID pluginId;
-    //private final String LOCAL_STORAGE_PATH="digital-asset-metadata/";
-    //private final FileLifeSpan FILE_LIFE_SPAN=FileLifeSpan.PERMANENT;
     AssetIssuerWalletManager assetIssuerWalletManager;
     ErrorManager errorManager;
     String walletPublicKey;
@@ -48,83 +44,11 @@ public class DigitalAssetMetadataVault implements DigitalAssetVault {
         setErrorManager(errorManager);
     }
 
-    @Override
-    public void setPluginId(UUID pluginId) throws CantSetObjectException{
-        if(pluginId==null){
-            throw new CantSetObjectException("pluginId is null");
-        }
-        this.pluginId=pluginId;
-    }
-
-    @Override
-    public void setPluginFileSystem(PluginFileSystem pluginFileSystem) throws CantSetObjectException{
-        if(pluginFileSystem==null){
-            throw new CantSetObjectException("pluginFileSystem is null");
-        }
-        this.pluginFileSystem=pluginFileSystem;
-    }
-
     public void setErrorManager(ErrorManager errorManager) throws CantSetObjectException{
         if(errorManager==null){
             throw new CantSetObjectException("ErrorManager is null");
         }
         this.errorManager=errorManager;
-    }
-
-    @Override
-    public void persistDigitalAssetMetadataInLocalStorage(DigitalAssetMetadata digitalAssetMetadata)throws CantCreateDigitalAssetFileException {
-        String digitalAssetMetadataFileName="no-file-name-assigned";
-        try{
-            String genesisTransaction=digitalAssetMetadata.getGenesisTransaction();
-            String digitalAssetMetadataInnerXML=digitalAssetMetadata.toString();
-            digitalAssetMetadataFileName=genesisTransaction+".xml";
-            PluginTextFile digitalAssetMetadataFile=this.pluginFileSystem.createTextFile(this.pluginId, this.LOCAL_STORAGE_PATH, digitalAssetMetadataFileName, FILE_PRIVACY, FILE_LIFE_SPAN);
-            digitalAssetMetadataFile.setContent(digitalAssetMetadataInnerXML);
-            digitalAssetMetadataFile.persistToMedia();
-        } catch(CantCreateFileException exception){
-            throw new CantCreateDigitalAssetFileException(exception,"Persisting Digital Asset in local storage","Can't create '"+this.LOCAL_STORAGE_PATH+digitalAssetMetadataFileName+"' file");
-        } catch (CantPersistFileException exception) {
-            throw new CantCreateDigitalAssetFileException(exception,"Persisting Digital Asset in local storage","Can't persist '"+this.LOCAL_STORAGE_PATH+digitalAssetMetadataFileName+"' file");
-        } catch (Exception exception){
-            throw new CantCreateDigitalAssetFileException(exception,"Persisting Digital Asset in local storage","Unexpected exception creating '"+this.LOCAL_STORAGE_PATH+digitalAssetMetadataFileName+"' file");
-        }
-
-    }
-
-    @Override
-    public DigitalAssetMetadata getDigitalAssetMetadataFromLocalStorage(String genesisTransaction) throws CantGetDigitalAssetFromLocalStorageException {
-        String digitalAssetMetadataFileName = "no-file-name-assigned";
-        try {
-            DigitalAssetMetadata digitalAssetMetadataObtainedFromFileStorage = new DigitalAssetMetadata();
-            digitalAssetMetadataFileName = genesisTransaction + ".xml";
-            PluginTextFile digitalAssetMetadataFile = this.pluginFileSystem.getTextFile(this.pluginId, this.LOCAL_STORAGE_PATH, digitalAssetMetadataFileName, FILE_PRIVACY, FILE_LIFE_SPAN);
-            String digitalAssetMetadataXMLString = digitalAssetMetadataFile.getContent();
-            digitalAssetMetadataObtainedFromFileStorage = (DigitalAssetMetadata) XMLParser.parseXML(digitalAssetMetadataXMLString, digitalAssetMetadataObtainedFromFileStorage);
-            return digitalAssetMetadataObtainedFromFileStorage;
-        } catch (FileNotFoundException exception) {
-            throw new CantGetDigitalAssetFromLocalStorageException(exception, "Getting Digital Asset file from local storage", "Cannot find " + this.LOCAL_STORAGE_PATH + digitalAssetMetadataFileName + "' file");
-        } catch (CantCreateFileException exception) {
-            throw new CantGetDigitalAssetFromLocalStorageException(exception, "Getting Digital Asset file from local storage", "Cannot create " + this.LOCAL_STORAGE_PATH + digitalAssetMetadataFileName + "' file");
-        } catch (Exception exception){
-            throw new CantGetDigitalAssetFromLocalStorageException(exception, "Getting Digital Asset file from local storage","Unexpected exception getting '"+this.LOCAL_STORAGE_PATH+digitalAssetMetadataFileName+"' file");
-        }
-
-    }
-
-    @Override
-    public void deleteDigitalAssetMetadataFromLocalStorage(String genesisTransaction) throws CantDeleteDigitalAssetFromLocalStorageException {
-        String digitalAssetMetadataFileName = "no-file-name-assigned";
-        try{
-            digitalAssetMetadataFileName = genesisTransaction + ".xml";
-            PluginTextFile digitalAssetMetadataFile = this.pluginFileSystem.getTextFile(this.pluginId, this.LOCAL_STORAGE_PATH, digitalAssetMetadataFileName, FILE_PRIVACY, FILE_LIFE_SPAN);
-            digitalAssetMetadataFile.delete();
-        } catch (FileNotFoundException exception) {
-            throw new CantDeleteDigitalAssetFromLocalStorageException(exception, "Deleting Digital Asset file from local storage", "Cannot find " + this.LOCAL_STORAGE_PATH + digitalAssetMetadataFileName + "' file");
-        } catch (CantCreateFileException exception) {
-            throw new CantDeleteDigitalAssetFromLocalStorageException(exception, "Deleting Digital Asset file from local storage", "Cannot create " + this.LOCAL_STORAGE_PATH + digitalAssetMetadataFileName + "' file");
-        } catch (Exception exception){
-            throw new CantDeleteDigitalAssetFromLocalStorageException(exception, "Deleting Digital Asset file from local storage","Unexpected exception getting '"+this.LOCAL_STORAGE_PATH+digitalAssetMetadataFileName+"' file");
-        }
     }
 
     public void deliverDigitalAssetMetadataToAssetWallet(CryptoTransaction genesisTransaction, AssetBalanceType assetBalanceType)throws CantDeliverDigitalAssetToAssetWalletException{
