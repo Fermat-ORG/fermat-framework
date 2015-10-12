343e3d51a98e55a1c283421af3662de609501c81
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-12 16:19:51 -0430

    Asset Factory Implementation Agent #1731 35%

--- DAP/plugin/middleware/fermat-dap-plugin-middleware-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/middleware/asset/issuer/developer/bitdubai/version_1/structure/events/AssetFactoryMiddlewareMonitorAgent.java
+++ DAP/plugin/middleware/fermat-dap-plugin-middleware-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/middleware/asset/issuer/developer/bitdubai/version_1/structure/events/AssetFactoryMiddlewareMonitorAgent.java
@@ -1,14 +1,27 @@
 package com.bitdubai.fermat_dap_plugin.layer.middleware.asset.issuer.developer.bitdubai.version_1.structure.events;
 
 import com.bitdubai.fermat_api.DealsWithPluginIdentity;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.dmp_world.Agent;
 import com.bitdubai.fermat_api.layer.dmp_world.wallet.exceptions.CantStartAgentException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantCreateDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
+import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.CantSetObjectException;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_issuing.interfaces.AssetIssuingManager;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantInitializeAssetMonitorAgentException;
+import com.bitdubai.fermat_dap_plugin.layer.middleware.asset.issuer.developer.bitdubai.version_1.structure.database.AssertFactoryMiddlewareDatabaseConstant;
+import com.bitdubai.fermat_dap_plugin.layer.middleware.asset.issuer.developer.bitdubai.version_1.structure.database.AssetFactoryMiddlewareDao;
+import com.bitdubai.fermat_dap_plugin.layer.middleware.asset.issuer.developer.bitdubai.version_1.structure.database.AssetFactoryMiddlewareDatabaseFactory;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
+import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.pip_platform_service.event_manager.interfaces.EventManager;
 
 import java.util.UUID;
 import java.util.logging.Logger;
@@ -17,12 +30,35 @@ import java.util.logging.Logger;
  * Created by franklin on 12/10/15.
  */
 public class AssetFactoryMiddlewareMonitorAgent implements Agent, DealsWithLogger, DealsWithErrors, DealsWithPluginDatabaseSystem, DealsWithPluginIdentity {
+    Database database;
     Thread agentThread;
     MonitorAgent monitorAgent;
     LogManager logManager;
     ErrorManager errorManager;
+    EventManager eventManager;
     PluginDatabaseSystem pluginDatabaseSystem;
+    AssetIssuingManager assetIssuingManager;
+    UUID pluginId;
 
+    public AssetFactoryMiddlewareMonitorAgent(EventManager eventManager,
+                                              PluginDatabaseSystem pluginDatabaseSystem,
+                                              ErrorManager errorManager,
+                                              AssetIssuingManager assetIssuingManager,
+                                              UUID pluginId) throws CantSetObjectException {
+        this.eventManager= eventManager;
+        this.pluginDatabaseSystem = pluginDatabaseSystem;
+        this.errorManager = errorManager;
+        setAssetIssuingManager(assetIssuingManager);
+        this.pluginId = pluginId;
+
+    }
+
+    private void setAssetIssuingManager(AssetIssuingManager assetIssuingManager) throws CantSetObjectException {
+        if(assetIssuingManager == null){
+            throw new CantSetObjectException("AssetIssuingManager is null");
+        }
+        this.assetIssuingManager = assetIssuingManager;
+    }
 
     @Override
     public void start() throws CantStartAgentException {
@@ -32,9 +68,21 @@ public class AssetFactoryMiddlewareMonitorAgent implements Agent, DealsWithLogge
         monitorAgent = new MonitorAgent();
 
 
+        ((DealsWithPluginDatabaseSystem) this.monitorAgent).setPluginDatabaseSystem(this.pluginDatabaseSystem);
+        ((DealsWithErrors) this.monitorAgent).setErrorManager(this.errorManager);
+
+        try {
+            ((MonitorAgent) this.monitorAgent).Initialize();
+        } catch (CantInitializeAssetMonitorAgentException exception) {
+            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_ASSET_FACTORY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, exception);
+        }
+
+
         this.agentThread = new Thread(monitorAgent);
         this.agentThread.start();
     }
+
+
 
     @Override
     public void stop() {
@@ -58,18 +106,58 @@ public class AssetFactoryMiddlewareMonitorAgent implements Agent, DealsWithLogge
 
     @Override
     public void setPluginId(UUID pluginId) {
-
+        this.pluginId = pluginId;
     }
 
     /**
      * Private class which implements runnable and is started by the Agent
      * Based on MonitorAgent created by Rodrigo Acosta
      */
-    private class MonitorAgent implements Runnable{
+    private class MonitorAgent implements Runnable, DealsWithPluginDatabaseSystem, DealsWithErrors{
 
+        ErrorManager errorManager;
+        PluginDatabaseSystem pluginDatabaseSystem;
+        public final int SLEEP_TIME = /*AssetIssuingTransactionNotificationAgent.AGENT_SLEEP_TIME*/5000;
+        int iterationNumber = 0;
+        AssetFactoryMiddlewareDao assetFactoryMiddlewareDao;
+        boolean threadWorking;
+        @Override
+        public void setErrorManager(ErrorManager errorManager) {
+            this.errorManager = errorManager;
+        }
+
+        @Override
+        public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+            this.pluginDatabaseSystem = pluginDatabaseSystem;
+        }
         @Override
         public void run() {
 
         }
+
+        public void Initialize() throws CantInitializeAssetMonitorAgentException {
+
+            try {
+
+                database = this.pluginDatabaseSystem.openDatabase(pluginId,  AssertFactoryMiddlewareDatabaseConstant.DATABASE_NAME);
+            }
+            catch (DatabaseNotFoundException databaseNotFoundException) {
+
+                Logger LOG = Logger.getGlobal();
+                LOG.info("Database in Asset Factory monitor agent doesn't exists");
+                AssetFactoryMiddlewareDatabaseFactory  assetFactoryMiddlewareDatabaseFactory = new AssetFactoryMiddlewareDatabaseFactory(this.pluginDatabaseSystem);
+                try {
+                    database = assetFactoryMiddlewareDatabaseFactory.createDatabase(pluginId,  AssertFactoryMiddlewareDatabaseConstant.DATABASE_NAME);
+                } catch (CantCreateDatabaseException cantCreateDatabaseException) {
+                    errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_ASSET_FACTORY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN,cantCreateDatabaseException);
+                    throw new CantInitializeAssetMonitorAgentException(cantCreateDatabaseException,"Initialize Monitor Agent - trying to create the plugin database","Please, check the cause");
+                }
+            } catch (CantOpenDatabaseException exception) {
+                errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_ASSET_FACTORY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, exception);
+                throw new CantInitializeAssetMonitorAgentException(exception,"Initialize Monitor Agent - trying to open the plugin database","Please, check the cause");
+            }
+        }
+
+
     }
 }
