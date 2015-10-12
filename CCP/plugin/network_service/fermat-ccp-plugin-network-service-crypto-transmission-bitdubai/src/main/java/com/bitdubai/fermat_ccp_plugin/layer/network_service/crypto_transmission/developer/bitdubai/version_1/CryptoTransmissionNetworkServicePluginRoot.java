c41d31791bf97714d1cb564ff65b3775a3ab6ce3
furszy <mati_fur@hotmail.com>
2015-10-12 13:19:09 -0300

    subapp community changes

--- CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-transmission-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_transmission/developer/bitdubai/version_1/CryptoTransmissionNetworkServicePluginRoot.java
+++ CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-transmission-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_transmission/developer/bitdubai/version_1/CryptoTransmissionNetworkServicePluginRoot.java
@@ -334,10 +334,17 @@ public class CryptoTransmissionNetworkServicePluginRoot implements CryptoTransmi
         listenersAdded.add(fermatEventListener);
 
 
+
+    }
+
+    /**
+     * Messages listeners
+     */
+    private void initializeMessagesListeners(){
         /*
          * Listen and handle Complete Request List Component Registered Notification Event
          */
-        fermatEventListener = eventManager.getNewListener(com.bitdubai.fermat_pip_api.layer.pip_platform_service.event_manager.enums.EventType.NEW_NETWORK_SERVICE_MESSAGE_SENT_NOTIFICATION);
+        FermatEventListener fermatEventListener = eventManager.getNewListener(com.bitdubai.fermat_pip_api.layer.pip_platform_service.event_manager.enums.EventType.NEW_NETWORK_SERVICE_MESSAGE_SENT_NOTIFICATION);
         fermatEventListener.setEventHandler(new NewReceiveMessagesNotificationEventHandler(cryptoTransmissionAgent));
         eventManager.addListener(fermatEventListener);
         listenersAdded.add(fermatEventListener);
@@ -349,7 +356,6 @@ public class CryptoTransmissionNetworkServicePluginRoot implements CryptoTransmi
         fermatEventListener.setEventHandler(new NewReceiveMessagesNotificationEventHandler(cryptoTransmissionAgent));
         eventManager.addListener(fermatEventListener);
         listenersAdded.add(fermatEventListener);
-
     }
 
     /**
@@ -796,7 +802,10 @@ public class CryptoTransmissionNetworkServicePluginRoot implements CryptoTransmi
                     identity
             );
 
+            // Initialize messages handlers
+            initializeMessagesListeners();
 
+            // start main threads
             cryptoTransmissionAgent.start();
 
 
