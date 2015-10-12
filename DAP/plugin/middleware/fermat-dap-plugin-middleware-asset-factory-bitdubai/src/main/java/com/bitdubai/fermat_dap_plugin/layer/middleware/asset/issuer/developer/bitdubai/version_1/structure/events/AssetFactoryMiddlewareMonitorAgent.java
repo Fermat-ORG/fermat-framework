fd3511e47252129569b5db7891db88c2e983afee
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-12 15:22:26 -0430

    Asset Factory Implementation Agent #1731 10%

--- DAP/plugin/middleware/fermat-dap-plugin-middleware-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/middleware/asset/issuer/developer/bitdubai/version_1/structure/events/AssetFactoryMiddlewareMonitorAgent.java
+++ DAP/plugin/middleware/fermat-dap-plugin-middleware-asset-factory-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/middleware/asset/issuer/developer/bitdubai/version_1/structure/events/AssetFactoryMiddlewareMonitorAgent.java
@@ -1,22 +1,75 @@
 package com.bitdubai.fermat_dap_plugin.layer.middleware.asset.issuer.developer.bitdubai.version_1.structure.events;
 
+import com.bitdubai.fermat_api.DealsWithPluginIdentity;
 import com.bitdubai.fermat_api.layer.dmp_world.Agent;
 import com.bitdubai.fermat_api.layer.dmp_world.wallet.exceptions.CantStartAgentException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
+import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
+import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
+import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
 
+import java.util.UUID;
 import java.util.logging.Logger;
 
 /**
  * Created by franklin on 12/10/15.
  */
-public class AssetFactoryMiddlewareMonitorAgent implements Agent {
+public class AssetFactoryMiddlewareMonitorAgent implements Agent, DealsWithLogger, DealsWithErrors, DealsWithPluginDatabaseSystem, DealsWithPluginIdentity {
+    Thread agentThread;
+    MonitorAgent monitorAgent;
+    LogManager logManager;
+    ErrorManager errorManager;
+    PluginDatabaseSystem pluginDatabaseSystem;
+
+
     @Override
     public void start() throws CantStartAgentException {
         Logger LOG = Logger.getGlobal();
         LOG.info("Asset Factory monitor agent starting");
+
+        monitorAgent = new MonitorAgent();
+
+
+        this.agentThread = new Thread(monitorAgent);
+        this.agentThread.start();
     }
 
     @Override
     public void stop() {
+        this.agentThread.interrupt();
+    }
 
+    @Override
+    public void setLogManager(LogManager logManager) {
+        this.logManager = logManager;
+    }
+
+    @Override
+    public void setErrorManager(ErrorManager errorManager) {
+        this.errorManager = errorManager;
+    }
+
+    @Override
+    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+        this.pluginDatabaseSystem = pluginDatabaseSystem;
+    }
+
+    @Override
+    public void setPluginId(UUID pluginId) {
+
+    }
+
+    /**
+     * Private class which implements runnable and is started by the Agent
+     * Based on MonitorAgent created by Rodrigo Acosta
+     */
+    private class MonitorAgent implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
     }
 }
