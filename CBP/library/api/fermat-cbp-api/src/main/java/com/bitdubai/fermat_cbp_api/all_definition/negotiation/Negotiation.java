6e5cd560e51df8fb24fda8da93b4bb70b87d8fc1
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2015-10-12 10:43:36 -0430

    definition of the negotiation and clause interfaces

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/negotiation/Negotiation.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/negotiation/Negotiation.java
@@ -5,36 +5,16 @@ import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.ReferenceCurrency;
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
 
+import java.util.Collection;
 import java.util.UUID;
 
 /**
  * Created by jorge on 09-10-2015.
  */
 public interface Negotiation {
-
     UUID getNegotiationId();
-
-    CurrencyType getMerchandiseCurrency();
-    CryptoDetail getMerchandiseCryptoDetail();
-    CashDetail getMerchandiseCashDetail();
-    BankDetail getMerchandiseBankDetail();
-
-    CurrencyType getPaymentCurrency();
-    CryptoDetail getPaymentCryptoDetail();
-    CashDetail getPaymentCashDetail();
-    BankDetail getPaymentBankDetail();
-
-    ReferenceCurrency getReferenceCurrency();
-    float getReferencePrice();
-    float getSuggestedPrice();
-    float getMarketPrice();
-
-    float getMerchandiseAmount();
-    float getPaymentAmount();
-
-    long getCreationDate();
-    long getLastUpdateDate();
-    long getExpirationDate();
-
+    long getStartDate();
     NegotiationStatus getStatus();
+
+    Collection<Clause> getClauses();
 }
