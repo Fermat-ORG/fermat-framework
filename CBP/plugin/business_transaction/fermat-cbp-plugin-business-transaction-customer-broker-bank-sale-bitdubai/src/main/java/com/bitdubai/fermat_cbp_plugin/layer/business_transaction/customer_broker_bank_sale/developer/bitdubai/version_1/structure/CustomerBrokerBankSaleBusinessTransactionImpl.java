0509e08292faf758717b7361008272e875676fda
yalayn <y.alayn@gmail.com>
2015-10-12 23:06:34 -0430

    Dao CBP - Busines Transaction - Customer Broker Bank Sale

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-broker-bank-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_broker_bank_sale/developer/bitdubai/version_1/structure/CustomerBrokerBankSaleBusinessTransactionImpl.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-broker-bank-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_broker_bank_sale/developer/bitdubai/version_1/structure/CustomerBrokerBankSaleBusinessTransactionImpl.java
@@ -1,41 +1,57 @@
 package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_broker_bank_sale.developer.bitdubai.version_1.structure;
 
 import com.bitdubai.fermat_cbp_api.all_definition.business_transaction.BusinessTransaction;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.BankCurrencyType;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.BankOperationType;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.BusinessTransactionStatus;
-import com.bitdubai.fermat_cbp_api.all_definition.enums.CashCurrencyType;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.CurrencyType;
-import com.bitdubai.fermat_cbp_api.layer.cbp_business_transaction.cash_money_stock_replenishment.interfaces.CashMoneyStockReplenishment;
+import com.bitdubai.fermat_cbp_api.layer.cbp_business_transaction.customer_broke_bank_sale.interfaces.CustomerBrokerBankSale;
 
 import java.util.UUID;
 
 /**
  * Created by Yordin Alayn on 29.09.15.
  */
-public class CustomerBrokerBankSaleBusinessTransactionImpl implements BusinessTransaction, CashMoneyStockReplenishment{
+public class CustomerBrokerBankSaleBusinessTransactionImpl implements BusinessTransaction, CustomerBrokerBankSale {
 
     private UUID transactionId;
+    private UUID contractId;
     private String publicKeyBroker;
+    private String publicKeyCustomer;
+    private UUID paymentTransactionId;
+    private CurrencyType paymentCurrency;
     private CurrencyType merchandiseCurrency;
     private float merchandiseAmount;
     private UUID executionTransactionId;
-    private CashCurrencyType cashCurrencyType;
+    private BankCurrencyType bankCurrencyType;
+    private BankOperationType bankOperationType;
     private BusinessTransactionStatus transactionStatus;
 
     public CustomerBrokerBankSaleBusinessTransactionImpl(
             UUID transactionId,
+            UUID contractId,
             String publicKeyBroker,
+            String publicKeyCustomer,
+            UUID paymentTransactionId,
+            CurrencyType paymentCurrency,
             CurrencyType merchandiseCurrency,
             float merchandiseAmount,
             UUID executionTransactionId,
-            CashCurrencyType cashCurrencyType,
+            BankCurrencyType bankCurrencyType,
+            BankOperationType bankOperationType,
             BusinessTransactionStatus transactionStatus
     ){
         this.transactionId = transactionId;
+        this.contractId = contractId;
         this.publicKeyBroker = publicKeyBroker;
+        this.publicKeyCustomer = publicKeyCustomer;
+        this.paymentTransactionId = paymentTransactionId;
+        this.paymentCurrency = paymentCurrency;
         this.merchandiseCurrency = merchandiseCurrency;
         this.merchandiseAmount = merchandiseAmount;
         this.executionTransactionId = executionTransactionId;
-        this.cashCurrencyType = cashCurrencyType;
+        this.bankCurrencyType = bankCurrencyType;
+        this.bankOperationType = bankOperationType;
         this.transactionStatus = transactionStatus;
     }
 
@@ -44,8 +60,24 @@ public class CustomerBrokerBankSaleBusinessTransactionImpl implements BusinessTr
     public void setIdTransaction(UUID id) { this.transactionId = id; }
 
     @Override
+    public UUID getContractId(){ return this.contractId; }
+    public void setContractId(UUID id) { this.contractId = id; }
+
+    @Override
     public String getPublicKeyBroker(){ return this.publicKeyBroker; }
     public  void setPublicKeyBroker(String publicKey){ this.publicKeyBroker = publicKey; }
+
+    @Override
+    public String getPublicKeyCustomer(){ return this.publicKeyCustomer; }
+    public  void setPublicKeyCustomer(String publicKey){ this.publicKeyCustomer = publicKey; }
+
+    @Override
+    public UUID getPaymentTransactionId(){ return this.paymentTransactionId; }
+    public void setPaymentTransactionId(UUID paymentId){ this.paymentTransactionId = paymentId; }
+
+    @Override
+    public CurrencyType getPaymentCurrency(){ return this.paymentCurrency; }
+    public void setPaymentCurrency(CurrencyType payment){ this.paymentCurrency = payment; }
 
     @Override
     public CurrencyType getMerchandiseCurrency(){ return this.merchandiseCurrency; }
@@ -60,8 +92,12 @@ public class CustomerBrokerBankSaleBusinessTransactionImpl implements BusinessTr
     public void setExecutionTransactionId(UUID execution){ this.executionTransactionId = execution; }
 
     @Override
-    public CashCurrencyType getCashCurrencyType(){ return this.cashCurrencyType; }
-    public void setCashCurrencyType(CashCurrencyType currencyType){ this.cashCurrencyType = currencyType; }
+    public BankCurrencyType getBankCurrencyType(){ return this.bankCurrencyType; }
+    public void setBankCurrencyType(BankCurrencyType bankCurrency){ this.bankCurrencyType = bankCurrency; }
+
+    @Override
+    public BankOperationType getBankOperationType(){ return this.bankOperationType; }
+    public void setBankOperationType(BankOperationType bankOperation){ this.bankOperationType = bankOperation; }
 
     @Override
     public BusinessTransactionStatus getStatus(){ return this.transactionStatus; }
