422fc09ffd7d6eb158b3b3ac7520cacf9b1d01cf
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-16 12:28:39 -0430

    Aseet User Wallet Module Implementation #1465 50%

---
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_module/wallet_asset_user/interfaces/AssetUserWalletSupAppModuleManager.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_dap_api.layer.dap_module.wallet_asset_user.interfaces;
+
+/**
+ * Created by franklin on 16/10/15.
+ */
+public interface AssetUserWalletSupAppModuleManager {
+}
--- DAP/plugin/wallet_module/fermat-dap-plugin-wallet-module-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/module/asset/user/developer/bitdubai/version_1/AssetWalletUserModulePluginRoot.java
+++ DAP/plugin/wallet_module/fermat-dap-plugin-wallet-module-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/module/asset/user/developer/bitdubai/version_1/AssetWalletUserModulePluginRoot.java
@@ -1,19 +1,92 @@
 package com.bitdubai.fermat_dap_plugin.layer.module.asset.user.developer.bitdubai.version_1;
 
+import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.Plugin;
+import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
+import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
+import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
+import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
+import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
  * Created by Franklin on 07/09/15.
  */
-public class AssetWalletUserModulePluginRoot implements Plugin {
+public class AssetWalletUserModulePluginRoot implements Plugin, Service, DealsWithLogger, LogManagerForDevelopers, DealsWithErrors {
 
     UUID pluginId;
+
+    /**
+     * DealsWithErros interface member variable
+     */
+    ErrorManager errorManager;
+
+    /**
+     * DealsWithLogger interface member variable
+     */
+    LogManager logManager;
+
+    /**
+     * Service Interface member variables.
+     */
+    ServiceStatus serviceStatus = ServiceStatus.CREATED;
+
+    static Map<String, LogLevel> newLoggingLevel = new HashMap<String, LogLevel>();
 
     @Override
     public void setId(UUID pluginId) {
         this.pluginId = pluginId;
     }
 
+    @Override
+    public void setLogManager(LogManager logManager) {
+
+    }
+
+    @Override
+    public List<String> getClassesFullPath() {
+        return null;
+    }
+
+    @Override
+    public void setLoggingLevelPerClass(Map<String, LogLevel> newLoggingLevel) {
+
+    }
+
+    @Override
+    public void start() throws CantStartPluginException {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public ServiceStatus getStatus() {
+        return null;
+    }
+
+    @Override
+    public void setErrorManager(ErrorManager errorManager) {
+
+    }
 }
