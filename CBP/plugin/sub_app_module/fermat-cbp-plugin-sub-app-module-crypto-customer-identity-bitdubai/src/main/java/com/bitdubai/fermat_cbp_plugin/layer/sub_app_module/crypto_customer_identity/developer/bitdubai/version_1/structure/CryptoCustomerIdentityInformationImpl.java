46bea568aa9ec76fbf2db9f805db32b8db229da2
Angel Veloz <vlzangel91@gmail.com>
2015-10-16 17:47:29 -0430

    CryptoCustomerIdentityInformationImpl

--- CBP/plugin/sub_app_module/fermat-cbp-plugin-sub-app-module-crypto-customer-identity-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/sub_app_module/crypto_customer_identity/developer/bitdubai/version_1/structure/CryptoCustomerIdentityInformationImpl.java
+++ CBP/plugin/sub_app_module/fermat-cbp-plugin-sub-app-module-crypto-customer-identity-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/sub_app_module/crypto_customer_identity/developer/bitdubai/version_1/structure/CryptoCustomerIdentityInformationImpl.java
@@ -1,5 +1,8 @@
 package com.bitdubai.fermat_cbp_plugin.layer.sub_app_module.crypto_customer_identity.developer.bitdubai.version_1.structure;
 
+import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.AsymmetricCryptography;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.IdentityPublished;
+import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantCreateMessageSignatureException;
 import com.bitdubai.fermat_cbp_api.layer.cbp_sub_app_module.crypto_customer_identity.interfaces.CryptoCustomerIdentityInformation;
 
 /**
@@ -7,28 +10,51 @@ import com.bitdubai.fermat_cbp_api.layer.cbp_sub_app_module.crypto_customer_iden
  */
 public class CryptoCustomerIdentityInformationImpl implements CryptoCustomerIdentityInformation {
 
-    private final String publicKey;
-    private final String name;
-    private final byte[] profileImage;
+    private final String alias;
+    private String publicKey;
+    private String privateKey;
+    private byte[] profileImage;
+    private IdentityPublished publicKeyPublished;
 
-    public CryptoCustomerIdentityInformationImpl(final String publicKey, final String name,  final byte[] profileImage){
+    public CryptoCustomerIdentityInformationImpl(final String alias, String publicKey, String privateKey, final byte[] profileImage, IdentityPublished publicKeyPublished) {
+        this.alias = alias;
         this.publicKey = publicKey;
-        this.name = name;
+        this.privateKey = privateKey;
         this.profileImage = profileImage;
+        this.publicKeyPublished = publicKeyPublished;
+    }
+
+    @Override
+    public String getAlias() {
+        return this.alias;
     }
 
     @Override
     public String getPublicKey() {
-        return publicKey;
-    }
-
-    @Override
-    public String getName() {
-        return name;
+        return this.publicKey;
     }
 
     @Override
     public byte[] getProfileImage() {
-        return profileImage;
+        return this.profileImage;
+    }
+
+    @Override
+    public void setNewProfileImage(byte[] imageBytes) {
+        this.profileImage = imageBytes;
+    }
+
+    @Override
+    public IdentityPublished getPublicKeyPublished() {
+        return this.publicKeyPublished;
+    }
+
+    @Override
+    public String createMessageSignature(String message) throws CantCreateMessageSignatureException {
+        try {
+            return AsymmetricCryptography.createMessageSignature(message, this.privateKey);
+        } catch (Exception ex) {
+            throw new CantCreateMessageSignatureException(CantCreateMessageSignatureException.DEFAULT_MESSAGE, ex, "Message: " + message, "The message could be invalid");
+        }
     }
 }
