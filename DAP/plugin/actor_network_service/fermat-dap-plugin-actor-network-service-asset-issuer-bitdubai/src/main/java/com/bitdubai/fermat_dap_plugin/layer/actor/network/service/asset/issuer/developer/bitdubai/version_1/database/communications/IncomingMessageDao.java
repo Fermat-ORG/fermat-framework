4d3958ff10dcff3641b689a4c95f3715fbde98df
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-19 09:45:04 -0430

    Create Asset Issuer Network Service Implementation

--- DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/issuer/developer/bitdubai/version_1/database/communications/IncomingMessageDao.java
+++ DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/issuer/developer/bitdubai/version_1/database/communications/IncomingMessageDao.java
@@ -1,7 +1,23 @@
 package com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.issuer.developer.bitdubai.version_1.database.communications;
 
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+
 /**
  * Created by franklin on 17/10/15.
  */
 public class IncomingMessageDao {
+    /**
+     * Represent the dataBase
+     */
+    private Database dataBase;
+
+    /**
+     * Constructor with parameters
+     *
+     * @param dataBase
+     */
+    public IncomingMessageDao(Database dataBase) {
+        super();
+
+    }
 }
--- DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/issuer/developer/bitdubai/version_1/database/communications/OutgoingMessageDao.java
+++ DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/issuer/developer/bitdubai/version_1/database/communications/OutgoingMessageDao.java
@@ -1,7 +1,23 @@
 package com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.issuer.developer.bitdubai.version_1.database.communications;
 
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+
 /**
  * Created by franklin on 17/10/15.
  */
 public class OutgoingMessageDao {
+    /**
+     * Represent the dataBase
+     */
+    private Database dataBase;
+
+    /**
+     * Constructor with parameters
+     *
+     * @param dataBase
+     */
+    public OutgoingMessageDao(Database dataBase) {
+        super();
+        this.dataBase = dataBase;
+    }
 }
