f10667dac9d39079b21dd027f43e9777ae591296
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-10-19 09:57:55 -0430

    Create Asset Issuer Network Service Implementation

--- DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/issuer/developer/bitdubai/version_1/database/communications/IncomingMessageDao.java
+++ DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/issuer/developer/bitdubai/version_1/database/communications/IncomingMessageDao.java
@@ -1,6 +1,7 @@
 package com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.issuer.developer.bitdubai.version_1.database.communications;
 
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
 
 /**
  * Created by franklin on 17/10/15.
@@ -18,6 +19,24 @@ public class IncomingMessageDao {
      */
     public IncomingMessageDao(Database dataBase) {
         super();
+        this.dataBase = dataBase;
+    }
 
+    /**
+     * Return the Database
+     *
+     * @return Database
+     */
+    Database getDataBase() {
+        return dataBase;
+    }
+
+    /**
+     * Return the DatabaseTable
+     *
+     * @return DatabaseTable
+     */
+    DatabaseTable getDatabaseTable() {
+        return getDataBase().getTable(CommunicationNetworkServiceDatabaseConstants.INCOMING_MESSAGES_TABLE_NAME);
     }
 }
