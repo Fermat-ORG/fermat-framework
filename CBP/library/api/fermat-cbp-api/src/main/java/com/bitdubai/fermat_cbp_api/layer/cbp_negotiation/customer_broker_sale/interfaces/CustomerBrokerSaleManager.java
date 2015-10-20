67090c2470e644aa071a6713107e34e95d35faa9
vlzangel <vlzangel@gmail.com>
2015-10-20 16:26:47 -0430

    Refactory CustomerBrokerSaleManager
    Refactory CustomerBrokerSaleManager

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_negotiation/customer_broker_sale/interfaces/CustomerBrokerSaleManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_negotiation/customer_broker_sale/interfaces/CustomerBrokerSaleManager.java
@@ -3,6 +3,7 @@ package com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_sale.i
 import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
 import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_sale.exceptions.CantCreateCustomerBrokerSaleException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_sale.exceptions.CantListSaleNegotianionsException;
 
 import java.util.Collection;
 
@@ -15,7 +16,7 @@ public interface CustomerBrokerSaleManager {
     void cancelNegotiation(CustomerBrokerSale negotiation);
     void closeNegotiation(CustomerBrokerSale negotiation);
 
-    Collection<CustomerBrokerSale> getNegotiations();
+    Collection<CustomerBrokerSale> getNegotiations() throws CantListSaleNegotianionsException;
     Collection<CustomerBrokerSale> getNegotiations(NegotiationStatus status);
     Collection<CustomerBrokerSale> getNegotiationsByCustomer(ActorIdentity customer);
     Collection<CustomerBrokerSale> getNegotiationsByBroker(ActorIdentity broker);
