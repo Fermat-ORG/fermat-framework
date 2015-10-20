474267fa9a65a8e6c1f250714b2d5c649ce76f0d
vlzangel <vlzangel@gmail.com>
2015-10-20 16:21:29 -0430

    Refactory CustomerBrokerSalePluginRoot
    Refactory CustomerBrokerSalePluginRoot

--- CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_sale/developer/bitdubai/version_1/CustomerBrokerSalePluginRoot.java
+++ CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_sale/developer/bitdubai/version_1/CustomerBrokerSalePluginRoot.java
@@ -5,12 +5,18 @@ import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
 import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
+import com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.database.CustomerBrokerSaleNegotiationDao;
+import com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerSaleNegotiationDaoException;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.pip_platform_service.error_manager.ErrorManager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -18,55 +24,88 @@ import java.util.UUID;
 /**
  * Created by jorge on 12-10-2015.
  */
-public class CustomerBrokerSalePluginRoot implements DealsWithErrors, DealsWithLogger, LogManagerForDevelopers, Service, Plugin {
+public class CustomerBrokerSalePluginRoot implements DealsWithErrors, DealsWithLogger, DealsWithPluginDatabaseSystem, LogManagerForDevelopers, Service, Plugin {
+
+    private ErrorManager errorManager;
+    private LogManager logManager;
+    private PluginDatabaseSystem pluginDatabaseSystem;
+    private UUID pluginId;
+    private static Map<String, LogLevel> newLoggingLevel = new HashMap<>();
+    private CustomerBrokerSaleNegotiationDao customerBrokerSaleNegotiationDao;
+    private ServiceStatus serviceStatus = ServiceStatus.CREATED;
 
     @Override
     public void setId(UUID pluginId) {
-
+        this.pluginId = pluginId;
     }
 
     @Override
     public void setErrorManager(ErrorManager errorManager) {
-
+        this.errorManager = errorManager;
     }
 
     @Override
     public void setLogManager(LogManager logManager) {
-
+        this.logManager = logManager;
     }
 
     @Override
     public List<String> getClassesFullPath() {
-        return null;
+        List<String> returnedClasses = new ArrayList<String>();
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.CryptoBrokerIdentityPluginRoot");
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.structure.CustomerBrokerSaleNegotiation");
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.database.CustomerBrokerSaleNegotiationDao");
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.database.CustomerBrokerSaleNegotiationDatabaseFactory");
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.database.CustomerBrokerSaleNegotiationDatabaseConstants");
+        return returnedClasses;
     }
 
     @Override
     public void setLoggingLevelPerClass(Map<String, LogLevel> newLoggingLevel) {
-
+        for (Map.Entry<String, LogLevel> pluginPair : newLoggingLevel.entrySet()) {
+            if (CustomerBrokerSalePluginRoot.newLoggingLevel.containsKey(pluginPair.getKey())) {
+                CustomerBrokerSalePluginRoot.newLoggingLevel.remove(pluginPair.getKey());
+                CustomerBrokerSalePluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
+            } else {
+                CustomerBrokerSalePluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
+            }
+        }
     }
 
     @Override
     public void start() throws CantStartPluginException {
-
+        this.serviceStatus = ServiceStatus.STARTED;
+        try {
+            this.customerBrokerSaleNegotiationDao = new CustomerBrokerSaleNegotiationDao(pluginDatabaseSystem);
+            this.customerBrokerSaleNegotiationDao.initialize(pluginId);
+        } catch (CantInitializeCustomerBrokerSaleNegotiationDaoException cantInitializeExtraUserRegistryException) {
+            //errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_DESIGNER_IDENTITY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, cantInitializeExtraUserRegistryException);
+            // throw new CantStartPluginException(cantInitializeExtraUserRegistryException, Plugins.BITDUBAI_ACTOR_DEVELOPER);
+        }
     }
 
     @Override
     public void pause() {
-
+        this.serviceStatus = ServiceStatus.PAUSED;
     }
 
     @Override
     public void resume() {
-
+        this.serviceStatus = ServiceStatus.STARTED;
     }
 
     @Override
     public void stop() {
-
+        this.serviceStatus = ServiceStatus.STOPPED;
     }
 
     @Override
     public ServiceStatus getStatus() {
-        return null;
+        return serviceStatus;
+    }
+
+    @Override
+    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
+        this.pluginDatabaseSystem = pluginDatabaseSystem;
     }
 }
