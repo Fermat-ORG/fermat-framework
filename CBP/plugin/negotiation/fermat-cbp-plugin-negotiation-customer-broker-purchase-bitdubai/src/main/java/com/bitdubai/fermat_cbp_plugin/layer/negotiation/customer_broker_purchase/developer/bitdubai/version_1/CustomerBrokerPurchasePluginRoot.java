2127cccf4f05c5e079d25610235feaee5517af48
vlzangel <vlzangel@gmail.com>
2015-10-20 15:56:51 -0430

    Refactory CustomerBrokerPurchasePluginRoot
    Refactory CustomerBrokerPurchasePluginRoot
    getNegotiations(NegotiationStatus status)

--- CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_purchase/developer/bitdubai/version_1/CustomerBrokerPurchasePluginRoot.java
+++ CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_purchase/developer/bitdubai/version_1/CustomerBrokerPurchasePluginRoot.java
@@ -149,7 +149,15 @@ public class CustomerBrokerPurchasePluginRoot implements CustomerBrokerPurchaseM
 
     @Override
     public Collection<CustomerBrokerPurchase> getNegotiations(NegotiationStatus status) throws CantListNegotianionsException {
-        return null;
+        try {
+            Collection<CustomerBrokerPurchase> negotiations = new ArrayList<CustomerBrokerPurchase>();
+            negotiations = customerBrokerPurchaseNegotiationDao.getNegotiations(status);
+            return negotiations;
+        } catch (CantLoadTableToMemoryException e) {
+            throw new CantListNegotianionsException(CantListNegotianionsException.DEFAULT_MESSAGE, e, "", "");
+        } catch (InvalidParameterException e) {
+            throw new CantListNegotianionsException(CantListNegotianionsException.DEFAULT_MESSAGE, e, "", "");
+        }
     }
 
     @Override
