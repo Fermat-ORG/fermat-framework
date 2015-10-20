83edf4d964553bc4d1f4fc1701d6920386b45129
yalayn <y.alayn@gmail.com>
2015-10-20 16:06:24 -0430

    Database Structure CBP - Crypto Broker Wallet

---
+++ CBP/plugin/wallet/fermat-cbp-plugin-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet/crypto_broker/developer/bitdubai/version_1/database/CryptoBrokerWalletDatabaseDao.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.database;
+
+/**
+ * Created by yordin on 20/10/15.
+ */
+public class CryptoBrokerWalletDatabaseDao {
+}
---
+++ CBP/plugin/wallet/fermat-cbp-plugin-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet/crypto_broker/developer/bitdubai/version_1/exceptions/CantInitializeCryptoBrokerWalletDatabaseException.java
@@ -0,0 +1,33 @@
+package com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.exceptions;
+
+import com.bitdubai.fermat_api.FermatException;
+/**
+ * The Class <code>package com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.exceptions.CantInitializeCryptoBrokerWalletDatabaseException</code>
+ * is thrown when an error occurs initializing database
+ * <p/>
+ *
+ * Created by Yordin Alayn - (y.alayn@gmail.com) on 18/10/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class CantInitializeCryptoBrokerWalletDatabaseException extends FermatException {
+
+    public static final String DEFAULT_MESSAGE = "CAN'T INTIALIZE CRYPTO BROKER WALLET DATABASE EXCEPTION";
+
+    public CantInitializeCryptoBrokerWalletDatabaseException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    public CantInitializeCryptoBrokerWalletDatabaseException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    public CantInitializeCryptoBrokerWalletDatabaseException(final String message) {
+        this(message, null);
+    }
+
+    public CantInitializeCryptoBrokerWalletDatabaseException() {
+        this(DEFAULT_MESSAGE);
+    }
+}
