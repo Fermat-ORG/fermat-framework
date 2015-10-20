073e138b051432156419fde08ff8896bdfa8953a
vlzangel <vlzangel@gmail.com>
2015-10-20 10:48:05 -0430

    createCustomerBrokerPurchaseNegotiation
    Método createCustomerBrokerPurchaseNegotiation en el
    CustomerBrokerSaleNegotiationDao

--- CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_sale/developer/bitdubai/version_1/database/CustomerBrokerSaleNegotiationDao.java
+++ CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_sale/developer/bitdubai/version_1/database/CustomerBrokerSaleNegotiationDao.java
@@ -2,9 +2,15 @@ package com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.de
 
 import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantInsertRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStatus;
+import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purchase.exceptions.CantCreateCustomerBrokerPurchaseException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchase;
 import com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerSaleClausesDaoException;
 import com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerSaleLogsDaoException;
 import com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_sale.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerSaleNegotiationDaoException;
@@ -43,6 +49,37 @@ public class CustomerBrokerSaleNegotiationDao {
                 throw new CantInitializeCustomerBrokerSaleNegotiationDaoException(CantInitializeCustomerBrokerSaleNegotiationDaoException.DEFAULT_MESSAGE, FermatException.wrapException(exception), null, null);
             }
         }
+
+        public CustomerBrokerPurchase createCustomerBrokerPurchaseNegotiation(
+                String publicKeyCustomer,
+                String publicKeyBroker,
+                long startDataTime
+        ) throws CantCreateCustomerBrokerPurchaseException {
+
+            try {
+                DatabaseTable PurchaseNegotiationTable = this.database.getTable(CustomerBrokerSaleNegotiationDatabaseConstants.NEGOTIATIONS_TABLE_NAME);
+                DatabaseTableRecord recordToInsert   = PurchaseNegotiationTable.getEmptyRecord();
+
+                UUID negotiationId = UUID.randomUUID();
+
+                loadRecordAsNew(
+                        recordToInsert,
+                        negotiationId,
+                        publicKeyCustomer,
+                        publicKeyBroker,
+                        startDataTime
+                );
+
+                PurchaseNegotiationTable.insertRecord(recordToInsert);
+
+                return newCustomerBrokerSaleNegotiation(negotiationId, publicKeyCustomer, publicKeyBroker, startDataTime, NegotiationStatus.OPEN.getCode());
+
+            } catch (CantInsertRecordException e) {
+                throw new CantCreateCustomerBrokerPurchaseException("An exception happened",e,"","");
+            }
+
+        }
+
 
     /*
         Private methods
