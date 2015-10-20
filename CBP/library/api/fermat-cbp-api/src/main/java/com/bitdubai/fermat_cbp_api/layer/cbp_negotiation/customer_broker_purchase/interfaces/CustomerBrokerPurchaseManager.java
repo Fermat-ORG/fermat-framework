d56cae7a8e6995dcea7cb99b2c4addbf53d7e38f
vlzangel <vlzangel@gmail.com>
2015-10-20 16:26:09 -0430

    Refactory CustomerBrokerPurchaseManager
    Refactory CustomerBrokerPurchaseManager

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_negotiation/customer_broker_purchase/interfaces/CustomerBrokerPurchaseManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_negotiation/customer_broker_purchase/interfaces/CustomerBrokerPurchaseManager.java
@@ -3,7 +3,7 @@ package com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purcha
 import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
 import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purchase.exceptions.CantCreateCustomerBrokerPurchaseException;
-import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purchase.exceptions.CantListNegotianionsException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_negotiation.customer_broker_purchase.exceptions.CantListPurchaseNegotianionsException;
 
 import java.util.Collection;
 
@@ -16,8 +16,8 @@ public interface CustomerBrokerPurchaseManager {
     void cancelNegotiation(CustomerBrokerPurchase negotiation);
     void closeNegotiation(CustomerBrokerPurchase negotiation);
 
-    Collection<CustomerBrokerPurchase> getNegotiations() throws CantListNegotianionsException;
-    Collection<CustomerBrokerPurchase> getNegotiations(NegotiationStatus status) throws CantListNegotianionsException;
-    Collection<CustomerBrokerPurchase> getNegotiationsByCustomer(ActorIdentity customer) throws CantListNegotianionsException;
-    Collection<CustomerBrokerPurchase> getNegotiationsByBroker(ActorIdentity broker) throws CantListNegotianionsException;
+    Collection<CustomerBrokerPurchase> getNegotiations() throws CantListPurchaseNegotianionsException;
+    Collection<CustomerBrokerPurchase> getNegotiations(NegotiationStatus status) throws CantListPurchaseNegotianionsException;
+    Collection<CustomerBrokerPurchase> getNegotiationsByCustomer(ActorIdentity customer) throws CantListPurchaseNegotianionsException;
+    Collection<CustomerBrokerPurchase> getNegotiationsByBroker(ActorIdentity broker) throws CantListPurchaseNegotianionsException;
 }
