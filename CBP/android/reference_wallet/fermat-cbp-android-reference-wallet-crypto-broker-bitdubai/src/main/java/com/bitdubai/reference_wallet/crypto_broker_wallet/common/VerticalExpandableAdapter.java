f5e81d703e160bf96c93bbd26e201c60036b3902
Nelson Ramirez <nelsonalfo@gmail.com>
2015-10-21 18:33:24 -0430

    agregando el expandable reclycler view de matias a el fragmento de open negotiations

---
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/common/VerticalExpandableAdapter.java
@@ -0,0 +1,90 @@
+package com.bitdubai.reference_wallet.crypto_broker_wallet.common;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.ExpandableRecyclerAdapter;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.ParentListItem;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.VerticalChild;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.VerticalChildViewHolder;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.VerticalParent;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.VerticalParentViewHolder;
+
+import java.util.List;
+
+/**
+ * An example custom implementation of the ExpandableRecyclerAdapter.
+ *
+ * @author Ryan Brooks
+ * @version 1.0
+ * @since 5/27/2015
+ */
+public class VerticalExpandableAdapter extends ExpandableRecyclerAdapter<VerticalParentViewHolder, VerticalChildViewHolder> {
+
+    private LayoutInflater mInflater;
+
+    /**
+     * Public primary constructor.
+     *
+     * @param parentItemList the list of parent items to be displayed in the RecyclerView
+     */
+    public VerticalExpandableAdapter(Context context, List<? extends ParentListItem> parentItemList) {
+        super(parentItemList);
+        mInflater = LayoutInflater.from(context);
+    }
+
+    /**
+     * OnCreateViewHolder implementation for parent items. The desired ParentViewHolder should
+     * be inflated here
+     *
+     * @param parent for inflating the View
+     * @return the user's custom parent ViewHolder that must extend ParentViewHolder
+     */
+    @Override
+    public VerticalParentViewHolder onCreateParentViewHolder(ViewGroup parent) {
+        View view = mInflater.inflate(R.layout.cbp_list_item_parent, parent, false);
+        return new VerticalParentViewHolder(view);
+    }
+
+    /**
+     * OnCreateViewHolder implementation for child items. The desired ChildViewHolder should
+     * be inflated here
+     *
+     * @param parent for inflating the View
+     * @return the user's custom parent ViewHolder that must extend ParentViewHolder
+     */
+    @Override
+    public VerticalChildViewHolder onCreateChildViewHolder(ViewGroup parent) {
+        View view = mInflater.inflate(R.layout.cbp_list_item_child, parent, false);
+        return new VerticalChildViewHolder(view);
+    }
+
+    /**
+     * OnBindViewHolder implementation for parent items. Any data or view modifications of the
+     * parent view should be performed here.
+     *
+     * @param parentViewHolder the ViewHolder of the parent item created in OnCreateParentViewHolder
+     * @param position the position in the RecyclerView of the item
+     */
+    @Override
+    public void onBindParentViewHolder(VerticalParentViewHolder parentViewHolder, int position, ParentListItem parentListItem) {
+        VerticalParent verticalParent = (VerticalParent) parentListItem;
+        parentViewHolder.bind(verticalParent.getParentNumber(), verticalParent.getParentText());
+    }
+
+    /**
+     * OnBindViewHolder implementation for child items. Any data or view modifications of the
+     * child view should be performed here.
+     *
+     * @param childViewHolder the ViewHolder of the child item created in OnCreateChildViewHolder
+     * @param position the position in the RecyclerView of the item
+     */
+    @Override
+    public void onBindChildViewHolder(VerticalChildViewHolder childViewHolder, int position, Object childListItem) {
+        VerticalChild verticalChild = (VerticalChild) childListItem;
+        childViewHolder.bind(verticalChild.getChildText());
+    }
+}
--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/home/OpenNegotiationsTabFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/home/OpenNegotiationsTabFragment.java
@@ -2,34 +2,140 @@ package com.bitdubai.reference_wallet.crypto_broker_wallet.fragments.home;
 
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatWalletFragment;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.ExpandableRecyclerAdapter;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.VerticalChild;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.VerticalParent;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.common.VerticalExpandableAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class OpenNegotiationsTabFragment extends FermatFragment {
+public class OpenNegotiationsTabFragment extends FermatWalletFragment implements ExpandableRecyclerAdapter.ExpandCollapseListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+
+    private RecyclerView mRecyclerView;
+    private VerticalExpandableAdapter mExpandableAdapter;
 
 
-    public OpenNegotiationsTabFragment() {
-        // Required empty public constructor
-    }
-
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     */
     public static OpenNegotiationsTabFragment newInstance() {
         return new OpenNegotiationsTabFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_open_negotiations, container, false);
+
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.open_negotiations_recycler_view);
+
+        // Create a new adapter with 20 test data items
+        mExpandableAdapter = new VerticalExpandableAdapter(getActivity(), setUpTestData(5));
+
+        // Attach this activity to the Adapter as the ExpandCollapseListener
+        mExpandableAdapter.setExpandCollapseListener(this);
+
+        // Set the RecyclerView's adapter to the ExpandableAdapter we just created
+        mRecyclerView.setAdapter(mExpandableAdapter)
+        ;
+        // Set the layout manager to a LinearLayout manager for vertical list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return rootView;
     }
 
+
+    /**
+     * Save the instance state of the adapter to keep expanded/collapsed states when rotating or
+     * pausing the activity.
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        mExpandableAdapter.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onListItemExpanded(int position) {
+        //String toastMessage = getString(R.string.item_expanded, position);
+        Toast.makeText(getActivity(), "item expanded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onListItemCollapsed(int position) {
+        //String toastMessage = getString(R.string.item_collapsed, position);
+        Toast.makeText(getActivity(), "Item collapsed", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Method to set up test data used in the RecyclerView.
+     * <p/>
+     * Each child list item contains a string.
+     * Each parent list item contains a number corresponding to the number of the parent and a string
+     * that contains a message.
+     * Each parent also contains a list of children which is generated in this. Every odd numbered
+     * parent gets one child and every even numbered parent gets two children.
+     *
+     * @return A List of Objects that contains all parent items. Expansion of children are handled in the adapter
+     */
+    private List<VerticalParent> setUpTestData(int numItems) {
+        List<VerticalParent> verticalParentList = new ArrayList<>();
+
+        for (int i = 0; i < numItems; i++) {
+            List<VerticalChild> childItemList = new ArrayList<>();
+
+            VerticalChild verticalChild = new VerticalChild();
+            verticalChild.setChildText("hijo 1: " + i);
+            childItemList.add(verticalChild);
+
+            // Evens get 2 children, odds get 1
+            if (i % 2 == 0) {
+                VerticalChild verticalChild2 = new VerticalChild();
+                verticalChild2.setChildText("hijo 2" + i);
+                childItemList.add(verticalChild2);
+            }
+
+            VerticalParent verticalParent = new VerticalParent();
+            verticalParent.setChildItemList(childItemList);
+            verticalParent.setParentNumber(i);
+            verticalParent.setParentText("texto padre");
+            if (i == 0) {
+                verticalParent.setInitiallyExpanded(true);
+            }
+            verticalParentList.add(verticalParent);
+        }
+
+        return verticalParentList;
+    }
 
 }
