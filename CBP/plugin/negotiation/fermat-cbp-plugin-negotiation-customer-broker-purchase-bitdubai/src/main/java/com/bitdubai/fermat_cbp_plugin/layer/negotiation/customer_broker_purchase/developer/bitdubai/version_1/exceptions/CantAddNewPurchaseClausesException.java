0bf20f8306d9147a687d7a637ff2322cf27634ad
vlzangel <vlzangel@gmail.com>
2015-10-23 14:56:09 -0430

    CantAddNewPurchaseClausesException
    CantAddNewPurchaseClausesException

---
+++ CBP/plugin/negotiation/fermat-cbp-plugin-negotiation-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation/customer_broker_purchase/developer/bitdubai/version_1/exceptions/CantAddNewPurchaseClausesException.java
@@ -0,0 +1,34 @@
+package com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_purchase.developer.bitdubai.version_1.exceptions;
+
+import com.bitdubai.fermat_api.FermatException;
+
+/**
+ * The Class <code>package com.bitdubai.fermat_cbp_plugin.layer.negotiation.customer_broker_purchase.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerPurchaseNegotiationDatabaseException</code>
+ * is thrown when an error occurs initializing database
+ * <p/>
+ *
+ * Created by Jorge Gonzalez - (jorgeejgonzalez@gmail.com) on 12/10/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class CantAddNewPurchaseClausesException extends FermatException {
+
+    public static final String DEFAULT_MESSAGE = "CAN'T ADD NEW PURCHASE CLAUSES EXCEPTION";
+
+    public CantAddNewPurchaseClausesException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    public CantAddNewPurchaseClausesException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    public CantAddNewPurchaseClausesException(final String message) {
+        this(message, null);
+    }
+
+    public CantAddNewPurchaseClausesException() {
+        this(DEFAULT_MESSAGE);
+    }
+}
