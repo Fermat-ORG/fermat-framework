c06fd03db75df0854d05d2b1eb682592fbc759f8
javafrank <contrerasfrank@gmail.com>
2015-10-26 10:47:19 -0430

    #1693 remove changes in plugin implementation, leave original content

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_issuing/developer/bitdubai/version_1/structure/events/AssetIssuingTransactionMonitorAgent.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-issuing-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_issuing/developer/bitdubai/version_1/structure/events/AssetIssuingTransactionMonitorAgent.java
@@ -66,9 +66,6 @@ public class AssetIssuingTransactionMonitorAgent implements Agent,DealsWithLogge
     DigitalAssetIssuingVault digitalAssetIssuingVault;
     BitcoinNetworkManager bitcoinNetworkManager;
 //TODO: clean up this class
-    public AssetIssuingTransactionMonitorAgent() {
-
-    }
 
     public AssetIssuingTransactionMonitorAgent(EventManager eventManager,
                                                PluginDatabaseSystem pluginDatabaseSystem,
