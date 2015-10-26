96541f627554353423e6acd504d24fc51e8070ca
Nerio Indriago <nerioindriago@gmail.com>
2015-10-26 14:56:38 -0430

    Actualizacion a los Actors para mejorar la implementacion de los Record para los Actor Network Service

--- DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/redeem_point/RedeemPointActorRecord.java
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/redeem_point/RedeemPointActorRecord.java
@@ -1,4 +1,4 @@
-package com.bitdubai.fermat_dap_plugin.layer.actor.redeem.point.developer.bitdubai.version_1.structure;
+package com.bitdubai.fermat_dap_api.layer.dap_actor.redeem_point;
 
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.ConnectionState;
