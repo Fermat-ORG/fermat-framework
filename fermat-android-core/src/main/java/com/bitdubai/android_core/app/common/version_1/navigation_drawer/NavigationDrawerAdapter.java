727a0144b93b984e5ac69f6f9c1439c9b4d875e5
furszy <mati_fur@hotmail.com>
2015-10-27 14:49:54 -0300

    navigation drawer reverted

--- fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/navigation_drawer/NavigationDrawerAdapter.java
+++ fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/navigation_drawer/NavigationDrawerAdapter.java
@@ -1,24 +1,24 @@
 package com.bitdubai.android_core.app.common.version_1.navigation_drawer;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bitdubai.fermat.R;
 import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
 import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.MenuItem;
-import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserInformation;
-import com.bitdubai.sub_app.intra_user_community.common.UtilsFuncs;
-import com.bitdubai.sub_app.intra_user_community.holders.CryptoBrokerIdentityInfoViewHolder;
+
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mati on 2015.10.26..
  */
 public class NavigationDrawerAdapter extends FermatAdapter<MenuItem,MenuItemHolder> {
 
-    public NavigationDrawerAdapter(Context context, ArrayList<MenuItem> dataSet) {
+    public NavigationDrawerAdapter(Context context, List<MenuItem> dataSet) {
         super(context, dataSet);
     }
 
@@ -29,19 +29,33 @@ public class NavigationDrawerAdapter extends FermatAdapter<MenuItem,MenuItemHold
 
     @Override
     protected int getCardViewResource() {
-        return com.bitdubai.sub_app.intra_user_community.R.layout.intra_user_identity_list_item;
+        return R.layout.navigation_drawer_row;
     }
 
     @Override
     protected void bindHolder(final MenuItemHolder holder, final MenuItem data, final int position) {
         holder.getName().setText(data.getLabel());
+        switch (position) {
+            case 1:
+                holder.getThumbnail().setImageResource(R.drawable.btn_drawer_home_active);
+                break;
+            case 2:
+                holder.getThumbnail().setImageResource(R.drawable.btn_drawer_profile_normal);
+                break;
+            case 3:
+                holder.getThumbnail().setImageResource(R.drawable.ic_action_wallet);
+                break;
+            case 4:
+                holder.getThumbnail().setImageResource(R.drawable.ic_action_factory);
+                break;
+            case 5:
+                holder.getThumbnail().setImageResource(R.drawable.btn_drawer_logout_normal);
 
-//        byte[] profileImage = data.getProfileImage();
-//        Bitmap imageBitmap = profileImage == null ?
-//                BitmapFactory.decodeResource(context.getResources(), com.bitdubai.sub_app.intra_user_community.R.drawable.deniz_profile_picture) :
-//                BitmapFactory.decodeByteArray(profileImage, 0, profileImage.length);
-//
-//        Bitmap roundedBitmap = UtilsFuncs.getRoundedShape(imageBitmap);
-//        holder.getIdentityImage().setImageBitmap(roundedBitmap);
+                break;
+
+            default:
+                holder.getThumbnail().setImageResource(R.drawable.unknown_icon);
+                break;
+        }
     }
 }
