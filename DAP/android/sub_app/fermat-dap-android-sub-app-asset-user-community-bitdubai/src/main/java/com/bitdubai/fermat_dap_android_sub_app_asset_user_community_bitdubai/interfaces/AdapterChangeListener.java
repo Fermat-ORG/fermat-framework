7e7d3bcadfb921aa65320dba037728351e946cc6
Francisco Vasquez <fvasquezjatar@gmail.com>
2015-10-28 00:39:54 -0430

    Dap asset user AdapterChangeListener definition

---
+++ DAP/android/sub_app/fermat-dap-android-sub-app-asset-user-community-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_asset_user_community_bitdubai/interfaces/AdapterChangeListener.java
@@ -0,0 +1,11 @@
+package com.bitdubai.fermat_dap_android_sub_app_asset_user_community_bitdubai.interfaces;
+
+import java.util.List;
+
+/**
+ * Created by francisco on 28/10/15.
+ */
+public interface AdapterChangeListener<M> {
+
+    void onDataSetChanged(List<M> dataSet);
+}
