2b2a867d07edf703cac87bc7cb89794e60e97439
natalia <natalia_veronica_c@hotmail.com>
2015-10-29 15:43:50 -0300

    Corrections on Bitcoin Wallet add contact

--- CCP/android/reference_wallet/fermat-ccp-android-reference-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/ReceiveTransactionsFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-reference-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/ReceiveTransactionsFragment.java
@@ -264,15 +264,16 @@ public class ReceiveTransactionsFragment extends FermatWalletListFragment<Crypto
                     {
                         try
                         {
-                            cryptoWallet.convertConnectionToContact(walletContact.name,
-                                                                    Actors.INTRA_USER,
-                                                                    walletContact.actorPublicKey,
-                                                                    new byte[0],
-                                                                    Actors.INTRA_USER,
-                                                                    intraUserModuleManager.getActiveIntraUserIdentity().getPublicKey(),
-                                                                    "reference_wallet"/*referenceWalletSession.getWalletSessionType().getWalletPublicKey()*/ ,
-                                                                    CryptoCurrency.BITCOIN,
-                                                                    BlockchainNetworkType.TEST);
+                            if(!walletContact.isConnection)
+                                cryptoWallet.convertConnectionToContact(walletContact.name,
+                                                                        Actors.INTRA_USER,
+                                                                        walletContact.actorPublicKey,
+                                                                        new byte[0],
+                                                                        Actors.INTRA_USER,
+                                                                        intraUserModuleManager.getActiveIntraUserIdentity().getPublicKey(),
+                                                                        "reference_wallet"/*referenceWalletSession.getWalletSessionType().getWalletPublicKey()*/ ,
+                                                                        CryptoCurrency.BITCOIN,
+                                                                        BlockchainNetworkType.TEST);
 
                         }
                         catch (CantGetActiveLoginIdentityException e) {
