d6991246494a072b860fb3b1bbb134640b746847
Luis Campo <campusprize@gmail.com>
2015-10-30 17:24:27 -0430

    Tested DigitalAssetDistributionVault.setDigitalAssetMetadataAssetIssuerWalletDebit() AssetDistributionTransactionManager.setAssetDistributionDatabaseDao()

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_transaction_manager/SetAssetDistributionDatabaseDaoTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_transaction_manager/SetAssetDistributionDatabaseDaoTest.java
@@ -1,19 +1,21 @@
-package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.digital_asset_distributor;
+package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_transaction_manager;
 
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.AssetVaultManager;
 import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.CantSetObjectException;
-import com.bitdubai.fermat_dap_api.layer.dap_network_services.asset_transmission.interfaces.AssetTransmissionNetworkServiceManager;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.AssetDistributionTransactionManager;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.DigitalAssetDistributor;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.UUID;
@@ -21,13 +23,14 @@ import java.util.UUID;
 import static com.googlecode.catchexception.CatchException.catchException;
 import static com.googlecode.catchexception.CatchException.caughtException;
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Luis Campo (campusprize@gmail.com) on 20/10/15.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class SetAssetDistributionDatabaseDaoTest {
-    private AssetDistributionDao assetDistributionDatabaseDao;
+    private AssetDistributionDao assetDistributionDatabaseDao = Mockito.mock(AssetDistributionDao.class);
     private AssetDistributionTransactionManager mockAssetDistributionTransactionManager;
     @Mock
     private AssetVaultManager assetVaultManager;
@@ -40,12 +43,16 @@ public class SetAssetDistributionDatabaseDaoTest {
     private PluginDatabaseSystem pluginDatabaseSystem;
     @Mock
     private PluginFileSystem pluginFileSystem;
-
+    private Database mockDatabase = Mockito.mock(Database.class);
     @Before
     public void init() throws Exception {
         mockAssetDistributionTransactionManager = new AssetDistributionTransactionManager(assetVaultManager, errorManager, UUID.randomUUID(), pluginDatabaseSystem,
                 pluginFileSystem);
-        assetDistributionDatabaseDao = new AssetDistributionDao(pluginDatabaseSystem, UUID.randomUUID());
+        setUpMockitoRules();
+    }
+
+    private void setUpMockitoRules() throws Exception {
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(mockDatabase);
     }
 
     @Test
--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/digital_asset_distribution_vault/SetDigitalAssetMetadataAssetIssuerWalletDebitTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/digital_asset_distribution_vault/SetDigitalAssetMetadataAssetIssuerWalletDebitTest.java
@@ -1,27 +1,21 @@
 package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.digital_asset_distribution_vault;
 
 import com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.mocks.MockDigitalAssetMetadataForTesting;
+import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.ECCKeyPair;
 import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
 import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
 import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoStatus;
 import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
-import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.AssetVaultManager;
 import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.DigitalAssetMetadata;
-import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.CantSetObjectException;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_issuer.interfaces.ActorAssetIssuer;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_issuer.interfaces.ActorAssetIssuerManager;
-import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.AssetIssuerWalletTransactionRecordWrapper;
-import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.exceptions.CantRegisterCreditException;
-import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.exceptions.CantRegisterDebitException;
 import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.interfaces.AssetIssuerWallet;
 import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.interfaces.AssetIssuerWalletBalance;
+import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_issuer_wallet.interfaces.AssetIssuerWalletManager;
 import com.bitdubai.fermat_dap_api.layer.dap_wallet.common.enums.BalanceType;
-import com.bitdubai.fermat_dap_api.layer.dap_wallet.common.exceptions.CantGetTransactionsException;
-import com.bitdubai.fermat_dap_api.layer.dap_wallet.common.exceptions.CantLoadWalletException;
-import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.AssetDistributionTransactionManager;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.DigitalAssetDistributionVault;
-import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.DigitalAssetDistributor;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 
 import org.junit.Before;
@@ -35,6 +29,7 @@ import java.util.UUID;
 import static com.googlecode.catchexception.CatchException.catchException;
 import static com.googlecode.catchexception.CatchException.caughtException;
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Luis Campo (campusprize@gmail.com) on 29/10/15.
@@ -52,13 +47,29 @@ public class SetDigitalAssetMetadataAssetIssuerWalletDebitTest {
     private DigitalAssetMetadata digitalAssetMetadata;
     private CryptoTransaction genesisTransaction;
 
+    @Mock
+    private ActorAssetIssuerManager actorAssetIssuerManager;
+    @Mock
+    private ActorAssetIssuer actorAssetIssuer;
+    @Mock
+    private AssetIssuerWalletManager assetIssuerWalletManager;
+    @Mock
+    AssetIssuerWalletBalance assetIssuerWalletBalance;
+    @Mock
+    private AssetIssuerWallet assetIssuerWallet;
+    private BalanceType typeBalance;
     @Before
     public void init() throws Exception {
         pluginId = UUID.randomUUID();
         mockDigitalAssetDistributionVault = new DigitalAssetDistributionVault(pluginId, pluginFileSystem, errorManager);
+        mockDigitalAssetDistributionVault.walletPublicKey = "walletPublicKey";
+        mockDigitalAssetDistributionVault.setActorAssetIssuerManager(actorAssetIssuerManager);
+        mockDigitalAssetDistributionVault.setAssetIssuerWalletManager(assetIssuerWalletManager);
+
         digitalAssetMetadata = new MockDigitalAssetMetadataForTesting();
         genesisTransaction = new CryptoTransaction();
         String typeCoin = CryptoCurrency.BITCOIN.getCode();
+        typeBalance = BalanceType.getByCode("AVAILABLE");
         System.out.println("Type coin: "+typeCoin);
         CryptoCurrency cryptoCurrency = CryptoCurrency.getByCode(typeCoin);
         CryptoAddress addressFrom = new CryptoAddress("mxJJSdXdKQLS4NeX6Y8tXFFoNASQnBShtv", cryptoCurrency);
@@ -70,15 +81,26 @@ public class SetDigitalAssetMetadataAssetIssuerWalletDebitTest {
         genesisTransaction.setCryptoStatus(CryptoStatus.ON_CRYPTO_NETWORK);
         genesisTransaction.setTransactionHash("d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43");
         System.out.println("Initialized data successfully");
+        setUpMockitoRules();
+    }
+
+    private void setUpMockitoRules() throws Exception {
+        when(assetIssuerWallet.getBookBalance(typeBalance)).thenReturn(assetIssuerWalletBalance);
+        when(actorAssetIssuerManager.getActorAssetIssuer()).thenReturn(actorAssetIssuer);
+        when(assetIssuerWalletManager.loadAssetIssuerWallet("walletPublicKey")).thenReturn(assetIssuerWallet);
     }
 
     @Test
     public void setDigitalAssetMetadataAssetIssuerWalletDebitSuccesFullTest() throws Exception {
         System.out.println("Test method setDigitalAssetMetadataAssetIssuerWalletDebitSuccesFullTest()");
-        BalanceType typeBalance = BalanceType.getByCode("AVAILABLE");
+
         catchException(mockDigitalAssetDistributionVault).setDigitalAssetMetadataAssetIssuerWalletDebit(digitalAssetMetadata, genesisTransaction, typeBalance);
         Exception thrown = caughtException();
         assertThat(thrown)
                 .isNull();
     }
+
+
+
+
 }
