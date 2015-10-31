a13b506d7b8113f8758d0eceb4a2828cfbba69d0
Luis Campo <campusprize@gmail.com>
2015-10-31 08:41:08 -0430

    asset-distribution  creating test for method start of the class AssetDistributionRecorderService

---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_recorder_service/StartTest.java
@@ -0,0 +1,63 @@
+package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_recorder_service;
+
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventListener;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantStartServiceException;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.AssetDistributionTransactionManager;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.events.AssetDistributionRecorderService;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Luis Campo (campusprize@gmail.com) on 31/10/15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class StartTest {
+
+    @Mock
+    private FermatEventListener fermatEventListener1;
+    @Mock
+    private  FermatEventListener fermatEventListener2;
+    @Mock
+    private FermatEventListener fermatEventListener3;
+    @Mock
+    private  FermatEventListener fermatEventListener4;
+    @Mock
+    private  FermatEventListener fermatEventListener5;
+    @Mock
+    private ErrorManager errorManager;
+    private UUID pluginId;
+    @Mock
+    private PluginDatabaseSystem pluginDatabaseSystem;
+    private AssetDistributionRecorderService assetDistributionRecorderService;
+    private Database mockDatabase = Mockito.mock(Database.class);
+
+    @Before
+    public void init() throws Exception {
+        pluginId = UUID.randomUUID();
+        AssetDistributionDao assetDistributionDao = new AssetDistributionDao(pluginDatabaseSystem, pluginId);
+        assetDistributionRecorderService = new AssetDistributionRecorderService(assetDistributionDao);
+        setUpMockitoRules();
+    }
+    private void setUpMockitoRules() throws Exception {
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(mockDatabase);
+    }
+
+    @Test
+    public void StartRecordServiceSucces () throws CantStartServiceException {
+        assetDistributionRecorderService.start();
+    }
+}
