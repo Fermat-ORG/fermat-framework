4d129f8677a6ed7e979b5c3ee3e5c08a47132a27
Luis Campo <campusprize@gmail.com>
2015-11-02 09:28:24 -0430

    asset-distribution Database problem solved. test method AssetDistributionRecorderService.start () successful

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_recorder_service/StartTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_recorder_service/StartTest.java
@@ -1,14 +1,19 @@
 package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_recorder_service;
 
+import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventListener;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseFactory;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantStartServiceException;
-import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.AssetDistributionTransactionManager;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.events.AssetDistributionRecorderService;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.enums.EventType;
+import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +24,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.UUID;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 /**
@@ -26,7 +32,8 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class StartTest {
-
+    @Mock
+    private EventManager eventManager;
     @Mock
     private FermatEventListener fermatEventListener1;
     @Mock
@@ -43,21 +50,31 @@ public class StartTest {
     @Mock
     private PluginDatabaseSystem pluginDatabaseSystem;
     private AssetDistributionRecorderService assetDistributionRecorderService;
-    private Database mockDatabase = Mockito.mock(Database.class);
+    @Mock
+    private DatabaseFactory mockDatabaseFactory;
+    private Database database = Mockito.mock(Database.class);
+    private AssetDistributionDao assetDistributionDao = Mockito.mock(AssetDistributionDao.class);
 
     @Before
     public void init() throws Exception {
         pluginId = UUID.randomUUID();
-        AssetDistributionDao assetDistributionDao = new AssetDistributionDao(pluginDatabaseSystem, pluginId);
         assetDistributionRecorderService = new AssetDistributionRecorderService(assetDistributionDao);
+        assetDistributionRecorderService.setEventManager(eventManager);
         setUpMockitoRules();
     }
     private void setUpMockitoRules() throws Exception {
-        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(mockDatabase);
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(database);
+        when(eventManager.getNewListener(EventType.INCOMING_ASSET_ON_CRYPTO_NETWORK_WAITING_TRANSFERENCE_ASSET_USER)).thenReturn(fermatEventListener1);
+        when(eventManager.getNewListener( EventType.INCOMING_ASSET_ON_BLOCKCHAIN_WAITING_TRANSFERENCE_ASSET_USER)).thenReturn(fermatEventListener2);
+        when(eventManager.getNewListener(EventType.INCOMING_ASSET_REVERSED_ON_CRYPTO_NETWORK_WAITING_TRANSFERENCE_ASSET_USER)).thenReturn(fermatEventListener3);
+        when(eventManager.getNewListener(EventType.INCOMING_ASSET_REVERSED_ON_BLOCKCHAIN_WAITING_TRANSFERENCE_ASSET_USER)).thenReturn(fermatEventListener4);
+        when(eventManager.getNewListener(EventType.RECEIVED_NEW_DIGITAL_ASSET_METADATA_NOTIFICATION)).thenReturn(fermatEventListener5);
     }
 
     @Test
     public void StartRecordServiceSucces () throws CantStartServiceException {
         assetDistributionRecorderService.start();
+        ServiceStatus serviceStatus = assetDistributionRecorderService.getStatus();
+        assertThat(serviceStatus).isEqualTo(ServiceStatus.STARTED);
     }
 }
