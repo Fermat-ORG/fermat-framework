6d1ac75bc0611b0a70cd65244cf245463695f3b8
Angel Veloz <vlzangel91@gmail.com>
2015-11-02 04:10:26 -0430

    Refactory CustomerBrokerPurchasePluginRoot

--- CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_purchase/developer/bitdubai/version_1/CustomerBrokerPurchasePluginRoot.java
+++ CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_purchase/developer/bitdubai/version_1/CustomerBrokerPurchasePluginRoot.java
@@ -5,11 +5,23 @@ import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
 import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ContractStatus;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.CurrencyType;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ReferenceCurrency;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_purchase.exceptions.CantCreateCustomerBrokerPurchaseException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_purchase.exceptions.CantDeleteCustomerBrokerPurchaseException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_purchase.exceptions.CantGetListCustomerBrokerPurchaseException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_purchase.exceptions.CantupdateCustomerBrokerPurchaseException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_purchase.interfaces.CustomerBrokerPurchase;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_purchase.interfaces.CustomerBrokerPurchaseManager;
+import com.bitdubai.fermat_cbp_plugin.layer.contract.customer_broker_purchase.developer.bitdubai.version_1.database.CustomerBrokerPurchaseContractDao;
+import com.bitdubai.fermat_cbp_plugin.layer.contract.customer_broker_purchase.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerPurchaseContractDatabaseException;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 
@@ -21,11 +33,12 @@ import java.util.UUID;
 /**
  * Created by angel on 02/11/15.
  */
-public class CustomerBrokerPurchasePluginRoot implements DealsWithErrors, DealsWithLogger, DealsWithPluginDatabaseSystem, LogManagerForDevelopers, Service, Plugin {
+public class CustomerBrokerPurchasePluginRoot implements CustomerBrokerPurchaseManager, DealsWithErrors, DealsWithLogger, DealsWithPluginDatabaseSystem, LogManagerForDevelopers, Service, Plugin {
 
     private ErrorManager errorManager;
     private LogManager logManager;
     private PluginDatabaseSystem pluginDatabaseSystem;
+    private CustomerBrokerPurchaseContractDao customerBrokerPurchaseContractDao;
     private UUID pluginId;
     private ServiceStatus serviceStatus = ServiceStatus.CREATED;
 
@@ -63,15 +76,13 @@ public class CustomerBrokerPurchasePluginRoot implements DealsWithErrors, DealsW
     @Override
     public void start() throws CantStartPluginException {
         this.serviceStatus = ServiceStatus.STARTED;
-        /*
         try {
-            this.customerBrokerPurchaseContractDao = new CustomerBrokerPurchaseContractDao(pluginDatabaseSystem);
-            this.customerBrokerPurchaseContractDao.initialize(pluginId);
-        } catch (CantInitializeCustomerBrokerPurchaseContractDaoException cantInitializeCustomerBrokerPurchaseContractDaoException) {
+            this.customerBrokerPurchaseContractDao = new CustomerBrokerPurchaseContractDao(pluginDatabaseSystem, this.pluginId);
+            this.customerBrokerPurchaseContractDao.initializeDatabase();
+        } catch (CantInitializeCustomerBrokerPurchaseContractDatabaseException cantInitializeCustomerBrokerPurchaseContractDaoException) {
             //errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_DESIGNER_IDENTITY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, cantInitializeExtraUserRegistryException);
             // throw new CantStartPluginException(cantInitializeExtraUserRegistryException, Plugins.BITDUBAI_ACTOR_DEVELOPER);
         }
-        */
     }
 
     @Override
@@ -98,4 +109,49 @@ public class CustomerBrokerPurchasePluginRoot implements DealsWithErrors, DealsW
     public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
         this.pluginDatabaseSystem = pluginDatabaseSystem;
     }
+
+    /*
+    *   Methods of Menager
+    * */
+
+        @Override
+        public List<CustomerBrokerPurchase> getAllCustomerBrokerPurchaseFromCurrentDeviceUser() throws CantGetListCustomerBrokerPurchaseException {
+            return this.customerBrokerPurchaseContractDao.getAllCustomerBrokerPurchaseFromCurrentDeviceUser();
+        }
+
+        @Override
+        public CustomerBrokerPurchase getCustomerBrokerPurchaseForContractId(UUID ContractId) throws CantGetListCustomerBrokerPurchaseException {
+            return this.customerBrokerPurchaseContractDao.getCustomerBrokerPurchaseForContractId(ContractId);
+        }
+
+        @Override
+        public DatabaseTableRecord getCustomerBrokerSaleContractTable() {
+            return this.customerBrokerPurchaseContractDao.getCustomerBrokerPurchaseContractTable();
+        }
+
+        @Override
+        public CustomerBrokerPurchase createCustomerBrokerPurchase(String publicKeyCustomer, String publicKeyBroker, Float merchandiseAmount, CurrencyType merchandiseCurrency, Float referencePrice, ReferenceCurrency referenceCurrency, Float paymentAmount, CurrencyType paymentCurrency, long paymentExpirationDate, long merchandiseDeliveryExpirationDate) throws CantCreateCustomerBrokerPurchaseException {
+            return this.customerBrokerPurchaseContractDao.createCustomerBrokerPurchase(
+                    publicKeyCustomer,
+                    publicKeyBroker,
+                    merchandiseAmount,
+                    merchandiseCurrency,
+                    referencePrice,
+                    referenceCurrency,
+                    paymentAmount,
+                    paymentCurrency,
+                    paymentExpirationDate,
+                    merchandiseDeliveryExpirationDate
+            );
+        }
+
+        @Override
+        public void updateCustomerBrokerPurchase(UUID contractId, ContractStatus status) throws CantupdateCustomerBrokerPurchaseException {
+            this.customerBrokerPurchaseContractDao.updateCustomerBrokerPurchase(contractId, status);
+        }
+
+        @Override
+        public void deleteCustomerBrokerPurchase(UUID contractID) throws CantDeleteCustomerBrokerPurchaseException {
+            this.customerBrokerPurchaseContractDao.deleteCustomerBrokerPurchase(contractID);
+        }
 }
