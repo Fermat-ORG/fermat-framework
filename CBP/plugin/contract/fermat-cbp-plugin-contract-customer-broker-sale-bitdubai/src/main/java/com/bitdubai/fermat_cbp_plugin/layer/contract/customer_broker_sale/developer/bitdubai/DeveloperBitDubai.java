f2a97479b0fedf9e2762d9826b41ac4f0081e8e2
Angel Veloz <vlzangel91@gmail.com>
2015-11-02 03:10:04 -0430

    Refactory DeveloperBitDubai Contract Sale

--- CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_sale/developer/bitdubai/DeveloperBitDubai.java
+++ CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_sale/developer/bitdubai/DeveloperBitDubai.java
@@ -5,6 +5,7 @@ import com.bitdubai.fermat_api.PluginDeveloper;
 import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
 import com.bitdubai.fermat_api.layer.all_definition.enums.TimeFrequency;
 import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
+import com.bitdubai.fermat_cbp_plugin.layer.contract.customer_broker_sale.developer.bitdubai.version_1.CustomerBrokerSalePluginRoot;
 
 /**
  * Created by Angel on 16.09.15.
@@ -13,15 +14,14 @@ public class DeveloperBitDubai implements PluginDeveloper, PluginLicensor {
 
     Plugin plugin;
 
+    public DeveloperBitDubai() {
+        plugin = new CustomerBrokerSalePluginRoot();
+    }
+
     @Override
     public Plugin getPlugin() {
         return plugin;
     }
-
-    public DeveloperBitDubai() {
-        plugin = null;
-    }
-
 
     @Override
     public int getAmountToPay() {
