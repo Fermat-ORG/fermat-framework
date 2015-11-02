ccfa2af587978534d89b73a88559c81695f739bd
Angel Veloz <vlzangel91@gmail.com>
2015-11-02 03:35:50 -0430

    Refactory CustomerBrokerPurchaseContractDao

--- CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_purchase/developer/bitdubai/version_1/database/CustomerBrokerPurchaseContractDao.java
+++ CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_purchase/developer/bitdubai/version_1/database/CustomerBrokerPurchaseContractDao.java
@@ -2,10 +2,15 @@ package com.bitdubai.fermat_cbp_plugin.layer.contract.customer_broker_purchase.d
 
 import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantCreateDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantInsertRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.CurrencyType;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ReferenceCurrency;
 import com.bitdubai.fermat_cbp_plugin.layer.contract.customer_broker_purchase.developer.bitdubai.version_1.exceptions.CantInitializeCustomerBrokerPurchaseContractDatabaseException;
 
 import java.util.UUID;
@@ -47,4 +52,48 @@ public class CustomerBrokerPurchaseContractDao {
                 }
             }
         }
+
+        public CustomerBrokerPurchase createCustomerBrokerPurchase(
+                String publicKeyCustomer,
+                String publicKeyBroker,
+                float merchandiseAmount,
+                CurrencyType merchandiseCurrency,
+                float referencePrice,
+                ReferenceCurrency referenceCurrency,
+                float paymentAmount,
+                CurrencyType paymentCurrency,
+                long paymentExpirationDate,
+                long merchandiseDeliveryExpirationDate
+        ) throws CantCreateCustomerBrokerPurchaseException {
+    
+            try {
+                DatabaseTable PurchaseContractTable = this.database.getTable(CustomerBrokerPurchaseContractDatabaseConstants.CONTRACT_PURCHASE_TABLE_NAME);
+                DatabaseTableRecord recordToInsert   = PurchaseContractTable.getEmptyRecord();
+    
+                UUID contractID = UUID.randomUUID();
+    
+                loadRecordAsNew(
+                        recordToInsert,
+                        contractID,
+                        publicKeyCustomer,
+                        publicKeyBroker,
+                        merchandiseAmount,
+                        merchandiseCurrency,
+                        referencePrice,
+                        referenceCurrency,
+                        paymentAmount,
+                        paymentCurrency,
+                        paymentExpirationDate,
+                        merchandiseDeliveryExpirationDate
+                );
+    
+                PurchaseContractTable.insertRecord(recordToInsert);
+    
+                return null;
+            } catch (CantInsertRecordException e) {
+                throw new CantCreateCustomerBrokerPurchaseException("An exception happened",e,"","");
+            }
+    
+        }
+
 }
