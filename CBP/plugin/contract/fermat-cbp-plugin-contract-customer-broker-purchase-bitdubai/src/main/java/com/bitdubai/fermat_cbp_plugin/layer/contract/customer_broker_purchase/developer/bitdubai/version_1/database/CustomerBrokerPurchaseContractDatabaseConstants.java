645c2eb8f945c3428b8274d051851b4884eb0b98
Angel Veloz <vlzangel91@gmail.com>
2015-11-02 03:16:37 -0430

    Refactory CustomerBrokerPurchaseContractDatabaseConstants

--- CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_purchase/developer/bitdubai/version_1/database/CustomerBrokerPurchaseContractDatabaseConstants.java
+++ CBP/plugin/contract/fermat-cbp-plugin-contract-customer-broker-purchase-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/contract/customer_broker_purchase/developer/bitdubai/version_1/database/CustomerBrokerPurchaseContractDatabaseConstants.java
@@ -13,23 +13,23 @@ package com.bitdubai.fermat_cbp_plugin.layer.contract.customer_broker_purchase.d
 public class CustomerBrokerPurchaseContractDatabaseConstants {
 
     /**
-     * Contract database table definition.
+     * Contract Purchase database table definition.
      */
-    static final String CONTRACT_TABLE_NAME = "contract_purchase";
+    static final String CONTRACT_PURCHASE_TABLE_NAME = "contract_purchase";
 
-    static final String CONTRACT_CONTRACT_ID_COLUMN_NAME = "contract_id";
-    static final String CONTRACT_CUSTOMER_PUBLIC_KEY_COLUMN_NAME = "customer_public_key";
-    static final String CONTRACT_BROKER_USER_PUBLIC_KEY_COLUMN_NAME = "broker_user_public_key";
-    static final String CONTRACT_PAYMENT_CURRENCY_COLUMN_NAME = "payment_currency";
-    static final String CONTRACT_MERCHANDISE_CURRENCY_COLUMN_NAME = "merchandise_currency";
-    static final String CONTRACT_REFERENCE_PRICE_COLUMN_NAME = "reference_price";
-    static final String CONTRACT_REFERENCE_CURRENCY_COLUMN_NAME = "reference_currency";
-    static final String CONTRACT_PAYMENT_AMOUNT_COLUMN_NAME = "payment_amount";
-    static final String CONTRACT_MERCHANDISE_AMOUNT_COLUMN_NAME = "merchandise_amount";
-    static final String CONTRACT_PAYMENT_EXPIRATION_DATE_COLUMN_NAME = "payment_expiration_date";
-    static final String CONTRACT_MERCHANDISE_DELIVERY_EXPIRATION_DATE_COLUMN_NAME = "merchandise_delivery_expiration_date";
-    static final String CONTRACT_STATUS_COLUMN_NAME = "status";
+    static final String CONTRACT_PURCHASE_CONTRACT_ID_COLUMN_NAME = "contract_id";
+    static final String CONTRACT_PURCHASE_CUSTOMER_PUBLIC_KEY_COLUMN_NAME = "customer_public_key";
+    static final String CONTRACT_PURCHASE_BROKER_USER_PUBLIC_KEY_COLUMN_NAME = "broker_user_public_key";
+    static final String CONTRACT_PURCHASE_PAYMENT_CURRENCY_COLUMN_NAME = "payment_currency";
+    static final String CONTRACT_PURCHASE_MERCHANDISE_CURRENCY_COLUMN_NAME = "merchandise_currency";
+    static final String CONTRACT_PURCHASE_REFERENCE_PRICE_COLUMN_NAME = "reference_price";
+    static final String CONTRACT_PURCHASE_REFERENCE_CURRENCY_COLUMN_NAME = "reference_currency";
+    static final String CONTRACT_PURCHASE_PAYMENT_AMOUNT_COLUMN_NAME = "payment_amount";
+    static final String CONTRACT_PURCHASE_MERCHANDISE_AMOUNT_COLUMN_NAME = "merchandise_amount";
+    static final String CONTRACT_PURCHASE_PAYMENT_EXPIRATION_DATE_COLUMN_NAME = "payment_expiration_date";
+    static final String CONTRACT_PURCHASE_MERCHANDISE_DELIVERY_EXPIRATION_DATE_COLUMN_NAME = "merchandise_delivery_expiration_date";
+    static final String CONTRACT_PURCHASE_STATUS_COLUMN_NAME = "status";
 
-    static final String CONTRACT_FIRST_KEY_COLUMN = "contract_id";
+    static final String CONTRACT_PURCHASE_FIRST_KEY_COLUMN = "contract_id";
 
 }
