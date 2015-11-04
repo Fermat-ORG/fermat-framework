bbaf70017dcfb2735f5cff80ea5932e67c86d09b
Luis Campo <campusprize@gmail.com>
2015-11-04 15:58:12 -0430

    asset-distribution   Deleted unused class

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_transaction_manager/SetDigitalAssetDistributionVaultTest.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_transaction_manager;
-
-/**
- * Created by luis on 20/10/15.
- */
-public class SetDigitalAssetDistributionVaultTest {
-}
