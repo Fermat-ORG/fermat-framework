c7433f56ccf2611b8749ba0072849b7146f88b05
natalia <natalia_veronica_c@hotmail.com>
2015-11-05 13:10:16 -0300

    Outgoing intra actor - Transaction Agent not run #2528

--- CCP/plugin/transaction/fermat-ccp-plugin-transaction-outgoing-intra-actor-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/transaction/outgoing_intra_actor/developer/bitdubai/version_1/OutgoingIntraActorTransactionPluginRoot.java
+++ CCP/plugin/transaction/fermat-ccp-plugin-transaction-outgoing-intra-actor-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/transaction/outgoing_intra_actor/developer/bitdubai/version_1/OutgoingIntraActorTransactionPluginRoot.java
@@ -217,6 +217,8 @@ public class OutgoingIntraActorTransactionPluginRoot extends AbstractPlugin impl
                                                                                             this.cryptoTransmissionNetworkServiceManager);
             this.transactionProcessorAgent.start();
 
+            this.serviceStatus = ServiceStatus.STARTED;
+
         } catch (CantInitializeOutgoingIntraActorDaoException e) {
             reportUnexpectedException(e);
         } catch (Exception e) {
