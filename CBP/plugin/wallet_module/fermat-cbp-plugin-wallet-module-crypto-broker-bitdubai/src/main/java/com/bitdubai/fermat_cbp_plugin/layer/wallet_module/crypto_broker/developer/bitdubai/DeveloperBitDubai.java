8e6e0e5d4757d7e0603726196daabf501b34b5a3
Nelson Ramirez <nelsonalfo@gmail.com>
2015-11-05 18:08:09 -0430

    Implementados prototipos de los metodos getNegotiationsWaitingForBroker() y getNegotiationsWaitingForCustomer() en el  Crypto Broker Wallet Module
     Issue #2415

--- CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_broker/developer/bitdubai/DeveloperBitDubai.java
+++ CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_broker/developer/bitdubai/DeveloperBitDubai.java
@@ -8,13 +8,17 @@ import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
 import com.bitdubai.fermat_cbp_plugin.layer.wallet_module.crypto_broker.developer.bitdubai.version_1.CryptoBrokerWalletModulePluginRoot;
 
 /**
- * Created by Angel on 16.09.15.
+ * Developer class of the Crypto Broker Wallet Module
+ *
+ * @author Nelson Ramirez
+ * @version 1.0
+ * @since 5/11/2015
  */
 public class DeveloperBitDubai implements PluginDeveloper, PluginLicensor {
 
     Plugin plugin;
 
-    public DeveloperBitDubai () {
+    public DeveloperBitDubai() {
         plugin = new CryptoBrokerWalletModulePluginRoot();
     }
 
--- CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_broker/developer/bitdubai/version_1/CryptoBrokerWalletModulePluginRoot.java
+++ CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_broker/developer/bitdubai/version_1/CryptoBrokerWalletModulePluginRoot.java
@@ -1,6 +1,7 @@
 package com.bitdubai.fermat_cbp_plugin.layer.wallet_module.crypto_broker.developer.bitdubai.version_1;
 
 import com.bitdubai.fermat_api.CantStartPluginException;
+import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
 import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
@@ -11,6 +12,7 @@ import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
 import com.bitdubai.fermat_cbp_api.layer.cbp_wallet_module.crypto_broker.exceptions.CantGetCryptoBrokerWalletException;
 import com.bitdubai.fermat_cbp_api.layer.cbp_wallet_module.crypto_broker.interfaces.CryptoBrokerWallet;
 import com.bitdubai.fermat_cbp_api.layer.cbp_wallet_module.crypto_broker.interfaces.CryptoBrokerWalletModuleManager;
+import com.bitdubai.fermat_cbp_plugin.layer.wallet_module.crypto_broker.developer.bitdubai.version_1.structure.CryptoBrokerWalletModuleCryptoBrokerWalletManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.DealsWithErrors;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 
@@ -18,13 +20,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 /**
  * Module for the Crypto Broker Wallet
  *
  * @author Nelson Ramirez
  * @version 1.0
- * @since 05-11-2015
+ * @since 05/11/2015
  */
 public class CryptoBrokerWalletModulePluginRoot implements DealsWithErrors, DealsWithLogger, LogManagerForDevelopers, Service, Plugin, CryptoBrokerWalletModuleManager {
 
@@ -56,7 +59,18 @@ public class CryptoBrokerWalletModulePluginRoot implements DealsWithErrors, Deal
 
     @Override
     public CryptoBrokerWallet getCryptoBrokerWallet(String walletPublicKey) throws CantGetCryptoBrokerWalletException {
-        return null;
+        try {
+            LogLevel logLevel = CryptoBrokerWalletModulePluginRoot.getLogLevelByClass(this.getClass().getName());
+
+            logManager.log(logLevel, "CryptoBrokerWallet instantiation started...", null, null);
+            CryptoBrokerWallet walletModuleManager = new CryptoBrokerWalletModuleCryptoBrokerWalletManager();
+            logManager.log(logLevel, "CryptoBrokerWallet instantiation finished successfully.", null, null);
+
+            return walletModuleManager;
+
+        } catch (Exception e) {
+            throw new CantGetCryptoBrokerWalletException(CantGetCryptoBrokerWalletException.DEFAULT_MESSAGE, FermatException.wrapException(e));
+        }
     }
 
     @Override
@@ -118,5 +132,27 @@ public class CryptoBrokerWalletModulePluginRoot implements DealsWithErrors, Deal
     @Override
     public ServiceStatus getStatus() {
         return this.serviceStatus;
+    }
+
+    /**
+     * Static method to get the logging level from any class under root.
+     *
+     * @param className
+     * @return
+     */
+    public static LogLevel getLogLevelByClass(String className) {
+        try {
+            /**
+             * sometimes the classname may be passed dinamically with an $moretext
+             * I need to ignore whats after this.
+             */
+            String[] correctedClass = className.split(Pattern.quote("$"));
+            return CryptoBrokerWalletModulePluginRoot.newLoggingLevel.get(correctedClass[0]);
+        } catch (Exception e) {
+            /**
+             * If I couldn't get the correct loggin level, then I will set it to minimal.
+             */
+            return DEFAULT_LOG_LEVEL;
+        }
     }
 }
