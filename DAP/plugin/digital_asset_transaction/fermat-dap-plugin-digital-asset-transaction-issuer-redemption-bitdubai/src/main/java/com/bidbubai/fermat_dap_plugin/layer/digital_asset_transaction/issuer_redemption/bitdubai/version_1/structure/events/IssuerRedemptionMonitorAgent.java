7cbc528f45cc53c8073708303f086c0642323b01
Manuel Perez <darkpriestrelative@gmail.com>
2015-11-06 12:28:00 -0430

    Fix isAssetsByReceptionStatus method in AssetReceptionDao

---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-issuer-redemption-bitdubai/src/main/java/com/bidbubai/fermat_dap_plugin/layer/digital_asset_transaction/issuer_redemption/bitdubai/version_1/structure/events/IssuerRedemptionMonitorAgent.java
@@ -0,0 +1,9 @@
+package com.bidbubai.fermat_dap_plugin.layer.digital_asset_transaction.issuer_redemption.bitdubai.version_1.structure.events;
+
+
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 09/10/15.
+ */
+public class IssuerRedemptionMonitorAgent {
+}
