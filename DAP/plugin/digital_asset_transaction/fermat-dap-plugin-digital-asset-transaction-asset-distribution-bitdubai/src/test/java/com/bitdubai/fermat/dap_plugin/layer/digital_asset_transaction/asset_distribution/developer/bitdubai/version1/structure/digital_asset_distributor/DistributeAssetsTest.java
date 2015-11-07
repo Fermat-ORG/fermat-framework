9ff09a131bfaf912108e687d1173b571fee943ba
Luis Campo <campusprize@gmail.com>
2015-11-07 09:13:26 -0430

    asset-distribution  Created Mocks (ActorAssetIssuer, ActorAssetIssuerManager)  Tested DigitalAssetDistributor.distributeAssets()

---
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/digital_asset_distributor/DistributeAssetsTest.java
@@ -0,0 +1,156 @@
+package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.digital_asset_distributor;
+
+import com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.mocks.MockActorAssetIssuerManager;
+import com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.mocks.MockDigitalAssetMetadataForTesting;
+import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
+import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoStatus;
+import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FilePrivacy;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginTextFile;
+import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.exceptions.CantGetGenesisTransactionException;
+import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.interfaces.BitcoinNetworkManager;
+import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.DigitalAssetMetadata;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_issuer.interfaces.ActorAssetIssuer;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_issuer.interfaces.ActorAssetIssuerManager;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces.ActorAssetUser;
+import com.bitdubai.fermat_dap_api.layer.dap_network_services.asset_transmission.interfaces.AssetTransmissionNetworkServiceManager;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_distribution.exceptions.CantDistributeDigitalAssetsException;
+import com.bitdubai.fermat_dap_api.layer.dap_wallet.common.enums.BalanceType;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.developer_utils.mocks.MockActorAssetUserForTesting;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.exceptions.CantDeliverDigitalAssetException;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.DigitalAssetDistributionVault;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.DigitalAssetDistributor;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Luis Campo (campusprize@gmail.com) on 04/11/15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DistributeAssetsTest {
+    @Mock
+    private ActorAssetIssuer actorAssetIssuer;
+    @Mock
+    private MockActorAssetIssuerManager actorAssetIssuerManager;
+    @Mock
+    private ErrorManager errorManager;
+    @Mock
+    private PluginFileSystem pluginFileSystem;
+    @Mock
+    private BitcoinNetworkManager bitcoinNetworkManager;
+    @Mock
+    private AssetTransmissionNetworkServiceManager assetTransmissionNetworkServiceManager;
+    @Mock
+    private PluginTextFile digitalAssetFile;
+
+    private UUID pluginId;
+    private AssetDistributionDao assetDistributionDao = mock(AssetDistributionDao.class);
+    private DigitalAssetDistributionVault digitalAssetDistributionVault;
+    private MockDigitalAssetMetadataForTesting mockDigitalAssetMetadata;
+    private DigitalAssetDistributor digitalAssetDistributor;
+    private CryptoTransaction cryptoTransaction;
+    private CryptoTransaction genesisTransaction;
+    private List<CryptoTransaction> listGenesisTransaction;
+    private HashMap<DigitalAssetMetadata, ActorAssetUser> digitalAssetsToDistribute;
+
+    private FilePrivacy FILE_PRIVACY;
+    private FileLifeSpan FILE_LIFE_SPAN;
+    private String fileName= "file.xml";
+    private String digitalAssetFileStoragePath = "digital-asset-transmission/digital-asset";
+
+
+    @Before
+    public void init () throws Exception {
+        FILE_PRIVACY = FilePrivacy.getByCode(FilePrivacy.PUBLIC.getCode());
+        FILE_LIFE_SPAN = FileLifeSpan.getByCode(FileLifeSpan.PERMANENT.getCode());
+        pluginId = UUID.randomUUID();
+        actorAssetIssuerManager = new MockActorAssetIssuerManager();
+        digitalAssetDistributionVault = Mockito.mock(DigitalAssetDistributionVault.class);//new DigitalAssetDistributionVault(pluginId, pluginFileSystem, errorManager);
+        digitalAssetDistributor = new DigitalAssetDistributor(errorManager, pluginId, pluginFileSystem);
+        digitalAssetDistributor.setActorAssetIssuerManager(actorAssetIssuerManager);
+        digitalAssetDistributor.setDigitalAssetDistributionVault(digitalAssetDistributionVault);
+        digitalAssetDistributor.setWalletPublicKey("walletPublicKey");
+        digitalAssetDistributor.setAssetDistributionDao(assetDistributionDao);
+        digitalAssetDistributor.setBitcoinCryptoNetworkManager(bitcoinNetworkManager);
+        digitalAssetDistributor.setAssetTransmissionNetworkServiceManager(assetTransmissionNetworkServiceManager);
+
+                mockDigitalAssetMetadata = new MockDigitalAssetMetadataForTesting();
+
+        genesisTransaction = new CryptoTransaction();
+        String typeCoin = CryptoCurrency.BITCOIN.getCode();
+        BalanceType typeBalance = BalanceType.getByCode("AVAILABLE");
+        System.out.println("Type coin: "+typeCoin);
+        CryptoCurrency cryptoCurrency = CryptoCurrency.getByCode(typeCoin);
+        CryptoAddress addressFrom = new CryptoAddress("mxJJSdXdKQLS4NeX6Y8tXFFoNASQnBShtv", cryptoCurrency);
+        CryptoAddress addressTo = new CryptoAddress("mxJJSdXdKQLS4NeX6Y8tXFFoNASQnBShtv", cryptoCurrency);
+        genesisTransaction.setAddressFrom(addressFrom);
+        genesisTransaction.setAddressTo(addressTo);
+        genesisTransaction.setCryptoAmount(10);
+        genesisTransaction.setCryptoCurrency(cryptoCurrency);
+        genesisTransaction.setCryptoStatus(CryptoStatus.ON_CRYPTO_NETWORK);
+        genesisTransaction.setTransactionHash("d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43");
+        listGenesisTransaction =  new ArrayList<CryptoTransaction>();
+        listGenesisTransaction.add(genesisTransaction);
+        cryptoTransaction=new CryptoTransaction(mockDigitalAssetMetadata.getGenesisTransaction(),
+                mockDigitalAssetMetadata.getDigitalAsset().getGenesisAddress(),
+                addressTo,CryptoCurrency.BITCOIN, 100000,CryptoStatus.ON_BLOCKCHAIN);
+        cryptoTransaction.setOp_Return(mockDigitalAssetMetadata.getDigitalAssetHash());
+        digitalAssetDistributor.cryptoTransaction = cryptoTransaction;
+
+        ActorAssetUser actorAssetUser = new MockActorAssetUserForTesting();
+        digitalAssetsToDistribute = new HashMap<DigitalAssetMetadata, ActorAssetUser>();
+        digitalAssetsToDistribute.put(mockDigitalAssetMetadata, actorAssetUser);
+        //setUpMockitoRules();
+    }
+
+    private void setUpMockitoRules() throws Exception {
+        when(bitcoinNetworkManager.getGenesisTransaction(mockDigitalAssetMetadata.getGenesisTransaction())).thenReturn(listGenesisTransaction);
+    }
+
+    @Test
+    public void distributeAssetsTest () throws CantDistributeDigitalAssetsException {
+        digitalAssetDistributor.distributeAssets(digitalAssetsToDistribute);
+    }
+
+    @Test
+    public void distributeAssetsTestThrowsCCantDistributeDigitalAssetsExceptionTest () throws CantGetGenesisTransactionException, CantDistributeDigitalAssetsException {
+        when(bitcoinNetworkManager.getGenesisTransaction(mockDigitalAssetMetadata.getGenesisTransaction())).thenReturn(null);
+        catchException(digitalAssetDistributor).distributeAssets(null);
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantDistributeDigitalAssetsException.class);
+    }
+
+    /*@Test
+    public void checkDigitalAssetMetadataThrowsCantGetGenesisTransactionExceptionDigitalAssetModifyTest () throws CantDeliverDigitalAssetException, CantGetGenesisTransactionException {
+        cryptoTransaction.setOp_Return("ffg55dsds55dsdsrfeetgga5");
+        digitalAssetDistributor.cryptoTransaction = cryptoTransaction;
+        catchException(digitalAssetDistributor).checkDigitalAssetMetadata(mockDigitalAssetMetadata);
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantDeliverDigitalAssetException.class);
+    }*/
+}
