7dfd1390790e8bbe4d4d63aff029d96498dcbac4
Luis Campo <campusprize@gmail.com>
2015-11-08 07:57:43 -0430

    asset-distribution  Tested methos AssetDistributionDao.updateEventStatus("eventId")  Update test updateDistributionStatusByGenesisTransaction(DistributionStatus.ASSET_ACCEPTED, genesisTransaction)

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/UpdateDistributionStatusByGenesisTransactionTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/UpdateDistributionStatusByGenesisTransactionTest.java
@@ -1,6 +1,5 @@
 package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_dao;
 
-import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoStatus;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
@@ -8,9 +7,7 @@ import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseS
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantExecuteQueryException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_dap_api.layer.all_definition.enums.DistributionStatus;
-import com.bitdubai.fermat_dap_api.layer.dap_network_services.asset_transmission.interfaces.DigitalAssetMetadataTransaction;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.UnexpectedResultReturnedFromDatabaseException;
-import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.exceptions.CantCheckAssetDistributionProgressException;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
 
@@ -27,7 +24,6 @@ import java.util.UUID;
 import static com.googlecode.catchexception.CatchException.catchException;
 import static com.googlecode.catchexception.CatchException.caughtException;
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -35,8 +31,6 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class UpdateDistributionStatusByGenesisTransactionTest {
-    @Mock
-    private DigitalAssetMetadataTransaction digitalAssetMetadataTransaction;
     @Mock
     Database database;
     @Mock
@@ -77,8 +71,6 @@ public class UpdateDistributionStatusByGenesisTransactionTest {
     @Test
     public void updateDistributionStatusByGenesisTransactionThrowsCantExecuteQueryException() throws Exception {
         String genesisTransaction = "d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43";
-        UUID distributionId = UUID.randomUUID();
-
         when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenThrow(new CantOpenDatabaseException("error"));
         catchException(mockAssetDistributionDao).updateDistributionStatusByGenesisTransaction(DistributionStatus.ASSET_ACCEPTED, genesisTransaction);
         Exception thrown = caughtException();
@@ -90,8 +82,6 @@ public class UpdateDistributionStatusByGenesisTransactionTest {
     @Test
     public void updateDistributionStatusByGenesisTransactionThrowsUnexpectedResultReturnedFromDatabaseException() throws Exception {
         String genesisTransaction = "d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43";
-        UUID distributionId = UUID.randomUUID();
-
         when(databaseTable.getRecords()).thenReturn(recordsForException);
         catchException(mockAssetDistributionDao).updateDistributionStatusByGenesisTransaction(DistributionStatus.ASSET_ACCEPTED, genesisTransaction);
         Exception thrown = caughtException();
--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/UpdateEventStatusTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/UpdateEventStatusTest.java
@@ -1,13 +1,29 @@
 package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_dao;
 
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantExecuteQueryException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.UnexpectedResultReturnedFromDatabaseException;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Luis Campo (campusprize@gmail.com) on 03/10/15.
@@ -15,10 +31,59 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class UpdateEventStatusTest {
     @Mock
+    Database database;
+    @Mock
+    DatabaseTable databaseTable;
+    @Mock
+    DatabaseTableRecord databaseTableRecord;
+    UUID pluginId;
+    @Mock
+    PluginDatabaseSystem pluginDatabaseSystem;
     private AssetDistributionDao mockAssetDistributionDao;
+    List<DatabaseTableRecord> records, recordsForException;
+
+    @Before
+    public void init () throws Exception {
+
+        pluginId = UUID.randomUUID();
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(database);
+        mockAssetDistributionDao = new AssetDistributionDao(pluginDatabaseSystem, pluginId);
+        records = new LinkedList<>();
+        records.add(databaseTableRecord);
+        recordsForException = new LinkedList<>();
+        recordsForException.add(databaseTableRecord);
+        recordsForException.add(databaseTableRecord);
+        setUpGeneralMockitoRules();
+    }
+
+    private void setUpGeneralMockitoRules() throws Exception{
+        when(database.getTable(AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_EVENTS_RECORDED_TABLE_NAME)).thenReturn(databaseTable);
+        when(databaseTable.getRecords()).thenReturn(records);
+    }
 
     @Test
     public void updateEventStatusTest () throws CantExecuteQueryException, UnexpectedResultReturnedFromDatabaseException {
         mockAssetDistributionDao.updateEventStatus("eventId");
+    }
+
+    @Test
+    public void updateEventStatusThrowsCantExecuteQueryException() throws Exception {
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenThrow(new CantOpenDatabaseException("error"));
+        catchException(mockAssetDistributionDao).updateEventStatus("eventId");
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantExecuteQueryException.class);
+    }
+
+    @Test
+    public void updateEventStatusThrowsUnexpectedResultReturnedFromDatabaseException() throws Exception {
+        when(databaseTable.getRecords()).thenReturn(recordsForException);
+        catchException(mockAssetDistributionDao).updateEventStatus("eventId");
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantExecuteQueryException.class);
+        assertThat(thrown.getCause()).isNotNull().isInstanceOf(UnexpectedResultReturnedFromDatabaseException.class);
     }
 }
