6205aa43f298afaffcaa12822ef12c4428ceb126
Luis Campo <campusprize@gmail.com>
2015-11-08 07:07:19 -0430

    asset-distribution  Tested methos class AssetDistributionDao isGenesisTransactionRegistered(genesisTransaction) isPendingIncomingCryptoEvents() isPendingNetworkLayerEvents() isPendingTransactions(CryptoStatus.PENDING_SUBMIT) persistDigitalAsset(genesisTransaction, localStoragePath, digitalAssetHash, actorReceiverPublicKey, actorReceiverBitcoinAddress) Created test in class AssetDistributionDatabaseConstants

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/IsPendingIncomingCryptoEventsTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/IsPendingIncomingCryptoEventsTest.java
@@ -1,9 +1,13 @@
 package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_dao;
 
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantExecuteQueryException;
-import com.bitdubai.fermat_dap_api.layer.dap_network_services.asset_transmission.interfaces.DigitalAssetMetadataTransaction;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,27 +15,62 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.mock;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Luis Campo (campusprize@gmail.com) on 02/10/15.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class IsPendingIncomingCryptoEventsTest {
+    @Mock
+    Database database;
+    @Mock
+    DatabaseTable databaseTable;
+    @Mock
+    DatabaseTableRecord databaseTableRecord;
+    UUID pluginId;
+    @Mock
+    PluginDatabaseSystem pluginDatabaseSystem;
+    List<DatabaseTableRecord> records;
     private AssetDistributionDao mockAssetDistributionDao;
-    @Mock
-    private PluginDatabaseSystem pluginDatabaseSystem;
-    @Mock
-    private DigitalAssetMetadataTransaction digitalAssetMetadataTransaction;
 
     @Before
     public void init () throws Exception {
-        mockAssetDistributionDao = mock(AssetDistributionDao.class);
+
+        pluginId = UUID.randomUUID();
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(database);
+        mockAssetDistributionDao = new AssetDistributionDao(pluginDatabaseSystem, pluginId);
+        records = new LinkedList<>();
+        records.add(databaseTableRecord);
+        setUpGeneralMockitoRules();
+    }
+
+    private void setUpGeneralMockitoRules() throws Exception{
+        when(database.getTable(AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_EVENTS_RECORDED_TABLE_NAME)).thenReturn(databaseTable);
+        when(databaseTable.getRecords()).thenReturn(records);
     }
 
 
     @Test
     public void isPendingIncomingCryptoEventsTest () throws CantExecuteQueryException {
-        mockAssetDistributionDao.isPendingIncomingCryptoEvents();
+        boolean isPending = mockAssetDistributionDao.isPendingIncomingCryptoEvents();
+        assertThat(isPending).isTrue();
+    }
+
+    @Test
+    public void isPendingIncomingCryptoEventsThrowsCantExecuteQueryException() throws Exception {
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenThrow(new CantOpenDatabaseException("error"));
+        catchException(mockAssetDistributionDao).isPendingIncomingCryptoEvents();
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantExecuteQueryException.class);
     }
 }
