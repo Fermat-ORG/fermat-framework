fdc471dcbc15817a0be4ae0a1ead039f1ab1f311
Luis Campo <campusprize@gmail.com>
2015-11-08 07:29:48 -0430

    asset-distribution  Tested methos AssetDistributionDao.persistDistributionId(genesisTransaction, distributionId)

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/PersistDistributionIdTest.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-distribution-bitdubai/src/test/java/com/bitdubai/fermat/dap_plugin/layer/digital_asset_transaction/asset_distribution/developer/bitdubai/version1/structure/asset_distribution_dao/PersistDistributionIdTest.java
@@ -1,14 +1,29 @@
 package com.bitdubai.fermat.dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version1.structure.asset_distribution_dao;
 
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantPersistsTransactionUUIDException;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.UnexpectedResultReturnedFromDatabaseException;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDao;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_distribution.developer.bitdubai.version_1.structure.database.AssetDistributionDatabaseConstants;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Luis Campo (campusprize@gmail.com) on 03/10/15.
@@ -16,12 +31,67 @@ import java.util.UUID;
 @RunWith(MockitoJUnitRunner.class)
 public class PersistDistributionIdTest {
     @Mock
+    Database database;
+    @Mock
+    DatabaseTable databaseTable;
+    @Mock
+    DatabaseTableRecord databaseTableRecord;
+    UUID pluginId;
+    @Mock
+    PluginDatabaseSystem pluginDatabaseSystem;
     private AssetDistributionDao mockAssetDistributionDao;
+    List<DatabaseTableRecord> records, recordsForException;
+
+    @Before
+    public void init () throws Exception {
+
+        pluginId = UUID.randomUUID();
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenReturn(database);
+        mockAssetDistributionDao = new AssetDistributionDao(pluginDatabaseSystem, pluginId);
+        records = new LinkedList<>();
+        records.add(databaseTableRecord);
+        recordsForException = new LinkedList<>();
+        recordsForException.add(databaseTableRecord);
+        recordsForException.add(databaseTableRecord);
+        setUpGeneralMockitoRules();
+    }
+
+    private void setUpGeneralMockitoRules() throws Exception{
+        when(database.getTable(AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_TABLE_NAME)).thenReturn(databaseTable);
+        when(databaseTable.getRecords()).thenReturn(records);
+    }
 
     @Test
     public void persistDistributionIdTest () throws CantPersistsTransactionUUIDException {
         String genesisTransaction = "d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43";
         UUID distributionId = UUID.randomUUID();
         mockAssetDistributionDao.persistDistributionId(genesisTransaction, distributionId);
+    }
+
+    @Test
+    public void persistDistributionIdThrowsCantPersistsTransactionUUIDException() throws Exception {
+        String genesisTransaction = "d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43";
+        UUID distributionId = UUID.randomUUID();
+
+        when(pluginDatabaseSystem.openDatabase(pluginId, AssetDistributionDatabaseConstants.ASSET_DISTRIBUTION_DATABASE)).thenThrow(new CantOpenDatabaseException("error"));
+        catchException(mockAssetDistributionDao).persistDistributionId(genesisTransaction, distributionId);
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantPersistsTransactionUUIDException.class);
+    }
+
+    @Test
+    public void persistDistributionIdThrowsUnexpectedResultReturnedFromDatabaseException() throws Exception {
+        String genesisTransaction = "d21633ba23f70118185227be58a63527675641ad37967e2aa461559f577aec43";
+        UUID distributionId = UUID.randomUUID();
+
+        when(databaseTable.getRecords()).thenReturn(recordsForException);
+        catchException(mockAssetDistributionDao).persistDistributionId(genesisTransaction, distributionId);
+        Exception thrown = caughtException();
+        assertThat(thrown)
+                .isNotNull()
+                .isInstanceOf(CantPersistsTransactionUUIDException.class);
+        assertThat(thrown.getCause()).isNotNull().isInstanceOf(UnexpectedResultReturnedFromDatabaseException.class);
     }
 }
