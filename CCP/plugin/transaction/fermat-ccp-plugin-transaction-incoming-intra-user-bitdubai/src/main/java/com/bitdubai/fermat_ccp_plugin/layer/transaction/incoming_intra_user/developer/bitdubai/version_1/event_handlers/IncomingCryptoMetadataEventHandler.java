c0af22a375a2667514db3010014d3d2c067c0e73
natalia <natalia_veronica_c@hotmail.com>
2015-11-10 18:26:59 -0300

    Incoming and outgoing intra user. Add listener for crypto transmission events. #1889

--- CCP/plugin/transaction/fermat-ccp-plugin-transaction-incoming-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/transaction/incoming_intra_user/developer/bitdubai/version_1/event_handlers/IncomingCryptoMetadataEventHandler.java
+++ CCP/plugin/transaction/fermat-ccp-plugin-transaction-incoming-intra-user-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/transaction/incoming_intra_user/developer/bitdubai/version_1/event_handlers/IncomingCryptoMetadataEventHandler.java
@@ -1,26 +1,74 @@
 package com.bitdubai.fermat_ccp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.event_handlers;
 
 import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventMonitor;
 import com.bitdubai.fermat_api.layer.dmp_transaction.TransactionServiceNotStartedException;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEvent;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventHandler;
+import com.bitdubai.fermat_ccp_api.layer.network_service.intra_actor.interfaces.IntraUserManager;
+import com.bitdubai.fermat_ccp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.IncomingIntraUserTransactionPluginRoot;
+import com.bitdubai.fermat_ccp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.exceptions.CantProcessMetaDataNotificationsExceptions;
+import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
+
 
 /**
  * Created by loui on 23/02/15.
  */
 public class IncomingCryptoMetadataEventHandler implements FermatEventHandler {
-    com.bitdubai.fermat_ccp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.structure.IncomingIntraUserEventRecorderService incomingIntraUserEventRecorderService;
+    /**
+     * listener ACTOR_NETWORK_SERVICE_NEW_NOTIFICATIONS event
+     * Get Pendings Notifications from Network Services
+     */
+    private IncomingIntraUserTransactionPluginRoot incomingIntraUserPluginRoot = null;
+    EventManager eventManager;
+    FermatEventMonitor fermatEventMonitor;
 
-    public IncomingCryptoMetadataEventHandler(com.bitdubai.fermat_ccp_plugin.layer.transaction.incoming_intra_user.developer.bitdubai.version_1.structure.IncomingIntraUserEventRecorderService incomingIntraUserEventRecorderService){
-        this.incomingIntraUserEventRecorderService = incomingIntraUserEventRecorderService;
+    IntraUserManager intraUserNetworkServiceManager;
+
+    public void setIntraWalletUserManager(IncomingIntraUserTransactionPluginRoot incomingIntraUserPluginRoot){
+        this.incomingIntraUserPluginRoot = incomingIntraUserPluginRoot;
+
     }
-    
-    @Override
-    public void handleEvent(FermatEvent fermatEvent) throws FermatException {
-        if (this.incomingIntraUserEventRecorderService.getServiceStatus().equals(ServiceStatus.STARTED))
-            this.incomingIntraUserEventRecorderService.saveEvent(fermatEvent);
-        else
-            throw new TransactionServiceNotStartedException();
+
+    public void setEventManager(EventManager eventManager){
+        this.eventManager = eventManager;
+
     }
+
+    public void setEventManager(FermatEventMonitor fermatEventMonitor){
+        this.fermatEventMonitor = fermatEventMonitor;
+
+    }
+
+
+
+        @Override
+        public void handleEvent(FermatEvent fermatEvent) throws FermatException {
+
+            System.out.println("LLEGADA DE METADATA PENDIENTES DE INCOMING!!!");
+            if (((Service) this.incomingIntraUserPluginRoot).getStatus() == ServiceStatus.STARTED){
+
+                try {
+
+
+                    incomingIntraUserPluginRoot.processNotifications();
+                }
+                catch(CantProcessMetaDataNotificationsExceptions e)
+                {
+                    this.fermatEventMonitor.handleEventException(e, fermatEvent);
+                }
+
+                catch(Exception e)
+                {
+                    this.fermatEventMonitor.handleEventException(e, fermatEvent);
+                }
+
+            }
+            else {
+                throw new TransactionServiceNotStartedException();
+            }
+
+        }
 }
