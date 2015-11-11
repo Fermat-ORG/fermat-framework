34649591a34ddd9fed6765021a16f0be57918493
Nerio Indriago <nerioindriago@gmail.com>
2015-11-11 11:56:36 -0430

    minor Update #1485

--- DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/redeem/point/developer/bitdubai/DeveloperBitDubai.java
+++ DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/redeem/point/developer/bitdubai/DeveloperBitDubai.java
@@ -11,7 +11,7 @@ import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
 import com.bitdubai.fermat_dap_plugin.layer.actor.redeem.point.developer.bitdubai.version_1.RedeemPointActorPluginRoot;
 
 /**
- * Created by PatricioGesualdi on 10/11/2015.
+ * Created by Nerio on 10/11/2015.
  */
 public class DeveloperBitDubai extends AbstractPluginDeveloper implements PluginLicensor {
 
@@ -26,7 +26,9 @@ public class DeveloperBitDubai extends AbstractPluginDeveloper implements Plugin
             registerVersion(new RedeemPointActorPluginRoot());
         }
         catch (CantRegisterVersionException e) {
-            throw new CantStartPluginDeveloperException(e, "Plugin Developer BitDubai", "Can't register Plugins version DAP Asset Issuer");
+            throw new CantStartPluginDeveloperException(e,
+                    "Plugin Developer BitDubai",
+                    "Can't register Plugins version DAP Redeem Point");
         }
     }
 
