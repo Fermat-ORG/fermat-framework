a8483cbe281c0b328b39c627e5014af89e573dfc
vlzangel <vlzangel@gmail.com>
2015-11-11 10:43:20 -0430

    CryptoBrokerActor
    CryptoBrokerActor

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActor.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActor.java
@@ -1,12 +1,16 @@
 package com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.interfaces;
 
 import com.bitdubai.fermat_cbp_api.all_definition.actor.Actor;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ContractStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
 import com.bitdubai.fermat_cbp_api.all_definition.negotiation.Clause;
 import com.bitdubai.fermat_cbp_api.all_definition.negotiation.CustomerBrokerNegotiation;
 import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantCreateSaleNegotiationException;
 import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantGetSaleNegotiationException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_sale.exceptions.CantCreateContractSaleException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_sale.exceptions.CantGetListContractSaleException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_contract.customer_broker_sale.interfaces.CustomerBrokerSaleContract;
 
 import java.util.Collection;
 import java.util.UUID;
@@ -16,10 +20,22 @@ import java.util.UUID;
  */
 public interface CryptoBrokerActor extends Actor {
 
-    Collection<ActorIdentity> getConnectedCustomers();
+    Collection<ActorIdentity>               createCustomerConnection(ActorIdentity cryptoCustomer);
+    Collection<ActorIdentity>               getConnectedCustomers();
 
-    CustomerBrokerNegotiation createSale(ActorIdentity cryptoCustomer, Collection<Clause> clauses) throws CantCreateSaleNegotiationException;
-    CustomerBrokerNegotiation getSale(UUID negotiationId) throws CantGetSaleNegotiationException;
-    Collection<CustomerBrokerNegotiation> getSales() throws CantGetSaleNegotiationException;
-    Collection<CustomerBrokerNegotiation> getSales(NegotiationStatus status) throws CantGetSaleNegotiationException;
+    /* Negotiations  */
+
+        CustomerBrokerNegotiation               createNegotiationSale(ActorIdentity cryptoCustomer, Collection<Clause> clauses) throws CantCreateSaleNegotiationException;
+
+        CustomerBrokerNegotiation               getNegotiationSale(UUID negotiationId) throws CantGetSaleNegotiationException;
+        Collection<CustomerBrokerNegotiation>   getNegotiationSales() throws CantGetSaleNegotiationException;
+        Collection<CustomerBrokerNegotiation>   getNegotiationSales(NegotiationStatus status) throws CantGetSaleNegotiationException;
+
+    /* Contracts  */
+
+        CustomerBrokerNegotiation               createContractSale(ActorIdentity cryptoCustomer, Collection<Clause> clauses) throws CantCreateContractSaleException;
+
+        CustomerBrokerSaleContract getContractSale(UUID ContractId) throws CantGetListContractSaleException;
+        Collection<CustomerBrokerSaleContract>   getContractSales() throws CantGetListContractSaleException;
+        Collection<CustomerBrokerSaleContract>   getContractSales(ContractStatus status) throws CantGetListContractSaleException;
 }
