5cf7c037e51e651f4f11b195ec3890f6d388b2a6
vlzangel <vlzangel@gmail.com>
2015-11-11 10:43:38 -0430

    CantCreateContractSaleException
    CantCreateContractSaleException

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_contract/customer_broker_sale/exceptions/CantCreateContractSaleException.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_contract/customer_broker_sale/exceptions/CantCreateContractSaleException.java
@@ -5,7 +5,7 @@ import com.bitdubai.fermat_api.FermatException;
 /**
  * Created by Angel on 2015.09.16..
  */
-public class CantCreateCustomerBrokerSaleException extends FermatException {
+public class CantCreateContractSaleException extends FermatException {
 
     public static final String DEFAULT_MESSAGE = "CAN'T CREATE THE CONTRACT CUSTOMER BROKER SALE";
 
@@ -17,7 +17,7 @@ public class CantCreateCustomerBrokerSaleException extends FermatException {
      * @param context        a String that provides the values of the variables that could have affected the exception
      * @param possibleReason an explicative reason of why we believe this exception was most likely thrown
      */
-    public CantCreateCustomerBrokerSaleException(String message, Exception cause, String context, String possibleReason) {
+    public CantCreateContractSaleException(String message, Exception cause, String context, String possibleReason) {
         super(message, cause, context, possibleReason);
     }
 }
