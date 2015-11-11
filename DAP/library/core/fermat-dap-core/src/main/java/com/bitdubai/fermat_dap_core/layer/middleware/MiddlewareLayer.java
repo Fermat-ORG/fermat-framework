dd3a8aa01409b8eec5983f5615be285df231202e
lnacosta <laion.cj91@gmail.com>
2015-11-11 18:33:28 -0300

    bug

--- DAP/library/core/fermat-dap-core/src/main/java/com/bitdubai/fermat_dap_core/layer/middleware/MiddlewareLayer.java
+++ DAP/library/core/fermat-dap-core/src/main/java/com/bitdubai/fermat_dap_core/layer/middleware/MiddlewareLayer.java
@@ -4,7 +4,7 @@ import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_class
 import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantRegisterPluginException;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantStartLayerException;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
-import com.bitdubai.fermat_dap_core.layer.network_service.asset_transmission.AssetTransmissionPluginSubsystem;
+import com.bitdubai.fermat_dap_core.layer.middleware.asset_factory.AssetFactoryPluginSubsystem;
 
 /**
  * Created by lnacosta - (laion.cj91@gmail.com) on 11/11/2015.
@@ -22,7 +22,7 @@ public class MiddlewareLayer extends AbstractLayer {
 
         try {
 
-            registerPlugin(new AssetTransmissionPluginSubsystem());
+            registerPlugin(new AssetFactoryPluginSubsystem());
 
         } catch(CantRegisterPluginException e) {
 
