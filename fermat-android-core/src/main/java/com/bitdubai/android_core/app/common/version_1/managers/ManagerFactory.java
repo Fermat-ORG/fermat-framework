0c94e02a4e57ec706493956c60734b641df6f48d
Francisco Vasquez <fvasquezjatar@gmail.com>
2015-11-11 22:10:17 -0430

    ManagerFactory DAP User Identity PluginRoot Fixed

--- fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/managers/ManagerFactory.java
+++ fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/managers/ManagerFactory.java
@@ -56,9 +56,6 @@ public class ManagerFactory {
             case DAP_ASSETS_COMMUNITY_ISSUER:
                 moduleManager = (ModuleManager) corePlatformContext.getPlugin(Plugins.BITDUBAI_DAP_ASSET_ISSUER_COMMUNITY_SUB_APP_MODULE);
                 break;
-            case DAP_ASSETS_IDENTITY_USER:
-                moduleManager = (ModuleManager) corePlatformContext.getPlugin(Plugins.BITDUBAI_DAP_ASSET_USER_COMMUNITY_SUB_APP_MODULE);
-                break;
             case DAP_ASSETS_COMMUNITY_REDEEM_POINT:
                 moduleManager = (ModuleManager) corePlatformContext.getPlugin(Plugins.BITDUBAI_DAP_REDEEM_POINT_COMMUNITY_SUB_APP_MODULE);
                 break;
@@ -70,6 +67,9 @@ public class ManagerFactory {
                 break;
             case DAP_ASSETS_IDENTITY_ISSUER:
                 moduleManager = (ModuleManager) corePlatformContext.getPlugin(Plugins.BITDUBAI_DAP_ASSET_ISSUER_IDENTITY);
+                break;
+            case DAP_ASSETS_IDENTITY_USER:
+                moduleManager = (ModuleManager) corePlatformContext.getPlugin(Plugins.BITDUBAI_DAP_ASSET_USER_IDENTITY);
                 break;
 //            case BITDUBAI_CRYPTO_WALLET_WALLET_MODULE:
 //                moduleManager = (ModuleManager)corePlatformContext.getPlugin(Plugins.BITDUBAI_CRYPTO_WALLET_WALLET_MODULE);
