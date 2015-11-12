534d4dcced2b5c209246d916a6f4e98d7b4c1556
mati <mati_fur@hotmail.com>
2015-11-12 02:44:50 -0300

    various changes

--- fermat-android-api/src/main/java/com/bitdubai/fermat_android_api/engine/PaintActivtyFeactures.java
+++ fermat-android-api/src/main/java/com/bitdubai/fermat_android_api/engine/PaintActivtyFeactures.java
@@ -1,9 +1,14 @@
 package com.bitdubai.fermat_android_api.engine;
 
 import android.app.ActionBar;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.RelativeLayout;
+
+import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
 
 import java.util.List;
 
@@ -17,7 +22,11 @@ public interface PaintActivtyFeactures {
 
     public RelativeLayout getActivityHeader();
 
-    public void changeNavigationDrawerAdapter(ListAdapter listAdapter);
+    public void changeNavigationDrawerAdapter(FermatAdapter adapter);
+
+    public void addNavigationViewHeader(View view);
+
+    public RelativeLayout getToolbarHeader();
 
     public void invalidate();
 }
