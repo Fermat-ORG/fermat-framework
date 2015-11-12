8ed27be27901ff0ab7939683dee805c27e4edd20
hendry19901990 <elnegroevaristo@gmail.com>
2015-11-12 12:00:23 -0430

    Upload  basic structure of the Plugin P2P Communications Network Client #2647

---
+++ P2P/plugin/communication/fermat-p2p-plugin-ws-communications-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/network/client/developer/bitdubai/version_2/WsCommunicationsNetworkClientPluginRoot.java
@@ -0,0 +1,36 @@
+/*
+* @#WsCommunicationsNetworkClientPluginRoot.java - 2015
+* Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+* BITDUBAI/CONFIDENTIAL
+*/
+package com.bitdubai.fermat_p2p_plugin.layer.ws.communications.network.client.developer.bitdubai.version_2;
+
+import com.bitdubai.fermat_api.Plugin;
+
+import java.util.UUID;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.ws.communications.network.client.developer.bitdubai.version_2.WsCommunicationsNetworkClientPluginRoot</code>
+ * <p/>
+ * Created by Hendry Rodriguez - (elnegroevaristo@gmail.com) on 12/11/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class WsCommunicationsNetworkClientPluginRoot implements Plugin {
+
+
+    /**
+     * DealsWithPluginIdentity Interface member variables.
+     */
+    private UUID pluginId;
+
+    @Override
+    public void setId(UUID pluginId) {
+        this.pluginId=pluginId;
+    }
+
+
+
+}
