7a31986bc40091a3c7fcce6356dd2d6aa80c134d
hendry19901990 <elnegroevaristo@gmail.com>
2015-11-13 16:43:21 -0430

    Upload created P2P - Communication - Network Client - Implementation - Database - Database Factory Class #30, P2P - Communication - Network Client - Implementation - Database - Database Constants Class #31, P2P - Communication - Network Client - Implementation - Database - Developer Database Factory Class #32, P2P - Communication - Network Client - Implementation - Database - Database Factory Exceptions Class #33, P2P - Communication - Network Client - Implementation - Database - DAO's Class #34

---
+++ P2P/plugin/communication/fermat-p2p-plugin-ws-communications-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/network/client/developer/bitdubai/version_1/structure/exceptions/CantReadRecordDataBaseException.java
@@ -0,0 +1,18 @@
+/*
+* @#CantReadRecordDataBaseException.java - 2015
+* Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+* BITDUBAI/CONFIDENTIAL
+*/
+package com.bitdubai.fermat_p2p_plugin.layer.ws.communications.network.client.developer.bitdubai.version_1.structure.exceptions;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.ws.communications.network.client.developer.bitdubai.version_1.structure.exceptions.CantReadRecordDataBaseException</code>
+ * <p/>
+ * Created by Hendry Rodriguez - (elnegroevaristo@gmail.com) on 13/11/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class CantReadRecordDataBaseException {
+}
