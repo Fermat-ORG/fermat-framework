ec63eb1748d50a6cbe545ec477a1240e4016d5e9
yalayn <y.alayn@gmail.com>
2015-11-15 18:08:17 -0430

    CBP-Actor-Crypto Broker - Implementation Database #2697

---
+++ CBP/plugin/actor/fermat-cbp-plugin-actor-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/actor/crypto_customer/developer/bitdubai/version_1/database/CryptoCustomerActorDatabaseConstants.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.actor.crypto_customer.developer.bitdubai.version_1.database;
+
+/**
+ * Created by Yordin Alayn on 15/11/15.
+ */
+public class CryptoCustomerActorDatabaseConstants {
+}
---
+++ CBP/plugin/actor/fermat-cbp-plugin-actor-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/actor/crypto_customer/developer/bitdubai/version_1/database/CryptoCustomerActorDatabaseFactory.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.actor.crypto_customer.developer.bitdubai.version_1.database;
+
+/**
+ * Created by yordin on 15/11/15.
+ */
+public class CryptoCustomerActorDatabaseFactory {
+}
