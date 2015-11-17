e6d02872a47766fbccf65f5d8ceb210ee540249e
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-11-17 11:35:43 -0430

    CBP - Business Transaction - Bank Money Restock - QA - Refactor name Layer in stock-transactions  #2720 #2723

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-bank-money-restock-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/cash_money_restock/developer/bitdubai/version_1/database/BusinessTransactionBankMoneyRestockDatabaseDao.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.cash_money_restock.developer.bitdubai.version_1.database;
-
-/**
- * Created by franklin on 17/11/15.
- */
-public class BusinessTransactionBankMoneyRestockDatabaseDao {
-}
--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-bank-money-restock-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/cash_money_restock/developer/bitdubai/version_1/exceptions/DatabaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.cash_money_restock.developer.bitdubai.version_1.exceptions;
-
-/**
- * Created by franklin on 17/11/15.
- */
-public class DatabaseOperationException {
-}
