e20d051823b14207e6a6f118bdb0e5da52ddc911
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-11-17 12:24:08 -0430

    CBP - Business Transaction - Bank Money Restock - QA - Refactor name Layer in stock-transactions  #2720 #2723

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_stock_transactions/bank_money_restock/interfaces/BankMoneyRestockManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_stock_transactions/bank_money_restock/interfaces/BankMoneyRestockManager.java
@@ -1,7 +1,8 @@
 package com.bitdubai.fermat_cbp_api.layer.cbp_stock_transactions.bank_money_restock.interfaces;
 
 import com.bitdubai.fermat_cbp_api.all_definition.enums.FiatCurrency;
-import com.bitdubai.fermat_cbp_api.layer.cbp_business_transaction.bank_money_restock.exceptions.CantCreateBankMoneyRestockException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_stock_transactions.bank_money_restock.exceptions.CantCreateBankMoneyRestockException;
+
 
 /**
  * Created by franklin on 16/11/15.
