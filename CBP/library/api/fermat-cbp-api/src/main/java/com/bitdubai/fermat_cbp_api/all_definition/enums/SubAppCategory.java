d4bd8a3b9f2085c271af4784523178db5fcbae0d
lnacosta <laion.cj91@gmail.com>
2015-11-17 16:00:14 -0300

    enums back

---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/enums/SubAppCategory.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_api.all_definition.enums;
+
+/**
+ * Created by natalia on 16/09/15.
+ */
+public enum SubAppCategory {
+}
---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/enums/WalletType.java
@@ -0,0 +1,32 @@
+package com.bitdubai.fermat_cbp_api.all_definition.enums;
+
+import com.bitdubai.fermat_api.layer.all_definition.enums.interfaces.FermatEnum;
+import com.bitdubai.fermat_api.layer.all_definition.exceptions.InvalidParameterException;
+
+/**
+ * Created by natalia on 16/09/15.
+ */
+public enum WalletType  implements FermatEnum {
+    NICHE("NICHE"),
+    REFERENCE("REFER"), ;
+
+    private String code;
+
+    WalletType(String code){
+        this.code = code;
+    }
+
+    @Override
+    public String getCode(){
+        return this.code;
+    }
+
+    public static WalletType getByCode(String code) throws InvalidParameterException {
+        switch (code){
+            case "NICHE": return WalletType.NICHE;
+            case "REFER": return WalletType.REFERENCE;
+            //Modified by Manuel Perez on 03/08/2015
+            default: throw new InvalidParameterException(InvalidParameterException.DEFAULT_MESSAGE, null, "Code Received: " + code, "This Code Is Not Valid for the WalletType enum");
+        }
+    }
+}
