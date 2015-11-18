550821c08987653a3edd2a757463d66e976b6fdc
Nelson Ramirez <nelsonalfo@gmail.com>
2015-11-18 14:22:13 -0430

    implementado fragmento Contract Hisroty y se creo un layout para el

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/contracts_history/ContractsHistoryActivityFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/contracts_history/ContractsHistoryActivityFragment.java
@@ -5,29 +5,41 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_android_api.ui.enums.FermatRefreshTypes;
 import com.bitdubai.fermat_android_api.ui.fragments.FermatWalletListFragment;
 import com.bitdubai.fermat_android_api.ui.interfaces.FermatListItemListeners;
+import com.bitdubai.fermat_android_api.ui.util.FermatDividerItemDecoration;
 import com.bitdubai.fermat_api.layer.all_definition.enums.UISource;
+import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Activities;
 import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Wallets;
 import com.bitdubai.fermat_cbp_api.layer.cbp_sub_app_module.crypto_broker_identity.interfaces.CryptoBrokerIdentityInformation;
 import com.bitdubai.fermat_cbp_api.layer.cbp_wallet_module.crypto_broker.interfaces.ContractBasicInformation;
+import com.bitdubai.fermat_cbp_api.layer.cbp_wallet_module.crypto_broker.interfaces.CryptoBrokerWallet;
 import com.bitdubai.fermat_cbp_api.layer.cbp_wallet_module.crypto_broker.interfaces.CryptoBrokerWalletModuleManager;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.exceptions.CantGetActiveLoginIdentityException;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedUIExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedWalletExceptionSeverity;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.common.adapters.ContractHistoryAdapter;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.common.navigationDrawer.NavigationViewAdapter;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.session.CryptoBrokerWalletSession;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.util.CommonLogger;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.util.FragmentsCommons;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -43,19 +55,12 @@ public class ContractsHistoryActivityFragment extends FermatWalletListFragment<C
     private CryptoBrokerWalletModuleManager moduleManager;
     private ErrorManager errorManager;
 
+    // Data
+    private ArrayList<ContractBasicInformation> contractHistoryList;
+
 
     public static ContractsHistoryActivityFragment newInstance() {
         return new ContractsHistoryActivityFragment();
-    }
-
-    @Override
-    public FermatAdapter getAdapter() {
-        return null;
-    }
-
-    @Override
-    public RecyclerView.LayoutManager getLayoutManager() {
-        return null;
     }
 
     @Override
@@ -65,21 +70,87 @@ public class ContractsHistoryActivityFragment extends FermatWalletListFragment<C
         try {
             moduleManager = ((CryptoBrokerWalletSession) walletSession).getModuleManager();
             errorManager = walletSession.getErrorManager();
+            contractHistoryList = (ArrayList) getMoreDataAsync(FermatRefreshTypes.NEW, 0);
         } catch (Exception ex) {
             CommonLogger.exception(TAG, ex.getMessage(), ex);
             if (errorManager != null)
                 errorManager.reportUnexpectedWalletException(Wallets.CBP_CRYPTO_BROKER_WALLET,
                         UnexpectedWalletExceptionSeverity.DISABLES_THIS_FRAGMENT, ex);
         }
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected boolean hasMenu() {
+        return true;
+    }
+
+    @Override
+    public FermatAdapter getAdapter() {
+        if (adapter == null) {
+            adapter = new ContractHistoryAdapter(getActivity(), contractHistoryList);
+            adapter.setFermatListEventListener(this);
+        }
+        return adapter;
+    }
+
+    @Override
+    public RecyclerView.LayoutManager getLayoutManager() {
+        if (layoutManager == null) {
+            layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        }
+        return layoutManager;
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_contracts_history_activity;
+    }
+
+    @Override
+    protected int getSwipeRefreshLayoutId() {
+        return R.id.swipe_refresh;
+    }
+
+    @Override
+    protected int getRecyclerLayoutId() {
+        return R.id.contracts_history_recycler_view;
+    }
+
+    @Override
+    protected boolean recyclerHasFixedSize() {
+        return true;
+    }
+
+    @Override
+    protected void initViews(View layout) {
+        super.initViews(layout);
 
         configureActionBar();
+        configureNavigationDrawer();
 
+        RecyclerView.ItemDecoration itemDecoration = new FermatDividerItemDecoration(getActivity(), R.drawable.cbw_divider_shape);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
+    private void configureActionBar() {
+        Toolbar toolbar = getToolbar();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            toolbar.setBackground(getResources().getDrawable(R.drawable.cbw_action_bar_gradient_colors, null));
+        else
+            toolbar.setBackground(getResources().getDrawable(R.drawable.cbw_action_bar_gradient_colors));
+    }
+
+    private void configureNavigationDrawer() {
         Activity activity = getActivity();
         LayoutInflater layoutInflater = activity.getLayoutInflater();
         try {
@@ -89,60 +160,64 @@ public class ContractsHistoryActivityFragment extends FermatWalletListFragment<C
         } catch (CantGetActiveLoginIdentityException e) {
             errorManager.reportUnexpectedUIException(UISource.VIEW, UnexpectedUIExceptionSeverity.UNSTABLE, e);
         }
-
-        return rootView;
     }
 
     @Override
     public void onItemClickListener(CryptoBrokerIdentityInformation data, int position) {
-
+        walletSession.setData("contract_data", data);
+        changeActivity(Activities.CBP_CRYPTO_BROKER_WALLET_CLOSE_CONTRACT_DETAILS);
     }
 
     @Override
     public void onLongItemClickListener(CryptoBrokerIdentityInformation data, int position) {
+    }
 
+    @Override
+    public List<ContractBasicInformation> getMoreDataAsync(FermatRefreshTypes refreshType, int pos) {
+        List<ContractBasicInformation> data = new ArrayList<>();
+
+        if (moduleManager != null) {
+            try {
+                CryptoBrokerWallet wallet = moduleManager.getCryptoBrokerWallet(WALLET_PUBLIC_KEY);
+                data.addAll(wallet.getContractsHistory(null, 0, 20));
+
+            } catch (Exception ex) {
+                CommonLogger.exception(TAG, ex.getMessage(), ex);
+                if (errorManager != null)
+                    errorManager.reportUnexpectedWalletException(
+                            Wallets.CBP_CRYPTO_BROKER_WALLET,
+                            UnexpectedWalletExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_FRAGMENT,
+                            ex);
+            }
+        } else {
+            Toast.makeText(getActivity(),
+                    "Sorry, an error happened in ContractHistoryActivityFragment (Module == null)",
+                    Toast.LENGTH_SHORT).
+                    show();
+        }
+
+        return data;
     }
 
     @Override
     public void onPostExecute(Object... result) {
-
+        isRefreshing = false;
+        if (isAttached) {
+            swipeRefreshLayout.setRefreshing(false);
+            if (result != null && result.length > 0) {
+                contractHistoryList = (ArrayList) result[0];
+                if (adapter != null)
+                    adapter.changeDataSet(contractHistoryList);
+            }
+        }
     }
 
     @Override
     public void onErrorOccurred(Exception ex) {
-
-    }
-
-    @Override
-    protected boolean hasMenu() {
-        return false;
-    }
-
-    @Override
-    protected int getLayoutResource() {
-        return 0;
-    }
-
-    @Override
-    protected int getSwipeRefreshLayoutId() {
-        return 0;
-    }
-
-    @Override
-    protected int getRecyclerLayoutId() {
-        return 0;
-    }
-
-    @Override
-    protected boolean recyclerHasFixedSize() {
-        return false;
-    }
-
-    private void configureActionBar() {
-        Toolbar toolbar = getToolbar();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            toolbar.setBackground(getResources().getDrawable(R.drawable.cbw_action_bar_gradient_colors, null));
-        else
-            toolbar.setBackground(getResources().getDrawable(R.drawable.cbw_action_bar_gradient_colors));
+        isRefreshing = false;
+        if (isAttached) {
+            swipeRefreshLayout.setRefreshing(false);
+            CommonLogger.exception(TAG, ex.getMessage(), ex);
+        }
     }
 }
