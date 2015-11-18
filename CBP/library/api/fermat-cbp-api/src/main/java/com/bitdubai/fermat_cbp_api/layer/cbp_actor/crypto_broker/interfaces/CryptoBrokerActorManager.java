740f32f400f7d2e9a8a23bf08943ecbe46e4f9c7
vlzangel <vlzangel@gmail.com>
2015-11-18 13:26:27 -0430

    Refactory Interface CryptoBrokerActorManager

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActorManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActorManager.java
@@ -1,10 +1,20 @@
 package com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.interfaces;
 
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantAcceptConnectionRequestException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantCancelConnectionRequestException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantDenyConnectionRequestException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantDisconnectException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantListPendingConnectionNewsException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantRequestConnectionException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.ConnectionRequestNotFoundException;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.interfaces.CryptoBrokerConnectionNew;
+import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.interfaces.CryptoBrokerData;
 import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantCreateCryptoBrokerActorException;
 import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantGetCryptoBrokerActorException;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -33,7 +43,12 @@ public interface CryptoBrokerActorManager {
 
     /* Communication with the network service */
 
-        void getPendingConnectionNews();
+        void requestConnection(final CryptoBrokerData cryptoBrokerData) throws CantRequestConnectionException;
+        void disconnect(final String publicKey) throws CantDisconnectException;
+        void denyConnection(final UUID requestId) throws CantDenyConnectionRequestException, ConnectionRequestNotFoundException;
+        void cancelConnection(final UUID requestId) throws CantCancelConnectionRequestException, ConnectionRequestNotFoundException;
+        void acceptConnection(final UUID requestId) throws CantAcceptConnectionRequestException, ConnectionRequestNotFoundException;
+        List<CryptoBrokerConnectionNew> getPendingConnectionNews() throws CantListPendingConnectionNewsException;
 
         void requestReceivedProcessed(ActorIdentity cryptoCustomer);
         void requestSentProcessed(ActorIdentity cryptoCustomer);
