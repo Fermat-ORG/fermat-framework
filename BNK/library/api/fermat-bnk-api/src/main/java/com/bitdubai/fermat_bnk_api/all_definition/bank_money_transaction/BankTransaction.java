fec6959300eb66c1a9ea720bd3d4f723c1bbc361
memo <guillermo20@gmail.com>
2015-11-18 12:34:30 -0430

    minor changes to interfaces hold and unhold

--- BNK/library/api/fermat-bnk-api/src/main/java/com/bitdubai/fermat_bnk_api/all_definition/bank_money_transaction/BankTransaction.java
+++ BNK/library/api/fermat-bnk-api/src/main/java/com/bitdubai/fermat_bnk_api/all_definition/bank_money_transaction/BankTransaction.java
@@ -1,13 +1,15 @@
 package com.bitdubai.fermat_bnk_api.all_definition.bank_money_transaction;
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
+import com.bitdubai.fermat_bnk_api.all_definition.enums.BankOperationType;
+import com.bitdubai.fermat_bnk_api.all_definition.enums.TransactionType;
 
 import java.util.UUID;
 
 /**
  * Created by memo on 18/11/15.
  */
-public interface BankTransactionParameters {
+public interface BankTransaction {
 
     UUID getTransactionId();
 
@@ -29,4 +31,11 @@ public interface BankTransactionParameters {
 
 
     String getMemo();
+
+    BankOperationType getBankOperationType();
+
+    TransactionType getTransactionType();
+
+    long getTimestamp();
+
 }
