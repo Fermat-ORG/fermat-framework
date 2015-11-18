1abbcc9ce448283ecc7c9ebc725cb2606314be60
vlzangel <vlzangel@gmail.com>
2015-11-18 08:59:06 -0430

    Interface CryptoBrokerActorWalletIdentity
    Interface CryptoBrokerActorWalletIdentity

---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActorWalletIdentity.java
@@ -0,0 +1,15 @@
+package com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.interfaces;
+
+import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
+
+import java.util.UUID;
+
+/**
+ * Created by angel on 17/11/15.
+ */
+public interface CryptoBrokerActorWalletIdentity {
+
+    ActorIdentity getCryptoBroker();
+    UUID getWallet();
+
+}
