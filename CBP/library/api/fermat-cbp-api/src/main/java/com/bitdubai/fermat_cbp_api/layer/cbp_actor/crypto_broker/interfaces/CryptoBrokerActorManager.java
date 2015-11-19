2b93aadf69d155c827ea3ae03426906d6f9a4287
vlzangel <vlzangel@gmail.com>
2015-11-19 16:03:38 -0430

    CryptoBrokerActorManager
    CryptoBrokerActorManager

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActorManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/cbp_actor/crypto_broker/interfaces/CryptoBrokerActorManager.java
@@ -1,20 +1,10 @@
 package com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.interfaces;
 
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantAcceptConnectionRequestException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantCancelConnectionRequestException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantDenyConnectionRequestException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantDisconnectException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantListPendingConnectionNewsException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.CantRequestConnectionException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.exceptions.ConnectionRequestNotFoundException;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.interfaces.CryptoBrokerConnectionNew;
-import com.bitdubai.fermat_cbp_api.layer.actor_network_service.crypto_broker.interfaces.CryptoBrokerData;
-import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantCreateCryptoBrokerActorException;
-import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantGetCryptoBrokerActorException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantCreateNewBrokerIdentityWalletRelationshipException;
+import com.bitdubai.fermat_cbp_api.layer.cbp_actor.crypto_broker.exceptions.CantGetListBrokerIdentityWalletRelationshipException;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -23,34 +13,10 @@ import java.util.UUID;
 
 public interface CryptoBrokerActorManager {
 
-    /* Identity management a broker */
+    BrokerIdentityWalletRelationship createNewBrokerIdentityWalletRelationship(ActorIdentity identity, UUID wallet) throws CantCreateNewBrokerIdentityWalletRelationshipException;
 
-        CryptoBrokerActor createNewCryptoBroker(ActorIdentity identity) throws CantCreateCryptoBrokerActorException;
-        CryptoBrokerActor getCryptoBroker(ActorIdentity identity) throws CantGetCryptoBrokerActorException;
-
-    /* Management of customers of broker */
-
-        Collection<ActorIdentity> createBrokerConnection(ActorIdentity cryptoCustomer);
-        Collection<ActorIdentity> getAllConnectedBrokers();
-        ActorIdentity getConnectedCustomerByCustomer(ActorIdentity cryptoCustomer);
-
-    /* Management of Associations of the Wallets of broker */
-
-        void createAssociationCryptoBrokerIdentityWallet(CryptoBrokerActorWalletIdentity association);
-        Collection<CryptoBrokerActorWalletIdentity> getAllAssociationCryptoBrokerIdentityWallet();
-        CryptoBrokerActorWalletIdentity getAssociationCryptoBrokerIdentityWalletByIdentity(ActorIdentity cryptoBroker);
-        CryptoBrokerActorWalletIdentity getAssociationCryptoBrokerIdentityWalletByWallet(UUID wallet);
-
-    /* Communication with the network service */
-
-        void requestConnection(final CryptoBrokerData cryptoBrokerData) throws CantRequestConnectionException;
-        void disconnect(final String publicKey) throws CantDisconnectException;
-        void denyConnection(final UUID requestId) throws CantDenyConnectionRequestException, ConnectionRequestNotFoundException;
-        void cancelConnection(final UUID requestId) throws CantCancelConnectionRequestException, ConnectionRequestNotFoundException;
-        void acceptConnection(final UUID requestId) throws CantAcceptConnectionRequestException, ConnectionRequestNotFoundException;
-        List<CryptoBrokerConnectionNew> getPendingConnectionNews() throws CantListPendingConnectionNewsException;
-
-        void requestReceivedProcessed(ActorIdentity cryptoCustomer);
-        void requestSentProcessed(ActorIdentity cryptoCustomer);
+    Collection<BrokerIdentityWalletRelationship> getAllBrokerIdentityWalletRelationship(ActorIdentity identity) throws CantGetListBrokerIdentityWalletRelationshipException;
+    BrokerIdentityWalletRelationship getBrokerIdentityWalletRelationshipByIdentity(ActorIdentity identity) throws CantGetListBrokerIdentityWalletRelationshipException;
+    BrokerIdentityWalletRelationship getBrokerIdentityWalletRelationshipByWallet(UUID wallet) throws CantGetListBrokerIdentityWalletRelationshipException;
 
 }
