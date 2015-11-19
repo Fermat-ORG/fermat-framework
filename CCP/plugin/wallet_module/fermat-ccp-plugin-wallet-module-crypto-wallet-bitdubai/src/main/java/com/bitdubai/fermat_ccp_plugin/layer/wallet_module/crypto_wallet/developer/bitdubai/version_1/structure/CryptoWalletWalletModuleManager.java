1ed0263684cf1ab1fd8017de22ecc0e83d905095
natalia <natalia_veronica_c@hotmail.com>
2015-11-19 18:40:57 -0300

    Incoming Money Notification - Error reporting BTC arrival #2961

--- CCP/plugin/wallet_module/fermat-ccp-plugin-wallet-module-crypto-wallet-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/version_1/structure/CryptoWalletWalletModuleManager.java
+++ CCP/plugin/wallet_module/fermat-ccp-plugin-wallet-module-crypto-wallet-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/version_1/structure/CryptoWalletWalletModuleManager.java
@@ -1600,7 +1600,7 @@ public class CryptoWalletWalletModuleManager implements CryptoWallet {
                 case DEBIT:
                     try {
                         involvedActor = getActorByActorPublicKeyAndType(bitcoinWalletTransaction.getActorToPublicKey(), bitcoinWalletTransaction.getActorToType(),intraUserLoggedInPublicKey);
-                        WalletContactRecord walletContactRecord = walletContactsRegistry.getWalletContactByActorAndWalletPublicKey(bitcoinWalletTransaction.getActorFromPublicKey(), walletPublicKey);
+                        WalletContactRecord walletContactRecord = walletContactsRegistry.getWalletContactByActorAndWalletPublicKey(bitcoinWalletTransaction.getActorToPublicKey(), walletPublicKey);
                         if (walletContactRecord != null)
                             contactId = walletContactRecord.getContactId();
 
