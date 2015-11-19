4a0b329e55f6098a10844542ee7b2bc373d750df
memo <guillermo20@gmail.com>
2015-11-19 14:23:43 -0430

    minor changes to de database columns

---
+++ BNK/plugin/bank_money_transaction/fermat-bnk-plugin-bank-money-transaction-deposit-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/bank_money_transaction/deposit/developer/bitdubai/version_1/DepositBankMoneyTransactionPluginRoot.java
@@ -0,0 +1,41 @@
+package com.bitdubai.fermat_bnk_plugin.layer.bank_money_transaction.deposit.developer.bitdubai.version_1;
+
+
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+
+import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
+import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_bnk_api.all_definition.bank_money_transaction.BankTransaction;
+import com.bitdubai.fermat_bnk_api.all_definition.bank_money_transaction.BankTransactionParameters;
+import com.bitdubai.fermat_bnk_api.layer.bnk_bank_money_transaction.deposit.exceptions.CantMakeDepositTransactionException;
+import com.bitdubai.fermat_bnk_api.layer.bnk_bank_money_transaction.deposit.interfaces.DepositManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
+
+/**
+ * Created by memo on 19/11/15.
+ */
+public class DepositBankMoneyTransactionPluginRoot extends AbstractPlugin implements DepositManager{
+
+
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM   , layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER         )
+    private ErrorManager errorManager;
+
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM         , addon = Addons.PLUGIN_DATABASE_SYSTEM)
+    private PluginDatabaseSystem pluginDatabaseSystem;
+
+
+    public DepositBankMoneyTransactionPluginRoot(PluginVersionReference pluginVersionReference) {
+        super(new PluginVersionReference(new Version()));
+    }
+
+    @Override
+    public BankTransaction makeDeposit(BankTransactionParameters parameters) throws CantMakeDepositTransactionException {
+        return null;
+    }
+}
+
---
+++ BNK/plugin/bank_money_transaction/fermat-bnk-plugin-bank-money-transaction-deposit-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/bank_money_transaction/deposit/developer/bitdubai/version_1/structure/DepositBankMoneyTransactionManager.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_bnk_plugin.layer.bank_money_transaction.deposit.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by memo on 19/11/15.
+ */
+public class DepositBankMoneyTransactionManager {
+}
