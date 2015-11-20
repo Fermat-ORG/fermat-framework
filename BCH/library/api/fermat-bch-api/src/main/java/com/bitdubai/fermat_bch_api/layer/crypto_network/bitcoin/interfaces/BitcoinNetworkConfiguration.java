0d9d0dc2f2e98557c0ced376048a19c5d134c3f2
javafrank <contrerasfrank@gmail.com>
2015-11-20 15:27:23 -0430

    IP local configuration for testing

--- BCH/library/api/fermat-bch-api/src/main/java/com/bitdubai/fermat_bch_api/layer/crypto_network/bitcoin/interfaces/BitcoinNetworkConfiguration.java
+++ BCH/library/api/fermat-bch-api/src/main/java/com/bitdubai/fermat_bch_api/layer/crypto_network/bitcoin/interfaces/BitcoinNetworkConfiguration.java
@@ -9,9 +9,9 @@ import org.bitcoinj.params.TestNet3Params;
  */
 public interface BitcoinNetworkConfiguration {
     public static final NetworkParameters DEFAULT_NETWORK_PARAMETERS = RegTestParams.get();
-    public static final String BITCOIN_FULL_NODE_1_IP = "192.168.1.140";
+    public static final String BITCOIN_FULL_NODE_1_IP = "192.168.0.18";
     public static final int  BITCOIN_FULL_NODE_1_PORT = 19020;
-    public static final String BITCOIN_FULL_NODE_2_IP = "192.168.1.140";
+    public static final String BITCOIN_FULL_NODE_2_IP = "192.168.0.18";
     public static final int  BITCOIN_FULL_NODE_2_PORT = 19030;
 
 
