4f92a95a7d54bd5837340cadfcfa64c52bfcf72a
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-11-20 17:00:30 -0300

    fix how I was saving keys usage stats to avoid missing information

--- BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/bitcoin/developer/bitdubai/version_1/structure/BitcoinCryptoNetworkManager.java
+++ BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/bitcoin/developer/bitdubai/version_1/structure/BitcoinCryptoNetworkManager.java
@@ -118,6 +118,11 @@ public class BitcoinCryptoNetworkManager implements TransactionProtocolManager, 
          */
         for (BlockchainNetworkType blockchainNetworkType : blockchainNetworkTypes){
             /**
+             * I will update the detailed stats table with the keys information for each network type and vault
+             */
+            updateDetailedCryptoStats(cryptoVault, blockchainNetworkType, keyList);
+
+            /**
              * load (if any) existing wallet.
              */
             Wallet wallet = getWallet(blockchainNetworkType, keyList);
@@ -167,11 +172,6 @@ public class BitcoinCryptoNetworkManager implements TransactionProtocolManager, 
 
                 bitcoinCryptoNetworkMonitor.start();
             }
-
-            /**
-             * I will update the detailed stats table with the keys information for each network type and vault
-             */
-            updateDetailedCryptoStats(cryptoVault, blockchainNetworkType, keyList);
         }
     }
 
