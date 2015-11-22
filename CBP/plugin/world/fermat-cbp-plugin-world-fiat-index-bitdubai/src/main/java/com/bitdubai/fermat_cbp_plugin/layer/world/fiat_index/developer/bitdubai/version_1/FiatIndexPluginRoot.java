c1d08062bb5e8f3b3c6926b5f56b6b73d74ea389
abicelis <abicelis@gmail.com>
2015-11-21 23:25:28 -0430

    Updates al FiatIndex Plugin - iIteligencia a los IndexProviders para que escojan un correcto indice, eliminen indices en 0, o extremos

--- CBP/plugin/world/fermat-cbp-plugin-world-fiat-index-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/world/fiat_index/developer/bitdubai/version_1/FiatIndexPluginRoot.java
+++ CBP/plugin/world/fermat-cbp-plugin-world-fiat-index-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/world/fiat_index/developer/bitdubai/version_1/FiatIndexPluginRoot.java
@@ -2,8 +2,6 @@ package com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai
 
 import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.FermatException;
-import com.bitdubai.fermat_api.Plugin;
-import com.bitdubai.fermat_api.Service;
 
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
@@ -14,9 +12,6 @@ import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseT
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
 
-import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
-
-import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
@@ -26,28 +21,26 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
-import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
-import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
-import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
+import com.bitdubai.fermat_cbp_api.layer.world.exceptions.CantCreateFiatIndexException;
+import com.bitdubai.fermat_cbp_api.layer.world.exceptions.CantGetFiatIndexException;
 import com.bitdubai.fermat_cbp_api.layer.world.interfaces.FiatIndex;
 import com.bitdubai.fermat_cbp_api.layer.world.interfaces.FiatIndexManager;
 import com.bitdubai.fermat_api.layer.world.exceptions.CantGetIndexException;
-import com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai.version_1.structure.IndexHelper;
-import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.DealsWithErrors;
+import com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai.version_1.database.FiatIndexWorldDao;
+import com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai.version_1.database.FiatIndexWorldDeveloperDatabaseFactory;
+import com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai.version_1.enums.FiatIndexProviders;
+import com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai.version_1.exceptions.CantInitializeFiatIndexWorldDatabaseException;
+import com.bitdubai.fermat_cbp_plugin.layer.world.fiat_index.developer.bitdubai.version_1.interfaces.IndexProvider;
+import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantDeliverDatabaseException;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedPluginExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.regex.Pattern;
 
 /**
  * Created by Alejandro Bicelis on 11/2/2015.
@@ -71,8 +64,9 @@ public class FiatIndexPluginRoot extends AbstractPlugin implements DatabaseManag
 
 
 
-    //TODO: este indexHelper deberia ser un FiatIndexManagerImpl
-    IndexHelper indexHelper = new IndexHelper();
+    FiatIndexWorldDao dao;
+
+
 
     /*
      * PluginRoot Constructor
@@ -89,7 +83,7 @@ public class FiatIndexPluginRoot extends AbstractPlugin implements DatabaseManag
     @Override
     public void start() throws CantStartPluginException {
         try {
-            //fiatIndexManagerImpl = new FiatIndexManagerImpl(pluginDatabaseSystem, pluginId, errorManager);
+            dao = new FiatIndexWorldDao(pluginDatabaseSystem, pluginId, errorManager);
         } catch (Exception e) {
             errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
             throw new CantStartPluginException(CantStartPluginException.DEFAULT_MESSAGE, FermatException.wrapException(e), null, null);
@@ -113,19 +107,37 @@ public class FiatIndexPluginRoot extends AbstractPlugin implements DatabaseManag
 
     @Override
     public FiatIndex getCurrentIndex(FiatCurrency currency) throws CantGetIndexException {
-        return indexHelper.getCurrentIndex(currency);
+        String currencyCode = currency.getCode();
+        IndexProvider ip = FiatIndexProviders.valueOf(currencyCode).getProviderInstance();
+        FiatIndex fiatIndex = ip.getCurrentIndex(currency);
+
+       /* try {
+            dao.saveFiatIndex(fiatIndex);
+
+        } catch(CantCreateFiatIndexException e) {
+            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+        }*/
+
+        return fiatIndex;
     }
 
     @Override
-    public FiatIndex getIndexInDate(FiatCurrency currency, long timestamp) throws CantGetIndexException {
+    public Collection<FiatIndex> getIndexListFromDate(FiatCurrency currency, long timestamp) throws CantGetIndexException {
+        //Todo: implement this in dao
         return null;
     }
 
     @Override
     public Collection<FiatIndex> getQueriedIndexHistory(FiatCurrency currency) {
-        return null;
-    }
+        Collection<FiatIndex> fiatIndexList = null;
 
+        try {
+            fiatIndexList =  dao.getQueriedIndexHistory(currency);
+        } catch(CantGetFiatIndexException e) {
+            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+        }
+        return fiatIndexList;
+    }
 
 
 
@@ -135,16 +147,27 @@ public class FiatIndexPluginRoot extends AbstractPlugin implements DatabaseManag
      */
     @Override
     public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
-        return null;
+        FiatIndexWorldDeveloperDatabaseFactory factory = new FiatIndexWorldDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        return factory.getDatabaseList(developerObjectFactory);
     }
 
     @Override
     public List<DeveloperDatabaseTable> getDatabaseTableList(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase) {
-        return null;
+        FiatIndexWorldDeveloperDatabaseFactory factory = new FiatIndexWorldDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        return factory.getDatabaseTableList(developerObjectFactory);
     }
 
     @Override
     public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
-        return null;
+        FiatIndexWorldDeveloperDatabaseFactory factory = new FiatIndexWorldDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        List<DeveloperDatabaseTableRecord> tableRecordList = null;
+        try {
+            factory.initializeDatabase();
+            tableRecordList = factory.getDatabaseTableContent(developerObjectFactory, developerDatabaseTable);
+        } catch(CantInitializeFiatIndexWorldDatabaseException cantInitializeException) {
+            FermatException e = new CantDeliverDatabaseException("Database cannot be initialized", cantInitializeException, "FiatIndexPluginRoot", "");
+            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
+        }
+        return tableRecordList;
     }
 }
