c6dd624a38e07f0e250ba84e6af6dda4bfe56cb4
lnacosta <laion.cj91@gmail.com>
2015-11-23 09:41:05 -0300

    cleaning

--- WPD/library/api/fermat-wpd-api/src/main/java/com/bitdubai/fermat_wpd_api/layer/wpd_desktop_module/wallet_manager/interfaces/DealsWithWalletManagerDesktopModule.java
+++
@@ -1,8 +0,0 @@
-package com.bitdubai.fermat_wpd_api.layer.wpd_desktop_module.wallet_manager.interfaces;
-
-/**
- * Created by rodrigo on 9/23/15.
- */
-public interface DealsWithWalletManagerDesktopModule {
-    void setWalletManagerModule(WalletManagerModule walletManagerModule);
-}
