c57063e7b2e1dbb06419dede394ac692fb6d9f5e
hendry19901990 <elnegroevaristo@gmail.com>
2015-11-23 18:37:54 -0430

    Upload new events handler to notificate when close the connection vpn

---
+++ DAP/plugin/actor_network_service/fermat-dap-plugin-actor-network-service-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/network/service/asset/user/developer/bitdubai/version_1/event_handlers/VPNConnectionCloseNotificationEventHandler.java
@@ -0,0 +1,18 @@
+/*
+* @#VPNConnectionCloseNotificationEventHandler.java - 2015
+* Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+* BITDUBAI/CONFIDENTIAL
+*/
+package com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.user.developer.bitdubai.version_1.event_handlers;
+
+/**
+ * The Class <code>com.bitdubai.fermat_dap_plugin.layer.actor.network.service.asset.user.developer.bitdubai.version_1.event_handlers.VPNConnectionCloseNotificationEventHandler</code>
+ * <p/>
+ * Created by Hendry Rodriguez - (elnegroevaristo@gmail.com) on 23/11/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class VPNConnectionCloseNotificationEventHandler {
+}
