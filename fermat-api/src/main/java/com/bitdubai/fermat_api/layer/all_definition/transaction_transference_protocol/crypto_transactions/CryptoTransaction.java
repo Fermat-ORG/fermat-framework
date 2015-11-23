244ff48c864956ced5153fff5954e32d9d700d3a
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-11-23 10:37:31 -0300

    implemented New static method to get a CryptoTransaction from a Bitcoin Transaction

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/transaction_transference_protocol/crypto_transactions/CryptoTransaction.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/transaction_transference_protocol/crypto_transactions/CryptoTransaction.java
@@ -3,6 +3,13 @@ package com.bitdubai.fermat_api.layer.all_definition.transaction_transference_pr
 import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
 import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
 
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.script.ScriptChunk;
+
+import java.util.Map;
+
 /**
  * Created by eze on 11/06/15.
  */
@@ -18,6 +25,15 @@ public class CryptoTransaction{
     private String op_Return;
 
 
+    /**
+     * Overloaded constructor
+     * @param transactionHash
+     * @param addressFrom
+     * @param addressTo
+     * @param cryptoCurrency
+     * @param cryptoAmount
+     * @param cryptoStatus
+     */
     public CryptoTransaction(String transactionHash,
                              CryptoAddress addressFrom,
                              CryptoAddress addressTo,
@@ -33,10 +49,95 @@ public class CryptoTransaction{
         this.cryptoStatus = cryptoStatus;
     }
 
+
     /**
      * Default constructor.
      */
     public CryptoTransaction(){}
+
+    /**
+     * Static method that creates a CryptoTransaction from a Bitcoin Transaction
+     * Some CryptoTransaction properties, depends whether this is an incoming or outgoing
+     * transaction.
+     * @param transaction
+     * @return
+     */
+    public static CryptoTransaction getCryptoTransaction(Transaction transaction){
+        CryptoTransaction cryptoTransaction = new CryptoTransaction();
+        cryptoTransaction.setTransactionHash(transaction.getHashAsString());
+        cryptoTransaction.setCryptoCurrency(CryptoCurrency.BITCOIN);
+        cryptoTransaction.setOp_Return(getOpReturn(transaction));
+        cryptoTransaction.setBlockHash(getBlockHash(transaction));
+        cryptoTransaction.setCryptoStatus(getTransactionCryptoStatus(transaction));
+
+        return cryptoTransaction;
+    }
+
+    /**
+     * Gets the CryptoStatus of the trasaction.
+     * @param transaction
+     * @return
+     */
+    private static CryptoStatus getTransactionCryptoStatus(Transaction transaction) {
+        try{
+            int depth = transaction.getConfidence().getDepthInBlocks();
+
+            if (depth == 0)
+                return CryptoStatus.ON_CRYPTO_NETWORK;
+            else if(depth == 1)
+                return CryptoStatus.ON_BLOCKCHAIN;
+            else if (depth >= 2)
+                return CryptoStatus.IRREVERSIBLE;
+            else
+                return CryptoStatus.PENDING_SUBMIT;
+        } catch (Exception e){
+            return CryptoStatus.ON_CRYPTO_NETWORK;
+        }
+    }
+
+    /**
+     * Will get the first block where this transaction was included, if any.
+     * @param transaction
+     * @return
+     */
+    private static String getBlockHash(Transaction transaction) {
+        for (Map.Entry<Sha256Hash, Integer> entry : transaction.getAppearsInHashes().entrySet()){
+            Sha256Hash hash = entry.getKey();
+            return hash.toString();
+        }
+        /**
+         * will return null if the transaction is not in a block
+         */
+        return null;
+    }
+
+    /**
+     * Gets the OP_Return, if any, from the transaction output
+     * @param transaction
+     * @return
+     */
+    private static String getOpReturn(Transaction transaction) {
+        String hash = "";
+        try{
+            for (TransactionOutput output : transaction.getOutputs()){
+                /**
+                 * if this is an OP_RETURN output, I will get the hash
+                 */
+                if (output.getScriptPubKey().isOpReturn()){
+                    /**
+                     * I get the chunks of the Script to get the op_Return value
+                     */
+                    for (ScriptChunk chunk : output.getScriptPubKey().getChunks()){
+                        if (chunk.equalsOpCode(64))
+                            hash = new String(chunk.data);
+                    }
+                }
+            }
+        } catch (Exception e){
+            return "";
+        }
+        return hash;
+    }
 
     /**
      * Getters
