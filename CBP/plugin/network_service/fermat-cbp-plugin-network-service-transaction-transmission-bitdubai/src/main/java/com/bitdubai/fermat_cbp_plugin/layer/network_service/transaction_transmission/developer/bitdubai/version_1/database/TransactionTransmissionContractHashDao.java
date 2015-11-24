332d2521952de19a56639ef5ca9e1cd8c77e6a34
Manuel <darkpriestrelative@gmail.com>
2015-11-24 11:07:03 -0430

    Add new method in #3371

--- CBP/plugin/network_service/fermat-cbp-plugin-network-service-transaction-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/transaction_transmission/developer/bitdubai/version_1/database/TransactionTransmissionContractHashDao.java
+++ CBP/plugin/network_service/fermat-cbp-plugin-network-service-transaction-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/transaction_transmission/developer/bitdubai/version_1/database/TransactionTransmissionContractHashDao.java
@@ -2,11 +2,15 @@ package com.bitdubai.fermat_cbp_plugin.layer.network_service.transaction_transmi
 
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantCreateDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantInsertRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
+import com.bitdubai.fermat_cbp_api.layer.network_service.TransactionTransmission.interfaces.BusinessTransaction;
 import com.bitdubai.fermat_cbp_plugin.layer.network_service.transaction_transmission.developer.bitdubai.version_1.exceptions.CantInitializeNetworkServiceDatabaseException;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.transaction_transmission.developer.bitdubai.version_1.exceptions.CantInsertRecordDataBaseException;
 
 import java.util.UUID;
 
@@ -82,5 +86,40 @@ public class TransactionTransmissionContractHashDao {
         return getDataBase().getTable(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_TABLE_NAME);
     }
 
+    public void saveCryptoTransmissionMetadata(BusinessTransaction businessTransaction) throws CantInsertRecordDataBaseException {
+
+        try {
+
+            DatabaseTable databaseTable = database.getTable(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_TABLE_NAME);
+            DatabaseTableRecord databaseTableEmptyRecord = databaseTable.getEmptyRecord();
+
+            databaseTableEmptyRecord = buildDatabaseRecord(databaseTableEmptyRecord, businessTransaction);
+
+            databaseTable.insertRecord(databaseTableEmptyRecord);
+
+        } catch (CantInsertRecordException e) {
+
+            throw new CantInsertRecordDataBaseException(CantInsertRecordException.DEFAULT_MESSAGE,e, "Exception not handled by the plugin, there is a problem in database and I cannot insert the record.","");
+        }
+    }
+
+    private DatabaseTableRecord buildDatabaseRecord(DatabaseTableRecord record,
+                                                    BusinessTransaction businessTransaction) {
+
+        record.setUUIDValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_TRANSMISSION_ID_COLUMN_NAME, businessTransaction.getTransactionId());
+        //if(businessTransaction.getRequestId()!=null) record.setUUIDValue(CryptoTransmissionNetworkServiceDatabaseConstants.CRYPTO_TRANSMISSION_METADATA_REQUEST_ID_COLUMN_NAME,  businessTransaction.getRequestId());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_CONTRACT_HASH_COLUMN_NAME, businessTransaction.getContractHash());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_CONTRACT_STATUS_COLUMN_NAME, businessTransaction.getContractStatus().getCode());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_CONTRACT_ID_COLUMN_NAME, businessTransaction.getContractId());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_SENDER_PUBLIC_KEY_COLUMN_NAME , businessTransaction.getSenderId());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_SENDER_TYPE_COLUMN_NAME, businessTransaction.getSenderType().getCode());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_RECEIVER_PUBLIC_KEY_COLUMN_NAME, businessTransaction.getReceiverId());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_RECEIVER_TYPE_COLUMN_NAME, businessTransaction.getReceiverType().getCode());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_NEGOTIATION_ID_COLUMN_NAME, businessTransaction.getNegotiationId());
+        record.setStringValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_TRANSACTION_TYPE_COLUMN_NAME , businessTransaction.getType().getCode());
+        record.setLongValue(CommunicationNetworkServiceDatabaseConstants.TRANSACTION_TRANSMISSION_HASH_TIMESTAMP_COLUMN_NAME, businessTransaction.getTimestamp());
+
+        return record;
+    }
 
 }
