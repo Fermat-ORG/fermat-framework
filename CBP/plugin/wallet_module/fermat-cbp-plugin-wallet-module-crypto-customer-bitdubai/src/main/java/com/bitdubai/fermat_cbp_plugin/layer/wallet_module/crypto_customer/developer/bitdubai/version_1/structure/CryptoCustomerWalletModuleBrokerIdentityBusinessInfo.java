029a74035001bf849b1f38aebae2092e36b4d3f6
Nelson Ramirez <nelsonalfo@gmail.com>
2015-11-24 17:18:22 -0430

    agregando data dummy para probar los metodos creados en el module

--- CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_customer/developer/bitdubai/version_1/structure/CryptoCustomerWalletModuleBrokerIdentityBusinessInfo.java
+++ CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_customer/developer/bitdubai/version_1/structure/CryptoCustomerWalletModuleBrokerIdentityBusinessInfo.java
@@ -1,28 +1,35 @@
 package com.bitdubai.fermat_cbp_plugin.layer.wallet_module.crypto_customer.developer.bitdubai.version_1.structure;
 
+import com.bitdubai.fermat_api.layer.all_definition.enums.interfaces.FermatEnum;
 import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantCreateMessageSignatureException;
-import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
-import com.bitdubai.fermat_cbp_api.layer.identity.crypto_broker.interfaces.CryptoBrokerIdentity;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_customer.interfaces.BrokerIdentityBusinessInfo;
 
 /**
  * Created by nelson on 24/11/15.
  */
-public class CryptoCustomerWalletModuleCryptoBrokerIdentity implements CryptoBrokerIdentity {
+public class CryptoCustomerWalletModuleBrokerIdentityBusinessInfo implements BrokerIdentityBusinessInfo {
 
     private String publicKey;
     private String alias;
     private byte[] img;
+    private FermatEnum merchandiseCurrency;
 
 
-    public CryptoCustomerWalletModuleCryptoBrokerIdentity(String alias, byte[] img, String publicKey) {
+    public CryptoCustomerWalletModuleBrokerIdentityBusinessInfo(String alias, byte[] img, String publicKey, FermatEnum merchandiseCurrency) {
         this.alias = alias;
         this.img = img;
         this.publicKey = publicKey;
+        this.merchandiseCurrency = merchandiseCurrency;
     }
 
     @Override
     public String getAlias() {
         return alias;
+    }
+
+    @Override
+    public FermatEnum getMerchandiseCurrency() {
+        return merchandiseCurrency;
     }
 
     @Override
