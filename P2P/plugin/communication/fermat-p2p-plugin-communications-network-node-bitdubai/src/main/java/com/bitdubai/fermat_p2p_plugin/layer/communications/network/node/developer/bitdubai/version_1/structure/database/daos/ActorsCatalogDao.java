0160c65764a54e10d37ca767ad09657ce5ab900f
Roberto Requena <rart3001@gmail.com>
2015-11-23 21:29:52 -0430

    New Daos class

---
+++ P2P/plugin/communication/fermat-p2p-plugin-communications-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/database/daos/ActorsCatalogDao.java
@@ -0,0 +1,50 @@
+/*
+ * @#ActorsCatalogDao.java - 2015
+ * Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+ * BITDUBAI/CONFIDENTIAL
+ */
+package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.daos;
+
+import com.bitdubai.fermat_api.layer.all_definition.exceptions.InvalidParameterException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableRecord;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.entities.ActorsCatalog;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.daos.ActorsCatalogDao</code>
+ * <p/>
+ * Created by Roberto Requena - (rart3001@gmail.com) on 23/11/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class ActorsCatalogDao extends AbstractBaseDao<ActorsCatalog> {
+
+    /**
+     * Constructor with parameter
+     *
+     * @param dataBase
+     */
+    public ActorsCatalogDao(Database dataBase) {
+        super(dataBase, "", "");
+    }
+
+    /**
+     * (non-javadoc)
+     * @see AbstractBaseDao#getEntityFromDatabaseTableRecord(DatabaseTableRecord)
+     */
+    @Override
+    protected ActorsCatalog getEntityFromDatabaseTableRecord(DatabaseTableRecord record) throws InvalidParameterException {
+        return null;
+    }
+
+    /**
+     * (non-javadoc)
+     * @see AbstractBaseDao#getDatabaseTableRecordFromEntity
+     */
+    @Override
+    protected DatabaseTableRecord getDatabaseTableRecordFromEntity(ActorsCatalog entity) {
+        return null;
+    }
+}
