52f9fcc7f07d073dceab194ca22640ffc1d3bc10
Víctor A. Mars M <marsvicam@gmail.com>
2015-11-24 13:13:30 -0430

    Created placeholder implementation for the message reception. I still need to know how I'll receive that message but the final implementation should be this one.

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-appropriation-stats-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/appropriation_stats/developer/bitdubai/version_1/structure/events/AppropriationStatsMonitorAgent.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-appropriation-stats-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/appropriation_stats/developer/bitdubai/version_1/structure/events/AppropriationStatsMonitorAgent.java
@@ -2,13 +2,16 @@ package com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.appropria
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.util.XMLParser;
 import com.bitdubai.fermat_api.layer.dmp_world.Agent;
 import com.bitdubai.fermat_api.layer.dmp_world.wallet.exceptions.CantStartAgentException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
 import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.CantSetObjectException;
+import com.bitdubai.fermat_dap_api.layer.all_definition.network_service_message.AssetAppropriationContentMessage;
 import com.bitdubai.fermat_dap_api.layer.all_definition.util.Validate;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.appropriation_stats.developer.bitdubai.version_1.AppropriationStatsDigitalAssetTransactionPluginRoot;
+import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.appropriation_stats.developer.bitdubai.version_1.structure.database.AppropriationStatsDAO;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedPluginExceptionSeverity;
 
@@ -118,6 +121,17 @@ public class AppropriationStatsMonitorAgent implements Agent {
         }
 
         private void doTheMainTask() {
+
+            //TODO HERE GOES THE NOTIFICATION THAT I RECEIVED A NEW MESSAGE IN THE ISSUER DEVICE.
+            //THEN I ASK FOR THE MESSAGE TO THE RESPECTIVE PLUGIN
+
+            String message = "";
+            AssetAppropriationContentMessage contentMessage = (AssetAppropriationContentMessage) XMLParser.parseXML(message, new AssetAppropriationContentMessage());
+            try (AppropriationStatsDAO dao = new AppropriationStatsDAO(pluginDatabaseSystem, pluginId)) {
+                dao.assetAppropriated(contentMessage.getDigitalAssetAppropriated(), contentMessage.getUserThatAppropriate()); //That should be all...
+            } catch (Exception e) {
+                errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_ASSET_APPROPRIATION_STATS_TRANSACTION, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, e);
+            }
         }
 
 
