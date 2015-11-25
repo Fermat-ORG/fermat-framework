99ce7b9483d6fe208c0c2f7633922fb78fc539d7
Guillermo Gutierrez <guillermo20@gmail.com>
2015-11-25 15:05:24 -0430

    avance en implementacion de hold bank Transaction

---
+++ BNK/plugin/bank_money_transaction/fermat-bnk-plugin-bank-money-transaction-hold-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/bank_money_transaction/hold/developer/bitdubai/version_1/HoldBankMoneyTransactionPluginRoot.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_bnk_plugin.layer.bank_money_transaction.hold.developer.bitdubai.version_1;
+
+/**
+ * Created by memo on 25/11/15.
+ */
+public class HoldBankMoneyTransactionPluginRoot {
+}
