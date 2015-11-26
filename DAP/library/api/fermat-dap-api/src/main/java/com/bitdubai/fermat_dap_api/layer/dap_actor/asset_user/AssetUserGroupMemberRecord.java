1e967b8821a4f5d6569dc5b42de9eb9597900b04
Luis Campo <campusprize@gmail.com>
2015-11-25 23:00:21 -0430

    fix #3436 Implemented Public Interface  ActorAssetUserGroupMember

---
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/AssetUserGroupMemberRecord.java
@@ -0,0 +1,30 @@
+package com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user;
+
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces.ActorAssetUserGroupMember;
+
+/**
+ * Created by Luis Campo on 25/11/15.
+ */
+public class AssetUserGroupMemberRecord implements ActorAssetUserGroupMember {
+
+    private String groupId;
+    private String actorPublicKey;
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void setActorPublicKey(String actorPublicKey) {
+        this.actorPublicKey = actorPublicKey;
+    }
+
+    @Override
+    public String getGroupId() {
+        return null;
+    }
+
+    @Override
+    public String getActorPublicKey() {
+        return null;
+    }
+}
