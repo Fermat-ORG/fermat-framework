08a5d19a4acb74bb73a3863ddd9c72da8defe1a4
Luis Campo <campusprize@gmail.com>
2015-11-25 22:47:30 -0430

    fix #3398 Created Public Interface  ActorAssetUserGroup

---
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/interfaces/ActorAssetUserGroup.java
@@ -0,0 +1,19 @@
+package com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces;
+
+/**
+ * Created by lcampo on 25/11/15.
+ */
+public interface ActorAssetUserGroup {
+    /**
+     * Returns the group id
+     * @return groupId
+     */
+    String getGroupId();
+
+    /**
+     * Returns the name of the group
+     * @return groupName
+     */
+    String getGroupName();
+
+}
