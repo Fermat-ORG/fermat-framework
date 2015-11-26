e3851de0986f6b52899152cdcc4a3669814ac7b4
abicelis <abicelis@gmail.com>
2015-11-26 01:01:27 -0430

    Implementacion del cash unhold plugin

--- CSH/plugin/cash_money_transaction/fermat-csh-plugin-cash-money-transaction-unhold-bitdubai/src/main/java/com/bitdubai/fermat_csh_plugin/layer/cash_money_transaction/unhold/developer/bitdubai/version_1/structure/CashMoneyTransactionUnholdProcessorAgent.java
+++ CSH/plugin/cash_money_transaction/fermat-csh-plugin-cash-money-transaction-unhold-bitdubai/src/main/java/com/bitdubai/fermat_csh_plugin/layer/cash_money_transaction/unhold/developer/bitdubai/version_1/structure/CashMoneyTransactionUnholdProcessorAgent.java
@@ -1,11 +1,12 @@
-package com.bitdubai.fermat_csh_plugin.layer.cash_money_transaction.hold.developer.bitdubai.version_1.structure;
+package com.bitdubai.fermat_csh_plugin.layer.cash_money_transaction.unhold.developer.bitdubai.version_1.structure;
 
 import com.bitdubai.fermat_api.FermatAgent;
 import com.bitdubai.fermat_api.layer.all_definition.enums.AgentStatus;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
-import com.bitdubai.fermat_csh_api.layer.csh_cash_money_transaction.hold.exceptions.CantGetHoldTransactionException;
-import com.bitdubai.fermat_csh_api.layer.csh_cash_money_transaction.hold.interfaces.CashHoldTransaction;
-import com.bitdubai.fermat_csh_plugin.layer.cash_money_transaction.hold.developer.bitdubai.version_1.exceptions.CantUpdateHoldTransactionException;
+import com.bitdubai.fermat_csh_api.layer.csh_cash_money_transaction.unhold.exceptions.CantCreateUnholdTransactionException;
+import com.bitdubai.fermat_csh_api.layer.csh_cash_money_transaction.unhold.exceptions.CantGetUnholdTransactionException;
+import com.bitdubai.fermat_csh_api.layer.csh_cash_money_transaction.unhold.interfaces.CashUnholdTransaction;
+import com.bitdubai.fermat_csh_plugin.layer.cash_money_transaction.unhold.developer.bitdubai.version_1.exceptions.CantUpdateUnholdTransactionException;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedPluginExceptionSeverity;
 
@@ -14,18 +15,18 @@ import java.util.List;
 /**
  * Created by Alejandro Bicelis on 11/19/2015.
  */
-public class CashMoneyTransactionHoldProcessorAgent extends FermatAgent {
+public class CashMoneyTransactionUnholdProcessorAgent extends FermatAgent {
 
     private static final int SLEEP = 5000;
 
     private Thread agentThread;
 
     private final ErrorManager errorManager;
-    private final CashMoneyTransactionHoldManager holdTransactionManager;
+    private final CashMoneyTransactionUnholdManager unholdTransactionManager;
 
-    public CashMoneyTransactionHoldProcessorAgent(final ErrorManager errorManager, final CashMoneyTransactionHoldManager holdManager) {
+    public CashMoneyTransactionUnholdProcessorAgent(final ErrorManager errorManager, final CashMoneyTransactionUnholdManager unholdManager) {
         this.errorManager = errorManager;
-        this.holdTransactionManager = holdManager;
+        this.unholdTransactionManager = unholdManager;
 
         this.agentThread = new Thread(new Runnable() {
             @Override
@@ -75,37 +76,37 @@ public class CashMoneyTransactionHoldProcessorAgent extends FermatAgent {
 
     private void doTheMainTask() {
 
-        List<CashHoldTransaction> transactionList;
+        List<CashUnholdTransaction> transactionList;
 
         try {
-            transactionList = holdTransactionManager.getAcknowledgedTransactionList();
-        } catch (CantGetHoldTransactionException e) {
-            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+            transactionList = unholdTransactionManager.getAcknowledgedTransactionList();
+        } catch (CantGetUnholdTransactionException e) {
+            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_UNHOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
             return;
         }
 
         /*
          * For each new (acknowledged) transaction, this thread:
          * Changes its status to Pending
-         * Tries to hold funds in wallet
+         * Tries to unhold funds in wallet
          * If successfull, changes transaction status to Confirmed
          * If not, changes transaction status to Rejected.
          */
 
 
-        for(CashHoldTransaction transaction : transactionList) {
+        for(CashUnholdTransaction transaction : transactionList) {
 
             try {
-                //TODO: cashMoneyWalletManager.loadCashMoneyWallet.hold(cashMoneyWalletTransaction);
-                holdTransactionManager.setTransactionStatusToConfirmed(transaction.getTransactionId());
-            /*} catch (CantCreateHoldTransactionException e) {
+                //TODO: cashMoneyWalletManager.loadCashMoneyWallet.unhold(cashMoneyWalletTransaction);
+                unholdTransactionManager.setTransactionStatusToConfirmed(transaction.getTransactionId());
+            /*} catch (CantCreateUnholdTransactionException e) {
                 try {
-                    holdTransactionManager.setTransactionStatusToRejected(transaction.getTransactionId());
-                } catch (CantUpdateHoldTransactionException ex) {
-                    errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, ex);
+                    unholdTransactionManager.setTransactionStatusToRejected(transaction.getTransactionId());
+                } catch (CantUpdateUnholdTransactionException ex) {
+                    errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_UNHOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, ex);
                 }*/
-            } catch (CantUpdateHoldTransactionException e) {
-                errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_HOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+            } catch (CantUpdateUnholdTransactionException e) {
+                errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_CSH_MONEY_TRANSACTION_UNHOLD, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
             }
 
             //TODO: Lanzar un evento al plugin que envio la transaccion para avisarle que se updateo el status de su transaccion.
