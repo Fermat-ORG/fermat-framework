232b10657212fc12934eff569d76505a4200b872
abicelis <abicelis@gmail.com>
2015-11-25 21:43:59 -0430

    Inclusion del CSH platform en el FermatSystem en el core

--- fermat-core/src/main/java/com/bitdubai/fermat_core/FermatSystem.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/FermatSystem.java
@@ -39,6 +39,7 @@ import com.bitdubai.fermat_api.layer.resources.ResourcesManager;
 import com.bitdubai.fermat_bch_core.BCHPlatform;
 import com.bitdubai.fermat_cbp_core.CBPPlatform;
 import com.bitdubai.fermat_ccp_core.CCPPlatform;
+import com.bitdubai.fermat_csh_core.CSHPlatform;
 import com.bitdubai.fermat_dap_core.DAPPlatform;
 import com.bitdubai.fermat_p2p_core.P2PPlatform;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
@@ -90,6 +91,7 @@ public final class FermatSystem {
             fermatSystemContext.registerPlatform(new BCHPlatform());
             fermatSystemContext.registerPlatform(new CBPPlatform());
             fermatSystemContext.registerPlatform(new CCPPlatform());
+            fermatSystemContext.registerPlatform(new CSHPlatform());
             fermatSystemContext.registerPlatform(new DAPPlatform());
             fermatSystemContext.registerPlatform(new P2PPlatform());
             fermatSystemContext.registerPlatform(new PIPPlatform());
