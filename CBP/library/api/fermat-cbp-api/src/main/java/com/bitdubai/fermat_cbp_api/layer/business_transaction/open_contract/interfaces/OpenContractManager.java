39b7e6e336ae4d9c38e3b471e392f61ba948ea61
Manuel <darkpriestrelative@gmail.com>
2015-11-26 16:20:54 -0430

    Fix #2977

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/open_contract/interfaces/OpenContractManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/open_contract/interfaces/OpenContractManager.java
@@ -1,7 +1,26 @@
 package com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.interfaces;
 
+import com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.enums.OpenContractStatus;
+
 /**
+ * The interface <code>com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.interfaces.OpenContractManager</code>
+ * provides the methods to interact with the Open Contract Business Transaction.
+ * <p>
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 19/11/15.
  */
 public interface OpenContractManager {
+
+    /**
+     * This method opens a new contract by a negotiation Id
+     * @param negotiationId
+     */
+    void openContract(String negotiationId);
+
+    /**
+     * This method returns the status from the Open Contract Business Transaction.
+     * @param negotiationId
+     * @return
+     */
+    OpenContractStatus getOpenContractStatus(String negotiationId);
+
 }
