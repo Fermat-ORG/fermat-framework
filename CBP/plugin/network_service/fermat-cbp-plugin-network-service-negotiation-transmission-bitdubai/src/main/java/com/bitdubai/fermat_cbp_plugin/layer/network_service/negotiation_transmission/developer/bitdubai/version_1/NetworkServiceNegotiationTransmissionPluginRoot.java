7e00999815b0d27d3fc980d76e173350447814e3
yalayn <y.alayn@gmail.com>
2015-11-26 10:30:51 -0430

    CBP-Network Service - Negotiation Transaction

--- CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/version_1/NetworkServiceNegotiationTransmissionPluginRoot.java
+++ CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/version_1/NetworkServiceNegotiationTransmissionPluginRoot.java
@@ -1,5 +1,8 @@
 package com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1;
 
+import com.bitdubai.fermat_api.CantStartPluginException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededPluginReference;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.components.enums.PlatformComponentType;
@@ -10,16 +13,44 @@ import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabase;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTable;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.events.EventSource;
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventListener;
 import com.bitdubai.fermat_api.layer.all_definition.network_service.enums.NetworkServiceType;
 import com.bitdubai.fermat_api.layer.all_definition.network_service.interfaces.NetworkServiceConnectionManager;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantCreateDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_api.layer.osa_android.location_system.Location;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.event_handlers.CompleteComponentConnectionRequestNotificationEventHandler;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.event_handlers.CompleteComponentRegistrationNotificationEventHandler;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.event_handlers.CompleteRequestListComponentRegisteredNotificationEventHandler;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.event_handlers.FailureComponentConnectionRequestNotificationEventHandler;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.event_handlers.NewReceiveMessagesNotificationEventHandler;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.structure.CommunicationNetworkServiceConnectionManager;
+import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.communication.structure.CommunicationRegistrationProcessNetworkServiceAgent;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.abstract_classes.AbstractNetworkService;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.exceptions.CantLoadKeyPairException;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.template.database.CommunicationNetworkServiceDatabaseConstants;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.template.database.CommunicationNetworkServiceDatabaseFactory;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.template.exceptions.CantInitializeNetworkServiceDatabaseException;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.enums.P2pEventType;
+import com.bitdubai.fermat_p2p_api.layer.p2p_communication.WsCommunicationsCloudClientManager;
 import com.bitdubai.fermat_p2p_api.layer.p2p_communication.commons.contents.FermatMessage;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by Yordin Alayn on 16.09.15.
@@ -27,19 +58,163 @@ import java.util.List;
 
 public class NetworkServiceNegotiationTransmissionPluginRoot extends AbstractNetworkService implements DatabaseManagerForDevelopers {
 
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_FILE_SYSTEM)
+    protected PluginFileSystem pluginFileSystem        ;
+
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_DATABASE_SYSTEM)
+    private PluginDatabaseSystem pluginDatabaseSystem;
+
+    @NeededPluginReference(platform = Platforms.COMMUNICATION_PLATFORM, layer = Layers.COMMUNICATION         , plugin = Plugins.WS_CLOUD_CLIENT)
+    private WsCommunicationsCloudClientManager wsCommunicationsCloudClientManager;
+
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM   , layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER         )
+    private ErrorManager errorManager;
+
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM   , layer = Layers.PLATFORM_SERVICE, addon = Addons.EVENT_MANAGER         )
+    private EventManager eventManager;
+
+    private List<FermatEventListener> listenersAdded;
+
+    /*Represent the remoteNetworkServicesRegisteredList*/
+    private List<PlatformComponentProfile> remoteNetworkServicesRegisteredList;
+
+    /*Represent the cryptoPaymentRequestNetworkServiceConnectionManager*/
+    private CommunicationNetworkServiceConnectionManager communicationNetworkServiceConnectionManager;
+
+    /*Represent the dataBase*/
+    private Database dataBase;
+
+    /*Represent CommunicationRegistrationProcessNetworkServiceAgent*/
+    private CommunicationRegistrationProcessNetworkServiceAgent communicationRegistrationProcessNetworkServiceAgent;
+
+
+
+
+
+
+
+    /*CONSTRUCTOR*/
+
     public NetworkServiceNegotiationTransmissionPluginRoot() {
         super(
-            new PluginVersionReference(new Version()),
-            PlatformComponentType.NETWORK_SERVICE,
-            NetworkServiceType.NEGOTIATION_TRANSMISSION,
-            "Negotiation Transmission Network Service",
-            "NegotiationTransmissionNetworkService",
-            null,
-            EventSource.NETWORK_SERVICE_NEGOTIATION_TRANSMISSION
+                new PluginVersionReference(new Version()),
+                PlatformComponentType.NETWORK_SERVICE,
+                NetworkServiceType.NEGOTIATION_TRANSMISSION,
+                "Negotiation Transmission Network Service",
+                "NegotiationTransmissionNetworkService",
+                null,
+                EventSource.NETWORK_SERVICE_NEGOTIATION_TRANSMISSION
         );
     }
 
-    /*DatabaseManagerForDevelopers Interface implementation.*/
+    /*SERVICE*/
+
+    @Override
+    public void start(){
+//        System.out.println("Starting Transaction Transmission Network Service");
+//        this.serviceStatus = ServiceStatus.STARTED;
+        /*Load KeyPair*/
+        /*try {
+            loadKeyPair(pluginFileSystem);
+        } catch (CantLoadKeyPairException e) {
+            errorManager.reportUnexpectedPluginException(this.getPluginVersionReference(), UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+            throw new CantStartPluginException(e, "", "Problem trying to load the key pair of the plugin.");
+        }*/
+        System.out.println("********* Crypto Addresses: Starting. ");
+
+        /*Validate required resources*/
+        //validateInjectedResources();
+
+
+        /*Initialize crypto payment request dao*/
+        /*try {
+            cryptoAddressesNetworkServiceDao = new CryptoAddressesNetworkServiceDao(pluginDatabaseSystem, pluginId);
+            cryptoAddressesNetworkServiceDao.initialize();
+        } catch(CantInitializeNetworkServiceDatabaseException e) {
+            CantStartPluginException pluginStartException = new CantStartPluginException(e, "", "Problem initializing crypto addresses dao.");
+            errorManager.reportUnexpectedPluginException(this.getPluginVersionReference(), UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, pluginStartException);
+            throw pluginStartException;
+        }*/
+
+        try {
+            /*Initialize the data base*/
+            initializeCommunicationDb();
+
+            /*Initialize listeners*/
+            initializeListener();
+
+            /*Verify if the communication cloud client is active*/
+            if (!wsCommunicationsCloudClientManager.isDisable()){
+                /*Initialize the agent and start*/
+                communicationRegistrationProcessNetworkServiceAgent = new CommunicationRegistrationProcessNetworkServiceAgent(this, wsCommunicationsCloudClientManager.getCommunicationsCloudClientConnection());
+                communicationRegistrationProcessNetworkServiceAgent.start();
+            }
+
+            /*List Network Service Register*/
+            remoteNetworkServicesRegisteredList = new CopyOnWriteArrayList<>();
+
+            /*Initilize service*/
+            this.serviceStatus = ServiceStatus.STARTED;
+
+        } catch (CantInitializeNetworkServiceDatabaseException exception) {
+
+            StringBuffer contextBuffer = new StringBuffer();
+            contextBuffer.append("Plugin ID: " + pluginId);
+            contextBuffer.append(CantStartPluginException.CONTEXT_CONTENT_SEPARATOR);
+            contextBuffer.append("Database Name: " + CommunicationNetworkServiceDatabaseConstants.DATA_BASE_NAME);
+
+            String context = contextBuffer.toString();
+            String possibleCause = "The Template Database triggered an unexpected problem that wasn't able to solve by itself";
+            CantStartPluginException pluginStartException = new CantStartPluginException(CantStartPluginException.DEFAULT_MESSAGE, exception, context, possibleCause);
+
+            /*errorManager.reportUnexpectedPluginException(this.getPluginVersionReference(),UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, pluginStartException);
+            throw pluginStartException;*/
+        }
+
+        System.out.println("********* Crypto Addresses: Successful start. ");
+
+    }
+
+    @Override
+    public void pause() {
+
+        // pause connections manager.
+        communicationNetworkServiceConnectionManager.pause();
+
+        this.serviceStatus = ServiceStatus.PAUSED;
+    }
+
+    @Override
+    public void resume() {
+
+        // resume connections manager.
+        communicationNetworkServiceConnectionManager.resume();
+
+        this.serviceStatus = ServiceStatus.STARTED;
+    }
+
+    @Override
+    public void stop() {
+
+        // remove all listeners from the event manager and from the plugin.
+        for (FermatEventListener listener: listenersAdded)
+            eventManager.removeListener(listener);
+
+        listenersAdded.clear();
+
+        // close all connections.
+        communicationNetworkServiceConnectionManager.closeAllConnection();
+
+        // set to not registered.
+        register = Boolean.FALSE;
+
+        this.serviceStatus = ServiceStatus.STOPPED;
+    }
+
+    /*END SERVICE*/
+
+    /*DATABASEMANAGERFORDEVELOPERS.*/
+
     @Override
     public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
         return null;
@@ -54,7 +229,7 @@ public class NetworkServiceNegotiationTransmissionPluginRoot extends AbstractNet
     public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
         return null;
     }
-    /*END DatabaseManagerForDevelopers Interface implementation.*/
+    /*END DATABASEMANAGERFORDEVELOPERS*/
 
     @Override
     public FermatManager getManager() {
@@ -115,9 +290,76 @@ public class NetworkServiceNegotiationTransmissionPluginRoot extends AbstractNet
 
     }
 
-    @Override
-    public void start(){
-        System.out.println("Starting Transaction Transmission Network Service");
-        this.serviceStatus = ServiceStatus.STARTED;
+    /*PRIVATE*/
+
+    /*This method validate is all required resource are injected into the plugin root by the platform*/
+    private void validateInjectedResources() throws CantStartPluginException {
+         /*If all resources are inject*/
+        if (wsCommunicationsCloudClientManager == null ||
+            pluginDatabaseSystem               == null ||
+            errorManager                       == null ||
+            eventManager                       == null ) {
+            String context =
+                    "Plugin ID:                          " + pluginId                           + CantStartPluginException.CONTEXT_CONTENT_SEPARATOR +
+                    "wsCommunicationsCloudClientManager: " + wsCommunicationsCloudClientManager + CantStartPluginException.CONTEXT_CONTENT_SEPARATOR +
+                    "pluginDatabaseSystem:               " + pluginDatabaseSystem               + CantStartPluginException.CONTEXT_CONTENT_SEPARATOR +
+                    "errorManager:                       " + errorManager                       + CantStartPluginException.CONTEXT_CONTENT_SEPARATOR +
+                    "eventManager:                       " + eventManager;
+            String possibleCause = "No all required resource are injected";
+            CantStartPluginException pluginStartException = new CantStartPluginException(CantStartPluginException.DEFAULT_MESSAGE, null, context, possibleCause);
+            errorManager.reportUnexpectedPluginException(this.getPluginVersionReference(), UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, pluginStartException);
+            throw pluginStartException;
+        }
     }
+
+    /*This method initialize the database*/
+    private void initializeCommunicationDb() throws CantInitializeNetworkServiceDatabaseException {
+        try {
+            this.dataBase = this.pluginDatabaseSystem.openDatabase(pluginId, CommunicationNetworkServiceDatabaseConstants.DATA_BASE_NAME);
+        } catch (CantOpenDatabaseException cantOpenDatabaseException) {
+            errorManager.reportUnexpectedPluginException(this.getPluginVersionReference(), UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, cantOpenDatabaseException);
+            throw new CantInitializeNetworkServiceDatabaseException(cantOpenDatabaseException);
+        } catch (DatabaseNotFoundException e) {
+            CommunicationNetworkServiceDatabaseFactory communicationLayerNetworkServiceDatabaseFactory = new CommunicationNetworkServiceDatabaseFactory(pluginDatabaseSystem);
+            try {
+                this.dataBase = communicationLayerNetworkServiceDatabaseFactory.createDatabase(pluginId, CommunicationNetworkServiceDatabaseConstants.DATA_BASE_NAME);
+            } catch (CantCreateDatabaseException cantCreateDatabaseException) {
+                errorManager.reportUnexpectedPluginException(this.getPluginVersionReference(), UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, cantCreateDatabaseException);
+                throw new CantInitializeNetworkServiceDatabaseException(cantCreateDatabaseException);
+            }
+        }
+    }
+
+    private void initializeListener(){
+         /*Listen and handle Complete Component Registration Notification Event*/
+        FermatEventListener fermatEventListener = eventManager.getNewListener(P2pEventType.COMPLETE_COMPONENT_REGISTRATION_NOTIFICATION);
+        fermatEventListener.setEventHandler(new CompleteComponentRegistrationNotificationEventHandler(this));
+        eventManager.addListener(fermatEventListener);
+        listenersAdded.add(fermatEventListener);
+
+         /*Listen and handle Complete Request List Component Registered Notification Event*/
+        fermatEventListener = eventManager.getNewListener(P2pEventType.COMPLETE_REQUEST_LIST_COMPONENT_REGISTERED_NOTIFICATION);
+        fermatEventListener.setEventHandler(new CompleteRequestListComponentRegisteredNotificationEventHandler(this));
+        eventManager.addListener(fermatEventListener);
+        listenersAdded.add(fermatEventListener);
+
+        /*Listen and handle Complete Request List Component Registered Notification Event*/
+        fermatEventListener = eventManager.getNewListener(P2pEventType.COMPLETE_COMPONENT_CONNECTION_REQUEST_NOTIFICATION);
+        fermatEventListener.setEventHandler(new CompleteComponentConnectionRequestNotificationEventHandler(this));
+        eventManager.addListener(fermatEventListener);
+        listenersAdded.add(fermatEventListener);
+
+        /*failure connection*/
+        fermatEventListener = eventManager.getNewListener(P2pEventType.FAILURE_COMPONENT_CONNECTION_REQUEST_NOTIFICATION);
+        fermatEventListener.setEventHandler(new FailureComponentConnectionRequestNotificationEventHandler(this));
+        eventManager.addListener(fermatEventListener);
+        listenersAdded.add(fermatEventListener);
+
+        /*new message*/
+        fermatEventListener = eventManager.getNewListener(P2pEventType.NEW_NETWORK_SERVICE_MESSAGE_RECEIVE_NOTIFICATION);
+        fermatEventListener.setEventHandler(new NewReceiveMessagesNotificationEventHandler(this));
+        eventManager.addListener(fermatEventListener);
+        listenersAdded.add(fermatEventListener);
+    }
+    /*END PRIVATE*/
 }
