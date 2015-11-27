d82651b61ec94d165f5e9d1ea05cfd38e4170000
hendry19901990 <elnegroevaristo@gmail.com>
2015-11-27 10:00:14 -0430

    upload Dao modified

---
+++ P2P/plugin/communication/fermat-p2p-plugin-communications-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/client/developer/bitdubai/version_1/structure/channels/CommunicationsNetworkClientChannel.java
@@ -0,0 +1,18 @@
+/*
+* @#CommunicationsNetworkClientChannel.java - 2015
+* Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+* BITDUBAI/CONFIDENTIAL
+*/
+package com.bitdubai.fermat_p2p_plugin.layer.communications.network.client.developer.bitdubai.version_1.structure.channels;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.communications.network.client.developer.bitdubai.version_1.structure.channels.CommunicationsNetworkClientChannel</code>
+ * <p/>
+ * Created by Hendry Rodriguez - (elnegroevaristo@gmail.com) on 27/11/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class CommunicationsNetworkClientChannel {
+}
