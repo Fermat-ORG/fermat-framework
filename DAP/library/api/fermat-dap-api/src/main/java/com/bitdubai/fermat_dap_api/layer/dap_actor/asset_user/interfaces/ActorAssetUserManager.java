2c160083c7ebd7296b535f49331769990c699c3a
Luis Campo <campusprize@gmail.com>
2015-11-26 22:44:31 -0430

    fix #3400 Add methods in the interface ActorAssetUserManager for managing user groups

--- DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/interfaces/ActorAssetUserManager.java
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_actor/asset_user/interfaces/ActorAssetUserManager.java
@@ -2,10 +2,16 @@ package com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.interfaces;
 
 import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_issuer.interfaces.ActorAssetIssuer;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.AssetUserGroupMemberRecord;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.AssetUserGroupRecord;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantAssetUserActorNotFoundException;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantConnectToAssetUserException;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantCreateAssetUserActorException;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantCreateAssetUserGroupException;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantDeleteAssetUserGroupException;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantGetAssetUserActorsException;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantGetAssetUserGroupExcepcion;
+import com.bitdubai.fermat_dap_api.layer.dap_actor.asset_user.exceptions.CantUpdateAssetUserGroupException;
 
 import java.util.List;
 
@@ -63,4 +69,71 @@ public interface ActorAssetUserManager extends FermatManager {
      * @throws CantConnectToAssetUserException
      */
     void connectToActorAssetUser(ActorAssetIssuer requester, List<ActorAssetUser> actorAssetUsers) throws CantConnectToAssetUserException;
+
+    /**
+     * The method <code>createAssetUserGroup</code> Register a group in database Actor Asset User
+     * @param assetUserGroup
+     * @throws CantCreateAssetUserGroupException
+     */
+    void createAssetUserGroup (ActorAssetUserGroup assetUserGroup) throws CantCreateAssetUserGroupException;
+
+    /**
+     * The method <code>updateAssetUserGroup</code> Update a group in database Actor Asset User
+     * @param assetUserGroup
+     * @throws CantUpdateAssetUserGroupException
+     */
+    void updateAssetUserGroup(ActorAssetUserGroup assetUserGroup) throws CantUpdateAssetUserGroupException;
+
+    /**
+     * The method <code>deleteAssetUserGroup</code> Delete a group in database Actor Asset User
+     * @param assetUserGroupId
+     * @throws CantDeleteAssetUserGroupException
+     */
+    void deleteAssetUserGroup(String assetUserGroupId) throws CantDeleteAssetUserGroupException;
+
+    /**
+     * The method <code>addAssetUserToGroup</code> Add a user to a group
+     * @param actorAssetUserGroupMember
+     * @throws CantCreateAssetUserGroupException
+     */
+    void addAssetUserToGroup (ActorAssetUserGroupMember actorAssetUserGroupMember) throws CantCreateAssetUserGroupException;
+
+    /**
+     * The method <code>removeAssetUserFromGroup</code> Remove a user from group
+     * @param assetUserGroupMember
+     * @throws CantCreateAssetUserGroupException
+     */
+    void removeAssetUserFromGroup (ActorAssetUserGroupMember assetUserGroupMember) throws CantCreateAssetUserGroupException;
+
+    /**
+     * The method <code>getAssetUserGroupsList</code> Returns a list of groups
+     * @return
+     * @throws CantGetAssetUserGroupExcepcion
+     */
+    List<ActorAssetUserGroup>  getAssetUserGroupsList() throws CantGetAssetUserGroupExcepcion;
+
+
+    /**
+     * The method <code>getListActorAssetUserByGroups</code> Returns a list of groups by name
+     * @param groupName
+     * @return
+     * @throws CantGetAssetUserActorsException
+     */
+    List<ActorAssetUser> getListActorAssetUserByGroups (String groupName) throws CantGetAssetUserActorsException;
+
+    /**
+     * The method <code>getListAssetUserGroupsByActorAssetUser</code> Returns a list of groups by asset user
+     * @param actorAssetUserPublicKey
+     * @return
+     * @throws CantGetAssetUserGroupExcepcion
+     */
+    List<ActorAssetUserGroup> getListAssetUserGroupsByActorAssetUser (String actorAssetUserPublicKey) throws CantGetAssetUserGroupExcepcion;
+
+    /**
+     * The method <code>getAssetUserGroup</code> Returns a group by id
+     * @param groupId
+     * @return
+     * @throws CantGetAssetUserGroupExcepcion
+     */
+    ActorAssetUserGroup getAssetUserGroup(String groupId) throws CantGetAssetUserGroupExcepcion;
 }
