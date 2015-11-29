97f42795ba8546abe4bea631bc100f01d9d7f5a0
Angel Ivan Veloz Varela <vlzangel91@gmail.com>
2015-11-29 00:11:59 -0430

    Refactory interface ContractClause

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/contract/ContractClause.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/contract/ContractClause.java
@@ -2,16 +2,18 @@ package com.bitdubai.fermat_cbp_api.all_definition.contract;
 
 import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseStatus;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseType;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ContractClauseStatus;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ContractClauseType;
 
 import java.util.UUID;
 
 /**
  * Created by angel on 17/11/15.
  */
+
 public interface ContractClause {
     UUID getClauseId();
-    ClauseType getType();
-    String getValue();
-    ClauseStatus getStatus();
-    String getProposedBy();
+    ContractClauseType getType();
+    Integer getExecutionOrder();
+    ContractClauseStatus getStatus();
 }
