ebe8ef2d8d855b09661c76deb10b412e1826de5c
Manuel <darkpriestrelative@gmail.com>
2015-11-30 09:45:24 -0430

    Remove, temporally, implements contract from ContractRecord class

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/structure/OpenContractTransactionManager.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/structure/OpenContractTransactionManager.java
@@ -1,18 +1,14 @@
 package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.open_contract.developer.bitdubai.version_1.structure;
 
-import com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.enums.ContractType;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.enums.OpenContractStatus;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.exceptions.CantOpenContractException;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.interfaces.OpenContractManager;
 import com.bitdubai.fermat_cbp_api.layer.contract.customer_broker_purchase.interfaces.CustomerBrokerContractPurchaseManager;
 import com.bitdubai.fermat_cbp_api.layer.contract.customer_broker_sale.interfaces.CustomerBrokerContractSaleManager;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchaseNegotiation;
-import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchaseNegotiationManager;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.interfaces.CustomerBrokerSaleNegotiation;
-import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.interfaces.CustomerBrokerSaleNegotiationManager;
 import com.bitdubai.fermat_cbp_api.layer.network_service.TransactionTransmission.interfaces.TransactionTransmissionManager;
 import com.bitdubai.fermat_cbp_api.layer.world.interfaces.FiatIndex;
-import com.bitdubai.fermat_cbp_api.layer.world.interfaces.FiatIndexManager;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 26/11/15.
