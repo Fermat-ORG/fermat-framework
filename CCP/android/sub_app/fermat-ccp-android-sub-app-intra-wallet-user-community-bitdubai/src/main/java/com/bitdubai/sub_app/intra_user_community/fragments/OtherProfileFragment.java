c1e1e0c1b94c85564ef75721b1075860f4e937af
josemanueldsds <josemanueldsds@gmail.com>
2015-11-29 23:06:44 -0430

    Added OtherProfileFragment

---
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/OtherProfileFragment.java
@@ -0,0 +1,66 @@
+package com.bitdubai.sub_app.intra_user_community.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatRoundedImageView;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
+import com.bitdubai.fermat_ccp_api.layer.module.intra_user.exceptions.CantGetActiveLoginIdentityException;
+import com.bitdubai.sub_app.intra_user_community.R;
+import com.bitdubai.sub_app.intra_user_community.common.navigation_drawer.NavigationViewAdapter;
+import com.bitdubai.sub_app.intra_user_community.common.utils.FragmentsCommons;
+import com.bitdubai.sub_app.intra_user_community.session.IntraUserSubAppSession;
+
+/**
+ * Created by josemanueldsds on 29/11/15.
+ */
+public class OtherProfileFragment extends FermatFragment {
+
+    private View rootView;
+    private IntraUserSubAppSession intraUserSubAppSession;
+    private FermatRoundedImageView userProfileAvatar;
+    private FermatTextView userName;
+    private FermatTextView userEmail;
+
+    /**
+     * Create a new instance of this fragment
+     *
+     * @return InstalledFragment instance object
+     */
+    public static OtherProfileFragment newInstance() {
+        return new OtherProfileFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        try {
+            rootView = inflater.inflate(R.layout.intra_user_other_profile, container, false);
+            userProfileAvatar = (FermatRoundedImageView)rootView.findViewById(R.id.img_user_avatar);
+            userName = (FermatTextView)rootView.findViewById(R.id.username);
+            userEmail = (FermatTextView)rootView.findViewById(R.id.email);
+            setUpScreen(inflater);
+        } catch (CantGetActiveLoginIdentityException e) {
+            e.printStackTrace();
+        }
+        return rootView;
+    }
+
+    private void setUpScreen(LayoutInflater layoutInflater) throws CantGetActiveLoginIdentityException {
+        /**
+         * add navigation header
+         */
+        addNavigationHeader(FragmentsCommons.setUpHeaderScreen(layoutInflater, getActivity(), intraUserSubAppSession.getIntraUserModuleManager().getActiveIntraUserIdentity()));
+
+        /**
+         * Navigation view items
+         */
+        NavigationViewAdapter navigationViewAdapter = new NavigationViewAdapter(getActivity(), null);
+        setNavigationDrawer(navigationViewAdapter);
+    }
+
+}
