f30dde7514a96ecdb8fc47c6286dd4e4a69f4fa8
Guillermo Gutierrez <guillermo20@gmail.com>
2015-11-30 16:40:32 -0430

    arreglada logica para debitar de la cuenta en el withdraw transaction

--- BNK/plugin/bank_money_transaction/fermat-bnk-plugin-bank-money-transaction-withdraw-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/bank_money_transaction/withdraw/developer/bitdubai/version_1/structure/WithdrawBankMoneyTransactionManager.java
+++ BNK/plugin/bank_money_transaction/fermat-bnk-plugin-bank-money-transaction-withdraw-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/bank_money_transaction/withdraw/developer/bitdubai/version_1/structure/WithdrawBankMoneyTransactionManager.java
@@ -1,13 +1,16 @@
 package com.bitdubai.fermat_bnk_plugin.layer.bank_money_transaction.withdraw.developer.bitdubai.version_1.structure;
 
 import com.bitdubai.fermat_api.CantStartPluginException;
+import com.bitdubai.fermat_api.FermatException;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_bnk_api.all_definition.bank_money_transaction.BankTransaction;
 import com.bitdubai.fermat_bnk_api.all_definition.bank_money_transaction.BankTransactionParameters;
 import com.bitdubai.fermat_bnk_api.layer.bnk_bank_money_transaction.withdraw.exceptions.CantMakeWithdrawTransactionException;
 import com.bitdubai.fermat_bnk_api.layer.bnk_bank_money_transaction.withdraw.interfaces.WithdrawManager;
+import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.exceptions.CantCalculateBalanceException;
 import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.exceptions.CantRegisterCreditException;
+import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.exceptions.CantRegisterDebitException;
 import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.interfaces.BankMoneyWallet;
 import com.bitdubai.fermat_bnk_plugin.layer.bank_money_transaction.withdraw.developer.bitdubai.version_1.database.WithdrawBankMoneyTransactionDao;
 import com.bitdubai.fermat_bnk_plugin.layer.bank_money_transaction.withdraw.developer.bitdubai.version_1.exceptions.CantInitializeWithdrawBankMoneyTransactionDatabaseException;
@@ -52,16 +55,19 @@ public class WithdrawBankMoneyTransactionManager implements WithdrawManager {
     public BankTransaction makeWithdraw(BankTransactionParameters parameters) throws CantMakeWithdrawTransactionException {
         withdrawBankMoneyTransactionDao.registerWithdrawTransaction(parameters);
         try{
-            bankMoneyWallet.getAvailableBalance().credit(bankMoneyTransactionRecord);
-            bankMoneyWallet.getBookBalance().credit(bankMoneyTransactionRecord);
-        }catch (CantRegisterCreditException e){
-            throw new CantMakeWithdrawTransactionException(CantRegisterCreditException.DEFAULT_MESSAGE,e,null,null);
+            if(parameters.getAmount()<bankMoneyWallet.getAvailableBalance().getBalance(parameters.getAccount())&& parameters.getAmount()<bankMoneyWallet.getBookBalance().getBalance(parameters.getAccount())){
+                bankMoneyWallet.getAvailableBalance().debit(bankMoneyTransactionRecord);
+                bankMoneyWallet.getBookBalance().debit(bankMoneyTransactionRecord);
+            }
+            else{
+                throw new CantMakeWithdrawTransactionException(CantMakeWithdrawTransactionException.DEFAULT_MESSAGE + " no posee suficiente fondos para realizar esta transaccion",null,"no posee suficiente fondos para realizar esta transaccion",null);
+            }
+        }catch (CantRegisterDebitException |CantCalculateBalanceException e){
+            throw new CantMakeWithdrawTransactionException(CantRegisterDebitException.DEFAULT_MESSAGE,e,null,null);
         }
         //TODO: retornar objeto de bank transaction
         return null;
     }
-
-
     public void setBankMoneyWallet(BankMoneyWallet bankMoneyWallet) {
         this.bankMoneyWallet = bankMoneyWallet;
     }
