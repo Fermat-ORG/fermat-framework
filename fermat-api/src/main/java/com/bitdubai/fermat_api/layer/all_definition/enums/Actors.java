4b3db51374493cbbd9a35c37fb4f766a4fe89989
pmgesualdi <pmgesualdi@hotmail.com>
2015-11-30 17:29:20 -0300

    Formatted Actors Enum. Changed Genders Enum Comment.

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/enums/Actors.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/enums/Actors.java
@@ -3,56 +3,67 @@ package com.bitdubai.fermat_api.layer.all_definition.enums;
 import com.bitdubai.fermat_api.layer.all_definition.enums.interfaces.FermatEnum;
 import com.bitdubai.fermat_api.layer.all_definition.exceptions.InvalidParameterException;
 
+/**
+ * The enum class <code>com.bitdubai.fermat_api.layer.all_definition.enums</code>
+ * Lists all the Actors available on fermat.
+ * <p/>
+ * Updated by pmgesualdi - (pmgesualdi@hotmail.com) on 30/11/2015.
+ */
+
 public enum Actors implements FermatEnum {
+    /**
+     * To make the code more readable, please keep the elements in the Enum sorted alphabetically.
+     */
+    DEVICE_USER             ("DUS"),
+    EXTRA_USER              ("EUS"),
+    INTRA_USER              ("IUS"),
+    SHOP                    ("SHP"),
 
-    DEVICE_USER("DUS"),
-    INTRA_USER("IUS"),
-    EXTRA_USER("EUS"),
-    SHOP("SHP"),
+    CBP_CRYPTO_BROKER       ("CBPCRBR"),
+    CBP_CRYPTO_CUSTOMER     ("CBPCRCU"),
 
-    DAP_ASSET_ISSUER("DAPASIS"),
-    DAP_ASSET_USER("DAPASUS"),
-    DAP_ASSET_REDEEM_POINT("DAPARP"),
+    CCM_INTRA_WALLET_USER   ("CCMIU"),
+    CCP_INTRA_WALLET_USER   ("CCPIU"),
 
-    CCP_INTRA_WALLET_USER("CCPIU"),
-    CCM_INTRA_WALLET_USER("CCMIU"),
-
-    CBP_CRYPTO_BROKER   ("CBPCRBR"),
-    CBP_CRYPTO_CUSTOMER ("CBPCRCU"),
+    DAP_ASSET_ISSUER        ("DAPASIS"),
+    DAP_ASSET_REDEEM_POINT  ("DAPARP"),
+    DAP_ASSET_USER          ("DAPASUS"),
 
     ;
 
-    private String code;
+    private final String code;
 
-    Actors(String code) {
+    Actors(final String code) {
         this.code = code;
     }
 
-    public String getCode() {
-        return this.code;
-    }
-
-    public static Actors getByCode(String code)/*throws InvalidParameterException*/ {
+    public static Actors getByCode(String code) throws IllegalArgumentException {
 
         switch (code) {
             case "DUS":     return DEVICE_USER;
-            case "IUS":     return INTRA_USER;
             case "EUS":     return EXTRA_USER;
+            case "IUS":     return INTRA_USER;
             case "SHP":     return SHOP;
-
-            case "DAPASIS": return DAP_ASSET_ISSUER;
-            case "DAPASUS": return DAP_ASSET_USER;
-            case "DAPARP":  return DAP_ASSET_REDEEM_POINT;
-
-            case "CCPIU":   return CCP_INTRA_WALLET_USER;
-            case "CCMIU":   return CCM_INTRA_WALLET_USER;
 
             case "CBPCRBR": return CBP_CRYPTO_BROKER;
             case "CBPCRCU": return CBP_CRYPTO_CUSTOMER;
 
-            default:
-                throw new IllegalArgumentException("The code"+code+" is not valid for the Actors enum.");
+            case "CCMIU":   return CCM_INTRA_WALLET_USER;
+            case "CCPIU":   return CCP_INTRA_WALLET_USER;
 
+            case "DAPASIS": return DAP_ASSET_ISSUER;
+            case "DAPARP":  return DAP_ASSET_REDEEM_POINT;
+            case "DAPASUS": return DAP_ASSET_USER;
+
+            default:
+                throw new IllegalArgumentException(
+                        "The code"+code+" is not valid for the Actors enum."
+                );
         }
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
     }
 }
