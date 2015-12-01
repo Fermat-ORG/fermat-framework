c0686800d6354579aa04b30429f495c79f69a9a1
Manuel <darkpriestrelative@gmail.com>
2015-11-30 21:44:02 -0430

    Create Event Recorder service  #2976

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/exceptions/CantStartServiceException.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/exceptions/CantStartServiceException.java
@@ -1,7 +1,73 @@
 package com.bitdubai.fermat_cbp_api.all_definition.exceptions;
 
+import com.bitdubai.fermat_api.layer.CBPException;
+
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 30/11/15.
  */
-public class CantStartServiceException {
+public class CantStartServiceException extends CBPException {
+
+    /**
+     * Represent the default message
+     */
+    public static final String DEFAULT_MESSAGE = "CANNOT SET AN OBJECT";
+
+    /**
+     * Constructor with parameters
+     *
+     * @param message
+     * @param cause
+     * @param context
+     * @param possibleReason
+     */
+    public CantStartServiceException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    /**
+     * Constructor with parameters
+     *
+     * @param cause
+     * @param context
+     * @param possibleReason
+     */
+    public CantStartServiceException(Exception cause, String context, String possibleReason) {
+        super(DEFAULT_MESSAGE , cause, context, possibleReason);
+    }
+
+    /**
+     * Constructor with parameters
+     *
+     * @param message
+     * @param cause
+     */
+    public CantStartServiceException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    /**
+     * Constructor with parameter
+     *
+     * @param message
+     */
+    public CantStartServiceException(final String message) {
+        this(message, null);
+    }
+
+    /**
+     * Constructor with parameter
+     *
+     * @param exception
+     */
+    public CantStartServiceException(final Exception exception) {
+        this(exception.getMessage());
+        setStackTrace(exception.getStackTrace());
+    }
+
+    /**
+     * Constructor
+     */
+    public CantStartServiceException() {
+        this(DEFAULT_MESSAGE);
+    }
 }
--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/event_handler/OpenContractRecorderService.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/event_handler/OpenContractRecorderService.java
@@ -1,7 +1,106 @@
 package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.open_contract.developer.bitdubai.version_1.event_handler;
 
+import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventHandler;
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventListener;
+import com.bitdubai.fermat_cbp_api.all_definition.events.CBPService;
+import com.bitdubai.fermat_cbp_api.all_definition.events.enums.EventType;
+import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantSaveEventException;
+import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantSetObjectException;
+import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantStartServiceException;
+import com.bitdubai.fermat_cbp_api.layer.network_service.TransactionTransmission.events.IncomingBusinessTransactionContractHash;
+import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.open_contract.developer.bitdubai.version_1.database.OpenContractBusinessTransactionDao;
+import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 30/11/15.
  */
-public class OpenContractRecorderService {
+public class OpenContractRecorderService implements CBPService {
+    /**
+     * DealsWithEvents Interface member variables.
+     */
+    private EventManager eventManager;
+    private List<FermatEventListener> listenersAdded = new ArrayList<>();
+    OpenContractBusinessTransactionDao openContractBusinessTransactionDao;
+    /**
+     * TransactionService Interface member variables.
+     */
+    private ServiceStatus serviceStatus = ServiceStatus.CREATED;
+
+    public OpenContractRecorderService(
+            OpenContractBusinessTransactionDao openContractBusinessTransactionDao,
+                                       EventManager eventManager) throws CantStartServiceException {
+        try {
+            setDatabaseDao(openContractBusinessTransactionDao);
+            setEventManager(eventManager);
+        } catch (CantSetObjectException exception) {
+            throw new CantStartServiceException(exception,
+                    "Cannot set the asset distribution database handler",
+                    "The database handler is null");
+        }
+    }
+
+    private void setDatabaseDao(OpenContractBusinessTransactionDao openContractBusinessTransactionDao)
+            throws CantSetObjectException {
+        if(openContractBusinessTransactionDao==null){
+            throw new CantSetObjectException("The OpenContractBusinessTransactionDao is null");
+        }
+        this.openContractBusinessTransactionDao=openContractBusinessTransactionDao;
+    }
+
+    public void setEventManager(EventManager eventManager) {
+        this.eventManager = eventManager;
+    }
+
+   public void incomingBusinessTransactionContractHashEventHandler(IncomingBusinessTransactionContractHash event) throws CantSaveEventException {
+        //Logger LOG = Logger.getGlobal();
+        //LOG.info("EVENT TEST, I GOT AN EVENT:\n"+event);
+        this.openContractBusinessTransactionDao.saveNewEvent(event.getEventType().getCode(), event.getSource().getCode());
+        //LOG.info("CHECK THE DATABASE");
+    }
+
+
+    @Override
+    public void start() throws CantStartServiceException {
+//TODO: finish this
+        try {
+            /**
+             * I will initialize the handling of com.bitdubai.platform events.
+             */
+            FermatEventListener fermatEventListener;
+            FermatEventHandler fermatEventHandler;
+            fermatEventListener = eventManager.getNewListener(EventType.INCOMING_BUSINESS_TRANSACTION_CONTRACT_HASH);
+            fermatEventHandler = new IncomingBusinessTransactionContractHashEventHandler();
+            ((IncomingBusinessTransactionContractHashEventHandler) fermatEventHandler).setOpenContractRecorderService(this);
+            fermatEventListener.setEventHandler(fermatEventHandler);
+            eventManager.addListener(fermatEventListener);
+            listenersAdded.add(fermatEventListener);
+
+            this.serviceStatus = ServiceStatus.STARTED;
+        } catch (CantSetObjectException exception){
+            throw new CantStartServiceException(exception,"Starting the AssetDistributionRecorderService", "The AssetDistributionRecorderService is probably null");
+        }
+
+    }
+
+    @Override
+    public void stop() {
+        removeRegisteredListeners();
+        this.serviceStatus = ServiceStatus.STOPPED;
+    }
+
+    private void removeRegisteredListeners(){
+        for (FermatEventListener fermatEventListener : listenersAdded) {
+            eventManager.removeListener(fermatEventListener);
+        }
+        listenersAdded.clear();
+    }
+
+    @Override
+    public ServiceStatus getStatus() {
+        return this.serviceStatus;
+    }
 }
