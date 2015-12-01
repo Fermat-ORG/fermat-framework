1d99881e8f2a586b3d6e4ead06225d73af72417a
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-11-30 23:47:24 -0430

    CBP - wallet - Crypto Broker - Implementation - Plug-in Root #3483

--- CBP/plugin/wallet/fermat-cbp-plugin-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet/crypto_broker/developer/bitdubai/version_1/CryptoBrokerWalletPluginRoot.java
+++ CBP/plugin/wallet/fermat-cbp-plugin-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet/crypto_broker/developer/bitdubai/version_1/CryptoBrokerWalletPluginRoot.java
@@ -3,7 +3,10 @@ package com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitd
 import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.Plugin;
 import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.AsymmetricCryptography;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DatabaseManagerForDevelopers;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabase;
@@ -11,12 +14,23 @@ import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseT
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
 import com.bitdubai.fermat_api.layer.all_definition.developer.LogManagerForDevelopers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.util.Version;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DealsWithPluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.DealsWithPluginFileSystem;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.FilePrivacy;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginTextFile;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantCreateFileException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantLoadFileException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.CantPersistFileException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.exceptions.FileNotFoundException;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.DealsWithLogger;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
@@ -43,166 +57,78 @@ import java.util.UUID;
 
 /**
  * Created by Yordin Alayn on 19.10.15.
+ * Modified by Franklin Marcano 30.11.2015
  */
-public class CryptoBrokerWalletPluginRoot implements CryptoBrokerWalletManager,
-        DatabaseManagerForDevelopers,
-        DealsWithPluginDatabaseSystem,
-        LogManagerForDevelopers,
-        DealsWithErrors,
-        DealsWithLogger,
-        DealsWithDeviceUser,
-        DealsWithPluginFileSystem,
-        Plugin,
-        Service{
+public class CryptoBrokerWalletPluginRoot extends AbstractPlugin implements
+        CryptoBrokerWalletManager//,
+        //DatabaseManagerForDevelopers
+        {
 
-    /*Variables.*/
-    private CryptoBrokerWalletDatabaseDao cryptoBrokerWalletDatabaseDao;
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_DATABASE_SYSTEM)
+    private PluginDatabaseSystem pluginDatabaseSystem;
 
-    private ErrorManager errorManager;
-
-    private LogManager logManager;
-
-    private DeviceUserManager deviceUserManager;
-
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_FILE_SYSTEM)
     private PluginFileSystem pluginFileSystem;
 
-    private static Map<String, LogLevel> newLoggingLevel = new HashMap<>();
-
-    private UUID pluginId;
-
-    private ServiceStatus serviceStatus = ServiceStatus.CREATED;
-
-    private PluginDatabaseSystem pluginDatabaseSystem;
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM   , layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER         )
+    private ErrorManager errorManager;
 
     public static final String CRYPTO_BROKER_WALLET_PRIVATE_KEYS_FILE_NAME = "cryptoBrokerWalletPrivateKeyWallet";
 
-    /*CryptoBrokerMAnager Interface Implementation */
-    @Override
-    public CryptoBrokerWallet createNewCryptoBrokerWallet(String cryptoBroker) throws CantCreateCryptoBrokerWalletException {
-        try {
-            CryptoBrokerWalletImpl CryptoBrokerWallet = new CryptoBrokerWalletImpl(AsymmetricCryptography.generateECCKeyPair(), cryptoBroker, cryptoBrokerWalletDatabaseDao);
-            CryptoBrokerWallet.createWallet();
-            return CryptoBrokerWallet;
-        } catch (Exception e) {
-            throw new CantCreateCryptoBrokerWalletException("CRYPTO BROKER WALLET", e, "CAN'T CREATE NEW CRYPTO BROKER WALLET", "");
-        }
+    private static final String CRYPTO_BROKER_WALLET_FILE_NAME = "walletsIds";
+    private Map<String, UUID> cryptoBrokerWallet = new HashMap<>();
+    boolean existWallet = false;
+    String walletPublicKey = "walletPublicKeyTest";
+
+
+    public CryptoBrokerWalletPluginRoot() {
+        super(new PluginVersionReference(new Version()));
     }
 
-    @Override
-    public CryptoBrokerWallet getCryptoBrokerWallet(String cryptoBroker) throws CryptoBrokerWalletNotFoundException {
-        try {
-            CryptoBrokerWalletImpl CryptoBrokerWallet = new CryptoBrokerWalletImpl(AsymmetricCryptography.generateECCKeyPair(), cryptoBroker, cryptoBrokerWalletDatabaseDao);
-            CryptoBrokerWallet.getWallet();
-            return CryptoBrokerWallet;
-        } catch (CantGetCryptoBrokerWalletImplException e) {
-            throw new CryptoBrokerWalletNotFoundException("CAN'T GET CRYPTO BROKER IDENTITY", e, "Error getting Crypto Broker Wallet", "");
-        } catch (Exception e) {
-            throw new CryptoBrokerWalletNotFoundException("CAN'T CREATE NEW CRYPTO BROKER IDENTITY", e, "Error getting current logged in device user", "");
-        }
-    }
+//    @Override
+//    public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
+////        CryptoBrokerWalletDeveloperDatabaseFactory dbFactory = new CryptoBrokerWalletDeveloperDatabaseFactory(this.pluginDatabaseSystem, this.pluginId);
+////        return dbFactory.getDatabaseList(developerObjectFactory);
+//        return null;
+//    }
+//
+//    @Override
+//    public List<DeveloperDatabaseTable> getDatabaseTableList(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase) {
+////        CryptoBrokerWalletDeveloperDatabaseFactory dbFactory = new CryptoBrokerWalletDeveloperDatabaseFactory(this.pluginDatabaseSystem, this.pluginId);
+////        return dbFactory.getDatabaseTableList(developerObjectFactory);
+//        return null;
+//    }
+//
+//    @Override
+//    public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
+////        try {
+////            CryptoBrokerWalletDeveloperDatabaseFactory dbFactory = new CryptoBrokerWalletDeveloperDatabaseFactory(this.pluginDatabaseSystem, this.pluginId);
+////            dbFactory.initializeDatabase();
+////            return dbFactory.getDatabaseTableContent(developerObjectFactory, developerDatabaseTable);
+////        } catch (CantInitializeCryptoBrokerWalletDatabaseException e) {
+////            this.errorManager.reportUnexpectedPluginException(Plugins.CRYPTO_BROKER_IDENTITY, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, e);
+////        }
+////        return new ArrayList<>();
+//        return null;
+//    }
 
-    /*DatabaseManagerForDevelopers Interface Implementation.*/
-    @Override
-    public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
-        CryptoBrokerWalletDeveloperDatabaseFactory dbFactory = new CryptoBrokerWalletDeveloperDatabaseFactory(this.pluginDatabaseSystem, this.pluginId);
-        return dbFactory.getDatabaseList(developerObjectFactory);
-    }
-
-    @Override
-    public List<DeveloperDatabaseTable> getDatabaseTableList(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase) {
-        CryptoBrokerWalletDeveloperDatabaseFactory dbFactory = new CryptoBrokerWalletDeveloperDatabaseFactory(this.pluginDatabaseSystem, this.pluginId);
-        return dbFactory.getDatabaseTableList(developerObjectFactory);
-    }
-
-    @Override
-    public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
-        try {
-            CryptoBrokerWalletDeveloperDatabaseFactory dbFactory = new CryptoBrokerWalletDeveloperDatabaseFactory(this.pluginDatabaseSystem, this.pluginId);
-            dbFactory.initializeDatabase();
-            return dbFactory.getDatabaseTableContent(developerObjectFactory, developerDatabaseTable);
-        } catch (CantInitializeCryptoBrokerWalletDatabaseException e) {
-            this.errorManager.reportUnexpectedPluginException(Plugins.CRYPTO_BROKER_IDENTITY, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, e);
-        }
-        return new ArrayList<>();
-    }
-
-    /*DealsWithPluginDatabaseSystem interface Implementation.*/
-    @Override
-    public void setPluginDatabaseSystem(PluginDatabaseSystem pluginDatabaseSystem) {
-        this.pluginDatabaseSystem = pluginDatabaseSystem;
-
-    }
-
-    /*LogManagerForDevelopers Interface Implementation.*/
-    @Override
-    public List<String> getClassesFullPath() {
-        List<String> returnedClasses = new ArrayList<String>();
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.CryptoBrokerWalletPluginRoot");
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.structure.CryptoBrokerStockTransactionRecordImpl");
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.database.CryptoBrokerWalletDatabaseDao");
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.database.CryptoBrokerWalletDatabaseFactory");
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.wallet.crypto_broker.developer.bitdubai.version_1.database.CryptoBrokerWalletDatabaseConstants");
-        return returnedClasses;
-    }
-
-    @Override
-    public void setLoggingLevelPerClass(Map<String, LogLevel> newLoggingLevel) {
-        for (Map.Entry<String, LogLevel> pluginPair : newLoggingLevel.entrySet()) {
-            if (CryptoBrokerWalletPluginRoot.newLoggingLevel.containsKey(pluginPair.getKey())) {
-                CryptoBrokerWalletPluginRoot.newLoggingLevel.remove(pluginPair.getKey());
-                CryptoBrokerWalletPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
-            } else {
-                CryptoBrokerWalletPluginRoot.newLoggingLevel.put(pluginPair.getKey(), pluginPair.getValue());
-            }
-        }
-    }
-
-    /*DealWithErrors Interface Implementation.*/
-    @Override
-    public void setErrorManager(ErrorManager errorManager) {
-        this.errorManager = errorManager;
-    }
-
-    /*DealsWithLogger Interface Implementation.*/
-    @Override
-    public void setLogManager(LogManager logManager) {
-        this.logManager = logManager;
-    }
-
-    /*DealsWithDeviceUser Interface Implementation.*/
-    @Override
-    public void setDeviceUserManager(DeviceUserManager deviceUserManager) {
-        this.deviceUserManager = deviceUserManager;
-    }
-
-    /*DealWithPluginFileSystem Interface Implementation.*/
-    @Override
-    public void setPluginFileSystem(PluginFileSystem pluginFileSystem) {
-        this.pluginFileSystem = pluginFileSystem;
-    }
-
-    /*PlugIn Interface Implementation.*/
-    @Override
-    public void setId(UUID pluginId) {
-        this.pluginId = pluginId;
-    }
-
-    @Override
-    public FermatManager getManager() {
-        return null;
-    }
-
-    /*Service Interface Implementation.*/
     @Override
     public void start() throws CantStartPluginException {
         this.serviceStatus = ServiceStatus.STARTED;
-        try {
-            this.cryptoBrokerWalletDatabaseDao = new CryptoBrokerWalletDatabaseDao(pluginDatabaseSystem, this.pluginFileSystem, this.pluginId);
-            this.cryptoBrokerWalletDatabaseDao.initialize();
-        } catch (CantInitializeCryptoBrokerWalletDatabaseException cantInitializeExtraUserRegistryException) {
-            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_DESIGNER_IDENTITY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, cantInitializeExtraUserRegistryException);
-            throw new CantStartPluginException(cantInitializeExtraUserRegistryException, Plugins.BITDUBAI_DESIGNER_IDENTITY);
-        }
+        System.out.println("Init Crypto Broker Wallet");
+//        try{
+//
+//        }catch(CantStartPluginException exception){
+//            errorManager.reportUnexpectedPluginException(Plugins.CRYPTO_BROKER_WALLET, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, exception);
+//            throw exception;
+//        }
+//        try {
+//            this.cryptoBrokerWalletDatabaseDao = new CryptoBrokerWalletDatabaseDao(pluginDatabaseSystem, this.pluginFileSystem, this.pluginId);
+//            this.cryptoBrokerWalletDatabaseDao.initialize();
+//        } catch (CantInitializeCryptoBrokerWalletDatabaseException cantInitializeExtraUserRegistryException) {
+//            errorManager.reportUnexpectedPluginException(Plugins.BITDUBAI_DESIGNER_IDENTITY, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, cantInitializeExtraUserRegistryException);
+//            throw new CantStartPluginException(cantInitializeExtraUserRegistryException, Plugins.BITDUBAI_DESIGNER_IDENTITY);
+//        }
 
     }
 
@@ -222,7 +148,47 @@ public class CryptoBrokerWalletPluginRoot implements CryptoBrokerWalletManager,
     }
 
     @Override
-    public ServiceStatus getStatus() {
-        return serviceStatus;
+    public CryptoBrokerWallet createCryptoBrokerWallet(String walletPublicKey) throws CantCreateCryptoBrokerWalletException {
+        return null;
+    }
+
+    @Override
+    public CryptoBrokerWallet loadCryptoBrokerWallet(String walletPublicKey) throws CryptoBrokerWalletNotFoundException {
+        return null;
+    }
+
+    private boolean loadWalletIssuerMap() throws CantStartPluginException {
+        PluginTextFile walletFile = getWalletFile();
+        String[] stringWallet = walletFile.getContent().split(";", -1);
+
+        for (String stringWalletId : stringWallet)
+            if (!stringWalletId.equals("")) {
+                String[] idPair = stringWalletId.split(",", -1);
+                cryptoBrokerWallet.put(idPair[0], UUID.fromString(idPair[1]));
+                existWallet = true;
+            }
+        return existWallet;
+    }
+
+    private PluginTextFile getWalletFile() throws CantStartPluginException {
+        try {
+            PluginTextFile walletFile = pluginFileSystem.getTextFile(pluginId, "", CRYPTO_BROKER_WALLET_FILE_NAME, FilePrivacy.PRIVATE, FileLifeSpan.PERMANENT);
+            walletFile.loadFromMedia();
+            return walletFile;
+        } catch (FileNotFoundException | CantCreateFileException exception) {
+            return createWalletFile();
+        } catch (CantLoadFileException exception) {
+            throw new CantStartPluginException(CantStartPluginException.DEFAULT_MESSAGE, exception, null, null);
+        }
+    }
+
+    private PluginTextFile createWalletFile() throws CantStartPluginException {
+        try {
+            PluginTextFile walletFile = pluginFileSystem.createTextFile(pluginId, "", CRYPTO_BROKER_WALLET_FILE_NAME, FilePrivacy.PRIVATE, FileLifeSpan.PERMANENT);
+            walletFile.persistToMedia();
+            return walletFile;
+        } catch (CantCreateFileException | CantPersistFileException exception) {
+            throw new CantStartPluginException(CantStartPluginException.DEFAULT_MESSAGE, exception, null, null);
+        }
     }
 }
