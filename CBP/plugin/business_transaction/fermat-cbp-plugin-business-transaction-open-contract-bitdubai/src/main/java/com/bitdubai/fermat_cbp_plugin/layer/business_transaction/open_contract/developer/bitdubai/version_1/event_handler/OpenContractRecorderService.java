ff1572281d748d8c6b7fceeabbe78588c0d25e9b
Manuel <darkpriestrelative@gmail.com>
2015-12-01 11:09:04 -0430

    Created IncomingNewContractStatusUpdateEventHandler

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/event_handler/OpenContractRecorderService.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/event_handler/OpenContractRecorderService.java
@@ -10,6 +10,7 @@ import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantSetObjectExcept
 import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantStartServiceException;
 import com.bitdubai.fermat_cbp_api.layer.network_service.TransactionTransmission.events.IncomingBusinessTransactionContractHash;
 import com.bitdubai.fermat_cbp_api.layer.network_service.TransactionTransmission.events.IncomingConfirmBusinessTransactionContract;
+import com.bitdubai.fermat_cbp_api.layer.network_service.TransactionTransmission.events.IncomingNewContractStatusUpdate;
 import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.open_contract.developer.bitdubai.version_1.database.OpenContractBusinessTransactionDao;
 import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
 
@@ -71,6 +72,13 @@ public class OpenContractRecorderService implements CBPService {
         //LOG.info("CHECK THE DATABASE");
     }
 
+    public void incomingNewContractStatusUpdateEventHandler(IncomingNewContractStatusUpdate event) throws CantSaveEventException {
+        //Logger LOG = Logger.getGlobal();
+        //LOG.info("EVENT TEST, I GOT AN EVENT:\n"+event);
+        this.openContractBusinessTransactionDao.saveNewEvent(event.getEventType().getCode(), event.getSource().getCode());
+        //LOG.info("CHECK THE DATABASE");
+    }
+
     @Override
     public void start() throws CantStartServiceException {
 //TODO: finish this
@@ -90,6 +98,13 @@ public class OpenContractRecorderService implements CBPService {
             fermatEventListener = eventManager.getNewListener(EventType.INCOMING_CONFIRM_BUSINESS_TRANSACTION_CONTRACT);
             fermatEventHandler = new IncomingConfirmBusinessTransactionContractEventHandler();
             ((IncomingConfirmBusinessTransactionContractEventHandler) fermatEventHandler).setOpenContractRecorderService(this);
+            fermatEventListener.setEventHandler(fermatEventHandler);
+            eventManager.addListener(fermatEventListener);
+            listenersAdded.add(fermatEventListener);
+
+            fermatEventListener = eventManager.getNewListener(EventType.INCOMING_NEW_CONTRACT_STATUS_UPDATE);
+            fermatEventHandler = new IncomingNewContractStatusUpdateEventHandler();
+            ((IncomingNewContractStatusUpdateEventHandler) fermatEventHandler).setOpenContractRecorderService(this);
             fermatEventListener.setEventHandler(fermatEventHandler);
             eventManager.addListener(fermatEventListener);
             listenersAdded.add(fermatEventListener);
