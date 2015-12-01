5a66136a99cead10346c5e178ab57f80fce5222d
josemanueldsds <josemanueldsds@gmail.com>
2015-11-30 22:46:29 -0430

    Create class friend list

---
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/ConnectionFriendListFragment.java
@@ -0,0 +1,19 @@
+package com.bitdubai.sub_app.intra_user_community.fragments;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
+
+/**
+ * Created by josemanueldsds on 30/11/15.
+ */
+public class ConnectionFriendListFragment extends FermatFragment {
+
+
+    /**
+     * Create a new instance of this fragment
+     *
+     * @return InstalledFragment instance object
+     */
+    public static ConnectionFriendListFragment newInstance() {
+        return new ConnectionFriendListFragment();
+    }
+}
