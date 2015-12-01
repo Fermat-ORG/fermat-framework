0d0474c659277525d8ea97443f9f0e6c3bdc659e
josemanueldsds <josemanueldsds@gmail.com>
2015-12-01 00:59:03 -0430

    update other profile fragment

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/ConnectionOtherProfileFragment.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/ConnectionOtherProfileFragment.java
@@ -10,6 +10,8 @@ import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatRoundedImageView;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.exceptions.CantGetActiveLoginIdentityException;
+import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserModuleManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
 import com.bitdubai.sub_app.intra_user_community.R;
 import com.bitdubai.sub_app.intra_user_community.common.navigation_drawer.NavigationViewAdapter;
 import com.bitdubai.sub_app.intra_user_community.common.utils.FragmentsCommons;
@@ -25,6 +27,8 @@ public class ConnectionOtherProfileFragment extends FermatFragment {
     private FermatRoundedImageView userProfileAvatar;
     private FermatTextView userName;
     private FermatTextView userEmail;
+    private IntraUserModuleManager moduleManager;
+    private ErrorManager errorManager;
 
     /**
      * Create a new instance of this fragment
@@ -33,6 +37,16 @@ public class ConnectionOtherProfileFragment extends FermatFragment {
      */
     public static ConnectionOtherProfileFragment newInstance() {
         return new ConnectionOtherProfileFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setting up  module
+        intraUserSubAppSession = ((IntraUserSubAppSession) subAppsSession);
+        moduleManager = intraUserSubAppSession.getModuleManager();
+        errorManager = subAppsSession.getErrorManager();
     }
 
     @Nullable
