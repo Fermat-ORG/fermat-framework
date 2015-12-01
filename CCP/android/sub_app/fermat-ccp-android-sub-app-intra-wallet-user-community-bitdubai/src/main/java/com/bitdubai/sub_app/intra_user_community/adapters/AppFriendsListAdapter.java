831ee4932739e231fa29103c3be331dba1074a8b
josemanueldsds <josemanueldsds@gmail.com>
2015-12-01 00:58:03 -0430

    add friend list adapter

---
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/adapters/AppFriendsListAdapter.java
@@ -0,0 +1,46 @@
+package com.bitdubai.sub_app.intra_user_community.adapters;
+
+import android.content.Context;
+import android.view.View;
+
+import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserModuleManager;
+import com.bitdubai.sub_app.intra_user_community.R;
+import com.bitdubai.sub_app.intra_user_community.holders.AppFriendsListHolder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.util.List;
+
+/**
+ * Created by josemanueldsds on 30/11/15.
+ */
+public class AppFriendsListAdapter extends FermatAdapter<IntraUserModuleManager, AppFriendsListHolder> {
+
+    public AppFriendsListAdapter(Context context, List<IntraUserModuleManager> dataSet) {
+        super(context, dataSet);
+    }
+
+    @Override
+    protected AppFriendsListHolder createHolder(View itemView, int type) {
+        return null;
+    }
+
+    @Override
+    protected int getCardViewResource() {
+        return R.layout.intra_user_row_friend_list;
+    }
+
+    @Override
+    protected void bindHolder(AppFriendsListHolder holder, IntraUserModuleManager data, int position) {
+        try {
+            holder.friendName.setText("Jeniffer");
+            Picasso.with(context)
+                    .load("http://www.garuyo.com/sites/default/files/jennifer-lawrence-signo-del-zodiaco-leo-1.jpg")
+                    .placeholder(R.drawable.profile_image_round)
+                    .into((Target) holder.friendAvatar);
+        } catch (Exception ex) {
+
+        }
+    }
+}
