d572232b76d85592c8b33cbdfbca107566d9dba6
Nelson Ramirez <nelsonalfo@gmail.com>
2015-12-02 23:39:49 -0430

    actualizando la estructura de navegacion de las wallets

---
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/common/navigationDrawer/BrokerNavigationViewPainter.java
@@ -0,0 +1,67 @@
+package com.bitdubai.reference_wallet.crypto_broker_wallet.common.navigationDrawer;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_ccp_api.layer.module.intra_user.exceptions.CantGetActiveLoginIdentityException;
+import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserLoginIdentity;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.util.FragmentsCommons;
+
+/**
+ * Created by mati on 2015.11.24..
+ */
+public class BrokerNavigationViewPainter implements com.bitdubai.fermat_android_api.engine.NavigationViewPainter {
+
+    private final IntraUserLoginIdentity intraUserLoginIdentity;
+    private Activity activity;
+
+    public BrokerNavigationViewPainter(Activity activity, IntraUserLoginIdentity intraUserLoginIdentity) {
+        this.activity = activity;
+        this.intraUserLoginIdentity = intraUserLoginIdentity;
+    }
+
+    @Override
+    public View addNavigationViewHeader() {
+        try {
+            return FragmentsCommons.setUpHeaderScreen(activity.getLayoutInflater(), activity, intraUserLoginIdentity);
+        } catch (CantGetActiveLoginIdentityException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public FermatAdapter addNavigationViewAdapter() {
+        try {
+            NavigationViewAdapter navigationViewAdapter = new NavigationViewAdapter(activity);
+            //setNavigationDrawer(navigationViewAdapter);
+            return navigationViewAdapter;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public ViewGroup addNavigationViewBodyContainer(LayoutInflater layoutInflater, ViewGroup base) {
+        RelativeLayout relativeLayout = (RelativeLayout) layoutInflater.inflate(R.layout.navigation_view_bottom, base, true);
+        return relativeLayout;
+    }
+
+    @Override
+    public Drawable addBodyBackground() {
+        return ContextCompat.getDrawable(activity.getApplicationContext(), R.drawable.bg_drawer_body);
+    }
+
+    @Override
+    public int addBodyBackgroundColor() {
+        return 0;
+    }
+}
--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/OpenNegotiationDetailsFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/OpenNegotiationDetailsFragment.java
@@ -3,27 +3,57 @@ package com.bitdubai.reference_wallet.crypto_broker_wallet.fragments.common;
 
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatWalletFragment;
+import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Wallets;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_broker.interfaces.CryptoBrokerWallet;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_broker.interfaces.CryptoBrokerWalletModuleManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.ErrorManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.UnexpectedWalletExceptionSeverity;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.session.CryptoBrokerWalletSession;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.util.CommonLogger;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OpenNegotiationDetailsFragment extends FermatWalletFragment {
+    private static final String TAG = "OpenNegotiationDetails";
 
+    // Fermat Managers
+    private CryptoBrokerWalletModuleManager moduleManager;
+    private CryptoBrokerWallet walletManager;
+    private ErrorManager errorManager;
 
-    public OpenNegotiationDetailsFragment() {
-        // Required empty public constructor
-    }
 
     public static OpenNegotiationDetailsFragment newInstance() {
         return new OpenNegotiationDetailsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            moduleManager = ((CryptoBrokerWalletSession) walletSession).getModuleManager();
+            walletManager = moduleManager.getCryptoBrokerWallet(walletSession.getAppPublicKey());
+
+            errorManager = walletSession.getErrorManager();
+        } catch (Exception ex) {
+            CommonLogger.exception(TAG, ex.getMessage(), ex);
+            if (errorManager != null)
+                errorManager.reportUnexpectedWalletException(Wallets.CBP_CRYPTO_BROKER_WALLET,
+                        UnexpectedWalletExceptionSeverity.DISABLES_THIS_FRAGMENT, ex);
+        }
+
     }
 
     @Override
@@ -31,6 +61,14 @@ public class OpenNegotiationDetailsFragment extends FermatWalletFragment {
 
         View rootView = inflater.inflate(R.layout.cbw_fragment_negotiation_details_activity, container, false);
 
+        configureToolbar();
+
+        initViews(rootView);
+
+        return rootView;
+    }
+
+    private void initViews(View rootView) {
         final Button paymentDateValue = (Button) rootView.findViewById(R.id.payment_date_value);
         paymentDateValue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,9 +122,17 @@ public class OpenNegotiationDetailsFragment extends FermatWalletFragment {
                 timePicker.show(getFragmentManager(), "timePicker");
             }
         });
-
-        return rootView;
     }
 
+    private void configureToolbar() {
+        Toolbar toolbar = getToolbar();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            toolbar.setBackground(getResources().getDrawable(R.drawable.cbw_action_bar_gradient_colors, null));
+        else
+            toolbar.setBackground(getResources().getDrawable(R.drawable.cbw_action_bar_gradient_colors));
+
+        toolbar.setTitleTextColor(Color.WHITE);
+        if (toolbar.getMenu() != null) toolbar.getMenu().clear();
+    }
 }
