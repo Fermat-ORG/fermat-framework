266d4baf4653e774b44b80f58be436fd5c543376
Guillermo Gutierrez <guillermo20@gmail.com>
2015-12-03 15:13:04 -0430

    añadidos campos requeridos que usara nelson en interfaz grafica cuando consulte los datos de una cuenta en la Wallet.

--- BNK/library/api/fermat-bnk-api/src/main/java/com/bitdubai/fermat_bnk_api/layer/bnk_wallet/bank_money/interfaces/BankAccountNumber.java
+++ BNK/library/api/fermat-bnk-api/src/main/java/com/bitdubai/fermat_bnk_api/layer/bnk_wallet/bank_money/interfaces/BankAccountNumber.java
@@ -1,11 +1,16 @@
 package com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.interfaces;
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
+import com.bitdubai.fermat_bnk_api.all_definition.enums.BankAccountType;
 
 /**
  * Created by memo on 24/11/15.
  */
 public interface BankAccountNumber {
+
+    String getBankName();
+
+    BankAccountType getAccountType();
 
     String getAlias();
 
--- BNK/plugin/wallet/fermat-bnk-plugin-wallet-bank-money-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/wallet/bank_money/developer/bitdubai/version_1/structure/BankAccountNumberImpl.java
+++ BNK/plugin/wallet/fermat-bnk-plugin-wallet-bank-money-bitdubai/src/main/java/com/bitdubai/fermat_bnk_plugin/layer/wallet/bank_money/developer/bitdubai/version_1/structure/BankAccountNumberImpl.java
@@ -1,6 +1,7 @@
 package com.bitdubai.fermat_bnk_plugin.layer.wallet.bank_money.developer.bitdubai.version_1.structure;
 
 import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
+import com.bitdubai.fermat_bnk_api.all_definition.enums.BankAccountType;
 import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.interfaces.BankAccountNumber;
 
 /**
@@ -31,5 +32,17 @@ public class BankAccountNumberImpl implements BankAccountNumber {
     @Override
     public FiatCurrency getCurrencyType() {
         return currency;
+    }
+
+    //TODO: agregar bankName como setting y retornal e acountType;
+
+    @Override
+    public String getBankName() {
+        return null;
+    }
+
+    @Override
+    public BankAccountType getAccountType() {
+        return null;
     }
 }
