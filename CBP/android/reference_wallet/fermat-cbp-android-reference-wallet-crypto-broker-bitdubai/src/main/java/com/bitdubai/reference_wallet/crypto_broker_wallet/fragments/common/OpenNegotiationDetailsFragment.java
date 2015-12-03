e9847792c495459c2a97f6d60e4b1b0425953fdb
Nelson Ramirez <nelsonalfo@gmail.com>
2015-12-02 21:36:18 -0430

    agregando lo que he echo de negotiation details

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/OpenNegotiationDetailsFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/OpenNegotiationDetailsFragment.java
@@ -1,11 +1,13 @@
 package com.bitdubai.reference_wallet.crypto_broker_wallet.fragments.common;
 
 
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatWalletFragment;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
@@ -25,10 +27,65 @@ public class OpenNegotiationDetailsFragment extends FermatWalletFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.cbw_fragment_negotiation_details_activity, container, false);
+
+        final Button paymentDateValue = (Button) rootView.findViewById(R.id.payment_date_value);
+        paymentDateValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = DatePickerFragment.getNewInstance(paymentDateValue);
+                datePicker.show(getFragmentManager(), "datePicker");
+            }
+        });
+
+        final Button paymentTimeValue = (Button) rootView.findViewById(R.id.payment_time_value);
+        paymentTimeValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = TimePickerFragment.getNewInstance(paymentTimeValue);
+                timePicker.show(getFragmentManager(), "timePicker");
+            }
+        });
+
+        final Button deliveryDateValue = (Button) rootView.findViewById(R.id.delivery_date_value);
+        deliveryDateValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = DatePickerFragment.getNewInstance(deliveryDateValue);
+                datePicker.show(getFragmentManager(), "datePicker");
+            }
+        });
+
+        final Button deliveryTimeValue = (Button) rootView.findViewById(R.id.delivery_time_value);
+        deliveryTimeValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = TimePickerFragment.getNewInstance(deliveryTimeValue);
+                timePicker.show(getFragmentManager(), "timePicker");
+            }
+        });
+
+        final Button expirationDateValue = (Button) rootView.findViewById(R.id.expiration_date_value);
+        expirationDateValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = DatePickerFragment.getNewInstance(expirationDateValue);
+                datePicker.show(getFragmentManager(), "datePicker");
+            }
+        });
+
+        final Button expirationTimeValue = (Button) rootView.findViewById(R.id.expiration_time_value);
+        expirationTimeValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = TimePickerFragment.getNewInstance(expirationTimeValue);
+                timePicker.show(getFragmentManager(), "timePicker");
+            }
+        });
+
+        return rootView;
     }
 
 
