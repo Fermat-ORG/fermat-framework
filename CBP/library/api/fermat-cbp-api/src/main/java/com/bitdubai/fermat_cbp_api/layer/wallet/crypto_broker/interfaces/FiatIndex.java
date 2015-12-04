9ab1d9938527d557679d5d260baf357c585ae1a2
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2015-12-04 19:04:12 -0430

    CBP - wallet - Crypto Broker - Implementation #3482

---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/wallet/crypto_broker/interfaces/FiatIndex.java
@@ -0,0 +1,13 @@
+package com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.interfaces;
+
+import com.bitdubai.fermat_api.layer.all_definition.enums.interfaces.FermatEnum;
+
+/**
+ * Created by franklin on 04/12/15.
+ */
+public interface FiatIndex {
+    //TODO; Documentar y excepciones
+    FermatEnum getMerchandise();
+    float getSalePrice();
+    float getPurchasePrice();
+}
