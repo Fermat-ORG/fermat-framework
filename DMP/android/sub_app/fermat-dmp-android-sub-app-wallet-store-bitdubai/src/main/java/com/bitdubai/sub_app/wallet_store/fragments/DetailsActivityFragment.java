b5f7ec26dcc25f89b8ff283f46b33085aecda8e0
Nelson Ramirez <nelsonalfo@gmail.com>
2015-12-04 10:49:32 -0430

    subiendo cambpso restantes

--- DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-store-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_store/fragments/DetailsActivityFragment.java
+++ DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-store-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_store/fragments/DetailsActivityFragment.java
@@ -29,7 +29,6 @@ import com.bitdubai.sub_app.wallet_store.session.WalletStoreSubAppSession;
 import com.bitdubai.sub_app.wallet_store.util.UtilsFuncs;
 import com.wallet_store.bitdubai.R;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -100,18 +99,19 @@ public class DetailsActivityFragment extends FermatFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.wallet_store_fragment_details_activity, container, false);
 
-        developerName = (FermatTextView) rootView.findViewById(R.id.wallet_developer_name);
-        shortDescription = (FermatTextView) rootView.findViewById(R.id.wallet_short_description);
-        walletName = (FermatTextView) rootView.findViewById(R.id.wallet_name);
-        walletIcon = (ImageView) rootView.findViewById(R.id.wallet_icon);
-        walletBanner = (ImageView) rootView.findViewById(R.id.wallet_banner);
-        publisherName = (FermatTextView) rootView.findViewById(R.id.wallet_publisher_name);
-        totalInstalls = (FermatTextView) rootView.findViewById(R.id.wallet_total_installs);
-        readMoreLink = (FermatTextView) rootView.findViewById(R.id.read_more_link);
-        installButton = (FermatButton) rootView.findViewById(R.id.wallet_install_button);
-        uninstallButton = (FermatButton) rootView.findViewById(R.id.wallet_uninstall_button);
-        previewImagesRecyclerView = (RecyclerView) rootView.findViewById(R.id.wallet_screenshots_recycler_view);
-        noPreviewImages = (FermatTextView) rootView.findViewById(R.id.no_preview_images);
+        developerName = (FermatTextView) rootView.findViewById(R.id.ws_developer);
+        publisherName = (FermatTextView) rootView.findViewById(R.id.ws_publisher);
+        walletName = (FermatTextView) rootView.findViewById(R.id.ws_name);
+        walletIcon = (ImageView) rootView.findViewById(R.id.ws_icon);
+        walletBanner = (ImageView) rootView.findViewById(R.id.ws_banner);
+        shortDescription = (FermatTextView) rootView.findViewById(R.id.ws_short_description);
+        totalInstalls = (FermatTextView) rootView.findViewById(R.id.ws_total_installs);
+        readMoreLink = (FermatTextView) rootView.findViewById(R.id.ws_read_more);
+        installButton = (FermatButton) rootView.findViewById(R.id.ws_install_button);
+        uninstallButton = (FermatButton) rootView.findViewById(R.id.ws_uninstall_button);
+        previewImagesRecyclerView = (RecyclerView) rootView.findViewById(R.id.ws_screenshots);
+        noPreviewImages = (FermatTextView) rootView.findViewById(R.id.ws_no_preview);
+
 
         setupDataInViews();
 
@@ -142,7 +142,9 @@ public class DetailsActivityFragment extends FermatFragment {
         final InstallationStatus installStatus = catalogItem.getInstallationStatus();
         int resId = UtilsFuncs.INSTANCE.getInstallationStatusStringResource(installStatus);
         resId = (resId == R.string.wallet_status_installed) ? R.string.wallet_status_open : resId;
-        installButton.setText(resId);
+
+        // TODO Corregir esto para que tome el string en base al estado de la instalacion (resId)
+        installButton.setText(R.string.wallet_status_open);
         installButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
