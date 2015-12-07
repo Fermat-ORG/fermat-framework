da6ba3e5e8b5dc9435c7981a77c851f9be09a4a9
natalia <natalia_veronica_c@hotmail.com>
2015-12-07 13:56:52 -0300

    Crypto Payment Request NS - Add ConnectionNotification Event Handler #3586

--- CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-payment-request-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_payment_request/developer/bitdubai/version_1/communication/event_handlers/FailureComponentConnectionRequestNotificationEventHandler.java
+++ CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-payment-request-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_payment_request/developer/bitdubai/version_1/communication/event_handlers/FailureComponentConnectionRequestNotificationEventHandler.java
@@ -1,7 +1,14 @@
 package com.bitdubai.fermat_ccp_plugin.layer.network_service.crypto_payment_request.developer.bitdubai.version_1.communication.event_handlers;
 
+import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.Service;
+import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEvent;
+import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventHandler;
+import com.bitdubai.fermat_api.layer.all_definition.network_service.interfaces.NetworkService;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.abstract_classes.AbstractNetworkService;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.template.event_handlers.AbstractFailureComponentConnectionRequestNotificationEventHandler;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.events.FailureComponentConnectionRequestNotificationEvent;
 
 /**
  * The Class <code>com.bitdubai.fermat_ccp_plugin.layer.network_service.crypto_payment_request.developer.bitdubai.version_1.communication.event_handlers.FailureComponentConnectionRequestNotificationEventHandler</code>
@@ -12,11 +19,50 @@ import com.bitdubai.fermat_p2p_api.layer.all_definition.common.network_services.
  * @version 1.0
  * @since Java JDK 1.7
  */
-public final class FailureComponentConnectionRequestNotificationEventHandler extends AbstractFailureComponentConnectionRequestNotificationEventHandler {
+public final class FailureComponentConnectionRequestNotificationEventHandler implements FermatEventHandler {
 
-    public FailureComponentConnectionRequestNotificationEventHandler(final AbstractNetworkService networkService) {
+    /*
+    * Represent the networkService
+    */
+    private NetworkService networkService;
 
-        super(networkService);
+    /**
+     * Constructor with parameter
+     *
+     * @param networkService
+     */
+    public FailureComponentConnectionRequestNotificationEventHandler(NetworkService networkService) {
+        this.networkService = networkService;
     }
 
+    /**
+     * (non-Javadoc)
+     *
+     * @see FermatEventHandler#handleEvent(FermatEvent)
+     *
+     * @param platformEvent
+     * @throws Exception
+     */
+    @Override
+    public void handleEvent(FermatEvent platformEvent) throws FermatException {
+
+        //System.out.println("FailureComponentConnectionRequestNotificationEventListener - handleEvent platformEvent ="+platformEvent.getEventType() );
+
+        if (((Service) this.networkService).getStatus() == ServiceStatus.STARTED) {
+
+            FailureComponentConnectionRequestNotificationEvent failureComponentConnectionRequestNotificationEvent = (FailureComponentConnectionRequestNotificationEvent) platformEvent;
+
+            if (failureComponentConnectionRequestNotificationEvent.getNetworkServiceApplicant().getPlatformComponentType()  == networkService.getPlatformComponentType() &&
+                    failureComponentConnectionRequestNotificationEvent.getNetworkServiceApplicant().getNetworkServiceType() == networkService.getNetworkServiceType()){
+
+                /*
+                 *  networkService make the job
+                 */
+                this.networkService.handleFailureComponentRegistrationNotificationEvent(failureComponentConnectionRequestNotificationEvent.getNetworkServiceApplicant(), failureComponentConnectionRequestNotificationEvent.getRemoteParticipant());
+
+            }
+
+
+        }
+    }
 }
