18a0c75ad812ddc18ca77780828658cad3d41442
abicelis <abicelis@gmail.com>
2015-12-08 00:35:25 -0430

    Currency Exchange Rates superlayer v0 implementation

---
+++ CER/library/api/fermat-cer-api/src/main/java/com/bitdubai/fermat_cer_api/layer/provider/interfaces/CurrencyExchangeRateProviderManager.java
@@ -0,0 +1,20 @@
+package com.bitdubai.fermat_cer_api.layer.provider.interfaces;
+
+import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
+import com.bitdubai.fermat_api.layer.world.exceptions.CantGetIndexException;
+import com.bitdubai.fermat_cer_api.layer.provider.exceptions.UnsupportedCurrencyPairException;
+
+import java.util.Collection;
+
+/**
+ * Created by Alejandro Bicelis on 12/7/2015.
+ */
+public interface CurrencyExchangeRateProviderManager extends FermatManager {
+
+    Collection<CurrencyPair> getSupportedCurrencyPairs();
+    boolean isCurrencyPairSupported(CurrencyPair currencyPair) throws IllegalArgumentException;
+    ExchangeRate getCurrentExchangeRate(CurrencyPair currencyPair) throws UnsupportedCurrencyPairException, CantGetIndexException;
+    Collection<ExchangeRate> getExchangeRateListFromDate(CurrencyPair currencyPair, long timestamp) throws UnsupportedCurrencyPairException, CantGetIndexException;
+    Collection<ExchangeRate> getQueriedExchangeRateHistory(CurrencyPair currencyPair) throws UnsupportedCurrencyPairException, CantGetIndexException;
+
+}
