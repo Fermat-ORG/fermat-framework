84dd502d0aa0bbfd4084593d53f79abaef4b0e9f
abicelis <abicelis@gmail.com>
2015-12-09 15:23:19 -0430

    CSH - BalanceSummaryFragment

--- CSH/android/reference_wallet/fermat-csh-android-reference-wallet-cash-money-bitdubai/src/main/java/com/bitdubai/reference_wallet/cash_money_wallet/fragments/home/BalanceSummaryFragment.java
+++ CSH/android/reference_wallet/fermat-csh-android-reference-wallet-cash-money-bitdubai/src/main/java/com/bitdubai/reference_wallet/cash_money_wallet/fragments/home/BalanceSummaryFragment.java
@@ -24,7 +24,7 @@ public class BalanceSummaryFragment extends FermatWalletFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.clear_project, container, false);
+        return inflater.inflate(R.layout.balance_summary, container, false);
     }
 
 }
