3a0371a4a4d7fc7aaa542b2427c830b6c3c4514e
Víctor A. Mars M <marsvicam@gmail.com>
2015-12-10 09:32:46 -0430

    Merged with acostarodrigo/develop. Fixed 4 conflicts caused after refactor DAPException.

--- DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_transaction/common/exceptions/RecordsNotFoundException.java
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_transaction/common/exceptions/RecordsNotFoundException.java
@@ -1,6 +1,6 @@
 package com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions;
 
-import com.bitdubai.fermat_api.layer.DAPException;
+import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.DAPException;
 
 /**
  * Created by Víctor A. Mars M. (marsvicam@gmail.com) on 26/10/15.
--- DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_transaction/common/interfaces/AbstractDigitalAssetVault.java
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_transaction/common/interfaces/AbstractDigitalAssetVault.java
@@ -1,6 +1,6 @@
 package com.bitdubai.fermat_dap_api.layer.dap_transaction.common.interfaces;
 
-import com.bitdubai.fermat_api.layer.DAPException;
+import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.DAPException;
 import com.bitdubai.fermat_api.layer.all_definition.transaction_transference_protocol.crypto_transactions.CryptoTransaction;
 import com.bitdubai.fermat_api.layer.all_definition.util.XMLParser;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.FileLifeSpan;
