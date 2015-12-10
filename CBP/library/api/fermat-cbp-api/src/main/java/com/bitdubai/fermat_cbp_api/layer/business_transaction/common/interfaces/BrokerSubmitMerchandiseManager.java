9ef4c166f370768f1c5da4360f5e48829e25f6d0
Manuel <darkpriestrelative@gmail.com>
2015-12-10 09:47:23 -0430

    Creating CBP-Business Transaction Layer common exceptions

---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/common/interfaces/BrokerSubmitMerchandiseManager.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_api.layer.business_transaction.common.interfaces;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 10/12/15.
+ */
+public class BrokerSubmitMerchandiseManager {
+}
