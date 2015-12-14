0fad4e728e214abeacf9b3d80dd091131c54db8a
lnacosta <laion.cj91@gmail.com>
2015-12-14 16:31:15 -0300

    asdasdasd

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/actor_connection/crypto_customer/utils/CryptoCustomerLinkedActorIdentity.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/actor_connection/crypto_customer/utils/CryptoCustomerLinkedActorIdentity.java
@@ -5,14 +5,14 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Actors;
 
 /**
  * The class <code>com.bitdubai.fermat_cbp_api.layer.actor_connection.crypto_broker.utils.CryptoCustomerLinkedActorIdentity</code>
- * represents a crypto broker actor identity.
+ * represents a crypto customer actor identity.
  * <p>
  * Created by Leon Acosta - (laion.cj91@gmail.com) on 20/11/2015.
  */
 public final class CryptoCustomerLinkedActorIdentity extends LinkedActorIdentity {
 
     public CryptoCustomerLinkedActorIdentity(final String publicKey) {
-        super(publicKey, Actors.CBP_CRYPTO_BROKER);
+        super(publicKey, Actors.CBP_CRYPTO_CUSTOMER);
     }
 
 }
