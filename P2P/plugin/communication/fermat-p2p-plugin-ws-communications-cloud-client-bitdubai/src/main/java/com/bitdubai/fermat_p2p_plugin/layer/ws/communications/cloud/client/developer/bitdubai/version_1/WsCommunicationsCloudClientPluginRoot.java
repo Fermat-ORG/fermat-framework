0de41d0a3ff0ba210938c807ca3e3f49ab838787
javafrank <contrerasfrank@gmail.com>
2015-12-14 19:15:40 -0430

    #3749 develop ui for redeem point wallet

--- P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
+++ P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
@@ -59,7 +59,7 @@ public class WsCommunicationsCloudClientPluginRoot extends AbstractPlugin implem
     /**
      * Represent the SERVER_IP
      */
-     public static final String SERVER_IP = "192.168.1.113";
+     public static final String SERVER_IP = ServerConf.SERVER_IP_PRODUCCTION;
 
     /*
      * Hold the list of event listeners
