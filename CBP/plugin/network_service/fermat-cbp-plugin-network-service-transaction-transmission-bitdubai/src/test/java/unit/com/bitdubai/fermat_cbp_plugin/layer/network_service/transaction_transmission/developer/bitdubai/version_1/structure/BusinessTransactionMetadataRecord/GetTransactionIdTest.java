36f1d313222e89cde66698f7dcfba33807bcf1d3
Gabriel Araujo <gabe_512@hotmail.com>
2015-12-14 22:45:23 -0430

    #3077 CBP - Network Service - Transaction Transmission - Testing - Unit Testing
    All Test of the BusinessTransactionMetadataRecord PLugin had've been tested.
    
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

--- CBP/plugin/network_service/fermat-cbp-plugin-network-service-transaction-transmission-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/network_service/transaction_transmission/developer/bitdubai/version_1/structure/BusinessTransactionMetadataRecord/GetTransactionIdTest.java
+++ CBP/plugin/network_service/fermat-cbp-plugin-network-service-transaction-transmission-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/network_service/transaction_transmission/developer/bitdubai/version_1/structure/BusinessTransactionMetadataRecord/GetTransactionIdTest.java
@@ -1,30 +1,31 @@
 package unit.com.bitdubai.fermat_cbp_plugin.layer.network_service.transaction_transmission.developer.bitdubai.version_1.structure.BusinessTransactionMetadataRecord;
 
-import com.bitdubai.fermat_api.layer.all_definition.components.enums.PlatformComponentType;
 import com.bitdubai.fermat_cbp_plugin.layer.network_service.transaction_transmission.developer.bitdubai.version_1.structure.BusinessTransactionMetadataRecord;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.UUID;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * Created by Gabriel Araujo (gabe_512@hotmail.com) on 08/12/15.
+ * Created by root on 14/12/15.
  */
 @RunWith(MockitoJUnitRunner.class)
-public class GetSenderTypeTest {
+public class GetTransactionIdTest {
 
-    private PlatformComponentType platformComponentType = PlatformComponentType.ACTOR_CRYPTO_CUSTOMER;
+    private UUID transactionId = UUID.randomUUID();
 
     @Test
-    public void getSenderType() throws Exception{
+    public void getTransactionId() throws Exception{
         BusinessTransactionMetadataRecord businessTransactionMetadataRecord = mock(BusinessTransactionMetadataRecord.class);
-        when(businessTransactionMetadataRecord.getSenderType()).thenReturn(platformComponentType);
-        assertThat(businessTransactionMetadataRecord.getSenderType()).isNotNull();
+        when(businessTransactionMetadataRecord.getTransactionId()).thenReturn(transactionId);
+        assertThat(businessTransactionMetadataRecord.getTransactionId()).isNotNull();
         System.out.println("Test run");
     }
+
 }
