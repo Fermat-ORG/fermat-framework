4fe0fd4a8f5d432ae655715712f0448fa070eff8
josemanueldsds <josemanueldsds@gmail.com>
2015-12-15 22:44:05 -0430

    Update fragment factory

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragmentFactory/IntraUserFragmentFactory.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragmentFactory/IntraUserFragmentFactory.java
@@ -3,14 +3,12 @@ package com.bitdubai.sub_app.intra_user_community.fragmentFactory;
 import com.bitdubai.fermat_android_api.engine.FermatSubAppFragmentFactory;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.exceptions.FragmentNotFoundException;
-import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionsListFragment;
 import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionNotificationsFragment;
 import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionOtherProfileFragment;
 import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionSettingsFragment;
 import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionsFragment;
+import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionsListFragment;
 import com.bitdubai.sub_app.intra_user_community.fragments.ConnectionsWorldFragment;
-import com.bitdubai.sub_app.intra_user_community.fragments.RegisterIntraUserFragment;
-import com.bitdubai.sub_app.intra_user_community.fragments.RequestConnectionsFragment;
 import com.bitdubai.sub_app.intra_user_community.preference_settings.IntraUserPreferenceSettings;
 import com.bitdubai.sub_app.intra_user_community.session.IntraUserSubAppSession;
 
@@ -28,28 +26,8 @@ public class IntraUserFragmentFactory extends FermatSubAppFragmentFactory<IntraU
         FermatFragment currentFragment = null;
 
         switch (fragments) {
-            case CWP_WALLET_STORE_MAIN_ACTIVITY:
-               // currentFragment = HomeFragment.newInstance(1);
-                break;
-            case CWP_WALLET_STORE_DETAIL_ACTIVITY:
-                //currentFragment = OldConnectionsListFragment.newInstance();
-                break;
-            case CWP_WALLET_STORE_MORE_DETAIL_ACTIVITY:
-                //currentFragment = OldConnectionsListFragment.newInstance();
-                break;
             case CWP_WALLET_STORE_ALL_FRAGMENT:
                 currentFragment = ConnectionsWorldFragment.newInstance();
-                //currentFragment = OldConnectionsListFragment.newInstance();
-                break;
-            case CWP_WALLET_STORE_FREE_FRAGMENT:
-                currentFragment = RegisterIntraUserFragment.newInstance();
-                break;
-            case CWP_WALLET_STORE_PAID_FRAGMENT:
-                currentFragment = RequestConnectionsFragment.newInstance();
-                //currentFragment = ConnectionsRequestListFragment.newInstance();
-                break;
-            case CCP_SUB_APP_INTRA_USER_COMMUNITY_FRAGMENT:
-                currentFragment = RequestConnectionsFragment.newInstance();
                 break;
             case CCP_SUB_APP_INTRA_USER_COMMUNITY_CONNECTIONS_FRAGMENT:
                 currentFragment = ConnectionsFragment.newInstance();
