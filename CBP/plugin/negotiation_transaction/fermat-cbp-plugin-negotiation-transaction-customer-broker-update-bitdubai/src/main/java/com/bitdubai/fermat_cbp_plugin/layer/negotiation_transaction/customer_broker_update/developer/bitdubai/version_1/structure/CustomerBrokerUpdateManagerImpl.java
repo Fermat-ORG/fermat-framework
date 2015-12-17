72a39e739b00ce66739b2ffc7f2d63febc2ac929
yalayn <y.alayn@gmail.com>
2015-12-17 17:29:10 -0430

    CBP-Negotiation Transaction - Customer Broker Update - Internal Structure #3257 #3258 #3259

--- CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-update-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_update/developer/bitdubai/version_1/structure/CustomerBrokerUpdateManagerImpl.java
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-update-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_update/developer/bitdubai/version_1/structure/CustomerBrokerUpdateManagerImpl.java
@@ -1,7 +1,71 @@
 package com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_update.developer.bitdubai.version_1.structure;
 
+import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchaseNegotiation;
+import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchaseNegotiationManager;
+import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.interfaces.CustomerBrokerSaleNegotiation;
+import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.interfaces.CustomerBrokerSaleNegotiationManager;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.exceptions.CantCancelNegotiationException;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.exceptions.CantCreateCustomerBrokerUpdatePurchaseNegotiationTransactionException;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.exceptions.CantCreateCustomerBrokerUpdateSaleNegotiationTransactionException;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.exceptions.CantGetCustomerBrokerUpdateNegotiationTransactionException;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.exceptions.CantGetListCustomerBrokerUpdateNegotiationTransactionException;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.interfaces.CustomerBrokerUpdate;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_update.interfaces.CustomerBrokerUpdateManager;
+import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_update.developer.bitdubai.version_1.database.CustomerBrokerUpdateNegotiationTransactionDatabaseDao;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Yordin Alayn on 16.12.15.
  */
-public class CustomerBrokerUpdateManagerImpl {
+public class CustomerBrokerUpdateManagerImpl implements CustomerBrokerUpdateManager {
+
+    /*Represent the Transaction database DAO */
+    private CustomerBrokerUpdateNegotiationTransactionDatabaseDao   customerBrokerUpdateNegotiationTransactionDatabaseDao;
+
+    /*Represent the Transaction Negotiation Purchase*/
+    private CustomerBrokerUpdatePurchaseNegotiationTransaction      customerBrokerUpdatePurchaseNegotiationTransaction;
+
+    /*Represent the Transaction Negotiation Sale*/
+    private CustomerBrokerUpdateSaleNegotiationTransaction          customerBrokerUpdateSaleNegotiationTransaction;
+
+    /*Represent the Negotiation Purchase*/
+    private CustomerBrokerPurchaseNegotiationManager                customerBrokerPurchaseNegotiationManager;
+
+    /*Represent the Negotiation Sale*/
+    private CustomerBrokerSaleNegotiationManager                    customerBrokerSaleNegotiationManager;
+
+    public CustomerBrokerUpdateManagerImpl(
+        CustomerBrokerUpdateNegotiationTransactionDatabaseDao customerBrokerUpdateNegotiationTransactionDatabaseDao
+    ){
+        this.customerBrokerUpdateNegotiationTransactionDatabaseDao = customerBrokerUpdateNegotiationTransactionDatabaseDao;
+    }
+
+    //UPDATE THE PURCHASE NEGOTIATION TRANSACTION
+    public void createCustomerBrokerUpdatePurchaseNegotiationTranasction(CustomerBrokerPurchaseNegotiation customerBrokerPurchaseNegotiation)
+        throws CantCreateCustomerBrokerUpdatePurchaseNegotiationTransactionException{
+
+    }
+
+    //UPDATE THE SALE NEGOTIATION TRANSACTION
+    public void createCustomerBrokerUpdateSaleNegotiationTranasction(CustomerBrokerSaleNegotiation customerBrokerSaleNegotiation)
+        throws CantCreateCustomerBrokerUpdateSaleNegotiationTransactionException{
+
+    }
+
+    //CANCEL THE NEGOTIATION INDICATE
+    public void cancelNegotiation(UUID negotiationId) throws CantCancelNegotiationException{
+
+    }
+
+    //GET THE NEW NEGOTIATION TRANSACTION FOR THE INDICATE ID
+    public CustomerBrokerUpdate getCustomerBrokerNewNegotiationTranasction(UUID transactionId) throws CantGetCustomerBrokerUpdateNegotiationTransactionException{
+        return null;
+    }
+
+    //LIST THE UPDATE NEGOTIATION TRANSACTION
+    public List<CustomerBrokerUpdate> getAllCustomerBrokerNewNegotiationTranasction() throws CantGetListCustomerBrokerUpdateNegotiationTransactionException{
+        return null;
+    }
 }
