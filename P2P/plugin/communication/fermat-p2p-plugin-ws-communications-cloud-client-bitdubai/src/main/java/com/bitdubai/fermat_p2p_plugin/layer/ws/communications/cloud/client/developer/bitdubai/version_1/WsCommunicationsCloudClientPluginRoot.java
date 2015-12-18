b3417b70bb6feafbc2320c77e561e69a769a272a
javafrank <contrerasfrank@gmail.com>
2015-12-17 19:39:08 -0430

    #3657 design of asset detail view

--- P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
+++ P2P/plugin/communication/fermat-p2p-plugin-ws-communications-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
@@ -59,7 +59,7 @@ public class WsCommunicationsCloudClientPluginRoot extends AbstractPlugin implem
     /**
      * Represent the SERVER_IP
      */
-     public static final String SERVER_IP = "192.168.0.18";
+     public static final String SERVER_IP = ServerConf.SERVER_IP_PRODUCCTION;
 
     /*
      * Hold the list of event listeners
