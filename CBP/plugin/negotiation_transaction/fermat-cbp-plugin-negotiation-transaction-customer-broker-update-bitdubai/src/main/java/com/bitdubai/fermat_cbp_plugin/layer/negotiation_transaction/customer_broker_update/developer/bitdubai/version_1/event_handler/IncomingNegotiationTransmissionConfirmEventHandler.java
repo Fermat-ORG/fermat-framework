3211ce65e8d77c341d6d5bd637c8da6f5e2c4b5b
yalayn <y.alayn@gmail.com>
2015-12-18 22:11:22 -0430

    CBP-Negotiation Transaction - Customer Broker Update - Event Handler

---
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-update-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_update/developer/bitdubai/version_1/event_handler/IncomingNegotiationTransmissionConfirmEventHandler.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_update.developer.bitdubai.version_1.event_handler;
+
+/**
+ * Created by Yordin Alayn 18.12.15
+ */
+public class IncomingNegotiationTransmissionConfirmEventHandler {
+}
