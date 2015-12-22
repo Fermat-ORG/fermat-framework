f4737e031694b85889e0b9b5af471af94874d858
josemanueldsds <josemanueldsds@gmail.com>
2015-12-22 02:43:33 -0430

    Add user phrase and new buttons connection states

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/ConnectionOtherProfileFragment.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/ConnectionOtherProfileFragment.java
@@ -20,7 +20,8 @@ import android.widget.Toast;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.utils.ImagesUtils;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
-import com.bitdubai.fermat_ccp_api.layer.actor.intra_user.exceptions.CantCreateNewDeveloperException;
+import com.bitdubai.fermat_api.layer.actor_connection.common.enums.ConnectionState;
+import com.bitdubai.fermat_ccp_api.layer.actor.intra_user.interfaces.IntraWalletUserActorManager;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.exceptions.CantGetActiveLoginIdentityException;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserInformation;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserModuleManager;
@@ -40,7 +41,7 @@ import com.bitdubai.sub_app.intra_user_community.session.IntraUserSubAppSession;
  * Creado por Jose Manuel De Sousa on 29/11/15.
  */
 @SuppressWarnings({"FieldCanBeLocal", "unused"})
-public class ConnectionOtherProfileFragment extends FermatFragment implements MessageReceiver {
+public class ConnectionOtherProfileFragment extends FermatFragment implements MessageReceiver, View.OnClickListener {
 
     public static final String INTRA_USER_SELECTED = "intra_user";
     private Resources res;
@@ -55,6 +56,13 @@ public class ConnectionOtherProfileFragment extends FermatFragment implements Me
     private Button connect;
     private CryptoWalletIntraUserActor identity;
     private Button disconnect;
+    private int MAX = 1;
+    private int OFFSET = 0;
+    private FermatTextView userPhrase;
+    private Button connectionRequestSend;
+    private Button connectionRequestRejected;
+    private IntraWalletUserActorManager intraWalletUserActorManager;
+    private ConnectionState connectionState;
 
     /**
      * Create a new instance of this fragment
@@ -84,28 +92,61 @@ public class ConnectionOtherProfileFragment extends FermatFragment implements Me
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_connections_other_profile, container, false);
         userProfileAvatar = (ImageView) rootView.findViewById(R.id.img_user_avatar);
+        userPhrase = (FermatTextView) rootView.findViewById(R.id.userPhrase);
         userName = (FermatTextView) rootView.findViewById(R.id.username);
         userEmail = (FermatTextView) rootView.findViewById(R.id.email);
+        connectionRequestSend = (Button) rootView.findViewById(R.id.btn_connection_request_send);
+        connectionRequestRejected = (Button) rootView.findViewById(R.id.btn_connection_request_reject);
         connect = (Button) rootView.findViewById(R.id.btn_conect);
         disconnect = (Button) rootView.findViewById(R.id.btn_disconect);
+        connectionRequestSend.setVisibility(View.GONE);
+        connectionRequestRejected.setVisibility(View.GONE);
         connect.setVisibility(View.GONE);
         disconnect.setVisibility(View.GONE);
-        try{
-        if(moduleManager.isActorConnected(intraUserInformation.getPublicKey())) {
-            disconnect.setVisibility(View.VISIBLE);
-            connect.setVisibility(View.GONE);
-        }else {
-            connect.setVisibility(View.VISIBLE);
-            disconnect.setVisibility(View.GONE);
-        }
-        }catch (CantCreateNewDeveloperException e) {
+        connectionRequestRejected.setOnClickListener(this);
+        connectionRequestSend.setOnClickListener(this);
+        connect.setOnClickListener(this);
+        disconnect.setOnClickListener(this);
+
+        connectionSend();
+
+        /*try {
+            if (intraWalletUserActorManager.getIntraUsersConnectionStatus(intraUserInformation.getPublicKey()) != null){
+                switch (intraWalletUserActorManager.getIntraUsersConnectionStatus(intraUserInformation.getPublicKey())) {
+                    case BLOCKED_LOCALLY:
+                    case BLOCKED_REMOTELY:
+                    case CANCELLED_LOCALLY:
+                    case CANCELLED_REMOTELY:
+                        connectionRejected();
+                        break;
+                    case CONNECTED:
+                        disconnectRequest();
+                        break;
+                    case NO_CONNECTED:
+                    case DISCONNECTED_LOCALLY:
+                    case DISCONNECTED_REMOTELY:
+                    case ERROR:
+                    case DENIED_LOCALLY:
+                    case DENIED_REMOTELY:
+                        connectRequest();
+                        break;
+                    case PENDING_LOCALLY_ACCEPTANCE:
+                    case PENDING_REMOTELY_ACCEPTANCE:
+                        connectionSend();
+                        break;
+                }
+            }else {
+                connectRequest();
+            }
+
+        } catch (CantGetIntraUsersConnectedStateException e) {
             e.printStackTrace();
-        }
+        }*/
 
         try {
             userName.setText(intraUserInformation.getName());
-            userEmail.setText("Unknow");
-            if(intraUserInformation.getProfileImage() != null) {
+            userPhrase.setText(intraUserInformation.getPhrase());
+            if (intraUserInformation.getProfileImage() != null) {
                 Bitmap bitmap;
                 if (intraUserInformation.getProfileImage().length > 0) {
                     bitmap = BitmapFactory.decodeByteArray(intraUserInformation.getProfileImage(), 0, intraUserInformation.getProfileImage().length);
@@ -114,7 +155,7 @@ public class ConnectionOtherProfileFragment extends FermatFragment implements Me
                 }
                 bitmap = Bitmap.createScaledBitmap(bitmap, 110, 110, true);
                 userProfileAvatar.setImageDrawable(ImagesUtils.getRoundedBitmap(getResources(), bitmap));
-            }else{
+            } else {
                 Bitmap bitmap;
                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.profile_image);
                 bitmap = Bitmap.createScaledBitmap(bitmap, 110, 110, true);
@@ -123,41 +164,74 @@ public class ConnectionOtherProfileFragment extends FermatFragment implements Me
         } catch (Exception ex) {
             Toast.makeText(getActivity().getApplicationContext(), "Oooops! recovering from system error", Toast.LENGTH_SHORT).show();
         }
-
-        connect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ConnectDialog connectDialog;
-                try {
-                    connectDialog = new ConnectDialog(getActivity(), (IntraUserSubAppSession) appSession, (SubAppResourcesProviderManager) appResourcesProviderManager, intraUserInformation, moduleManager.getActiveIntraUserIdentity());
-                    connectDialog.setTitle("Connection Request");
-                    connectDialog.setDescription("Do you want to send ");
-                    connectDialog.setUsername(intraUserInformation.getName());
-                    connectDialog.setSecondDescription("a connection request");
-                    connectDialog.show();
-                } catch (CantGetActiveLoginIdentityException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        disconnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                final DisconectDialog disconectDialog;
-                try {
-                    disconectDialog = new DisconectDialog(getActivity(), (IntraUserSubAppSession) appSession, (SubAppResourcesProviderManager) appResourcesProviderManager, intraUserInformation, moduleManager.getActiveIntraUserIdentity());
-                    disconectDialog.setTitle("Disconnect");
-                    disconectDialog.setDescription("Want to disconnect from");
-                    disconectDialog.setUsername(intraUserInformation.getName());
-                    disconectDialog.show();
-                } catch (CantGetActiveLoginIdentityException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
         return rootView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btn_conect) {
+            ConnectDialog connectDialog;
+            try {
+                connectDialog = new ConnectDialog(getActivity(), (IntraUserSubAppSession) appSession, (SubAppResourcesProviderManager) appResourcesProviderManager, intraUserInformation, moduleManager.getActiveIntraUserIdentity());
+                connectDialog.setTitle("Connection Request");
+                connectDialog.setDescription("Do you want to send ");
+                connectDialog.setUsername(intraUserInformation.getName());
+                connectDialog.setSecondDescription("a connection request");
+                connectDialog.show();
+            } catch (CantGetActiveLoginIdentityException e) {
+                e.printStackTrace();
+
+            }
+        }
+        if (i == R.id.btn_disconect) {
+            final DisconectDialog disconectDialog;
+            try {
+                disconectDialog = new DisconectDialog(getActivity(), (IntraUserSubAppSession) appSession, (SubAppResourcesProviderManager) appResourcesProviderManager, intraUserInformation, moduleManager.getActiveIntraUserIdentity());
+                disconectDialog.setTitle("Disconnect");
+                disconectDialog.setDescription("Want to disconnect from");
+                disconectDialog.setUsername(intraUserInformation.getName());
+                disconectDialog.show();
+            } catch (CantGetActiveLoginIdentityException e) {
+                e.printStackTrace();
+            }
+        }
+        if (i == R.id.btn_connection_request_send) {
+            Toast.makeText(getActivity(), "The connection request has been sent\n you need to wait until the user responds", Toast.LENGTH_SHORT).show();
+        }
+        if (i == R.id.btn_connection_request_reject) {
+            Toast.makeText(getActivity(), "The connection request has been rejected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void connectionSend() {
+        connectionRequestSend.setVisibility(View.VISIBLE);
+        connect.setVisibility(View.GONE);
+        disconnect.setVisibility(View.GONE);
+        connectionRequestRejected.setVisibility(View.GONE);
+    }
+
+    private void connectRequest() {
+        connectionRequestSend.setVisibility(View.GONE);
+        connect.setVisibility(View.VISIBLE);
+        disconnect.setVisibility(View.GONE);
+        connectionRequestRejected.setVisibility(View.GONE);
+    }
+
+    private void disconnectRequest() {
+        connectionRequestSend.setVisibility(View.GONE);
+        connect.setVisibility(View.GONE);
+        disconnect.setVisibility(View.VISIBLE);
+        connectionRequestRejected.setVisibility(View.GONE);
+    }
+
+    private void connectionRejected() {
+        connectionRequestSend.setVisibility(View.GONE);
+        connect.setVisibility(View.GONE);
+        disconnect.setVisibility(View.GONE);
+        connectionRequestRejected.setVisibility(View.VISIBLE);
     }
 
     private Drawable getImgDrawable(byte[] customerImg) {
@@ -184,12 +258,10 @@ public class ConnectionOtherProfileFragment extends FermatFragment implements Me
     public void onMessageReceive(Context context, Intent data) {
         Bundle extras = data != null ? data.getExtras() : null;
         if (extras != null && extras.containsKey(Constants.BROADCAST_CONNECTED_UPDATE)) {
-            disconnect.setVisibility(View.VISIBLE);
-            connect.setVisibility(View.GONE);
+            connectionSend();
         }
-        if(extras != null && extras.containsKey(Constants.BROADCAST_DISCONNECTED_UPDATE)){
-            disconnect.setVisibility(View.GONE);
-            connect.setVisibility(View.VISIBLE);
+        if (extras != null && extras.containsKey(Constants.BROADCAST_DISCONNECTED_UPDATE)) {
+            connectRequest();
         }
     }
 
@@ -197,5 +269,4 @@ public class ConnectionOtherProfileFragment extends FermatFragment implements Me
     public IntentFilter getBroadcastIntentChannel() {
         return new IntentFilter(Constants.LOCAL_BROADCAST_CHANNEL);
     }
-
 }
