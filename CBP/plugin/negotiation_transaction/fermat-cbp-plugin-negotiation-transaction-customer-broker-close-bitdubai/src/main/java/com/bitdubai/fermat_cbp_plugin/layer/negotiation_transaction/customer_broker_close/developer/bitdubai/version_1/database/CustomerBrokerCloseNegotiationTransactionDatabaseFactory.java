eb647a8a1a8afec714d8cb51e98cbe2ab5451c73
yalayn <y.alayn@gmail.com>
2015-12-22 01:30:34 -0430

    CBP-Negotiation Transaction-Customer Broker Close - Database #3271 #3272 #3273 #3274

---
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-close-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_close/developer/bitdubai/version_1/database/CustomerBrokerCloseNegotiationTransactionDatabaseFactory.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_close.developer.bitdubai.version_1.database;
+
+/**
+ * Created by yordin on 22/12/15.
+ */
+public class CustomerBrokerCloseNegotiationTransactionDatabaseFactory {
+}
