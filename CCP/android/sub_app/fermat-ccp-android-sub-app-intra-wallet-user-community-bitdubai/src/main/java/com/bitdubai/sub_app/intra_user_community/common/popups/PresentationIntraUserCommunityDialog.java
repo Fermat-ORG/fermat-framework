becdb5d4183b9efa26948ac5e8805d46032a3ec4
josemanueldsds <josemanueldsds@gmail.com>
2015-12-22 19:40:36 -0430

    Upadate presentation dialog

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/common/popups/PresentationIntraUserCommunityDialog.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/common/popups/PresentationIntraUserCommunityDialog.java
@@ -6,7 +6,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.interfaces.SubAppsSession;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatButton;
@@ -15,52 +18,86 @@ import com.bitdubai.fermat_pip_api.layer.network_service.subapp_resources.SubApp
 import com.bitdubai.sub_app.intra_user_community.R;
 
 /**
- * Created by mati on 2015.11.27..
+ * @author Jose manuel de Sousa
  */
+@SuppressWarnings({"FieldCanBeLocal", "unused"})
 public class PresentationIntraUserCommunityDialog extends FermatDialog<SubAppsSession, SubAppResourcesProviderManager> implements View.OnClickListener {
-
+    public static final int TYPE_PRESENTATION = 1;
+    public static final int TYPE_PRESENTATION_WITHOUT_IDENTITIES = 2;
     private final Activity activity;
+    private final int type;
     private FermatButton startCommunity;
     private CheckBox dontShowAgainCheckBox;
+    private Button btn_left;
+    private Button btn_right;
+    private ImageView image_view_left;
+    private FrameLayout container_john_doe;
+    private ImageView image_view_right;
+    private FrameLayout container_jane_doe;
+
 
     /**
      * Constructor using Session and Resources
      *
-     * @param activity
      * @param fermatSession parent class of walletSession and SubAppSession
      * @param resources     parent class of WalletResources and SubAppResources
      */
-    public PresentationIntraUserCommunityDialog(Activity activity, SubAppsSession fermatSession, SubAppResourcesProviderManager resources) {
+    public PresentationIntraUserCommunityDialog(Activity activity, SubAppsSession fermatSession, SubAppResourcesProviderManager resources, int type) {
         super(activity, fermatSession, resources);
         this.activity = activity;
+        this.type = type;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        startCommunity = (FermatButton) findViewById(R.id.start_community);
-        dontShowAgainCheckBox = (CheckBox) findViewById(R.id.checkbox_not_show);
-        startCommunity.setOnClickListener(this);
+        switch (type) {
+            case TYPE_PRESENTATION:
+                dontShowAgainCheckBox = (CheckBox) findViewById(R.id.checkbox_not_show);
+                image_view_left = (ImageView) findViewById(R.id.image_view_left);
+                image_view_right = (ImageView) findViewById(R.id.image_view_right);
+                container_john_doe = (FrameLayout) findViewById(R.id.container_john_doe);
+                container_jane_doe = (FrameLayout) findViewById(R.id.container_jane_doe);
+                btn_left = (Button) findViewById(R.id.btn_left);
+                btn_right = (Button) findViewById(R.id.btn_right);
+                btn_left.setOnClickListener(this);
+                btn_right.setOnClickListener(this);
+                break;
+            case TYPE_PRESENTATION_WITHOUT_IDENTITIES:
+                dontShowAgainCheckBox = (CheckBox) findViewById(R.id.checkbox_not_show);
+                startCommunity = (FermatButton) findViewById(R.id.start_community);
+                startCommunity.setOnClickListener(this);
+                break;
+        }
 
 
     }
 
     @Override
     protected int setLayoutId() {
-        return R.layout.tutorial_intra_user_community;
+        switch (type) {
+            case TYPE_PRESENTATION:
+                return R.layout.tutorial_intra_user_community;
+            case TYPE_PRESENTATION_WITHOUT_IDENTITIES:
+                return R.layout.tutorial_intra_user_community_whitout_identity;
+        }
+        return 0;
     }
 
     @Override
     protected int setWindowFeature() {
-        return Window.FEATURE_NO_TITLE;    }
+        return Window.FEATURE_NO_TITLE;
+    }
 
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
-        if (id == R.id.start_community) {
+        if (id == R.id.btn_left) {
+            dismiss();
+        } else if (id == R.id.btn_right) {
+            dismiss();
+        } else if (id == R.id.start_community) {
             SharedPreferences pref = getContext().getSharedPreferences("don't show dialog more", Context.MODE_PRIVATE);
             SharedPreferences.Editor edit = pref.edit();
             if (dontShowAgainCheckBox.isChecked()) {
