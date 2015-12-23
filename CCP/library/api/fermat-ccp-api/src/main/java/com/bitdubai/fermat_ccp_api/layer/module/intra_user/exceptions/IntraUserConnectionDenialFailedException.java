1e7b1b83bc83115afce97be35efca5e437e7b25a
lnacosta <laion.cj91@gmail.com>
2015-12-23 18:04:16 -0300

    little changes (evade use of unnecessary casting)

--- CCP/library/api/fermat-ccp-api/src/main/java/com/bitdubai/fermat_ccp_api/layer/module/intra_user/exceptions/IntraUserConnectionDenialFailedException.java
+++ CCP/library/api/fermat-ccp-api/src/main/java/com/bitdubai/fermat_ccp_api/layer/module/intra_user/exceptions/IntraUserConnectionDenialFailedException.java
@@ -5,7 +5,7 @@ import com.bitdubai.fermat_api.FermatException;
 /**
  * Created by eze on 2015.07.30..
  */
-public class IntraUserConectionDenegationFailedException extends FermatException {
+public class IntraUserConnectionDenialFailedException extends FermatException {
     /**
      * This is the constructor that every inherited FermatException must implement
      *
@@ -14,7 +14,7 @@ public class IntraUserConectionDenegationFailedException extends FermatException
      * @param context        a String that provides the values of the variables that could have affected the exception
      * @param possibleReason an explicative reason of why we believe this exception was most likely thrown
      */
-    public IntraUserConectionDenegationFailedException(String message, Exception cause, String context, String possibleReason) {
+    public IntraUserConnectionDenialFailedException(String message, Exception cause, String context, String possibleReason) {
         super(message, cause, context, possibleReason);
     }
 }
