04931e04c1f041663574be998115d5eabdadf883
Manuel <darkpriestrelative@gmail.com>
2015-12-22 21:21:25 -0430

    Fix #3950

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/broker_submit_online_merchandise/interfaces/BrokerSubmitOnlineMerchandiseManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/broker_submit_online_merchandise/interfaces/BrokerSubmitOnlineMerchandiseManager.java
@@ -1,4 +1,4 @@
-package com.bitdubai.fermat_cbp_api.layer.business_transaction.broker_submit_online_merchandise;
+package com.bitdubai.fermat_cbp_api.layer.business_transaction.broker_submit_online_merchandise.interfaces;
 
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.common.exceptions.CantSubmitMerchandiseException;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.common.interfaces.BrokerSubmitMerchandiseManager;
