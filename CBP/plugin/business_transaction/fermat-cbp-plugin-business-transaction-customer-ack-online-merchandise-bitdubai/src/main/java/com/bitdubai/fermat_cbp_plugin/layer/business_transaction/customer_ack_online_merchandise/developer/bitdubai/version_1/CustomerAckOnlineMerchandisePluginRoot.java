7ceb58e54cd3b70d56b77f8c8bfc1bb50a5b57b2
Manuel <darkpriestrelative@gmail.com>
2015-12-23 09:38:40 -0430

    Starting Customer Ack Online Merchandise plugin #4025

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-ack-online-merchandise-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_ack_online_merchandise/developer/bitdubai/version_1/CustomerAckOnlineMerchandisePluginRoot.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-ack-online-merchandise-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_ack_online_merchandise/developer/bitdubai/version_1/CustomerAckOnlineMerchandisePluginRoot.java
@@ -95,7 +95,7 @@ public class CustomerAckOnlineMerchandisePluginRoot extends AbstractPlugin imple
     @Override
     public List<String> getClassesFullPath() {
         List<String> returnedClasses = new ArrayList<String>();
-        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.CustomerOnlinePaymentPluginRoot");
+        returnedClasses.add("com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_ack_online_merchandise.developer.bitdubai.version_1.CustomerAckOnlineMerchandisePluginRoot");
         return returnedClasses;
     }
 
