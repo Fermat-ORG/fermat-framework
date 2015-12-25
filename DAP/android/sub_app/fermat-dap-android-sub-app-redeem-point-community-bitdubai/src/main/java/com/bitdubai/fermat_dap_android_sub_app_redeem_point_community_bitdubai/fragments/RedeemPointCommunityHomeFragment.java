04301005d94fe8ba2a828df65783058c19e114e9
Nerio Indriago <nerioindriago@gmail.com>
2015-12-25 01:37:34 -0430

    DAP - Sub App - Asset Redeem Point Community - QA - Android - FIX #4095

--- DAP/android/sub_app/fermat-dap-android-sub-app-redeem-point-community-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_redeem_point_community_bitdubai/fragments/RedeemPointCommunityHomeFragment.java
+++ DAP/android/sub_app/fermat-dap-android-sub-app-redeem-point-community-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_redeem_point_community_bitdubai/fragments/RedeemPointCommunityHomeFragment.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,16 +15,20 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.FermatFragment;
 import com.bitdubai.fermat_android_api.ui.interfaces.FermatWorkerCallBack;
 import com.bitdubai.fermat_android_api.ui.util.FermatWorker;
 import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.R;
-import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.adapters.ActorAdapter;
+import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.adapters.RedeemPointCommunityAdapter;
 import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.interfaces.AdapterChangeListener;
 import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.models.Actor;
 import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.sessions.AssetRedeemPointCommunitySubAppSession;
+import com.bitdubai.fermat_dap_api.layer.all_definition.exceptions.CantGetIdentityRedeemPointException;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.redeem_point.RedeemPointActorRecord;
 import com.bitdubai.fermat_dap_api.layer.dap_actor.redeem_point.interfaces.ActorAssetRedeemPoint;
 import com.bitdubai.fermat_dap_api.layer.dap_sub_app_module.redeem_point_community.interfaces.RedeemPointCommunitySubAppModuleManager;
@@ -37,9 +40,9 @@ import java.util.List;
 /**
  * Home Fragment
  */
-public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.OnRefreshListener {
+public class RedeemPointCommunityHomeFragment extends FermatFragment implements SwipeRefreshLayout.OnRefreshListener {
 
-    private RedeemPointCommunitySubAppModuleManager manager;
+    private static RedeemPointCommunitySubAppModuleManager manager;
     private List<Actor> actors;
     ErrorManager errorManager;
 
@@ -47,15 +50,18 @@ public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.O
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
     private GridLayoutManager layoutManager;
-    private ActorAdapter adapter;
+    private RedeemPointCommunityAdapter adapter;
+    private View rootView;
+    private LinearLayout emptyView;
+    private int offset = 0;
 
     /**
      * Flags
      */
     private boolean isRefreshing = false;
 
-    public static HomeFragment newInstance() {
-        return new HomeFragment();
+    public static RedeemPointCommunityHomeFragment newInstance() {
+        return new RedeemPointCommunityHomeFragment();
     }
 
     @Override
@@ -70,16 +76,15 @@ public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.O
         }
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.home_dap_redeem_point_fragment, container, false);
-        configureToolbar();
+
+        rootView = inflater.inflate(R.layout.home_dap_redeem_point_fragment, container, false);
         recyclerView = (RecyclerView) rootView.findViewById(R.id.gridView);
         recyclerView.setHasFixedSize(true);
         layoutManager = new GridLayoutManager(getActivity(), 3, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new ActorAdapter(getActivity());
+        adapter = new RedeemPointCommunityAdapter(getActivity());
         adapter.setAdapterChangeListener(new AdapterChangeListener<Actor>() {
             @Override
             public void onDataSetChanged(List<Actor> dataSet) {
@@ -91,12 +96,17 @@ public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.O
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setColorSchemeColors(Color.BLUE, Color.BLUE);
 
+        rootView.setBackgroundColor(Color.parseColor("#000b12"));
+        emptyView = (LinearLayout) rootView.findViewById(R.id.empty_view);
+        swipeRefreshLayout.setRefreshing(true);
+        onRefresh();
+
         return rootView;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.clear();
+        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.dap_community_redeem_point_home_menu, menu);
     }
 
@@ -112,16 +122,16 @@ public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.O
             });
     }
 
-    private void configureToolbar() {
-        Toolbar toolbar = getPaintActivtyFeactures().getToolbar();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            toolbar.setBackground(getResources().getDrawable(R.drawable.dap_action_bar_gradient_colors, null));
-        else
-            toolbar.setBackground(getResources().getDrawable(R.drawable.dap_action_bar_gradient_colors));
-
-        toolbar.setTitleTextColor(Color.WHITE);
-    }
+//    private void configureToolbar() {
+//        Toolbar toolbar = getPaintActivtyFeactures().getToolbar();
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+//            toolbar.setBackground(getResources().getDrawable(R.drawable.dap_action_bar_gradient_colors, null));
+//        else
+//            toolbar.setBackground(getResources().getDrawable(R.drawable.dap_action_bar_gradient_colors));
+//
+//        toolbar.setTitleTextColor(Color.WHITE);
+//    }
 
 
     @Override
@@ -170,6 +180,26 @@ public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.O
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void showEmpty(boolean show, View emptyView) {
+        Animation anim = AnimationUtils.loadAnimation(getActivity(),
+                show ? android.R.anim.fade_in : android.R.anim.fade_out);
+        if (show &&
+                (emptyView.getVisibility() == View.GONE || emptyView.getVisibility() == View.INVISIBLE)) {
+            emptyView.setAnimation(anim);
+            emptyView.setVisibility(View.VISIBLE);
+            if (adapter != null)
+                adapter.changeDataSet(null);
+        } else if (!show && emptyView.getVisibility() == View.VISIBLE) {
+            emptyView.setAnimation(anim);
+            emptyView.setVisibility(View.GONE);
+        }
+    }
+
+    private void setUpScreen(LayoutInflater layoutInflater) throws CantGetIdentityRedeemPointException {
+
+    }
+
     @Override
     public void onRefresh() {
         if (!isRefreshing) {
@@ -195,8 +225,14 @@ public class HomeFragment extends FermatFragment implements SwipeRefreshLayout.O
                         if (getActivity() != null && adapter != null) {
                             actors = (ArrayList<Actor>) result[0];
                             adapter.changeDataSet(actors);
+                            if (actors.isEmpty()) {
+                                showEmpty(true, emptyView);
+                            } else {
+                                showEmpty(false, emptyView);
+                            }
                         }
-                    }
+                    } else
+                        showEmpty(true, emptyView);
                 }
 
                 @Override
