0d0795cd46bcfb18dc42a60bc67172779cd5c066
yalayn <y.alayn@gmail.com>
2015-12-27 18:11:53 -0430

    CBP-Netgotiation Transaction - Customer Broker Close - Crypto Address utils

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/negotiation_transaction/customer_broker_close/utils/WalletManagerSelector.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/negotiation_transaction/customer_broker_close/utils/WalletManagerSelector.java
@@ -1,7 +1,26 @@
 package com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_close.utils;
 
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_close.exceptions.CantIdentifyWalletManagerException;
+import com.bitdubai.fermat_wpd_api.layer.wpd_middleware.wallet_manager.interfaces.WalletManagerManager;
+
 /**
- * Created by yordin on 27/12/15.
+ * Created by Yordin Alayn on 27.12.15.
  */
 public class WalletManagerSelector {
+
+    private final WalletManagerManager walletManagerManager;
+
+    public WalletManagerSelector(final WalletManagerManager walletManagerManager) {
+        this.walletManagerManager = walletManagerManager;
+    }
+
+    public final WalletManagerManager getWalletManager(final Platforms platform) throws CantIdentifyWalletManagerException {
+        switch(platform) {
+
+            case CRYPTO_CURRENCY_PLATFORM: return walletManagerManager;
+            default:throw new CantIdentifyWalletManagerException("CBP-NEGOTIATION TRANSACTION-CUSTOMER BROKER CLOSE. Unexpected Platform: "+platform.toString()+" - "+ platform.getCode());
+
+        }
+    }
 }
