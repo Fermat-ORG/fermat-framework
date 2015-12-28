d757692739d0c4fd66622c28d308689f7316a738
Guillermo Gutierrez <guillermo20@gmail.com>
2015-12-28 10:07:58 -0430

    mostrando transacciones en reference wallet

--- BNK/android/reference_wallet/fermat-bnk-android-reference-wallet-bank-money-bitdubai/src/main/java/com/bitdubai/reference_wallet/bank_money_wallet/fragments/summary/AccountDetailFragment.java
+++ BNK/android/reference_wallet/fermat-bnk-android-reference-wallet-bank-money-bitdubai/src/main/java/com/bitdubai/reference_wallet/bank_money_wallet/fragments/summary/AccountDetailFragment.java
@@ -1,22 +1,42 @@
 package com.bitdubai.reference_wallet.bank_money_wallet.fragments.summary;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_android_api.ui.enums.FermatRefreshTypes;
 import com.bitdubai.fermat_android_api.ui.fragments.FermatWalletListFragment;
+import com.bitdubai.fermat_android_api.ui.interfaces.FermatListItemListeners;
+import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Wallets;
 import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.interfaces.BankAccountNumber;
 import com.bitdubai.fermat_bnk_api.layer.bnk_wallet.bank_money.interfaces.BankMoneyTransactionRecord;
+import com.bitdubai.fermat_bnk_api.layer.bnk_wallet_module.interfaces.BankMoneyWalletModuleManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedWalletExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
 import com.bitdubai.reference_wallet.bank_money_wallet.R;
+import com.bitdubai.reference_wallet.bank_money_wallet.session.BankMoneyWalletSession;
+import com.bitdubai.reference_wallet.bank_money_wallet.util.CommonLogger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by memo on 08/12/15.
  */
-public class AccountDetailFragment extends FermatWalletListFragment<BankMoneyTransactionRecord> {
+public class AccountDetailFragment extends FermatWalletListFragment<BankMoneyTransactionRecord> implements FermatListItemListeners<BankMoneyTransactionRecord>, DialogInterface.OnDismissListener  {
 
+
+    private BankMoneyWalletModuleManager moduleManager;
+    private ErrorManager errorManager;
+    private ArrayList<BankMoneyTransactionRecord> transactionList;
+    private String walletPublicKey= "banking_wallet";
+    private BankAccountNumber bankAccountNumber;
+
+    private static final String TAG = "AccountListActivityFragment";
     public AccountDetailFragment() {
     }
 
@@ -35,7 +55,30 @@ public class AccountDetailFragment extends FermatWalletListFragment<BankMoneyTra
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         BankAccountNumber data = (BankAccountNumber)appSession.getData("account_data");
+        try {
+
+            moduleManager = ((BankMoneyWalletSession) appSession).getModuleManager();
+            errorManager = appSession.getErrorManager();
+        } catch (Exception ex) {
+            CommonLogger.exception(TAG, ex.getMessage(), ex);
+            if (errorManager != null)
+                errorManager.reportUnexpectedWalletException(
+                        Wallets.BNK_BANKING_WALLET, UnexpectedWalletExceptionSeverity.DISABLES_THIS_FRAGMENT, ex);
+        }
         System.out.println("DATA ="+data.getAccount());
+        bankAccountNumber = data;
+        transactionList = (ArrayList)getMoreDataAsync(FermatRefreshTypes.NEW, 0);
+    }
+
+    @Override
+    protected void initViews(View layout) {
+        super.initViews(layout);
+        configureToolbar();
+        //showOrHideNoAccountListView(accountsList.isEmpty());
+    }
+
+    private void configureToolbar() {
+        getToolbar().setBackgroundColor(getResources().getColor(R.color.background_header));
     }
 
     @Override
@@ -81,5 +124,38 @@ public class AccountDetailFragment extends FermatWalletListFragment<BankMoneyTra
     @Override
     public RecyclerView.LayoutManager getLayoutManager() {
         return null;
+    }
+
+    @Override
+    public void onItemClickListener(BankMoneyTransactionRecord data, int position) {
+
+    }
+
+    @Override
+    public void onLongItemClickListener(BankMoneyTransactionRecord data, int position) {
+
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialogInterface) {
+
+    }
+
+    @Override
+    public List<BankMoneyTransactionRecord> getMoreDataAsync(FermatRefreshTypes refreshType, int pos){
+        List<BankMoneyTransactionRecord> data = new ArrayList<>();
+        if (moduleManager != null) {
+            try {
+                data.addAll(moduleManager.getBankingWallet().getTransactions(bankAccountNumber.getAccount()));
+
+            } catch (Exception ex) {
+                if (errorManager != null)
+                    errorManager.reportUnexpectedWalletException(Wallets.BNK_BANKING_WALLET, UnexpectedWalletExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_FRAGMENT, ex);
+            }
+        } else {
+            Toast.makeText(getActivity(), "Sorry, an error happened in BalanceSummaryFragment (Module == null)", Toast.LENGTH_SHORT).show();
+        }
+        return data;
+
     }
 }
