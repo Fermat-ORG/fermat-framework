f14c9722cc21636e2e8c48c6e64f3f0d49128228
Nelson Ramirez <nelsonalfo@gmail.com>
2015-12-29 18:52:46 -0430

    creada logica para la pantalla de crear locacion

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/CreateNewLocationFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/CreateNewLocationFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatButton;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatEditText;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Country;
+import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Activities;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
 
-/**
- * Created by nelson on 29/12/15.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CreateNewLocationFragment extends AbstractFermatFragment {
+    public static final String NEW_LOCATION = "location";
+
+    private Country[] countries;
+    private Country selectedCountry;
+
 
     public static CreateNewLocationFragment newInstance() {
         return new CreateNewLocationFragment();
@@ -23,6 +38,70 @@ public class CreateNewLocationFragment extends AbstractFermatFragment {
 
         View layout = inflater.inflate(R.layout.cbw_fragement_create_new_location, container, false);
 
+        countries = Country.values();
+
+        final Spinner countrySpinner = (Spinner) layout.findViewById(R.id.cbw_country_spinner);
+        countrySpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedCountry = countries[position];
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, getList(countries));
+        countrySpinner.setAdapter(adapter);
+        countrySpinner.setSelection(0);
+
+        final FermatEditText cityTextView = (FermatEditText) layout.findViewById(R.id.cbw_city_edit_text);
+
+        final FermatEditText stateEditText = (FermatEditText) layout.findViewById(R.id.cbw_state_edit_text);
+        final FermatEditText zipCodeEditText = (FermatEditText) layout.findViewById(R.id.cbw_zip_code_edit_text);
+        final FermatEditText addressLineOneEditText = (FermatEditText) layout.findViewById(R.id.cbw_address_line_1_edit_text);
+        final FermatEditText addressLineTwoEditText = (FermatEditText) layout.findViewById(R.id.cbw_address_line_2_edit_text);
+
+
+        final FermatButton createLocationButton = (FermatButton) layout.findViewById(R.id.cbw_create_new_location_button);
+        createLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuffer location = new StringBuffer();
+
+                location.append(selectedCountry.getCountry());
+
+                if (cityTextView.getText().toString().length() > 0)
+                    location.append(cityTextView.getText().toString()).append(", ");
+
+                if (stateEditText.getText().toString().length() > 0)
+                    location.append(stateEditText.getText().toString()).append(", ");
+
+                if (zipCodeEditText.getText().toString().length() > 0)
+                    location.append(zipCodeEditText.getText().toString()).append(", ");
+
+                if (addressLineOneEditText.getText().toString().length() > 0)
+                    location.append(addressLineOneEditText.getText().toString()).append(", ");
+
+                if (addressLineTwoEditText.getText().toString().length() > 0)
+                    location.append(addressLineTwoEditText.getText().toString()).append(", ");
+
+                if (location.length() > 0) {
+                    appSession.setData(NEW_LOCATION, location);
+                    changeActivity(Activities.CBP_CRYPTO_BROKER_WALLET_SET_LOCATIONS, appSession.getAppPublicKey());
+
+                } else {
+                    Toast.makeText(getActivity(), "Need to set the fields", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
         return layout;
+    }
+
+    private List<String> getList(Country[] countries) {
+        List<String> data = new ArrayList<>();
+
+        for (int i = 0; i < countries.length; i++)
+            data.add(countries[i].getCountry());
+
+        return data;
     }
 }
