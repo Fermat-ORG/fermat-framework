42bbea0b08bd665dea8113218f7e98fda1009b9a
yalayn <y.alayn@gmail.com>
2015-12-29 14:59:25 -0430

    CBP - Network Service - Negotiation Transmission - Reference

---
+++ CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/version_1/structure/NegotiationTransmissionManagerImpl.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by yordin on 29/12/15.
+ */
+public class NegotiationTransmissionManagerImpl {
+}
