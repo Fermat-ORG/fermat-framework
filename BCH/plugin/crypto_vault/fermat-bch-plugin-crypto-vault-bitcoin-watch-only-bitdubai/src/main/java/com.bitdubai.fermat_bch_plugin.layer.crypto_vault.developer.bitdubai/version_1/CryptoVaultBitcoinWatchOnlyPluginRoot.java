cbf919085735ec851fd30b33e20c4fa0b885578b
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-12-30 16:20:03 -0300

    Fix #2505

--- BCH/plugin/crypto_vault/fermat-bch-plugin-crypto-vault-bitcoin-watch-only-bitdubai/src/main/java/com.bitdubai.fermat_bch_plugin.layer.crypto_vault.developer.bitdubai/version_1/CryptoVaultBitcoinWatchOnlyPluginRoot.java
+++ BCH/plugin/crypto_vault/fermat-bch-plugin-crypto-vault-bitcoin-watch-only-bitdubai/src/main/java/com.bitdubai.fermat_bch_plugin.layer.crypto_vault.developer.bitdubai/version_1/CryptoVaultBitcoinWatchOnlyPluginRoot.java
@@ -1,7 +1,39 @@
 package com.bitdubai.fermat_bch_plugin.layer.crypto_vault.developer.bitdubai.version_1;
 
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+import com.bitdubai.fermat_api.layer.all_definition.enums.BlockchainNetworkType;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
+import com.bitdubai.fermat_api.layer.all_definition.money.CryptoAddress;
+import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_bch_api.layer.crypto_vault.exceptions.GetNewCryptoAddressException;
+import com.bitdubai.fermat_bch_api.layer.crypto_vault.watch_only_vault.ExtendedPublicKey;
+import com.bitdubai.fermat_bch_api.layer.crypto_vault.watch_only_vault.exceptions.CantInitializeWatchOnlyVaultException;
+import com.bitdubai.fermat_bch_api.layer.crypto_vault.watch_only_vault.interfaces.WatchOnlyVaultManager;
+
+import javax.annotation.Nullable;
+
 /**
  * Created by rodrigo on 12/30/15.
  */
-public class CryptoVaultBitcoinWatchOnlyPluginRoot {
+public class CryptoVaultBitcoinWatchOnlyPluginRoot extends AbstractPlugin implements WatchOnlyVaultManager {
+
+    public CryptoVaultBitcoinWatchOnlyPluginRoot() {
+        super(new PluginVersionReference(new Version()));
+    }
+
+        @Override
+    public void initialize(ExtendedPublicKey extendedPublicKey) throws CantInitializeWatchOnlyVaultException {
+
+    }
+
+    @Override
+    public CryptoAddress getCryptoAddress(@Nullable BlockchainNetworkType blockchainNetworkType) throws GetNewCryptoAddressException {
+        return null;
+    }
+
+    @Override
+    public Platforms getPlatform() {
+        return null;
+    }
 }
