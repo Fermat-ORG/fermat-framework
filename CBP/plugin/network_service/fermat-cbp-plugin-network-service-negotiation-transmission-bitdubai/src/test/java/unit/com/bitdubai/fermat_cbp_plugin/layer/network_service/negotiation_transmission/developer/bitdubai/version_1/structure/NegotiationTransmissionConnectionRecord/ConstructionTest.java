d018ef5f1465684bbdc7f1f83c734b7ac634d280
yalayn <y.alayn@gmail.com>
2015-12-30 18:12:02 -0430

    CBP - Network Service - Negotiation Transmission - Unit Tests

--- CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/version_1/structure/NegotiationTransmissionConnectionRecord/ConstructionTest.java
+++ CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/version_1/structure/NegotiationTransmissionConnectionRecord/ConstructionTest.java
@@ -2,6 +2,7 @@ package unit.com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_tr
 
 import com.bitdubai.fermat_cbp_plugin.layer.network_service.negotiation_transmission.developer.bitdubai.version_1.structure.NegotiationTransmissionConnectionRecord;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -18,24 +19,71 @@ public class ConstructionTest {
 
     private UUID    id                  = UUID.randomUUID();
 
-    private String  actorPublicKey      = "18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725";;
+    private String  actorPublicKey      = "18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725";
+
+    private String  actorPublicKey2     = "0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B235";
 
     private String  ipkNetworkService   = "ipkNetworkService";
 
+    private String  ipkNetworkService2  = "ipkNetworkService2";
+
     private String  lastConnection      = "301215";
 
-    private NegotiationTransmissionConnectionRecord testMonitorAgent;
+    private NegotiationTransmissionConnectionRecord testObj1, testObj2;
+
+    @Before
+    public void setUp(){
+        System.out.print("\n*** UNNIT TESTS: NegotiationTransmissionConnectionRecord - ConstructionTest");
+        testObj1 = new NegotiationTransmissionConnectionRecord(
+                id,
+                actorPublicKey,
+                ipkNetworkService,
+                lastConnection
+        );
+    }
 
     @Test
     public void Construction_ValidParameters_NewObjectCreated() {
+        System.out.print("\n* Construction_ValidParameters_NewObjectCreated");
+        assertThat(testObj1).isNotNull();
+    }
 
-        testMonitorAgent = new NegotiationTransmissionConnectionRecord(
-            id,
-            actorPublicKey,
-            ipkNetworkService,
-            lastConnection
+    @Test
+    public void Equals_SameValues_True(){
+        System.out.print("\n* Equals_SameValues_True");
+        testObj2 = new NegotiationTransmissionConnectionRecord(
+                id,
+                actorPublicKey,
+                ipkNetworkService,
+                lastConnection
         );
-        assertThat(testMonitorAgent).isNotNull();
+        assertThat(testObj1).isEqualTo(testObj1);
+        assertThat(testObj1.hashCode()).isEqualTo(testObj1.hashCode());
+    }
 
+    @Test
+    public void Equals_DifferentActorPublicKey_False(){
+        System.out.print("\n* Equals_DifferentActorPublicKey_False");
+        testObj2 = new NegotiationTransmissionConnectionRecord(
+                id,
+                actorPublicKey2,
+                ipkNetworkService,
+                lastConnection
+        );
+        assertThat(testObj1).isNotEqualTo(testObj2);
+        assertThat(testObj1.hashCode()).isNotEqualTo(testObj2.hashCode());
+    }
+
+    @Test
+    public void Equals_DifferentIpkNetworkService_False(){
+        System.out.print("\n* Equals_DifferentIpkNetworkService_False");
+        testObj2 = new NegotiationTransmissionConnectionRecord(
+                id,
+                actorPublicKey,
+                ipkNetworkService2,
+                lastConnection
+        );
+        assertThat(testObj1).isNotEqualTo(testObj2);
+        assertThat(testObj1.hashCode()).isNotEqualTo(testObj2.hashCode());
     }
 }
