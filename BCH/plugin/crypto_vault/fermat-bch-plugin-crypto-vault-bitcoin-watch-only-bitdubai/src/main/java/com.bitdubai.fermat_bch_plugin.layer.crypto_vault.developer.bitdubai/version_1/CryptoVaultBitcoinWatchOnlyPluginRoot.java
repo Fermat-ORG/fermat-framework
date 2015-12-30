d79cddab3f9e5a55fa6b694385fa85a44c9ab697
acostarodrigo <acosta_rodrigo@hotmail.com>
2015-12-30 19:51:02 -0300

    Fix #2504

--- BCH/plugin/crypto_vault/fermat-bch-plugin-crypto-vault-bitcoin-watch-only-bitdubai/src/main/java/com.bitdubai.fermat_bch_plugin.layer.crypto_vault.developer.bitdubai/version_1/CryptoVaultBitcoinWatchOnlyPluginRoot.java
+++ BCH/plugin/crypto_vault/fermat-bch-plugin-crypto-vault-bitcoin-watch-only-bitdubai/src/main/java/com.bitdubai.fermat_bch_plugin.layer.crypto_vault.developer.bitdubai/version_1/CryptoVaultBitcoinWatchOnlyPluginRoot.java
@@ -23,40 +23,77 @@ import javax.annotation.Nullable;
 /**
  * Created by rodrigo on 12/30/15.
  */
-public class CryptoVaultBitcoinWatchOnlyPluginRoot extends AbstractPlugin implements WatchOnlyVaultManager, DatabaseManagerForDevelopers {
+public class CryptoVaultBitcoinWatchOnlyPluginRoot extends AbstractPlugin implements DatabaseManagerForDevelopers, WatchOnlyVaultManager {
 
+    /**
+     * Constructor
+     */
     public CryptoVaultBitcoinWatchOnlyPluginRoot() {
         super(new PluginVersionReference(new Version()));
     }
 
+    /**
+     * DatabaseManagerForDevelopers implementation
+     * @param developerObjectFactory
+     * @return
+     */
     @Override
     public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
         return null;
     }
 
+    /**
+     * DatabaseManagerForDevelopers implementation
+     * @param developerObjectFactory
+     * @param developerDatabase
+     * @return
+     */
     @Override
     public List<DeveloperDatabaseTable> getDatabaseTableList(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase) {
         return null;
     }
 
+    /**
+     * DatabaseManagerForDevelopers implementation
+     * @param developerObjectFactory
+     * @param developerDatabase
+     * @param developerDatabaseTable
+     * @return
+     */
     @Override
     public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
         return null;
     }
 
+    /**
+     * Will initialize the Vault by deriving the keys, starting the agents and start monitoring the network
+     * @param extendedPublicKey
+     * @throws CantInitializeWatchOnlyVaultException
+     */
     @Override
     public void initialize(ExtendedPublicKey extendedPublicKey) throws CantInitializeWatchOnlyVaultException {
+        if (extendedPublicKey == null)
+            throw new CantInitializeWatchOnlyVaultException(CantInitializeWatchOnlyVaultException.DEFAULT_MESSAGE, null, "Extended public Key received is null. Can't go on.", null);
 
     }
 
+    /**
+     * Generates a Crypto Address for the specified Network.
+     * @param blockchainNetworkType DEFAULT if null value is passed.
+     * @return the newly generated crypto Address.
+     */
     @Override
     public CryptoAddress getCryptoAddress(@Nullable BlockchainNetworkType blockchainNetworkType) throws GetNewCryptoAddressException {
         return null;
     }
 
+    /**
+     * Gets the serving platform this vault is working on.
+     * @return a Fermat platform
+     */
     @Override
     public Platforms getPlatform() {
-        return null;
+        return Platforms.BLOCKCHAINS;
     }
 
 
