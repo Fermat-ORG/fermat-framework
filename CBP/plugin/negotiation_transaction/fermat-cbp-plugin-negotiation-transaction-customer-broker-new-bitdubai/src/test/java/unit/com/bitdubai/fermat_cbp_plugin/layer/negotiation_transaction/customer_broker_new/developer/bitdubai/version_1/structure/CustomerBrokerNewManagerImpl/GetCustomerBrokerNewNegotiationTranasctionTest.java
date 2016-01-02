c90334af96a0949646ebba59395d992787f59a0d
yalayn <y.alayn@gmail.com>
2016-01-02 12:45:23 -0430

    CBP - Negotiation Transaction - Customer Broker New - Unit Tests

---
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-new-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_new/developer/bitdubai/version_1/structure/CustomerBrokerNewManagerImpl/GetCustomerBrokerNewNegotiationTranasctionTest.java
@@ -0,0 +1,35 @@
+package unit.com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_new.developer.bitdubai.version_1.structure.CustomerBrokerNewManagerImpl;
+
+import com.bitdubai.fermat_cbp_api.layer.negotiation_transaction.customer_broker_new.interfaces.CustomerBrokerNew;
+import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_new.developer.bitdubai.version_1.structure.CustomerBrokerNewManagerImpl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.UUID;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Yordin Alayn on 02.01.16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class GetCustomerBrokerNewNegotiationTranasctionTest {
+
+    private UUID transactionId = UUID.randomUUID();
+
+    @Test
+    public void getCustomerBrokerNewNegotiationTranasction() throws Exception{
+
+        CustomerBrokerNew customerBrokerNew = null;
+
+        CustomerBrokerNewManagerImpl customerBrokerNewManagerImpl = mock(CustomerBrokerNewManagerImpl.class, Mockito.RETURNS_DEEP_STUBS);
+        when(customerBrokerNewManagerImpl.getCustomerBrokerNewNegotiationTranasction(transactionId)).thenReturn(customerBrokerNew).thenCallRealMethod();
+        assertThat(customerBrokerNewManagerImpl.getCustomerBrokerNewNegotiationTranasction(transactionId)).isNotNull();
+
+    }
+}
