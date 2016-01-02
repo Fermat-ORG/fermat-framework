28b17aee2bd2bafe26cf642a63af6a6a6777e3d9
mati <mati_fur@hotmail.com>
2016-01-02 11:19:02 -0300

    desktop notifications improved

--- DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-manager-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_manager/adapter/DesktopAdapter.java
+++ DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-manager-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_manager/adapter/DesktopAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Build;
 import android.view.View;
@@ -83,23 +84,31 @@ public class DesktopAdapter extends FermatAdapter<Item, FermatAppHolder> impleme
             if(data.getIconResource()!=0)
                 if(data.getType()!= InterfaceType.EMPTY) {
                     //holder.thumbnail.setImageResource(data.getIconResource());
-                    LayerDrawable icon = (LayerDrawable) holder.thumbnail.getDrawable();
+                    //LayerDrawable icon = (LayerDrawable) holder.thumbnail.getDrawable();
                     // Update LayerDrawable's BadgeDrawable
-                    if(data.getIconResource()!=-1) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            icon.setDrawableByLayerId(R.id.ic_icon, context.getDrawable(data.getIconResource()));
-                        }
-                        if (position == 0) {
-                            if(DEKSTOP==fragmentWhoUseThisAdapter)
-                            BadgeDrawable.Utils.setBadgeCount(context, icon, 2, icon.getId(1));
-                        }
-                    }else {
-                        holder.thumbnail.setVisibility(View.GONE);
-                        icon.getDrawable(0).setVisible(false,true);
-                        icon.getDrawable(1).setVisible(false,true);
-                        icon.setDrawableByLayerId(R.id.ic_icon,null);
-                        icon.setVisible(false,true);
+//                    if(data.getIconResource()!=-1) {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                        Drawable[] layers = new Drawable[]{context.getDrawable(data.getIconResource()),new BadgeDrawable.BadgeDrawableBuilder(context).setTextSize(38).setCount(data.getNotifications()).build()};
+                        LayerDrawable icon = new LayerDrawable(layers);
+                        holder.thumbnail.setImageDrawable(icon);
+                    }else{
+                        holder.thumbnail.setImageResource(data.getIconResource());
                     }
+
+//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                            icon.setDrawableByLayerId(R.id.ic_icon, context.getDrawable(data.getIconResource()));
+//                        }
+//                        if (position == 0) {
+//                            if(DEKSTOP==fragmentWhoUseThisAdapter)
+//                            BadgeDrawable.Utils.setBadgeCount(context, icon, 2, icon.getId(1));
+//                        }
+//                    }else {
+//                        holder.thumbnail.setVisibility(View.GONE);
+//                        icon.getDrawable(0).setVisible(false,true);
+//                        icon.getDrawable(1).setVisible(false,true);
+//                        icon.setDrawableByLayerId(R.id.ic_icon,null);
+//                        icon.setVisible(false,true);
+//                    }
                 }
             if(data.getType()== InterfaceType.FOLDER){
                 holder.thumbnail.setVisibility(View.GONE);
