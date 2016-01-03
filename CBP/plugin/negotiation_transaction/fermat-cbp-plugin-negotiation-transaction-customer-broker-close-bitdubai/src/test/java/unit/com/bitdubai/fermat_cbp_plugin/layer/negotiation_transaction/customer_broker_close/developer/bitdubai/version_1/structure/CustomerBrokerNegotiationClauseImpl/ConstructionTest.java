a2ea783dca58734476e72291c6b84eed0f5c0f85
yalayn <y.alayn@gmail.com>
2016-01-03 02:23:30 -0430

    CBP - Negotiation Transaction - Customer Broker Close - Unit Tests

--- CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-close-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_close/developer/bitdubai/version_1/structure/CustomerBrokerNegotiationClauseImpl/ConstructionTest.java
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-close-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_close/developer/bitdubai/version_1/structure/CustomerBrokerNegotiationClauseImpl/ConstructionTest.java
@@ -1,7 +1,45 @@
 package unit.com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_close.developer.bitdubai.version_1.structure.CustomerBrokerNegotiationClauseImpl;
 
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseStatus;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseType;
+import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_close.developer.bitdubai.version_1.structure.CustomerBrokerNegotiationClauseImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 /**
- * Created by yordin on 02/01/16.
+ * Created by Yordin Alayn on 02.01.16.
  */
 public class ConstructionTest {
+
+    private final UUID          clauseId    = UUID.randomUUID();
+    private final ClauseType    type        = ClauseType.BROKER_CRYPTO_ADDRESS;
+    private final String        value       = "value";
+    private final ClauseStatus  status      = ClauseStatus.AGREED;
+    private final String        proposedBy  = "proposedBy";
+    private final short         indexOrder  = 1;
+
+    private CustomerBrokerNegotiationClauseImpl testObj1;
+
+    @Before
+    public void setUp(){
+        testObj1 = new CustomerBrokerNegotiationClauseImpl(
+            clauseId,
+            type,
+            value,
+            status,
+            proposedBy,
+            indexOrder
+        );
+    }
+
+    @Test
+    public void Construction_ValidParameters_NewObjectCreated() {
+        System.out.print("\n* Construction_ValidParameters_NewObjectCreated");
+        assertThat(testObj1).isNotNull();
+    }
 }
