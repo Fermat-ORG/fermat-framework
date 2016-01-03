fe801ba6b00f81accec7d8ffeb7558f05fe06f35
yalayn <y.alayn@gmail.com>
2016-01-03 02:32:13 -0430

    CBP - Negotiation Transaction - Customer Broker Close - Unit Tests

--- CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-close-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_close/developer/bitdubai/version_1/structure/CustomerBrokerNegotiationClauseImpl/ConstructionTest.java
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-close-bitdubai/src/test/java/unit/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_close/developer/bitdubai/version_1/structure/CustomerBrokerNegotiationClauseImpl/ConstructionTest.java
@@ -6,6 +6,8 @@ import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_bro
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.UUID;
 
@@ -14,13 +16,19 @@ import static org.fest.assertions.api.Assertions.assertThat;
 /**
  * Created by Yordin Alayn on 02.01.16.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ConstructionTest {
 
     private final UUID          clauseId    = UUID.randomUUID();
+
     private final ClauseType    type        = ClauseType.BROKER_CRYPTO_ADDRESS;
+
     private final String        value       = "value";
+
     private final ClauseStatus  status      = ClauseStatus.AGREED;
+
     private final String        proposedBy  = "proposedBy";
+
     private final short         indexOrder  = 1;
 
     private CustomerBrokerNegotiationClauseImpl testObj1;
