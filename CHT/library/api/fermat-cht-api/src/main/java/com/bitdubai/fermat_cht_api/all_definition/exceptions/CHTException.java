36b588acf869fe63fd39133b4360f4988353cb6a
Gabriel Araujo <gabe_512@hotmail.com>
2016-01-04 13:21:31 -0430

    CHTException for CHAT PLATFORM EXCEPTION CantInitializeCommunicationNetworkServiceConnectionManagerException
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

---
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/all_definition/exceptions/CHTException.java
@@ -0,0 +1,37 @@
+package com.bitdubai.fermat_cht_api.all_definition.exceptions;
+
+import com.bitdubai.fermat_api.FermatException;
+
+/**
+ * Created by Gabriel Araujo (gabe_512@hotmail.com) 04/01/2016.
+ */
+// TODO DELETE OR MOVE THIS, IT DOESN'T BELONG HERE
+public class CHTException extends FermatException {
+
+    public static final String DEFAULT_MESSAGE = "THE CHAT PLATFORM HAS DETECTED AN EXCEPTION";
+
+    public CHTException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    public CHTException(Exception cause, String context, String possibleReason) {
+        super(DEFAULT_MESSAGE , cause, context, possibleReason);
+    }
+
+    public CHTException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    public CHTException(final String message) {
+        this(message, null);
+    }
+
+    public CHTException(final Exception exception) {
+        this(exception.getMessage());
+        setStackTrace(exception.getStackTrace());
+    }
+
+    public CHTException() {
+        this(DEFAULT_MESSAGE);
+    }
+}
