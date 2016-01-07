385f89dab705abb5e05936a4b85edea342a4e525
Gabriel Araujo <gabe_512@hotmail.com>
2016-01-07 01:32:54 -0430

    CHT - Network Service - chat - Implementation - Public Interfaces - chatManager #4137
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

--- CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/network_service/chat/interfaces/ChatMetada.java
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/network_service/chat/interfaces/ChatMetada.java
@@ -1,38 +1,35 @@
 package com.bitdubai.fermat_cht_api.layer.network_service.chat.interfaces;
 
-import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
-import com.bitdubai.fermat_api.layer.all_definition.components.enums.PlatformComponentType;
 import com.bitdubai.fermat_cht_api.layer.network_service.chat.enums.ChatMessageStatus;
+import com.bitdubai.fermat_cht_api.layer.network_service.chat.enums.DistributionStatus;
+import java.sql.Timestamp;
 
 /**
  * Created by root on 05/01/16.
  */
 public interface ChatMetada {
 
+    Integer getIdChat();
+
+    Integer getIdObjecto();
+
+    String getLocalActorType();
+
+    String getLocalActorPubKey();
+
+    String getRemoteActorType();
+
+    String getRemoteActorPubKey();
+
+    String getChatName();
+
     ChatMessageStatus getChatMessageStatus();
 
-    /**
-     * Get the Receiver Id
-     *
-     * @return String
-     */
-    String getReceiverId();
+    Timestamp getDate();
 
-    /**
-     * The platform component that this event is destinated for.
-     * @return {@link PlatformComponentType}
-     */
-    PlatformComponentType getReceiverType();
-    /**
-     * Get the Sender Id
-     *
-     * @return String
-     */
-    String getSenderId();
+    Integer getIdMessage();
 
-    /**
-     * The platform component that send this event.
-     * @return {@link PlatformComponentType}
-     */
-    PlatformComponentType getSenderType();
+    String getMessage();
+    DistributionStatus getDistributionStatus();
+
 }
