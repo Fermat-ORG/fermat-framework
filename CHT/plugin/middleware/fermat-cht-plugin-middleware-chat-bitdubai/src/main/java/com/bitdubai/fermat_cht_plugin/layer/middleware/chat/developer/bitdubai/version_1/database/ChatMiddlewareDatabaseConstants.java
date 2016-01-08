6b9ae79609cbb4b9f170203e9e93a03026e40d63
Manuel <darkpriestrelative@gmail.com>
2016-01-07 20:32:19 -0430

    Fix database structure from Middleware Chat plugin

--- CHT/plugin/middleware/fermat-cht-plugin-middleware-chat-bitdubai/src/main/java/com/bitdubai/fermat_cht_plugin/layer/middleware/chat/developer/bitdubai/version_1/database/ChatMiddlewareDatabaseConstants.java
+++ CHT/plugin/middleware/fermat-cht-plugin-middleware-chat-bitdubai/src/main/java/com/bitdubai/fermat_cht_plugin/layer/middleware/chat/developer/bitdubai/version_1/database/ChatMiddlewareDatabaseConstants.java
@@ -1,4 +1,4 @@
-package com.bitdubai.fermat_cht_plugin.layer.middleware.chat.version_1.database;
+package com.bitdubai.fermat_cht_plugin.layer.middleware.chat.developer.bitdubai.version_1.database;
 
 /**
  * The Class <code>com.bitdubai.fermat_cht_plugin.layer.middleware.chat.version_1.database.ChatMiddlewareDatabaseConstants</code>
@@ -17,7 +17,7 @@ public class ChatMiddlewareDatabaseConstants {
     public static final String CHATS_TABLE_NAME = "chats";
 
     public static final String CHATS_ID_CHAT_COLUMN_NAME = "id_chat";
-    public static final String CHATS_ID_OBJETO_COLUMN_NAME = "id_objeto";
+    public static final String CHATS_ID_OBJECT_COLUMN_NAME = "id_object";
     public static final String CHATS_LOCAL_ACTOR_TYPE_COLUMN_NAME = "local_actor_type";
     public static final String CHATS_LOCAL_ACTOR_PUB_KEY_COLUMN_NAME = "local_actor_pub_key";
     public static final String CHATS_REMOTE_ACTOR_TYPE_COLUMN_NAME = "remote_actor_type";
@@ -34,7 +34,7 @@ public class ChatMiddlewareDatabaseConstants {
      */
     public static final String MESSAGE_TABLE_NAME = "message";
 
-    public static final String MESSAGE_ID_MENSAJE_COLUMN_NAME = "id_mensaje";
+    public static final String MESSAGE_ID_MESSAGE_COLUMN_NAME = "id_message";
     public static final String MESSAGE_ID_CHAT_COLUMN_NAME = "id_chat";
     public static final String MESSAGE_TEXT_MESSAGE_COLUMN_NAME = "text_message";
     public static final String MESSAGE_STATUS_COLUMN_NAME = "status";
