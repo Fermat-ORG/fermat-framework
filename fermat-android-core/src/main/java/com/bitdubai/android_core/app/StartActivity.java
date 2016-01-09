597aa6499ad3003ad3f44f629186d42866b5f690
mati <mati_fur@hotmail.com>
2016-01-09 15:26:04 -0300

    android core various changes

--- fermat-android-core/src/main/java/com/bitdubai/android_core/app/StartActivity.java
+++ fermat-android-core/src/main/java/com/bitdubai/android_core/app/StartActivity.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.app.FragmentActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
