b16c1e75156cc0fa24e4b941eb54d2c8cb140b97
Nerio Indriago <nerioindriago@gmail.com>
2016-01-09 00:51:33 -0430

    DAP - Wallet - Redeem Point - QA - Bug Fix Android #3 - #4358

--- DAP/android/reference_wallet/fermat-dap-android-reference-wallet-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_wallet_asset_issuer_bitdubai/common/adapters/MyAssetsAdapter.java
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-asset-issuer-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_wallet_asset_issuer_bitdubai/common/adapters/MyAssetsAdapter.java
@@ -30,7 +30,7 @@ public class MyAssetsAdapter extends FermatAdapter<DigitalAsset, MyAssetsViewHol
 
     @Override
     protected int getCardViewResource() {
-        return R.layout.dap_walletasset_issuer_asset_item;
+        return R.layout.dap_wallet_asset_issuer_asset_item;
     }
 
     @Override
