14f6950aa515d0b31cd44ff62e4e74934c1427b9
Nelson Ramirez <nelsonalfo@gmail.com>
2016-01-09 11:33:49 -0430

    aplicadas las instrucciones "compileOptions {  sourceCompatibility JavaVersion.VERSION_1_7 targetCompatibility JavaVersion.VERSION_1_7 }" en todos los proyectos android de CHT, CSH, BNK y CBP y " apply plugin: 'java'  sourceCompatibility = 1.7  targetCompatibility = 1.7" en todos los plugins de CHT, CSH, BNK y CBP. Ahora si compila sin problemas con JDK 1.8 segun pruebas realizadas por mi

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/start_negotiation/StartNegotiationActivityFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/start_negotiation/StartNegotiationActivityFragment.java
@@ -5,26 +5,48 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+import com.bitdubai.fermat_api.layer.pip_engine.interfaces.ResourceProviderManager;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_customer.exceptions.CantGetCryptoCustomerWalletException;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_customer.interfaces.CryptoCustomerWalletManager;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_customer.interfaces.CryptoCustomerWalletModuleManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
 import com.bitdubai.reference_wallet.crypto_customer_wallet.R;
+import com.bitdubai.reference_wallet.crypto_customer_wallet.session.CryptoCustomerWalletSession;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StartNegotiationActivityFragment extends AbstractFermatFragment {
+public class StartNegotiationActivityFragment extends AbstractFermatFragment<CryptoCustomerWalletSession, ResourceProviderManager> {
 
+    private static final String TAG = "StartNegotiationFrag";
 
-    public StartNegotiationActivityFragment() {
-        // Required empty public constructor
-    }
+    private CryptoCustomerWalletManager walletmanager;
+    private ErrorManager errorManager;
+
 
     public static StartNegotiationActivityFragment newInstance() {
         return new StartNegotiationActivityFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            CryptoCustomerWalletModuleManager moduleManager = appSession.getModuleManager();
+            walletmanager = moduleManager.getCryptoCustomerWallet(appSession.getAppPublicKey());
+            errorManager = appSession.getErrorManager();
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+
     }
 
     @Override
