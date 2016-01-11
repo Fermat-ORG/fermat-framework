d52f3a32b92766f1bf10bb21639b18dd8b085e74
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-01-11 16:12:10 -0430

    Chat - Implementation - Public Interfaces - ChatManager #4315

--- CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/sup_app_module/interfaces/ChatManager.java
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/sup_app_module/interfaces/ChatManager.java
@@ -1,8 +1,59 @@
 package com.bitdubai.fermat_cht_api.layer.sup_app_module.interfaces;
 
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantDeleteChatException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantDeleteContactException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantDeleteMesssageException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantGetChatException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantGetContactException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantGetMessageException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantNewEmptyChatException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantNewEmptyContactException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantNewEmptyMessageException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantSaveChatException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantSaveContactException;
+import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantSaveMessageException;
+import com.bitdubai.fermat_cht_api.layer.middleware.interfaces.Chat;
+import com.bitdubai.fermat_cht_api.layer.middleware.interfaces.Contact;
+import com.bitdubai.fermat_cht_api.layer.middleware.interfaces.Message;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by franklin on 06/01/16.
  */
 public interface ChatManager {
     //TODO: Implementar los metodos que necesiten manejar el module
+    //Documentar
+    List<Chat> getChats() throws CantGetChatException;
+
+    Chat getChatByChatId(UUID chatId) throws CantGetChatException;
+
+    Chat newEmptyInstanceChat() throws CantNewEmptyChatException;
+
+    void saveChat(Chat chat) throws CantSaveChatException;
+
+    void deleteChat(Chat chat) throws CantDeleteChatException;
+
+    List<Message> getMessages() throws CantGetMessageException;
+
+    Message getMessageByChatId(UUID chatId) throws CantGetMessageException;
+
+    Message getMessageByMessageId(UUID messageId) throws CantGetMessageException;
+
+    Message newEmptyInstanceMessage() throws CantNewEmptyMessageException;
+
+    void saveMessage(Message message) throws CantSaveMessageException;
+
+    void deleteMessage(Message message) throws CantDeleteMesssageException;
+
+    List<Contact> getContacts() throws CantGetContactException;
+
+    Contact getContactByContactId(UUID contactId) throws CantGetContactException;
+
+    Contact newEmptyInstanceContact() throws CantNewEmptyContactException;
+
+    void saveContact(Contact contact) throws CantSaveContactException;
+
+    void deleteContact(Contact contact) throws CantDeleteContactException;
 }
