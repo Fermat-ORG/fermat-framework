0e24ff59de41141e6c68e9e21d31ed731ceb5c3e
vlzangel <vlzangel@gmail.com>
2016-01-13 16:04:25 -0430

    interfaces correcciones
    interfaces correcciones

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/actor/crypto_broker/interfaces/CryptoBrokerActorManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/actor/crypto_broker/interfaces/CryptoBrokerActorManager.java
@@ -3,10 +3,7 @@ package com.bitdubai.fermat_cbp_api.layer.actor.crypto_broker.interfaces;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
 import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
 import com.bitdubai.fermat_cbp_api.layer.actor.crypto_broker.exceptions.CantCreateNewBrokerIdentityWalletRelationshipException;
-import com.bitdubai.fermat_cbp_api.layer.actor.crypto_broker.exceptions.CantCreateNewBrokerSettingsException;
 import com.bitdubai.fermat_cbp_api.layer.actor.crypto_broker.exceptions.CantGetListBrokerIdentityWalletRelationshipException;
-import com.bitdubai.fermat_cbp_api.layer.actor.crypto_broker.exceptions.CantGetListBrokerSettingsException;
-import com.bitdubai.fermat_cbp_api.layer.actor.crypto_broker.exceptions.CantUpdateBrokerSettingsException;
 
 import java.util.Collection;
 import java.util.UUID;
@@ -48,54 +45,5 @@ public interface CryptoBrokerActorManager extends FermatManager {
      * @throws CantGetListBrokerIdentityWalletRelationshipException
      */
     BrokerIdentityWalletRelationship getBrokerIdentityWalletRelationshipByWallet(UUID wallet) throws CantGetListBrokerIdentityWalletRelationshipException;
-
-    /*=========================================
-
-        Methods of the Settngs
-
-     =========================================*/
-
-        /**
-         *
-         * @param brokerSettings
-         * @throws CantCreateNewBrokerSettingsException
-         */
-        void createBrokerSetting(BrokerSettings brokerSettings) throws CantCreateNewBrokerSettingsException;
-
-        /**
-         *
-         * @param brokerSettings
-         * @throws CantUpdateBrokerSettingsException
-         */
-        void updateBrokerSetting(BrokerSettings brokerSettings) throws CantUpdateBrokerSettingsException;
-
-        /**
-         *
-         * @return
-         * @throws CantGetListBrokerSettingsException
-         */
-        Collection<BrokerSettings> getAllBrokerSettings() throws CantGetListBrokerSettingsException;
-
-        /**
-         *
-         * @return
-         * @throws CantGetListBrokerSettingsException
-         */
-        Collection<BrokerSettings> getAllBrokerSettingsConnected() throws CantGetListBrokerSettingsException;
-
-        /**
-         *
-         * @param identity
-         * @return
-         * @throws CantGetListBrokerSettingsException
-         */
-        Collection<BrokerSettings> getBrokerSettingsConnectedByIdentity(ActorIdentity identity) throws CantGetListBrokerSettingsException;
-
-        /**
-         *
-         * @return
-         * @throws CantGetListBrokerSettingsException
-         */
-        BrokerSettings getBrokerSettingsLocalActor() throws CantGetListBrokerSettingsException;
 
 }
