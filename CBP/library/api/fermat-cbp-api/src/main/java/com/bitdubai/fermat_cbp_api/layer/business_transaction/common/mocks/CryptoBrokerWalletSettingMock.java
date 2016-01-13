bad78716f23aa216c7ca75135b9e0c7897f9e646
Manuel <darkpriestrelative@gmail.com>
2016-01-13 16:08:55 -0430

    Creating test for submitMerchandise #3945

---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/common/mocks/CryptoBrokerWalletSettingMock.java
@@ -0,0 +1,51 @@
+package com.bitdubai.fermat_cbp_api.layer.business_transaction.common.mocks;
+
+import com.bitdubai.fermat_cbp_api.all_definition.enums.CurrencyType;
+import com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.exceptions.CantGetCryptoBrokerWalletSettingException;
+import com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.exceptions.CantSaveCryptoBrokerWalletSettingException;
+import com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.interfaces.setting.CryptoBrokerWalletAssociatedSetting;
+import com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.interfaces.setting.CryptoBrokerWalletProviderSetting;
+import com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.interfaces.setting.CryptoBrokerWalletSetting;
+import com.bitdubai.fermat_cbp_api.layer.wallet.crypto_broker.interfaces.setting.CryptoBrokerWalletSettingSpread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 13/01/16.
+ */
+public class CryptoBrokerWalletSettingMock implements CryptoBrokerWalletSetting {
+    @Override
+    public void saveCryptoBrokerWalletSpreadSetting(CryptoBrokerWalletSettingSpread cryptoBrokerWalletSettingSpread) throws CantSaveCryptoBrokerWalletSettingException {
+
+    }
+
+    @Override
+    public CryptoBrokerWalletSettingSpread getCryptoBrokerWalletSpreadSetting() throws CantGetCryptoBrokerWalletSettingException {
+        return null;
+    }
+
+    @Override
+    public void saveCryptoBrokerWalletAssociatedSetting(CryptoBrokerWalletAssociatedSetting cryptoBrokerWalletAssociatedSetting) throws CantSaveCryptoBrokerWalletSettingException {
+
+    }
+
+    @Override
+    public List<CryptoBrokerWalletAssociatedSetting> getCryptoBrokerWalletAssociatedSettings() throws CantGetCryptoBrokerWalletSettingException {
+        List<CryptoBrokerWalletAssociatedSetting> cryptoBrokerWalletAssociatedSettingList=new ArrayList<>();
+        cryptoBrokerWalletAssociatedSettingList.add(new CryptoBrokerWalletAssociatedSettingMock("CryptoWalletPublicKey", CurrencyType.CRYPTO_MONEY));
+        cryptoBrokerWalletAssociatedSettingList.add(new CryptoBrokerWalletAssociatedSettingMock("BankWalletPublicKey", CurrencyType.BANK_MONEY));
+        cryptoBrokerWalletAssociatedSettingList.add(new CryptoBrokerWalletAssociatedSettingMock("CashWalletPublicKey", CurrencyType.CASH_DELIVERY_MONEY));
+        return cryptoBrokerWalletAssociatedSettingList;
+    }
+
+    @Override
+    public void saveCryptoBrokerWalletProviderSetting(CryptoBrokerWalletProviderSetting cryptoBrokerWalletProviderSetting) throws CantSaveCryptoBrokerWalletSettingException {
+
+    }
+
+    @Override
+    public List<CryptoBrokerWalletProviderSetting> getCryptoBrokerWalletProviderSettings() throws CantGetCryptoBrokerWalletSettingException {
+        return null;
+    }
+}
