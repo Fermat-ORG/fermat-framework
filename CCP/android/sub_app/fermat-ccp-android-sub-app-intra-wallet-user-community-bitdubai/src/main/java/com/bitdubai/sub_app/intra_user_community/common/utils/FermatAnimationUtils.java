14e11ec29c1590722573b99c8c7309054f414994
josemanueldsds <josemanueldsds@gmail.com>
2016-01-18 00:49:39 -0430

    Optimization utils

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/common/utils/FermatAnimationUtils.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/common/utils/FermatAnimationUtils.java
@@ -1,15 +1,19 @@
 package com.bitdubai.sub_app.intra_user_community.common.utils;
 
+import android.annotation.SuppressLint;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+
 import com.bitdubai.sub_app.intra_user_community.R;
 
 import java.util.List;
 
 /**
  * Created by mati on 2015.10.27..
+ * Modified by Jose manuel De Sousa Dos Santos on 2016.01.17
  */
-public class FernatAnimationUtils {
+@SuppressWarnings("unused")
+public class FermatAnimationUtils {
 
     /**
      * Show or Hide any view
@@ -17,6 +21,7 @@ public class FernatAnimationUtils {
      * @param show true if you want to show the view, otherwise false
      * @param view View object to show or hide
      */
+    @SuppressLint("PrivateResource")
     public static void showView(boolean show, View view) {
         if (view == null)
             return;
@@ -32,6 +37,7 @@ public class FernatAnimationUtils {
     /**
      * Show or hide empty view if needed
      */
+    @SuppressLint("PrivateResource")
     public static void showEmpty(boolean isAttached, View empty, List data) {
         if (!isAttached || empty == null)
             return;
