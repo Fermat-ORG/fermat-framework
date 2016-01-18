f42d9f6ce2aa982af4f85d3b84f030f27d5932da
Exilum <jc.juaco@gmail.com>
2016-01-18 09:42:10 -0430

    Intra Actor Network Service Plugin Root : vpn close handler improved.

--- CCP/plugin/network_service/fermat-ccp-plugin-network-service-intra-actor-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/intra_user/developer/bitdubai/version_1/IntraActorNetworkServicePluginRoot.java
+++ CCP/plugin/network_service/fermat-ccp-plugin-network-service-intra-actor-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/intra_user/developer/bitdubai/version_1/IntraActorNetworkServicePluginRoot.java
@@ -1254,6 +1254,35 @@ public class IntraActorNetworkServicePluginRoot extends AbstractPlugin implement
 
             if(vpnConnectionCloseNotificationEvent.getNetworkServiceApplicant() == getNetworkServiceType()){
 
+
+                System.out.println("----------------------------\n" +
+                        "CHANGING OUTGOING NOTIFICATIONS RECORDS " +
+                        "THAT HAVE THE PROTOCOL STATE SET TO SENT" +
+                        "TO PROCESSING SEND IN ORDER TO ENSURE PROPER RECEPTION :"
+                        + "\n-------------------------------------------------");
+
+
+                try {
+
+                    List<ActorNetworkServiceRecord> lstActorRecord = getOutgoingNotificationDao().listRequestsByProtocolState(
+                            ActorProtocolState.SENT
+                    );
+
+
+                    for (ActorNetworkServiceRecord cpr : lstActorRecord) {
+                        getOutgoingNotificationDao().changeProtocolState(cpr.getId(), ActorProtocolState.PROCESSING_SEND);
+
+                    }
+                } catch (CantListIntraWalletUsersException e) {
+                    e.printStackTrace();
+                } catch (CantUpdateRecordDataBaseException e) {
+                    e.printStackTrace();
+                } catch (CantUpdateRecordException e) {
+                    e.printStackTrace();
+                } catch (RequestNotFoundException e) {
+                    e.printStackTrace();
+                }
+
                 if(communicationNetworkServiceConnectionManager != null)
                     communicationNetworkServiceConnectionManager.closeConnection(vpnConnectionCloseNotificationEvent.getRemoteParticipant().getIdentityPublicKey());
 
