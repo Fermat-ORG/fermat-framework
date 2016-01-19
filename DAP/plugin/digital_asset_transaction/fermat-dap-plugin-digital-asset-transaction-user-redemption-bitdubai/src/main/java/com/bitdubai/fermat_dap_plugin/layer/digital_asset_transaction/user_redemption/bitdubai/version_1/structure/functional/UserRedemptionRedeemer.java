f5562fa74d8dbbaf81ccacfecd2c2c50b750af73
Víctor A. Mars M <marsvicam@gmail.com>
2016-01-19 17:37:58 -0430

    Now these plugins cancel the previous transaction when attempting to resend the bitcoins.

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-user-redemption-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/user_redemption/bitdubai/version_1/structure/functional/UserRedemptionRedeemer.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-user-redemption-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/user_redemption/bitdubai/version_1/structure/functional/UserRedemptionRedeemer.java
@@ -6,6 +6,7 @@ import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.Cant
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantLoadTableToMemoryException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantUpdateRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
+import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.exceptions.CantCancellBroadcastTransactionException;
 import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.exceptions.CantGetCryptoTransactionException;
 import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.interfaces.BitcoinNetworkManager;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.exceptions.CantCreateBitcoinTransactionException;
@@ -145,6 +146,11 @@ public class UserRedemptionRedeemer extends AbstractDigitalAssetSwap {
                     persistDigitalAsset(digitalAssetMetadata, actorAssetRedeemPoint);
                 } else {
                     System.out.println("ASSET REDEMPTION IS NOT FIRST TRANSACTION");
+                    try {
+                        bitcoinNetworkManager.cancelBroadcast(digitalAssetMetadata.getLastTransactionHash());
+                    } catch (CantCancellBroadcastTransactionException e) {
+                        //Ok, maybe I don't have it.
+                    }
                     userRedemptionDao.updateActorAssetRedeemPoint(actorAssetRedeemPoint, digitalAssetMetadata.getGenesisTransaction());
                 }
 
