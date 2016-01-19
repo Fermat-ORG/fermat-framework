1c4d443e5d49dc48fe9eacb28bcd3588ad7be669
Roberto Requena <rart3001@gmail.com>
2016-01-19 18:05:34 -0430

    Update vpn client

---
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/structure/tyrus/vpn/WsCommunicationTyrusVPNClient.java
@@ -0,0 +1,378 @@
+/*
+ * @#WsCommunicationVPNClient.java - 2015
+ * Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+ * BITDUBAI/CONFIDENTIAL
+ */
+package com.bitdubai.fermat_p2p_plugin.layer.ws.communications.cloud.client.developer.bitdubai.version_1.structure.tyrus.vpn;
+
+import com.bitdubai.fermat_api.layer.all_definition.components.interfaces.PlatformComponentProfile;
+import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.AsymmetricCryptography;
+import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.ECCKeyPair;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.contents.FermatMessageCommunication;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.contents.FermatPacketCommunicationFactory;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.contents.FermatPacketDecoder;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.contents.FermatPacketEncoder;
+import com.bitdubai.fermat_p2p_api.layer.p2p_communication.commons.client.CommunicationsVPNConnection;
+import com.bitdubai.fermat_p2p_api.layer.p2p_communication.commons.contents.FermatMessage;
+import com.bitdubai.fermat_p2p_api.layer.p2p_communication.commons.contents.FermatPacket;
+import com.bitdubai.fermat_p2p_api.layer.p2p_communication.commons.enums.FermatPacketType;
+import com.bitdubai.fermat_p2p_plugin.layer.ws.communications.cloud.client.developer.bitdubai.version_1.structure.tyrus.conf.CLoudClientConfigurator;
+import com.bitdubai.fermat_p2p_plugin.layer.ws.communications.cloud.client.developer.bitdubai.version_1.structure.tyrus.conf.CloudClientVpnConfigurator;
+import com.bitdubai.fermat_p2p_plugin.layer.ws.communications.cloud.client.developer.bitdubai.version_1.structure.vpn.WsCommunicationVPNClientManagerAgent;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.framing.FramedataImpl1;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.ws.communications.cloud.client.developer.bitdubai.version_1.structure.vpn.WsCommunicationVPNClient</code>
+ * <p/>
+ * Created by Roberto Requena - (rart3001@gmail.com) on 12/09/15.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+@ClientEndpoint(configurator = CloudClientVpnConfigurator.class)
+public class WsCommunicationTyrusVPNClient implements CommunicationsVPNConnection{
+
+    /**
+     * Represent the vpnClientIdentity
+     */
+    private ECCKeyPair vpnClientIdentity;
+
+    /**
+     * Represent the remoteParticipant of the vpn
+     */
+    private PlatformComponentProfile remoteParticipant;
+
+    /**
+     * Represent the remoteParticipantNetworkService of the vpn
+     */
+    private PlatformComponentProfile remoteParticipantNetworkService;
+
+    /**
+     * Represent the vpnServerIdentity
+     */
+    private String vpnServerIdentity;
+
+    /**
+     * Represent the pending incoming messages cache
+     */
+    private List<FermatMessage> pendingIncomingMessages;
+
+    /**
+     * Represent the wsCommunicationVPNClientManagerAgent
+     */
+    private WsCommunicationTyrusVPNClientManagerAgent wsCommunicationTyrusVPNClientManagerAgent;
+
+    /**
+     * Represent the clientConnection
+     */
+    private Session vpnClientConnection;
+
+    /**
+     * Constructor with parameters
+     *  @param wsCommunicationTyrusVPNClientManagerAgent
+     * @param vpnClientIdentity
+     * @param remoteParticipant
+     * @param remoteParticipantNetworkService
+     * @param vpnServerIdentity
+     */
+    public WsCommunicationTyrusVPNClient(WsCommunicationTyrusVPNClientManagerAgent wsCommunicationTyrusVPNClientManagerAgent, ECCKeyPair vpnClientIdentity, PlatformComponentProfile remoteParticipant, PlatformComponentProfile remoteParticipantNetworkService, String vpnServerIdentity) {
+        this.wsCommunicationTyrusVPNClientManagerAgent = wsCommunicationTyrusVPNClientManagerAgent;
+        this.vpnClientIdentity = vpnClientIdentity;
+        this.remoteParticipant = remoteParticipant;
+        this.remoteParticipantNetworkService = remoteParticipantNetworkService;
+        this.vpnServerIdentity = vpnServerIdentity;
+        this.pendingIncomingMessages = new ArrayList<>();
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println(" --------------------------------------------------------------------- ");
+        System.out.println(" WsCommunicationVPNClient - Starting method onOpen");
+        System.out.println(" WsCommunicationVPNClient - VPN Server id = " + session.getId());
+        this.vpnClientConnection = session;
+    }
+
+
+    @OnMessage
+    public void onMessage(String fermatPacketEncode) {
+
+        System.out.println(" --------------------------------------------------------------------- ");
+        System.out.println(" WsCommunicationVPNClient - Starting method onMessage(String)");
+        System.out.println(" WsCommunicationVPNClient - encode fermatPacket " + fermatPacketEncode);
+
+        /**
+         * Decode the message with the client identity
+         */
+        FermatPacket fermatPacketReceive = FermatPacketDecoder.decode(fermatPacketEncode, vpnClientIdentity.getPrivateKey());
+        System.out.println(" WsCommunicationsCloudClientChannel - decode fermatPacket " + FermatPacketDecoder.decode(fermatPacketEncode, vpnClientIdentity.getPrivateKey()));
+
+        /*
+         * Validate the signature
+         */
+        validateFermatPacketSignature(fermatPacketReceive);
+
+        if (fermatPacketReceive.getFermatPacketType() == FermatPacketType.MESSAGE_TRANSMIT){
+
+            /*
+             * Get the platformComponentProfile from the message content and decrypt
+             */
+            String messageContentJsonStringRepresentation = AsymmetricCryptography.decryptMessagePrivateKey(fermatPacketReceive.getMessageContent(), vpnClientIdentity.getPrivateKey());
+
+            System.out.println("WsCommunicationVPNClient - messageContentJsonStringRepresentation = "+messageContentJsonStringRepresentation);
+
+            /*
+             * Get the message object
+             */
+            FermatMessage fermatMessage = new FermatMessageCommunication().fromJson(messageContentJsonStringRepresentation);
+
+            System.out.println("WsCommunicationVPNClient - fermatMessage = "+fermatMessage);
+
+            /*
+             * Add to the list
+             */
+            pendingIncomingMessages.add(fermatMessage);
+
+            System.out.println("WsCommunicationVPNClient - pendingIncomingMessages.size() = " + pendingIncomingMessages.size());
+
+        }else {
+            System.out.println("WsCommunicationVPNClient - Packet type " + fermatPacketReceive.getFermatPacketType() + "is not supported");
+        }
+
+    }
+
+
+    @OnClose
+    public void onClose(final Session session, final CloseReason reason)  {
+
+        if(!vpnClientConnection.isOpen()) {
+            return;
+        }
+
+        System.out.println(" --------------------------------------------------------------------- ");
+        System.out.println(" WsCommunicationVPNClient - Starting method onClose");
+        System.out.println("Socket " + session.getId() + " is disconnect! code = " + reason.getCloseCode() + "[" + reason.getCloseCode().getCode() + "] reason = " + reason.getReasonPhrase());
+
+
+        switch (reason.getCloseCode().getCode()) {
+
+            case 1002:
+            case 1006:
+
+                    try {
+                        wsCommunicationTyrusVPNClientManagerAgent.riseVpnConnectionLooseNotificationEvent(remoteParticipantNetworkService.getNetworkServiceType(), remoteParticipant);
+                        System.out.println(" WsCommunicationVPNClient - Connection loose,  trying to reconnect");
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                break;
+
+            default:
+
+                    try {
+                        wsCommunicationTyrusVPNClientManagerAgent.riseVpnConnectionCloseNotificationEvent(remoteParticipantNetworkService.getNetworkServiceType(), remoteParticipant);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                break;
+        }
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable t) {
+
+        System.out.println(" --------------------------------------------------------------------- ");
+        System.out.println(" WsCommunicationVPNClient - Starting method onError");
+        t.printStackTrace();
+        onClose(vpnClientConnection, new CloseReason(CloseReason.CloseCodes.PROTOCOL_ERROR, t.getMessage()));
+    }
+
+    @Override
+    public void close() {
+        onClose(vpnClientConnection, new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "The cloud client close the connection, intentionally."));
+    }
+
+    /**
+     * Validate the signature of the packet
+     * @param fermatPacketReceive
+     */
+    private void validateFermatPacketSignature(FermatPacket fermatPacketReceive){
+
+        System.out.println(" WsCommunicationVPNClient - validateFermatPacketSignature");
+
+         /*
+         * Validate the signature
+         */
+        boolean isValid = AsymmetricCryptography.verifyMessageSignature(fermatPacketReceive.getSignature(), fermatPacketReceive.getMessageContent(), vpnServerIdentity);
+
+        System.out.println(" WsCommunicationVPNClient - isValid = " + isValid);
+
+        /*
+         * if not valid signature
+         */
+        if (!isValid){
+            throw new RuntimeException("Fermat Packet received has not a valid signature, go to close this connection maybe is compromise");
+        }
+
+    }
+
+    /**
+     * (non-javadoc)
+     * @see CommunicationsVPNConnection#sendMessage(FermatMessage)
+     */
+    @Override
+    public void sendMessage(FermatMessage fermatMessage){
+
+        System.out.println("WsCommunicationVPNClient - sendMessage");
+
+        /*
+         * Validate parameter
+         */
+        if (fermatMessage == null){
+            throw new IllegalArgumentException("The fermatMessage is required, can not be null");
+        }
+
+         /*
+         * Construct a fermat packet whit the message to transmit
+         */
+        FermatPacket fermatPacketRequest = FermatPacketCommunicationFactory.constructFermatPacketEncryptedAndSinged(vpnServerIdentity,                  //Destination
+                                                                                                                    vpnClientIdentity.getPublicKey(),   //Sender
+                                                                                                                    fermatMessage.toJson(),             //Message Content
+                                                                                                                    FermatPacketType.MESSAGE_TRANSMIT,  //Packet type
+                                                                                                                    vpnClientIdentity.getPrivateKey()); //Sender private key
+        /*
+         * Send the encode packet to the server
+         */
+        vpnClientConnection.getAsyncRemote().sendText(FermatPacketEncoder.encode(fermatPacketRequest));
+
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see CommunicationsVPNConnection#getUnreadMessagesCount()
+     */
+    public int getUnreadMessagesCount() {
+
+        /*
+         * Validate if not null
+         */
+        if (pendingIncomingMessages != null){
+
+            /*
+             * Return the size of the cache
+             */
+            return pendingIncomingMessages.size();
+
+        }
+
+        /*
+         * Empty cache return 0
+         */
+        return 0;
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see CommunicationsVPNConnection#readNextMessage()
+     */
+    public FermatMessage readNextMessage() {
+
+        /*
+         * Validate no empty
+         */
+        if(!pendingIncomingMessages.isEmpty()) {
+
+            /*
+             * Return the next message
+             */
+            return pendingIncomingMessages.iterator().next();
+
+        }else {
+
+            //TODO: CREATE A APPROPRIATE EXCEPTION
+            throw new RuntimeException();
+        }
+
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see CommunicationsVPNConnection#removeMessageRead(FermatMessage)
+     */
+    @Override
+    public void removeMessageRead(FermatMessage fermatMessage) {
+
+        /*
+         * Remove the message
+         */
+        pendingIncomingMessages.remove(fermatMessage);
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see CommunicationsVPNConnection#isActive()
+     */
+    @Override
+    public boolean isActive() {
+        return vpnClientConnection.isOpen();
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see CommunicationsVPNConnection#getRemoteParticipant()
+     */
+    public PlatformComponentProfile getRemoteParticipant() {
+        return remoteParticipant;
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see CommunicationsVPNConnection#getRemoteParticipantNetworkService()
+     */
+    public PlatformComponentProfile getRemoteParticipantNetworkService() {
+        return remoteParticipantNetworkService;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return vpnClientConnection.isOpen();
+    }
+
+    @Override
+    public String toString() {
+        return "WsCommunicationVPNClient{" +
+                "vpnClientIdentity=" + vpnClientIdentity +
+                ", remoteParticipant=" + remoteParticipant +
+                ", remoteParticipantNetworkService=" + remoteParticipantNetworkService +
+                ", vpnServerIdentity='" + vpnServerIdentity + '\'' +
+                '}';
+    }
+}
