353aab581c4c6c9630172b8b2e774d7bd3d0d357
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-01-19 17:35:45 -0430

    Crypto Customer Wallet Module Interfaces Creation #1507 - New Method

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/wallet_module/crypto_broker/interfaces/CryptoBrokerWalletManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/wallet_module/crypto_broker/interfaces/CryptoBrokerWalletManager.java
@@ -14,7 +14,9 @@ import com.bitdubai.fermat_cbp_api.all_definition.negotiation.NegotiationBankAcc
 import com.bitdubai.fermat_cbp_api.all_definition.negotiation.NegotiationLocations;
 import com.bitdubai.fermat_cbp_api.layer.contract.customer_broker_sale.exceptions.CantGetListCustomerBrokerContractSaleException;
 import com.bitdubai.fermat_cbp_api.layer.contract.customer_broker_sale.interfaces.CustomerBrokerContractSale;
+import com.bitdubai.fermat_cbp_api.layer.identity.crypto_broker.exceptions.CantCreateCryptoBrokerIdentityException;
 import com.bitdubai.fermat_cbp_api.layer.identity.crypto_broker.exceptions.CantListCryptoBrokerIdentitiesException;
+import com.bitdubai.fermat_cbp_api.layer.identity.crypto_broker.exceptions.CryptoBrokerIdentityAlreadyExistsException;
 import com.bitdubai.fermat_cbp_api.layer.identity.crypto_broker.interfaces.CryptoBrokerIdentity;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.exceptions.CantCreateBankAccountSaleException;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.exceptions.CantCreateLocationSaleException;
@@ -110,6 +112,20 @@ public interface CryptoBrokerWalletManager extends WalletManager {
     CryptoBrokerWalletProviderSetting newEmptyCryptoBrokerWalletProviderSetting() throws CantNewEmptyCryptoBrokerWalletProviderSettingException;
 
     NegotiationBankAccount newEmptyNegotiationBankAccount(UUID bankAccountId, String bankAccount, FiatCurrency fiatCurrency) throws CantNewEmptyNegotiationBankAccountException;
+
+    /**
+     * Through the method <code>createCryptoBrokerIdentity</code> you can create a new crypto broker identity.
+     *
+     * @param alias  the alias of the crypto broker that we want to create.
+     * @param image  an image that represents the crypto broker. it will be shown to other actors when they try to connect.
+     *
+     * @return an instance of the recent created crypto broker identity.
+     *
+     * @throws CantCreateCryptoBrokerIdentityException if something goes wrong.
+     */
+    CryptoBrokerIdentity createCryptoBrokerIdentity(final String alias,
+                                                    final byte[] image) throws CantCreateCryptoBrokerIdentityException   ,
+            CryptoBrokerIdentityAlreadyExistsException;
 
     void saveWalletSetting(CryptoBrokerWalletSettingSpread cryptoBrokerWalletSettingSpread, String publicKeyWalletCryptoBrokerInstall) throws CantSaveCryptoBrokerWalletSettingException, CryptoBrokerWalletNotFoundException, CantGetCryptoBrokerWalletSettingException;
 
