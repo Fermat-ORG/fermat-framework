c1a0b51c15db73575fc781ef56eee3377518e21a
Exilum <jc.juaco@gmail.com>
2016-01-20 17:30:14 -0430

    New Validations

--- fermat-api/src/main/java/com/bitdubai/fermat_api/layer/modules/interfaces/FermatSettings.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/modules/interfaces/FermatSettings.java
@@ -4,4 +4,5 @@ package com.bitdubai.fermat_api.layer.modules.interfaces;
  * Created by mati on 2015.11.21..
  */
 public interface FermatSettings {
+    public void setIsPresentationHelpEnabled(boolean b);
 }
