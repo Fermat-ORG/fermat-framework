5178e04978b769c5b0733b6f00437cd9a612603b
Nelson Ramirez <nelsonalfo@gmail.com>
2016-01-20 15:47:44 -0430

    fix minimo

--- P2P/plugin/communication/fermat-p2p-plugin-communication-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-cloud-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/ws/communications/cloud/client/developer/bitdubai/version_1/WsCommunicationsCloudClientPluginRoot.java
@@ -95,7 +95,7 @@ public class WsCommunicationsCloudClientPluginRoot extends AbstractPlugin implem
     /**
      * Represent the uri
      */
-    private URI uri;x
+    private URI uri;
 
     /**
      * Represent the clientIdentity
