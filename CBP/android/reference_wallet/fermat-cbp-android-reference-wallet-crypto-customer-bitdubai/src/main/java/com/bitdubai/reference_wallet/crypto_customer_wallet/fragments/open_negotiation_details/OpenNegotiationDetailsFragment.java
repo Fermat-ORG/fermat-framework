1b60f828c66cb0c157e2f21b16b62b0cde511515
yalayn <y.alayn@gmail.com>
2016-01-22 19:03:52 -0430

    CBP - Reference Wallet - Crypto Customer

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/open_negotiation_details/OpenNegotiationDetailsFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/open_negotiation_details/OpenNegotiationDetailsFragment.java
@@ -1,20 +1,48 @@
 package com.bitdubai.reference_wallet.crypto_customer_wallet.fragments.open_negotiation_details;
 
 import android.app.Fragment;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.utils.ImagesUtils;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
+import com.bitdubai.fermat_api.layer.pip_engine.interfaces.ResourceProviderManager;
+import com.bitdubai.fermat_api.layer.world.interfaces.Currency;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseType;
+import com.bitdubai.fermat_cbp_api.all_definition.identity.ActorIdentity;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.ClauseInformation;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.CustomerBrokerNegotiationInformation;
 import com.bitdubai.reference_wallet.crypto_customer_wallet.R;
+import com.bitdubai.reference_wallet.crypto_customer_wallet.session.CryptoCustomerWalletSession;
+
+import java.util.Map;
+import java.util.UUID;
 
 
 /**
  * A simple {@link Fragment} subclass.
+ * Modified by Yordin Alayn 22.01.16
  */
-public class OpenNegotiationDetailsFragment extends AbstractFermatFragment {
+public class OpenNegotiationDetailsFragment extends AbstractFermatFragment<CryptoCustomerWalletSession, ResourceProviderManager> {
 
+    private static final String TAG = "OpenNegotiationFrag";
+
+    private ImageView brokerImage;
+    private FermatTextView sellingDetails;
+    private FermatTextView exchangeRateSummary;
+    private FermatTextView brokerName;
+    private RecyclerView recyclerView;
 
     public OpenNegotiationDetailsFragment() {
         // Required empty public constructor
@@ -25,11 +53,80 @@ public class OpenNegotiationDetailsFragment extends AbstractFermatFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View layout = inflater.inflate(R.layout.ccw_fragment_open_negotiation_details_activity, container, false);
+
+        configureToolbar();
+        initViews(layout);
+        bindData();
+
+        return layout;
+//        return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
+    /*PRIVATE METHOD*/
+
+    //VIEW TOOLBAR
+    private void configureToolbar() {
+
+        Toolbar toolbar = getToolbar();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            toolbar.setBackground(getResources().getDrawable(R.drawable.ccw_action_bar_gradient_colors, null));
+        else
+            toolbar.setBackground(getResources().getDrawable(R.drawable.ccw_action_bar_gradient_colors));
+
+        toolbar.setTitleTextColor(Color.WHITE);
+        if (toolbar.getMenu() != null) toolbar.getMenu().clear();
+
+    }
+
+    //VIEW INIT
+    private void initViews(View rootView) {
+
+        brokerImage = (ImageView) rootView.findViewById(R.id.ccw_customer_image);
+        brokerName = (FermatTextView) rootView.findViewById(R.id.ccw_broker_name);
+        sellingDetails = (FermatTextView) rootView.findViewById(R.id.ccw_selling_summary);
+        exchangeRateSummary = (FermatTextView) rootView.findViewById(R.id.ccw_buying_exchange_rate);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.ccw_open_negotiation_details_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
+    }
+
+    //VIEW DATE
+    private void bindData() {
+
+        CustomerBrokerNegotiationInformation negotiationInformation = (CustomerBrokerNegotiationInformation) appSession.getData(CryptoCustomerWalletSession.NEGOTIATION_DATA);
+        ActorIdentity broker = negotiationInformation.getBroker();
+        Map<ClauseType, ClauseInformation> clauses = negotiationInformation.getClauses();
+
+        String merchandise = clauses.get(ClauseType.CUSTOMER_CURRENCY).getValue();
+        String exchangeAmount = clauses.get(ClauseType.EXCHANGE_RATE).getValue();
+        String paymentCurrency = clauses.get(ClauseType.BROKER_CURRENCY).getValue();
+        String amount = clauses.get(ClauseType.CUSTOMER_CURRENCY_QUANTITY).getValue();
+
+
+        //Negotiation Summary
+        Drawable brokerImg = getImgDrawable(broker.getProfileImage());
+        brokerImage.setImageDrawable(brokerImg);
+        brokerName.setText(broker.getAlias());
+        sellingDetails.setText(getResources().getString(R.string.ccw_selling_details, amount, merchandise));
+        exchangeRateSummary.setText(getResources().getString(R.string.ccw_exchange_rate_summary, merchandise, exchangeAmount, paymentCurrency));
+
+    }
+
+    private Drawable getImgDrawable(byte[] customerImg) {
+
+        Resources res = getResources();
+
+        if (customerImg != null && customerImg.length > 0)
+            return ImagesUtils.getRoundedBitmap(res, customerImg);
+
+        return ImagesUtils.getRoundedBitmap(res, R.drawable.person);
+
+    }
+    /*END PRIVATE METHOD*/
 
 
 }
