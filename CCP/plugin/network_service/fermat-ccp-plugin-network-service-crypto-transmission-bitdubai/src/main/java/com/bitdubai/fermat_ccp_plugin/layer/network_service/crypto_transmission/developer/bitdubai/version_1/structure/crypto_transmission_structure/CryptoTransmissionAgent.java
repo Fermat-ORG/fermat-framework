7ad49d51af6e4947824433f04d9e2b2917c9ee26
mati <mati_fur@hotmail.com>
2016-01-22 01:41:58 -0300

    Crypto transmission improve and communication threads implemented

--- CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-transmission-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_transmission/developer/bitdubai/version_1/structure/crypto_transmission_structure/CryptoTransmissionAgent.java
+++ CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-transmission-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/network_service/crypto_transmission/developer/bitdubai/version_1/structure/crypto_transmission_structure/CryptoTransmissionAgent.java
@@ -350,20 +350,26 @@ public class CryptoTransmissionAgent {
 
                         try {
 
+                            Gson gson = new Gson();
+                            String jsonMetadata = null;
+                            switch (cryptoTransmissionMetadata.getCryptoTransmissionMetadataStates()){
+                                case SEEN_BY_OWN_NETWORK_SERVICE_WAITING_FOR_RESPONSE:
+                                    jsonMetadata = gson.toJson(cryptoTransmissionMetadata);
+                                    break;
+                                case SEEN_BY_DESTINATION_VAULT:
 
+                                    break;
+                                case CREDITED_IN_DESTINATION_WALLET:
 
+                                    break;
+
+                            }
                             System.out.print("-----------------------\n" +
                                     "ENVIANDO CRYPTO METADATA!!!!! -----------------------\n" +
                                     "-----------------------\n A: " + cryptoTransmissionMetadata.getDestinationPublicKey());
 
-                            // Si se encuentra conectado paso la metadata al dao de la capa de comunicacion para que lo envie
-                            Gson gson = new Gson();
-                            String jsonMetadata = gson.toJson(cryptoTransmissionMetadata);
-
-                            // Envio el mensaje a la capa de comunicacion
 
                             communicationNetworkServiceLocal.sendMessage(cryptoTransmissionMetadata.getSenderPublicKey(),cryptoTransmissionMetadata.getDestinationPublicKey(),jsonMetadata);
-
                             //Cambio estado de base de datos a PROCESSING_SEND_COMMUNICATION_DATABASE
                             outgoingCryptoTransmissionMetadataDAO.changeCryptoTransmissionProtocolState(
                                     cryptoTransmissionMetadata.getTransactionId(),
