08b3ce1900fb9cbb311ad1a7ad4887f5871c57e2
Exilum <jc.juaco@gmail.com>
2016-01-28 18:28:43 -0430

    Bitcoin Wallet Transaction record issue fixed.

--- CCP/plugin/wallet_module/fermat-ccp-plugin-wallet-module-crypto-wallet-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/version_1/structure/CryptoWalletWalletModuleManager.java
+++ CCP/plugin/wallet_module/fermat-ccp-plugin-wallet-module-crypto-wallet-bitdubai/src/main/java/com/bitdubai/fermat_ccp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/version_1/structure/CryptoWalletWalletModuleManager.java
@@ -1249,7 +1249,7 @@ public class CryptoWalletWalletModuleManager implements CryptoWallet {
             switch (bitcoinWalletTransaction.getTransactionType()) {
                 case CREDIT:
                     try {
-                        involvedActor = getActorByActorPublicKeyAndType(bitcoinWalletTransaction.getActorToPublicKey(), bitcoinWalletTransaction.getActorToType(), intraUserLoggedInPublicKey);
+                        involvedActor = getActorByActorPublicKeyAndType(bitcoinWalletTransaction.getActorFromPublicKey(), bitcoinWalletTransaction.getActorToType(), intraUserLoggedInPublicKey);
                         WalletContactRecord walletContactRecord = walletContactsRegistry.getWalletContactByActorAndWalletPublicKey(bitcoinWalletTransaction.getActorToPublicKey(),walletPublicKey);
                         if (walletContactRecord != null)
                             contactId = walletContactRecord.getContactId();
