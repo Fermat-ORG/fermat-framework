5fb6725dbe2d90bc5ac177ac6c933139cadf7ed9
Manuel <darkpriestrelative@gmail.com>
2016-01-28 10:04:52 -0430

    Deleting old folders

--- fermat-dmp-android-core/src/main/java/com/bitdubai/android_core/app/shell/ShellApp.java
+++
@@ -1,9 +0,0 @@
-package com.bitdubai.android_core.app.shell;
-
-import com.bitdubai.android_core.app.AndroidApp;
-
-/**
- * Created by ciencias on 01.01.15.
- */
-public class ShellApp implements AndroidApp {
-}
--- fermat-dmp-android-core/src/main/java/com/bitdubai/android_core/app/subapp/cash_out_point/CashOutPointSubApp.java
+++
@@ -1,9 +0,0 @@
-package com.bitdubai.android_core.app.subapp.cash_out_point;
-
-import com.bitdubai.android_core.app.subapp.SubApp;
-
-/**
- * Created by ciencias on 01.01.15.
- */
-public class CashOutPointSubApp implements SubApp {
-}
