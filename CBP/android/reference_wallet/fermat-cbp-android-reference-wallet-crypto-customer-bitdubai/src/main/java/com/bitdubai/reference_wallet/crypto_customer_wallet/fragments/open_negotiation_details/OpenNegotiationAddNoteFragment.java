e0b6e584889e906e8bbd5ea046061840249036dd
yalayn <y.alayn@gmail.com>
2016-01-29 16:45:01 -0430

    CBP - Reference Wallet - Crypto Customer - Open Negotiation - Add Note

---
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/open_negotiation_details/OpenNegotiationAddNoteFragment.java
@@ -0,0 +1,52 @@
+package com.bitdubai.reference_wallet.crypto_customer_wallet.fragments.open_negotiation_details;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+import com.bitdubai.fermat_api.layer.pip_engine.interfaces.ResourceProviderManager;
+import com.bitdubai.reference_wallet.crypto_customer_wallet.R;
+import com.bitdubai.reference_wallet.crypto_customer_wallet.session.CryptoCustomerWalletSession;
+
+/**
+ * Created by Yordin Alayn on 29.01.16.
+ */
+public class OpenNegotiationAddNoteFragment  extends AbstractFermatFragment<CryptoCustomerWalletSession, ResourceProviderManager> {
+
+    public static OpenNegotiationAddNoteFragment newInstance(){
+        return new OpenNegotiationAddNoteFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View layout = inflater.inflate(R.layout.ccw_fragment_open_negotiation_add_note_activity, container, false);
+
+        configureToolbar();
+//        initViews(layout);
+//        bindData();
+
+        return layout;
+    }
+
+    /*PRIVATE METHOD*/
+    private void configureToolbar() {
+
+        Toolbar toolbar = getToolbar();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            toolbar.setBackground(getResources().getDrawable(R.drawable.ccw_action_bar_gradient_colors, null));
+        else
+            toolbar.setBackground(getResources().getDrawable(R.drawable.ccw_action_bar_gradient_colors));
+
+        toolbar.setTitleTextColor(Color.WHITE);
+        if (toolbar.getMenu() != null) toolbar.getMenu().clear();
+
+    }
+    /*END PRIVATE METHOD*/
+}
--- WPD/plugin/engine/fermat-wpd-plugin-engine-wallet-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/WalletRuntimeEnginePluginRoot.java
+++ WPD/plugin/engine/fermat-wpd-plugin-engine-wallet-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/WalletRuntimeEnginePluginRoot.java
@@ -2702,6 +2702,32 @@ public class WalletRuntimeEnginePluginRoot extends AbstractPlugin implements
         runtimeActivity.setStartFragment(Fragments.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_DETAILS.getKey());
 
 
+        // Activity: Open Negotiation Add Note
+        runtimeActivity = new Activity();
+        runtimeActivity.setType(Activities.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_ADD_NOTE);
+        runtimeActivity.setActivityType(Activities.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_ADD_NOTE.getCode());
+        runtimeActivity.setBackActivity(Activities.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_DETAILS);
+        runtimeActivity.setBackPublicKey(publicKey);
+        runtimeWalletNavigationStructure.addActivity(runtimeActivity);
+
+        runtimeTitleBar = new TitleBar();
+        runtimeTitleBar.setLabel("Negotiation Note");
+        runtimeTitleBar.setLabelSize(titleBarLabelSize);
+        runtimeTitleBar.setTitleColor(titleBarLabelColor);
+        runtimeTitleBar.setIsTitleTextStatic(true);
+        runtimeTitleBar.setIconName("back");
+        runtimeActivity.setTitleBar(runtimeTitleBar);
+
+        runtimeStatusBar = new StatusBar();
+        runtimeStatusBar.setColor(statusBarColor);
+        runtimeActivity.setStatusBar(runtimeStatusBar);
+
+        runtimeFragment = new Fragment();
+        runtimeFragment.setType(Fragments.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_ADD_NOTE.getKey());
+        runtimeActivity.addFragment(Fragments.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_ADD_NOTE.getKey(), runtimeFragment);
+        runtimeActivity.setStartFragment(Fragments.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_ADD_NOTE.getKey());
+
+
         // Activity: Close Negotiation details - Open Contract
         runtimeActivity = new Activity();
         runtimeActivity.setType(Activities.CBP_CRYPTO_CUSTOMER_WALLET_CLOSE_NEGOTIATION_DETAILS_OPEN_CONTRACT);
