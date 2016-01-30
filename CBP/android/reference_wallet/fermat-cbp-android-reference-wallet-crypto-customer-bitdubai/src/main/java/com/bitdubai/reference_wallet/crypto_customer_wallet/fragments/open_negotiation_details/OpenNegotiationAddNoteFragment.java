d2ee8f1302ff14af92dbcd93f70adca5abc2e209
yalayn <y.alayn@gmail.com>
2016-01-30 00:25:39 -0430

    CBP - Reference Wallet - Crypto Customer - Open Negotiation - Add Note

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/open_negotiation_details/OpenNegotiationAddNoteFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/fragments/open_negotiation_details/OpenNegotiationAddNoteFragment.java
@@ -7,19 +7,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Activities;
 import com.bitdubai.fermat_api.layer.pip_engine.interfaces.ResourceProviderManager;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.CustomerBrokerNegotiationInformation;
 import com.bitdubai.reference_wallet.crypto_customer_wallet.R;
-import com.bitdubai.reference_wallet.crypto_customer_wallet.common.holders.open_negotiation.AddNoteViewHolder;
 import com.bitdubai.reference_wallet.crypto_customer_wallet.session.CryptoCustomerWalletSession;
 
 /**
  * Created by Yordin Alayn on 29.01.16.
  */
-public class OpenNegotiationAddNoteFragment  extends AbstractFermatFragment<CryptoCustomerWalletSession, ResourceProviderManager>
-        implements AddNoteViewHolder.OnNoteButtonButtonsClickListener{
+public class OpenNegotiationAddNoteFragment  extends AbstractFermatFragment<CryptoCustomerWalletSession, ResourceProviderManager>{
+
+    private CustomerBrokerNegotiationInformation negotiationInfo;
+
+    public OpenNegotiationAddNoteFragment(){
+
+    }
 
     public static OpenNegotiationAddNoteFragment newInstance(){
         return new OpenNegotiationAddNoteFragment();
@@ -31,20 +39,10 @@ public class OpenNegotiationAddNoteFragment  extends AbstractFermatFragment<Cryp
         View layout = inflater.inflate(R.layout.ccw_fragment_open_negotiation_add_note_activity, container, false);
 
         configureToolbar();
-//        initViews(layout);
-//        bindData();
+        initViews(layout);
+        bindData();
 
         return layout;
-    }
-
-    @Override
-    public void onCancelNoteButtonClicked(){
-        Toast.makeText(getActivity(), "CANCEL NOTE", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onSaveNoteButtonClicked(){
-        Toast.makeText(getActivity(), "SAVE NOTE", Toast.LENGTH_LONG).show();
     }
 
     /*PRIVATE METHOD*/
@@ -59,6 +57,34 @@ public class OpenNegotiationAddNoteFragment  extends AbstractFermatFragment<Cryp
 
         toolbar.setTitleTextColor(Color.WHITE);
         if (toolbar.getMenu() != null) toolbar.getMenu().clear();
+
+    }
+
+    private void initViews(View layout){
+
+        final EditText noteEditText = (EditText) layout.findViewById(R.id.ccw_open_negotiation_text_note);
+        if(negotiationInfo.getMemo() != null)
+            noteEditText.setText(negotiationInfo.getMemo());
+
+        Button cancelNoteButton = (Button) layout.findViewById(R.id.ccw_open_negotiation_cancel_note);
+        cancelNoteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                changeActivity(Activities.CBP_CRYPTO_CUSTOMER_WALLET_OPEN_NEGOTIATION_DETAILS, appSession.getAppPublicKey());
+            }
+        });
+
+        Button saveNoteButton = (Button) layout.findViewById(R.id.ccw_open_negotiation_save_note);
+        saveNoteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "SAVE NOTE", Toast.LENGTH_LONG).show();
+                negotiationInfo.setMemo(noteEditText.getText().toString());
+            }
+        });
+    }
+
+    private void bindData() {
+
+        negotiationInfo = (CustomerBrokerNegotiationInformation) appSession.getData(CryptoCustomerWalletSession.NEGOTIATION_DATA);
 
     }
     /*END PRIVATE METHOD*/
