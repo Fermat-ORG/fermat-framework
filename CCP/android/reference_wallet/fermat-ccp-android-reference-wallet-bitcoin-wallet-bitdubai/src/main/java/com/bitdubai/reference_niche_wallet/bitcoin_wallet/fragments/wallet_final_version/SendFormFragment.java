1d52c515515ac1b7f660b08f86331273dd34bcad
mati <mati_fur@hotmail.com>
2016-01-30 13:37:34 -0300

    Merge branch 'develop' of https://github.com/nattyco/fermat into develop
    Conflicts:
    	CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendFormFragment.java

--- CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendFormFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendFormFragment.java
@@ -543,7 +543,12 @@ public class SendFormFragment extends AbstractFermatFragment<ReferenceWalletSess
                     EditText txtAmount = (EditText) rootView.findViewById(R.id.amount);
                     String amount = txtAmount.getText().toString();
 
-                    if (!amount.equals("") && amount != null && Double.parseDouble(amount) != 0) {
+                    BigDecimal money = new BigDecimal("0");
+
+                    if (!amount.equals(""))
+                        money = new BigDecimal("0");
+
+                    if(!amount.equals("") && !money.equals(new BigDecimal(0))) {
                         try {
                             String notes = null;
                             if (txt_notes.getText().toString().length() != 0) {
