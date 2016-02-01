539f98fa6c4ddc8c03ce9cc3bebb352db49d533f
Guillermo <guillermo20@gmail.com>
2016-02-01 16:32:25 -0430

    cambios menores

--- WPD/plugin/engine/fermat-wpd-plugin-engine-wallet-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/WalletRuntimeEnginePluginRoot.java
+++ WPD/plugin/engine/fermat-wpd-plugin-engine-wallet-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/WalletRuntimeEnginePluginRoot.java
@@ -2974,7 +2974,7 @@ public class WalletRuntimeEnginePluginRoot extends AbstractPlugin implements
         Tab runtimeTab;
 
         final String publicKey = "banking_wallet";
-        final String statusBarColor = "#074264";
+        final String statusBarColor = "#391459";
         final String titleBarLabelColor = "#FFFFFF";
         final int titleBarLabelSize = 16;
 
