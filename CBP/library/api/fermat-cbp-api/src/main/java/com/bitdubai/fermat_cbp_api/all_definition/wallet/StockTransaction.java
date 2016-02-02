cee85c635879d6af20b4efbdd4f6a5e919037c0f
JDVilchez <josvilchezalmera@gmail.com>
2016-02-02 23:50:10 +0000

    CBP - wallet - Crypto Broker - QA - Review Documentation Methods and Handle Exception #4653

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/wallet/StockTransaction.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/all_definition/wallet/StockTransaction.java
@@ -13,29 +13,88 @@ import java.util.UUID;
  * Created by jorge on 30-09-2015.
  */
 public interface StockTransaction {
-    //TODO: Documentar
 
+    /**
+     * The method <code>getTransactionId</code> returns the id of the transaction
+     *
+     * @return an UUID of the transaction
+     */
     UUID getTransactionId();
 
+    /**
+     * The method <code>getBalanceType</code> returns balance type
+     *
+     * @return BalanceType
+     */
     BalanceType getBalanceType();
 
+    /**
+     * The method <code>getTransactionType</code> returns the transaction type
+     *
+     * @return TransactionType
+     */
     TransactionType getTransactionType();
 
+    /**
+     * The method <code>getCurrencyType</code> returns the currency type
+     *
+     * @return CurrencyType
+     */
     CurrencyType getCurrencyType();
 
+    /**
+     * The method <code>getMerchandise</code> returns the merchandise
+     *
+     * @return Currency
+     */
     Currency getMerchandise();
 
+    /**
+     * The method <code>getWalletPublicKey</code> returns public key of the wallet
+     *
+     * @return an String of the wallet public key
+     */
     String getWalletPublicKey();
 
+    /**
+     * The method <code>getBrokerPublicKey</code> returns the public key of the broker
+     *
+     * @return an String of the broker public key
+     */
     String getBrokerPublicKey();
 
+    /**
+     * The method <code>getAmount</code> returns the amount
+     *
+     * @return a BigDecimal of the amount
+     */
     BigDecimal getAmount();
 
+    /**
+     * The method <code>getTimestamp</code> returns the timeStamp
+     *
+     * @return a long of the timeStamp
+     */
     long getTimestamp();
 
+    /**
+     * The method <code>getMemo</code> returns the memo
+     *
+     * @return an String of the memo
+     */
     String getMemo();
 
+    /**
+     * The method <code>getPriceReference</code> returns the price reference
+     *
+     * @return a BigDecimal of the price reference
+     */
     BigDecimal getPriceReference();
 
+    /**
+     * The method <code>getOriginTransaction</code> returns the origin transaction
+     *
+     * @return OriginTransaction
+     */
     OriginTransaction getOriginTransaction();
 }
