658b4b0682c9d258c231e29e73475d981656e2a9
alexanderejm <alex_jimenez76@hotmail.com>
2016-02-02 00:07:58 -0430

    #4516

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/database/CustomerOnlinePaymentBusinessTransactionDaoTest/getCustomerOnlinePaymentRecordTest.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/database/CustomerOnlinePaymentBusinessTransactionDaoTest/getCustomerOnlinePaymentRecordTest.java
@@ -2,16 +2,35 @@ package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_onlin
 
 import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDao;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by alexander jimenez (alex_jimenez76@hotmail.com) on 31/01/16.
  */
 public class getCustomerOnlinePaymentRecordTest {
+    @Mock
     private CustomerOnlinePaymentBusinessTransactionDao customerOnlinePaymentBusinessTransactionDao;
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void getCustomerOnlinePaymentRecord_Should_Run_Once() throws Exception{
+        customerOnlinePaymentBusinessTransactionDao.getCustomerOnlinePaymentRecord(anyString());
+        verify(customerOnlinePaymentBusinessTransactionDao,Mockito.times(1)).getCustomerOnlinePaymentRecord(anyString());
+    }
 
     @Test(expected = Exception.class)
-    public void isContractHashInDatabase_Should_Return_Exception() throws Exception{
+    public void getCustomerOnlinePaymentRecord_Should_Return_Exception() throws Exception{
+        customerOnlinePaymentBusinessTransactionDao = new CustomerOnlinePaymentBusinessTransactionDao(null,null,null);
         customerOnlinePaymentBusinessTransactionDao.getCustomerOnlinePaymentRecord(null);
     }
 }
--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/database/CustomerOnlinePaymentBusinessTransactionDaoTest/initializeTest.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/database/CustomerOnlinePaymentBusinessTransactionDaoTest/initializeTest.java
@@ -6,23 +6,15 @@ import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseFactory
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableFactory;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDao;
-import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDatabaseConstants;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.UUID;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by alexander jimenez (alex_jimenez76@hotmail.com) on 01/02/16.
@@ -40,34 +32,13 @@ public class initializeTest {
     private Database mockDatabase;
     @Mock
     DeveloperObjectFactory mockObjectFactory;
-    private UUID testId;
-    private String testDataBaseName;
-    //private CustomerOnlinePaymentBusinessTransactionDatabaseFactory customerOnlinePaymentBusinessTransactionDatabaseFactory;
     @Mock
     private CustomerOnlinePaymentBusinessTransactionDao customerOnlinePaymentBusinessTransactionDao;
 
-    public void setUpTestValues(){
-        testId = UUID.randomUUID();
-        testDataBaseName = CustomerOnlinePaymentBusinessTransactionDatabaseConstants.DATABASE_NAME;
-    }
-
-    public void setUpGeneralMockitoRules() throws Exception{
-        when(mockDatabase.getDatabaseFactory()).thenReturn(mockDatabaseFactory);
-        when(mockPluginDatabaseSystem.createDatabase(testId, testDataBaseName)).thenReturn(mockDatabase);
-        when(mockDatabaseFactory.newTableFactory(any(UUID.class), anyString())).thenReturn(mockTableFactory);
-    }
-
-    @Before
-    public void setUp() throws Exception{
-        setUpTestValues();
-        setUpGeneralMockitoRules();
-    }
-
     @Test
-    public void TestInitialize() throws Exception{
+    public void TestInitialize_Should_Run_Once() throws Exception{
         customerOnlinePaymentBusinessTransactionDao.initialize();
         verify(customerOnlinePaymentBusinessTransactionDao,Mockito.times(1)).initialize();
-
     }
 
     @Test(expected = Exception.class)
