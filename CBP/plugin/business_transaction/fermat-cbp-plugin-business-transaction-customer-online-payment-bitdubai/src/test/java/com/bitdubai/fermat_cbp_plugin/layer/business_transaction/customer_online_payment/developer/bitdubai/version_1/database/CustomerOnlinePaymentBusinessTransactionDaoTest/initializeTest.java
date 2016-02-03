6c56bbc5c24368a897728ffea3abb1777a6d784f
alexanderejm <alex_jimenez76@hotmail.com>
2016-02-03 06:11:52 -0430

    #4516 New updates/fixes to the following test test: 1)CustomerOnlinePaymentBusinessTransactionDaoTest.InitializeTest 2)CustomerOnlinePaymentBusinessTransactionDatabaseFactoryTest.DatabaseTest 3)CustomerOnlinePaymentPluginRootTest.testSetLogginglevelPerClass
    New Test:
    1)CustomerOnlinePaymentBusinessTransactionDeveloperDatabaseFactoryTest,getDatabaseListTest
    2)CustomerOnlinePaymentBusinessTransactionDeveloperDatabaseFactoryTest,getDatabaseTableListTest
    3)CustomerOnlinePaymentBusinessTransactionDeveloperDatabaseFactoryTest,initializeDatabaseTest
    4)CustomerOnlinePaymentRecorderServiceTest.testStart
    5)CustomerOnlinePaymentPluginRootTest.testGetClassesFullPath

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/database/CustomerOnlinePaymentBusinessTransactionDaoTest/initializeTest.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/database/CustomerOnlinePaymentBusinessTransactionDaoTest/initializeTest.java
@@ -1,20 +1,18 @@
 package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDaoTest;
 
-import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseFactory;
-import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTableFactory;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
 import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 
-import static org.mockito.Mockito.verify;
+import java.util.UUID;
+
 
 /**
  * Created by alexander jimenez (alex_jimenez76@hotmail.com) on 01/02/16.
@@ -25,25 +23,22 @@ public class initializeTest {
     @Mock
     private PluginDatabaseSystem mockPluginDatabaseSystem;
     @Mock
-    private DatabaseFactory mockDatabaseFactory;
-    @Mock
-    private DatabaseTableFactory mockTableFactory;
-    @Mock
     private Database mockDatabase;
-    @Mock
-    DeveloperObjectFactory mockObjectFactory;
-    @Mock
+
+    private UUID testId;
     private CustomerOnlinePaymentBusinessTransactionDao customerOnlinePaymentBusinessTransactionDao;
+
 
     @Test
     public void TestInitialize_Should_Run_Once() throws Exception{
+        testId = UUID.randomUUID();
+        customerOnlinePaymentBusinessTransactionDao = new CustomerOnlinePaymentBusinessTransactionDao(mockPluginDatabaseSystem,testId,mockDatabase);
         customerOnlinePaymentBusinessTransactionDao.initialize();
-        verify(customerOnlinePaymentBusinessTransactionDao,Mockito.times(1)).initialize();
     }
 
     @Test(expected = Exception.class)
     public void TestCreateDatabase_Should_Return_Exception() throws Exception{
-        customerOnlinePaymentBusinessTransactionDao = new CustomerOnlinePaymentBusinessTransactionDao(null,null,null);
+        customerOnlinePaymentBusinessTransactionDao = new CustomerOnlinePaymentBusinessTransactionDao(null,testId,mockDatabase);
         customerOnlinePaymentBusinessTransactionDao.initialize();
     }
 }
