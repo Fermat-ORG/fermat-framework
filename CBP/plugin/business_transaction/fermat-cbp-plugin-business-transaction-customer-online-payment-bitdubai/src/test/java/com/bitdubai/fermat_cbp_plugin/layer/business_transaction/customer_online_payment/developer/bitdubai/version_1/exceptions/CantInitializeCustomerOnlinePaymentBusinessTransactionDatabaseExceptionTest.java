5c53e47601a9490ad6c88e43539a0635d2320bdc
alexanderejm <alex_jimenez76@hotmail.com>
2016-02-05 02:34:09 -0430

    #4516 New updates/fixes to the following:
    1)CustomerOnlinePaymentBusinessTransactionDao has seen changes to support for another methodunit testing
    
    New Test:
    1)CustomerOnlinePaymentBusinessTransactionDao.getOnCryptoNetworkCryptoStatusListTest
    2)CustomerOnlinePaymentBusinessTransactionDao,getPendingEventsTest
    3)CustomerOnlinePaymentBusinessTransactionDao,getPendingToSubmitConfirmListTest
    4)CustomerOnlinePaymentBusinessTransactionDao.getPendingToSubmitCryptoListTest
    5)CustomerOnlinePaymentBusinessTransactionDao.getPendingToSubmitNotificationsListTest
    6)CantInitializeCustomerOnlinePaymentBusinessTransactionDatabaseExceptionTest
    7)CustomerOnlinePaymentTransactionManagerTest.parseToLongTest

---
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-online-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_online_payment/developer/bitdubai/version_1/exceptions/CantInitializeCustomerOnlinePaymentBusinessTransactionDatabaseExceptionTest.java
@@ -0,0 +1,46 @@
+package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.exceptions;
+
+import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseFilterType;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.DatabaseTable;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDao;
+import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.database.CustomerOnlinePaymentBusinessTransactionDatabaseConstants;
+import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_online_payment.developer.bitdubai.version_1.exceptions.CantInitializeCustomerOnlinePaymentBusinessTransactionDatabaseException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+
+import java.util.UUID;
+
+/**
+ * Created by alexander jimenez (alex_jimenez76@hotmail.com) on 04/02/16.
+ */
+public class CantInitializeCustomerOnlinePaymentBusinessTransactionDatabaseExceptionTest {
+    @Mock
+    private PluginDatabaseSystem mockPluginDatabaseSystem;
+
+    private Database mockDatabase;
+    @Mock
+    private DatabaseTable databaseTable;
+    private UUID testId;
+    private CustomerOnlinePaymentBusinessTransactionDao customerOnlinePaymentBusinessTransactionDao;
+    private CustomerOnlinePaymentBusinessTransactionDao customerOnlinePaymentBusinessTransactionDaoSpy;
+
+    @Before
+    public void setup()throws Exception{
+        testId = UUID.randomUUID();
+        customerOnlinePaymentBusinessTransactionDao = new CustomerOnlinePaymentBusinessTransactionDao(mockPluginDatabaseSystem,testId, mockDatabase);
+        customerOnlinePaymentBusinessTransactionDaoSpy = PowerMockito.spy(customerOnlinePaymentBusinessTransactionDao);
+        MockitoAnnotations.initMocks(this);
+        PowerMockito.doReturn(databaseTable).when(customerOnlinePaymentBusinessTransactionDaoSpy, "getDatabaseContractTable");
+    }
+    @Test(expected = CantInitializeCustomerOnlinePaymentBusinessTransactionDatabaseException.class)
+    public void CustomerOnlinePaymentBusinessTransactionDaoInitTest()throws Exception{
+        customerOnlinePaymentBusinessTransactionDao = new CustomerOnlinePaymentBusinessTransactionDao(null,null,mockDatabase);
+        customerOnlinePaymentBusinessTransactionDao.initialize();
+    }
+}
