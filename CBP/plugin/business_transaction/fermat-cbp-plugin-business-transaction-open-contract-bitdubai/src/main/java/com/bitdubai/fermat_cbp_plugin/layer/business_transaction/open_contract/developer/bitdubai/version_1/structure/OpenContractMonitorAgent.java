00b0aba0e88ba309c248f9e6d0d83f4ff0622f83
alexanderejm <alex_jimenez76@hotmail.com>
2016-02-11 18:43:02 -0430

    #4825 open contract thread fixed

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/structure/OpenContractMonitorAgent.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-open-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/open_contract/developer/bitdubai/version_1/structure/OpenContractMonitorAgent.java
@@ -111,7 +111,7 @@ public class OpenContractMonitorAgent implements
             errorManager.reportUnexpectedPluginException(Plugins.OPEN_CONTRACT, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, exception);
         }
 
-        this.agentThread = new Thread(monitorAgent);
+        this.agentThread = new Thread(monitorAgent,this.getClass().getSimpleName());
         this.agentThread.start();
 
     }
