2af598d7a80aac678075641dc43cac3c04d36d06
JDVilchez <josvilchezalmera@gmail.com>
2016-02-11 17:37:52 +0000

    [fermat] CBP - User Level Business Transaction - Layer - Assign class name to every Thread (Agent) in the User Level Business Transaction laye (#4827)

--- CBP/plugin/user_level_business_transaction/fermat-cbp-plugin-user-level-business-transaction-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/user_level_business_transaction/customer_broker_sale/developer/bitdubai/version_1/structure/events/UserLevelBusinessTransactionCustomerBrokerSaleMonitorAgent.java
+++ CBP/plugin/user_level_business_transaction/fermat-cbp-plugin-user-level-business-transaction-customer-broker-sale-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/user_level_business_transaction/customer_broker_sale/developer/bitdubai/version_1/structure/events/UserLevelBusinessTransactionCustomerBrokerSaleMonitorAgent.java
@@ -140,7 +140,7 @@ public class UserLevelBusinessTransactionCustomerBrokerSaleMonitorAgent extends 
                     process();
             }
         }
-                , "UserLevelBusinessTransactionCustomerBrokerSaleMonitorAgent");
+                , this.getClass().getSimpleName());
     }
 
     @Override
