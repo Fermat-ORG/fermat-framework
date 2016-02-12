89fea5bd903791161d7e374aabcdc5d114733039
alexanderejm <alex_jimenez76@hotmail.com>
2016-02-12 00:55:09 -0430

    #4824 new test were added

---
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-customer-offline-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/customer_offline_payment/developer/bitdubai/version_1/database/CustomerOfflinePaymentBusinessTransactionDaoTest/getCustomerOfflinePaymentRecordTest.java
@@ -0,0 +1,36 @@
+package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_offline_payment.developer.bitdubai.version_1.database.CustomerOfflinePaymentBusinessTransactionDaoTest;
+
+import com.bitdubai.fermat_cbp_plugin.layer.business_transaction.customer_offline_payment.developer.bitdubai.version_1.database.CustomerOfflinePaymentBusinessTransactionDao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by alexander jimenez (alex_jimenez76@hotmail.com) on 01/02/16.
+ */
+public class getCustomerOfflinePaymentRecordTest {
+    @Mock
+    private CustomerOfflinePaymentBusinessTransactionDao customerOfflinePaymentBusinessTransactionDao;
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void getCustomerOfflinePaymentRecord_Should_Run_Once() throws Exception{
+        customerOfflinePaymentBusinessTransactionDao.getCustomerOfflinePaymentRecord(anyString());
+        verify(customerOfflinePaymentBusinessTransactionDao,Mockito.times(1)).getCustomerOfflinePaymentRecord(anyString());
+    }
+
+    @Test(expected = Exception.class)
+    public void getCustomerOfflinePaymentRecord_Should_Return_Exception() throws Exception{
+        customerOfflinePaymentBusinessTransactionDao = new CustomerOfflinePaymentBusinessTransactionDao(null,null,null);
+        customerOfflinePaymentBusinessTransactionDao.getCustomerOfflinePaymentRecord(null);
+    }
+}
