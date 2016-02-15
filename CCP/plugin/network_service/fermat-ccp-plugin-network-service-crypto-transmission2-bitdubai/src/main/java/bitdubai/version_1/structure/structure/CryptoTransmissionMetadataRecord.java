6952ccfe6e36f2823608b0d288f3676f6c2ee6de
natalia <natalia_veronica_c@hotmail.com>
2016-02-15 14:27:50 -0300

    Crypto Transmission Record json fix

--- CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-transmission2-bitdubai/src/main/java/bitdubai/version_1/structure/structure/CryptoTransmissionMetadataRecord.java
+++ CCP/plugin/network_service/fermat-ccp-plugin-network-service-crypto-transmission2-bitdubai/src/main/java/bitdubai/version_1/structure/structure/CryptoTransmissionMetadataRecord.java
@@ -13,7 +13,6 @@ import java.util.UUID;
  */
 public class CryptoTransmissionMetadataRecord extends CryptoTransmissionMessage implements CryptoTransmissionMetadata {
 
-
     private UUID requestId;
     private CryptoCurrency cryptoCurrency;
     private long cryptoAmount;
@@ -21,9 +20,6 @@ public class CryptoTransmissionMetadataRecord extends CryptoTransmissionMessage 
     private String paymentDescription;
     private CryptoTransmissionMetadataType cryptoTransmissionMetadataType;
     private long timestamp;
-
-    private CryptoTransmissionMessageType cryptoTransmissionMessageType;
-
     public CryptoTransmissionMetadataRecord(UUID transactionId, CryptoTransmissionMessageType cryptoTransmissionMessage, UUID requestId, CryptoCurrency cryptoCurrency, long cryptoAmount, String senderPublicKey, String destinationPublicKey, String associatedCryptoTransactionHash, String paymentDescription, CryptoTransmissionProtocolState cryptoTransmissionProtocolState, CryptoTransmissionMetadataType cryptoTransmissionMetadataType, long timestamp, boolean pendingFlag, int sentCount, CryptoTransmissionMetadataState cryptoTransmissionMetadataState) {
         super(transactionId, cryptoTransmissionMessage,senderPublicKey,destinationPublicKey,cryptoTransmissionProtocolState,cryptoTransmissionMetadataState,pendingFlag,sentCount);
         this.requestId = requestId;
@@ -34,60 +30,45 @@ public class CryptoTransmissionMetadataRecord extends CryptoTransmissionMessage 
         this.cryptoTransmissionMetadataType = cryptoTransmissionMetadataType;
         this.timestamp = timestamp;
     }
-
     @Override
     public UUID getRequestId() {
         return requestId;
     }
-
     @Override
     public CryptoCurrency getCryptoCurrency() {
         return cryptoCurrency;
     }
-
     @Override
     public long getCryptoAmount() {
         return cryptoAmount;
     }
-
     @Override
     public String getSenderPublicKey() {
         return super.getSenderPublicKey();
     }
-
     @Override
     public String getDestinationPublicKey() {
         return super.getDestinationPublicKey();
     }
-
     @Override
     public String getAssociatedCryptoTransactionHash() {
         return associatedCryptoTransactionHash;
     }
-
     @Override
     public String getPaymentDescription() {
         return paymentDescription;
     }
-
-
     public CryptoTransmissionMetadataType getCryptoTransmissionMetadataType() {
         return cryptoTransmissionMetadataType;
     }
-
-
     @Override
     public CryptoTransmissionProtocolState getCryptoTransmissionProtocolState() {
         return super.getCryptoTransmissionProtocolState();
     }
-
-
     @Override
     public CryptoTransmissionMetadataState getCryptoTransmissionMetadataStates() {
         return super.getCryptoTransmissionMetadataState();
     }
-
-
     @Override
     public void changeCryptoTransmissionProtocolState(CryptoTransmissionProtocolState cryptoTransmissionProtocolState) {
         setProtocolState(cryptoTransmissionProtocolState);
@@ -96,39 +77,24 @@ public class CryptoTransmissionMetadataRecord extends CryptoTransmissionMessage 
     public void changeMetadataState(CryptoTransmissionMetadataState cryptoTransmissionNotificationStates) {
         setCryptoTransmissionMetadataState(cryptoTransmissionNotificationStates);
     }
-
     @Override
     public void setTypeMetadata(CryptoTransmissionMetadataType cryptoTransmissionMetadataType) {
         this.cryptoTransmissionMetadataType =cryptoTransmissionMetadataType;
     }
-
-
-    public void setCryptoTransmissionMessageType(CryptoTransmissionMessageType cryptoTransmissionMessageType) {
-        this.cryptoTransmissionMessageType =cryptoTransmissionMessageType;
-    }
-
     @Override
     public boolean isPendigToRead() {
         return super.isPendigToRead();
     }
-
     @Override
     public void confirmRead() {
         setPendingToRead(true);
     }
-
     @Override
     public long getTimestamp(){
         return timestamp;
     }
-
     @Override
     public void setPendingToRead(boolean pending) {
         super.setPendigToRead(pending);
     }
-
-
-
-
-
 }
