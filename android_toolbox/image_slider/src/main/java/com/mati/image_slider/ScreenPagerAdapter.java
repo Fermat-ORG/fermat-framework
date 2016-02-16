9e0f40d883e03f662bceacccd5dbc601f136b3a9
mati <mati_fur@hotmail.com>
2016-02-16 01:22:02 -0300

    Slider implemented FIX #4877

---
+++ android_toolbox/image_slider/src/main/java/com/mati/image_slider/ScreenPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.mati.image_slider;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+
+import java.util.List;
+
+
+/**
+ * Created by Matias on 23/07/2015.
+ */
+
+/**
+ * ScreenPagerAdapter to add new subApp
+ */
+public class ScreenPagerAdapter extends FragmentPagerAdapter {
+
+
+    private List<AbstractFermatFragment> fragments;
+
+    /**
+     * @param fm
+     * @param fragments
+     */
+
+    public ScreenPagerAdapter(FragmentManager fm, List<AbstractFermatFragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+    /*
+     * Return the Fragment associated with a specified position.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragments.get(position);
+    }
+
+    /*
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+
+    public List<AbstractFermatFragment> getLstCurrentFragments() {
+        return fragments;
+    }
+
+}
---
+++ android_toolbox/image_slider/src/main/java/com/mati/image_slider/SliderAdapter.java
@@ -0,0 +1,7 @@
+package com.mati.image_slider;
+
+/**
+ * Created by mati on 2016.02.16..
+ */
+public class SliderAdapter {
+}
