859925fe464866444391fecc24e9ee1bdb5d3bdc
jinmyjbv <jinmybohorquez@gmail.com>
2016-02-17 14:00:53 -0430

    DAP - Sub App - Asset User Community - QA - Android #4094
    changes: items in the action overflow.

--- DAP/android/sub_app/fermat-dap-android-sub-app-asset-user-community-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_asset_user_community_bitdubai/fragments/UserCommuinityGroupUsersFragment.java
+++ DAP/android/sub_app/fermat-dap-android-sub-app-asset-user-community-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_sub_app_asset_user_community_bitdubai/fragments/UserCommuinityGroupUsersFragment.java
@@ -229,8 +229,8 @@ public class UserCommuinityGroupUsersFragment extends AbstractFermatFragment imp
                 return true;
             }
         });
-        menu.add(0, SessionConstantsAssetUserCommunity.IC_ACTION_USER_COMMUNITY_HELP_GROUP_MEMBERS, 0, "help").setIcon(R.drawable.dap_community_user_help_icon)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, SessionConstantsAssetUserCommunity.IC_ACTION_USER_COMMUNITY_HELP_GROUP_MEMBERS, 0, "Help").setIcon(R.drawable.dap_community_user_help_icon)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 
     }
     private void setUpPresentation(boolean checkButton) {
