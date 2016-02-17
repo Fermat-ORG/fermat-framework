804f141ac326315376b5ac758802b011721cd1eb
mati <mati_fur@hotmail.com>
2016-02-17 00:33:35 -0300

    search view created

--- android_toolbox/toolbar_widgets/src/main/java/com/mati/toolbar_widgets/search_view/SearchViewWidget.java
+++ android_toolbox/toolbar_widgets/src/main/java/com/mati/toolbar_widgets/search_view/SearchViewWidget.java
@@ -32,7 +32,7 @@ public class SearchViewWidget extends ViewGroup {
         this.menu = menu;
         this.toolbar = toolbar;
         this.searchViewCallback = searchViewCallback;
-        init();
+        //init();
     }
 
     public SearchViewWidget(Context context, AttributeSet attrs) {
@@ -58,68 +58,68 @@ public class SearchViewWidget extends ViewGroup {
         }
     }
 
-    private void init(){
-        LayoutInflater inflater = (LayoutInflater) getContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.search_edit_text, this);
-        searchEditText = (EditText) findViewById(R.id.search);
-        closeSearch = (ImageView) findViewById(R.id.close_search);
-        toolbarTitle = toolbar.getTitle();
-        try {
-
-            menu.add(0, IC_SEARCH, 0, "search").setIcon(R.drawable.ic_action_search)
-                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-            MenuItem menuItem = menu.findItem(IC_SEARCH).setVisible(true);
-            menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem item) {
-                    setMenuElementsVisibility(false);
-                    toolbar.setTitle("");
-                    toolbar.addView(getRootView());
-                    if (closeSearch != null)
-                        closeSearch.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                setMenuElementsVisibility(true);
-                                toolbar.removeView(getRootView());
-                                toolbar.setTitle(toolbarTitle);
-                                if(searchViewCallback!=null)searchViewCallback.onCloseView();
-                            }
-                        });
-
-                    if (searchEditText != null) {
-                        searchEditText.addTextChangedListener(new TextWatcher() {
-                            @Override
-                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                            }
-
-                            @Override
-                            public void onTextChanged(final CharSequence s, int start, int before, int count) {
-                                if (s.length() > 0) {
-                                    if(searchViewCallback!=null)searchViewCallback.onTextChange(s,start,before,count);
-                                } else {
-                                    setMenuElementsVisibility(true);
-                                    toolbar.removeView(getRootView());
-                                    toolbar.setTitle(toolbarTitle);
-                                    if(searchViewCallback!=null)searchViewCallback.onCloseView();
-                                }
-                            }
-
-                            @Override
-                            public void afterTextChanged(Editable s) {
-
-                            }
-                        });
-                    }
-                    return false;
-                }
-            });
-
-        } catch (Exception e) {
-            searchViewCallback.onErrorOccur(e);
-        }
-    }
+//    private void init(){
+//        LayoutInflater inflater = (LayoutInflater) getContext()
+//                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        inflater.inflate(R.layout.search_edit_text, this);
+//        searchEditText = (EditText) findViewById(R.id.search);
+//        closeSearch = (ImageView) findViewById(R.id.close_search);
+//        toolbarTitle = toolbar.getTitle();
+//        try {
+//
+//            menu.add(0, IC_SEARCH, 0, "search").setIcon(R.drawable.ic_action_search)
+//                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//            MenuItem menuItem = menu.findItem(IC_SEARCH).setVisible(true);
+//            menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+//                @Override
+//                public boolean onMenuItemClick(MenuItem item) {
+//                    setMenuElementsVisibility(false);
+//                    toolbar.setTitle("");
+//                    toolbar.addView(getRootView());
+//                    if (closeSearch != null)
+//                        closeSearch.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//                                setMenuElementsVisibility(true);
+//                                toolbar.removeView(getRootView());
+//                                toolbar.setTitle(toolbarTitle);
+//                                if(searchViewCallback!=null)searchViewCallback.onCloseView();
+//                            }
+//                        });
+//
+//                    if (searchEditText != null) {
+//                        searchEditText.addTextChangedListener(new TextWatcher() {
+//                            @Override
+//                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//                            }
+//
+//                            @Override
+//                            public void onTextChanged(final CharSequence s, int start, int before, int count) {
+//                                if (s.length() > 0) {
+//                                    if(searchViewCallback!=null)searchViewCallback.onTextChange(s,start,before,count);
+//                                } else {
+//                                    setMenuElementsVisibility(true);
+//                                    toolbar.removeView(getRootView());
+//                                    toolbar.setTitle(toolbarTitle);
+//                                    if(searchViewCallback!=null)searchViewCallback.onCloseView();
+//                                }
+//                            }
+//
+//                            @Override
+//                            public void afterTextChanged(Editable s) {
+//
+//                            }
+//                        });
+//                    }
+//                    return false;
+//                }
+//            });
+//
+//        } catch (Exception e) {
+//            searchViewCallback.onErrorOccur(e);
+//        }
+//    }
 
 
     public static class Builder{
