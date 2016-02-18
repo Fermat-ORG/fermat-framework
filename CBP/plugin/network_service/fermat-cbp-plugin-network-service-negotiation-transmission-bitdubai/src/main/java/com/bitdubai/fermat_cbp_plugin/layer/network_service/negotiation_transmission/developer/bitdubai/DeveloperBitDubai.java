df072c331c1864a05102c2fe86448e91841f8f06
franklinmarcano1970 <franklinmarcano1970>
2016-02-18 08:38:37 -0430

    Fix

--- CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/DeveloperBitDubai.java
+++ CBP/plugin/network_service/fermat-cbp-plugin-network-service-negotiation-transmission-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/network_service/negotiation_transmission/developer/bitdubai/DeveloperBitDubai.java
@@ -24,8 +24,8 @@ public class DeveloperBitDubai extends AbstractPluginDeveloper implements Plugin
     @Override
     public void start() throws CantStartPluginDeveloperException {
         try {
-            this.registerVersion(new NetworkServiceNegotiationTransmissionPluginRoot());
-//            this.registerVersion(new NetworkServiceNegotiationTransmissionNew());
+//            this.registerVersion(new NetworkServiceNegotiationTransmissionPluginRoot());
+            this.registerVersion(new NetworkServiceNegotiationTransmissionNew());
         } catch (CantRegisterVersionException e) {
             throw new CantStartPluginDeveloperException(e, "", "Error registering plugin versions for the developer.");
         }
