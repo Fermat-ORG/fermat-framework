000741966263a79b05a60897b532aa98f061f765
Nelson Ramirez <nelsonalfo@gmail.com>
2016-02-17 21:20:07 -0430

    agregados dos nuevos viewHolders: uno para amounts y otro para fecha de expiracion

---
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/common/holders/negotiation_details/clauseViewHolder/AmountViewHolder.java
@@ -0,0 +1,88 @@
+package com.bitdubai.reference_wallet.crypto_broker_wallet.common.holders.negotiation_details.clauseViewHolder;
+
+import android.view.View;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatButton;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseType;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.ClauseInformation;
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.CustomerBrokerNegotiationInformation;
+import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
+
+import java.util.Map;
+
+
+/**
+ *Created by Yordin Alayn on 22.01.16.
+ * Based in AmountToBuyViewHolder of Star_negotiation by nelson
+ */
+public class AmountViewHolder extends ClauseViewHolder implements View.OnClickListener {
+
+    private FermatTextView currencyTextValue;
+    private FermatTextView amountText;
+    private FermatButton amountValue;
+
+    public AmountViewHolder(View itemView, int holderType) {
+        super(itemView, holderType);
+
+
+        currencyTextValue = (FermatTextView) itemView.findViewById(R.id.cbw_amount_currency);
+        amountText = (FermatTextView) itemView.findViewById(R.id.cbw_amount_text);
+        amountValue = (FermatButton) itemView.findViewById(R.id.cbw_amount_value);
+        amountValue.setOnClickListener(this);
+    }
+
+    @Override
+    public void bindData(CustomerBrokerNegotiationInformation data, ClauseInformation clause, int position) {
+        super.bindData(data, clause, position);
+
+        currencyTextValue.setText(clause.getValue());
+        amountValue.setText(clause.getValue());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener != null)
+            listener.onClauseClicked(amountValue, clause, clausePosition);
+    }
+
+    @Override
+    public void setViewResources(int titleRes, int positionImgRes, int... stringResources) {
+        titleTextView.setText(titleRes);
+        clauseNumberImageView.setImageResource(positionImgRes);
+        amountText.setText(stringResources[0]);
+    }
+
+    @Override
+    protected int getConfirmButtonRes() {
+        return R.id.cbw_confirm_button;
+    }
+
+    @Override
+    protected int getClauseNumberImageViewRes() {
+        return R.id.cbw_clause_number;
+    }
+
+    @Override
+    protected int getTitleTextViewRes() {
+        return R.id.cbw_card_view_title;
+    }
+
+    @Override
+    protected void onAcceptedStatus() {
+        amountText.setTextColor(getColor(R.color.description_text_status_accepted));
+        currencyTextValue.setTextColor(getColor(R.color.text_value_status_accepted));
+    }
+
+    @Override
+    protected void setChangedStatus() {
+        amountText.setTextColor(getColor(R.color.description_text_status_changed));
+        currencyTextValue.setTextColor(getColor(R.color.text_value_status_changed));
+    }
+
+    @Override
+    protected void onToConfirmStatus() {
+        amountText.setTextColor(getColor(R.color.description_text_status_confirm));
+        currencyTextValue.setTextColor(getColor(R.color.text_value_status_confirm));
+    }
+}
