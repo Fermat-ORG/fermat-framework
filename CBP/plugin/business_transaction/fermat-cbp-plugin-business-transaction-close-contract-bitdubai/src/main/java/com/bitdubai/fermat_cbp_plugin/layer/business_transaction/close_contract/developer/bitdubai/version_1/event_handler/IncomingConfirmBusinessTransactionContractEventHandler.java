ac510c001df9902bfed9f0d5832711a74dfa9b39
juan sulbaran <sulbaranja@gmail.com>
2016-02-21 23:09:00 -0430

    #4931 and #4930 fixed

--- CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-close-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/close_contract/developer/bitdubai/version_1/event_handler/IncomingConfirmBusinessTransactionContractEventHandler.java
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-close-contract-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/close_contract/developer/bitdubai/version_1/event_handler/IncomingConfirmBusinessTransactionContractEventHandler.java
@@ -1,16 +1,21 @@
 package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.close_contract.developer.bitdubai.version_1.event_handler;
 
 import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEvent;
 import com.bitdubai.fermat_api.layer.dmp_transaction.TransactionServiceNotStartedException;
 import com.bitdubai.fermat_cbp_api.all_definition.exceptions.CantSaveEventException;
 import com.bitdubai.fermat_cbp_api.layer.network_service.transaction_transmission.events.IncomingConfirmBusinessTransactionContract;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 07/12/15.
  */
 public class IncomingConfirmBusinessTransactionContractEventHandler extends AbstractCloseContractEventHandler {
+
+    ErrorManager errorManager;
     @Override
     public void handleEvent(FermatEvent fermatEvent) throws FermatException {
         if(this.closeContractRecorderService.getStatus()== ServiceStatus.STARTED) {
@@ -18,17 +23,36 @@ public class IncomingConfirmBusinessTransactionContractEventHandler extends Abst
             try {
                 this.closeContractRecorderService.incomingConfirmBusinessTransactionContractEventHandler((IncomingConfirmBusinessTransactionContract) fermatEvent);
             } catch(CantSaveEventException exception){
+
+
+
+                errorManager.reportUnexpectedPluginException(
+                        Plugins.CLOSE_CONTRACT,
+                        UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN,
+                        exception);
                 throw new CantSaveEventException(exception,"Handling the IncomingConfirmBusinessTransactionContractEventHandler", "Check the cause");
             } catch(ClassCastException exception){
                 //Logger LOG = Logger.getGlobal();
                 //LOG.info("EXCEPTION DETECTOR----------------------------------");
                 //exception.printStackTrace();
+                errorManager.reportUnexpectedPluginException(
+                        Plugins.CLOSE_CONTRACT,
+                        UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN,
+                        exception);
+
                 throw new CantSaveEventException(FermatException.wrapException(exception), "Handling the IncomingConfirmBusinessTransactionContractEventHandler", "Cannot cast this event");
             } catch(Exception exception){
+
+                errorManager.reportUnexpectedPluginException(
+                        Plugins.CLOSE_CONTRACT,
+                        UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN,
+                        exception);
                 throw new CantSaveEventException(exception,"Handling the IncomingConfirmBusinessTransactionContractEventHandler", "Unexpected exception");
             }
 
         }else {
+
+
             throw new TransactionServiceNotStartedException();
         }
     }
