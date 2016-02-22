5fcdec4ff1eb0d4f3310bb624eee69d5e42e7c15
mati <mati_fur@hotmail.com>
2016-02-22 12:22:17 -0300

    settings icon

--- fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/top_settings/FermatNetworkSettings.java
+++ fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/top_settings/FermatNetworkSettings.java
@@ -2,6 +2,7 @@ package com.bitdubai.android_core.app.common.version_1.top_settings;
 
 import com.mati.fermat_preference_settings.settings.FermatPreferenceFragment;
 import com.mati.fermat_preference_settings.settings.interfaces.PreferenceSettingsItem;
+import com.mati.fermat_preference_settings.settings.models.PreferenceSettingsDialogItem;
 import com.mati.fermat_preference_settings.settings.models.PreferenceSettingsEditText;
 
 import java.util.Arrays;
@@ -22,7 +23,13 @@ public class FermatNetworkSettings extends FermatPreferenceFragment{
     protected List<PreferenceSettingsItem> setSettingsItems() {
         PreferenceSettingsEditText preferenceSettingsEditText = new PreferenceSettingsEditText("IP","enter new IP");
         PreferenceSettingsEditText preferenceSettingsEditText2 = new PreferenceSettingsEditText("Port","enter new Port");
-        return Arrays.asList(new PreferenceSettingsItem[]{preferenceSettingsEditText, preferenceSettingsEditText2});
+        PreferenceSettingsDialogItem preferenceSettingsDialogItem = new PreferenceSettingsDialogItem() {
+            @Override
+            public String getText() {
+                return "toca";
+            }
+        };
+        return Arrays.asList(new PreferenceSettingsItem[]{preferenceSettingsEditText, preferenceSettingsEditText2,preferenceSettingsDialogItem});
     }
 
     @Override
