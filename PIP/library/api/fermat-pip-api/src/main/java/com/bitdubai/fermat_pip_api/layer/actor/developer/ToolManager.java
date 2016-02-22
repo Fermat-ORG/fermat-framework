d868ab1076305b8ddd729722fb263ca148f0d086
Nerio Indriago <nerioindriago@gmail.com>
2016-02-22 00:39:05 -0430

    Clean Code PIP - #4711

--- PIP/library/api/fermat-pip-api/src/main/java/com/bitdubai/fermat_pip_api/layer/actor/developer/ToolManager.java
+++ PIP/library/api/fermat-pip-api/src/main/java/com/bitdubai/fermat_pip_api/layer/actor/developer/ToolManager.java
@@ -8,8 +8,8 @@ import com.bitdubai.fermat_pip_api.layer.actor.exception.CantGetLogTool;
  */
 public interface ToolManager extends FermatManager {
 
-public DatabaseTool getDatabaseTool() throws com.bitdubai.fermat_pip_api.layer.actor.exception.CantGetDataBaseTool;
+DatabaseTool getDatabaseTool() throws com.bitdubai.fermat_pip_api.layer.actor.exception.CantGetDataBaseTool;
 
-public LogTool getLogTool() throws CantGetLogTool;
+LogTool getLogTool() throws CantGetLogTool;
 
 }
