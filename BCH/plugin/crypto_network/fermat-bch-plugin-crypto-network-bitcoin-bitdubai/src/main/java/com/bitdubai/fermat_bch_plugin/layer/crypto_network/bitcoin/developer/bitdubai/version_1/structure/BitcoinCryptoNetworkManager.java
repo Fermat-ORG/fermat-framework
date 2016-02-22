f7b5e6a8e1120f0a2970ea2bae2961c27ff0acdb
Nerio Indriago <nerioindriago@gmail.com>
2016-02-22 00:47:07 -0430

    Clean Code BCH - #4711

--- BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/bitcoin/developer/bitdubai/version_1/structure/BitcoinCryptoNetworkManager.java
+++ BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/bitcoin/developer/bitdubai/version_1/structure/BitcoinCryptoNetworkManager.java
@@ -258,10 +258,7 @@ public class BitcoinCryptoNetworkManager implements TransactionProtocolManager {
          * I compare both lists.
          */
         newAddresses.removeAll(watchedAddresses);
-        if (newAddresses.isEmpty())
-            return false;
-        else
-            return true;
+        return !newAddresses.isEmpty();
     }
 
     /**
@@ -343,10 +340,7 @@ public class BitcoinCryptoNetworkManager implements TransactionProtocolManager {
         /**
          * If there are still keys, then we have new ones.
          */
-        if (tempKeyList.size() > 0)
-            return true;
-        else
-            return false;
+        return tempKeyList.size() > 0;
     }
 
     /**
@@ -356,10 +350,7 @@ public class BitcoinCryptoNetworkManager implements TransactionProtocolManager {
      * @return
      */
     private boolean isAgentRunning(BlockchainNetworkType blockchainNetworkType) {
-        if (runningAgents.get(blockchainNetworkType) == null)
-            return false;
-        else
-            return true;
+        return runningAgents.get(blockchainNetworkType) != null;
     }
 
     /**
