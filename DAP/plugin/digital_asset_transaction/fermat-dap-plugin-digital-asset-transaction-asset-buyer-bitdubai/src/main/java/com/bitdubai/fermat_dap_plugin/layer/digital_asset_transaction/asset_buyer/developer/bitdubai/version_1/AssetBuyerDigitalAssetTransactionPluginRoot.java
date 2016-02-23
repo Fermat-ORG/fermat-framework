143971a1fc69640fff0bb3d62202b10423c05f3b
Víctor A. Mars M <marsvicam@gmail.com>
2016-02-22 21:04:02 -0430

    CHANGE: Modification of AssetBuyerManager interface
    Issue: #4809

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-buyer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_buyer/developer/bitdubai/version_1/AssetBuyerDigitalAssetTransactionPluginRoot.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-buyer-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/digital_asset_transaction/asset_buyer/developer/bitdubai/version_1/AssetBuyerDigitalAssetTransactionPluginRoot.java
@@ -14,15 +14,14 @@ import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseT
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTableRecord;
 import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.BlockchainNetworkType;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.Database;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
-import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantLoadTableToMemoryException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantOpenDatabaseException;
-import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.CantUpdateRecordException;
 import com.bitdubai.fermat_api.layer.osa_android.database_system.exceptions.DatabaseNotFoundException;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.interfaces.BitcoinNetworkManager;
@@ -36,7 +35,6 @@ import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_buyer.exceptions.
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_buyer.interfaces.AssetBuyerManager;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantDeliverDatabaseException;
 import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.CantStartServiceException;
-import com.bitdubai.fermat_dap_api.layer.dap_transaction.common.exceptions.RecordsNotFoundException;
 import com.bitdubai.fermat_dap_api.layer.dap_wallet.asset_user_wallet.interfaces.AssetUserWalletManager;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_buyer.developer.bitdubai.version_1.developer_utils.AssetBuyerDeveloperDatabaseFactory;
 import com.bitdubai.fermat_dap_plugin.layer.digital_asset_transaction.asset_buyer.developer.bitdubai.version_1.structure.database.AssetBuyerDAO;
@@ -108,7 +106,7 @@ public class AssetBuyerDigitalAssetTransactionPluginRoot extends AbstractPlugin 
         try {
             assetBuyingVault = new AssetBuyingVault(pluginId, pluginFileSystem);
             dao = new AssetBuyerDAO(pluginId, pluginDatabaseSystem, actorAssetUserManager, assetUserWalletManager, assetBuyingVault);
-            transactionManager = new AssetBuyerTransactionManager(dao, actorAssetUserManager);
+            transactionManager = new AssetBuyerTransactionManager(dao, actorAssetUserManager, assetTransmission);
             initializeMonitorAgent();
             initializeRecorderService();
             super.start();
@@ -176,36 +174,27 @@ public class AssetBuyerDigitalAssetTransactionPluginRoot extends AbstractPlugin 
     }
 
     @Override
-    public void acceptNegotiation(UUID negotiationId) throws CantProcessBuyingTransactionException {
+    public void acceptAsset(UUID negotiationId) throws CantProcessBuyingTransactionException {
         try {
-            transactionManager.acceptNegotiation(negotiationId);
-        } catch (RecordsNotFoundException | CantLoadTableToMemoryException | CantUpdateRecordException e) {
-            throw new CantProcessBuyingTransactionException(FermatException.wrapException(e));
+            transactionManager.acceptAsset(negotiationId);
+        } catch (DAPException e) {
+            throw new CantProcessBuyingTransactionException(e);
         }
     }
 
     @Override
-    public void declineNegotiation(UUID negotiationId) throws CantProcessBuyingTransactionException {
+    public void declineAsset(UUID negotiationId) throws CantProcessBuyingTransactionException {
         try {
-            transactionManager.declineNegotiation(negotiationId);
-        } catch (RecordsNotFoundException | CantLoadTableToMemoryException | CantUpdateRecordException e) {
-            throw new CantProcessBuyingTransactionException(FermatException.wrapException(e));
+            transactionManager.declineAsset(negotiationId);
+        } catch (DAPException e) {
+            throw new CantProcessBuyingTransactionException(e);
         }
     }
 
     @Override
-    public void changeDeal(UUID negotiationId, int newQuantityToBuy) throws CantProcessBuyingTransactionException {
+    public List<AssetNegotiation> getNewNegotiations(BlockchainNetworkType networkType) throws CantGetBuyingTransactionsException {
         try {
-            transactionManager.changeDeal(negotiationId, newQuantityToBuy);
-        } catch (DAPException | CantLoadTableToMemoryException | CantUpdateRecordException e) {
-            throw new CantProcessBuyingTransactionException(FermatException.wrapException(e));
-        }
-    }
-
-    @Override
-    public List<AssetNegotiation> getNewNegotiations() throws CantGetBuyingTransactionsException {
-        try {
-            return transactionManager.getNewNegotiations();
+            return transactionManager.getNewNegotiations(networkType);
         } catch (DAPException e) {
             throw new CantGetBuyingTransactionsException(FermatException.wrapException(e));
         }
