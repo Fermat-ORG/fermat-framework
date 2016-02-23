87a9abbb3b3641348b86f59b9efe1aa319317227
mati <mati_fur@hotmail.com>
2016-02-23 16:24:07 -0300

    expandable recycler created

--- expandable_recycler_view/src/main/java/com/mati/expandable_recycler_view/ExpandableRecyclerView.java
+++ expandable_recycler_view/src/main/java/com/mati/expandable_recycler_view/ExpandableRecyclerView.java
@@ -1,7 +1,81 @@
 package com.mati.expandable_recycler_view;
 
+import android.support.v7.widget.RecyclerView;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.interfaces.FermatSession;
+import com.bitdubai.fermat_android_api.ui.enums.FermatRefreshTypes;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.ExpandableRecyclerAdapter;
+import com.bitdubai.fermat_android_api.ui.expandableRecicler.ParentListItem;
+import com.bitdubai.fermat_android_api.ui.fragments.FermatWalletExpandableListFragment;
+import com.bitdubai.fermat_api.layer.pip_engine.interfaces.ResourceProviderManager;
+
+import java.util.List;
+
 /**
  * Created by mati on 2016.02.23..
  */
-public class ExpandableRecyclerView {
+public class ExpandableRecyclerView<M extends ParentListItem,S extends FermatSession,RE extends ResourceProviderManager> extends FermatWalletExpandableListFragment<M,S,RE> {
+
+
+
+    private void ExpandableRecyclerView(){}
+
+
+    @Override
+    protected boolean hasMenu() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return 0;//R.layout.base_main;
+    }
+
+    @Override
+    protected int getSwipeRefreshLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected int getRecyclerLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected boolean recyclerHasFixedSize() {
+        return false;
+    }
+
+    @Override
+    public List<M> getMoreDataAsync(FermatRefreshTypes refreshType, int pos) {
+        return null;
+    }
+
+    @Override
+    public void onPostExecute(Object... result) {
+
+    }
+
+    @Override
+    public void onErrorOccurred(Exception ex) {
+
+    }
+
+    @Override
+    public ExpandableRecyclerAdapter getAdapter() {
+        return null;
+    }
+
+    @Override
+    public RecyclerView.LayoutManager getLayoutManager() {
+        return null;
+    }
+
+    public static class Builder{
+
+        private boolean hasMenu = false;
+
+
+    }
+
 }
