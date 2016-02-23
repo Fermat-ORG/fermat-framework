9c37700f6eb511830074e5a8839a9bbb7fc3cc80
javafrank <contrerasfrank@gmail.com>
2016-02-23 14:04:24 -0430

    #4976 #4942 issuer wallet changes - move Help menu to bottom and show text …

--- DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_wallet_redeem_point_bitdubai/fragments/RedeemPointDetailActivityFragment.java
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_wallet_redeem_point_bitdubai/fragments/RedeemPointDetailActivityFragment.java
@@ -196,8 +196,8 @@ public class RedeemPointDetailActivityFragment extends FermatWalletListFragment<
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        menu.add(0, SessionConstantsRedeemPoint.IC_ACTION_REDEEM_HELP_DETAIL, 0, "Help").setIcon(R.drawable.dap_asset_redeem_help_icon)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, SessionConstantsRedeemPoint.IC_ACTION_REDEEM_HELP_DETAIL, 0, "Help")
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_WITH_TEXT);
     }
 
     @Override
