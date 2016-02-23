f88f7e35b92113b61b49c0f67bca2251c4412bae
Penelope <pennyxz@gmail.com>
2016-02-22 20:47:55 -0430

    FIX #4966 DAP - Sub App - Asset Redeem Point Community - QA - Android - Connecting State

--- DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/redeem/point/developer/bitdubai/version_1/RedeemPointActorPluginRoot.java
+++ DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point-bitdubai/src/main/java/com/bitdubai/fermat_dap_plugin/layer/actor/redeem/point/developer/bitdubai/version_1/RedeemPointActorPluginRoot.java
@@ -371,6 +371,15 @@ public class RedeemPointActorPluginRoot extends AbstractPlugin implements
         }
     }
 
+    @Override
+    public void updateRedeemPointDAPConnectionStateActorNetworService(String actorPublicKey, DAPConnectionState state) throws com.bitdubai.fermat_dap_api.layer.dap_actor.redeem_point.exceptions.CantUpdateRedeemPointException{
+        try {
+            this.redeemPointActorDao.updateRedeemPointRegisteredDAPConnectionState(actorPublicKey,state);
+        } catch (CantUpdateRedeemPointException e) {
+            throw new com.bitdubai.fermat_dap_api.layer.dap_actor.redeem_point.exceptions.CantUpdateRedeemPointException("CAN'T UPDATE REDEEM POINT", e, "", "");
+        }
+    }
+
     public void handleCompleteActorAssetRedeemPointRegistrationNotificationEvent(ActorAssetRedeemPoint actorAssetRedeemPoint) {
         System.out.println("***************************************************************");
         System.out.println("Actor Asset Redeem Point se Registro " + actorAssetRedeemPoint.getName());
