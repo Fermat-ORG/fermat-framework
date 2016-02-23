d4e4071d7dace54acca7ebdada699d5097c699d7
franklinmarcano1970 <franklinmarcano1970>
2016-02-22 20:56:33 -0430

    CCP - Crypto Transaction - Hold update Interface for BlockChainNetworkType #4898 - Constans

--- CBP/plugin/stock_transactions/fermat-cbp-plugin-stock-transactions-crypto-money-restock-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/stock_transactions/crypto_money_restock/developer/bitdubai/version_1/database/StockTransactionsCrpytoMoneyRestockDatabaseConstants.java
+++ CBP/plugin/stock_transactions/fermat-cbp-plugin-stock-transactions-crypto-money-restock-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/stock_transactions/crypto_money_restock/developer/bitdubai/version_1/database/StockTransactionsCrpytoMoneyRestockDatabaseConstants.java
@@ -31,6 +31,7 @@ public class StockTransactionsCrpytoMoneyRestockDatabaseConstants {
     static final String CRYPTO_MONEY_RESTOCK_ORIGIN_TRANSACTION_COLUMN_NAME    = "origin_transaction";
     static final String CRYPTO_MONEY_RESTOCK_PRICE_REFERENCE_COLUMN_NAME       = "price_reference";
     static final String CRYPTO_MONEY_RESTOCK_ORIGIN_TRANSACTION_ID_COLUMN_NAME = "origin_transaction_id";
+    static final String CRYPTO_MONEY_RESTOCK_BLOCK_CHAIN_NETWORK_TYPE_COLUMN_NAME      = "block_chain_network_type";
 
     static final String CRYPTO_MONEY_RESTOCK_FIRST_KEY_COLUMN                    = "transaction_id";
 }
