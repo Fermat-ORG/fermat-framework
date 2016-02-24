97d67e548d1720a23eb89ce9f439be15b961fd87
Gabriel Araujo <gabe_512@hotmail.com>
2016-02-24 18:45:54 -0430

    #4828 CHT - NetworkService - CHAT - Rebuild the plugin structure based on fermat-ccp-plugin-network-service-intra-actor-bitdubai
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

--- CHT/plugin/network_service/fermat-cht-plugin-network-service-chat-bitdubai/src/main/java/com/bitdubai/fermat_cht_plugin/layer/network_service/chat/developer/bitdubai/version_1/database/ChatNetworkServiceDataBaseConstants.java
+++ CHT/plugin/network_service/fermat-cht-plugin-network-service-chat-bitdubai/src/main/java/com/bitdubai/fermat_cht_plugin/layer/network_service/chat/developer/bitdubai/version_1/database/ChatNetworkServiceDataBaseConstants.java
@@ -13,7 +13,7 @@ package com.bitdubai.fermat_cht_plugin.layer.network_service.chat.developer.bitd
  */
 public final class ChatNetworkServiceDataBaseConstants {
 
-    public static final String DATA_BASE_NAME  = "cache_table";
+    public static final String DATA_BASE_NAME  = "chatmetadata_table";
 
 
     /**
@@ -21,27 +21,27 @@ public final class ChatNetworkServiceDataBaseConstants {
      */
     public static final String CHAT_METADATA_TRANSACTION_RECORD_TABLE = "chat_metadata_transaction_record_table";
 
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_ID_COLUMN_NAME = "outgoing_transaction_id";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_HASH_COLUMN_NAME = "outgoing_transaction_hash";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_IDCHAT_COLUMN_NAME = "outgoing_idchat";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_IDOBJECTO_COLUMN_NAME = "outgoing_idobjecto";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_LOCALACTORTYPE_COLUMN_NAME = "outgoing_localactortype";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_LOCALACTORPUBKEY_COLUMN_NAME = "outgoing_localactorpubkey";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_REMOTEACTORTYPE_COLUMN_NAME = "outgoing_remoteactortype";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_REMOTEACTORPUBKEY_COLUMN_NAME = "outgoing_remoteactorpubkey";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_CHATNAME_COLUMN_NAME = "outgoing_chatname";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_CHATSTATUS_COLUMN_NAME = "outgoing_chatstatus";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_MESSAGE_STATUS_COLUMN_NAME = "outgoing_chatmessagestatus";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_DATE_COLUMN_NAME = "outgoing_date";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_IDMENSAJE_COLUMN_NAME = "outgoing_idmensaje";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_MESSAGE_COLUMN_NAME = "outgoing_message";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_DISTRIBUTIONSTATUS_COLUMN_NAME = "outgoing_distributionstatus";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_PROCCES_STATUS_COLUMN_NAME = "outgoing_process";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_PROTOCOL_STATE_COLUMN_NAME = "outgoing_protocol_state";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_SENTDATE_COLUMN_NAME = "outgoing_sent_date";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_READ_MARK_COLUMN_NAME = "outgoing_mark_read";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_SENT_COUNT_COLUMN_NAME ="outgoing_sent_count";
-    public static final String CHAT_METADATA_TRANSACTION_RECORD_XML_MSG_REPRESENTATION = "outgoing_xml";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_ID_COLUMN_NAME = "chatmetadata_transaction_id";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_RESPONSE_TO_COLUMN_NAME = "chatmetadata_transaction_hash";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_IDCHAT_COLUMN_NAME = "chatmetadata_idchat";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_IDOBJECTO_COLUMN_NAME = "chatmetadata_idobjecto";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_LOCALACTORTYPE_COLUMN_NAME = "chatmetadata_localactortype";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_LOCALACTORPUBKEY_COLUMN_NAME = "chatmetadata_localactorpubkey";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_REMOTEACTORTYPE_COLUMN_NAME = "chatmetadata_remoteactortype";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_REMOTEACTORPUBKEY_COLUMN_NAME = "chatmetadata_remoteactorpubkey";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_CHATNAME_COLUMN_NAME = "chatmetadata_chatname";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_CHATSTATUS_COLUMN_NAME = "chatmetadata_chatstatus";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_MESSAGE_STATUS_COLUMN_NAME = "chatmetadata_chatmessagestatus";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_DATE_COLUMN_NAME = "chatmetadata_date";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_IDMENSAJE_COLUMN_NAME = "chatmetadata_idmensaje";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_MESSAGE_COLUMN_NAME = "chatmetadata_message";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_DISTRIBUTIONSTATUS_COLUMN_NAME = "chatmetadata_distributionstatus";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_PROCCES_STATUS_COLUMN_NAME = "chatmetadata_process";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_PROTOCOL_STATE_COLUMN_NAME = "chatmetadata_protocol_state";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_SENTDATE_COLUMN_NAME = "chatmetadata_sent_date";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_READ_MARK_COLUMN_NAME = "chatmetadata_mark_read";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_SENT_COUNT_COLUMN_NAME ="chatmetadata_sent_count";
+    public static final String CHAT_METADATA_TRANSACTION_RECORD_XML_MSG_REPRESENTATION = "chatmetadata_xml";
 
 
     public static final String CHAT_METADATA_TRANSACTION_RECORD_FIRST_KEY_COLUMN = CHAT_METADATA_TRANSACTION_RECORD_ID_COLUMN_NAME;
