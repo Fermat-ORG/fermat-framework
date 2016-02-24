9b3d015885cb0cfe9f0b1596db40811c59cd6f10
Penelope <pennyxz@gmail.com>
2016-02-24 12:03:34 -0430

    DAP - Wallet - Asset User - QA - Android - Users To Transfer List View Update #4989 Changes in Holder

--- DAP/android/reference_wallet/fermat-dap-android-reference-wallet-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_wallet_asset_user_bitdubai/holders/AssetTransferSelectUsersHolder.java
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-asset-user-bitdubai/src/main/java/com/bitdubai/fermat_dap_android_wallet_asset_user_bitdubai/holders/AssetTransferSelectUsersHolder.java
@@ -2,9 +2,12 @@ package com.bitdubai.fermat_dap_android_wallet_asset_user_bitdubai.holders;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bitdubai.fermat_android_api.layer.definition.wallet.utils.ImagesUtils;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
 import com.bitdubai.fermat_android_api.ui.holders.FermatViewHolder;
 import com.bitdubai.fermat_dap_android_wallet_asset_user_bitdubai.R;
@@ -21,6 +24,8 @@ public class AssetTransferSelectUsersHolder extends FermatViewHolder {
 
     private FermatTextView nameText;
     private ImageView selectUsersButton;
+    private ImageView imageViewTransfer;
+
 
     /**
      * Constructor
@@ -33,8 +38,9 @@ public class AssetTransferSelectUsersHolder extends FermatViewHolder {
         this.context = context;
         res = itemView.getResources();
 
-        nameText = (FermatTextView) itemView.findViewById(R.id.userName);
-        selectUsersButton = (ImageView) itemView.findViewById(R.id.selectUserButton);
+        nameText = (FermatTextView) itemView.findViewById(R.id.userNameTransfer);
+        selectUsersButton = (ImageView) itemView.findViewById(R.id.selectUserTransferButton);
+        imageViewTransfer = (ImageView) itemView.findViewById(R.id.imageView_user_tranfer_avatar);
     }
 
     public void bind(final User user) {
@@ -56,5 +62,11 @@ public class AssetTransferSelectUsersHolder extends FermatViewHolder {
                 }
             }
         });
+
+        if (user.getActorAssetUser().getProfileImage() != null && user.getActorAssetUser().getProfileImage().length > 0) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(user.getActorAssetUser().getProfileImage(), 0, user.getActorAssetUser().getProfileImage().length);
+            bitmap = Bitmap.createScaledBitmap(bitmap, 120, 120, true);
+            imageViewTransfer.setImageDrawable(ImagesUtils.getRoundedBitmap(context.getResources(), bitmap));
+        }
     }
 }
