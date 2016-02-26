26b328e099644214bd0c6c2a000be613eff482b9
Nelson Ramirez <nelsonalfo@gmail.com>
2016-02-26 12:13:02 -0430

    Actualizando Adapters para que manejen List<EarningsPairDetails>

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/common/holders/EarningsOverviewViewHolder.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/common/holders/EarningsOverviewViewHolder.java
@@ -6,12 +6,14 @@ import android.view.View;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
 import com.bitdubai.fermat_android_api.ui.holders.FermatViewHolder;
+import com.bitdubai.fermat_api.layer.all_definition.enums.TimeFrequency;
+import com.bitdubai.fermat_api.layer.world.interfaces.Currency;
+import com.bitdubai.fermat_cbp_api.layer.middleware.matching_engine.interfaces.EarningsPairDetail;
 import com.bitdubai.reference_wallet.crypto_broker_wallet.R;
-import com.bitdubai.reference_wallet.crypto_broker_wallet.common.models.EarningTestData;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.Calendar;
+
 
 /**
  * Created by nelson on 27/01/16.
@@ -22,22 +24,22 @@ public class EarningsOverviewViewHolder extends FermatViewHolder {
     private final FermatTextView dateTextView;
 
     public EarningsOverviewViewHolder(View itemView) {
-        super(itemView);
+        super(itemView, 0);
 
         differenceTextView = (FermatTextView) itemView.findViewById(R.id.cbw_earning_value_difference_item);
         dateTextView = (FermatTextView) itemView.findViewById(R.id.cbw_earning_value_date_item);
     }
 
-    public void bind(EarningTestData data, int timeField) {
+    public void bind(EarningsPairDetail data, TimeFrequency frequency, Currency earningCurrency) {
         final NumberFormat numberFormat = DecimalFormat.getInstance();
 
-        final String diff = numberFormat.format(data.getDifference());
-        final String currencyCode = data.getCurrency().getCode();
+        final String diff = numberFormat.format(data.getAmount());
+        final String currencyCode = earningCurrency.getCode();
 
-        if (data.getDifference() > 0) {
+        if (data.getAmount() > 0) {
             differenceTextView.setText(String.format("+ %s %s", diff, currencyCode));
             differenceTextView.setTextColor(Color.parseColor("#39ab89"));
-        } else if (data.getDifference() < 0) {
+        } else if (data.getAmount() < 0) {
             differenceTextView.setText(String.format("%s %s", diff, currencyCode));
             differenceTextView.setTextColor(Color.parseColor("#d14846"));
         } else {
@@ -46,15 +48,15 @@ public class EarningsOverviewViewHolder extends FermatViewHolder {
         }
 
         CharSequence formattedDate;
-        switch (timeField) {
-            case Calendar.MONTH:
-                formattedDate = DateFormat.format("MMM yyyy", data.getTimestamp());
+        switch (frequency) {
+            case MONTHLY:
+                formattedDate = DateFormat.format("MMM yyyy", data.getToTimestamp());
                 break;
-            case Calendar.DATE:
-                formattedDate = DateFormat.format("dd MMM yyyy", data.getTimestamp());
+            case YEARLY:
+                formattedDate = DateFormat.format("yyyy", data.getToTimestamp());
                 break;
             default:
-                formattedDate = DateFormat.format("MMM yyyy", data.getTimestamp());
+                formattedDate = DateFormat.format("MMM yyyy", data.getToTimestamp());
                 break;
         }
 
