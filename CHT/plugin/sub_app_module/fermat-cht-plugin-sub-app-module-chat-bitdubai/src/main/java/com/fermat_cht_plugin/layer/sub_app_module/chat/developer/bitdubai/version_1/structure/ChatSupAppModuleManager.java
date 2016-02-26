359bb2c2d101f5e34638e5fb227748c562f74a15
Manuel <darkpriestrelative@gmail.com>
2016-02-26 16:03:41 -0430

    Fix #5018

--- CHT/plugin/sub_app_module/fermat-cht-plugin-sub-app-module-chat-bitdubai/src/main/java/com/fermat_cht_plugin/layer/sub_app_module/chat/developer/bitdubai/version_1/structure/ChatSupAppModuleManager.java
+++ CHT/plugin/sub_app_module/fermat-cht-plugin-sub-app-module-chat-bitdubai/src/main/java/com/fermat_cht_plugin/layer/sub_app_module/chat/developer/bitdubai/version_1/structure/ChatSupAppModuleManager.java
@@ -1,5 +1,6 @@
 package com.fermat_cht_plugin.layer.sub_app_module.chat.developer.bitdubai.version_1.structure;
 
+import com.bitdubai.fermat_api.layer.all_definition.components.enums.PlatformComponentType;
 import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantDeleteChatException;
 import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantDeleteContactException;
 import com.bitdubai.fermat_cht_api.all_definition.exceptions.CantDeleteMessageException;
@@ -19,6 +20,7 @@ import com.bitdubai.fermat_cht_api.layer.middleware.interfaces.Message;
 import com.bitdubai.fermat_cht_api.layer.middleware.interfaces.MiddlewareChatManager;
 import com.bitdubai.fermat_cht_api.layer.sup_app_module.interfaces.ChatManager;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -127,5 +129,17 @@ public class ChatSupAppModuleManager implements ChatManager {
     @Override
     public String getNetworkServicePublicKey() throws CantGetNetworkServicePublicKeyException {
         return middlewareChatManager.getNetworkServicePublicKey();
+    }
+
+    /**
+     * This method return a HashMap with the possible self identities.
+     * The HashMap contains a Key-value like PlatformComponentType-ActorPublicKey.
+     * If there no identities created in any platform, this hashMaps contains the public chat Network
+     * Service.
+     * @return
+     */
+    @Override
+    public HashMap<PlatformComponentType, String> getSelfIdentities() {
+        return middlewareChatManager.getSelfIdentities();
     }
 }
