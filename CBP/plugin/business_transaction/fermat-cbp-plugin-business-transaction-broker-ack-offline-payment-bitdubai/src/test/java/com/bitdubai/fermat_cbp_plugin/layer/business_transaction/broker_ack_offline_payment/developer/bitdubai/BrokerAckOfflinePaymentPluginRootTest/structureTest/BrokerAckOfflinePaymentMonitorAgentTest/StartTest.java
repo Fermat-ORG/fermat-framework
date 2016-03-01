27762557861fde5f384b020403e99893a2b5d19f
juan sulbaran <sulbaranja@gmail.com>
2016-02-29 21:05:54 -0430

    #4876 start service fixed

---
+++ CBP/plugin/business_transaction/fermat-cbp-plugin-business-transaction-broker-ack-offline-payment-bitdubai/src/test/java/com/bitdubai/fermat_cbp_plugin/layer/business_transaction/broker_ack_offline_payment/developer/bitdubai/BrokerAckOfflinePaymentPluginRootTest/structureTest/BrokerAckOfflinePaymentMonitorAgentTest/StartTest.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cbp_plugin.layer.business_transaction.broker_ack_offline_payment.developer.bitdubai.BrokerAckOfflinePaymentPluginRootTest.structureTest.BrokerAckOfflinePaymentMonitorAgentTest;
+
+/**
+ * Created by Juan Sulbaran sulbaranja@gmail.com on 29/02/16.
+ */
+public class StartTest {
+}
