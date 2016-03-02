58541acedda52f721c343fc75b766d0313d83553
Exilum <jc.juaco@gmail.com>
2016-03-02 00:05:34 -0430

    A new Item was added to the toolbox.

---
+++ android_toolbox/fermat_statistics/src/main/java/com/juaco/fermat_statistics/interfaces/Fermat_market_data.java
@@ -0,0 +1,41 @@
+package com.juaco.fermat_statistics.interfaces;
+
+import android.app.Activity;
+
+import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.IndexInfoSummary;
+import com.bitdubai.fermat_cer_api.all_definition.interfaces.ExchangeRate;
+
+import java.util.Collection;
+
+/**
+ * Created by root on 01/03/16.
+ */
+public class Fermat_market_data {
+
+
+    IndexInfoSummary indexInfo;
+
+    Collection<ExchangeRate> collection;
+
+    Activity activity;
+
+
+
+    public Fermat_market_data(IndexInfoSummary indexInfo, Collection<ExchangeRate> collection, Activity activity){
+        this.indexInfo = indexInfo;
+        this.collection = collection;
+        this.activity = activity;
+    }
+
+    public IndexInfoSummary getIndexInfo() {
+        return indexInfo;
+    }
+
+    public Collection<ExchangeRate> getCollection() {
+        return collection;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+}
