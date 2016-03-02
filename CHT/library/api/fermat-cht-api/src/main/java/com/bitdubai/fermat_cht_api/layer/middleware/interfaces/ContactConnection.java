7b660e619e3be19944b78ea487bc5d5329d6f0d5
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-01 23:10:47 -0430

    CHT-Middleware-Conection tabs to show the list #5042 - Interface

--- CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/middleware/interfaces/ContactConnection.java
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/middleware/interfaces/ContactConnection.java
@@ -21,4 +21,7 @@ public interface ContactConnection {
     void setRemoteActorPublicKey(String remoteActorPublicKey);
     long getCreationDate();
     void setCreationDate(long creationDate);
+    byte[] getProfileImage();
+    void setProfileImage(byte[] profileImage);
+
 }
