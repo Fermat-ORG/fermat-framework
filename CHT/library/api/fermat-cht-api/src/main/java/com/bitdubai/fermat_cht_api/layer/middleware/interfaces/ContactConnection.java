626e940999b5ca6f61f29cf245c687a4fc2546b5
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-01 21:02:59 -0430

    CHT-Middleware-Conection tabs to show the list #5042 - Interface

---
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/middleware/interfaces/ContactConnection.java
@@ -0,0 +1,24 @@
+package com.bitdubai.fermat_cht_api.layer.middleware.interfaces;
+
+import com.bitdubai.fermat_api.layer.all_definition.components.enums.PlatformComponentType;
+
+import java.util.UUID;
+
+/**
+ * Created by franklin on 08/01/16.
+ */
+public interface ContactConnection {
+    //Documentar
+    UUID getContactId();
+    void setContactId(UUID contactId);
+    String getRemoteName();
+    void setRemoteName(String remoteName);
+    String getAlias();
+    void setAlias(String alias);
+    PlatformComponentType getRemoteActorType();
+    void setRemoteActorType(PlatformComponentType remoteActorType);
+    String getRemoteActorPublicKey();
+    void setRemoteActorPublicKey(String remoteActorPublicKey);
+    long getCreationDate();
+    void setCreationDate(long creationDate);
+}
