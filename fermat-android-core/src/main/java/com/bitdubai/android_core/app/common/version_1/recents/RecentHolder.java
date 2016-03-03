f7a6c1d298d4e6b4cf9f392a9c4f8ebdd2be1c82
mati <mati_fur@hotmail.com>
2016-03-03 19:00:12 -0300

    recents apps #5070

---
+++ fermat-android-core/src/main/java/com/bitdubai/android_core/app/common/version_1/recents/RecentHolder.java
@@ -0,0 +1,15 @@
+package com.bitdubai.android_core.app.common.version_1.recents;
+
+import android.view.View;
+
+import com.wirelesspienetwork.overview.model.ViewHolder;
+
+/**
+ * Created by mati on 2016.03.03..
+ */
+public class RecentHolder extends ViewHolder<View,RecentApp>{
+
+    public RecentHolder(View view) {
+        super(view);
+    }
+}
