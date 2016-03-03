0b3bf41f9e2258877272dd5c90229b56a10682f1
Guillermo <guillermo20@gmail.com>
2016-03-03 18:40:06 -0430

    arreglado bug intentando crear el contrato.

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/open_contract/interfaces/AbstractOpenContract.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/open_contract/interfaces/AbstractOpenContract.java
@@ -1,8 +1,11 @@
 package com.bitdubai.fermat_cbp_api.layer.business_transaction.open_contract.interfaces;
 
 import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
 import com.bitdubai.fermat_api.layer.all_definition.exceptions.InvalidParameterException;
 import com.bitdubai.fermat_api.layer.world.exceptions.CantGetIndexException;
+import com.bitdubai.fermat_api.layer.world.interfaces.Currency;
 import com.bitdubai.fermat_cbp_api.all_definition.contract.ContractClause;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseType;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.ContractClauseStatus;
@@ -43,11 +46,11 @@ public abstract class AbstractOpenContract {
             throws InvalidParameterException, CantGetIndexException {
 
         ContractSaleRecord contractRecord=new ContractSaleRecord();
-        MoneyType merchandiseCurrency;
+        Currency merchandiseCurrency = CryptoCurrency.BITCOIN;
         float merchandiseAmount;
         long merchandiseDeliveryExpirationDate;
         float paymentAmount;
-        MoneyType paymentCurrency;
+        Currency paymentCurrency = FiatCurrency.US_DOLLAR;
         long paymentExpirationDate;
         String clauseValue;
         long dayTime;
@@ -62,7 +65,12 @@ public abstract class AbstractOpenContract {
             switch (clauseType){
 
                 case BROKER_CURRENCY:
-                    merchandiseCurrency= MoneyType.getByCode(clauseValue);
+                    if(FiatCurrency.codeExists(clauseValue)){
+                        merchandiseCurrency= FiatCurrency.getByCode(clauseValue);
+                    }
+                    if(CryptoCurrency.codeExists(clauseValue)){
+                        merchandiseCurrency= CryptoCurrency.getByCode(clauseValue);
+                    }
                     contractRecord.setMerchandiseCurrency(merchandiseCurrency);
                     break;
                 case BROKER_CURRENCY_QUANTITY:
@@ -78,7 +86,12 @@ public abstract class AbstractOpenContract {
                     contractRecord.setPaymentAmount(paymentAmount);
                     break;
                 case CUSTOMER_CURRENCY:
-                    paymentCurrency= MoneyType.getByCode(clauseValue);
+                    if(FiatCurrency.codeExists(clauseValue)){
+                        paymentCurrency= FiatCurrency.getByCode(clauseValue);
+                    }
+                    if(CryptoCurrency.codeExists(clauseValue)){
+                        paymentCurrency= CryptoCurrency.getByCode(clauseValue);
+                    }
                     contractRecord.setPaymentCurrency(paymentCurrency);
                     break;
                 case CUSTOMER_DATE_TIME_TO_DELIVER:
