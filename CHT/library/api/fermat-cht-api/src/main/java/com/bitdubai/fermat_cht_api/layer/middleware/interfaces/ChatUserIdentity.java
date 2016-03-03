0d67df2ebc99473a18bdf64df777c99608a12a63
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-03 14:52:17 -0430

    CHT-Middleware-Conection tabs to show the list #5042 - Implementation new property

--- CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/middleware/interfaces/ChatUserIdentity.java
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/middleware/interfaces/ChatUserIdentity.java
@@ -1,5 +1,6 @@
 package com.bitdubai.fermat_cht_api.layer.middleware.interfaces;
 
+import com.bitdubai.fermat_api.layer.all_definition.components.enums.PlatformComponentType;
 import com.bitdubai.fermat_api.layer.modules.common_classes.ActiveActorIdentityInformation;
 
 /**
@@ -28,4 +29,12 @@ public interface ChatUserIdentity extends ActiveActorIdentityInformation {
      * @throws
      */
     String createMessageSignature(String message);
+
+    /**
+     * This method let an intra user sign a message with his unique private key
+     * @param
+     * @return the platformComponentType
+     * @throws
+     */
+    PlatformComponentType getPlatformComponentType();
 }
