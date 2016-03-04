59124f0f6e6d9bdc3c83b2cd7c56bfbadc05a580
Manuel <darkpriestrelative@gmail.com>
2016-03-04 05:33:11 -0430

    Clean up ART and WRD code

---
+++ WRD/library/api/fermat-wrd-api/src/main/java/com/bitdubai/fermat_wrd_api/all_definitions/exceptions/WRDException.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_wrd_api.all_definitions.exceptions;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 03/03/16.
+ */
+public class WRDException {
+}
---
+++ WRD/plugin/api/fermat-wrd-plugin-api-tokenly-bitdubai/src/main/java/com/bitdubai/fermat_wrd_plugin/layer/api/tokenly/developer/bitdubai/version_1/structure/TokenlyProcessor.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_wrd_plugin.layer.api.tokenly.developer.bitdubai.version_1.structure;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 03/03/16.
+ */
+public class TokenlyProcessor {
+}
