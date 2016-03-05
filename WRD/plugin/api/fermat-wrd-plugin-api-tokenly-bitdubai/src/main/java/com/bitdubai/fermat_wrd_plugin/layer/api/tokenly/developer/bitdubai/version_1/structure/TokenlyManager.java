d0ea45905a6121643adb89b6c34ddd74997e4aeb
Manuel <darkpriestrelative@gmail.com>
2016-03-04 22:41:01 -0430

    Creating  TokenlyManager class #5102

---
+++ WRD/plugin/api/fermat-wrd-plugin-api-tokenly-bitdubai/src/main/java/com/bitdubai/fermat_wrd_plugin/layer/api/tokenly/developer/bitdubai/version_1/structure/TokenlyManager.java
@@ -0,0 +1,14 @@
+package com.bitdubai.fermat_wrd_plugin.layer.api.tokenly.developer.bitdubai.version_1.structure;
+
+import com.bitdubai.fermat_wrd_api.layer.api.tokenly.interfaces.TokenlyApiManager;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 04/03/16.
+ */
+public class TokenlyManager implements TokenlyApiManager {
+    @Override
+    public String getBotURLByBotId(String botId) {
+        //TODO: to implement
+        return null;
+    }
+}
