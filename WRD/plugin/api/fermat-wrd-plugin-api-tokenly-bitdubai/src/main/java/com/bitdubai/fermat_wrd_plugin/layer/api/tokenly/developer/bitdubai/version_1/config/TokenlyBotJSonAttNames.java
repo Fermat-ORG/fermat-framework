fae91593ea1b87e18edb337863053a7379611ed3
Manuel <darkpriestrelative@gmail.com>
2016-03-05 21:26:18 -0430

    Fix #5113

---
+++ WRD/plugin/api/fermat-wrd-plugin-api-tokenly-bitdubai/src/main/java/com/bitdubai/fermat_wrd_plugin/layer/api/tokenly/developer/bitdubai/version_1/config/TokenlyBotJSonAttNames.java
@@ -0,0 +1,68 @@
+package com.bitdubai.fermat_wrd_plugin.layer.api.tokenly.developer.bitdubai.version_1.config;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 05/03/16.
+ */
+public class TokenlyBotJSonAttNames {
+
+    /**
+     * Represents the bot id.
+     */
+    public static final String ID = "id";
+
+    /**
+     * Represents the bot name.
+     */
+    public static final String NAME = "name";
+
+    /**
+     * Represents the bot address.
+     */
+    public static final String ADDRESS = "address";
+
+    /**
+     * Represents the bot username.
+     */
+    public static final String USERNAME = "username";
+
+    /**
+     * Represents the bot description.
+     */
+    public static final String DESCRIPTION = "description";
+
+    /**
+     * Represents the bot description html.
+     */
+    public static final String DESCRIPTION_HTML = "descriptionHtml";
+
+    /**
+     * Represents the bot background image details.
+     */
+    public static final String BACKGROUND_DETAILS = "backgroundImageDetails";
+
+    /**
+     * Represents the background overlay settings.
+     */
+    public static final String BACKGROUND_OVERLAY_SETTINGS = "backgroundOverlaySettings";
+
+    /**
+     * Represents the bot swaps.
+     */
+    public static final String SWAPS = "swaps";
+
+    /**
+     * Represents the swap creation date.
+     */
+    public static final String CREATED_AT = "createdAt";
+
+    /**
+     * Represents the swap update date.
+     */
+    public static final String UPDATED_AT = "createdAt";
+
+    /**
+     * Represents the swap completion date.
+     */
+    public static final String COMPLETED_AT = "createdAt";
+
+}
