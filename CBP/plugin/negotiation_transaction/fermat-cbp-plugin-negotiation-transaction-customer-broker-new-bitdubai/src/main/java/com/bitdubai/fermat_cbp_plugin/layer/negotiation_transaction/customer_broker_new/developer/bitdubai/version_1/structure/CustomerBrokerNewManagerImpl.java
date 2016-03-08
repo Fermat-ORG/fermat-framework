0ea845313c88c988fd8f3fda6a4fe3bea18699b3
Yordin Alayn <y.alayn@gmail.com>
2016-03-07 22:33:14 -0430

    CBP - Negotiation Transaction - Customer Broker New - Error Manager

--- CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-new-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_new/developer/bitdubai/version_1/structure/CustomerBrokerNewManagerImpl.java
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-new-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_new/developer/bitdubai/version_1/structure/CustomerBrokerNewManagerImpl.java
@@ -1,6 +1,8 @@
 package com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_new.developer.bitdubai.version_1.structure;
 
 import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchaseNegotiation;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_purchase.interfaces.CustomerBrokerPurchaseNegotiationManager;
 import com.bitdubai.fermat_cbp_api.layer.negotiation.customer_broker_sale.interfaces.CustomerBrokerSaleNegotiation;
@@ -16,6 +18,8 @@ import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_bro
 import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_new.developer.bitdubai.version_1.exceptions.CantNewPurchaseNegotiationTransactionException;
 import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_new.developer.bitdubai.version_1.exceptions.CantNewSaleNegotiationTransactionException;
 import com.bitdubai.fermat_cbp_plugin.layer.negotiation_transaction.customer_broker_new.developer.bitdubai.version_1.exceptions.CantRegisterCustomerBrokerNewNegotiationTransactionException;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,15 +46,24 @@ public class CustomerBrokerNewManagerImpl implements CustomerBrokerNewManager {
     /*Represent the Negotiation Sale*/
     private CustomerBrokerSaleNegotiationManager                customerBrokerSaleNegotiationManager;
 
+    /*Represent the Error Manager*/
+    private ErrorManager                                        errorManager;
+
+    /*Represent Plugin Version*/
+    private PluginVersionReference                              pluginVersionReference;
 
     public CustomerBrokerNewManagerImpl(
         CustomerBrokerNewNegotiationTransactionDatabaseDao  customerBrokerNewNegotiationTransactionDatabaseDao,
         CustomerBrokerPurchaseNegotiationManager            customerBrokerPurchaseNegotiationManager,
-        CustomerBrokerSaleNegotiationManager                customerBrokerSaleNegotiationManager
+        CustomerBrokerSaleNegotiationManager                customerBrokerSaleNegotiationManager,
+        ErrorManager                                        errorManager,
+        PluginVersionReference                              pluginVersionReference
     ){
         this.customerBrokerNewNegotiationTransactionDatabaseDao = customerBrokerNewNegotiationTransactionDatabaseDao;
         this.customerBrokerPurchaseNegotiationManager           = customerBrokerPurchaseNegotiationManager;
         this.customerBrokerSaleNegotiationManager               = customerBrokerSaleNegotiationManager;
+        this.errorManager                                       = errorManager;
+        this.pluginVersionReference                             = pluginVersionReference;
     }
 
     /*IMPLEMENTATION CustomerBrokerNewManager*/
@@ -63,15 +76,19 @@ public class CustomerBrokerNewManagerImpl implements CustomerBrokerNewManager {
 
                 System.out.print("\n\n**** 2) MOCK NEGOTIATION TRANSACTION - CUSTOMER BROKER NEW - MANAGER - PURCHASE NEGOTIATION****\n");
                 customerBrokerNewPurchaseNegotiationTransaction = new CustomerBrokerNewPurchaseNegotiationTransaction(
-                        customerBrokerPurchaseNegotiationManager,
-                        customerBrokerNewNegotiationTransactionDatabaseDao
+                    customerBrokerPurchaseNegotiationManager,
+                    customerBrokerNewNegotiationTransactionDatabaseDao,
+                    errorManager,
+                    pluginVersionReference
                 );
                 customerBrokerNewPurchaseNegotiationTransaction.sendPurchaseNegotiationTranasction(customerBrokerPurchaseNegotiation);
 
             }
         } catch (CantNewPurchaseNegotiationTransactionException e){
+            errorManager.reportUnexpectedPluginException(this.pluginVersionReference, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
             throw new CantCreateCustomerBrokerNewPurchaseNegotiationTransactionException(e.getMessage(),e, CantCreateCustomerBrokerNewPurchaseNegotiationTransactionException.DEFAULT_MESSAGE, "ERROR CREATE CUSTOMER BROKER NEW PURCHASE NEGOTIATION TRANSACTION, UNKNOWN FAILURE.");
         } catch (Exception e){
+            errorManager.reportUnexpectedPluginException(this.pluginVersionReference, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
             throw new CantCreateCustomerBrokerNewPurchaseNegotiationTransactionException(e.getMessage(), FermatException.wrapException(e), CantCreateCustomerBrokerNewPurchaseNegotiationTransactionException.DEFAULT_MESSAGE, "ERROR CREATE CUSTOMER BROKER NEW PURCHASE NEGOTIATION TRANSACTION, UNKNOWN FAILURE.");
         }
     }
@@ -84,8 +101,10 @@ public class CustomerBrokerNewManagerImpl implements CustomerBrokerNewManager {
             return customerBrokerNewNegotiationTransactionDatabaseDao.getRegisterCustomerBrokerNewNegotiationTranasction(transactionId);
 
         } catch (CantRegisterCustomerBrokerNewNegotiationTransactionException e){
+            errorManager.reportUnexpectedPluginException(this.pluginVersionReference, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
             throw new CantGetCustomerBrokerNewNegotiationTransactionException(e.getMessage(), e, CantGetCustomerBrokerNewNegotiationTransactionException.DEFAULT_MESSAGE, "ERROR GET CUSTOMER BROKER NEW NEGOTIATION TRANSACTION, UNKNOWN FAILURE.");
         } catch (Exception e){
+            errorManager.reportUnexpectedPluginException(this.pluginVersionReference, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
             throw new CantGetCustomerBrokerNewNegotiationTransactionException(e.getMessage(), FermatException.wrapException(e), CantCreateCustomerBrokerNewSaleNegotiationTransactionException.DEFAULT_MESSAGE, "ERROR GET CUSTOMER BROKER NEW NEGOTIATION TRANSACTION, UNKNOWN FAILURE.");
         }
 
@@ -99,8 +118,10 @@ public class CustomerBrokerNewManagerImpl implements CustomerBrokerNewManager {
             return customerBrokerNewNegotiationTransactionDatabaseDao.getAllRegisterCustomerBrokerNewNegotiationTranasction();
 
         } catch (CantRegisterCustomerBrokerNewNegotiationTransactionException e){
+            errorManager.reportUnexpectedPluginException(this.pluginVersionReference, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
             throw new CantGetListCustomerBrokerNewNegotiationTransactionException(e.getMessage(), e, CantGetListCustomerBrokerNewNegotiationTransactionException.DEFAULT_MESSAGE, "ERROR GET LIST CUSTOMER BROKER NEW NEGOTIATION TRANSACTION, UNKNOWN FAILURE.");
         } catch (Exception e){
+            errorManager.reportUnexpectedPluginException(this.pluginVersionReference, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,e);
             throw new CantGetListCustomerBrokerNewNegotiationTransactionException(e.getMessage(), FermatException.wrapException(e), CantGetListCustomerBrokerNewNegotiationTransactionException.DEFAULT_MESSAGE, "ERROR GET LIST CUSTOMER BROKER NEW NEGOTIATION TRANSACTION, UNKNOWN FAILURE.");
         }
     }
