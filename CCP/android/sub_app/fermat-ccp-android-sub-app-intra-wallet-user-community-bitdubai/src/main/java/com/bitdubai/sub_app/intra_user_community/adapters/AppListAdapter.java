1ef2af71be63c135c5e576065e7546b49b262f03
natalia <natalia_veronica_c@hotmail.com>
2016-03-09 14:41:57 -0300

    Community Offine set red color

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/adapters/AppListAdapter.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/adapters/AppListAdapter.java
@@ -89,7 +89,8 @@ public class AppListAdapter extends FermatAdapter<IntraUserInformation, AppWorld
                 break;
         }
         holder.row_connection_state.setText(data.getState());
-        holder.row_connection_state.setTextColor(Color.RED);
+        if(data.getState().equals("Offline"))
+            holder.row_connection_state.setTextColor(Color.RED);
         holder.name.setText(data.getName());
         byte[] profileImage = data.getProfileImage();
         if (profileImage != null && profileImage.length > 0) {
