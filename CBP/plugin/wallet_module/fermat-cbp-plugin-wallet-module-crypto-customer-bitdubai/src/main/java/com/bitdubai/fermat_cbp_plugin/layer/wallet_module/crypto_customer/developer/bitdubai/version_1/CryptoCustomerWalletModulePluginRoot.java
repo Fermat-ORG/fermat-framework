db5fd60b4973af40801a46810f9f7cc952970a0c
alejandro <abicelis@gmail.com>
2016-03-08 21:47:51 -0430

    CBP - Customer wallet module - getCompletionDateFromContractStatus() implementation + ContractBasicData modifications so that they include contractId

--- CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_customer/developer/bitdubai/version_1/CryptoCustomerWalletModulePluginRoot.java
+++ CBP/plugin/wallet_module/fermat-cbp-plugin-wallet-module-crypto-customer-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/wallet_module/crypto_customer/developer/bitdubai/version_1/CryptoCustomerWalletModulePluginRoot.java
@@ -20,6 +20,10 @@ import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
 import com.bitdubai.fermat_cbp_api.layer.actor.crypto_customer.interfaces.ActorExtraDataManager;
 import com.bitdubai.fermat_cbp_api.layer.actor_connection.crypto_broker.interfaces.CryptoBrokerActorConnectionManager;
+import com.bitdubai.fermat_cbp_api.layer.business_transaction.broker_ack_offline_payment.interfaces.BrokerAckOfflinePaymentManager;
+import com.bitdubai.fermat_cbp_api.layer.business_transaction.broker_ack_online_payment.interfaces.BrokerAckOnlinePaymentManager;
+import com.bitdubai.fermat_cbp_api.layer.business_transaction.broker_submit_offline_merchandise.interfaces.BrokerSubmitOfflineMerchandiseManager;
+import com.bitdubai.fermat_cbp_api.layer.business_transaction.broker_submit_online_merchandise.interfaces.BrokerSubmitOnlineMerchandiseManager;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.customer_ack_offline_merchandise.interfaces.CustomerAckOfflineMerchandiseManager;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.customer_ack_online_merchandise.interfaces.CustomerAckOnlineMerchandiseManager;
 import com.bitdubai.fermat_cbp_api.layer.business_transaction.customer_offline_payment.interfaces.CustomerOfflinePaymentManager;
@@ -104,6 +108,18 @@ public class CryptoCustomerWalletModulePluginRoot extends AbstractPlugin impleme
     @NeededPluginReference(platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.BUSINESS_TRANSACTION, plugin = Plugins.CUSTOMER_ACK_OFFLINE_MERCHANDISE)
     CustomerAckOfflineMerchandiseManager customerAckOfflineMerchandiseManager;
 
+    @NeededPluginReference(platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.BUSINESS_TRANSACTION, plugin = Plugins.BROKER_ACK_ONLINE_PAYMENT)
+    BrokerAckOnlinePaymentManager brokerAckOnlinePaymentManager;
+
+    @NeededPluginReference(platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.BUSINESS_TRANSACTION, plugin = Plugins.BROKER_ACK_OFFLINE_PAYMENT)
+    BrokerAckOfflinePaymentManager brokerAckOfflinePaymentManager;
+
+    @NeededPluginReference(platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.BUSINESS_TRANSACTION, plugin = Plugins.BROKER_SUBMIT_ONLINE_MERCHANDISE)
+    BrokerSubmitOnlineMerchandiseManager brokerSubmitOnlineMerchandiseManager;
+
+    @NeededPluginReference(platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.BUSINESS_TRANSACTION, plugin = Plugins.BROKER_SUBMIT_OFFLINE_MERCHANDISE)
+    BrokerSubmitOfflineMerchandiseManager brokerSubmitOfflineMerchandiseManager;
+
     @NeededPluginReference(platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.ACTOR_CONNECTION     , plugin = Plugins.CRYPTO_BROKER     )
     private CryptoBrokerActorConnectionManager cryptoBrokerActorConnectionManager;
 
@@ -144,6 +160,10 @@ public class CryptoCustomerWalletModulePluginRoot extends AbstractPlugin impleme
                         customerOfflinePaymentManager,
                         customerAckOnlineMerchandiseManager,
                         customerAckOfflineMerchandiseManager,
+                        brokerAckOnlinePaymentManager,
+                        brokerAckOfflinePaymentManager,
+                        brokerSubmitOnlineMerchandiseManager,
+                        brokerSubmitOfflineMerchandiseManager,
                         getSettingsManager(),
                         errorManager,
                         this.getPluginVersionReference()
