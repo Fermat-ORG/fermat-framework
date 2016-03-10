1c14b89ffe1cce9e048a23e8c3cb24afd30739e2
Gabriel Araujo <gabe_512@hotmail.com>
2016-03-09 22:55:35 -0430

    #5183
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

---
+++ ART/library/api/fermat-art-api/src/main/java/com/bitdubai/fermat_art_api/layer/actor_network_service/interfaces/artist/ArtistActor.java
@@ -0,0 +1,9 @@
+package com.bitdubai.fermat_art_api.layer.actor_network_service.interfaces.artist;
+
+import com.bitdubai.fermat_art_api.layer.actor_network_service.interfaces.ActorNotification;
+
+/**
+ * Created by Gabriel Araujo (gabe_512@hotmail.com) on 09/03/16.
+ */
+public interface ArtistActor extends ActorNotification {
+}
