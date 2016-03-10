b03ea9ca5beb87a240ceeb7cea9d6b7f5de2c783
Gabriel Araujo <gabe_512@hotmail.com>
2016-03-09 22:58:41 -0430

    #5197
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

---
+++ ART/library/api/fermat-art-api/src/main/java/com/bitdubai/fermat_art_api/layer/actor_network_service/exceptions/CantAskConnectionActorArtistNetworkServiceException.java
@@ -0,0 +1,33 @@
+package com.bitdubai.fermat_art_api.layer.actor_network_service.exceptions;
+
+import com.bitdubai.fermat_art_api.all_definition.exceptions.ARTException;
+
+/**
+ * Created by franklin on 15/10/15.
+ */
+public class CantAskConnectionActorArtistNetworkServiceException extends ARTException {
+
+
+    public static final String DEFAULT_MESSAGE = "CAN'T REGISTER NEW USER";
+
+    public CantAskConnectionActorArtistNetworkServiceException(final String message, final Exception cause, final String context, final String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    public CantAskConnectionActorArtistNetworkServiceException(final String message, final Exception cause) {
+        this(message, cause, "", "");
+    }
+
+    public CantAskConnectionActorArtistNetworkServiceException(final String message) {
+        this(message, null);
+    }
+
+    public CantAskConnectionActorArtistNetworkServiceException(final Exception exception) {
+        this(exception.getMessage());
+        setStackTrace(exception.getStackTrace());
+    }
+
+    public CantAskConnectionActorArtistNetworkServiceException() {
+        this(DEFAULT_MESSAGE);
+    }
+}
