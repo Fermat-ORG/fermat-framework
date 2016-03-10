ab5b41f2a23d348d9e5a18304341ac6cf3b229a1
mati <mati_fur@hotmail.com>
2016-03-10 15:07:39 -0300

    communities fragment created

---
+++ DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-manager-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_manager/fragment/CommunitiesFragment.java
@@ -0,0 +1,15 @@
+package com.bitdubai.sub_app.wallet_manager.fragment;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
+
+/**
+ * Created by mati on 2016.03.10..
+ */
+public class CommunitiesFragment extends AbstractFermatFragment {
+
+
+
+
+
+
+}
