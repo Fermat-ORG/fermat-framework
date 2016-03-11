2b0fd67b779f2a703e614a89c1145c843a825e15
Gabriel Araujo <gabe_512@hotmail.com>
2016-03-10 21:51:47 -0430

    #5217
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

--- ART/library/core/fermat-art-core/src/main/java/com/bitdubai/fermat_art_core/ARTPlatform.java
+++ ART/library/core/fermat-art-core/src/main/java/com/bitdubai/fermat_art_core/ARTPlatform.java
@@ -3,6 +3,7 @@ package com.bitdubai.fermat_art_core;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PlatformReference;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_art_core.layer.actor_network_service.ActorNetworkServiceLayer;
+import com.bitdubai.fermat_art_core.layer.identity.IdentityLayer;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.abstract_classes.AbstractPlatform;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.exceptions.CantRegisterLayerException;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.exceptions.CantStartPlatformException;
@@ -22,6 +23,7 @@ public class ARTPlatform extends AbstractPlatform {
         try {
 
             registerLayer(new ActorNetworkServiceLayer());
+            registerLayer(new IdentityLayer());
 
         } catch (CantRegisterLayerException e) {
 
