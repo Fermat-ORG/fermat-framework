b1d02cacc3a98718865f5215c9e10078f3250fdb
alexanderejm <alex_jimenez76@hotmail.com>
2016-03-15 01:14:47 -0430

    basic Art Fan Identity Module and Art Artist Identity Module updated

--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-fan-identity-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/fan_identity/developer/bitdubai/version_1/FanIdentityPluginRoot.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-fan-identity-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/fan_identity/developer/bitdubai/version_1/FanIdentityPluginRoot.java
@@ -3,15 +3,54 @@ package com.bitdubai.fermat_art_plugin.layer.sub_app_module.fan_identity.develop
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_art_api.all_definition.exceptions.CantPublishIdentityException;
+import com.bitdubai.fermat_art_api.all_definition.exceptions.IdentityNotFoundException;
+import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantCreateFanIdentityException;
+import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantGetFanIdentityException;
+import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantListFanIdentitiesException;
+import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantUpdateFanIdentityException;
+import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.FanIdentityAlreadyExistsException;
+import com.bitdubai.fermat_art_api.layer.identity.fan.interfaces.Fan;
+import com.bitdubai.fermat_art_api.layer.identity.fan.interfaces.FanIdentityManager;
+
+import java.util.List;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 08/03/16.
  */
-public class FanIdentityPluginRoot extends AbstractPlugin {
+public class FanIdentityPluginRoot extends AbstractPlugin implements FanIdentityManager {
     /**
      * Default constructor
      */
     public FanIdentityPluginRoot() {
         super(new PluginVersionReference(new Version()));
+    }
+
+    @Override
+    public List<Fan> listIdentitiesFromCurrentDeviceUser() throws CantListFanIdentitiesException {
+        List<Fan> listIdentitiesFromCurrentDeviceUser = listIdentitiesFromCurrentDeviceUser();
+        return listIdentitiesFromCurrentDeviceUser;
+    }
+
+    @Override
+    public Fan createArtistIdentity(String alias, byte[] imageBytes) throws CantCreateFanIdentityException, FanIdentityAlreadyExistsException {
+        Fan fan = createArtistIdentity(alias,imageBytes);
+        return fan;
+    }
+
+    @Override
+    public void updateFanIdentity(String alias, String publicKey, byte[] imageProfile, String external) throws CantUpdateFanIdentityException {
+
+    }
+
+    @Override
+    public Fan getFanIdentity(String publicKey) throws CantGetFanIdentityException, IdentityNotFoundException {
+        Fan fan = getFanIdentity(publicKey);
+        return fan;
+    }
+
+    @Override
+    public void publishIdentity(String publicKey) throws CantPublishIdentityException, IdentityNotFoundException {
+
     }
 }
