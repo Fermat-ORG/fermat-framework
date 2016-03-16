6c4ead6a1513552a0950096630ba74a0ab0f59f6
alexanderejm <alex_jimenez76@hotmail.com>
2016-03-15 22:56:35 -0430

    Fix FanIdentityPluginRoot and ArtistIdentityPluginRoot

--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-identity-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_identity/developer/bitdubai/version_1/ArtistIdentityPluginRoot.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-identity-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_identity/developer/bitdubai/version_1/ArtistIdentityPluginRoot.java
@@ -3,63 +3,15 @@ package com.bitdubai.fermat_art_plugin.layer.sub_app_module.artist_identity.deve
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
-import com.bitdubai.fermat_art_api.all_definition.enums.ArtistAcceptConnectionsType;
-import com.bitdubai.fermat_art_api.all_definition.enums.ExposureLevel;
-import com.bitdubai.fermat_art_api.all_definition.enums.ExternalPlatform;
-import com.bitdubai.fermat_art_api.all_definition.exceptions.CantHideIdentityException;
-import com.bitdubai.fermat_art_api.all_definition.exceptions.CantPublishIdentityException;
-import com.bitdubai.fermat_art_api.all_definition.exceptions.IdentityNotFoundException;
-import com.bitdubai.fermat_art_api.layer.identity.artist.exceptions.ArtistIdentityAlreadyExistsException;
-import com.bitdubai.fermat_art_api.layer.identity.artist.exceptions.CantCreateArtistIdentityException;
-import com.bitdubai.fermat_art_api.layer.identity.artist.exceptions.CantGetArtistIdentityException;
-import com.bitdubai.fermat_art_api.layer.identity.artist.exceptions.CantListArtistIdentitiesException;
-import com.bitdubai.fermat_art_api.layer.identity.artist.exceptions.CantUpdateArtistIdentityException;
-import com.bitdubai.fermat_art_api.layer.identity.artist.interfaces.Artist;
-import com.bitdubai.fermat_art_api.layer.identity.artist.interfaces.ArtistIdentityManager;
-
-import java.util.List;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 08/03/16.
  */
-public class ArtistIdentityPluginRoot extends AbstractPlugin implements ArtistIdentityManager{
+public class ArtistIdentityPluginRoot extends AbstractPlugin {
     /**
      * Default constructor
      */
     public ArtistIdentityPluginRoot() {
         super(new PluginVersionReference(new Version()));
-    }
-
-    @Override
-    public List<Artist> listIdentitiesFromCurrentDeviceUser() throws CantListArtistIdentitiesException {
-        List<Artist> listIdentitiesFromCurrentDeviceUser = listIdentitiesFromCurrentDeviceUser();
-        return listIdentitiesFromCurrentDeviceUser;
-    }
-
-    @Override
-    public Artist createArtistIdentity(String alias, byte[] imageBytes) throws CantCreateArtistIdentityException, ArtistIdentityAlreadyExistsException {
-        Artist artist = createArtistIdentity(alias,imageBytes);
-        return artist;
-    }
-
-    @Override
-    public void updateArtistIdentity(String alias, String publicKey, byte[] profileImage, String externalUserName, String externalAccessToken, ExternalPlatform externalPlatform, ExposureLevel exposureLevel, ArtistAcceptConnectionsType artistAcceptConnectionsType) throws CantUpdateArtistIdentityException {
-
-    }
-
-    @Override
-    public Artist getArtistIdentity(String publicKey) throws CantGetArtistIdentityException, IdentityNotFoundException {
-        Artist artist = getArtistIdentity(publicKey);
-        return artist;
-    }
-
-    @Override
-    public void publishIdentity(String publicKey) throws CantPublishIdentityException, IdentityNotFoundException {
-
-    }
-
-    @Override
-    public void hideIdentity(String publicKey) throws CantHideIdentityException, IdentityNotFoundException {
-
     }
 }
--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-fan-identity-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/fan_identity/developer/bitdubai/version_1/FanIdentityPluginRoot.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-fan-identity-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/fan_identity/developer/bitdubai/version_1/FanIdentityPluginRoot.java
@@ -1,56 +1,27 @@
 package com.bitdubai.fermat_art_plugin.layer.sub_app_module.fan_identity.developer.bitdubai.version_1;
 
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
-import com.bitdubai.fermat_art_api.all_definition.exceptions.CantPublishIdentityException;
-import com.bitdubai.fermat_art_api.all_definition.exceptions.IdentityNotFoundException;
-import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantCreateFanIdentityException;
-import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantGetFanIdentityException;
-import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantListFanIdentitiesException;
-import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.CantUpdateFanIdentityException;
-import com.bitdubai.fermat_art_api.layer.identity.fan.exceptions.FanIdentityAlreadyExistsException;
-import com.bitdubai.fermat_art_api.layer.identity.fan.interfaces.Fan;
-import com.bitdubai.fermat_art_api.layer.identity.fan.interfaces.FanIdentityManager;
 
-import java.util.List;
+import java.util.logging.ErrorManager;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 08/03/16.
  */
-public class FanIdentityPluginRoot extends AbstractPlugin implements FanIdentityManager {
+public class FanIdentityPluginRoot extends AbstractPlugin {
     /**
      * Default constructor
      */
     public FanIdentityPluginRoot() {
         super(new PluginVersionReference(new Version()));
     }
+    @NeededAddonReference(platform = Platforms.ART_PLATFORM, layer = Layers.WALLET_MODULE, addon = Addons.ERROR_MANAGER)
+    private ErrorManager errorManager;
 
-    @Override
-    public List<Fan> listIdentitiesFromCurrentDeviceUser() throws CantListFanIdentitiesException {
-        List<Fan> listIdentitiesFromCurrentDeviceUser = listIdentitiesFromCurrentDeviceUser();
-        return listIdentitiesFromCurrentDeviceUser;
-    }
 
-    @Override
-    public Fan createArtistIdentity(String alias, byte[] imageBytes) throws CantCreateFanIdentityException, FanIdentityAlreadyExistsException {
-        Fan fan = createArtistIdentity(alias,imageBytes);
-        return fan;
-    }
-
-    @Override
-    public void updateFanIdentity(String alias, String publicKey, byte[] imageProfile, String external) throws CantUpdateFanIdentityException {
-
-    }
-
-    @Override
-    public Fan getFanIdentity(String publicKey) throws CantGetFanIdentityException, IdentityNotFoundException {
-        Fan fan = getFanIdentity(publicKey);
-        return fan;
-    }
-
-    @Override
-    public void publishIdentity(String publicKey) throws CantPublishIdentityException, IdentityNotFoundException {
-
-    }
 }
