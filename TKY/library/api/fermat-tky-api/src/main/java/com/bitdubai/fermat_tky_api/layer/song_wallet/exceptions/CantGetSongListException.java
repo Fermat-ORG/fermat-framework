4a3e959b1901606adac5295eb28d04517c4f6b68
Manuel <darkpriestrelative@gmail.com>
2016-03-15 21:05:30 -0430

    Created CantGetSongListException

--- TKY/library/api/fermat-tky-api/src/main/java/com/bitdubai/fermat_tky_api/layer/song_wallet/exceptions/CantGetSongListException.java
+++ TKY/library/api/fermat-tky-api/src/main/java/com/bitdubai/fermat_tky_api/layer/song_wallet/exceptions/CantGetSongListException.java
@@ -1,15 +1,15 @@
-package com.bitdubai.fermat_tky_api.layer.external_api.exceptions;
+package com.bitdubai.fermat_tky_api.layer.song_wallet.exceptions;
 
 import com.bitdubai.fermat_tky_api.all_definitions.exceptions.TKYException;
 
 /**
- * Created by Manuel Perez (darkpriestrelative@gmail.com) on 11/03/16.
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 15/03/16.
  */
-public class CantGetBotException extends TKYException {
+public class CantGetSongListException extends TKYException {
 
-    public static final String DEFAULT_MESSAGE = "CANNOT GET SONG OBJECT FROM JSON RESPONSE";
+    public static final String DEFAULT_MESSAGE = "CANNOT GET SONG LIST";
 
-    public CantGetBotException(
+    public CantGetSongListException(
             final String message,
             final Exception cause,
             final String context,
@@ -17,29 +17,29 @@ public class CantGetBotException extends TKYException {
         super(message, cause, context, possibleReason);
     }
 
-    public CantGetBotException(
+    public CantGetSongListException(
             Exception cause,
             String context,
             String possibleReason) {
         super(DEFAULT_MESSAGE , cause, context, possibleReason);
     }
 
-    public CantGetBotException(
+    public CantGetSongListException(
             final String message,
             final Exception cause) {
         this(message, cause, "", "");
     }
 
-    public CantGetBotException(final String message) {
+    public CantGetSongListException(final String message) {
         this(message, null);
     }
 
-    public CantGetBotException(final Exception exception) {
+    public CantGetSongListException(final Exception exception) {
         this(exception.getMessage());
         setStackTrace(exception.getStackTrace());
     }
 
-    public CantGetBotException() {
+    public CantGetSongListException() {
         this(DEFAULT_MESSAGE);
     }
 }
