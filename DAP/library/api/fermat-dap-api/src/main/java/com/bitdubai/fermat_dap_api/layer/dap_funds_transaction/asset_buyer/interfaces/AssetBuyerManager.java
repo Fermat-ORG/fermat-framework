d48920df5169ba723dc7381acb4fb54de4033b31
Víctor A. Mars M <marsvicam@gmail.com>
2016-03-16 15:14:40 -0430

    REFACTOR: Move asset buyer from dap-api to its correct layer.

--- DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_funds_transaction/asset_buyer/interfaces/AssetBuyerManager.java
+++ DAP/library/api/fermat-dap-api/src/main/java/com/bitdubai/fermat_dap_api/layer/dap_funds_transaction/asset_buyer/interfaces/AssetBuyerManager.java
@@ -1,10 +1,9 @@
-package com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_buyer.interfaces;
+package com.bitdubai.fermat_dap_api.layer.dap_funds_transaction.asset_buyer.interfaces;
 
 import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.FermatManager;
 import com.bitdubai.fermat_api.layer.all_definition.enums.BlockchainNetworkType;
 import com.bitdubai.fermat_dap_api.layer.all_definition.digital_asset.AssetNegotiation;
-import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_buyer.exceptions.CantGetBuyingTransactionsException;
-import com.bitdubai.fermat_dap_api.layer.dap_transaction.asset_buyer.exceptions.CantProcessBuyingTransactionException;
+import com.bitdubai.fermat_dap_api.layer.dap_funds_transaction.asset_buyer.exceptions.CantGetBuyingTransactionsException;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,11 +15,11 @@ public interface AssetBuyerManager extends FermatManager {
     /**
      * @param negotiationId   the {@link UUID} that represents the negotiation that I am going to accept.
      * @param walletPublicKey the BTC wallet from where I'll get the bitcoins for buying that asset.
-     * @throws CantProcessBuyingTransactionException
+     * @throws com.bitdubai.fermat_dap_api.layer.dap_funds_transaction.asset_buyer.exceptions.CantProcessBuyingTransactionException
      */
-    void acceptAsset(UUID negotiationId, String walletPublicKey) throws CantProcessBuyingTransactionException;
+    void acceptAsset(UUID negotiationId, String walletPublicKey) throws com.bitdubai.fermat_dap_api.layer.dap_funds_transaction.asset_buyer.exceptions.CantProcessBuyingTransactionException;
 
-    void declineAsset(UUID negotiationId) throws CantProcessBuyingTransactionException;
+    void declineAsset(UUID negotiationId) throws com.bitdubai.fermat_dap_api.layer.dap_funds_transaction.asset_buyer.exceptions.CantProcessBuyingTransactionException;
 
     List<AssetNegotiation> getNewNegotiations(BlockchainNetworkType networkType) throws CantGetBuyingTransactionsException;
 }
