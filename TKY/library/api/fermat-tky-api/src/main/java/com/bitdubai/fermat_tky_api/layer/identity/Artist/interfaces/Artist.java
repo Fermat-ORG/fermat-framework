d1b714cab078cb9fde21c8a4baf86dc160ca201f
alexanderejm <alex_jimenez76@hotmail.com>
2016-03-16 22:08:03 -0430

    Erased fan and artist identities interfaces tha where not used

--- TKY/library/api/fermat-tky-api/src/main/java/com/bitdubai/fermat_tky_api/layer/identity/Artist/interfaces/Artist.java
+++
@@ -1,7 +0,0 @@
-package com.bitdubai.fermat_tky_api.layer.identity.Artist.interfaces;
-
-/**
- * Created by alexander on 3/15/16.
- */
-public interface Artist {
-}
--- TKY/library/api/fermat-tky-api/src/main/java/com/bitdubai/fermat_tky_api/layer/identity/Fan/interfaces/Fan.java
+++
@@ -1,9 +0,0 @@
-
-package com.bitdubai.fermat_tky_api.layer.identity.Fan.interfaces;
-
-
-/**
- * Created by alexander on 3/15/16.
- */
-public interface Fan {
-}
