33556e390c6a1eddbd3101c8362af7d25bf23038
alexanderejm <alex_jimenez76@hotmail.com>
2016-03-16 21:49:56 -0430

    Updates to SubAppModuleArtistIdentity

--- TKY/plugin/sub_app_module/fermat-tky-plugin-sub-app-module-artist-identity-bitdubai/src/main/java/com/bitdubai/fermat_tky_plugin/layer/sub_app_module/artist_identity/developer/bitdubai/version_1/ArtistIdentityPluginRoot.java
+++ TKY/plugin/sub_app_module/fermat-tky-plugin-sub-app-module-artist-identity-bitdubai/src/main/java/com/bitdubai/fermat_tky_plugin/layer/sub_app_module/artist_identity/developer/bitdubai/version_1/ArtistIdentityPluginRoot.java
@@ -1,17 +1,57 @@
 package com.bitdubai.fermat_tky_plugin.layer.sub_app_module.artist_identity.developer.bitdubai.version_1;
 
+import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
+import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
+import com.bitdubai.fermat_tky_plugin.layer.sub_app_module.artist_identity.developer.bitdubai.version_1.structure.ArtistIdentityManager;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 14/03/16.
  */
 public class ArtistIdentityPluginRoot extends AbstractPlugin {
+
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM, layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER)
+    private ErrorManager errorManager;
+
+    ArtistIdentityManager artistIdentityManager;
     /**
      * Default constructor
      */
     public ArtistIdentityPluginRoot() {
         super(new PluginVersionReference(new Version()));
+    }
+    private void initPluginManager(){
+        this.artistIdentityManager = new ArtistIdentityManager();
+    }
+
+    public void start() throws CantStartPluginException {
+        try{
+            initPluginManager();
+        } catch (Exception e) {
+            this.errorManager.reportUnexpectedPluginException(
+                    Plugins.TOKENLY_ARTIST_SUB_APP_MODULE,
+                    UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN,
+                    e);
+            throw new CantStartPluginException(
+                    CantStartPluginException.DEFAULT_MESSAGE,
+                    e, "Cant start Sub App Artist Identity Module plugin.",
+                    null);
+        }
+        /**
+         * nothing left to do.
+         */
+        this.serviceStatus = ServiceStatus.STARTED;
+    }
+    public void stop(){
+        this.serviceStatus = ServiceStatus.STOPPED;
     }
 }
--- TKY/plugin/sub_app_module/fermat-tky-plugin-sub-app-module-artist-identity-bitdubai/src/main/java/com/bitdubai/fermat_tky_plugin/layer/sub_app_module/artist_identity/developer/bitdubai/version_1/structure/ArtistIdentityManager.java
+++ TKY/plugin/sub_app_module/fermat-tky-plugin-sub-app-module-artist-identity-bitdubai/src/main/java/com/bitdubai/fermat_tky_plugin/layer/sub_app_module/artist_identity/developer/bitdubai/version_1/structure/ArtistIdentityManager.java
@@ -1,35 +1,48 @@
 package com.bitdubai.fermat_tky_plugin.layer.sub_app_module.artist_identity.developer.bitdubai.version_1.structure;
 
-import com.bitdubai.fermat_tky_api.layer.identity.Artist.interfaces.Artist;
+import com.bitdubai.fermat_tky_api.all_definitions.enums.ArtistAcceptConnectionsType;
+import com.bitdubai.fermat_tky_api.all_definitions.enums.ExposureLevel;
+import com.bitdubai.fermat_tky_api.all_definitions.enums.ExternalPlatform;
+import com.bitdubai.fermat_tky_api.all_definitions.exceptions.CantHideIdentityException;
+import com.bitdubai.fermat_tky_api.all_definitions.exceptions.IdentityNotFoundException;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.exceptions.ArtistIdentityAlreadyExistsException;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.exceptions.CantCreateArtistIdentityException;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.exceptions.CantGetArtistIdentityException;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.exceptions.CantListArtistIdentitiesException;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.exceptions.CantUpdateArtistIdentityException;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.interfaces.Artist;
+import com.bitdubai.fermat_tky_api.layer.identity.artist.interfaces.TokenlyArtistIdentityManager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Alexander Jimenez (alex_jimenez76@hotmail.com) on 3/15/16.
  */
-public class ArtistIdentityManager implements com.bitdubai.fermat_tky_api.layer.identity.Artist.interfaces.ArtistIdentityManager{
+public class ArtistIdentityManager implements TokenlyArtistIdentityManager {
+
     @Override
-    public List<Artist> listIdentitiesFromCurrentDeviceUser() {
+    public List<Artist> listIdentitiesFromCurrentDeviceUser() throws CantListArtistIdentitiesException {
         return null;
     }
 
     @Override
-    public Artist createArtistIdentity(String alias, byte[] imageBytes) {
+    public Artist createArtistIdentity(String alias, byte[] profileImage, String externalUserName, String externalAccessToken, ExternalPlatform externalPlatform, ExposureLevel exposureLevel, ArtistAcceptConnectionsType artistAcceptConnectionsType) throws CantCreateArtistIdentityException, ArtistIdentityAlreadyExistsException {
         return null;
     }
 
     @Override
-    public Artist getArtistIdentity(String publicKey) {
+    public void updateArtistIdentity(String alias, UUID id, String publicKey, byte[] profileImage, String externalUserName, String externalAccessToken, ExternalPlatform externalPlatform, ExposureLevel exposureLevel, ArtistAcceptConnectionsType artistAcceptConnectionsType) throws CantUpdateArtistIdentityException {
+
+    }
+
+    @Override
+    public Artist getArtistIdentity(UUID publicKey) throws CantGetArtistIdentityException, IdentityNotFoundException {
         return null;
     }
 
     @Override
-    public void publishIdentity(String publicKey) {
-
-    }
-
-    @Override
-    public void hideIdentity(String publicKey) {
+    public void hideIdentity(String publicKey) throws CantHideIdentityException, IdentityNotFoundException {
 
     }
 }
