83b797cbbd018573506e1407f585dbf4aa1574bf
alexanderejm <alex_jimenez76@hotmail.com>
2016-03-19 00:38:35 -0430

    Added WalletModule package to fermat_tky_core and added reference to TKYPlatform

--- TKY/library/core/fermat-tky-core/src/main/java/com/bitdubai/fermat_tky_core/TKYPlatform.java
+++ TKY/library/core/fermat-tky-core/src/main/java/com/bitdubai/fermat_tky_core/TKYPlatform.java
@@ -9,6 +9,7 @@ import com.bitdubai.fermat_tky_core.layer.external_api.ExternalApiLayer;
 import com.bitdubai.fermat_tky_core.layer.identity.IdentityLayer;
 import com.bitdubai.fermat_tky_core.layer.song_wallet.SongWalletLayer;
 import com.bitdubai.fermat_tky_core.layer.sub_app_module.SubAppModuleLayer;
+import com.bitdubai.fermat_tky_core.layer.wallet_module.WalletModuleLayer;
 
 /**
  * This class has all the necessary business logic to start the WRD platform.
@@ -29,6 +30,7 @@ public class TKYPlatform extends AbstractPlatform {
             registerLayer(new IdentityLayer());
             registerLayer(new SongWalletLayer());
             registerLayer(new SubAppModuleLayer());
+            registerLayer(new WalletModuleLayer());
 
         } catch (CantRegisterLayerException e) {
 
