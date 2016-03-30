2bfa74b0e97bcb9cc647da267a6d7fa2819cb51f
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-30 11:26:59 -0430

    CHT - Identity - Chat Identity - Implementation - Plug-in Root #5567 10%

---
+++ CHT/plugin/identity/fermat-cht-plugin-identity-chat-bitdubai/src/main/java/com/bitdubai/fermat_cht_plugin/layer/identity/chat/developer/bitdubai/version_1/ChatIdentityPluginRoot.java
@@ -0,0 +1,14 @@
+package com.bitdubai.fermat_cht_plugin.layer.identity.chat.developer.bitdubai.version_1;
+
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+
+/**
+ * Created by franklin on 30/03/16.
+ */
+public class ChatIdentityPluginRoot extends AbstractPlugin {
+    public ChatIdentityPluginRoot() {
+        super(new PluginVersionReference(new Version()));
+    }
+}
