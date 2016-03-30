37e0dc5f4b2da6e1db66e6249e8c114d80fd5add
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-30 11:57:08 -0430

    CHT - Identity - Chat Identity - Implementation - Database - Dao Class #5569 10%

---
+++ CHT/plugin/identity/fermat-cht-plugin-identity-chat-bitdubai/src/main/java/com/bitdubai/fermat_cht_plugin/layer/identity/chat/developer/bitdubai/version_1/database/ChatIdentityDatabaseDao.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cht_plugin.layer.identity.chat.developer.bitdubai.version_1.database;
+
+/**
+ * Created by franklin on 30/03/16.
+ */
+public class ChatIdentityDatabaseDao {
+}
