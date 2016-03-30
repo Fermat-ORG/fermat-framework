89f10c64365529fbc44f365162c7f7fe44e0500a
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-29 21:05:32 -0430

    CHT - Identity - Chat Identity - Implementation - Public Interfaces - ChatIdentityManager #5576

---
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/identity/interfaces/ChatIdentityManager.java
@@ -0,0 +1,52 @@
+package com.bitdubai.fermat_cht_api.layer.identity.interfaces;
+
+import com.bitdubai.fermat_cht_api.layer.identity.exceptions.CantCreateNewChatIdentityException;
+import com.bitdubai.fermat_cht_api.layer.identity.exceptions.CantGetChatIdentityException;
+import com.bitdubai.fermat_cht_api.layer.identity.exceptions.CantListChatIdentityException;
+import com.bitdubai.fermat_cht_api.layer.identity.exceptions.CantUpdateChatIdentityException;
+
+import java.util.List;
+
+/**
+ * Created by franklin on 29/03/16.
+ */
+public interface ChatIdentityManager {
+    /**
+     * The method <code>getIdentityAssetUsersFromCurrentDeviceUser</code> will give us a list of all the intra wallet users associated to the actual Device User logged in
+     *
+     * @return the list of Chat users associated to the current logged in Device User.
+     * @throws CantListChatIdentityException if something goes wrong.
+     */
+    List<ChatIdentity> getIdentityAssetUsersFromCurrentDeviceUser() throws CantListChatIdentityException;
+
+    /**
+     * The method <code>getIdentityAssetIssuer</code> will give Identity Asset Issuer associated
+     *
+     * @return Identity Chat associated.
+     * @throws CantGetChatIdentityException if something goes wrong.
+     */
+    ChatIdentity getIdentityAssetUser() throws CantGetChatIdentityException;
+
+    /**
+     * The method <code>createNewIdentityChat</code> creates a new intra wallet user Identity for the logged in Device User and returns the
+     * associated public key
+     *
+     * @param alias        the alias that the user choose as intra user identity
+     * @param profileImage the profile image to identify this identity
+     * @return the intra user created
+     * @throws CantCreateNewChatIdentityException if something goes wrong.
+     */
+    ChatIdentity createNewIdentityChat(String alias,
+                                                 byte[] profileImage) throws CantCreateNewChatIdentityException;
+
+    /**
+     * The method <code>updateIdentityChat</code> change a identity information data
+     *
+     * @param identityPublicKey
+     * @param identityAlias
+     * @param profileImage
+     * @throws CantUpdateChatIdentityException
+     */
+    void updateIdentityChat(String identityPublicKey, String identityAlias, byte[] profileImage) throws CantUpdateChatIdentityException;
+
+}
