171921cbdfa074447f1c52c38e97614799696f77
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-03-30 12:42:20 -0430

    CHT - Identity - Chat Identity - Plug-In #5564

--- CHT/library/core/fermat-cht-core/src/main/java/com/bitdubai/fermat_cht_core/layer/identity/chat/ChatIdentityPluginSubsystem.java
+++ CHT/library/core/fermat-cht-core/src/main/java/com/bitdubai/fermat_cht_core/layer/identity/chat/ChatIdentityPluginSubsystem.java
@@ -2,7 +2,7 @@ package com.bitdubai.fermat_cht_core.layer.identity.chat;
 
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginReference;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
-import com.bitdubai.fermat_cht_plugin.layer.middleware.chat.developer.bitdubai.DeveloperBitDubai;
+import com.bitdubai.fermat_cht_plugin.layer.identity.chat.developer.bitdubai.DeveloperBitDubai;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.abstract_classes.AbstractPluginSubsystem;
 import com.bitdubai.fermat_core_api.layer.all_definition.system.exceptions.CantStartSubsystemException;
 
@@ -17,11 +17,11 @@ public class ChatIdentityPluginSubsystem extends AbstractPluginSubsystem {
 
     @Override
     public void start() throws CantStartSubsystemException {
-//        try {
-//            registerDeveloper(new DeveloperBitDubai());
-//        } catch (Exception e) {
-//            System.err.println("Exception: " + e.getMessage());
-//            throw new CantStartSubsystemException(e, null, null);
-//        }
+        try {
+            registerDeveloper(new DeveloperBitDubai());
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            throw new CantStartSubsystemException(e, null, null);
+        }
     }
 }
