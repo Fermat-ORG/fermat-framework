54cf675edebf7e9b37fa8bd99b5c39301b0fc15d
Manuel <darkpriestrelative@gmail.com>
2016-03-31 21:22:53 -0430

    Created ArtistConnectionRequestUpdatesEventHandler

--- ART/plugin/actor_connection/fermat-art-plugin-actor-connection-artist-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/actor_connection/artist/developer/bitdubai/version1/event_handler/ArtistConnectionRequestUpdatesEventHandler.java
+++ ART/plugin/actor_connection/fermat-art-plugin-actor-connection-artist-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/actor_connection/artist/developer/bitdubai/version1/event_handler/ArtistConnectionRequestUpdatesEventHandler.java
@@ -6,7 +6,7 @@ import com.bitdubai.fermat_api.layer.all_definition.events.exceptions.Unexpected
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEvent;
 import com.bitdubai.fermat_api.layer.all_definition.events.interfaces.FermatEventHandler;
 import com.bitdubai.fermat_art_api.all_definition.events.enums.EventType;
-import com.bitdubai.fermat_art_api.layer.actor_network_service.interfaces.artist.events.ArtistConnectionRequestNewsEvent;
+import com.bitdubai.fermat_art_api.layer.actor_network_service.interfaces.artist.events.ArtistConnectionRequestUpdatesEvent;
 import com.bitdubai.fermat_art_plugin.layer.actor_connection.artist.developer.bitdubai.version1.ArtistActorConnectionPluginRoot;
 import com.bitdubai.fermat_art_plugin.layer.actor_connection.artist.developer.bitdubai.version1.exceptions.ArtistActorConnectionNotStartedException;
 import com.bitdubai.fermat_art_plugin.layer.actor_connection.artist.developer.bitdubai.version1.structure.ActorConnectionEventsActions;
@@ -14,17 +14,17 @@ import com.bitdubai.fermat_art_plugin.layer.actor_connection.artist.developer.bi
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 31/03/16.
  */
-public class ArtistConnectionRequestNewsEventHandler implements FermatEventHandler {
+public class ArtistConnectionRequestUpdatesEventHandler implements FermatEventHandler {
 
     private final ActorConnectionEventsActions actorConnectionEventActions          ;
     private final ArtistActorConnectionPluginRoot artistActorConnectionPluginRoot;
 
     /**
-     * Constructor with parameters.
+     * Constructor with parameters
      * @param actorConnectionEventActions
      * @param artistActorConnectionPluginRoot
      */
-    public ArtistConnectionRequestNewsEventHandler(
+    public ArtistConnectionRequestUpdatesEventHandler(
             final ActorConnectionEventsActions actorConnectionEventActions,
             final ArtistActorConnectionPluginRoot artistActorConnectionPluginRoot) {
 
@@ -40,11 +40,12 @@ public class ArtistConnectionRequestNewsEventHandler implements FermatEventHandl
      */
     @Override
     public void handleEvent(FermatEvent fermatEvent) throws FermatException {
+
         if (this.artistActorConnectionPluginRoot.getStatus() == ServiceStatus.STARTED) {
-            if (fermatEvent instanceof ArtistConnectionRequestNewsEvent) {
-                actorConnectionEventActions.handleNewsEvent();
+            if (fermatEvent instanceof ArtistConnectionRequestUpdatesEvent) {
+                actorConnectionEventActions.handleArtistUpdateEvent();
             } else {
-                EventType eventExpected = EventType.ARTIST_CONNECTION_REQUEST_NEWS;
+                EventType eventExpected = EventType.ARTIST_CONNECTION_REQUEST_UPDATES;
                 String context = "Event received: " + fermatEvent.getEventType().toString() + " - " + fermatEvent.getEventType().getCode()+"\n"+
                         "Event expected: " + eventExpected.toString()              + " - " + eventExpected.getCode();
                 throw new UnexpectedEventException(context);
