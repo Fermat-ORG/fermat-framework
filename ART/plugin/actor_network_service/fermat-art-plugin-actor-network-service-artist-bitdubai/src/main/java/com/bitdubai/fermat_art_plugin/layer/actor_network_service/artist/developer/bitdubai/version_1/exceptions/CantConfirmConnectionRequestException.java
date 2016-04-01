a3bf42156894e63a6c1d385cf954b6e32202388f
Gabriel Araujo <gabe_512@hotmail.com>
2016-04-01 12:12:43 -0430

    #5191

---
+++ ART/plugin/actor_network_service/fermat-art-plugin-actor-network-service-artist-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/actor_network_service/artist/developer/bitdubai/version_1/exceptions/CantConfirmConnectionRequestException.java
@@ -0,0 +1,24 @@
+package com.bitdubai.fermat_art_plugin.layer.actor_network_service.artist.developer.bitdubai.version_1.exceptions;
+
+
+import com.bitdubai.fermat_art_api.all_definition.exceptions.ARTException;
+
+/**
+ * The exception <code>com.bitdubai.fermat_cbp_plugin.layer.actor_network_service.crypto_broker.developer.bitdubai.version_1.exceptions.CantConfirmConnectionRequestException</code>
+ * is thrown when there is an error trying to confirm the connection request.
+ * <p>
+ * Created by Gabriel Araujo. (31/03/2016)
+ */
+public class CantConfirmConnectionRequestException extends ARTException {
+
+    private static final String DEFAULT_MESSAGE = "CAN'T CONFIRM CONNECTION REQUEST EXCEPTION";
+
+    public CantConfirmConnectionRequestException(String message, Exception cause, String context, String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    public CantConfirmConnectionRequestException(Exception cause, String context, String possibleReason) {
+        this(DEFAULT_MESSAGE, cause, context, possibleReason);
+    }
+
+}
