27921a2149da96865a676f94ee10bde26160401d
Roberto Requena <rart3001@gmail.com>
2016-04-04 09:57:05 -0430

    Initials import and updates

---
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/channels/processors/nodes/ReceivedActorCatalogTransactionsProcessor.java
@@ -0,0 +1,46 @@
+/*
+ * @#ReceivedActorCatalogTransactionsProcessor.java - 2016
+ * Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+ * BITDUBAI/CONFIDENTIAL
+ */
+package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.channels.processors.nodes;
+
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.data.Package;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.enums.PackageType;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.channels.WebSocketChannelServerEndpoint;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.channels.processors.PackageProcessor;
+
+import org.jboss.logging.Logger;
+
+import javax.websocket.Session;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.channels.processors.nodes.ReceivedActorCatalogTransactionsProcessor</code>
+ * <p/>
+ * Created by Roberto Requena - (rart3001@gmail.com) on 04/04/16.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+public class ReceivedActorCatalogTransactionsProcessor extends PackageProcessor {
+
+    /**
+     * Represent the LOG
+     */
+    private final Logger LOG = Logger.getLogger(ReceivedActorCatalogTransactionsProcessor.class.getName());
+
+    /**
+     * Constructor with parameter
+     *
+     * @param channel
+     * */
+    public ReceivedActorCatalogTransactionsProcessor(WebSocketChannelServerEndpoint channel) {
+        super(channel, PackageType.RECEIVE_ACTOR_CATALOG_TRANSACTIONS_REQUEST);
+    }
+
+    @Override
+    public void processingPackage(Session session, Package packageReceived) {
+
+    }
+}
