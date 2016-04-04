bc5b3699ba8b767255ebaf88e4ced39c482f446f
Roberto Requena <rart3001@gmail.com>
2016-04-04 10:34:46 -0430

    Initials import and updates

---
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/channels/client/WebSocketClientNodeChannel.java
@@ -0,0 +1,21 @@
+/*
+ * @#WebSocketClientNodeChannel.java - 2016
+ * Copyright bitDubai.com., All rights reserved.
+ * You may not modify, use, reproduce or distribute this software.
+ * BITDUBAI/CONFIDENTIAL
+ */
+package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.channels.client;
+
+import javax.websocket.ClientEndpoint;
+
+/**
+ * The Class <code>com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.channels.WebSocketClientNodeChannel.WebSocketClientNodeChannel</code>
+ * is the client to communicate nodes by the node client channel<p/>
+ * Created by Roberto Requena - (rart3001@gmail.com) on 04/04/16.
+ *
+ * @version 1.0
+ * @since Java JDK 1.7
+ */
+@ClientEndpoint
+public class WebSocketClientNodeChannel {
+}
