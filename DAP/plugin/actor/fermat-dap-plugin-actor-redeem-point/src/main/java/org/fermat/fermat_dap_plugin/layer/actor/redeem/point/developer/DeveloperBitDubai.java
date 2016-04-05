e2860f26664cf568cfd04b2d1c71388d5c84ac58
Víctor A. Mars M <marsvicam@gmail.com>
2016-04-05 17:29:38 -0430

    REFACTOR: Remove bitdubai from actor asset redeem point plugin.
    Associated Issue: #5728

---
+++ DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point/src/main/java/org/fermat/fermat_dap_plugin/layer/actor/redeem/point/developer/DeveloperBitDubai.java
@@ -0,0 +1,54 @@
+package org.fermat.fermat_dap_plugin.layer.actor.redeem.point.developer;
+
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPluginDeveloper;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantRegisterVersionException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantStartPluginDeveloperException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginDeveloperReference;
+import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Developers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.TimeFrequency;
+import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
+import org.fermat.fermat_dap_plugin.layer.actor.redeem.point.developer.version_1.RedeemPointActorPluginRoot;
+
+/**
+ * Created by Nerio on 10/11/2015.
+ */
+public class DeveloperBitDubai extends AbstractPluginDeveloper implements PluginLicensor {
+
+    public DeveloperBitDubai() {
+        super(new PluginDeveloperReference(Developers.BITDUBAI));
+    }
+
+    @Override
+    public void start() throws CantStartPluginDeveloperException {
+
+        try {
+            registerVersion(new RedeemPointActorPluginRoot());
+        }
+        catch (CantRegisterVersionException e) {
+            throw new CantStartPluginDeveloperException(e,
+                    "Plugin Developer BitDubai",
+                    "Can't register Plugins version DAP Redeem Point");
+        }
+    }
+
+    @Override
+    public int getAmountToPay() {
+        return 100;
+    }
+
+    @Override
+    public CryptoCurrency getCryptoCurrency() {
+        return CryptoCurrency.BITCOIN;
+    }
+
+    @Override
+    public String getAddress() {
+        return "13gpMizSNvQCbJzAPyGCUnfUGqFD8ryzcv";
+    }
+
+    @Override
+    public TimeFrequency getTimePeriod() {
+        return TimeFrequency.MONTHLY;
+    }
+}
---
+++ DAP/plugin/actor/fermat-dap-plugin-actor-redeem-point/src/main/java/org/fermat/fermat_dap_plugin/layer/actor/redeem/point/developer/version_1/exceptions/CantGetRedeemPointCryptoAddressTableException.java
@@ -0,0 +1,30 @@
+package org.fermat.fermat_dap_plugin.layer.actor.redeem.point.developer.version_1.exceptions;
+
+import com.bitdubai.fermat_api.FermatException;
+
+/**
+ * Created by Penelope Quintero on 08/02/16.
+ */
+public class CantGetRedeemPointCryptoAddressTableException extends FermatException {
+    /**
+     * This is the constructor that every inherited FermatException must implement
+     *
+     * @param message        the short description of the why this exception happened, there is a public static constant called DEFAULT_MESSAGE that can be used here
+     * @param cause          the exception that triggered the throwing of the current exception, if there are no other exceptions to be declared here, the cause should be null
+     * @param context        a String that provides the values of the variables that could have affected the exception
+     * @param possibleReason an explicative reason of why we believe this exception was most likely thrown
+     */
+    public CantGetRedeemPointCryptoAddressTableException(String message, Exception cause, String context, String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    /**
+     *
+     * @param message
+     * @param context
+     * @param possibleReason
+     */
+    public CantGetRedeemPointCryptoAddressTableException(String message, String context, String possibleReason) {
+        super(message, null, context, possibleReason);
+    }
+}
