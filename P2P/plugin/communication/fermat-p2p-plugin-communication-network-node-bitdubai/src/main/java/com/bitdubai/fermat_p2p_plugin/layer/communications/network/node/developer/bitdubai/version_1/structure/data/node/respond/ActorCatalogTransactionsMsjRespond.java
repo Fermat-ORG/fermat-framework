5537959e8b853c9aac88f5affdb1c410545a6b68
Roberto Requena <rart3001@gmail.com>
2016-04-05 09:49:09 -0430

    Add values

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/data/node/respond/ActorCatalogTransactionsMsjRespond.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/data/node/respond/ActorCatalogTransactionsMsjRespond.java
@@ -7,6 +7,9 @@
 package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.data.node.respond;
 
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.data.client.respond.MsgRespond;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.entities.ActorsCatalogTransaction;
+
+import java.util.List;
 
 /**
  * The Class <code>com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.data.node.respond.ActorCatalogTransactionsMsjRespond</code>
@@ -19,12 +22,27 @@ import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.da
 public class ActorCatalogTransactionsMsjRespond extends MsgRespond {
 
     /**
+     * Represent the list of transactions
+     */
+    private List<ActorsCatalogTransaction> actorsCatalogTransactions;
+
+    /**
      * Constructor with parameters
      *
      * @param status
      * @param details
      */
-    public ActorCatalogTransactionsMsjRespond(STATUS status, String details) {
+    public ActorCatalogTransactionsMsjRespond(STATUS status, String details, List<ActorsCatalogTransaction> actorsCatalogTransactions) {
         super(status, details);
+        this.actorsCatalogTransactions = actorsCatalogTransactions;
+    }
+
+    /**
+     * Get the list of actors catalog transactions
+     *
+     * @return List<ActorsCatalogTransaction>
+     */
+    public List<ActorsCatalogTransaction> getActorsCatalogTransactions() {
+        return actorsCatalogTransactions;
     }
 }
--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/data/node/respond/NodeCatalogTransactionsMsjRequest.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/data/node/respond/NodeCatalogTransactionsMsjRequest.java
@@ -7,6 +7,9 @@
 package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.data.node.respond;
 
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.data.client.respond.MsgRespond;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.entities.NodesCatalogTransaction;
+
+import java.util.List;
 
 /**
  * The Class <code>com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.data.node.respond.NodeCatalogTransactionsMsjRequest</code>
@@ -19,12 +22,26 @@ import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.da
 public class NodeCatalogTransactionsMsjRequest extends MsgRespond {
 
     /**
+     * Represent the list of transactions
+     */
+    private List<NodesCatalogTransaction> nodesCatalogTransactions;
+
+    /**
      * Constructor with parameters
      *
      * @param status
      * @param details
      */
-    public NodeCatalogTransactionsMsjRequest(STATUS status, String details) {
+    public NodeCatalogTransactionsMsjRequest(STATUS status, String details, List<NodesCatalogTransaction> nodesCatalogTransactions) {
         super(status, details);
+        this.nodesCatalogTransactions = nodesCatalogTransactions;
+    }
+
+    /**
+     * Get the list of node catalog nodesCatalogTransactions
+     * @return nodesCatalogTransactions
+     */
+    public List<NodesCatalogTransaction> getNodesCatalogTransactions() {
+        return nodesCatalogTransactions;
     }
 }
