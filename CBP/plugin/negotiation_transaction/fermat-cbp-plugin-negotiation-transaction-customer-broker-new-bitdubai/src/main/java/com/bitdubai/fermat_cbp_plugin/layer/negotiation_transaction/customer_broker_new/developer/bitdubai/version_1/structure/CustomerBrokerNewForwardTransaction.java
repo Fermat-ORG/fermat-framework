abd37614bdc629f753b6c646b23a40d3dfa1afc3
Yordin Alayn <y.alayn@gmail.com>
2016-04-05 00:03:28 -0430

    CBP - Negotiation Transaction - Customer Broker New - Forward Transaction

--- CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-new-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_new/developer/bitdubai/version_1/structure/CustomerBrokerNewForwardTransaction.java
+++ CBP/plugin/negotiation_transaction/fermat-cbp-plugin-negotiation-transaction-customer-broker-new-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/negotiation_transaction/customer_broker_new/developer/bitdubai/version_1/structure/CustomerBrokerNewForwardTransaction.java
@@ -59,7 +59,8 @@ public class CustomerBrokerNewForwardTransaction {
                             "\n - numberSend: "+Integer.toString(getNumberSend(transactionSend, transactionId))+
                             "\n");
 
-                    if (!negotiationTransaction.getStatusTransaction().getCode().equals(NegotiationTransactionStatus.PENDING_SUBMIT.getCode())) {
+//                    if (!negotiationTransaction.getStatusTransaction().getCode().equals(NegotiationTransactionStatus.PENDING_SUBMIT.getCode())) {
+                    if (negotiationTransaction.getStatusTransaction().getCode().equals(NegotiationTransactionStatus.SENDING_NEGOTIATION.getCode())) {
 
                         System.out.print("\n**** X) MOCK NEGOTIATION TRANSACTION - CUSTOMER BROKER NEW - FORWARD TRANSACTION - pendingToConfirmtTransaction" + transactionId + " ****\n");
 
