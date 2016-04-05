94c5cdb224256022d75b86d65ab750d85d1ebc36
JDVilchez <josvilchezalmera@gmail.com>
2016-04-05 23:45:07 +0000

    Develop Chat V2. ActorConnection and ActorNetworkService. Interfaces and core.

---
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/actor_network_service/utils/ChatConnectionRequest.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_cht_api.layer.actor_network_service.utils;
+
+/**
+ * Created by José D. Vilchez A. (josvilchezalmera@gmail.com) on 05/04/16.
+ */
+public class ChatConnectionRequest {
+}
