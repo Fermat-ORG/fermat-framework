efcd114778c347f781e29d9c851df57ef03c43fd
JDVilchez <josvilchezalmera@gmail.com>
2016-04-05 23:44:52 +0000

    Develop Chat V2. ActorConnection and ActorNetworkService. Interfaces and core.

---
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/actor_network_service/utils/ChatConnectionInformation.java
@@ -0,0 +1,76 @@
+package com.bitdubai.fermat_cht_api.layer.actor_network_service.utils;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+/**
+ * Created by José D. Vilchez A. (josvilchezalmera@gmail.com) on 05/04/16.
+ */
+public class ChatConnectionInformation {
+
+    private final UUID   connectionId        ;
+    private final String senderPublicKey     ;
+    private final String senderAlias         ;
+    private final byte[] senderImage         ;
+    private final String destinationPublicKey;
+    private final long   sendingTime         ;
+
+    public ChatConnectionInformation(final UUID connectionId        ,
+                                             final String senderPublicKey     ,
+                                             final String senderAlias         ,
+                                             final byte[] senderImage         ,
+                                             final String destinationPublicKey,
+                                             final long   sendingTime         ) {
+
+        this.connectionId         = connectionId        ;
+        this.senderPublicKey      = senderPublicKey     ;
+        this.senderAlias          = senderAlias         ;
+        this.senderImage          = senderImage         ;
+        this.destinationPublicKey = destinationPublicKey;
+        this.sendingTime          = sendingTime         ;
+    }
+
+    /**
+     * @return a string representing the public key.
+     */
+    public final String getSenderPublicKey() {
+        return senderPublicKey;
+    }
+
+    /**
+     * @return an array of bytes with the image exposed by the Crypto Broker.
+     */
+    public final byte[] getSenderImage() {
+        return senderImage;
+    }
+
+    /**
+     * @return a string representing the destination public key.
+     */
+    public final String getDestinationPublicKey() {
+        return destinationPublicKey;
+    }
+
+    /**
+     * @return the time when the action was performed.
+     */
+    public final long getSendingTime() {
+        return sendingTime;
+    }
+
+    public UUID getConnectionId() {
+        return connectionId;
+    }
+
+    @Override
+    public String toString() {
+        return "CryptoBrokerConnectionInformation{" +
+                "connectionId=" + connectionId +
+                ", senderPublicKey='" + senderPublicKey + '\'' +
+                ", senderAlias='" + senderAlias + '\'' +
+                ", senderImage=" + Arrays.toString(senderImage) +
+                ", destinationPublicKey='" + destinationPublicKey + '\'' +
+                ", sendingTime=" + sendingTime +
+                '}';
+    }
+}
