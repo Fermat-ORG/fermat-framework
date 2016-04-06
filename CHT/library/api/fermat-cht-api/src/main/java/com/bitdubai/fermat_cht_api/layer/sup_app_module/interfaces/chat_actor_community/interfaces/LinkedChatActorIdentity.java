b7a1af33b785825d48b0dfdfc5660a141de586d1
Eleazar Oroño <eorono@gmail.com>
2016-04-06 16:27:26 -0430

    modifications

--- CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/sup_app_module/interfaces/chat_actor_community/interfaces/LinkedChatActorIdentity.java
+++ CHT/library/api/fermat-cht-api/src/main/java/com/bitdubai/fermat_cht_api/layer/sup_app_module/interfaces/chat_actor_community/interfaces/LinkedChatActorIdentity.java
@@ -9,28 +9,14 @@ public interface LinkedChatActorIdentity {
 
     UUID getConnectionId();
 
-    /**
-     * The method <code>getPublicKey</code> returns the public key of the represented crypto broker
-     * @return the public key of the crypto broker
-     */
+
     String getPublicKey();
 
-    /**
-     * The method <code>getAlias</code> returns the name of the represented crypto broker
-     *
-     * @return the name of the crypto broker
-     */
+
     String getAlias();
 
-    /**
-     * The method <code>getProfileImage</code> returns the profile image of the represented crypto broker
-     *
-     * @return the profile image
-     */
+
     byte[] getImage();
 
-    /**
-     * The method <code>listCryptoBrokerWallets</code> returns the list of the public crypto customer wallets
-     * @return
-     */
+   
 }
