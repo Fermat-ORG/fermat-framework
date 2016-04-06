efbf2588c5fcb00362867c8a501776218820b791
josebricenor <josebricenor@gmail.com>
2016-04-06 09:36:05 -0430

    REFACTOR: Remove bitdubai from sub app module redeem point community plugin. #5806

--- DAP/plugin/sub_app_module/fermat-dap-plugin-sub-app-module-redeem-point-community/src/main/java/org/fermat/fermat_dap_plugin/layer/sub_app_module/redeem/point/developer/bitdubai/DeveloperBitDubai.java
+++ DAP/plugin/sub_app_module/fermat-dap-plugin-sub-app-module-redeem-point-community/src/main/java/org/fermat/fermat_dap_plugin/layer/sub_app_module/redeem/point/developer/bitdubai/DeveloperBitDubai.java
@@ -1,7 +1,5 @@
-package com.bitdubai.fermat_dap_plugin.layer.sub_app_module.redeem.point.developer.bitdubai;
+package org.fermat.fermat_dap_plugin.layer.sub_app_module.redeem.point.developer.bitdubai;
 
-import com.bitdubai.fermat_api.Plugin;
-import com.bitdubai.fermat_api.PluginDeveloper;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPluginDeveloper;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantRegisterVersionException;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantStartPluginDeveloperException;
@@ -10,7 +8,7 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Developers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.TimeFrequency;
 import com.bitdubai.fermat_api.layer.all_definition.license.PluginLicensor;
-import com.bitdubai.fermat_dap_plugin.layer.sub_app_module.redeem.point.developer.bitdubai.version_1.RedeemPointCommunitySubAppModulePluginRoot;
+import org.fermat.fermat_dap_plugin.layer.sub_app_module.redeem.point.developer.bitdubai.version_1.RedeemPointCommunitySubAppModulePluginRoot;
 
 
 /**
