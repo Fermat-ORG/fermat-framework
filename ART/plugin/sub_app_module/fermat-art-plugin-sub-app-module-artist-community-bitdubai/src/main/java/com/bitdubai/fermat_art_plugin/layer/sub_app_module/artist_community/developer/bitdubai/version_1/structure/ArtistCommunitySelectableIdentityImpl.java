c054fd559b8a9abbce3d7d22c33cf34eb1125ace
alexanderejm <alex_jimenez76@hotmail.com>
2016-04-06 22:27:52 -0430

    #5534 implemented information, search and selectableIdentity in structure

---
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_community/developer/bitdubai/version_1/structure/ArtistCommunitySelectableIdentityImpl.java
@@ -0,0 +1,67 @@
+package com.bitdubai.fermat_art_plugin.layer.sub_app_module.artist_community.developer.bitdubai.version_1.structure;
+
+import com.bitdubai.fermat_api.layer.all_definition.enums.Actors;
+import com.bitdubai.fermat_art_api.layer.identity.artist.interfaces.Artist;
+import com.bitdubai.fermat_art_api.layer.identity.fan.interfaces.Fanatic;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.artist.interfaces.ArtistCommunitySelectableIdentity;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.fan.exceptions.CantSelectIdentityException;
+
+/**
+ * Created by Alexander Jimenez (alex_jimenez76@hotmail.com) on 4/6/16.
+ */
+public final class ArtistCommunitySelectableIdentityImpl implements ArtistCommunitySelectableIdentity {
+
+    private final String publicKey;
+    private final Actors actorType;
+    private final String alias;
+    private final byte[] image;
+
+    ArtistCommunitySelectableIdentityImpl(String publicKey, Actors actorType, String alias, byte[] image){
+        this.publicKey = publicKey;
+        this.actorType = actorType;
+        this.alias = alias;
+        this.image = image;
+    }
+
+    ArtistCommunitySelectableIdentityImpl(final Artist ArtistIdentity) {
+
+        this.alias     = ArtistIdentity.getAlias()       ;
+        this.publicKey = ArtistIdentity.getPublicKey()   ;
+        this.actorType = Actors.ART_ARTIST              ;
+        this.image     = ArtistIdentity.getProfileImage();
+    }
+
+    ArtistCommunitySelectableIdentityImpl(final Fanatic FanaticIdentity) {
+
+        this.alias     = FanaticIdentity.getAlias()       ;
+        this.publicKey = FanaticIdentity.getPublicKey()   ;
+        this.actorType = Actors.ART_FAN              ;
+        this.image     = FanaticIdentity.getProfileImage();
+    }
+
+
+    @Override
+    public void select() throws CantSelectIdentityException {
+
+    }
+
+    @Override
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    @Override
+    public Actors getActorType() {
+        return actorType;
+    }
+
+    @Override
+    public String getAlias() {
+        return alias;
+    }
+
+    @Override
+    public byte[] getImage() {
+        return image;
+    }
+}
---
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_community/developer/bitdubai/version_1/structure/FanaticCommunitySelectableIdentityImpl.java
@@ -0,0 +1,47 @@
+package com.bitdubai.fermat_art_plugin.layer.sub_app_module.artist_community.developer.bitdubai.version_1.structure;
+
+import com.bitdubai.fermat_api.layer.all_definition.enums.Actors;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.fan.exceptions.CantSelectIdentityException;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.fan.interfaces.FanCommunitySelectableIdentity;
+
+/**
+ * Created by Alexander Jimenez (alex_jimenez76@hotmail.com) on 4/6/16.
+ */
+public class FanaticCommunitySelectableIdentityImpl implements FanCommunitySelectableIdentity {
+
+    String publicKey;
+    Actors actorType;
+    String alias;
+    byte[] image;
+
+    FanaticCommunitySelectableIdentityImpl(String publicKey, Actors actorType, String alias, byte[] image) {
+        this.publicKey = publicKey;
+        this.actorType = actorType;
+        this.alias = alias;
+        this.image = image;
+    }
+    @Override
+    public void select() throws CantSelectIdentityException {
+
+    }
+
+    @Override
+    public String getPublicKey() {
+        return this.publicKey;
+    }
+
+    @Override
+    public Actors getActorType() {
+        return this.actorType;
+    }
+
+    @Override
+    public String getAlias() {
+        return this.alias;
+    }
+
+    @Override
+    public byte[] getImage() {
+        return this.image;
+    }
+}
