51be77c42cad60eb0ed6d6ba303dc4f34c3c4dee
Víctor A. Mars M <marsvicam@gmail.com>
2016-04-07 19:19:50 -0430

    REFACTOR: remove bitdubai from package nomenclature on asset issuer subapp module
    Associated Issue: #5728

--- DAP/plugin/sub_app_module/fermat-dap-plugin-sub-app-module-asset-issuer-community/src/main/java/org/fermat/fermat_dap_plugin/layer/sub_app_module/asset/issuer/developer/version_1/structure/AssetIssuerCommunitySupAppModuleManager.java
+++ DAP/plugin/sub_app_module/fermat-dap-plugin-sub-app-module-asset-issuer-community/src/main/java/org/fermat/fermat_dap_plugin/layer/sub_app_module/asset/issuer/developer/version_1/structure/AssetIssuerCommunitySupAppModuleManager.java
@@ -1,4 +1,4 @@
-package org.fermat.fermat_dap_plugin.layer.sub_app_module.asset.issuer.developer.bitdubai.version_1.structure;
+package org.fermat.fermat_dap_plugin.layer.sub_app_module.asset.issuer.developer.version_1.structure;
 
 import org.fermat.fermat_dap_api.layer.dap_actor.asset_issuer.exceptions.CantAssetIssuerActorNotFoundException;
 import org.fermat.fermat_dap_api.layer.dap_actor.asset_issuer.exceptions.CantGetAssetIssuerActorsException;
