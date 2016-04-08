4abd7af0643f97fe8b19281a4632244f0a06cb8b
alexanderejm <alex_jimenez76@hotmail.com>
2016-04-08 03:02:13 -0430

    #5871

--- TKY/plugin/sub_app_module/fermat-tky-plugin-sub-app-module-fan-identity-bitdubai/src/main/java/com/bitdubai/fermat_tky_plugin/layer/sub_app_module/fan_identity/developer/bitdubai/version_1/FanIdentityPluginRoot.java
+++ TKY/plugin/sub_app_module/fermat-tky-plugin-sub-app-module-fan-identity-bitdubai/src/main/java/com/bitdubai/fermat_tky_plugin/layer/sub_app_module/fan_identity/developer/bitdubai/version_1/FanIdentityPluginRoot.java
@@ -1,9 +1,11 @@
 package com.bitdubai.fermat_tky_plugin.layer.sub_app_module.fan_identity.developer.bitdubai.version_1;
 
 import com.bitdubai.fermat_api.CantStartPluginException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractModule;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededPluginReference;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantGetModuleManagerException;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
@@ -11,16 +13,18 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_api.layer.modules.common_classes.ActiveActorIdentityInformation;
 import com.bitdubai.fermat_api.layer.modules.interfaces.ModuleManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
 import com.bitdubai.fermat_tky_api.layer.identity.fan.interfaces.TokenlyFanIdentityManager;
+import com.bitdubai.fermat_tky_api.layer.sub_app_module.fan.interfaces.TokenlyFanPreferenceSettings;
 import com.bitdubai.fermat_tky_plugin.layer.sub_app_module.fan_identity.developer.bitdubai.version_1.structure.FanIdentityManager;
 
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 14/03/16.
  */
-public class FanIdentityPluginRoot extends AbstractPlugin {
+public class FanIdentityPluginRoot extends AbstractModule<TokenlyFanPreferenceSettings, ActiveActorIdentityInformation> {
     @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM, layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER)
     private ErrorManager errorManager;
 
@@ -35,14 +39,6 @@ public class FanIdentityPluginRoot extends AbstractPlugin {
         super(new PluginVersionReference(new Version()));
     }
 
-    /**
-     * This method is used by the fermat-core to get the plugin manager in execution time.
-     * @return
-     */
-    public ModuleManager getManager(){
-        return this.fanIdentityManager;
-    }
-
     private void initPluginManager(){
         this.fanIdentityManager = new FanIdentityManager(errorManager,tokenlyFanIdentityManager);
     }
@@ -50,7 +46,9 @@ public class FanIdentityPluginRoot extends AbstractPlugin {
     @Override
     public void start() throws CantStartPluginException {
         try{
+            System.out.println("############ TKY fan Identity Start");
             initPluginManager();
+            System.out.println("############ TKY fan Identity Finish");
         } catch (Exception e) {
             this.errorManager.reportUnexpectedPluginException(
                     Plugins.TOKENLY_FAN_SUB_APP_MODULE,
@@ -70,5 +68,12 @@ public class FanIdentityPluginRoot extends AbstractPlugin {
     @Override
     public void stop(){
         this.serviceStatus = ServiceStatus.STOPPED;
+    }
+
+    @Override
+    public ModuleManager<TokenlyFanPreferenceSettings, ActiveActorIdentityInformation> getModuleManager() throws CantGetModuleManagerException {
+        if(fanIdentityManager == null)
+            initPluginManager();
+        return fanIdentityManager;
     }
 }
