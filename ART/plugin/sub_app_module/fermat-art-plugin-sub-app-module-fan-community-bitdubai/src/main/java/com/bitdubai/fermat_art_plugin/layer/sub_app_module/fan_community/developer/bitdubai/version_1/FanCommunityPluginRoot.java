3843c5e9bca29609a173971ac39634b1ef52e72b
alexanderejm <alex_jimenez76@hotmail.com>
2016-04-11 01:10:07 -0430

    FIX: Fix Plugin Reference and implemented AbstractModule in FanCommunity module
    updated FanCommunityPluginRoot to fix PluginReference
    
    included in #5870 and #5518

--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-fan-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/fan_community/developer/bitdubai/version_1/FanCommunityPluginRoot.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-fan-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/fan_community/developer/bitdubai/version_1/FanCommunityPluginRoot.java
@@ -1,9 +1,10 @@
 package com.bitdubai.fermat_art_plugin.layer.sub_app_module.fan_community.developer.bitdubai.version_1;
 
 import com.bitdubai.fermat_api.CantStartPluginException;
-import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractModule;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededPluginReference;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.exceptions.CantGetModuleManagerException;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
@@ -11,12 +12,15 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_api.layer.modules.common_classes.ActiveActorIdentityInformation;
 import com.bitdubai.fermat_api.layer.modules.interfaces.ModuleManager;
 import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_art_api.layer.actor_connection.fan.interfaces.FanActorConnectionManager;
 import com.bitdubai.fermat_art_api.layer.actor_network_service.interfaces.fan.FanManager;
 import com.bitdubai.fermat_art_api.layer.identity.artist.interfaces.ArtistIdentityManager;
 import com.bitdubai.fermat_art_api.layer.identity.fan.interfaces.FanaticIdentityManager;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.artist.interfaces.ArtistCommunitySelectableIdentity;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.fan.settings.FanCommunitySettings;
 import com.bitdubai.fermat_art_plugin.layer.sub_app_module.fan_community.developer.bitdubai.version_1.structure.FanCommunityManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
@@ -24,7 +28,7 @@ import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfac
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 08/03/16.
  */
-public class FanCommunityPluginRoot extends AbstractPlugin {
+public class FanCommunityPluginRoot extends AbstractModule<FanCommunitySettings, ActiveActorIdentityInformation> {
 
     @NeededPluginReference(platform = Platforms.ART_PLATFORM,         layer = Layers.IDENTITY,              plugin = Plugins.ARTIST_IDENTITY     )
     private ArtistIdentityManager artistIdentityManager;
@@ -32,13 +36,12 @@ public class FanCommunityPluginRoot extends AbstractPlugin {
     @NeededPluginReference(platform = Platforms.ART_PLATFORM,         layer = Layers.ACTOR_CONNECTION,              plugin = Plugins.FAN_ACTOR_CONNECTION)
     private FanActorConnectionManager fanActorConnectionManager;
 
-    //this next lines will stay commented until the Fan Identity is completed
 
-    /*@NeededPluginReference(platform = Platforms.ART_PLATFORM,         layer = Layers.ACTOR_NETWORK_SERVICE, plugin = Plugins.ARTIST_IDENTITY   )
+    @NeededPluginReference(platform = Platforms.ART_PLATFORM,         layer = Layers.ACTOR_NETWORK_SERVICE, plugin = Plugins.FAN   )
     private FanManager fanNetworkServiceManager;
 
-    @NeededPluginReference(platform = Platforms.ART_PLATFORM,         layer = Layers.IDENTITY,              plugin = Plugins.ARTIST_IDENTITY   )
-    private FanaticIdentityManager fanaticIdentityManager;*/
+    @NeededPluginReference(platform = Platforms.ART_PLATFORM,         layer = Layers.IDENTITY,              plugin = Plugins.FANATIC_IDENTITY   )
+    private FanaticIdentityManager fanaticIdentityManager;
 
     @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM,     layer = Layers.PLATFORM_SERVICE,      addon  = Addons.ERROR_MANAGER)
     private ErrorManager errorManager;
@@ -55,9 +58,7 @@ public class FanCommunityPluginRoot extends AbstractPlugin {
     }
 
     private void initPluginManager(){
-        //this next lines will stay commented until the Fan Identity is completed
-
-        /*this.fanCommunityManager = new FanCommunityManager(
+        this.fanCommunityManager = new FanCommunityManager(
                 artistIdentityManager,
                 fanActorConnectionManager,
                 fanNetworkServiceManager,
@@ -65,16 +66,15 @@ public class FanCommunityPluginRoot extends AbstractPlugin {
                 errorManager,
                 pluginFileSystem,
                 pluginId,
-                this.getPluginVersionReference());*/
-    }
-    public ModuleManager getManager(){
-        return this.fanCommunityManager;
+                this.getPluginVersionReference());
     }
 
     @Override
     public void start() throws CantStartPluginException {
         try{
+            System.out.println("############ ART FAN Community Start");
             initPluginManager();
+            System.out.println("############ ART FAN Community Start");
         } catch (Exception e) {
             this.errorManager.reportUnexpectedPluginException(
                     Plugins.FAN_COMMUNITY_SUB_APP_MODULE,
@@ -89,5 +89,12 @@ public class FanCommunityPluginRoot extends AbstractPlugin {
          * nothing left to do.
          */
         this.serviceStatus = ServiceStatus.STARTED;
+    }
+
+    @Override
+    public ModuleManager<FanCommunitySettings, ActiveActorIdentityInformation> getModuleManager() throws CantGetModuleManagerException {
+        if(fanCommunityManager == null)
+            initPluginManager();
+        return fanCommunityManager;
     }
 }
