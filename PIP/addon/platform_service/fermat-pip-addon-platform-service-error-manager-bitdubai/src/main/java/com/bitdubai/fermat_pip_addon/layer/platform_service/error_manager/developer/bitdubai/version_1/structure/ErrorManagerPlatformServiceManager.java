61441e5dd2f5013c14b6940df48c95cd6aa99f84
Manuel <darkpriestrelative@gmail.com>
2016-05-01 11:06:56 -0400

    Refactor: Art platform enum code.

--- PIP/addon/platform_service/fermat-pip-addon-platform-service-error-manager-bitdubai/src/main/java/com/bitdubai/fermat_pip_addon/layer/platform_service/error_manager/developer/bitdubai/version_1/structure/ErrorManagerPlatformServiceManager.java
+++ PIP/addon/platform_service/fermat-pip-addon-platform-service-error-manager-bitdubai/src/main/java/com/bitdubai/fermat_pip_addon/layer/platform_service/error_manager/developer/bitdubai/version_1/structure/ErrorManagerPlatformServiceManager.java
@@ -128,7 +128,7 @@ public final class ErrorManagerPlatformServiceManager implements ErrorManager {
 
     private void sendErrorReport(Platforms platforms){
         switch (platforms){
-            case ART_PLATFORM:
+            case PLATFORM_ART:
 
                     break;
             case BLOCKCHAINS:
