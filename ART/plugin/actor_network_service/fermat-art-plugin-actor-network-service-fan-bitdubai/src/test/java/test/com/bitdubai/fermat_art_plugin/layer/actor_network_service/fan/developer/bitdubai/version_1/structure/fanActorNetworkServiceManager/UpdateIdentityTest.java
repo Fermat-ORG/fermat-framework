a00b794464c46bad8a64ea65a7e970577c24fc4f
Gianfranco Ciafardone <gciafardone.gmail.com>
2016-05-05 09:33:28 -0430

    Unit testing for ART - Actor Network Service - Fan
    Include in: #5760
    
    Signed-off-by: Giankor <gciafardone@gmail.com>

--- ART/plugin/actor_network_service/fermat-art-plugin-actor-network-service-fan-bitdubai/src/test/java/test/com/bitdubai/fermat_art_plugin/layer/actor_network_service/fan/developer/bitdubai/version_1/structure/fanActorNetworkServiceManager/UpdateIdentityTest.java
+++ ART/plugin/actor_network_service/fermat-art-plugin-actor-network-service-fan-bitdubai/src/test/java/test/com/bitdubai/fermat_art_plugin/layer/actor_network_service/fan/developer/bitdubai/version_1/structure/fanActorNetworkServiceManager/UpdateIdentityTest.java
@@ -4,31 +4,29 @@ import com.bitdubai.fermat_art_api.layer.actor_network_service.exceptions.CantEx
 import com.bitdubai.fermat_art_api.layer.actor_network_service.interfaces.fan.util.FanExposingData;
 import com.bitdubai.fermat_art_plugin.layer.actor_network_service.fan.developer.bitdubai.version_1.structure.FanActorNetworkServiceManager;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.doCallRealMethod;
 
 /**
- * Created by gianco on 27/04/16.
+ * Created by gianco on 04/05/16.
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(FanActorNetworkServiceManager.class)
 public class UpdateIdentityTest {
 
+    final FanExposingData actor = null;
+
     @Test
-    public void updateIdentityTest () throws CantExposeIdentityException {
+    public void updateIdentityTest() throws CantExposeIdentityException {
 
         FanActorNetworkServiceManager fanActorNetworkServiceManager = PowerMockito.mock(FanActorNetworkServiceManager.class);
-        FanExposingData actor = new FanExposingData("public key","gianco",new byte[0]);
 
-        doCallRealMethod().when(fanActorNetworkServiceManager).updateIdentity(actor);
+        doCallRealMethod().when(fanActorNetworkServiceManager).exposeIdentity(actor);
+
     }
 }
