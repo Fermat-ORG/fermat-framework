80312841ab76e4b117697acc1798f4e10a35a34c
Angel Veloz <vlzangel91@gmail.com>
2016-05-06 22:32:00 -0430

    adding method getPlatformsSupported - CryptoCustomerWalletModuleManager

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/wallet_module/crypto_customer/interfaces/CryptoCustomerWalletModuleManager.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/wallet_module/crypto_customer/interfaces/CryptoCustomerWalletModuleManager.java
@@ -2,6 +2,7 @@ package com.bitdubai.fermat_cbp_api.layer.wallet_module.crypto_customer.interfac
 
 import com.bitdubai.fermat_api.layer.actor_connection.common.exceptions.CantListActorConnectionsException;
 import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.settings.exceptions.CantGetSettingsException;
 import com.bitdubai.fermat_api.layer.all_definition.settings.exceptions.CantPersistSettingsException;
 import com.bitdubai.fermat_api.layer.all_definition.settings.exceptions.SettingsNotFoundException;
@@ -137,6 +138,13 @@ public interface CryptoCustomerWalletModuleManager extends CBPWalletsModuleManag
      */
     Collection<BrokerIdentityBusinessInfo> getListOfConnectedBrokersAndTheirMerchandises()
             throws CantGetCryptoBrokerListException, CantGetListActorExtraDataException;
+
+    /**
+     *
+     * @param paymentCurrency
+     * @return list of platforms supporteds
+     */
+    Collection<Platforms> getPlatformsSupported(String customerPublicKey, String brokerPublicKey, String paymentCurrency) throws CantGetListActorExtraDataException;
 
     /**
      * Verify if thew wallet is configured or not
