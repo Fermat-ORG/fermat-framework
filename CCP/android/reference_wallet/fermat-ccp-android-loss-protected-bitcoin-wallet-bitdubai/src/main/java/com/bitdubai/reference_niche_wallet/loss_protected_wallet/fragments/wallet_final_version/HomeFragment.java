39476f1b6d75531b8f5b44223d83bfdb3ee6ea8a
gianelo <gianbarboza11@gmail.com>
2016-05-10 08:52:27 -0400

    CCP- Loss Protectd Wallet - Create Home Fragment #5661

--- CCP/android/reference_wallet/fermat-ccp-android-loss-protected-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/loss_protected_wallet/fragments/wallet_final_version/HomeFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-loss-protected-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/loss_protected_wallet/fragments/wallet_final_version/HomeFragment.java
@@ -428,7 +428,7 @@ public class HomeFragment extends AbstractFermatFragment<LossProtectedWalletSess
 
                 txt_earnOrLost.setText("USD "+
                         WalletUtils.formatAmountStringWithDecimalEntry(
-                            totalEarnedAndLostForToday,
+                            totalEarnedAndLostForToday*-1,
                             EARN_AND_LOST_MAX_DECIMAL_FORMAT,
                             EARN_AND_LOST_MIN_DECIMAL_FORMAT)+" lost");
 
