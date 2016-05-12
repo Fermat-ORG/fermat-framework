35be1ae9a66f31c7db67995dfa1c79201b8f535f
Angel Veloz <vlzangel91@gmail.com>
2016-05-12 17:25:58 -0430

    Changes ErrorManager - Sub App Module Broker Community - CryptoBrokerCommunitySubAppModulePluginRoot

--- CBP/plugin/sub_app_module/fermat-cbp-plugin-sub-app-module-crypto-broker-community-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/sub_app_module/crypto_broker_community/developer/bitdubai/version_1/CryptoBrokerCommunitySubAppModulePluginRoot.java
+++ CBP/plugin/sub_app_module/fermat-cbp-plugin-sub-app-module-crypto-broker-community-bitdubai/src/main/java/com/bitdubai/fermat_cbp_plugin/layer/sub_app_module/crypto_broker_community/developer/bitdubai/version_1/CryptoBrokerCommunitySubAppModulePluginRoot.java
@@ -32,9 +32,6 @@ import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.Err
 @PluginInfo(createdBy = "lnacosta", maintainerMail = "laion.cj91@gmail.com", platform = Platforms.CRYPTO_BROKER_PLATFORM, layer = Layers.SUB_APP_MODULE, plugin = Plugins.CRYPTO_BROKER_COMMUNITY)
 public class CryptoBrokerCommunitySubAppModulePluginRoot extends AbstractModule<CryptoBrokerCommunitySettings, CryptoBrokerCommunitySelectableIdentity> {
 
-    @NeededAddonReference (platform = Platforms.PLUG_INS_PLATFORM     , layer = Layers.PLATFORM_SERVICE     , addon  = Addons .ERROR_MANAGER     )
-    private ErrorManager errorManager;
-
     @NeededAddonReference (platform = Platforms.OPERATIVE_SYSTEM_API  , layer = Layers.SYSTEM               , addon  = Addons .PLUGIN_FILE_SYSTEM)
     private PluginFileSystem pluginFileSystem;
 
@@ -68,10 +65,9 @@ public class CryptoBrokerCommunitySubAppModulePluginRoot extends AbstractModule<
                     cryptoBrokerActorConnectionManager,
                     cryptoBrokerNetworkServiceManager,
                     cryptoCustomerIdentityManager,
-                    errorManager,
+                    this,
                     pluginFileSystem,
-                    pluginId,
-                    this.getPluginVersionReference());
+                    pluginId);
 
             this.serviceStatus = ServiceStatus.STARTED;
 
