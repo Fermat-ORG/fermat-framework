ff425a7f3add189526e77fd04dc02c944a0ad57a
Jose Cardozo <josejcb89@gmail.com>
2016-05-12 23:03:38 -0400

    REFACTOR: improvements in chat backgrounds design #5935

--- CHT/android/sub_app/fermat-cht-android-sub-app-chat-community-bitdubai/src/main/java/com/bitdubai/sub_app/chat_community/fragments/ConnectionsWorldFragment.java
+++ CHT/android/sub_app/fermat-cht-android-sub-app-chat-community-bitdubai/src/main/java/com/bitdubai/sub_app/chat_community/fragments/ConnectionsWorldFragment.java
@@ -309,11 +309,11 @@ public class ConnectionsWorldFragment
             emptyView.setAnimation(anim);
             emptyView.setVisibility(View.VISIBLE);
             noData.setAnimation(anim);
-            emptyView.setBackgroundResource(R.drawable.cht_comm_background_empty_screen);
+            emptyView.setBackgroundResource(R.drawable.cht_comm_background);
             noDatalabel.setAnimation(anim);
             noData.setVisibility(View.VISIBLE);
             noDatalabel.setVisibility(View.VISIBLE);
-            rootView.setBackgroundResource(R.drawable.cht_comm_background_empty_screen);
+            rootView.setBackgroundResource(R.drawable.cht_comm_background);
             if (adapter != null)
                 adapter.changeDataSet(null);
         } else if (!show /*&& emptyView.getVisibility() == View.VISIBLE*/) {
