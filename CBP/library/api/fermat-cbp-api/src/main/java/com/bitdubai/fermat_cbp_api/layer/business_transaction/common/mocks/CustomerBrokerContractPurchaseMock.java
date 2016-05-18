8b84ee67aff2c3f67ace904df7d4972d4300b057
Nelson Ramirez <nelsonalfo@gmail.com>
2016-05-18 15:09:13 -0400

    More Serializable Classes and interfaces

--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/common/mocks/CustomerBrokerContractPurchaseMock.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/common/mocks/CustomerBrokerContractPurchaseMock.java
@@ -4,12 +4,13 @@ import com.bitdubai.fermat_cbp_api.all_definition.contract.ContractClause;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.ContractStatus;
 import com.bitdubai.fermat_cbp_api.layer.contract.customer_broker_purchase.interfaces.CustomerBrokerContractPurchase;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 /** This mock is only for testing
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 04/01/16.
  */
-public class CustomerBrokerContractPurchaseMock implements CustomerBrokerContractPurchase {
+public class CustomerBrokerContractPurchaseMock implements CustomerBrokerContractPurchase, Serializable {
     @Override
     public String getContractId() {
         return "888052D7D718420BD197B647F3BB04128C9B71BC99DBB7BC60E78BDAC4DFC6E2";
--- CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/contract/customer_broker_purchase/interfaces/CustomerBrokerContractPurchase.java
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/contract/customer_broker_purchase/interfaces/CustomerBrokerContractPurchase.java
@@ -2,11 +2,14 @@ package com.bitdubai.fermat_cbp_api.layer.contract.customer_broker_purchase.inte
 
 import com.bitdubai.fermat_cbp_api.all_definition.contract.Contract;
 
+import java.io.Serializable;
+
+
 /**
  * Created by Angel on 2015.09.16..
  */
 
-public interface CustomerBrokerContractPurchase extends Contract{
+public interface CustomerBrokerContractPurchase extends Contract, Serializable{
 
 
 
