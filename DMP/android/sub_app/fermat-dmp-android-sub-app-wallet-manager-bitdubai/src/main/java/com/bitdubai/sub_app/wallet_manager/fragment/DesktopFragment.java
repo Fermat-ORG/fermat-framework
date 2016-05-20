82b7429fc08068ac4f730d7437489b4574418e4b
gianelo <gianbarboza11@gmail.com>
2016-05-20 00:18:21 -0400

    Create Android Fermat Wallet #6214

--- DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-manager-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_manager/fragment/DesktopFragment.java
+++ DMP/android/sub_app/fermat-dmp-android-sub-app-wallet-manager-bitdubai/src/main/java/com/bitdubai/sub_app/wallet_manager/fragment/DesktopFragment.java
@@ -337,7 +337,7 @@ public class DesktopFragment extends AbstractDesktopFragment<DesktopSession,SubA
 
                     }
 
-                if(installedWallet.getWalletPublicKey().equals(WalletsPublicKeys.CWP_LOSS_PROTECTED_WALLET.getCode())) {
+                if(installedWallet.getWalletPublicKey().equals(WalletsPublicKeys.CCP_LOSS_PROTECTED_WALLET.getCode())) {
                     Item item = new Item(installedWallet);
                     item.setIconResource(R.drawable.icon_loss_protected);
                     item.setPosition(8);
