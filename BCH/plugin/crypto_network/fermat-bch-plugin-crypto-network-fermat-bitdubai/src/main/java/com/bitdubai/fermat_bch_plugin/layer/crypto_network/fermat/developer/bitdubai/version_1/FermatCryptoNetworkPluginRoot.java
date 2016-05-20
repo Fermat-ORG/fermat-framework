31b07a3881d97e86192e8e921aefa41fc7a31282
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-05-20 20:14:04 -0300

    added new Plugin Fermat Crypto Network to BCH platform

--- BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-fermat-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/fermat/developer/bitdubai/version_1/FermatCryptoNetworkPluginRoot.java
+++ BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-fermat-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/fermat/developer/bitdubai/version_1/FermatCryptoNetworkPluginRoot.java
@@ -1,8 +1,17 @@
 package com.bitdubai.fermat_bch_plugin.layer.crypto_network.fermat.developer.bitdubai.version_1;
 
+import com.bitdubai.fermat_api.CantStartPluginException;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
 import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_bch_plugin.layer.crypto_network.fermat.developer.bitdubai.version_1.structure.FermatNetworkParameters;
+
+import org.bitcoinj.core.AbstractPeerEventListener;
+import org.bitcoinj.core.Context;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Peer;
+import org.bitcoinj.core.PeerGroup;
+import org.bitcoinj.utils.ContextPropagatingThreadFactory;
 
 /**
  * Created by rodrigo on 5/20/16.
@@ -11,5 +20,10 @@ public class FermatCryptoNetworkPluginRoot extends AbstractPlugin {
 
     public FermatCryptoNetworkPluginRoot() {
         super(new PluginVersionReference(new Version()));
+    }
+
+    @Override
+    public void start() throws CantStartPluginException {
+        System.out.println("****FermatNetwork*** started");
     }
 }
