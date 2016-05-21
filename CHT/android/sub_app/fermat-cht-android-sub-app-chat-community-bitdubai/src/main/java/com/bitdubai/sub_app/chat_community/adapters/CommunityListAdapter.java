d2f20642545f888c2eb6609f930da6a613bec83d
Jose Cardozo <josejcb89@gmail.com>
2016-05-21 12:32:02 -0400

    REFACTOR: Community and identity adequations for api 17 less than 22

--- CHT/android/sub_app/fermat-cht-android-sub-app-chat-community-bitdubai/src/main/java/com/bitdubai/sub_app/chat_community/adapters/CommunityListAdapter.java
+++ CHT/android/sub_app/fermat-cht-android-sub-app-chat-community-bitdubai/src/main/java/com/bitdubai/sub_app/chat_community/adapters/CommunityListAdapter.java
@@ -57,9 +57,9 @@ public class CommunityListAdapter extends FermatAdapter<ChatActorCommunityInform
         if(connectionState != null) {
             switch (connectionState) {
                 case CONNECTED:
-                        holder.connectedButton.setVisibility(View.VISIBLE);
-                        holder.blockedButton.setVisibility(View.GONE);
-                        holder.pendingButton.setVisibility(View.GONE);
+                    holder.connectedButton.setVisibility(View.VISIBLE);
+                    holder.blockedButton.setVisibility(View.GONE);
+                    holder.pendingButton.setVisibility(View.GONE);
                     break;
                 case BLOCKED_LOCALLY:
                     holder.connectedButton.setVisibility(View.GONE);
