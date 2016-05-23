f338a14ff161444580c4325629011be02a4c9f9f
alejandro <abicelis@gmail.com>
2016-05-23 16:08:13 -0400

    CER - FermatExchange - PluginRoot implementations, current exchange rate

---
+++ CER/plugin/provider/fermat-cer-plugin-provider-fermatexchange-bitdubai/src/main/java/com/bitdubai/fermat_cer_plugin/layer/provider/fermatexchange/developer/bitdubai/version_1/ProviderFermatExchangePluginRoot.java
@@ -0,0 +1,384 @@
+package com.bitdubai.fermat_cer_plugin.layer.provider.fermatexchange.developer.bitdubai.version_1;
+
+import com.bitdubai.fermat_api.CantStartPluginException;
+import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.annotations.NeededAddonReference;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.utils.PluginVersionReference;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DatabaseManagerForDevelopers;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabase;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTable;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperDatabaseTableRecord;
+import com.bitdubai.fermat_api.layer.all_definition.developer.DeveloperObjectFactory;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Addons;
+import com.bitdubai.fermat_api.layer.all_definition.enums.CryptoCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.enums.FiatCurrency;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
+import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
+import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_api.layer.osa_android.database_system.PluginDatabaseSystem;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
+import com.bitdubai.fermat_api.layer.world.interfaces.Currency;
+import com.bitdubai.fermat_cer_api.all_definition.enums.ExchangeRateType;
+import com.bitdubai.fermat_cer_api.all_definition.interfaces.CurrencyPair;
+import com.bitdubai.fermat_cer_api.all_definition.interfaces.ExchangeRate;
+import com.bitdubai.fermat_cer_api.all_definition.utils.CurrencyPairImpl;
+import com.bitdubai.fermat_cer_api.all_definition.utils.ExchangeRateImpl;
+import com.bitdubai.fermat_cer_api.layer.provider.exceptions.CantGetExchangeRateException;
+import com.bitdubai.fermat_cer_api.layer.provider.exceptions.CantGetProviderInfoException;
+import com.bitdubai.fermat_cer_api.layer.provider.exceptions.CantSaveExchangeRateException;
+import com.bitdubai.fermat_cer_api.layer.provider.exceptions.UnsupportedCurrencyPairException;
+import com.bitdubai.fermat_cer_api.layer.provider.interfaces.CurrencyExchangeRateProviderManager;
+import com.bitdubai.fermat_cer_api.layer.provider.utils.CurrencyPairHelper;
+import com.bitdubai.fermat_cer_api.layer.provider.utils.DateHelper;
+import com.bitdubai.fermat_cer_api.layer.provider.utils.HttpReader;
+import com.bitdubai.fermat_cer_plugin.layer.provider.fermatexchange.developer.bitdubai.version_1.database.FermatExchangeProviderDao;
+import com.bitdubai.fermat_cer_plugin.layer.provider.fermatexchange.developer.bitdubai.version_1.database.FermatExchangeProviderDeveloperDatabaseFactory;
+import com.bitdubai.fermat_cer_plugin.layer.provider.fermatexchange.developer.bitdubai.version_1.exceptions.CantInitializeFermatExchangeProviderDatabaseException;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
+import com.bitdubai.fermat_pip_api.layer.platform_service.event_manager.interfaces.EventManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Alejandro Bicelis on 23/05/2016.
+ */
+public class ProviderFermatExchangePluginRoot extends AbstractPlugin implements DatabaseManagerForDevelopers, CurrencyExchangeRateProviderManager {
+
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_DATABASE_SYSTEM)
+    private PluginDatabaseSystem pluginDatabaseSystem;
+
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_FILE_SYSTEM)
+    private PluginFileSystem pluginFileSystem;
+
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM, layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER)
+    private ErrorManager errorManager;
+
+    @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM, layer = Layers.PLATFORM_SERVICE, addon = Addons.EVENT_MANAGER)
+    private EventManager eventManager;
+
+    private FermatExchangeProviderDao dao;
+    private List<CurrencyPair> supportedCurrencyPairs = new ArrayList<>();
+
+
+    /*
+     * PluginRoot Constructor
+     */
+    public ProviderFermatExchangePluginRoot() {
+        super(new PluginVersionReference(new Version()));
+    }
+
+
+    /*
+     * Service interface implementation
+     */
+    @Override
+    public void start() throws CantStartPluginException {
+        System.out.println("PROVIDERFERMATEXCHANGE - PluginRoot START");
+
+        supportedCurrencyPairs.add(new CurrencyPairImpl(CryptoCurrency.FERMAT, FiatCurrency.VENEZUELAN_BOLIVAR));
+        supportedCurrencyPairs.add(new CurrencyPairImpl(CryptoCurrency.FERMAT, FiatCurrency.US_DOLLAR));
+        supportedCurrencyPairs.add(new CurrencyPairImpl(CryptoCurrency.FERMAT, CryptoCurrency.BITCOIN));
+
+        supportedCurrencyPairs.add(new CurrencyPairImpl(FiatCurrency.VENEZUELAN_BOLIVAR, CryptoCurrency.FERMAT));
+        supportedCurrencyPairs.add(new CurrencyPairImpl(FiatCurrency.US_DOLLAR, CryptoCurrency.FERMAT));
+        supportedCurrencyPairs.add(new CurrencyPairImpl(CryptoCurrency.BITCOIN, CryptoCurrency.BITCOIN));
+
+
+        try {
+            dao = new FermatExchangeProviderDao(pluginDatabaseSystem, pluginId, errorManager);
+            dao.initialize();
+            dao.initializeProvider("FermatExchange");
+        } catch (Exception e) {
+            errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+            throw new CantStartPluginException(CantStartPluginException.DEFAULT_MESSAGE, FermatException.wrapException(e), null, null);
+        }
+        serviceStatus = ServiceStatus.STARTED;
+    }
+
+
+
+
+    /*
+     * CurrencyExchangeRateProviderManager interface implementation
+     */
+
+    @Override
+    public String getProviderName() throws CantGetProviderInfoException {
+        return dao.getProviderName();
+    }
+
+    @Override
+    public UUID getProviderId() throws CantGetProviderInfoException {
+        return dao.getProviderId();
+    }
+
+    @Override
+    public Collection<CurrencyPair> getSupportedCurrencyPairs() {
+        return supportedCurrencyPairs;
+    }
+
+    @Override
+    public boolean isCurrencyPairSupported(CurrencyPair currencyPair) throws IllegalArgumentException {
+        for (CurrencyPair cp : supportedCurrencyPairs) {
+            if (cp.equals(currencyPair))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public ExchangeRate getCurrentExchangeRate(CurrencyPair currencyPair) throws UnsupportedCurrencyPairException, CantGetExchangeRateException {
+
+        if(!isCurrencyPairSupported(currencyPair))
+            throw new UnsupportedCurrencyPairException();
+
+
+        String url, params;
+        JSONObject json;
+        double purchase, sale = 0;
+
+        params = currencyPair.getFrom().getCode() + "_" + currencyPair.getTo().getCode();
+        url = "http://api.fermatexchange.com/ticker/?pairs=" + params;
+
+        try{
+
+            //json =  new JSONObject(HttpReader.getHTTPContent(url));
+
+            json = new JSONObject("{\"FER_VEF\":{\"timestamp\":\"1463844159\",\"purchase\":\"123.45\",\"sale\":\"456.78\"}}");
+            purchase = json.getJSONObject(params).getDouble("purchase");
+            sale = json.getJSONObject(params).getDouble("sale");
+
+        }catch (JSONException e) {
+            errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN, e);
+            throw new CantGetExchangeRateException(CantGetExchangeRateException.DEFAULT_MESSAGE, e, "FermatExchange CER Provider", "Cant Get exchange rate for " + currencyPair.getFrom().getCode() +  "-" + currencyPair.getTo().getCode());
+        }
+
+
+        ExchangeRateImpl exchangeRate = new ExchangeRateImpl(currencyPair.getFrom(), currencyPair.getTo(), sale, purchase, (new Date().getTime() / 1000));
+
+        try {
+            dao.saveCurrentExchangeRate(exchangeRate);
+        }catch (CantSaveExchangeRateException e) {
+            errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, e);
+        }
+        return exchangeRate;
+    }
+
+    @Override
+    public ExchangeRate getExchangeRateFromDate(CurrencyPair currencyPair, Calendar calendar) throws UnsupportedCurrencyPairException, CantGetExchangeRateException {
+
+        long timestamp = calendar.getTimeInMillis() / 1000L;
+
+        if(DateHelper.timestampIsInTheFuture(timestamp))
+            throw new CantGetExchangeRateException(CantGetExchangeRateException.DEFAULT_MESSAGE, "Provided timestamp is in the future");
+
+        if(!isCurrencyPairSupported(currencyPair))
+            throw new UnsupportedCurrencyPairException();
+
+        ExchangeRate requiredExchangeRate = null;
+
+        //Try to find ExchangeRate in database
+        try{
+            requiredExchangeRate = dao.getDailyExchangeRateFromDate(currencyPair, DateHelper.getStandarizedTimestampFromTimestamp(timestamp));
+            return requiredExchangeRate;
+        }catch(CantGetExchangeRateException e) {
+
+            //IF ExchangeRate not in database
+
+            //Determine cryptoCurrency base
+            Currency currencyFrom, currencyTo;
+            boolean invertExchange;
+
+            if (CryptoCurrency.codeExists(currencyPair.getFrom().getCode())) {
+                currencyFrom = currencyPair.getFrom();
+                currencyTo = currencyPair.getTo();
+                invertExchange = true;
+            } else {
+                currencyFrom = currencyPair.getTo();
+                currencyTo = currencyPair.getFrom();
+                invertExchange = false;
+            }
+
+            //Query API
+            List<ExchangeRate> exchangeRates = new ArrayList<>();
+            List<ExchangeRate> inverseExchangeRates = new ArrayList<>();
+            queryFermatExchangeExchangeRateHistoryAPI(exchangeRates, inverseExchangeRates, currencyFrom, currencyTo);
+
+            //Find requiredExchangeRate
+            long stdTimestamp = DateHelper.getStandarizedTimestampFromTimestamp(timestamp);
+            List<ExchangeRate> aux = (invertExchange ? exchangeRates : inverseExchangeRates);
+            for(ExchangeRate er : aux) {
+                if (er.getTimestamp() == stdTimestamp)
+                    requiredExchangeRate = er;
+            }
+
+            //Update database
+            try {
+                dao.updateDailyExchangeRateTable(new CurrencyPairImpl(currencyFrom, currencyTo), exchangeRates);
+                dao.updateDailyExchangeRateTable(new CurrencyPairImpl(currencyTo, currencyFrom), inverseExchangeRates);
+            } catch (CantSaveExchangeRateException eex) {
+                errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, eex);
+            }
+        }
+        return requiredExchangeRate;
+    }
+
+    @Override
+    public Collection<ExchangeRate> getDailyExchangeRatesForPeriod(CurrencyPair currencyPair, Calendar startCalendar, Calendar endCalendar) throws UnsupportedCurrencyPairException, CantGetExchangeRateException {
+
+        long startTimestamp = startCalendar.getTimeInMillis() / 1000L;
+        long endTimestamp = endCalendar.getTimeInMillis() / 1000L;
+
+
+        if(DateHelper.timestampIsInTheFuture(startTimestamp))
+            throw new CantGetExchangeRateException(CantGetExchangeRateException.DEFAULT_MESSAGE, "Provided startTimestamp is in the future");
+
+        if(!isCurrencyPairSupported(currencyPair))
+            throw new UnsupportedCurrencyPairException();
+
+        long stdStartTimestamp = DateHelper.getStandarizedTimestampFromTimestamp(startTimestamp);
+        long stdEndTimestamp = DateHelper.getStandarizedTimestampFromTimestamp(endTimestamp);
+        List<ExchangeRate> requiredExchangeRates = new ArrayList<>();
+        int requiredNumberOfDays = DateHelper.calculateDaysBetweenTimestamps(startTimestamp, endTimestamp);
+
+        //Try to find ExchangeRates in database
+        try{
+            requiredExchangeRates = dao.getDailyExchangeRatesForPeriod(currencyPair, stdStartTimestamp, stdEndTimestamp);
+            if(requiredExchangeRates.size() == requiredNumberOfDays)
+                return requiredExchangeRates;
+        }catch(CantGetExchangeRateException e) {/*Cant get them, continue*/}
+
+        //IF ExchangeRate not in database
+
+        //Determine cryptoCurrency base
+        Currency currencyFrom, currencyTo;
+        boolean invertExchange;
+
+        if (CryptoCurrency.codeExists(currencyPair.getFrom().getCode())) {
+            currencyFrom = currencyPair.getFrom();
+            currencyTo = currencyPair.getTo();
+            invertExchange = true;
+        } else {
+            currencyFrom = currencyPair.getTo();
+            currencyTo = currencyPair.getFrom();
+            invertExchange = false;
+        }
+
+        //Query API
+        List<ExchangeRate> exchangeRates = new ArrayList<>();
+        List<ExchangeRate> inverseExchangeRates = new ArrayList<>();
+        queryFermatExchangeExchangeRateHistoryAPI(exchangeRates, inverseExchangeRates, currencyFrom, currencyTo);
+
+        //Find requiredExchangeRate
+        List<ExchangeRate> aux = (invertExchange ? inverseExchangeRates : exchangeRates);
+        for(ExchangeRate er : aux) {
+            if (er.getTimestamp() >= stdStartTimestamp && er.getTimestamp() <= stdEndTimestamp)
+                requiredExchangeRates.add(er);
+        }
+
+        //Update database
+        try {
+            dao.updateDailyExchangeRateTable(new CurrencyPairImpl(currencyFrom, currencyTo), exchangeRates);
+            dao.updateDailyExchangeRateTable(new CurrencyPairImpl(currencyTo, currencyFrom), inverseExchangeRates);
+        } catch (CantSaveExchangeRateException eex) {
+            errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, eex);
+        }
+
+        return requiredExchangeRates;
+    }
+
+
+
+    @Override
+    public Collection<ExchangeRate> getQueriedExchangeRates(CurrencyPair currencyPair) throws UnsupportedCurrencyPairException, CantGetExchangeRateException {
+        if(!isCurrencyPairSupported(currencyPair))
+            throw new UnsupportedCurrencyPairException();
+
+        return dao.getQueriedExchangeRateHistory(ExchangeRateType.CURRENT, currencyPair);
+    }
+
+
+
+
+
+
+
+    /* INTERNAL FUNCTIONS */
+    private void queryFermatExchangeExchangeRateHistoryAPI(List<ExchangeRate> exchangeRates, List<ExchangeRate> inverseExchangeRates,
+                                                             Currency currencyFrom, Currency currencyTo) {
+
+        JSONObject json;
+
+        try {
+            json = new JSONObject(HttpReader.getHTTPContent("http://api.fermatexchange.com/historical/?pair=" + currencyFrom.getCode() + currencyTo.getCode()));
+            json = json.getJSONObject(currencyTo.getCode() + "_" + currencyFrom.getCode());
+
+            Iterator<?> keys = json.keys();
+            while( keys.hasNext() ) {
+                String key = (String)keys.next();
+                double value = Double.parseDouble(json.get(key).toString());
+
+                try{
+                    long time = DateHelper.getTimestampFromDateString(key);
+                    exchangeRates.add(new ExchangeRateImpl(currencyFrom, currencyTo, value, value, time));
+                    inverseExchangeRates.add(new ExchangeRateImpl(currencyTo, currencyFrom, 1/value, 1/value, time));      //Add inverse as well
+                } catch(ParseException ex) {}
+            }
+        } catch (JSONException ex) {
+            errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.NOT_IMPORTANT, ex);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+     * DatabaseManagerForDevelopers interface implementation
+     */
+    @Override
+    public List<DeveloperDatabase> getDatabaseList(DeveloperObjectFactory developerObjectFactory) {
+        FermatExchangeProviderDeveloperDatabaseFactory factory = new FermatExchangeProviderDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        return factory.getDatabaseList(developerObjectFactory);
+    }
+
+    @Override
+    public List<DeveloperDatabaseTable> getDatabaseTableList(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase) {
+        FermatExchangeProviderDeveloperDatabaseFactory factory = new FermatExchangeProviderDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        return factory.getDatabaseTableList(developerObjectFactory);
+    }
+
+    @Override
+    public List<DeveloperDatabaseTableRecord> getDatabaseTableContent(DeveloperObjectFactory developerObjectFactory, DeveloperDatabase developerDatabase, DeveloperDatabaseTable developerDatabaseTable) {
+        FermatExchangeProviderDeveloperDatabaseFactory factory = new FermatExchangeProviderDeveloperDatabaseFactory(pluginDatabaseSystem, pluginId);
+        List<DeveloperDatabaseTableRecord> tableRecordList = null;
+        try {
+            factory.initializeDatabase();
+            tableRecordList = factory.getDatabaseTableContent(developerObjectFactory, developerDatabaseTable);
+        } catch (CantInitializeFermatExchangeProviderDatabaseException e) {
+            errorManager.reportUnexpectedPluginException(Plugins.FERMATEXCHANGE, UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN, e);
+        }
+        return tableRecordList;
+    }
+}
