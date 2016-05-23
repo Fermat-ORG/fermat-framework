657735387f6be5693d5e908c654acdc4e20f2df5
Exilum <jc.juaco@gmail.com>
2016-05-23 17:13:07 -0430

    Adding new module for the new wallet called Fermat Wallet #6208

---
+++ CCP/plugin/wallet_module/fermat-ccp-plugin-wallet-module-fermat-wallet-bitdubai/src/test/java/unit/com/bitdubai/fermat_dmp_plugin/layer/wallet_module/crypto_wallet/developer/bitdubai/DeveloperBitDubai/PluginDeveloperInterfaceImplementationTest.java
@@ -0,0 +1,30 @@
+/*
+package unit.com.bitdubai.fermat_dmp_plugin.layer.wallet_module.crypto_wallet.developer.bitdubai.DeveloperBitDubai;
+
+import com.bitdubai.fermat_api.Plugin;
+import com.bitdubai.fermat_ccp_plugin.layer.wallet_module.crypto_wallet.developer.bitdubai.DeveloperBitDubai;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PluginDeveloperInterfaceImplementationTest extends TestCase {
+
+    DeveloperBitDubai developerBitDubai;
+
+    @Before
+    public void setUp() throws Exception {
+        developerBitDubai = new DeveloperBitDubai();
+    }
+
+    @Test
+    public void testGetPlugin_NotNull() throws Exception {
+        Plugin plugin = developerBitDubai.getFirstPluginVersion();
+        assertNotNull(plugin);
+    }
+}
+*/
