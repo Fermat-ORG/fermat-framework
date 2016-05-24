4bf7346822476b6e96a9dbfabdc25b8373492735
Lozadaa <richardalozada@hotmail.com>
2016-05-24 02:06:35 -0430

    REFACTOR: validation getContactStatus on chat contact details fragment

--- CHT/android/sub_app/fermat-cht-android-sub-app-chat-bitdubai/src/main/java/com/bitbudai/fermat_cht_android_sub_app_chat_bitdubai/fragments/ContactFragment.java
+++ CHT/android/sub_app/fermat-cht-android-sub-app-chat-bitdubai/src/main/java/com/bitbudai/fermat_cht_android_sub_app_chat_bitdubai/fragments/ContactFragment.java
@@ -116,7 +116,9 @@ public class ContactFragment extends AbstractFermatFragment {
             Contact con= chatSession.getSelectedContact();
             contactname.add(con.getAlias());
             contactid.add(con.getRemoteActorPublicKey());
-            contactalias.add(con.getContactStatus().toString());
+            if(con.getContactStatus() != null) {
+                contactalias.add(con.getContactStatus().toString());
+            }
             ByteArrayInputStream bytes = new ByteArrayInputStream(con.getProfileImage());
             BitmapDrawable bmd = new BitmapDrawable(bytes);
             contacticon.add(bmd.getBitmap());
