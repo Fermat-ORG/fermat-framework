4a937b0d89e9c7006b3e28a3e319cefa4b2e3b41
mati <mati_fur@hotmail.com>
2016-06-02 17:41:57 -0300

    ComboApps part 1

--- fermat-android-api/src/main/java/com/bitdubai/fermat_android_api/layer/definition/wallet/interfaces/FermatSession.java
+++ fermat-android-api/src/main/java/com/bitdubai/fermat_android_api/layer/definition/wallet/interfaces/FermatSession.java
@@ -1,62 +1,7 @@
 package com.bitdubai.fermat_android_api.layer.definition.wallet.interfaces;
 
-import com.bitdubai.fermat_api.layer.all_definition.runtime.FermatApp;
-import com.bitdubai.fermat_api.layer.modules.interfaces.ModuleManager;
-import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.ErrorManager;
-
-import java.util.List;
-
 /**
- * Created by Matias Furszyfer on 2015.10.18..
+ * Created by mati on 2016.06.02..
  */
-public interface FermatSession<A extends FermatApp,M extends ModuleManager>{
-
-    /**
-     *
-     * @param key
-     * @param object
-     */
-    void setData(String key, Object object);
-
-    /**
-     *
-     * @param key
-     * @return
-     */
-    Object getData(String key);
-
-    /**
-     *
-     */
-    void removeData(String key);
-
-    /**
-     *
-     * @return
-     */
-    ErrorManager getErrorManager();
-
-
-    String getAppPublicKey();
-
-    List<FermatApp> getPosibleConnections();
-
-    /**
-     * Devuelve el tipo de la app
-     *
-     * @return
-     */
-    A getFermatApp();
-
-
-    /**
-     * Devuelve el module manager
-     */
-    M getModuleManager();
-
-    /**
-     *  Devuelve la identidad activa
-     */
-    //I getIdentity();
-
+public interface FermatSession {
 }
---
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/navigation_structure/enums/AppStructureType.java
@@ -0,0 +1,7 @@
+package com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums;
+
+/**
+ * Created by mati on 2016.06.02..
+ */
+public enum AppStructureType {
+}
