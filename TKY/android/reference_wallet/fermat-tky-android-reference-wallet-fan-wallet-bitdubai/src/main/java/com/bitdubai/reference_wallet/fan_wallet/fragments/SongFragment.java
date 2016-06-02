7b8977798cd9219c56b885e6728badcc6449af71
lostwood <miguel_payarez@hotmail.com>
2016-06-01 20:48:07 -0400

    Feat:Remove the header of fan wallet in TKY platform

--- TKY/android/reference_wallet/fermat-tky-android-reference-wallet-fan-wallet-bitdubai/src/main/java/com/bitdubai/reference_wallet/fan_wallet/fragments/SongFragment.java
+++ TKY/android/reference_wallet/fermat-tky-android-reference-wallet-fan-wallet-bitdubai/src/main/java/com/bitdubai/reference_wallet/fan_wallet/fragments/SongFragment.java
@@ -72,8 +72,8 @@ public class SongFragment extends AbstractFermatFragment  {
 
     RecyclerView recyclerView;
     SwipeRefreshLayout swipeContainer;
-    ImageView headerImage;
-    TextView headerText;
+ //   ImageView headerImage;
+ //   TextView headerText;
     ImageView backgroundImage;
     String TAG="SONGFRAGMENT";
     View view;
@@ -160,8 +160,8 @@ public class SongFragment extends AbstractFermatFragment  {
     }
 
     void initValues(){
-        headerImage.setPadding(0,0,0,0);
-        headerImage.setImageResource(R.drawable.tky_background_no_songs_found);
+    //    headerImage.setPadding(0,0,0,0);
+    //    headerImage.setImageResource(R.drawable.tky_background_no_songs_found);
         compareViewAndDatabase();
         syncTokenlyAndUpdateThreads(true);
 
@@ -252,8 +252,8 @@ public class SongFragment extends AbstractFermatFragment  {
             swipeContainer.setColorSchemeColors(Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW);
             recyclerView = (RecyclerView) view.findViewById(R.id.rv);
 
-            headerImage=(ImageView)getToolbarHeader().getRootView().findViewById(R.id.tky_header_image);
-            headerText=(TextView)getToolbarHeader().getRootView().findViewById(R.id.tky_header_TextView);
+    //        headerImage=(ImageView)getToolbarHeader().getRootView().findViewById(R.id.tky_header_image);
+   //         headerText=(TextView)getToolbarHeader().getRootView().findViewById(R.id.tky_header_TextView);
             lManager = new LinearLayoutManager(view.getContext());
             recyclerView.setLayoutManager(lManager);
 
@@ -422,9 +422,9 @@ public class SongFragment extends AbstractFermatFragment  {
                 adapter.setFilter(items.get(i),true,i);
             }
         }
-        headerImage.setPadding(0,140,0,0);
-        headerImage.setImageBitmap(items.get(position).getImagen());
-        headerText.setText(items.get(position).getDescription());
+    //    headerImage.setPadding(0,140,0,0);
+    //    headerImage.setImageBitmap(items.get(position).getImagen());
+    //    headerText.setText(items.get(position).getDescription());
         items.get(position).setItemSelected(true);
         adapter.setFilter(items.get(position),true,position);
     }
@@ -518,8 +518,8 @@ public class SongFragment extends AbstractFermatFragment  {
                     String description=songOfBroadcast.getCopyright() + "\n" +
                             songOfBroadcast.getCredits()+"\n" +
                             songOfBroadcast.getReleaseDate();
-                    headerImage.setImageBitmap(albumArt);
-                    headerText.setText(description);
+          //          headerImage.setImageBitmap(albumArt);
+          //          headerText.setText(description);
 
                     items.add(new SongItems(albumArt, songOfBroadcast.getName(), songOfBroadcast.getComposers(), SongStatus.DOWNLOADING.getFriendlyName(), song_Id, 0, false,description,false));
                     adapter.setFilter(new SongItems(albumArt,
@@ -574,8 +574,8 @@ public class SongFragment extends AbstractFermatFragment  {
                      String description=   walletitems.getCopyright() + "\n" +
                              walletitems.getCredits()+"\n" +
                              walletitems.getReleaseDate();
-                    headerImage.setImageBitmap(albumArt);
-                        headerText.setText(description);
+          //          headerImage.setImageBitmap(albumArt);
+          //          headerText.setText(description);
 
                     items.add(new SongItems(albumArt, walletitems.getName(), walletitems.getComposers(), walletitems.getSongStatus().getFriendlyName(), walletitems.getSongId(), 0, false,description,false));
                     adapter.setFilter(new SongItems(albumArt,
