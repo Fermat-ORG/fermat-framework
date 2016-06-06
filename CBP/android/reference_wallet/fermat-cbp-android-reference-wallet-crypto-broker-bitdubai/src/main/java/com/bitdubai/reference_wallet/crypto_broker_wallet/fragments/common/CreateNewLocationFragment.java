85eb532829f5058292739f8438b9f2c22e07780b
Yordin Alayn <y.alayn@gmail.com>
2016-06-06 13:33:47 -0400

    CBP - Reference Wallet - Crypto Broker - Location Form

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/CreateNewLocationFragment.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-broker-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_broker_wallet/fragments/common/CreateNewLocationFragment.java
@@ -3,6 +3,9 @@ package com.bitdubai.reference_wallet.crypto_broker_wallet.fragments.common;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,7 @@ import android.widget.Toast;
 
 import com.bitdubai.fermat_android_api.layer.definition.wallet.AbstractFermatFragment;
 import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatEditText;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Country;
 import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Activities;
 import com.bitdubai.fermat_api.layer.all_definition.navigation_structure.enums.Wallets;
@@ -34,6 +38,9 @@ public class CreateNewLocationFragment extends AbstractFermatFragment<CryptoBrok
     // Data
     private Country[] countries;
     private Country selectedCountry;
+    private static int MAX_LENGHT_STATE = 30;
+    private static int MAX_LENGHT_CITY = 30;
+    private static int MAX_LENGHT_ADDRESS = 100;
 
     // UI
     private FermatEditText cityTextView;
@@ -41,10 +48,31 @@ public class CreateNewLocationFragment extends AbstractFermatFragment<CryptoBrok
     private FermatEditText zipCodeEditText;
     private FermatEditText addressLineOneEditText;
     private FermatEditText addressLineTwoEditText;
+    FermatTextView cityTextCount, stateTextCount, address1TextCount, address2TextCount;
 
     private CryptoBrokerWalletModuleManager walletManager;
     private ErrorManager errorManager;
 
+    private final TextWatcher cityTextWatcher = new TextWatcher() {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {cityTextCount.setText(String.valueOf(MAX_LENGHT_CITY - s.length()));}
+        public void afterTextChanged(Editable s) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    };
+    private final TextWatcher stateTextWatcher = new TextWatcher() {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {stateTextCount.setText(String.valueOf(MAX_LENGHT_STATE - s.length()));}
+        public void afterTextChanged(Editable s) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    };
+    private final TextWatcher address1TextWatcher = new TextWatcher() {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {address1TextCount.setText(String.valueOf(MAX_LENGHT_ADDRESS - s.length()));}
+        public void afterTextChanged(Editable s) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    };
+    private final TextWatcher address2TextWatcher = new TextWatcher() {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {address2TextCount.setText(String.valueOf(MAX_LENGHT_ADDRESS - s.length()));}
+        public void afterTextChanged(Editable s) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    };
 
     public static CreateNewLocationFragment newInstance() {
         return new CreateNewLocationFragment();
@@ -71,6 +99,26 @@ public class CreateNewLocationFragment extends AbstractFermatFragment<CryptoBrok
         zipCodeEditText = (FermatEditText) layout.findViewById(R.id.cbw_zip_code_edit_text);
         addressLineOneEditText = (FermatEditText) layout.findViewById(R.id.cbw_address_line_1_edit_text);
         addressLineTwoEditText = (FermatEditText) layout.findViewById(R.id.cbw_address_line_2_edit_text);
+        stateTextCount = (FermatTextView) layout.findViewById(R.id.cbw_state_edit_text_count);
+        cityTextCount = (FermatTextView) layout.findViewById(R.id.cbw_city_edit_text_count);
+        address1TextCount = (FermatTextView) layout.findViewById(R.id.cbw_address_line_1_edit_text_count);
+        address2TextCount = (FermatTextView) layout.findViewById(R.id.cbw_address_line_2_edit_text_count);
+
+        //Limit max length, add textWatchers
+        cityTextView.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_LENGHT_CITY)});
+        stateEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_LENGHT_STATE)});
+        addressLineOneEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_LENGHT_ADDRESS)});
+        addressLineTwoEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_LENGHT_ADDRESS)});
+
+        cityTextView.addTextChangedListener(cityTextWatcher);
+        stateEditText.addTextChangedListener(stateTextWatcher);
+        addressLineOneEditText.addTextChangedListener(address1TextWatcher);
+        addressLineTwoEditText.addTextChangedListener(address2TextWatcher);
+
+        cityTextCount.setText(String.valueOf(MAX_LENGHT_CITY));
+        stateTextCount.setText(String.valueOf(MAX_LENGHT_STATE));
+        address1TextCount.setText(String.valueOf(MAX_LENGHT_ADDRESS));
+        address2TextCount.setText(String.valueOf(MAX_LENGHT_ADDRESS));
 
         layout.findViewById(R.id.cbw_create_new_location_button).setOnClickListener(this);
 
