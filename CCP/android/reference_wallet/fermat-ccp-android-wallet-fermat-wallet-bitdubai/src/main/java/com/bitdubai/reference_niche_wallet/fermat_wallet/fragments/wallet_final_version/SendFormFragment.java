6cdee3212ac0d6dfd9dfd16820d022e28db882c1
gianelo <gianbarboza11@gmail.com>
2016-06-17 01:28:59 -0400

    #6285 Minor Changes

--- CCP/android/reference_wallet/fermat-ccp-android-wallet-fermat-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/fermat_wallet/fragments/wallet_final_version/SendFormFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-wallet-fermat-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/fermat_wallet/fragments/wallet_final_version/SendFormFragment.java
@@ -269,7 +269,7 @@ public class SendFormFragment extends AbstractFermatFragment<ReferenceAppFermatS
                 String newAmount = "";
                 if(bitcoinConverter != null) {
                     switch (position) {
-                        case 0:
+                        case 1:
                             text = "[btc]";
                             if (txtType.equals("[bits]")) {
                                 newAmount = bitcoinConverter.getBitcoinsFromBits(amount);
@@ -280,7 +280,7 @@ public class SendFormFragment extends AbstractFermatFragment<ReferenceAppFermatS
                             }
 
                             break;
-                        case 1:
+                        case 2:
                             text = "[bits]";
                             if (txtType.equals("[btc]")) {
                                 newAmount = bitcoinConverter.getBitsFromBTC(amount);
@@ -291,7 +291,7 @@ public class SendFormFragment extends AbstractFermatFragment<ReferenceAppFermatS
                             }
 
                             break;
-                        case 2:
+                        case 3:
                             text = "[satoshis]";
                             if (txtType.equals("[bits]")) {
                                 newAmount = bitcoinConverter.getSathoshisFromBits(amount);
