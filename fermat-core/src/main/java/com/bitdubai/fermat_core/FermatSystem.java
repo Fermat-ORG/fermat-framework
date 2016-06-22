70d20781ee2956c958d0a8d8fb3ed80273f56acb
Nerio Indriago <nerioindriago@gmail.com>
2016-06-22 18:28:19 -0400

    update activate Platform

--- fermat-core/src/main/java/com/bitdubai/fermat_core/FermatSystem.java
+++ fermat-core/src/main/java/com/bitdubai/fermat_core/FermatSystem.java
@@ -107,18 +107,18 @@ public final class FermatSystem {
         }
 
         try {
-           // fermatSystemContext.registerPlatform(new ARTPlatform());
+            fermatSystemContext.registerPlatform(new ARTPlatform());
             fermatSystemContext.registerPlatform(new BCHPlatform());
             fermatSystemContext.registerPlatform(new BNKPlatform());
-           fermatSystemContext.registerPlatform(new CBPPlatform());
+            fermatSystemContext.registerPlatform(new CBPPlatform());
             fermatSystemContext.registerPlatform(new CCPPlatform());
             fermatSystemContext.registerPlatform(new CERPlatform());
             fermatSystemContext.registerPlatform(new CHTPlatform());
             fermatSystemContext.registerPlatform(new CSHPlatform());
-           // fermatSystemContext.registerPlatform(new DAPPlatform());
+            fermatSystemContext.registerPlatform(new DAPPlatform());
             fermatSystemContext.registerPlatform(new P2PPlatform());
             fermatSystemContext.registerPlatform(new PIPPlatform());
-           // fermatSystemContext.registerPlatform(new TKYPlatform());
+            fermatSystemContext.registerPlatform(new TKYPlatform());
             fermatSystemContext.registerPlatform(new WPDPlatform());
 
         } catch(CantRegisterPlatformException e) {
