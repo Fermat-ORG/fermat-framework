ef3a3e405f25315e0f5ea02562f1ffaf0cba64d5
Nelson Ramirez <nelsonalfo@gmail.com>
2016-06-25 11:12:33 -0400

    fix

--- CBP/android/sub_app/fermat-cbp-android-sub-app-crypto-broker-community-bitdubai/src/main/java/com/bitdubai/sub_app/crypto_broker_community/common/adapters/AvailableActorsListAdapter.java
+++ CBP/android/sub_app/fermat-cbp-android-sub-app-crypto-broker-community-bitdubai/src/main/java/com/bitdubai/sub_app/crypto_broker_community/common/adapters/AvailableActorsListAdapter.java
@@ -1,20 +1,16 @@
 package com.bitdubai.sub_app.crypto_broker_community.common.adapters;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
 import com.bitdubai.fermat_android_api.ui.holders.FermatViewHolder;
-import com.bitdubai.fermat_api.layer.actor_connection.common.enums.ConnectionState;
 import com.bitdubai.fermat_cbp_api.layer.sub_app_module.crypto_broker_community.interfaces.CryptoBrokerCommunityInformation;
 import com.bitdubai.sub_app.crypto_broker_community.R;
-import com.bitdubai.sub_app.crypto_broker_community.common.holders.AppWorldHolder;
+import com.bitdubai.sub_app.crypto_broker_community.common.holders.AvailableActorsViewHolder;
 import com.bitdubai.sub_app.crypto_broker_community.common.holders.LoadingMoreViewHolder;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -25,21 +21,21 @@ import java.util.List;
  * @author lnacosta
  * @version 1.0.0
  */
-public class AppListAdapter extends FermatAdapter<CryptoBrokerCommunityInformation, FermatViewHolder> {
+public class AvailableActorsListAdapter extends FermatAdapter<CryptoBrokerCommunityInformation, FermatViewHolder> {
 
     public static final int DATA_ITEM = 1;
     public static final int LOADING_ITEM = 2;
     private boolean loadingData = true;
 
 
-    public AppListAdapter(Context context, List<CryptoBrokerCommunityInformation> dataSet) {
+    public AvailableActorsListAdapter(Context context, List<CryptoBrokerCommunityInformation> dataSet) {
         super(context, dataSet);
     }
 
     @Override
     protected FermatViewHolder createHolder(View itemView, int type) {
         if (type == DATA_ITEM)
-            return new AppWorldHolder(itemView, type);
+            return new AvailableActorsViewHolder(itemView, type);
         if (type == LOADING_ITEM)
             return new LoadingMoreViewHolder(itemView, type);
         return null;
@@ -52,9 +48,9 @@ public class AppListAdapter extends FermatAdapter<CryptoBrokerCommunityInformati
 
     protected int getCardViewResource(int type) {
         if (type == DATA_ITEM)
-            return R.layout.cbc_row_connections_world;
+            return R.layout.cbc_fragment_browser_tab_list_item;
         if (type == LOADING_ITEM)
-            return R.layout.loading_more_list_item;
+            return R.layout.cbc_view_loading_more_list_item;
         return 0;
     }
 
@@ -66,7 +62,7 @@ public class AppListAdapter extends FermatAdapter<CryptoBrokerCommunityInformati
 
     @Override
     public void onBindViewHolder(FermatViewHolder holder, int position) {
-        if (holder instanceof AppWorldHolder)
+        if (holder instanceof AvailableActorsViewHolder)
             super.onBindViewHolder(holder, position);
 
         else if (holder instanceof LoadingMoreViewHolder) {
