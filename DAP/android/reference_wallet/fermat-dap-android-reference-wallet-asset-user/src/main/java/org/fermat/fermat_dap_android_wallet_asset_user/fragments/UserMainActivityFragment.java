012ee30a6d300facdecef12548e9a8b23354d7ca
Nerio Indriago <nerioindriago@gmail.com>
2016-06-30 16:59:22 -0400

    update DAP wallets Search

--- DAP/android/reference_wallet/fermat-dap-android-reference-wallet-asset-user/src/main/java/org/fermat/fermat_dap_android_wallet_asset_user/fragments/UserMainActivityFragment.java
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-asset-user/src/main/java/org/fermat/fermat_dap_android_wallet_asset_user/fragments/UserMainActivityFragment.java
@@ -12,7 +12,7 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
+import android.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
