77c3329d8f70925a115c39ea99122e6500072af1
Nerio Indriago <nerioindriago@gmail.com>
2016-07-02 13:39:35 -0400

    update elements optionsMenu with new Searcher

--- CCP/android/reference_wallet/fermat-ccp-android-wallet-fermat-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/fermat_wallet/app_connection/FermatWalletSearcher.java
+++ CCP/android/reference_wallet/fermat-ccp-android-wallet-fermat-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/fermat_wallet/app_connection/FermatWalletSearcher.java
@@ -1,5 +1,8 @@
 package com.bitdubai.reference_niche_wallet.fermat_wallet.app_connection;
 
+import android.content.Context;
+
+import com.bitdubai.android_fermat_ccp_wallet_fermat.R;
 import com.bitdubai.fermat_android_api.core.ResourceSearcher;
 
 /**
@@ -7,4 +10,24 @@ import com.bitdubai.fermat_android_api.core.ResourceSearcher;
  */
 public class FermatWalletSearcher extends ResourceSearcher {
 
+    @Override
+    public int obtainResDrawable(Context context, int id) {
+        int resId = 0;
+        switch (id){
+            case 1:
+                resId = R.drawable.background_navigation_drawer;
+                break;
+            case 2:
+                resId = R.drawable.fw_help_icon;
+                break;
+            case 4:
+                resId = R.drawable.fmt_icon_withdrawall;
+                break;
+            case 3:
+                resId = R.drawable.fw_withdrawall_icon;
+                break;
+
+        }
+        return resId;
+    }
 }
--- DAP/android/sub_app/fermat-dap-android-sub-app-redeem-point-community/src/main/java/org/fermat/fermat_dap_android_sub_app_redeem_point_community/app_connection/CommunityRedeemPointFermatAppConnection.java
+++ DAP/android/sub_app/fermat-dap-android-sub-app-redeem-point-community/src/main/java/org/fermat/fermat_dap_android_sub_app_redeem_point_community/app_connection/CommunityRedeemPointFermatAppConnection.java
@@ -2,6 +2,7 @@ package org.fermat.fermat_dap_android_sub_app_redeem_point_community.app_connect
 
 import android.content.Context;
 
+import com.bitdubai.fermat_android_api.core.ResourceSearcher;
 import com.bitdubai.fermat_android_api.engine.FermatFragmentFactory;
 import com.bitdubai.fermat_android_api.engine.FooterViewPainter;
 import com.bitdubai.fermat_android_api.engine.HeaderViewPainter;
@@ -16,7 +17,6 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
-import com.bitdubai.fermat_dap_android_sub_app_redeem_point_community_bitdubai.R;
 
 import org.fermat.fermat_dap_android_sub_app_redeem_point_community.factory.AssetRedeemPointCommunityFragmentFactory;
 import org.fermat.fermat_dap_android_sub_app_redeem_point_community.navigation_drawer.RedeemPointCommunityNavigationViewPainter;
@@ -118,18 +118,7 @@ public class CommunityRedeemPointFermatAppConnection extends AppConnections<Refe
     }
 
     @Override
-    public int getResource(int id) {
-        int resId = 0;
-
-        switch (id) {
-            case 1:
-                resId = R.drawable.ic_geolacation;
-                break;
-            case 2:
-                resId = R.drawable.ic_menu_search;
-                break;
-        }
-
-        return resId;
+    public ResourceSearcher getResourceSearcher() {
+        return new RedeemCommunitySearcher();
     }
 }
