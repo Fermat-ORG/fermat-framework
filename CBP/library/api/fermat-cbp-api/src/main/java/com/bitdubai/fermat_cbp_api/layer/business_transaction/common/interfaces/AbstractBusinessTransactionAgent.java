cedb8a5ca9977f53bfa0637d0638b2f01ea09b40
Manuel <darkpriestrelative@gmail.com>
2016-07-03 12:21:55 -0400

    REFACTOR: implement AbstractAgent in Broker Ack Offline Payment plugin.

---
+++ CBP/library/api/fermat-cbp-api/src/main/java/com/bitdubai/fermat_cbp_api/layer/business_transaction/common/interfaces/AbstractBusinessTransactionAgent.java
@@ -0,0 +1,99 @@
+package com.bitdubai.fermat_cbp_api.layer.business_transaction.common.interfaces;
+
+import com.bitdubai.fermat_api.AbstractAgent;
+import com.bitdubai.fermat_api.FermatException;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.abstract_classes.AbstractPlugin;
+import com.bitdubai.fermat_api.layer.all_definition.common.system.interfaces.error_manager.enums.UnexpectedPluginExceptionSeverity;
+import com.bitdubai.fermat_api.layer.all_definition.exceptions.InvalidParameterException;
+import com.bitdubai.fermat_cbp_api.all_definition.exceptions.UnexpectedResultReturnedFromDatabaseException;
+
+import java.text.NumberFormat;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Manuel Perez (darkpriestrelative@gmail.com) on 03/07/16.
+ */
+public abstract class AbstractBusinessTransactionAgent
+        <T extends AbstractPlugin>
+        extends AbstractAgent {
+
+    protected T pluginRoot;
+
+    /**
+     * Default constructor with parameters
+     * @param sleepTime
+     * @param timeUnit
+     * @param initDelayTime
+     * @param pluginRoot
+     */
+    public AbstractBusinessTransactionAgent(
+            long sleepTime,
+            TimeUnit timeUnit,
+            long initDelayTime,
+            T pluginRoot) {
+        super(sleepTime, timeUnit, initDelayTime);
+        this.pluginRoot = pluginRoot;
+    }
+
+    @Override
+    protected Runnable agentJob() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                doTheMainTask();
+            }
+        };
+        return runnable;
+    }
+
+    @Override
+    protected void onErrorOccur() {
+        pluginRoot.reportError(
+                UnexpectedPluginExceptionSeverity.DISABLES_SOME_FUNCTIONALITY_WITHIN_THIS_PLUGIN,
+                new Exception(this.getClass().getName()+" Error"));
+    }
+
+    /**
+     * This method must implement the main method of the agent.
+     */
+    protected abstract void doTheMainTask();
+
+    /**
+     * This method must implement the checked the pending events.
+     * @param eventId
+     * @throws UnexpectedResultReturnedFromDatabaseException
+     */
+    protected abstract void checkPendingEvent(String eventId)
+            throws UnexpectedResultReturnedFromDatabaseException;
+
+    /**
+     * This method must implement ack confirmation event.
+     * @param contractHash
+     */
+    protected abstract void raiseAckConfirmationEvent(String contractHash);
+
+    /**
+     * This method parse a String object to a long object
+     *
+     * @param stringValue
+     *
+     * @return
+     *
+     * @throws InvalidParameterException
+     */
+    protected double parseToDouble(String stringValue) throws InvalidParameterException {
+        if (stringValue == null) {
+            throw new InvalidParameterException("Cannot parse a null string value to long");
+        } else {
+            try {
+                return NumberFormat.getInstance().parse(stringValue).doubleValue();
+            } catch (Exception exception) {
+                throw new InvalidParameterException(
+                        InvalidParameterException.DEFAULT_MESSAGE,
+                        FermatException.wrapException(exception),
+                        "Parsing String object to double",
+                        "Cannot parse " + stringValue + " string value to long");
+            }
+        }
+    }
+}
