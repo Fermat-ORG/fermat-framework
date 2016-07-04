f227b17b2ee3ea2ea55becf786ca7ed1d278ef9f
Yordin Alayn <y.alayn@gmail.com>
2016-07-04 09:50:25 -0400

    CBP - Reference Wallet - Crypto Customer - Date Time Zone

--- CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/common/holders/open_negotiation/DateTimeViewHolder.java
+++ CBP/android/reference_wallet/fermat-cbp-android-reference-wallet-crypto-customer-bitdubai/src/main/java/com/bitdubai/reference_wallet/crypto_customer_wallet/common/holders/open_negotiation/DateTimeViewHolder.java
@@ -5,10 +5,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.bitdubai.fermat_cbp_api.all_definition.enums.ClauseType;
 import com.bitdubai.fermat_cbp_api.all_definition.enums.NegotiationStepStatus;
+import com.bitdubai.fermat_cbp_api.all_definition.util.DateTimeZone;
 import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.ClauseInformation;
 import com.bitdubai.fermat_cbp_api.layer.wallet_module.common.interfaces.CustomerBrokerNegotiationInformation;
 import com.bitdubai.reference_wallet.crypto_customer_wallet.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.TimeZone;
 
 /**
  *Created by Yordin Alayn on 22.01.16.
@@ -19,7 +27,8 @@ public class DateTimeViewHolder extends ClauseViewHolder implements View.OnClick
     private Button buttonDate;
     private Button buttonTime;
     private TextView descriptionTextView;
-
+    private TextView youTimeZone;
+    private TextView otheTimeZone;
 
     public DateTimeViewHolder(View itemView) {
         super(itemView);
@@ -30,6 +39,9 @@ public class DateTimeViewHolder extends ClauseViewHolder implements View.OnClick
         buttonDate.setOnClickListener(this);
         buttonTime = (Button) itemView.findViewById(R.id.ccw_time_value);
         buttonTime.setOnClickListener(this);
+        youTimeZone = (TextView) itemView.findViewById(R.id.ccw_text_you_time_zone);
+        otheTimeZone = (TextView) itemView.findViewById(R.id.ccw_text_other_date);
+
     }
 
     @Override
@@ -40,8 +52,48 @@ public class DateTimeViewHolder extends ClauseViewHolder implements View.OnClick
         java.text.DateFormat dateFormat = DateFormat.getDateFormat(itemView.getContext());
 
         long timeInMillis = Long.valueOf(clause.getValue());
+
         buttonTime.setText(timeFormat.format(timeInMillis));
         buttonDate.setText(dateFormat.format(timeInMillis));
+
+        Map<ClauseType, ClauseInformation> clauses = data.getClauses();
+        ClauseInformation otherTimeZoneClause = clauses.get(ClauseType.BROKER_TIME_ZONE);
+
+        if(otherTimeZoneClause != null) {
+
+            String otheTimeZoneValue = "Undefined";
+
+            String youTimeZoneValue = TimeZone.getDefault().getID();
+            otheTimeZoneValue = otherTimeZoneClause.getValue();
+
+            if(!youTimeZoneValue.equals(otheTimeZoneValue)) {
+
+//                String dateTime = dateFormat.format(timeInMillis) + " " + timeFormat.format(timeInMillis);
+                youTimeZone.setVisibility(View.VISIBLE);
+                otheTimeZone.setVisibility(View.VISIBLE);
+
+                DateTimeZone dateTimeZoneOther = new DateTimeZone(otheTimeZoneValue, timeInMillis, "MM/dd/yyyy hh:mm a");
+
+                youTimeZone.setText(youTimeZoneValue);
+//                otheTimeZone.setText("Broker Date: " + getDateTimeOther(otheTimeZoneValue, dateTime) + " ( " + otheTimeZoneValue + " )");
+                otheTimeZone.setText("Broker Date: " + dateTimeZoneOther.getDate() + " ( " + otheTimeZoneValue + " )");
+
+            }
+
+
+//            DateTimeZone dateTimeZoneDelivery = new DateTimeZone(TimeZone.getDefault().getID(),timeInMillis,"MM/dd/yyyy hh:mm a");
+//            String dateTimeDelivery = dateTimeZoneDelivery.getDate();
+//            String dateTimeDeliveryUTC = dateTimeZoneDelivery.getDateUTC();
+//            String dateTimeToday    = dateTimeZoneDelivery.getDateTodayUTC();
+//
+//            System.out.print("\n *** TIME ZONE " +
+//                    "\n - Date:" +dateTimeDelivery+
+//                    "\n - Date UTC:" +dateTimeDeliveryUTC+
+//                    "\n - DateToday: "+dateTimeToday+
+//                    "\n - CompareTo: " + dateTimeDelivery.compareTo(dateTimeToday)+"\n");
+
+        }
+
     }
 
     @Override
@@ -49,6 +101,7 @@ public class DateTimeViewHolder extends ClauseViewHolder implements View.OnClick
         titleTextView.setText(titleRes);
         clauseNumberImageView.setImageResource(positionImgRes);
         descriptionTextView.setText(stringResources[0]);
+
     }
 
     @Override
@@ -88,4 +141,28 @@ public class DateTimeViewHolder extends ClauseViewHolder implements View.OnClick
                 break;
         }
     }
+
+    /*private String getDateTimeOther(String timeZone, String date){
+
+        String dateTime = "";
+        String formatDate = "MM/dd/yyyy hh:mm a";
+
+        try {
+            SimpleDateFormat sourceFormat = new SimpleDateFormat(formatDate);
+            sourceFormat.setTimeZone(TimeZone.getDefault());
+            Date parsed = sourceFormat.parse(date);
+
+            TimeZone tz = TimeZone.getTimeZone(timeZone);
+            SimpleDateFormat destFormat = new SimpleDateFormat(formatDate);
+            destFormat.setTimeZone(tz);
+
+            dateTime = destFormat.format(parsed);
+
+        } catch (ParseException e){
+            System.out.print("error.");
+        }
+
+        return dateTime;
+
+    }*/
 }
