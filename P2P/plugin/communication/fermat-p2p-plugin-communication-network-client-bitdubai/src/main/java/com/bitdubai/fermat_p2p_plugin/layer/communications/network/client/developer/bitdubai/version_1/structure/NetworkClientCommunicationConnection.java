c0cca6bd5766e62047ce44486db8de89d531a23d
lnacosta <laion.cj91@gmail.com>
2016-07-06 12:13:54 -0300

    android + network node

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/client/developer/bitdubai/version_1/structure/NetworkClientCommunicationConnection.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/client/developer/bitdubai/version_1/structure/NetworkClientCommunicationConnection.java
@@ -189,7 +189,7 @@ public class NetworkClientCommunicationConnection implements NetworkClientConnec
                 if(nodesListPosition >= 0){
                     i++;
 
-                    if(i > 4){
+                    if(i > 1){
 
                         pluginRoot.intentToConnectToOtherNode(nodesListPosition);
                         return Boolean.FALSE;
@@ -211,7 +211,7 @@ public class NetworkClientCommunicationConnection implements NetworkClientConnec
                 if(nodesListPosition >= 0){
                     i++;
 
-                    if(i > 4){
+                    if(i > 1){
 
                         pluginRoot.intentToConnectToOtherNode(nodesListPosition);
                         return Boolean.FALSE;
@@ -222,7 +222,7 @@ public class NetworkClientCommunicationConnection implements NetworkClientConnec
 
                             //System.out.println("# NetworkClientCommunicationConnection - Reconnect Failure Message: "+exception.getMessage()+" Cause: "+exception.getCause());
                             // To avoid potential DDoS when you don't limit number of reconnects, wait to the next try.
-                            Thread.sleep(5000);
+                            Thread.sleep(1000);
 
                         } catch (InterruptedException e) {
                             e.printStackTrace();
@@ -236,7 +236,7 @@ public class NetworkClientCommunicationConnection implements NetworkClientConnec
 
                         //System.out.println("# NetworkClientCommunicationConnection - Reconnect Failure Message: "+exception.getMessage()+" Cause: "+exception.getCause());
                         // To avoid potential DDoS when you don't limit number of reconnects, wait to the next try.
-                        Thread.sleep(5000);
+                        Thread.sleep(1000);
 
                     } catch (InterruptedException e) {
                         e.printStackTrace();
