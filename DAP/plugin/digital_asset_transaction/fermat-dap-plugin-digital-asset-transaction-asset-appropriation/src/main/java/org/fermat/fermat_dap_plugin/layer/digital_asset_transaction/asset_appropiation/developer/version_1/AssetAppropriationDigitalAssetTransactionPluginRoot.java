9951b638610394cb7a0432de5d2be165d7077758
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-07-07 16:45:45 -0300

    refactor in DAP - applied changes to use BlockchainManager interface

--- DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-appropriation/src/main/java/org/fermat/fermat_dap_plugin/layer/digital_asset_transaction/asset_appropiation/developer/version_1/AssetAppropriationDigitalAssetTransactionPluginRoot.java
+++ DAP/plugin/digital_asset_transaction/fermat-dap-plugin-digital-asset-transaction-asset-appropriation/src/main/java/org/fermat/fermat_dap_plugin/layer/digital_asset_transaction/asset_appropiation/developer/version_1/AssetAppropriationDigitalAssetTransactionPluginRoot.java
@@ -32,7 +32,8 @@ import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogLevel;
 import com.bitdubai.fermat_api.layer.osa_android.logger_system.LogManager;
 import com.bitdubai.fermat_bch_api.layer.crypto_module.crypto_address_book.interfaces.CryptoAddressBookManager;
-import com.bitdubai.fermat_bch_api.layer.crypto_network.bitcoin.interfaces.BitcoinNetworkManager;
+
+import com.bitdubai.fermat_bch_api.layer.crypto_network.manager.BlockchainManager;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.asset_vault.interfaces.AssetVaultManager;
 import com.bitdubai.fermat_bch_api.layer.crypto_vault.currency_vault.CryptoVaultManager;
 import com.bitdubai.fermat_ccp_api.layer.actor.extra_user.interfaces.ExtraUserManager;
@@ -106,7 +107,7 @@ public class AssetAppropriationDigitalAssetTransactionPluginRoot extends Abstrac
     private AssetUserWalletManager assetUserWalletManager;
 
     @NeededPluginReference(platform = Platforms.BLOCKCHAINS, layer = Layers.CRYPTO_NETWORK, plugin = Plugins.BITCOIN_NETWORK)
-    private BitcoinNetworkManager bitcoinNetworkManager;
+    private BlockchainManager bitcoinNetworkManager;
 
     @NeededPluginReference(platform = Platforms.BLOCKCHAINS, layer = Layers.CRYPTO_MODULE, plugin = Plugins.CRYPTO_ADDRESS_BOOK)
     private CryptoAddressBookManager cryptoAddressBookManager;
