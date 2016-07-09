ec04ab5d3b02380d4f5b66fdc98ca04e3b48181a
alejandro <abicelis@gmail.com>
2016-07-08 21:48:28 -0400

    BETA TESTING  - misc

--- WPD/plugin/engine/fermat-wpd-plugin-engine-app-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/provisory/SubAppAppsGenerator.java
+++ WPD/plugin/engine/fermat-wpd-plugin-engine-app-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/provisory/SubAppAppsGenerator.java
@@ -3782,7 +3782,7 @@ public class SubAppAppsGenerator {
         optionMenuItem = new OptionMenuItem(LOCATION_FILTER_OPTION_MENU_ID);
         optionMenuItem.setFermatDrawable(new FermatDrawable(LOCATION_FILTER_OPTION_MENU_ID, "location", owner, SourceLocation.DEVELOPER_RESOURCES));
         optionMenuItem.setLabel("Location");
-        optionMenuItem.setShowAsAction(OptionMenuItem.SHOW_AS_ACTION_IF_ROOM);
+        optionMenuItem.setShowAsAction(OptionMenuItem.SHOW_AS_ACTION_NEVER);
         optionsMenu.addMenuItem(optionMenuItem);
 
         optionMenuItem = new OptionMenuItem(HELP_OPTION_MENU_ID);
