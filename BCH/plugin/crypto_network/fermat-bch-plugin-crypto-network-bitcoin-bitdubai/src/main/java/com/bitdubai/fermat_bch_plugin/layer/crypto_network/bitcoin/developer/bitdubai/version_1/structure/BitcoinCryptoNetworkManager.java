7ca8ced0f7fe9735f433e4b1259f7813b01b1f05
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-07-21 17:14:40 -0300

    Added more controls to make sure we get the wallet or throw an error.

--- BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/bitcoin/developer/bitdubai/version_1/structure/BitcoinCryptoNetworkManager.java
+++ BCH/plugin/crypto_network/fermat-bch-plugin-crypto-network-bitcoin-bitdubai/src/main/java/com/bitdubai/fermat_bch_plugin/layer/crypto_network/bitcoin/developer/bitdubai/version_1/structure/BitcoinCryptoNetworkManager.java
@@ -371,7 +371,7 @@ public class BitcoinCryptoNetworkManager  implements TransactionProtocolManager 
      *
      * @return
      */
-    private Wallet getWallet(BlockchainNetworkType blockchainNetworkType, @Nullable List<ECKey> keyList) throws UnreadableWalletException {
+    private synchronized Wallet getWallet(BlockchainNetworkType blockchainNetworkType, @Nullable List<ECKey> keyList) throws UnreadableWalletException {
         Wallet wallet = null;
         File walletFile = new File(WALLET_PATH, blockchainNetworkType.getCode());
 
@@ -379,7 +379,12 @@ public class BitcoinCryptoNetworkManager  implements TransactionProtocolManager 
 
         // if the wallet file exists, I will get it from the Network Monitor
         if (walletFile.exists()){
-            BitcoinCryptoNetworkMonitor monitor = runningAgents.get(blockchainNetworkType);
+            BitcoinCryptoNetworkMonitor monitor = null;
+            try {
+                monitor = getMonitor(blockchainNetworkType);
+            } catch (NetworkMonitorIsNotRunningException e) {
+                //if there was an error, I will continue and try to get the wallet from file.
+            }
 
             if (monitor != null)
                 wallet = monitor.getWallet();
