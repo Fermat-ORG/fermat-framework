1b38dbb6bf0c2d5d78180f6673b459b1a6b8f256
RoyGF <roygufo@gmail.com>
2016-07-25 15:06:19 -0500

    Layout update 2

--- WPD/plugin/engine/fermat-wpd-plugin-engine-app-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/provisory/SubAppAppsGenerator.java
+++ WPD/plugin/engine/fermat-wpd-plugin-engine-app-runtime-bitdubai/src/main/java/com/bitdubai/fermat_wpd_plugin/layer/engine/wallet_runtime/developer/bitdubai/version_1/provisory/SubAppAppsGenerator.java
@@ -4445,7 +4445,7 @@ public class SubAppAppsGenerator {
         runtimeActivity.setStatusBar(statusBar);
 
         runtimeTitleBar = new TitleBar();
-        runtimeTitleBar.setLabel("Contact Profile");
+        runtimeTitleBar.setLabel("Contact Status");
         runtimeTitleBar.setLabelSize(16);
         runtimeTitleBar.setTitleColor("#FFFFFF");
         runtimeTitleBar.setIsTitleTextStatic(true);
