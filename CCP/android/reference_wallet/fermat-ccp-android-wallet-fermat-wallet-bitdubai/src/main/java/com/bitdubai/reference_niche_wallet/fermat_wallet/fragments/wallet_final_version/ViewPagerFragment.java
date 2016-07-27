59ed364e43347fcdbe77df8fdfac7c0179917bda
gianelo <gianbarboza11@gmail.com>
2016-07-27 15:18:36 -0400

    #6279 minor Changes

--- CCP/android/reference_wallet/fermat-ccp-android-wallet-fermat-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/fermat_wallet/fragments/wallet_final_version/ViewPagerFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-wallet-fermat-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/fermat_wallet/fragments/wallet_final_version/ViewPagerFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -104,6 +105,13 @@ public class ViewPagerFragment extends AbstractFermatFragment<ReferenceAppFermat
                  lstCurrencies);
 
          lstCurrencyView.setAdapter(arrayAdapter);
+
+         lstCurrencyView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 
+             }
+         });
          //StringWheelAdapter wheelAdapter = new StringWheelAdapter(lstCurrencies);
          //wheel.setAdapter(wheelAdapter);
 
