839dae2712e7ae16f2a1fb189198995d0c08fce3
Gabriel Araujo <gabe_512@hotmail.com>
2016-08-03 01:46:03 -0400

    Discommented container reconnect handler to handler connection lost as server down.
    Signed-off-by: Gabriel Araujo <gabe_512@hotmail.com>

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/client/developer/bitdubai/version_1/NetworkClientCommunicationPluginRoot.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-client-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/client/developer/bitdubai/version_1/NetworkClientCommunicationPluginRoot.java
@@ -273,6 +273,7 @@ public class NetworkClientCommunicationPluginRoot extends AbstractPlugin impleme
                     System.out.println("########################################\n");
                     if(deviceNetwork.getType() == ConnectionType.WI_FI || deviceNetwork.getType() == ConnectionType.MOBILE_DATA )
                         try {
+                            networkClientCommunicationConnection.setTryToReconnect(Boolean.TRUE);
                             connect();
                         }catch (Exception e){
                             e.printStackTrace();
@@ -289,6 +290,7 @@ public class NetworkClientCommunicationPluginRoot extends AbstractPlugin impleme
                             @Override
                             public void run() {
                                 try {
+                                    networkClientCommunicationConnection.setTryToReconnect(Boolean.FALSE);
                                     disconnect();
                                 } catch (Exception e) {
                                     e.printStackTrace();
