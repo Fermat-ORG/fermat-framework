e13651c51a642552fce6e4da3239386359448148
natalia <natalia_veronica_c@hotmail.com>
2016-08-09 14:30:05 -0300

    Merge branch 'fermatWallet' of https://github

--- fermat-api/src/main/java/com/bitdubai/fermat_api/AbstractAgent.java
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/AbstractAgent.java
@@ -4,6 +4,7 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.AgentStatus;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -11,32 +12,46 @@ import java.util.concurrent.TimeUnit;
  */
 public abstract class AbstractAgent {
 
-    protected AgentStatus status;
+    protected AgentStatus status = AgentStatus.CREATED;
 
     private long sleepTime;
     private TimeUnit timeUnit;
     private long initDelayTime = 0;
     private String threadName;
+    private boolean isDaemon;
     private ScheduledExecutorService scheduledExecutorService;
+    private int threadPriority;
 
     public AbstractAgent(long sleepTime, TimeUnit timeUnit) {
         this.sleepTime = sleepTime;
         this.timeUnit = timeUnit;
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     }
 
     public AbstractAgent(long sleepTime, TimeUnit timeUnit, long initDelayTime) {
-        this.sleepTime = sleepTime;
-        this.timeUnit = timeUnit;
+        this(sleepTime, timeUnit);
         this.initDelayTime = initDelayTime;
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public AbstractAgent(final String threadName,long sleepTime, TimeUnit timeUnit, long initDelayTime) {
+        this(sleepTime,timeUnit,initDelayTime);
+        this.threadName = threadName;
     }
 
 
     public void start() throws CantStartAgentException {
         if (isRunning())
             throw new CantStartAgentException("Agent is already running, first use stop() method to reset");
-        this.scheduledExecutorService.scheduleWithFixedDelay(agentJob(), initDelayTime, sleepTime, timeUnit);
+        else initExecutor();
+        this.scheduledExecutorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    agentJob();
+                }catch (Exception e){
+                    onErrorOccur(e);
+                }
+            }
+        }, initDelayTime, sleepTime, timeUnit);
         this.status = AgentStatus.STARTED;
     }
 
@@ -58,12 +73,38 @@ public abstract class AbstractAgent {
         return status == AgentStatus.STOPPED;
     }
 
+    public void setIsDaemon(boolean isDaemon) {
+        this.isDaemon = isDaemon;
+    }
+
+    public void setThreadPriority(int threadPriority) {
+        this.threadPriority = threadPriority;
+    }
+
+    private void initExecutor(){
+        if(scheduledExecutorService==null) {
+            ThreadFactory threadFactory = new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable r) {
+                    Thread thread = new Thread(r);
+                    if(threadName!=null) thread.setName(threadName);
+                    if (isDaemon)thread.setDaemon(true);
+                    if (threadPriority!=0)thread.setPriority(threadPriority);
+                    return thread;
+                }
+            };
+            scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(threadFactory);
+        }
+    }
+
     /**
      * In this method developer will have to implement the runnable used in the agent
      *
      * @return
      */
-    protected abstract Runnable agentJob();
+    protected abstract void agentJob();
 
-    protected abstract void onErrorOccur();
+    protected abstract void onErrorOccur(Exception e);
+
+
 }
