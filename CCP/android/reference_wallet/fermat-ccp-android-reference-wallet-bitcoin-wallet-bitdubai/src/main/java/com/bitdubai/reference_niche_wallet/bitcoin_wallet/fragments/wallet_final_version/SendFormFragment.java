a6138f02062f145026dd29e1540a6df9463d0983
natalia <natalia_veronica_c@hotmail.com>
2016-08-16 14:39:00 -0300

    Bitcoin Wallet changes #153

--- CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendFormFragment.java
+++ CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/fragments/wallet_final_version/SendFormFragment.java
@@ -905,6 +905,7 @@ public class SendFormFragment extends AbstractFermatFragment<ReferenceAppFermatS
 
     @Override
     public void onDestroy() {
+        appSession.setData("LastContactSelected",null);
         contactsAdapter = null;
         InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
