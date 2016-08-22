2f062ca93b172e53ed511302dc099f6490658d5b
gianelo <gianbarboza11@gmail.com>
2016-08-22 11:20:31 -0400

    empty grafics bitcoin wallet

--- CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/common/adapters/ReceivetransactionsExpandableAdapter.java
+++ CCP/android/reference_wallet/fermat-ccp-android-reference-wallet-bitcoin-wallet-bitdubai/src/main/java/com/bitdubai/reference_niche_wallet/bitcoin_wallet/common/adapters/ReceivetransactionsExpandableAdapter.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 
 import com.bitdubai.android_fermat_ccp_wallet_bitcoin.R;
 import com.bitdubai.fermat_android_api.ui.expandableRecicler.ExpandableRecyclerAdapter;
-import com.bitdubai.fermat_ccp_api.layer.actor.Actor;
 import com.bitdubai.fermat_ccp_api.layer.wallet_module.crypto_wallet.interfaces.CryptoWalletTransaction;
 import com.bitdubai.reference_niche_wallet.bitcoin_wallet.common.holders.GrouperViewHolder;
 import com.bitdubai.reference_niche_wallet.bitcoin_wallet.common.holders.TransactionViewHolder;
