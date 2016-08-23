68ce9155e37ab5662384a6bb6bbe4445ba8ddc8a
Jose Cardozo <josejcb89@gmail.com>
2016-08-23 00:21:23 -0500

    performance improvements

--- CHT/android/sub_app/fermat-cht-android-sub-app-chat-bitdubai/src/main/java/com/bitbudai/fermat_cht_android_sub_app_chat_bitdubai/adapters/ChatAdapterView.java
+++ CHT/android/sub_app/fermat-cht-android-sub-app-chat-bitdubai/src/main/java/com/bitbudai/fermat_cht_android_sub_app_chat_bitdubai/adapters/ChatAdapterView.java
@@ -112,7 +112,6 @@ public class ChatAdapterView extends LinearLayout {
     int CounterText;
     Boolean isOnline = false;
     Boolean textNeverChange = false;
-    static final int TIME_TO_REFRESH_TOOLBAR = 6000;
 
     public ChatAdapterView(Context context, ArrayList<ChatMessage> chatHistory,
                            ChatManager chatManager, ChatModuleManager moduleManager,
@@ -259,7 +258,7 @@ public class ChatAdapterView extends LinearLayout {
             }
 
 
-            if (adapter == null) {
+            if (adapter == null || adapter.chatMessages==null) {
                 adapter = new ChatAdapter(this.getContext(), (chatHistory != null) ? chatHistory : new ArrayList<ChatMessage>());
                 messagesContainer.setAdapter(adapter);
             } else {
@@ -541,36 +540,6 @@ public class ChatAdapterView extends LinearLayout {
                 }
             }
         }
-        //companionLabel.setText(leftName);
-//        } else {
-//            companionLabel.setText("Contacto");
-//        }
-
-        //if (background != -1) {
-        //    container.setBackgroundColor(background);
-        //}
-
-        messageET.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //messageET.setText("");
-            }
-        });
-
-//        messageET.setOnTouchListener(new OnTouchListener() {
-//            public boolean onTouch(View view, MotionEvent event) {
-//                // TODO Auto-generated method stub
-//                if (view.getId() == R.id.messageEdit) {
-//                    view.getParent().requestDisallowInterceptTouchEvent(true);
-//                    switch (event.getAction() & MotionEvent.ACTION_MASK) {
-//                        case MotionEvent.ACTION_UP:
-//                            view.getParent().requestDisallowInterceptTouchEvent(false);
-//                            break;
-//                    }
-//                }
-//                return false;
-//            }
-//        });
 
         sendBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -683,7 +652,7 @@ public class ChatAdapterView extends LinearLayout {
                     chatMessage.setDate(S);
                     chatMessage.setMe(true);
                     messageET.setText("");
-                    if (adapter == null) {
+                    if (adapter == null || adapter.chatMessages==null) {
                         adapter = new ChatAdapter(getContext(), (chatHistory != null) ? chatHistory : new ArrayList<ChatMessage>());
                         messagesContainer.setAdapter(adapter);
                     }
