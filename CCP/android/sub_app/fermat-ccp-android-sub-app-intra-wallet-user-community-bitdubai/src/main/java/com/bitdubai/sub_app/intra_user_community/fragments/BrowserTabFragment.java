06435775616b5156b368101f13c8bd4629288d4a
natalia <natalia_veronica_c@hotmail.com>
2016-09-01 23:25:59 -0300

    CCP Community changes

--- CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/BrowserTabFragment.java
+++ CCP/android/sub_app/fermat-ccp-android-sub-app-intra-wallet-user-community-bitdubai/src/main/java/com/bitdubai/sub_app/intra_user_community/fragments/BrowserTabFragment.java
@@ -66,6 +66,7 @@ import com.bitdubai.fermat_ccp_api.layer.module.intra_user.exceptions.CantGetAct
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserInformation;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserLoginIdentity;
 import com.bitdubai.fermat_ccp_api.layer.module.intra_user.interfaces.IntraUserModuleManager;
+import com.bitdubai.fermat_cht_api.layer.sup_app_module.interfaces.chat_actor_community.interfaces.ChatActorCommunityInformation;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.enums.ProfileStatus;
 import com.bitdubai.fermat_pip_api.layer.external_api.geolocation.interfaces.ExtendedCity;
 import com.bitdubai.sub_app.intra_user_community.R;
@@ -450,7 +451,7 @@ public class BrowserTabFragment
 
 
             if ((data.getState().equals(ProfileStatus.ONLINE) || data.getState().equals(ProfileStatus.UNKNOWN)) &&
-                connectionState.equals(ConnectionState.NO_CONNECTED)) {
+                !connectionState.equals(ConnectionState.PENDING_REMOTELY_ACCEPTANCE)) {
 
                 if (moduleManager.getActiveIntraUserIdentity() != null) {
                     if (!moduleManager.getActiveIntraUserIdentity().getPublicKey().isEmpty())
@@ -554,14 +555,16 @@ public class BrowserTabFragment
     @Override
     public void onLoadMoreData(int page, final int totalItemsCount) {
         adapter.setLoadingData(true);
-        fermatWorker = new FermatWorker(getActivity(), this) {
-            @Override
-            protected Object doInBackground() throws Exception {
-                return getMoreDataAsync(FermatRefreshTypes.NEW, totalItemsCount);
-            }
-        };
+       // fermatWorker = new FermatWorker(getActivity(), this) {
+         //   @Override
+          //  protected Object doInBackground() throws Exception {
+                offset = totalItemsCount;
+                onRefresh();
+                //return getMoreDataAsync(FermatRefreshTypes.NEW, totalItemsCount);
+           // }
+        //};
 
-        fermatWorker.execute();
+       // fermatWorker.execute();
     }
 
     @Override
@@ -673,37 +676,40 @@ public class BrowserTabFragment
     public void onActorReceived(final List<IntraUserInformation> result) {
         try {
             if (isAttached) {
-                swipeRefreshLayout.setRefreshing(false);
-                adapter.setLoadingData(false);
-                if (result != null && result.size() > 0) {
+                if (result != null /*&& result.size() > 0*/) {
                     if (getActivity() != null && adapter != null) {
                         if (offset == 0) {
                             if (lstIntraUserInformations != null) {
                                 lstIntraUserInformations.clear();
                                 lstIntraUserInformations.addAll(result);
+                            } else {
+                                lstIntraUserInformations = (ArrayList)result;
                             }
-                            adapter.changeDataSet(lstIntraUserInformations);
-                            ((EndlessScrollListener) scrollListener).notifyDataSetChanged();
+                            adapter.refreshEvents(lstIntraUserInformations);
                         } else {
-
-                            lstIntraUserInformations.addAll((ArrayList) result);
+                            for (IntraUserInformation info : result) {
+                                if (notInList(info)) {
+                                    lstIntraUserInformations.add(info);
+                                }
+                            }
                             adapter.notifyItemRangeInserted(offset, lstIntraUserInformations.size() - 1);
                         }
 
                         isRefreshing = false;
-                        offset = lstIntraUserInformations.size();
+                        //offset = lstIntraUserInformations.size();
                     }
-                } else{
+                } else {
 
                     isRefreshing = false;
                 }
+
+                offset = 0;
+                showOrHideEmptyView();
             }
-            showOrHideEmptyView();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 
     /**
      * Receiver class implemented
@@ -719,6 +725,7 @@ public class BrowserTabFragment
                     if (code.equals(CommunityBroadcasterConstants.USER_COMM_ACTOR_RECEIVED)) {
                         List<IntraUserInformation> actorProfiles = (List<IntraUserInformation>) fermatBundle.get(CommunityBroadcasterConstants.USER_COMM_ACTOR_LIST);
                         onActorReceived(actorProfiles);
+
                     }
                 }
             } catch (ClassCastException e) {
@@ -919,6 +926,16 @@ public class BrowserTabFragment
             _executor.shutdownNow();
         super.onStop();
     }
+
+
+    private boolean notInList(IntraUserInformation info) {
+        for (IntraUserInformation contact : lstIntraUserInformations) {
+            if (contact.getPublicKey().equals(info.getPublicKey()))
+                return false;
+        }
+        return true;
+    }
+
 
 }
 
